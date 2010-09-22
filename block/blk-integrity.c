a3f97c458a1d9e25a866f7e2ff8da571699a495e
Martin K. Petersen <martin.petersen@oracle.com>
2010-09-22 15:33:37 -0400

    block: Disable autoprotect
    Orabug: 17370630
    
    Not all filesystems keep their pages stable during I/O leading to
    checksum failures.  Disable automatic protection.
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- block/blk-integrity.c
+++ block/blk-integrity.c
@@ -402,7 +402,7 @@ int blk_integrity_register(struct gendisk *disk, struct blk_integrity *template)
 
 		kobject_uevent(&bi->kobj, KOBJ_ADD);
 
-		bi->flags |= INTEGRITY_FLAG_READ | INTEGRITY_FLAG_WRITE;
+		bi->flags = 0;	/* Disable checking by default */
 		bi->sector_size = queue_logical_block_size(disk->queue);
 		disk->integrity = bi;
 	} else
