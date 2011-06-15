e7677425c82ff105b13929befd294e5ade076759
Kris Van Hees <kris.van.hees@oracle.com>
2011-06-15 10:41:11 -0400

    dtrace: Remove debugging statements
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- kernel/dtrace/dtrace_os.c
+++ kernel/dtrace/dtrace_os.c
@@ -41,7 +41,6 @@ static cyclic_t *cyc_alloc(void)
 
 	mutex_lock(&cyclic_lock);
 
-printk(KERN_INFO "cyc_alloc: flst [O] %p\n", cyc_flst);
 	if (cyc_flst == NULL) {
 		unsigned long	nsize = cyc_size + CHUNKSIZE;
 		unsigned long	idx = nsize;
@@ -58,21 +57,15 @@ printk(KERN_INFO "cyc_alloc: flst [O] %p\n", cyc_flst);
 
 		idx = nsize;
 		cyc_flst = &cyc_arr[cyc_size];
-printk(KERN_INFO "cyc_alloc: flst [N] %p, size [N] %lu\n", cyc_flst, nsize);
 		cyc_arr[--idx].nxt = NULL;
-printk(KERN_INFO "cyc_alloc: cyc_arr[%lu] NULL\n", idx);
 		while (idx-- > cyc_size)
-{
 			cyc_arr[idx].nxt = &cyc_arr[idx + 1];
-printk(KERN_INFO "cyc_alloc: cyc_arr[%lu] %p\n", idx, cyc_arr[idx].nxt);
-}
 
 		cyc_size = nsize;
 	}
 
 	np = cyc_flst;
 	cyc_flst = cyc_flst->nxt;
-printk(KERN_INFO "cyc_alloc: cyc %p, flst [N] %p\n", np, cyc_flst);
 
 	mutex_unlock(&cyclic_lock);
 
@@ -88,8 +81,6 @@ static enum hrtimer_restart cyclic_fire_fn(struct hrtimer *timr)
 		cyc->cyc.hdlr.cyh_func(cyc->cyc.hdlr.cyh_arg);
 
 	hrtimer_forward_now(&cyc->cyc.timr, cyc->cyc.when.cyt_interval);
-printk(KERN_INFO "cyclic_fire_fn: Cyclic %p, hrtimer %p\n", cyc, timr);
-printk(KERN_INFO "cyclic_fire_fn:   Next expiry in %lld ns (interval %lld)\n", hrtimer_expires_remaining(&cyc->cyc.timr).tv64, cyc->cyc.when.cyt_interval.tv64);
 
 	return HRTIMER_RESTART;
 }
@@ -112,20 +103,13 @@ cyclic_id_t cyclic_add(cyc_handler_t *hdlr, cyc_time_t *when)
 
 	hrtimer_init(&cyc->cyc.timr, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	cyc->cyc.timr.function = cyclic_fire_fn;
-printk(KERN_INFO "cyclic_add: Adding %p, hrtimer %p\n", cyc, &cyc->cyc.timr);
 
 	if (cyc->cyc.when.cyt_when.tv64 == 0)
-{
-printk(KERN_INFO "cyclic_add:   Starting at relative %lld\n", cyc->cyc.when.cyt_interval.tv64);
 		hrtimer_start(&cyc->cyc.timr, cyc->cyc.when.cyt_interval,
 			      HRTIMER_MODE_REL_PINNED);
-}
 	else
-{
-printk(KERN_INFO "cyclic_add:   Starting at absolute %lld\n", cyc->cyc.when.cyt_when.tv64);
 		hrtimer_start(&cyc->cyc.timr, cyc->cyc.when.cyt_when,
 			      HRTIMER_MODE_ABS_PINNED);
-}
 
 	return (cyclic_id_t)cyc;
 }
@@ -138,7 +122,6 @@ void cyclic_remove(cyclic_id_t id)
 {
 	cyclic_t	*cyc = (cyclic_t *)id;
 
-printk(KERN_INFO "cyclic_add: Removing %p, hrtimer %p\n", cyc, &cyc->cyc.timr);
 	hrtimer_cancel(&cyc->cyc.timr);
 
 	mutex_lock(&cyclic_lock);
