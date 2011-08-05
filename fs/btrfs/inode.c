d3950679b10c3dcbe224a1d5315f1f0d9a833ecf
Josef Bacik <josef@redhat.com>
2011-08-05 10:25:38 -0400

    Btrfs: kill the durable block rsv stuff
    This is confusing code and isn't used by anything anymore, so delete it.
    
    Signed-off-by: Josef Bacik <josef@redhat.com>
    (cherry picked from commit 37be25bcb6d731914e126f8de59c4367f0d66b80)

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -2016,9 +2016,6 @@ int btrfs_orphan_add(struct btrfs_trans_handle *trans, struct inode *inode)
 	}
 	spin_unlock(&root->orphan_lock);
 
-	if (block_rsv)
-		btrfs_add_durable_block_rsv(root->fs_info, block_rsv);
-
 	/* grab metadata reservation from transaction handle */
 	if (reserve) {
 		ret = btrfs_orphan_reserve_metadata(trans, inode);
@@ -6313,7 +6310,6 @@ static int btrfs_truncate(struct inode *inode)
 	rsv = btrfs_alloc_block_rsv(root);
 	if (!rsv)
 		return -ENOMEM;
-	btrfs_add_durable_block_rsv(root->fs_info, rsv);
 
 	trans = btrfs_start_transaction(root, 4);
 	if (IS_ERR(trans)) {
