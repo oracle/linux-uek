be4118542ededac479b1e554f232d5d72cd24cbd
Andrew Vasquez <andrew.vasquez@qlogic.com>
2011-09-23 15:00:21 -0700

    qla2xxx: Use consistent DL mask for ELS/CT passthru requests.
    Driver is logging a slew of 'good' status requests:
    
    	[ 9178.507535] qla2xxx [0000:19:00.1]-503f:15: ELS-CT pass-through-ct pass-through error hdl=379 comp_status-status=0x15 error subcode 1=0x1820 error subcode 2=0x81c8 total_byte = 0x10.
    	[ 9178.525950] qla2xxx [0000:19:00.1]-503f:15: ELS-CT pass-through-ct pass-through error hdl=37c comp_status-status=0x15 error subcode 1=0x1820 error subcode 2=0x81c8 total_byte = 0x10.
    	[ 9178.543461] qla2xxx [0000:19:00.1]-503f:15: ELS-CT pass-through-ct pass-through error hdl=37f comp_status-status=0x15 error subcode 1=0x1820 error subcode 2=0x81c8 total_byte = 0x10.
    	[ 9178.555836] qla2xxx [0000:19:00.1]-503f:15: ELS-CT pass-through-ct pass-through error hdl=382 comp_status-status=0x15 error subcode 1=0x1820 error subcode 2=0x81c8 total_byte = 0x10.
    
    Change from
    
    	* ql_log() -> ql_dbg()
    	* ql_log_info -> ql_dbg_user
    
    Chad/Saurav, does this require a renumbering?
    
    JIRA Key: V2632FC-76

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -1092,7 +1092,7 @@ qla24xx_els_ct_entry(scsi_qla_host_t *vha, struct req_que *req,
 			bsg_job->reply->reply_payload_rcv_len =
 			    le16_to_cpu(((struct els_sts_entry_24xx*)pkt)->total_byte_count);
 
-			ql_log(ql_log_info, vha, 0x503f,
+			ql_dbg(ql_dbg_user, vha, 0x503f,
 			    "ELS-CT pass-through-%s error hdl=%x comp_status-status=0x%x "
 			    "error subcode 1=0x%x error subcode 2=0x%x total_byte = 0x%x.\n",
 			    type, sp->handle, comp_status, fw_status[1], fw_status[2],
@@ -1102,7 +1102,7 @@ qla24xx_els_ct_entry(scsi_qla_host_t *vha, struct req_que *req,
 			memcpy( fw_sts_ptr, fw_status, sizeof(fw_status));
 		}
 		else {
-			ql_log(ql_log_info, vha, 0x5040,
+			ql_dbg(ql_dbg_user, vha, 0x5040,
 			    "ELS-CT pass-through-%s error hdl=%x comp_status-status=0x%x "
 			    "error subcode 1=0x%x error subcode 2=0x%x.\n",
 			    type, sp->handle, comp_status,
@@ -1115,7 +1115,7 @@ qla24xx_els_ct_entry(scsi_qla_host_t *vha, struct req_que *req,
 			fw_sts_ptr = ((uint8_t*)bsg_job->req->sense) + sizeof(struct fc_bsg_reply);
 			memcpy( fw_sts_ptr, fw_status, sizeof(fw_status));
 		}
-		ql_dump_buffer(ql_dbg_async + ql_dbg_buffer, vha, 0x5056,
+		ql_dump_buffer(ql_dbg_user + ql_dbg_buffer, vha, 0x5056,
 				(uint8_t *)pkt, sizeof(*pkt));
 	}
 	else {
