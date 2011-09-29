658636f4de23c591e81f393a758ad0c04cde0122
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-09-29 10:16:50 +0800

    xen/acpi/sleep: Register to the acpi_suspend_lowlevel a callback.
    We piggyback on "x86/acpi: Provide registration for acpi_suspend_lowlevel."
    to register a Xen version of the callback. The callback does not
    do anything special - except it omits the x86_acpi_suspend_lowlevel.
    It does that b/c during suspend it tries to save cr8 values (which
    the hypervisor does not support), and then on resume path the
    cr3, cr8, idt, and gdt are all resumed which clashes with what
    the hypervisor has set up for the guest.
    
    Signed-off-by: Liang Tang <liang.tang@oracle.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/acpi.h
+++ include/xen/acpi.h
@@ -44,10 +44,22 @@ int xen_acpi_notify_hypervisor_state(u8 sleep_state,
 				     u32 pm1a_cnt, u32 pm1b_cnd,
 				     bool *skip_rest);
 
+static inline int xen_acpi_suspend_lowlevel(void)
+{
+	/*
+	 * Xen will save and restore CPU context, so
+	 * we can skip that and just go straight to
+	 * the suspend.
+	 */
+	acpi_enter_sleep_state(ACPI_STATE_S3);
+	return 0;
+}
 static inline void xen_acpi_sleep_register(void)
 {
-	if (xen_initial_domain())
+	if (xen_initial_domain()) {
+		acpi_suspend_lowlevel = xen_acpi_suspend_lowlevel;
 		__acpi_override_sleep = xen_acpi_notify_hypervisor_state;
+	}
 }
 #else
 static inline void xen_acpi_sleep_register(void)
