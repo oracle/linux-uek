8d6d9718fc6de2a587c847912b230fd156d49e86
Kris Van Hees <kris.van.hees@oracle.com>
2011-10-13 22:42:27 -0400

    Stacktrace dumping has been moved to the GPL-licensed dtrace_os.c because it depends on a symbol that is exported as GPL-only. Functionality in dtrace_isa that requires stacktrace dumping can now use dtrace_stacktrace().
    The GPL-licensed dtrace_os.h C header file is now made available through the
    /include/linux hierarchy, and it is included in dtrace.h.
    
    Fixed a bug in dtrace_relocs.c where section names where copied into a memory
    area that was 1 byte short, causing various unpleasant forms of behaviour.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

--- dtrace/systrace.h
+++ dtrace/systrace.h
@@ -26,31 +26,6 @@
 
 #include "dtrace.h"
 
-typedef void (*sys_call_ptr_t)(void);
-typedef long (*dt_sys_call_t)(uintptr_t, uintptr_t, uintptr_t, uintptr_t,
-			      uintptr_t, uintptr_t);
-
-typedef struct dtrace_syscalls {
-	const char	*name;
-	dtrace_id_t	stsy_entry;
-	dtrace_id_t	stsy_return;
-	dt_sys_call_t	stsy_underlying;
-	sys_call_ptr_t	*stsy_tblent;
-} dtrace_syscalls_t;
-
-typedef void (*dtrace_systrace_probe_t)(dtrace_id_t, uintptr_t, uintptr_t,
-					uintptr_t, uintptr_t, uintptr_t,
-					uintptr_t);
-
-typedef struct systrace_info {
-	dtrace_systrace_probe_t	*probep;
-	dtrace_systrace_probe_t	stub;
-	dt_sys_call_t		syscall;
-	dtrace_syscalls_t	sysent[NR_syscalls];
-} systrace_info_t;
-
-extern systrace_info_t *dtrace_syscalls_init(void);
-
 extern void systrace_provide(void *, const dtrace_probedesc_t *);
 extern int systrace_enable(void *arg, dtrace_id_t, void *);
 extern void systrace_disable(void *arg, dtrace_id_t, void *);
