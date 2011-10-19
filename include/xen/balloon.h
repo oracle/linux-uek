971ae8c716c245d8a4cf3e5cb6b3d99540bdda44
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-10-19 14:09:54 -0400

    xen: add an "highmem" parameter to alloc_xenballooned_pages
    Add an highmem parameter to alloc_xenballooned_pages, to allow callers to
    request lowmem or highmem pages.
    
    Fix the code style of free_xenballooned_pages' prototype.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    [v1: Contextual merge conflict - self-ballooning has been already merged]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/balloon.h
+++ include/xen/balloon.h
@@ -21,5 +21,6 @@ extern struct balloon_stats balloon_stats;
 
 void balloon_set_new_target(unsigned long target);
 
-int alloc_xenballooned_pages(int nr_pages, struct page** pages);
-void free_xenballooned_pages(int nr_pages, struct page** pages);
+int alloc_xenballooned_pages(int nr_pages, struct page **pages,
+		bool highmem);
+void free_xenballooned_pages(int nr_pages, struct page **pages);
