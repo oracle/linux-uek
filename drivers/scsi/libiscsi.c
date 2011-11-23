6930b6590a3caac75f3bb65295e615035da0ff38
Vikas Chaudhary <vikas.chaudhary@qlogic.com>
2011-11-22 22:23:46 -0500

    libiscsi: Added support to show targetalias in sysfs
    sysfs patch to view target alias:
      /sys/class/iscsi_session/session*/targetalias
    
    Signed-off-by: Vikas Chaudhary <vikas.chaudhary@qlogic.com>

--- drivers/scsi/libiscsi.c
+++ drivers/scsi/libiscsi.c
@@ -2822,6 +2822,7 @@ void iscsi_session_teardown(struct iscsi_cls_session *cls_session)
 	kfree(session->username);
 	kfree(session->username_in);
 	kfree(session->targetname);
+	kfree(session->targetalias);
 	kfree(session->initiatorname);
 	kfree(session->ifacename);
 
@@ -3257,6 +3258,8 @@ int iscsi_set_param(struct iscsi_cls_conn *cls_conn,
 		return iscsi_switch_str_param(&session->password_in, buf);
 	case ISCSI_PARAM_TARGET_NAME:
 		return iscsi_switch_str_param(&session->targetname, buf);
+	case ISCSI_PARAM_TARGET_ALIAS:
+		return iscsi_switch_str_param(&session->targetalias, buf);
 	case ISCSI_PARAM_TPGT:
 		sscanf(buf, "%d", &session->tpgt);
 		break;
@@ -3322,6 +3325,9 @@ int iscsi_session_get_param(struct iscsi_cls_session *cls_session,
 		break;
 	case ISCSI_PARAM_TARGET_NAME:
 		len = sprintf(buf, "%s\n", session->targetname);
+		break;
+	case ISCSI_PARAM_TARGET_ALIAS:
+		len = sprintf(buf, "%s\n", session->targetalias);
 		break;
 	case ISCSI_PARAM_TPGT:
 		len = sprintf(buf, "%d\n", session->tpgt);
