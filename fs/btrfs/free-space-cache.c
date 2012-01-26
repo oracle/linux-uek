1860037c74b36143a6828c0d546edab64e97fd8a
Josef Bacik <josef@redhat.com>
2012-01-26 15:01:12 -0500

    Btrfs: advance window_start if we're using a bitmap
    If we span a long area in a bitmap we could end up taking a lot of time
    searching to the next free area if we're searching from the original
    window_start, so advance window_start in order to make sure we don't do any
    superficial searching.  Thanks,
    
    Signed-off-by: Josef Bacik <josef@redhat.com>
    Signed-off-by: Chris Mason <chris.mason@oracle.com>
    (cherry picked from commit 9b23062840e7c685ef0a0b561285d6e3a3b6811b)

--- fs/btrfs/free-space-cache.c
+++ fs/btrfs/free-space-cache.c
@@ -2244,6 +2244,7 @@ u64 btrfs_alloc_from_cluster(struct btrfs_block_group_cache *block_group,
 						 offset_index);
 				continue;
 			}
+			cluster->window_start += bytes;
 		} else {
 			ret = entry->offset;
 
