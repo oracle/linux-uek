818291d8116edfde5d436f4e14f8a51f80e7ece6
Chris Mason <chris.mason@oracle.com>
2012-02-03 11:07:40 -0500

    RDS: check access on pages before doing copy_to_user
    Signed-off-by: Chris Mason <chris.mason@oracle.com>
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>

--- net/rds/page.c
+++ net/rds/page.c
@@ -57,6 +57,14 @@ int rds_page_copy_user(struct page *page, unsigned long offset,
 	void *addr;
 
 	if (to_user)
+		ret = access_ok(VERIFY_WRITE, ptr, bytes);
+	else
+		ret = access_ok(VERIFY_READ, ptr, bytes);
+
+	if (!ret)
+		return -EFAULT;
+
+	if (to_user)
 		rds_stats_add(s_copy_to_user, bytes);
 	else
 		rds_stats_add(s_copy_from_user, bytes);
