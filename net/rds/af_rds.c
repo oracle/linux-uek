b74d5cda68e322bd763912ea8f9d6f1a67b92bb0
Chris Mason <chris.mason@oracle.com>
2012-02-03 11:08:51 -0500

    RDS: don't use GFP_ATOMIC for sk_alloc in rds_create
    Signed-off-by: Chris Mason <chris.mason@oracle.com>
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>

--- net/rds/af_rds.c
+++ net/rds/af_rds.c
@@ -446,7 +446,7 @@ static int rds_create(struct net *net, struct socket *sock, int protocol)
 	if (sock->type != SOCK_SEQPACKET || protocol)
 		return -ESOCKTNOSUPPORT;
 
-	sk = sk_alloc(net, AF_RDS, GFP_ATOMIC, &rds_proto);
+	sk = sk_alloc(net, AF_RDS, GFP_KERNEL, &rds_proto);
 	if (!sk)
 		return -ENOMEM;
 
