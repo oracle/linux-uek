71f1282943e69b0664c57a23fd9900d994cf4f24
Chris Mason <chris.mason@oracle.com>
2012-02-03 11:07:40 -0500

    RDS: check access on pages before doing copy_to_user
    Signed-off-by: Chris Mason <chris.mason@oracle.com>
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 818291d8116edfde5d436f4e14f8a51f80e7ece6)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit e447815caac3950f1b2de783992a291d83877925)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 8fcef7f2ed684e257db38c9e576155aeab768ef0)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/page.c
+++ net/rds/page.c
@@ -58,6 +58,14 @@ int rds_page_copy_user(struct page *page, unsigned long offset,
 	void *addr;
 
 	if (to_user)
+		ret = access_ok(VERIFY_WRITE, ptr, bytes);
+	else
+		ret = access_ok(VERIFY_READ, ptr, bytes);
+
+	if (!ret)
+		return -EFAULT;
+
+	if (to_user)
 		rds_stats_add(s_copy_to_user, bytes);
 	else
 		rds_stats_add(s_copy_from_user, bytes);
