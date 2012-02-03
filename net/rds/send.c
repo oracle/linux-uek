d2795d0eca3ad01de14fab374c42dab7aad4b7cc
Chris Mason <chris.mason@oracle.com>
2012-02-03 11:07:54 -0500

    RDS: don't trust the LL_SEND_FULL bit
    We are seeing connections stuck with the LL_SEND_FULL bit getting
    set and never cleared.  This changes RDS to stop trusting the
    LL_SEND_FULL bit and kick krdsd after any time we
    see -ENOMEM from the ring allocation code.
    
    Signed-off-by: Chris Mason <chris.mason@oracle.com>
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit aa09605ce7ef5d52d6d787e27bf7a5a9c85c5241)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 5eb93bf7e34099b3d2f99bcd19806436b58bcd57)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 8ed4d5a0be31fb564cdedff72dfff97daca1c1b4)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/send.c
+++ net/rds/send.c
@@ -1095,8 +1095,10 @@ int rds_sendmsg(struct socket *sock, struct msghdr *msg, size_t payload_len)
 	 */
 	rds_stats_inc(s_send_queued);
 
-	if (!test_bit(RDS_LL_SEND_FULL, &conn->c_flags))
-		rds_send_xmit(conn);
+	ret = rds_send_xmit(conn);
+	if (ret == -ENOMEM || ret == -EAGAIN)
+		queue_delayed_work(rds_wq, &conn->c_send_w, 1);
+
 
 	rds_message_put(rm);
 	return payload_len;
@@ -1152,8 +1154,9 @@ rds_send_pong(struct rds_connection *conn, __be16 dport)
 	rds_stats_inc(s_send_queued);
 	rds_stats_inc(s_send_pong);
 
-	if (!test_bit(RDS_LL_SEND_FULL, &conn->c_flags))
-		rds_send_xmit(conn);
+	ret = rds_send_xmit(conn);
+	if (ret == -ENOMEM || ret == -EAGAIN)
+		queue_delayed_work(rds_wq, &conn->c_send_w, 1);
 
 	rds_message_put(rm);
 	return 0;
