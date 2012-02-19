a52d76aff3d3c248c733c47a6825230ce5b86efb
Sarveshwar Bandi <sarveshwar.bandi@emulex.com>
2012-02-20 04:23:28 +0530

    be2net: query link status in be_open()
    be2net gets an async link status notification from the FW when it creates
    an MCC queue. There are some cases in which this gratuitous notification
    is not received from FW. To cover this explicitly query the link status
    in be_open().
    
    Signed-off-by: Sarveshwar Bandi <sarveshwar.bandi@emulex.com>

--- drivers/net/benet/be.h
+++ drivers/net/benet/be.h
@@ -296,6 +296,8 @@ struct be_vf_cfg {
 	u32 vf_tx_rate;
 };
 
+#define BE_FLAGS_LINK_STATUS_INIT		1
+
 struct be_adapter {
 	struct pci_dev *pdev;
 	struct net_device *netdev;
@@ -344,6 +346,7 @@ struct be_adapter {
 	struct delayed_work work;
 	u16 work_counter;
 
+	u32 flags;
 	/* Ethtool knobs and info */
 	char fw_ver[FW_VER_LEN];
 	int if_handle;		/* Used to configure filtering */
@@ -531,7 +534,7 @@ static inline bool be_error(struct be_adapter *adapter)
 
 extern void be_cq_notify(struct be_adapter *adapter, u16 qid, bool arm,
 		u16 num_popped);
-extern void be_link_status_update(struct be_adapter *adapter, u32 link_status);
+extern void be_link_status_update(struct be_adapter *adapter, u8 link_status);
 extern void be_parse_stats(struct be_adapter *adapter);
 extern int be_load_fw(struct be_adapter *adapter, u8 *func);
 #endif				/* BE_H */
