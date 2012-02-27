ac43cf08de711f8cfe6655f0879c6f60f54eed49
Chad Dupuis <chad.dupuis@qlogic.com>
2012-02-27 12:33:56 +0530

    qla2xxx: Fix byte swapping in IPE print statement.
    Signed-off-by: Saurav Kashyap <saurav.kashyap@qlogic.com>

--- drivers/scsi/qla2xxx/qla_nx.c
+++ drivers/scsi/qla2xxx/qla_nx.c
@@ -3365,7 +3365,7 @@ void qla82xx_watchdog(scsi_qla_host_t *vha)
 					    QLA82XX_CRB_PEG_NET_3 + 0x3c),
 				    qla82xx_rd_32(ha,
 					    QLA82XX_CRB_PEG_NET_4 + 0x3c));
-				if(LSW(MSB(halt_status)) == 0x67)
+				if(MSB(LSW(halt_status)) == 0x67)
 					ql_log(ql_log_warn, vha, 0xb052,
 					    "Firmware aborted with "
 					    "error code 0x00006700. Device is "
