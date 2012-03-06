ce083e01fc34a17fae57cf6ed60c29f04720b359
françois romieu <romieu@fr.zoreil.com>
2012-03-06 01:14:12 +0000

    r8169: runtime resume before shutdown.
    With runtime PM, if the ethernet cable is disconnected, the device is
    transitioned to D3 state to conserve energy. If the system is shutdown
    in this state, any register accesses in rtl_shutdown are dropped on
    the floor. As the device was programmed by .runtime_suspend() to wake
    on link changes, it is thus brought back up as soon as the link recovers.
    
    Resuming every suspended device through the driver core would slow things
    down and it is not clear how many devices really need it now.
    
    Original report and D0 transition patch by Sameer Nanda. Patch has been
    changed to comply with advices by Rafael J. Wysocki and the PM folks.
    
    Reported-by: Sameer Nanda <snanda@chromium.org>
    (cherry picked from commit 2a15cd2ff488a9fdb55e5e34060f499853b27c77)
    Signed-off-by: Francois Romieu <romieu@fr.zoreil.com>
    Cc: Rafael J. Wysocki <rjw@sisk.pl>
    Cc: Hayes Wang <hayeswang@realtek.com>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Acked-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Joe Jin <joe.jin@oracle.com>

--- drivers/net/r8169.c
+++ drivers/net/r8169.c
@@ -6246,6 +6246,9 @@ static void rtl_shutdown(struct pci_dev *pdev)
 {
 	struct net_device *dev = pci_get_drvdata(pdev);
 	struct rtl8169_private *tp = netdev_priv(dev);
+	struct device *d = &pdev->dev;
+
+	pm_runtime_get_sync(d);
 
 	rtl8169_net_suspend(dev);
 
@@ -6263,6 +6266,8 @@ static void rtl_shutdown(struct pci_dev *pdev)
 		pci_wake_from_d3(pdev, true);
 		pci_set_power_state(pdev, PCI_D3hot);
 	}
+
+	pm_runtime_put_noidle(d);
 }
 
 static struct pci_driver rtl8169_pci_driver = {
