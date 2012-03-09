2ad6fdd087f272237571ca059ee3a4934aa849dd
Josef Bacik <josef@redhat.com>
2012-03-09 16:01:49 -0500

    Btrfs: introduce free_extent_buffer_stale
    Because btrfs cow's we can end up with extent buffers that are no longer
    necessary just sitting around in memory.  So instead of evicting these pages, we
    could end up evicting things we actually care about.  Thus we have
    free_extent_buffer_stale for use when we are freeing tree blocks.  This will
    make it so that the ref for the eb being in the radix tree is dropped as soon as
    possible and then is freed when the refcount hits 0 instead of waiting to be
    released by releasepage.  Thanks,
    
    Signed-off-by: Josef Bacik <josef@redhat.com>
    (cherry picked from commit 3083ee2e18b701122a3b841db83448543a87a583)
    
    Conflicts:
    
    	fs/btrfs/ctree.c
    	fs/btrfs/extent-tree.c
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -4915,12 +4915,7 @@ static int __btrfs_free_extent(struct btrfs_trans_handle *trans,
 
 		if (is_data) {
 			ret = btrfs_del_csums(trans, root, bytenr, num_bytes);
-			if (ret)
-				goto abort;
-		} else {
-			invalidate_mapping_pages(info->btree_inode->i_mapping,
-			     bytenr >> PAGE_CACHE_SHIFT,
-			     (bytenr + num_bytes - 1) >> PAGE_CACHE_SHIFT);
+			BUG_ON(ret);
 		}
 
 		ret = update_block_group(trans, root, bytenr, num_bytes, 0);
@@ -5930,6 +5925,7 @@ struct extent_buffer *btrfs_init_new_buffer(struct btrfs_trans_handle *trans,
 	btrfs_set_buffer_lockdep_class(root->root_key.objectid, buf, level);
 	btrfs_tree_lock(buf);
 	clean_tree_block(trans, root, buf);
+	clear_bit(EXTENT_BUFFER_STALE, &buf->bflags);
 
 	btrfs_set_lock_blocking(buf);
 	btrfs_set_buffer_uptodate(buf);
