43b24c377121180083a426cda7e90915b08e676f
Or Gerlitz <ogerlitz@mellanox.com>
2012-03-20 12:53:51 +0200

    mlx4_core/ib: set the IB port MTU to 2K
    'commit 096335b3f983 ("mlx4_core: Allow dynamic MTU configuration for IB
    ports")' overwrite the default port MTU and sets it as 4K. Since this
    directly impacts the HW VLs supported and Oracle workloads heavily uses
    all supported 8 VLs for traffic classification, 2K default needs to
    be kept as is.
    
    We initilise it to default 2k so that the feature(dynamic MTU configuration)
    is still available for non DB users to set the desired MTU value using sysctl.
    
    Also for CX2 cards, commit  596c5ff4b7b3 ("net/mlx4: adjust initial
    value of vl_cap in mlx4_SET_PORT") broke the vl_cap which made the
    supported VLs to 4 irrespective of MTU size.
    
    Orabug: 24946479
    
    Tested-by: Pierre Orzechowski <pierre.e.orzechowski@oracle.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    
    Orabug: 27510469
    
    (cherry picked from commit 7bdb7f0217fa0503887565fae9531df71f7d2583)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 31621012
    
    UEK5 => UEK6
    
    (cherry picked from commit ea3cde2f52c86a216cd107606fcb5b1606c26817)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx4/port.c
    
    Some of the hunks that were supposed to revert commit 596c5ff4b7b3
    ("net/mlx4: adjust initial value of vl_cap in mlx4_SET_PORT") are
    dropped since we rejected the reverted commit in the first place.
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    
    Orabug: 31621013
    
    UEK6 => LUCI
    
    (cherry picked from commit f0feab5db8c1b3ef6440a6b79be0a815aa015535)
    cherry-pick-repo=UEK/production/linux-uek.git
    unmodified-from-upstream: f0feab5db8c1b3ef6440a6b79be0a815aa015535
    
    Signed-off-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Reviewed-by: Sharon Liu <sharon.s.liu@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com

--- drivers/net/ethernet/mellanox/mlx4/main.c
+++ drivers/net/ethernet/mellanox/mlx4/main.c
@@ -2893,10 +2893,14 @@ static int mlx4_setup_hca(struct mlx4_dev *dev)
 				}
 			}
 
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 			if (mlx4_is_mfunc(dev))
 				dev->caps.port_ib_mtu[port] = IB_MTU_2048;
 			else
 				dev->caps.port_ib_mtu[port] = IB_MTU_4096;
+#else
+			dev->caps.port_ib_mtu[port] = IB_MTU_2048;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 
 			err = mlx4_SET_PORT(dev, port, mlx4_is_master(dev) ?
 					    dev->caps.pkey_table_len[port] : -1);
