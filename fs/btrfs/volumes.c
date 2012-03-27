3b43ac8d049f0b778931fd567dcb5198e8d7a95e
Ilya Dryomov <idryomov@gmail.com>
2012-03-27 17:09:18 +0300

    Btrfs: fix infinite loop in btrfs_shrink_device()
    If relocate of block group 0 fails with ENOSPC we end up infinitely
    looping because key.offset -= 1 statement in that case brings us back to
    where we started.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit 213e64da90d14537cd63f7090d6c4d1fcc75d9f8)

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -2279,7 +2279,7 @@ again:
 	key.offset = (u64)-1;
 	key.type = BTRFS_DEV_EXTENT_KEY;
 
-	while (1) {
+	do {
 		ret = btrfs_search_slot(NULL, root, &key, path, 0, 0);
 		if (ret < 0)
 			goto done;
@@ -2321,8 +2321,7 @@ again:
 			goto done;
 		if (ret == -ENOSPC)
 			failed++;
-		key.offset -= 1;
-	}
+	} while (key.offset-- > 0);
 
 	if (failed && !retried) {
 		failed = 0;
