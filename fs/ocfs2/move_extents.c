4dbc55242647c368bc3d6bbf1a4aebdcc5ea5ad8
Al Viro <viro@zeniv.linux.org.uk>
2012-04-12 19:52:19 -0400

    ocfs2: deal with __user misannotations
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit f6a5690324d5ab9c33bbc0a6b4cc59c7fa34eeec)
    
    Signed-off-by: Xiaowei.Hu <xiaowei.hu@oracle.com>

--- fs/ocfs2/move_extents.c
+++ fs/ocfs2/move_extents.c
@@ -1083,8 +1083,7 @@ int ocfs2_ioctl_move_extents(struct file *filp, void __user *argp)
 	context->file = filp;
 
 	if (argp) {
-		if (copy_from_user(&range, (struct ocfs2_move_extents *)argp,
-				   sizeof(range))) {
+		if (copy_from_user(&range, argp, sizeof(range))) {
 			status = -EFAULT;
 			goto out;
 		}
@@ -1139,8 +1138,7 @@ out:
 	 * length and new_offset even if failure happens somewhere.
 	 */
 	if (argp) {
-		if (copy_to_user((struct ocfs2_move_extents *)argp, &range,
-				sizeof(range)))
+		if (copy_to_user(argp, &range, sizeof(range)))
 			status = -EFAULT;
 	}
 
