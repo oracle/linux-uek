432a3a3ac3bff412279bfae7f356659c3adfbf1b
Yishai Hadas <yishaih@mellanox.com>
2012-04-15 17:51:06 +0300

    ib_core: Extending umem to support contiguous memory operations.
    New structure 'cmem' represents the contiguous allocated memory.
    Allocate, Free and 'Map to virtual address' operations.
    Cleanup mechanism.
    
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -47,6 +47,37 @@
 	 ((void *) &((struct ib_umem_chunk *) 0)->page_list[1] -	\
 	  (void *) &((struct ib_umem_chunk *) 0)->page_list[0]))
 
+
+
+static void ib_cmem_release(struct kref *ref)
+{
+
+	struct ib_cmem *cmem;
+	struct ib_cmem_block *cmem_block, *tmp;
+
+	cmem = container_of(ref, struct ib_cmem, refcount);
+
+	list_for_each_entry_safe(cmem_block, tmp, &cmem->ib_cmem_block, list) {
+		__free_pages(cmem_block->page, cmem->block_order);
+		list_del(&cmem_block->list);
+		kfree(cmem_block);
+	}
+
+	kfree(cmem);
+
+}
+
+/**
+ * ib_cmem_release_contiguous_pages - release memory allocated by
+ *                                              ib_cmem_alloc_contiguous_pages.
+ * @cmem: cmem struct to release
+ */
+void ib_cmem_release_contiguous_pages(struct ib_cmem *cmem)
+{
+	kref_put(&cmem->refcount, ib_cmem_release);
+}
+EXPORT_SYMBOL(ib_cmem_release_contiguous_pages);
+
 static void __ib_umem_release(struct ib_device *dev, struct ib_umem *umem, int dirty)
 {
 	struct ib_umem_chunk *chunk, *tmp;
@@ -66,6 +97,175 @@ static void __ib_umem_release(struct ib_device *dev, struct ib_umem *umem, int d
 		kfree(chunk);
 	}
 }
+
+
+static void cmem_vma_open(struct vm_area_struct *area)
+{
+	struct ib_cmem *ib_cmem;
+	ib_cmem = (struct ib_cmem *)(area->vm_private_data);
+
+	kref_get(&ib_cmem->refcount);
+}
+
+static void cmem_vma_close(struct vm_area_struct *area)
+{
+	struct ib_cmem *cmem;
+	cmem = (struct ib_cmem *)(area->vm_private_data);
+
+	ib_cmem_release_contiguous_pages(cmem);
+
+}
+
+
+static const struct vm_operations_struct cmem_contig_pages_vm_ops = {
+	.open = cmem_vma_open,
+	.close = cmem_vma_close
+};
+
+/**
+ * ib_cmem_map_contiguous_pages_to_vma - map contiguous pages into VMA
+ * @ib_cmem: cmem structure returned by ib_cmem_alloc_contiguous_pages
+ * @vma: VMA to inject pages into.
+ */
+int ib_cmem_map_contiguous_pages_to_vma(struct ib_cmem *ib_cmem,
+						struct vm_area_struct *vma)
+{
+
+	int ret;
+	int page_entry;
+	unsigned long ntotal_pages;
+	unsigned long ncontig_pages;
+	unsigned long total_size;
+	struct page *page;
+	int vma_entry_number = 0;
+	struct ib_cmem_block *ib_cmem_block = NULL;
+
+	total_size = vma->vm_end - vma->vm_start;
+	if (ib_cmem->length != total_size)
+		return -EINVAL;
+
+	if (total_size != PAGE_ALIGN(total_size)) {
+		WARN(1,
+		"ib_cmem_map: total size %lu not aligned to page size\n",
+		total_size);
+		return -EINVAL;
+	}
+
+	ntotal_pages = total_size >> PAGE_SHIFT;
+	ncontig_pages = 1 << ib_cmem->block_order;
+
+	list_for_each_entry(ib_cmem_block, &(ib_cmem->ib_cmem_block), list) {
+		page = ib_cmem_block->page;
+		for (page_entry = 0; page_entry < ncontig_pages; page_entry++) {
+			/* We reached end of vma - going out from both loops */
+			if (vma_entry_number >= ntotal_pages)
+				goto end;
+
+			ret = vm_insert_page(vma, vma->vm_start +
+				(vma_entry_number << PAGE_SHIFT), page);
+			if (ret < 0)
+				goto err_vm_insert;
+
+			vma_entry_number++;
+			page++;
+		}
+	}
+
+end:
+
+	/* We expect to have enough pages   */
+	if (vma_entry_number >= ntotal_pages) {
+		vma->vm_ops =  &cmem_contig_pages_vm_ops;
+		vma->vm_private_data = ib_cmem;
+		return 0;
+	}
+	/* Not expected but if we reached here
+	    not enough contiguous pages were registered
+	*/
+	ret = -EINVAL;
+
+err_vm_insert:
+
+	zap_vma_ptes(vma, vma->vm_start, total_size);
+	return ret;
+
+}
+EXPORT_SYMBOL(ib_cmem_map_contiguous_pages_to_vma);
+
+
+
+/**
+ * ib_cmem_alloc_contiguous_pages - allocate contiguous pages
+*  @context: userspace context to allocate memory for
+ * @total_size: total required size for that allocation.
+ * @page_size_order: order of one contiguous page.
+ */
+struct ib_cmem *ib_cmem_alloc_contiguous_pages(struct ib_ucontext *context,
+				unsigned long total_size,
+				unsigned long page_size_order)
+{
+	struct ib_cmem *cmem;
+	unsigned long ntotal_pages;
+	unsigned long ncontiguous_pages;
+	unsigned long ncontiguous_groups;
+	struct page *page;
+	int i;
+	int ncontiguous_pages_order;
+	struct ib_cmem_block *ib_cmem_block;
+
+	if (page_size_order < PAGE_SHIFT)
+		return ERR_PTR(-EINVAL);
+
+	cmem = kmalloc(sizeof *cmem, GFP_KERNEL);
+	if (!cmem)
+		return ERR_PTR(-ENOMEM);
+
+	kref_init(&cmem->refcount);
+	cmem->context   = context;
+	INIT_LIST_HEAD(&cmem->ib_cmem_block);
+
+	/* Total size is expected to be already page aligned -
+	    verifying anyway.
+	*/
+	ntotal_pages = PAGE_ALIGN(total_size) >> PAGE_SHIFT;
+	/* How many contiguous pages do we need in 1 block */
+	ncontiguous_pages = (1 << page_size_order) >> PAGE_SHIFT;
+	ncontiguous_pages_order = ilog2(ncontiguous_pages);
+	ncontiguous_groups = (ntotal_pages >> ncontiguous_pages_order)  +
+		(!!(ntotal_pages & (ncontiguous_pages - 1)));
+
+	for (i = 0; i < ncontiguous_groups; i++) {
+		/* Allocating the managed entry */
+		ib_cmem_block = kmalloc(sizeof(struct ib_cmem_block),
+				GFP_KERNEL);
+		if (!ib_cmem_block)
+			goto err_alloc;
+
+		page =  alloc_pages(GFP_HIGHUSER | __GFP_ZERO | __GFP_COMP,
+				ncontiguous_pages_order);
+		if (!page) {
+			kfree(ib_cmem_block);
+			/* We should deallocate previous succeeded allocatations
+			     if exists.
+			*/
+			goto err_alloc;
+		}
+
+		ib_cmem_block->page = page;
+		list_add_tail(&ib_cmem_block->list, &cmem->ib_cmem_block);
+	}
+
+	cmem->block_order = ncontiguous_pages_order;
+	cmem->length = total_size;
+	return cmem;
+
+err_alloc:
+	ib_cmem_release_contiguous_pages(cmem);
+	return ERR_PTR(-ENOMEM);
+
+}
+EXPORT_SYMBOL(ib_cmem_alloc_contiguous_pages);
+
 
 /**
  * ib_umem_get - Pin and DMA map userspace memory.
--- include/rdma/ib_umem.h
+++ include/rdma/ib_umem.h
@@ -52,6 +52,37 @@ struct ib_umem {
 	unsigned long		diff;
 };
 
+/* contiguous memory structure */
+struct ib_cmem {
+
+	struct ib_ucontext     *context;
+	size_t			length;
+	/* Link list of contiguous blocks being part of that cmem  */
+	struct list_head ib_cmem_block;
+
+	/* Order of cmem block,  2^ block_order will equal number
+	     of physical pages per block
+	*/
+	unsigned long    block_order;
+	/* Refernce counter for that memory area
+	  - When value became 0 pages will be returned to the kernel.
+	*/
+	struct kref refcount;
+};
+
+
+struct ib_cmem_block {
+
+	struct list_head	list;
+	/* page will point to the page struct of the head page
+	    in the current compound page.
+	    block order is saved once as part of ib_cmem.
+	*/
+	struct page            *page;
+};
+
+
+
 struct ib_umem_chunk {
 	struct list_head	list;
 	int                     nents;
@@ -65,6 +96,13 @@ struct ib_umem *ib_umem_get(struct ib_ucontext *context, unsigned long addr,
 			    size_t size, int access, int dmasync);
 void ib_umem_release(struct ib_umem *umem);
 int ib_umem_page_count(struct ib_umem *umem);
+
+int ib_cmem_map_contiguous_pages_to_vma(struct ib_cmem *ib_cmem,
+	struct vm_area_struct *vma);
+struct ib_cmem *ib_cmem_alloc_contiguous_pages(struct ib_ucontext *context,
+				unsigned long total_size,
+				unsigned long page_size_order);
+void ib_cmem_release_contiguous_pages(struct ib_cmem *cmem);
 
 #else /* CONFIG_INFINIBAND_USER_MEM */
 
