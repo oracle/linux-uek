40851c8229a89aca8a318570f4831980d6e228dd
Sarveshwar Bandi <sarveshwar.bandi@emulex.com>
2012-05-04 17:03:44 +0530

    be2net: update the driver version
    Signed-off-by: Sarveshwar Bandi <sarveshwar.bandi@emulex.com>

--- drivers/net/benet/be.h
+++ drivers/net/benet/be.h
@@ -34,7 +34,7 @@
 
 #include "be_hw.h"
 
-#define DRV_VER			"4.1.297o"
+#define DRV_VER			"4.2.220o"
 #define DRV_NAME		"be2net"
 #define BE_NAME			"ServerEngines BladeEngine2 10Gbps NIC"
 #define BE3_NAME		"ServerEngines BladeEngine3 10Gbps NIC"
