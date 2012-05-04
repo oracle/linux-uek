b7a659b68a7e24c8ee5dcfef2b83e247c53eddbe
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2012-05-04 14:06:02 -0700

    cpuidle: add checks to avoid NULL pointer dereference
    The existing check for dev == NULL in __cpuidle_register_device() is
    rendered useless because dev is dereferenced before the check itself.
    Moreover, correctly speaking, it is the job of the callers of this
    function, i.e., cpuidle_register_device() & cpuidle_enable_device() (which
    also happen to be exported functions) to ensure that
    __cpuidle_register_device() is called with a non-NULL dev.
    
    So add the necessary dev == NULL checks in the two callers and remove the
    (useless) check from __cpuidle_register_device().
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Len Brown <len.brown@intel.com>
    (cherry picked from commit 1b0a0e9a15b976d91f3b5ae619c6a8964c2818eb)
    
    Orabug: 17936264
    
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
    Conflicts:
    	drivers/cpuidle/cpuidle.c

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -221,6 +221,9 @@ int cpuidle_enable_device(struct cpuidle_device *dev)
 {
 	int ret, i;
 
+	if (!dev)
+		return -EINVAL;
+
 	if (dev->enabled)
 		return 0;
 	if (!cpuidle_get_driver() || !cpuidle_curr_governor)
@@ -348,6 +351,9 @@ static int __cpuidle_register_device(struct cpuidle_device *dev)
 int cpuidle_register_device(struct cpuidle_device *dev)
 {
 	int ret;
+
+	if (!dev)
+		return -EINVAL;
 
 	mutex_lock(&cpuidle_lock);
 
