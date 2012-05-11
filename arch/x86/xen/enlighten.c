c251d5fffdef7abbd33e0bceb4e209892e1e3cbb
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-05-11 16:32:27 -0400

    Revert "xen/mce: Change the machine check point"
    This reverts commit b821d4bf0d69b71ffef5755e3396d78b19932bcb.

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -521,7 +521,7 @@ static int cvt_gate_to_trap(int vector, const gate_desc *val,
 	 * Look for known traps using IST, and substitute them
 	 * appropriately.  The debugger ones are the only ones we care
 	 * about.  Xen will handle faults like double_fault and
-	 * nmi, so we should never see them.  Warn if
+	 * machine_check, so we should never see them.  Warn if
 	 * there's an unexpected IST-using fault handler.
 	 */
 	if (addr == (unsigned long)debug)
@@ -536,10 +536,7 @@ static int cvt_gate_to_trap(int vector, const gate_desc *val,
 		return 0;
 #ifdef CONFIG_X86_MCE
 	} else if (addr == (unsigned long)machine_check) {
-		if (xen_initial_domain())
-			addr = (unsigned long)machine_check;
-		else
-			return 0;
+		return 0;
 #endif
 	} else {
 		/* Some other trap using IST? */
