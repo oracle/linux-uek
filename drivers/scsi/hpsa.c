9642053133b8f961bc94d06ca9a7e547c0741d2f
Joe Jin <joe.jin@oracle.com>
2012-05-15 14:41:33 +0800

    [scsi] hpsa: Add IRQF_SHARED back in for the non-MSI(X) interrupt handler
    Add IRQF_SHARED back in for the non-MSI(X) interrupt handler.
    This backport from RHEL6U3.
    
    Signed-off-by: Joe Jin <joe.jin@oracle.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -4058,7 +4058,7 @@ static int hpsa_request_irq(struct ctlr_info *h,
 				IRQF_DISABLED, h->devname, h);
 	else
 		rc = request_irq(h->intr[h->intr_mode], intxhandler,
-				IRQF_DISABLED, h->devname, h);
+				IRQF_DISABLED | IRQF_SHARED, h->devname, h);
 	if (rc) {
 		dev_err(&h->pdev->dev, "unable to get irq %d for %s\n",
 		       h->intr[h->intr_mode], h->devname);
