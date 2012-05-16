84911f593a6509aae18820839bf2413441d1b593
Joe Jin <joe.jin@oracle.com>
2012-05-16 14:00:24 +0800

    bnx2: Add missing memory barrier in bnx2_start_xmit()
    Sync DMA descriptor before hitting the TX mailbox for weak memory model
    CPUs.
    
    There has been discussions several years ago about this.  Some believe
    that writel() should guarantee ordering.  Others want explicit barriers
    if necessary.  Today writel() does not have the ordering guarantee and
    many other drivers use explicit barriers.
    
    (backported from commit 94bf91baf3a16ec274de3cd913be3033c029f853)
    Signed-off-by: Vlad Zolotarov <vlad@scalemp.com>
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Joe Jin <joe.jin@oracle.com>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -6559,6 +6559,9 @@ bnx2_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	}
 	txbd->tx_bd_vlan_tag_flags |= TX_BD_FLAGS_END;
 
+	/* Sync BD data before updating TX mailbox */
+	wmb();
+
 	prod = NEXT_TX_BD(prod);
 	txr->tx_prod_bseq += skb->len;
 
