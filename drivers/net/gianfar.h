7541b6faa2f336f63d6d3e2c3dc682bdb8a2e68a
Joe Jin <joe.jin@oracle.com>
2012-05-18 11:10:25 +0800

    ethernet: Convert MAC_ADDR_LEN uses to ETH_ALEN
    Reduce the number of #defines, use the normal #define from if_ether.h
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (backported from commit 6a3c910ca04ecd69b16dae47b26097a92c533828)
    
    Signed-off-by: Joe Jin <joe.jin@oracle.com>

--- drivers/net/gianfar.h
+++ drivers/net/gianfar.h
@@ -64,9 +64,6 @@
  * will be the next highest multiple of 512 bytes. */
 #define INCREMENTAL_BUFFER_SIZE 512
 
-
-#define MAC_ADDR_LEN 6
-
 #define PHY_INIT_TIMEOUT 100000
 #define GFAR_PHY_CHANGE_TIME 2
 
