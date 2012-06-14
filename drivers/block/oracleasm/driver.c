d74debada016ec7d9894e638310f7d93bf742934
Martin K. Petersen <martin.petersen@oracle.com>
2012-06-14 01:44:07 -0400

    oracleasm: Data integrity support
    Add data integrity support to the oracleasm driver.
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    (cherry picked from commit b25990f3ecd4c588b048dc04a6ccafbbcbe3b36a)

--- drivers/block/oracleasm/driver.c
+++ drivers/block/oracleasm/driver.c
@@ -13,6 +13,8 @@
  *      required to support the userspace library.
  *
  * MODIFIED   (YYYY/MM/DD)
+ *	2008/09/01 - Martin K. Petersen <martin.petersen@oracle.com>
+ *		Data integrity changes.
  *      2004/01/02 - Joel Becker <joel.becker@oracle.com>
  *		Initial GPL header.
  *      2004/09/10 - Joel Becker <joel.becker@oracle.com>
@@ -85,6 +87,7 @@
 #include "proc.h"
 #include "transaction_file.h"
 #include "request.h"
+#include "integrity.h"
 
 #if PAGE_CACHE_SIZE % 1024
 #error Oh no, PAGE_CACHE_SIZE is not divisible by 1k! I cannot cope.
@@ -1150,6 +1153,10 @@ static void asm_end_ioc(struct asm_request *r, unsigned int bytes_done,
 			r->r_error = ASM_ERR_IO;
 			break;
 
+		case -EILSEQ:
+			r->r_error = asm_integrity_error(r);
+			break;
+
 		case -ENODEV:
 			r->r_error = ASM_ERR_NODEV;
 			r->r_status |= ASM_LOCAL_ERROR;
@@ -1204,6 +1211,7 @@ static int asm_submit_io(struct file *file,
 	struct asm_disk_info *d;
 	struct inode *disk_inode;
 	struct block_device *bdev;
+	struct oracleasm_integrity_v2 *it;
 
 	mlog_entry("(0x%p, 0x%p, 0x%p)\n", file, user_iocp, ioc);
 
@@ -1246,7 +1254,8 @@ static int asm_submit_io(struct file *file,
 	spin_unlock_irq(&ASMFS_FILE(file)->f_lock);
 
 	ret = -ENODEV;
-	args.fa_handle = (unsigned long)ioc->disk_asm_ioc;
+	args.fa_handle = (unsigned long)ioc->disk_asm_ioc &
+		~ASM_INTEGRITY_HANDLE_MASK;
 	args.fa_inode = ASMFS_I(inode);
 	disk_inode = ilookup5(asmdisk_mnt->mnt_sb,
 			      (unsigned long)args.fa_handle,
@@ -1318,6 +1327,11 @@ static int asm_submit_io(struct file *file,
 	     "Request 0x%p (user_ioc 0x%p) passed validation checks\n",
 	     r, user_iocp);
 
+	if (bdev_get_integrity(bdev))
+		it = (struct oracleasm_integrity_v2 *)ioc->check_asm_ioc;
+	else
+		it = NULL;
+
 	switch (ioc->operation_asm_ioc) {
 		default:
 			goto out_error;
@@ -1325,10 +1339,18 @@ static int asm_submit_io(struct file *file,
 
 		case ASM_READ:
 			rw = READ;
+
+			if (it && asm_integrity_check(it, bdev) < 0)
+				goto out_error;
+
 			break;
 
 		case ASM_WRITE:
 			rw = WRITE;
+
+			if (it && asm_integrity_check(it, bdev) < 0)
+				goto out_error;
+
 			break;
 
 		case ASM_NOOP:
@@ -1367,6 +1389,17 @@ static int asm_submit_io(struct file *file,
 	 */
 	r->r_bio->bi_sector = ioc->first_asm_ioc *
 		(bdev_physical_block_size(bdev) >> 9);
+
+	if (it) {
+		ret = asm_integrity_map(it, r, rw == READ);
+
+		if (ret < 0) {
+			mlog(ML_ERROR|ML_BIO,
+			     "Could not attach integrity payload\n");
+			bio_unmap_user(r->r_bio);
+			goto out_error;
+		}
+	}
 
 	/*
 	 * If the bio is a bounced bio, we have to put the
@@ -2012,6 +2045,7 @@ static void asm_cleanup_bios(struct file *file)
 
 		spin_unlock_irq(&afi->f_lock);
 		mlog(ML_BIO, "Unmapping bio 0x%p\n", bio);
+		asm_integrity_unmap(bio);
 		bio_unmap_user(bio);
 		spin_lock_irq(&afi->f_lock);
 	}
@@ -2329,10 +2363,13 @@ static ssize_t asmfs_svc_query_disk(struct file *file, char *buf, size_t size)
 
 	qd_info->qd_max_sectors = compute_max_sectors(bdev);
 	qd_info->qd_hardsect_size = bdev_physical_block_size(bdev);
+	qd_info->qd_feature = asm_integrity_format(bdev) &
+		ASM_INTEGRITY_QDF_MASK;
 	mlog(ML_ABI|ML_DISK,
 	     "Querydisk returning qd_max_sectors = %u and "
-	     "qd_hardsect_size = %d\n",
-	     qd_info->qd_max_sectors, qd_info->qd_hardsect_size);
+	     "qd_hardsect_size = %u, qd_integrity = %u\n",
+	     qd_info->qd_max_sectors, qd_info->qd_hardsect_size,
+	     asm_integrity_format(bdev));
 
 	ret = 0;
 
