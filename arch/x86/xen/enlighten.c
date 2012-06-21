d8f99dd3139a5991c48fa4eb6eefed77a6c06b27
Daniel Kiper <daniel.kiper@oracle.com>
2012-06-21 15:38:02 +0200

    x86/xen/enlighten: Add init and crash kexec/kdump hooks
    Add init and crash kexec/kdump hooks.
    
    Signed-off-by: Daniel Kiper <daniel.kiper@oracle.com>

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -31,6 +31,7 @@
 #include <linux/pci.h>
 #include <linux/gfp.h>
 #include <linux/memblock.h>
+#include <linux/kexec.h>
 
 #include <xen/xen.h>
 #include <xen/interface/xen.h>
@@ -66,6 +67,7 @@
 #include <asm/hypervisor.h>
 #include <asm/mwait.h>
 #include <asm/pci_x86.h>
+#include <asm/xen/kexec.h>
 
 #ifdef CONFIG_ACPI
 #include <asm/acpi.h>
@@ -1194,6 +1196,12 @@ static void xen_machine_power_off(void)
 
 static void xen_crash_shutdown(struct pt_regs *regs)
 {
+#ifdef CONFIG_KEXEC
+	if (kexec_crash_image) {
+		crash_save_cpu(regs, safe_smp_processor_id());
+		return;
+	}
+#endif
 	xen_reboot(SHUTDOWN_crash);
 }
 
@@ -1271,6 +1279,10 @@ asmlinkage void __init xen_start_kernel(void)
 	 */
 
 	xen_init_mmu_ops();
+
+#ifdef CONFIG_KEXEC
+	xen_init_kexec_ops();
+#endif
 
 	/* Prevent unwanted bits from being set in PTEs. */
 	__supported_pte_mask &= ~_PAGE_GLOBAL;
