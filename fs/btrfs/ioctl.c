f6c8a9750f2b378036018a7a8302fcf9b392683c
David Sterba <dsterba@suse.cz>
2012-06-22 06:30:39 -0600

    btrfs: join DEV_STATS ioctls to one
    Commit c11d2c236cc260b36 (Btrfs: add ioctl to get and reset the device
    stats) introduced two ioctls doing almost the same thing distinguished
    by just the ioctl number which encodes "do reset after read". I have
    suggested
    
    http://www.mail-archive.com/linux-btrfs@vger.kernel.org/msg16604.html
    
    to implement it via the ioctl args. This hasn't happen, and I think we
    should use a more clean way to pass flags and should not waste ioctl
    numbers.
    
    CC: Stefan Behrens <sbehrens@giantdisaster.de>
    Signed-off-by: David Sterba <dsterba@suse.cz>
    (cherry picked from commit b27f7c0c150f74564b5d4c6c24a03c5226bf6327)
    
    Conflicts:
    
    	fs/btrfs/ioctl.c
    	fs/btrfs/ioctl.h
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -3242,7 +3242,12 @@ static long btrfs_ioctl_get_dev_stats(struct btrfs_root *root,
 	if (IS_ERR(sa))
 		return PTR_ERR(sa);
 
-	ret = btrfs_get_dev_stats(root, sa, reset_after_read);
+	if ((sa->flags & BTRFS_DEV_STATS_RESET) && !capable(CAP_SYS_ADMIN)) {
+		kfree(sa);
+		return -EPERM;
+	}
+
+	ret = btrfs_get_dev_stats(root, sa);
 
 	if (copy_to_user(arg, sa, sizeof(*sa)))
 		ret = -EFAULT;
