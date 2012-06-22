fbe603026ba7df1c9aa76955d95c75e1bdb43354
Ilya Dryomov <idryomov@gmail.com>
2012-06-22 12:24:13 -0600

    Btrfs: resume balance on rw (re)mounts properly
    This introduces btrfs_resume_balance_async(), which, given that
    restriper state was recovered earlier by btrfs_recover_balance(),
    resumes balance in btrfs-balance kthread.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit 2b6ba629b5aac51e7099efbb43e2b403213aa7fb)
    
    Conflicts:
    
    	fs/btrfs/disk-io.c
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -1294,6 +1294,10 @@ static int btrfs_remount(struct super_block *sb, int *flags, char *data)
 		if (ret)
 			goto restore;
 
+		ret = btrfs_resume_balance_async(fs_info);
+		if (ret)
+			goto restore;
+
 		sb->s_flags &= ~MS_RDONLY;
 	}
 out:
