69a1b27d503aa72d95a0435725f3b33f936d260a
Nick Alcock <nick.alcock@oracle.com>
2012-06-29 21:06:28 +0100

    Provide symbol sizes in /proc/kallmodsyms.
    For modules, we can simply extract these sizes from the module symtab; for core
    kernel symbols, we must do subtraction as get_symbol_pos() did: this is now
    abstracted into a new get_symbol_size().
    
    Because /proc/kallmodsyms contains *all* symbols, where get_symbol_pos() was
    normally only called for a small subset of symbols, this exercises this code
    like never before, and has revealed a bug: the size of __per_cpu_end was being
    returned as a ludicrously vast value, because the next symbol after
    __per_cpu_end is far up the address space, in the kernel proper. Fixing this by
    forcing a size of zero for __per_cpu_end is easy enough.
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -206,12 +206,66 @@ int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 }
 EXPORT_SYMBOL_GPL(kallsyms_on_each_symbol);
 
+static unsigned long get_symbol_size(unsigned long kallsyms_addr)
+{
+	unsigned long size = 0;
+	unsigned long sym_addr = kallsyms_addresses[kallsyms_addr];
+	unsigned long used_i = 0;
+	unsigned long used_i_addr = 0;
+
+	/*
+	 * __per_cpu_end always has size zero.
+	 */
+	if (sym_addr == (unsigned long)__per_cpu_end)
+		return 0;
+
+	/*
+	 * Search for next non-aliased symbol.  Aliased symbols are symbols with
+	 * the same address.
+	 */
+	if (kallsyms_addr < (kallsyms_num_syms - 1)) {
+		unsigned long i;
+
+		for (i = kallsyms_addr + 1; i < kallsyms_num_syms; i++)
+			if (kallsyms_addresses[i] > sym_addr) {
+				size = kallsyms_addresses[i] - sym_addr;
+				used_i = i;
+				used_i_addr = kallsyms_addresses[i];
+				break;
+			}
+	}
+
+	/* If we found no next symbol, we use the end of the section. */
+	if (!size) {
+		unsigned long symbol_end;
+
+		if (is_kernel_inittext(sym_addr)) {
+			symbol_end = (unsigned long)_einittext;
+			printk (KERN_INFO "defaulting with symbol_end == _einittext\n");
+		}
+		else if (all_var) {
+			symbol_end = (unsigned long)_end;
+			printk (KERN_INFO "defaulting with symbol_end == _end\n");
+		} else {
+			symbol_end = (unsigned long)_etext;
+			printk (KERN_INFO "defaulting with symbol_end == _etext\n");
+		}
+
+		printk (KERN_INFO "defaulting: sym_addr = %lx, symbol_end = %lx, size = %lx\n",
+			sym_addr, symbol_end, size);
+
+		size = symbol_end - sym_addr;
+	}
+
+	return size;
+}
+
+
 static unsigned long get_symbol_pos(unsigned long addr,
 				    unsigned long *symbolsize,
 				    unsigned long *offset)
 {
-	unsigned long symbol_start = 0, symbol_end = 0;
-	unsigned long i, low, high, mid;
+	unsigned long low, high, mid;
 
 	/* This kernel should never had been booted. */
 	BUG_ON(!kallsyms_addresses);
@@ -229,36 +283,15 @@ static unsigned long get_symbol_pos(unsigned long addr,
 	}
 
 	/*
-	 * Search for the first aliased symbol. Aliased
-	 * symbols are symbols with the same address.
+	 * Search for the first aliased symbol.
 	 */
 	while (low && kallsyms_addresses[low-1] == kallsyms_addresses[low])
 		--low;
 
-	symbol_start = kallsyms_addresses[low];
-
-	/* Search for next non-aliased symbol. */
-	for (i = low + 1; i < kallsyms_num_syms; i++) {
-		if (kallsyms_addresses[i] > symbol_start) {
-			symbol_end = kallsyms_addresses[i];
-			break;
-		}
-	}
-
-	/* If we found no next symbol, we use the end of the section. */
-	if (!symbol_end) {
-		if (is_kernel_inittext(addr))
-			symbol_end = (unsigned long)_einittext;
-		else if (all_var)
-			symbol_end = (unsigned long)_end;
-		else
-			symbol_end = (unsigned long)_etext;
-	}
-
 	if (symbolsize)
-		*symbolsize = symbol_end - symbol_start;
+		*symbolsize = get_symbol_size(low);
 	if (offset)
-		*offset = addr - symbol_start;
+		*offset = addr - kallsyms_addresses[low];
 
 	return low;
 }
@@ -440,8 +473,8 @@ static int get_ksymbol_mod(struct kallsym_iter *iter)
 {
 	iter->builtin_module = 0;
 	if (module_get_kallsym(iter->pos - kallsyms_num_syms, &iter->value,
-				&iter->type, iter->name, iter->module_name,
-				&iter->exported) < 0)
+			       &iter->type, iter->name, iter->module_name,
+			       &iter->size, &iter->exported) < 0)
 		return 0;
 	return 1;
 }
@@ -462,6 +495,7 @@ static unsigned long get_ksymbol_core(struct kallsym_iter *iter)
 	iter->exported = 0;
 	iter->value = kallsyms_addresses[iter->pos];
 
+	iter->size = get_symbol_size(iter->pos);
 	iter->type = kallsyms_get_symbol_type(off);
 
 	off = kallsyms_expand_symbol(off, iter->name);
@@ -536,12 +570,18 @@ static int s_show_internal(struct seq_file *m, void *p, int builtin_modules)
 		 */
 		type = iter->exported ? toupper(iter->type) :
 					tolower(iter->type);
-		seq_printf(m, "%pK %c %s\t[%s]\n", (void *)iter->value,
-			   type, iter->name, iter->module_name);
-	} else {
+		if (builtin_modules)
+			seq_printf(m, "%pK %lx %c %s\t[%s]\n", (void *)iter->value,
+				   iter->size, type, iter->name, iter->module_name);
+		else
+			seq_printf(m, "%pK %c %s\t[%s]\n", (void *)iter->value,
+				   type, iter->name, iter->module_name);
+	} else if (builtin_modules)
+		seq_printf(m, "%pK %lx %c %s\n", (void *)iter->value,
+			   iter->size, iter->type, iter->name);
+	else
 		seq_printf(m, "%pK %c %s\n", (void *)iter->value,
 			   iter->type, iter->name);
-	}
 	return 0;
 }
 
