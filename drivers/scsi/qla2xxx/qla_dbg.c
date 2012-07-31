2d24a335c464ca1514d6e153516c23aaf642487d
Saurav Kashyap <saurav.kashyap@qlogic.com>
2012-07-31 13:04:51 +0530

    qla2xxx: Get fcal position map should not be called for p2p topology.
    JIRA Key: V2632FC-249
    
    Acked-by: Chad Dupuis <chad.dupuis@qlogic.com>
    Acked-by: Armen Baloyan <armen.baloyan@qlogic.com>

--- drivers/scsi/qla2xxx/qla_dbg.c
+++ drivers/scsi/qla2xxx/qla_dbg.c
@@ -13,7 +13,8 @@
  * ----------------------------------------------------------------------
  * | Module Init and Probe        |       0x0122       | 0x4b,0xba,0xfa |
  * | Mailbox commands             |       0x1153       | 0x111a-0x111b  |
- * | Device Discovery             |       0x2087       | 0x2020-0x2022  |
+ * | Device Discovery             |       0x2087       | 0x2020-0x2022, |
+ * |                              |                    | 0x2016         |
  * | Queue Command and IO tracing |       0x3030       | 0x3006,0x3008  |
  * |                              |                    | 0x302d,0x302e  |
  * | DPC Thread                   |       0x401d       | 0x4002,0x4013  |
