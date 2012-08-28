4e0ad5132c7cdd5fe3a95470b31f4e5d689eed12
Joe Jin <joe.jin@oracle.com>
2012-08-28 19:57:55 +0800

    htrimer: fix kabi break.
    Signed-off-by: Joe Jin <joe.jin@oracle.com>

--- include/linux/hrtimer.h
+++ include/linux/hrtimer.h
@@ -178,8 +178,7 @@ enum  hrtimer_base_type {
  */
 struct hrtimer_cpu_base {
 	raw_spinlock_t			lock;
-	unsigned int			active_bases;
-	unsigned int			clock_was_set;
+	unsigned long			active_bases;
 #ifdef CONFIG_HIGH_RES_TIMERS
 	ktime_t				expires_next;
 	int				hres_active;
@@ -190,6 +189,9 @@ struct hrtimer_cpu_base {
 	ktime_t				max_hang_time;
 #endif
 	struct hrtimer_clock_base	clock_base[HRTIMER_MAX_CLOCK_BASES];
+#ifndef __GENKSYMS__
+	unsigned int			clock_was_set;
+#endif
 };
 
 static inline void hrtimer_set_expires(struct hrtimer *timer, ktime_t time)
