79d817be22c606bd228f5bb4eb55e17db3bf89c3
Nick Alcock <nick.alcock@oracle.com>
2012-10-18 16:43:30 +0100

    Fix outright typos in the 3.6 forward-port.
    The mass cherry-pick of 3.6 support introduced some build-breaking typos. This
    fixes them (fixing here rather than where introduced for simplicity's sake and
    because we don't really expect past history to be compilable against 3.6 without
    changes).
    
    Signed-off-by: Nick Alcock <nick.alcock@oracle.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2926,7 +2926,7 @@ need_resched:
 			} else {
 				deactivate_task(rq, prev, DEQUEUE_SLEEP);
 				prev->on_rq = 0;
-	
+
 				/*
 				 * If a worker went to sleep, notify and ask
 				 * workqueue whether it wants to wake up a task
@@ -2934,7 +2934,7 @@ need_resched:
 				 */
 				if (prev->flags & PF_WQ_WORKER) {
 					struct task_struct *to_wakeup;
-	
+
 					to_wakeup = wq_worker_sleeping(prev, cpu);
 					if (to_wakeup)
 						try_to_wake_up_local(to_wakeup);
