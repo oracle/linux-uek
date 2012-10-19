2c830d99a7d3126a96162caac3606f864d59aff3
Kris Van Hees <kris.van.hees@oracle.com>
2012-10-19 04:37:53 -0400

    Internal performance measurement support code.
    The dt_perf provider implements a few probes that are used in performance
    (or more accurately put, overhead) measurements.  It uses an ioctl()
    interface to trigger N-count interations of invoking probes through
    various mechanisms, and a probe to post the results back to userspace.
    This code also adds an SDT probe in the DTrace kernel support code, just
    to measure overhead for triggering trap based SDT probes.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ dtrace/dt_perf_mod.c
@@ -0,0 +1,62 @@
+/*
+ * FILE:	dt_perf_mod.c
+ * DESCRIPTION:	DTrace Performance Test Probe: module handling
+ *
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ * Copyright 2011 Oracle, Inc.  All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#include <linux/module.h>
+
+#include "dtrace.h"
+#include "dtrace_dev.h"
+#include "dt_perf.h"
+
+MODULE_AUTHOR("Kris Van Hees (kris.van.hees@oracle.com)");
+MODULE_DESCRIPTION("DTrace Performance Test Probe");
+MODULE_VERSION("v0.1");
+MODULE_LICENSE("CDDL");
+
+static const dtrace_pattr_t dt_perf_attr = {
+{ DTRACE_STABILITY_EVOLVING, DTRACE_STABILITY_EVOLVING, DTRACE_CLASS_COMMON },
+{ DTRACE_STABILITY_PRIVATE, DTRACE_STABILITY_PRIVATE, DTRACE_CLASS_UNKNOWN },
+{ DTRACE_STABILITY_PRIVATE, DTRACE_STABILITY_PRIVATE, DTRACE_CLASS_ISA },
+{ DTRACE_STABILITY_EVOLVING, DTRACE_STABILITY_EVOLVING, DTRACE_CLASS_COMMON },
+{ DTRACE_STABILITY_PRIVATE, DTRACE_STABILITY_PRIVATE, DTRACE_CLASS_ISA },
+};
+
+DT_PROVIDER_POPS(dt_perf)
+
+static dtrace_pops_t dt_perf_pops = {
+	dt_perf_provide,
+	NULL,
+	dt_perf_enable,
+	dt_perf_disable,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	dt_perf_destroy
+};
+
+DT_PROVIDER_MODULE(dt_perf, DTRACE_PRIV_USER)
