61f5da8bff26ec360bab3e1f8d5955bc6dcac02e
Erez Shitrit <erezsh@mellanox.com>
2012-11-14 15:06:03 +0200

    ib/ipoib: debug prints instead of warn in tx_wc function
    In IB_WC_RNR_RETRY_EXC_ERR status print debug message else print error message.
    IB_WC_RNR_RETRY_EXC_ERR is part of the CM life cycle.
    
    Signed-off-by: Erez Shitrit <erezsh@mellanox.com>

--- drivers/infiniband/ulp/ipoib/ipoib_cm.c
+++ drivers/infiniband/ulp/ipoib/ipoib_cm.c
@@ -852,9 +852,15 @@ void ipoib_cm_handle_tx_wc(struct net_device *dev, struct ib_wc *wc)
 	    wc->status != IB_WC_WR_FLUSH_ERR) {
 		struct ipoib_neigh *neigh;
 
-		ipoib_dbg(priv, "failed cm send event "
-			   "(status=%d, wrid=%d vend_err %x)\n",
-			   wc->status, wr_id, wc->vendor_err);
+		/*IB_WC_RNR_RETRY_EXC_ERR error is part of the life cycle, so don't make waves.*/
+		if (IB_WC_RNR_RETRY_EXC_ERR != wc->status)
+			ipoib_warn(priv, "%s: failed cm send event "
+				   "(status=%d, wrid=%d vend_err %x)\n",
+				   __func__, wc->status, wr_id, wc->vendor_err);
+		else
+			ipoib_dbg(priv, "%s: failed cm send event "
+				   "(status=%d, wrid=%d vend_err %x)\n",
+				   __func__, wc->status, wr_id, wc->vendor_err);
 
 		spin_lock_irqsave(&priv->lock, flags);
 		neigh = tx->neigh;
