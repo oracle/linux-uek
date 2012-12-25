92da85bdf2c51dc189f6f3f498e96595c0d3f273
Eric Sandeen <sandeen@redhat.com>
2012-12-25 13:33:13 -0500

    ext4: remove unaligned AIO warning printk
    Orabug: 14096480
    Although I put this in, I now think it was a bad decision.  For most
    users, there is very little to be done in this case.  They get the
    message, once per day, with no real context or proposed action.  TBH,
    it generates support calls when it probably does not need to; the
    message sounds more dire than the situation really is.
    
    Just nuke it.  Normal investigation via blktrace or whatnot can
    reveal poor IO patterns if bad performance is encountered.
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -121,14 +121,6 @@ ext4_file_write_iter(struct kiocb *iocb, struct iov_iter *iter, loff_t pos)
 
 	/* Unaligned direct AIO must be serialized; see comment above */
 	if (unaligned_aio) {
-		static unsigned long unaligned_warn_time;
-
-		/* Warn about this once per day */
-		if (printk_timed_ratelimit(&unaligned_warn_time, 60*60*24*HZ))
-			ext4_msg(inode->i_sb, KERN_WARNING,
-				 "Unaligned AIO/DIO on inode %ld by %s; "
-				 "performance will be poor.",
-				 inode->i_ino, current->comm);
 		mutex_lock(ext4_aio_mutex(inode));
 		ext4_aiodio_wait(inode);
 	}
