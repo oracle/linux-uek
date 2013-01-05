8b2b11c8af27eb9541c4ba45d0cf4f5e3c61b508
Rony Efraim <ronye@mellanox.com>
2013-01-05 12:46:17 +0200

    net/mlx4: Add support to set default vlan id and priority for VF NIC
    Add support for ndo_set_vf_vlan to the Ethernet driver.
    Once invoked, this vport is considered to be in VST mode.
    In this mode, the PPF driver configures Ethernet QPs created by this VF to
    use the given vlan id and priority. Currently RoCE isn't supported in VST mode.
    
    The special values of VID=4095 or VID=0,UP=0 are considered as VGT.
    
    issue: 91903
    
    Change-Id: I776a3843b24b01942d297820ce8ca1dd8844af79
    Signed-off-by: Rony Efraim <ronye@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx4/fw.c
+++ drivers/net/ethernet/mellanox/mlx4/fw.c
@@ -110,6 +110,7 @@ static void dump_dev_cap_flags(struct mlx4_dev *dev, u64 flags)
 		[42] = "Multicast VEP steering support",
 		[48] = "Counters support",
 		[59] = "Port management change event support",
+		[60] = "eSwitch support",
 		[61] = "64 byte EQE support",
 		[62] = "64 byte CQE support",
 	};
