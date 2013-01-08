c93d142eaaaeea8c1f90ac0c72e50f20366beb5b
Jiri Pirko <jiri@resnulli.us>
2013-01-08 01:38:25 +0000

    net: init perm_addr in register_netdevice()
    Benefit from the fact that dev->addr_assign_type is set to NET_ADDR_PERM
    in case the device has permanent address.
    
    This also fixes the problem that many drivers do not set perm_addr at
    all.
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (cherry picked from commit 948b337e62ca96b6e7de06fdd5f6fe44769d7e9c)
    
    Orabug: 17280581
    
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5534,6 +5534,13 @@ int register_netdevice(struct net_device *dev)
 	dev_hold(dev);
 	list_netdevice(dev);
 
+	/* If the device has permanent device address, driver should
+	 * set dev_addr and also addr_assign_type should be set to
+	 * NET_ADDR_PERM (default value).
+	 */
+	if (dev->addr_assign_type == NET_ADDR_PERM)
+		memcpy(dev->perm_addr, dev->dev_addr, dev->addr_len);
+
 	/* Notify protocols, that a new device appeared. */
 	ret = call_netdevice_notifiers(NETDEV_REGISTER, dev);
 	ret = notifier_to_errno(ret);
