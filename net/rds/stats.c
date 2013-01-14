4a1c9a85944240aea1d4cbb43fdf2c4144a1adc4
Bang Nguyen <bang.nguyen@oracle.com>
2013-01-13 21:54:09 -0800

    RDS merge for UEK2
    This is merged code of Mellanox OFED R2, 0080 release; and ofa 4.1
    
    Orabug: 15997083
    
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 4c999f5b1cfe917e7f1cfd83e88af785cdeb8a99)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 249f1b30faaaae165e10231848bfd68fec283404)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit a3998ffdf24129c61b15cc44dc9dd58b4b3bf14c)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/stats.c
+++ net/rds/stats.c
@@ -75,6 +75,7 @@ static char *rds_stat_names[] = {
 	"cong_update_received",
 	"cong_send_error",
 	"cong_send_blocked",
+	"qos_threshold_exceeded",
 };
 
 void rds_stats_info_copy(struct rds_info_iterator *iter,
