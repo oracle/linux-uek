413278917948a2cb64396cc4e6da4f8f8e1314c9
Kris Van Hees <kris.van.hees@oracle.com>
2013-01-23 04:42:32 -0500

    Add basic development debugging framework.
    This patch adds some defines that can be used to provide debugging output in
    support of development.  This is debugging output that is controlled at build
    time rather than runtime.  For production builds, this will always be disabled.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>

---
+++ dtrace/dtrace_debug.h
@@ -0,0 +1,55 @@
+#ifndef _DTRACE_DEBUG_H_
+#define _DTRACE_DEBUG_H_
+
+#ifdef CONFIG_DT_DEBUG
+
+# undef DT_DBG_AGG
+# undef DT_DBG_BUF
+# define DT_DBG_DOF
+# define DT_DBG_IOCTL
+# undef DT_DBG_PROBE
+
+#else /* CONFIG_DT_DEBUG */
+
+# undef DT_DBG_AGG
+# undef DT_DBG_BUF
+# undef DT_DBG_DOF
+# undef DT_DBG_IOCTL
+# undef DT_DBG_PROBE
+
+#endif /* CONFIG_DT_DEBUG */
+
+/*
+ * Here are the actual actions for the various debug cases.
+ */
+#ifdef DT_DBG_AGG
+# define dt_dbg_agg(fmt, ...)	pr_info(fmt, ## __VA_ARGS__)
+#else
+# define dt_dbg_agg(fmt, ...)
+#endif
+
+#ifdef DT_DBG_BUF
+# define dt_dbg_buf(fmt, ...)	pr_info(fmt, ## __VA_ARGS__)
+#else
+# define dt_dbg_buf(fmt, ...)
+#endif
+
+#ifdef DT_DBG_DOF
+# define dt_dbg_dof(fmt, ...)	pr_info(fmt, ## __VA_ARGS__)
+#else
+# define dt_dbg_dof(fmt, ...)
+#endif
+
+#ifdef DT_DBG_IOCTL
+# define dt_dbg_ioctl(fmt, ...)	pr_info(fmt, ## __VA_ARGS__)
+#else
+# define dt_dbg_ioctl(fmt, ...)
+#endif
+
+#ifdef DT_DBG_PROBE
+# define dt_dbg_probe(fmt, ...)	pr_info(fmt, ## __VA_ARGS__)
+#else
+# define dt_dbg_probe(fmt, ...)
+#endif
+
+#endif /* _DTRACE_DEBUG_H_ */
