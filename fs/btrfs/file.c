27fabb08e36644688f639e7040b74004fd35ac97
Miao Xie <miaox@cn.fujitsu.com>
2013-01-28 12:34:55 +0000

    Btrfs: fix wrong sync_writers decrement in btrfs_file_aio_write()
    If the checks at the beginning of btrfs_file_aio_write() fail, we needn't
    decrease ->sync_writers, because we have not increased it. Fix it.
    
    Signed-off-by: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    (cherry picked from commit 0a3404dcff29a7589e8d6ce8c36f97c5411f85b4)

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -1563,9 +1563,10 @@ static ssize_t btrfs_file_write_iter(struct kiocb *iocb,
 		if (err < 0 && num_written > 0)
 			num_written = err;
 	}
-out:
+
 	if (sync)
 		atomic_dec(&BTRFS_I(inode)->sync_writers);
+out:
 	sb_end_write(inode->i_sb);
 	current->backing_dev_info = NULL;
 	return num_written ? num_written : err;
