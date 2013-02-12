a41ea67f8b5674e111913fe84012da518790af82
Ilya Dryomov <idryomov@gmail.com>
2013-02-12 16:27:46 +0000

    Btrfs: eliminate a use-after-free in btrfs_balance()
    Commit 5af3e8cc introduced a use-after-free at volumes.c:3139: bctl is freed
    above in __cancel_balance() in all cases except for balance pause.  Fix this
    by moving the offending check a couple statements above, the meaning of the
    check is preserved.
    
    Reported-by: Chris Mason <chris.mason@fusionio.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    (cherry picked from commit bf023ecfcaf114c410759f375073e78b0871c120)

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -3017,6 +3017,11 @@ int btrfs_balance(struct btrfs_balance_control *bctl,
 	mutex_lock(&fs_info->balance_mutex);
 	atomic_dec(&fs_info->balance_running);
 
+	if (bctl->sys.flags & BTRFS_BALANCE_ARGS_CONVERT) {
+		fs_info->num_tolerated_disk_barrier_failures =
+			btrfs_calc_num_tolerated_disk_barrier_failures(fs_info);
+	}
+
 	if (bargs) {
 		memset(bargs, 0, sizeof(*bargs));
 		update_ioctl_balance_args(fs_info, 0, bargs);
