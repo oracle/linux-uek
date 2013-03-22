25ff413d543f44da6c8077ae3f0558c95f33e799
Liu Bo <bo.li.liu@oracle.com>
2013-03-22 20:14:54 +0800

    fs: export fget_light to modules
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>

--- fs/file_table.c
+++ fs/file_table.c
@@ -355,6 +355,8 @@ struct file *fget_light(unsigned int fd, int *fput_needed)
 	return file;
 }
 
+EXPORT_SYMBOL(fget_light);
+
 struct file *fget_raw_light(unsigned int fd, int *fput_needed)
 {
 	struct file *file;
