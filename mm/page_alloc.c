359a2313b1bbe818a5ecbcbbf2526f6ad900e380
K. Y. Srinivasan <kys@microsoft.com>
2013-03-25 15:47:38 -0700

    mm: export split_page()
    This symbol will be used in the Hyper-V balloon driver to support 2M
    allocations.
    
    Signed-off-by: K.  Y.  Srinivasan <kys@microsoft.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 5853ff23c2f0f6c87a859e7f882eac3300b329a0)
    
    Orabug: 18276803
    
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1387,6 +1387,7 @@ void split_page(struct page *page, unsigned int order)
 	for (i = 1; i < (1 << order); i++)
 		set_page_refcounted(page + i);
 }
+EXPORT_SYMBOL_GPL(split_page);
 
 static int __isolate_free_page(struct page *page, unsigned int order)
 {
