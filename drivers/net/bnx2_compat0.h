0346fd99ce3b1dd8995cf4fe32413a7e8c1ca2e2
Jerry Snitselaar <jerry.snitselaar@oracle.com>
2013-04-16 10:27:02 -0700

    bnx2: update to broadcom version 2.2.3n
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

---
+++ drivers/net/bnx2_compat0.h
@@ -0,0 +1,27 @@
+/* bnx2_compat0.h: Broadcom NX2 network driver.
+ *
+ * Copyright (c) 2013 Broadcom Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * Written by: Michael Chan  (mchan@broadcom.com)
+ */
+
+
+#ifndef BNX2_COMPAT0_H
+#define BNX2_COMPAT0_H
+
+/* CFLAGs that affect struct sizes should be defined here */
+
+#if (LINUX_VERSION_CODE >= 0x020618)
+#define BNX2_NEW_NAPI	1
+#endif
+
+#if (LINUX_VERSION_CODE >= 0x30000) && !defined(HAVE_NDO_VLAN_RX_REGISTER)
+#define NEW_VLAN
+#endif
+
+
+#endif
