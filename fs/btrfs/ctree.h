5ffd1bcca3a88143a4b01b31dbce322ebdd58cb2
Tsutomu Itoh <t-itoh@jp.fujitsu.com>
2013-04-16 05:18:49 +0000

    Btrfs: remove unused argument of btrfs_extend_item()
    Argument 'trans' is not used in btrfs_extend_item().
    
    Signed-off-by: Tsutomu Itoh <t-itoh@jp.fujitsu.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    (cherry picked from commit 4b90c68015a7c0863292d6306501552d4ffa33ff)
    
    Orabug: 25948102
    CVE: CVE-2014-9710
    
    Signed-off-by: Kirtikar Kashyap <kirtikar.kashyap@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -2991,8 +2991,7 @@ int btrfs_copy_root(struct btrfs_trans_handle *trans,
 		      struct extent_buffer **cow_ret, u64 new_root_objectid);
 int btrfs_block_can_be_shared(struct btrfs_root *root,
 			      struct extent_buffer *buf);
-void btrfs_extend_item(struct btrfs_trans_handle *trans,
-		       struct btrfs_root *root, struct btrfs_path *path,
+void btrfs_extend_item(struct btrfs_root *root, struct btrfs_path *path,
 		       u32 data_size);
 void btrfs_truncate_item(struct btrfs_trans_handle *trans,
 			 struct btrfs_root *root,
