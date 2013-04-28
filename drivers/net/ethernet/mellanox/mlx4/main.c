93bf3dd8d1e10260c0b62055e9215ea3fa808fa4
Moshe Lazer <moshel@mellanox.com>
2013-04-28 17:19:17 +0300

    mlx4_core: Avoid setting ports for auto when only one port type is supported
    When only one port type is supported driver should reject requests
    to change mode to auto sense.
    
    Signed-off-by: Moshe Lazer <moshel@mellanox.com>
    (Ported from Mellanox OFED 2.4)
    
    Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>

--- drivers/net/ethernet/mellanox/mlx4/main.c
+++ drivers/net/ethernet/mellanox/mlx4/main.c
@@ -1134,6 +1134,13 @@ static ssize_t set_port_type(struct device *dev,
 		goto err_out;
 	}
 
+	if ((info->tmp_type & mdev->caps.supported_type[info->port]) !=
+	    info->tmp_type) {
+		mlx4_err(mdev, "Requested port type for port %d is not supported on this HCA\n",
+			 info->port);
+		return -EINVAL;
+	}
+
 	mlx4_stop_sense(mdev);
 	mutex_lock(&priv->port_mutex);
 	/* Possible type is always the one that was delivered */
