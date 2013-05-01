1c28b90fc617f65f0c9f640661c3bd06c0195427
Gairy Grannum <Gairy.Grannum@Emulex.Com>
2013-05-01 12:58:43 -0700

    lpfc: Update lpfc version for 8.3.7.10.4p driver release
    Orabug: 16749881
    
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- drivers/scsi/lpfc/lpfc_version.h
+++ drivers/scsi/lpfc/lpfc_version.h
@@ -18,7 +18,7 @@
  * included with this package.                                     *
  *******************************************************************/
 
-#define LPFC_DRIVER_VERSION "8.3.5.86.2p"
+#define LPFC_DRIVER_VERSION "8.3.7.10.4p"
 #define LPFC_DRIVER_NAME		"lpfc"
 
 /* Used for SLI 2/3 */
