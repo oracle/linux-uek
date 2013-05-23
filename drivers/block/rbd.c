97edbef04f81eff4e760a98d95d038c77877c9a0
Alex Elder <elder@inktank.com>
2013-05-22 20:54:25 -0500

    rbd: flush dcache after zeroing page data
    Orabug: 20304006
    
    Neither zero_bio_chain() nor zero_pages() contains a call to flush
    caches after zeroing a portion of a page.  This can cause problems
    on architectures that have caches that allow virtual address
    aliasing.
    
    This resolves:
        http://tracker.ceph.com/issues/4777
    
    Signed-off-by: Alex Elder <elder@inktank.com>
    Reviewed-by: Josh Durgin <josh.durgin@inktank.com>
    (cherry picked from commit e215605417b87732c6debf65da6d953016a1e5bc)
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -1128,6 +1128,7 @@ static void zero_bio_chain(struct bio *chain, int start_ofs)
 				buf = bvec_kmap_irq(bv, &flags);
 				memset(buf + remainder, 0,
 				       bv->bv_len - remainder);
+				flush_dcache_page(bv->bv_page);
 				bvec_kunmap_irq(buf, &flags);
 			}
 			pos += bv->bv_len;
@@ -1160,6 +1161,7 @@ static void zero_pages(struct page **pages, u64 offset, u64 end)
 		local_irq_save(flags);
 		kaddr = kmap_atomic(*page);
 		memset(kaddr + page_offset, 0, length);
+		flush_dcache_page(*page);
 		kunmap_atomic(kaddr);
 		local_irq_restore(flags);
 
