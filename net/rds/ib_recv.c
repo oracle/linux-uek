097d20c8a5db5ca248f7bae8ed3da42032d6abea
Bang Nguyen <bang.nguyen@oracle.com>
2013-06-05 15:49:10 -0700

    RDS: Rename HAIP parameters to Active Bonding
    Orabug: 16810395
    
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit f897d417ab3e4b8380d1e164110c59bd5b88e68b)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/ib_cm.c
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- net/rds/ib_recv.c
+++ net/rds/ib_recv.c
@@ -1262,10 +1262,14 @@ void rds_ib_recv_cqe_handler(struct rds_ib_connection *ic,
 	} else {
 		/* We expect errors as the qp is drained during shutdown */
 		if (rds_conn_up(conn) || rds_conn_connecting(conn))
-			rds_ib_conn_error(conn, "recv completion on "
-					  "%pI4 had status %u, disconnecting and "
-					  "reconnecting\n", &conn->c_faddr,
-					  wc->status);
+			rds_ib_conn_error(conn, "recv completion "
+					"<%pI4,%pI4,%d> had "
+					"status %u, disconnecting and "
+					"reconnecting\n",
+					&conn->c_laddr,
+					&conn->c_faddr,
+					conn->c_tos,
+					wc->status);
 	}
 
 	/*
