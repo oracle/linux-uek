fedda831396f530af179b44292f6801a380ae700
Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
2013-06-10 22:42:15 -0700

    iser: iscsiadm logout hangs
    Orabug: 16313854
    
    iscsi admin utility unable to log out from logged in iser session
    and hangs. the issue was caused by code in iser_drain_tx_cq that
    try to extract the iser conext from the completion message. to
    understand the fix please read previous commits related to oracle
    bug 16313854.
    
    Signed-off-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>

--- drivers/infiniband/ulp/iser/iser_verbs.c
+++ drivers/infiniband/ulp/iser/iser_verbs.c
@@ -769,7 +769,8 @@ static int iser_drain_tx_cq(struct iser_device  *device)
 
 	while (ib_poll_cq(cq, 1, &wc) == 1) {
 		tx_desc	= (struct iser_tx_desc *) (unsigned long) wc.wr_id;
-		ib_conn = wc.qp->qp_context;
+		ib_conn = rdma_id_2_qp_ctx(
+			(struct rdma_cm_id *)(wc.qp->qp_context));
 		if (wc.status == IB_WC_SUCCESS) {
 			if (wc.opcode == IB_WC_SEND)
 				iser_snd_completion(tx_desc, ib_conn);
