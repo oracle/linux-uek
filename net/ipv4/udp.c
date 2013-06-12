f9c171d52cb78f31cf1fe4900d10595239417e44
Eric Dumazet <edumazet@google.com>
2013-06-12 14:31:39 -0700

    udp: fix two sparse errors
    commit ba418fa357a7b3c ("soreuseport: UDP/IPv4 implementation")
    added following sparse errors :
    
    net/ipv4/udp.c:433:60: warning: cast from restricted __be16
    net/ipv4/udp.c:433:60: warning: incorrect type in argument 1 (different base types)
    net/ipv4/udp.c:433:60:    expected unsigned short [unsigned] [usertype] val
    net/ipv4/udp.c:433:60:    got restricted __be16 [usertype] sport
    net/ipv4/udp.c:433:60: warning: cast from restricted __be16
    net/ipv4/udp.c:433:60: warning: cast from restricted __be16
    net/ipv4/udp.c:514:60: warning: cast from restricted __be16
    net/ipv4/udp.c:514:60: warning: incorrect type in argument 1 (different base types)
    net/ipv4/udp.c:514:60:    expected unsigned short [unsigned] [usertype] val
    net/ipv4/udp.c:514:60:    got restricted __be16 [usertype] sport
    net/ipv4/udp.c:514:60: warning: cast from restricted __be16
    net/ipv4/udp.c:514:60: warning: cast from restricted __be16
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Orabug: 18459758
    
    (cherry picked from mainline commit 7c0cadc69ca2ac8893aa162ee80d92a805840909)
    Signed-off-by: Chuck Anderson <chuck.anderson@oracle.com>
    Reviewed-by: John Haxby <john.haxby@oracle.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -429,7 +429,7 @@ begin:
 			reuseport = sk->sk_reuseport;
 			if (reuseport) {
 				hash = inet_ehashfn(net, daddr, hnum,
-						    saddr, htons(sport));
+						    saddr, sport);
 				matches = 1;
 			}
 		} else if (score == badness && reuseport) {
@@ -510,7 +510,7 @@ begin:
 			reuseport = sk->sk_reuseport;
 			if (reuseport) {
 				hash = inet_ehashfn(net, daddr, hnum,
-						    saddr, htons(sport));
+						    saddr, sport);
 				matches = 1;
 			}
 		} else if (score == badness && reuseport) {
