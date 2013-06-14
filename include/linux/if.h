1f8a2e1f7dd2f6f15b15bbc356d581dbb9fde83e
Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
2013-06-13 21:38:45 -0700

    Fix IPoIB compilation issue.
    This resolves following compilation error:
    drivers/infiniband/ulp/ipoib/ipoib_main.c:2483: error: ‘IFF_EIPOIB_PIF’ undeclared (first use in this function)
    
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>

--- include/linux/if.h
+++ include/linux/if.h
@@ -4,11 +4,7 @@
 #include <linux/version.h>
 #include_next <linux/if.h>
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0))
-
 #define IFF_EIPOIB_PIF  0x100000       /* IPoIB PIF intf(eg ib0, ib1 etc.)*/
 #define IFF_EIPOIB_VIF  0x200000       /* IPoIB VIF intf(eg ib0.x, ib1.x etc.)*/
-
-#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,6,0)) */
 
 #endif /* __EXTEND_LINUX_IF_H_TO_3_6__ */
