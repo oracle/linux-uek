2c9b6f01ed3b58922778e865605158e40f6d6ef4
Haiyang Zhang <haiyangz@microsoft.com>
2013-06-17 15:36:49 -0700

    Fix the VLAN_TAG_PRESENT in netvsc_recv_callback()
    We should call __vlan_hwaccel_put_tag() only if the packet
    comes from vlan, otherwise VLAN_TAG_PRESENT will always be
    added.
    
    Reported-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: Haiyang Zhang <haiyangz@microsoft.com>
    Reviewed-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 93725cbd22ed716bea8dc479b4925d40a4dbd0c6)
    
    Orabug: 18276803
    
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -285,7 +285,8 @@ int netvsc_recv_callback(struct hv_device *device_obj,
 
 	skb->protocol = eth_type_trans(skb, net);
 	skb->ip_summed = CHECKSUM_NONE;
-	__vlan_hwaccel_put_tag(skb, packet->vlan_tci);
+	if (packet->vlan_tci & VLAN_TAG_PRESENT)
+		__vlan_hwaccel_put_tag(skb, packet->vlan_tci);
 
 	net->stats.rx_packets++;
 	net->stats.rx_bytes += packet->total_data_buflen;
