0e5b9e856efbee5992e3568db4531ac2ea6e9002
Bang Nguyen <bang.nguyen@oracle.com>
2013-06-17 11:53:15 -0700

    RDMA CM: Avoid possible SEGV during connection shutdown
    cma_release_port() may be called twice for the same rdma id during
    connection shutdown, causing hlist_del() to SEGV.
    
    Orabug: 16750726
    
    Signed-off-by: Chien-Hua Yen <chien.yen@oracle.com>
    (cherry picked from commit cfee888ccacc233d2045a26492ed92a25d468927)
    
    Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>
    
    Orabug: 27358296
    
    (cherry picked from commit 5c25b3ad05ae4e42b0700ff63ca3598da5b6fdfb)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
        drivers/infiniband/core/cma.c
    
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Reviewed-by: Avinash Repaka <avinash.repaka@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -1621,14 +1621,17 @@ static void cma_cancel_operation(struct rdma_id_private *id_priv,
 
 static void cma_release_port(struct rdma_id_private *id_priv)
 {
-	struct rdma_bind_list *bind_list = id_priv->bind_list;
+	struct rdma_bind_list *bind_list;
 	struct net *net = id_priv->id.route.addr.dev_addr.net;
 
-	if (!bind_list)
-		return;
-
 	mutex_lock(&lock);
+	bind_list = id_priv->bind_list;
+	if (!bind_list) {
+		mutex_unlock(&lock);
+		return;
+	}
 	hlist_del(&id_priv->node);
+	id_priv->bind_list = NULL;
 	if (hlist_empty(&bind_list->owners)) {
 		cma_ps_remove(net, bind_list->ps, bind_list->port);
 		kfree(bind_list);
