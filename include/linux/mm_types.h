c02dd30ccb2c5833a093f8a80cca1b16c3e7a6a7
Guru Anbalagane <guru.anbalagane@oracle.com>
2013-07-15 12:17:24 -0700

    mm: leave hugepage pmd
    Orabug: 17186750
    
    Signed-off-by: Guru Anbalagane <guru.anbalagane@oracle.com>
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -324,9 +324,9 @@ struct mm_struct {
 #ifdef CONFIG_MMU_NOTIFIER
 	struct mmu_notifier_mm *mmu_notifier_mm;
 #endif
-#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+/*#ifdef CONFIG_TRANSPARENT_HUGEPAGE */
 	pgtable_t pmd_huge_pte; /* protected by page_table_lock */
-#endif
+/* #endif */
 #ifdef CONFIG_CPUMASK_OFFSTACK
 	struct cpumask cpumask_allocation;
 #endif
