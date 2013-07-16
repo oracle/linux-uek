c03d49f7abcc3d263df30ceb1bdd3347138ec486
Jack Morgenstein <jackm@dev.mellanox.co.il>
2013-07-16 11:45:29 +0300

    mlx4_core: Fix GEN_EQE accessing uninitialixed mutex
    We extremely rarely see an Oops in procedure mlx4_GEN_EQE
    where we try grabbing an unitialized mutex.
    
    This can occur in only one of 2 ways:
    1. We are trying to generate an async event on an uninitialized slave.
    2. We are trying to generate an async event on an illegal slave number
       ( < 0 or >= num-slaves)
    
    To deal with possibility 1: move the mutex initialization from
    specific slave initialization sequence in procedure mlx_master_do_cmd to
    mlx4_multi_func_init.
    
    To deal with possibility 2: check in procedure mlx4_GEN_EQE that the
    slave number provided is in the proper range. In addition, check that
    the slave is active.
    
    In addition, remove the incorrect memset of the event_eq struct in
    the error flow of mlx_master_do_cmd.
    
    issue: 212594
    
    Change-Id: Ic4a3e18d9ddfc1705fc881ba5116dd7da654379c
    Signed-off-by: Jack Morgenstein <jackm@dev.mellanox.co.il>

--- drivers/net/ethernet/mellanox/mlx4/eq.c
+++ drivers/net/ethernet/mellanox/mlx4/eq.c
@@ -150,12 +150,9 @@ void mlx4_gen_slave_eqe(struct work_struct *work)
 		/* All active slaves need to receive the event */
 		if (slave == ALL_SLAVES) {
 			for (i = 0; i < dev->num_slaves; i++) {
-				if (i != dev->caps.function &&
-				    master->slave_state[i].active)
-					if (mlx4_GEN_EQE(dev, i, eqe))
-						mlx4_warn(dev, "Failed to "
-							  " generate event "
-							  "for slave %d\n", i);
+				if (mlx4_GEN_EQE(dev, i, eqe))
+					mlx4_warn(dev, "Failed to generate "
+						  "event for slave %d\n", i);
 			}
 		} else {
 			if (mlx4_GEN_EQE(dev, slave, eqe))
@@ -200,13 +197,13 @@ static void mlx4_slave_event(struct mlx4_dev *dev, int slave,
 			     struct mlx4_eqe *eqe)
 {
 	struct mlx4_priv *priv = mlx4_priv(dev);
-	struct mlx4_slave_state *s_slave =
-		&priv->mfunc.master.slave_state[slave];
 
-	if (!s_slave->active) {
-		/*mlx4_warn(dev, "Trying to pass event to inactive slave\n");*/
+	if (slave < 0 || slave >= dev->num_slaves ||
+	    slave == dev->caps.function)
 		return;
-	}
+
+	if (!priv->mfunc.master.slave_state[slave].active)
+		return;
 
 	slave_event(dev, slave, eqe);
 }
