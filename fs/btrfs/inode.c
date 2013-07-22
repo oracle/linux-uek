b6f762df315cfa518d511e23983f5796daf3928b
Josef Bacik <jbacik@fusionio.com>
2013-07-22 12:50:37 -0400

    Btrfs: reset ret in record_one_backref
    I was getting warnings when running find ./ -type f -exec btrfs fi defrag -f {}
    \; from record_one_backref because ret was set.  Turns out it was because it was
    set to 1 because the search slot didn't come out exact and we never reset it.
    So reset it to 0 right after the search so we don't leak this and get
    uneccessary warnings.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>
    Signed-off-by: Chris Mason <chris.mason@fusionio.com>
    (cherry picked from commit 50f1319cb5f7690e4d9de18d1a75ea89296d0e53)
    
    Orabug: 18098511
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Reviewed-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -2070,6 +2070,7 @@ static noinline int record_one_backref(u64 inum, u64 offset, u64 root_id,
 		WARN_ON(1);
 		return ret;
 	}
+	ret = 0;
 
 	while (1) {
 		cond_resched();
@@ -2119,8 +2120,6 @@ static noinline int record_one_backref(u64 inum, u64 offset, u64 root_id,
 		    old->len || extent_offset + num_bytes <=
 		    old->extent_offset + old->offset)
 			continue;
-
-		ret = 0;
 		break;
 	}
 
