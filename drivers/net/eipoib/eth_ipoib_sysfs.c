fa0029fcabcea5d2bf93e0d3b0d6ca2e3a296e30
Erez Shitrit <erezsh@mellanox.com>
2013-08-07 14:42:43 +0300

    eth_ipoib: fix compilation warnning.
    issue:288409
    
    Change-Id: I0dc8c63a3893b437763c39ed8423c23e8875bc7c
    Signed-off-by: Erez Shitrit <erezsh@mellanox.com>

--- drivers/net/eipoib/eth_ipoib_sysfs.c
+++ drivers/net/eipoib/eth_ipoib_sysfs.c
@@ -408,15 +408,15 @@ static ssize_t parent_show_served(struct device *d,
 
 }
 
-#define IP_ADDR_LEN 4/* in bytes */
+#define IP_ADDR_LEN 48/* in bytes: x.y.z.w*/
 
 static ssize_t parent_store_served(struct device *d,
 				   struct device_attribute *attr,
 				   const char *buffer, size_t count)
 {
-	char command[1024] = { 0, };
+	char command[512] = { 0, };
 	char *mac_str;
-	char ip_str[100] = { 0, };
+	char ip_str[IP_ADDR_LEN] = { 0, };
 	u8 mac[ETH_ALEN];
 	u16 vlan = VLAN_N_VID;
 	__be32 ip;
