eb4fa31a4a8f9d5974dc518edb2c5044926a0b61
Matthew Garrett <matthew.garrett@nebula.com>
2013-09-09 15:49:43 -0700

    uswsusp: Disable when securelevel is set
    Orabug: 21539498
    
    uswsusp allows a user process to dump and then restore kernel state, which
    makes it possible to modify the running kernel. Disable this if securelevel
    has been set.
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- kernel/power/user.c
+++ kernel/power/user.c
@@ -24,6 +24,7 @@
 #include <linux/console.h>
 #include <linux/cpu.h>
 #include <linux/freezer.h>
+#include <linux/security.h>
 
 #include <asm/uaccess.h>
 
@@ -50,6 +51,9 @@ static int snapshot_open(struct inode *inode, struct file *filp)
 	int error;
 
 	if (!hibernation_available())
+		return -EPERM;
+
+	if (get_securelevel() > 0)
 		return -EPERM;
 
 	lock_system_sleep();
