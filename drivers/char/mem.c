3fab26a9585e895ae41ce6462bc389327f9a0e63
Matthew Garrett <matthew.garrett@nebula.com>
2013-09-09 15:49:39 -0700

    Restrict /dev/mem and /dev/kmem when securelevel is set.
    Orabug: 21539498
    
    Allowing users to write to address space provides mechanisms that may permit
    modification of the kernel at runtime. Prevent this if securelevel has been
    set.
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -167,6 +167,9 @@ static ssize_t write_mem(struct file *file, const char __user *buf,
 	if (p != *ppos)
 		return -EFBIG;
 
+	if (get_securelevel() > 0)
+		return -EPERM;
+
 	if (!valid_phys_addr_range(p, count))
 		return -EFAULT;
 
@@ -512,6 +515,9 @@ static ssize_t write_kmem(struct file *file, const char __user *buf,
 	ssize_t virtr = 0;
 	char *kbuf; /* k-addr because vwrite() takes vmlist_lock rwlock */
 	int err = 0;
+
+	if (get_securelevel() > 0)
+		return -EPERM;
 
 	if (p < (unsigned long) high_memory) {
 		unsigned long to_write = min_t(unsigned long, count,
