ef8c879bcca2ab7a5f8830df9f76a70d3b04a004
Bang Nguyen <bang.nguyen@oracle.com>
2013-09-11 14:09:04 -0700

    RDMA CM: Add reason code for IB_CM_REJ_CONSUMER_DEFINED
    RDS: Support rolling downgrade from version 4.1 to 3.1
    
    Orabug: 17484682
    
    Signed-off-by: Giri Adari <giri.adari@oracle.com>
    Signed-off-by: Richard Frank <richard.frank@oracle.com>
    Signed-off-by: Chien-Hua Yen <chien.yen@oracle.com>
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 5e86bae962373cc71c860fb9cb992aef9e039786)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/ib_cm.c
    	drivers/infiniband/core/cm.c
    	drivers/infiniband/core/cma.c
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit e4a4c4526c83b605b2a9088fe5ff067514f29069)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit b6272e1bfcec8e40ed14ad8a86d068260c4bc09a)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.c
+++ net/rds/ib.c
@@ -295,7 +295,7 @@ static int rds_ib_conn_info_visitor(struct rds_connection *conn,
 				    void *buffer)
 {
 	struct rds_info_rdma_connection *iinfo = buffer;
-	struct rds_ib_connection *ic;
+	struct rds_ib_connection *ic = conn->c_transport_data;
 
 	/* We will only ever look at IB transports */
 	if (conn->c_trans != &rds_ib_transport)
@@ -306,6 +306,12 @@ static int rds_ib_conn_info_visitor(struct rds_connection *conn,
 
 	memset(&iinfo->src_gid, 0, sizeof(iinfo->src_gid));
 	memset(&iinfo->dst_gid, 0, sizeof(iinfo->dst_gid));
+
+	if (ic) {
+		iinfo->tos = conn->c_tos;
+		iinfo->sl = ic->i_sl;
+	}
+
 	if (rds_conn_state(conn) == RDS_CONN_UP) {
 		struct rds_ib_device *rds_ibdev;
 		struct rdma_dev_addr *dev_addr;
@@ -332,8 +338,6 @@ static int rds_ib_conn_info_visitor(struct rds_connection *conn,
 		iinfo->max_recv_wr = ic->i_recv_ring.w_nr;
 		iinfo->max_send_sge = rds_ibdev->max_sge;
 		rds_ib_get_mr_info(rds_ibdev, iinfo);
-		iinfo->tos = ic->conn->c_tos;
-		iinfo->sl = ic->i_sl;
 		iinfo->cache_allocs = atomic_read(&ic->i_cache_allocs);
 	}
 	return 1;
