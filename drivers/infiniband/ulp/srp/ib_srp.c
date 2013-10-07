a5bf097da9830e853c5ca8d879657840a2c11aab
Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
2013-10-06 17:04:24 -0700

    srp: disable fmr-pool cache to fix cpu lock-up.
    Orabug: 18158334
    
    Reported-and-Tested-by: Bart Van Assche <BVanAssche@fusionio.com>
    Cc: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    Cc: Guru Anbalagane <guru.anbalagane@oracle.com>
    Signed-off-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
    Reviewed-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>
    (cherry picked from commit 6a5e9891f3f8bf0afd30aae77ca92b1cdf0ac664)
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>
    
    Conflicts:
    	drivers/infiniband/ulp/srp/ib_srp.c

--- drivers/infiniband/ulp/srp/ib_srp.c
+++ drivers/infiniband/ulp/srp/ib_srp.c
@@ -2459,7 +2459,7 @@ static void srp_add_one(struct ib_device *device)
 		memset(&fmr_param, 0, sizeof fmr_param);
 		fmr_param.pool_size	    = SRP_FMR_POOL_SIZE;
 		fmr_param.dirty_watermark   = SRP_FMR_DIRTY_SIZE;
-		fmr_param.cache		    = 1;
+		fmr_param.cache		    = 0;
 		fmr_param.relaxed           = 0;
 		fmr_param.max_pages_per_fmr = max_pages_per_fmr;
 		fmr_param.page_shift	    = fmr_page_shift;
