8cb4c26e4fd60bda8324730b12d8ae63896f7e97
Tej Parkash <tej.parkash@qlogic.com>
2013-11-20 16:59:10 +0530

    qla4xxx: Driver not able to collect minidump in ISP84xx
    Problem:
    minidump data collection fails as driver reports data mismatch
    
    Solution:
    When the driver encounters a new entry type that it cannot process,
    it should just skip the entry and adjust the total buffer size by from
    subtracting the skipped bytes from it. This is to ensure that there
    is no data mismatch because of the new entries.
    
    Signed-off-by: Tej Parkash <tej.parkash@qlogic.com>
    Signed-off-by: Vikas Chaudhary <vikas.chaudhary@qlogic.com>
    
    Orabug: 17960365
    
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>
    Signed-off-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- drivers/scsi/qla4xxx/ql4_def.h
+++ drivers/scsi/qla4xxx/ql4_def.h
@@ -765,6 +765,7 @@ struct scsi_qla_host {
 	uint32_t fw_dump_capture_mask;
 	void *fw_dump_tmplt_hdr;
 	uint32_t fw_dump_tmplt_size;
+	uint32_t fw_dump_skip_size;
 
 	struct completion mbx_intr_comp;
 
