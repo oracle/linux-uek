402f341ea958cd5fe0ee02af4aa9ee7ae8e77abf
Ilya Dryomov <ilya.dryomov@inktank.com>
2014-01-09 20:08:21 +0200

    libceph: add ceph_kv{malloc,free}() and switch to them
    Orabug: 20304006
    
    Encapsulate kmalloc vs vmalloc memory allocation and freeing logic into
    two helpers, ceph_kvmalloc() and ceph_kvfree(), and switch to them.
    
    ceph_kvmalloc() kmalloc()'s a maximum of 8 pages, anything bigger is
    vmalloc()'ed with __GFP_HIGHMEM set.  This changes the existing
    behaviour:
    
    - for buffers (ceph_buffer_new()), from trying to kmalloc() everything
      and using vmalloc() just as a fallback
    
    - for messages (ceph_msg_new()), from going to vmalloc() for anything
      bigger than a page
    
    - for messages (ceph_msg_new()), from disallowing vmalloc() to use high
      memory
    
    Signed-off-by: Ilya Dryomov <ilya.dryomov@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>
    (cherry picked from commit eeb0bed5572b1282009dfc2635604df5a35d1a02)
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- net/ceph/ceph_common.c
+++ net/ceph/ceph_common.c
@@ -15,7 +15,7 @@
 #include <linux/slab.h>
 #include <linux/statfs.h>
 #include <linux/string.h>
-
+#include <linux/vmalloc.h>
 
 #include <linux/ceph/ceph_features.h>
 #include <linux/ceph/libceph.h>
@@ -167,6 +167,25 @@ int ceph_compare_options(struct ceph_options *new_opt,
 	return -1;
 }
 EXPORT_SYMBOL(ceph_compare_options);
+
+void *ceph_kvmalloc(size_t size, gfp_t flags)
+{
+	if (size <= (PAGE_SIZE << PAGE_ALLOC_COSTLY_ORDER)) {
+		void *ptr = kmalloc(size, flags | __GFP_NOWARN);
+		if (ptr)
+			return ptr;
+	}
+
+	return __vmalloc(size, flags | __GFP_HIGHMEM, PAGE_KERNEL);
+}
+
+void ceph_kvfree(const void *ptr)
+{
+	if (is_vmalloc_addr(ptr))
+		vfree(ptr);
+	else
+		kfree(ptr);
+}
 
 
 static int parse_fsid(const char *str, struct ceph_fsid *fsid)
