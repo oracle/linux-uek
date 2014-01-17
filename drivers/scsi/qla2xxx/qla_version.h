71986da289157a63daea0607e19b51ac20c6277b
Saurav Kashyap <saurav.kashyap@qlogic.com>
2014-01-18 03:57:06 +0530

    qla2xxx: Correct the port number assignment for ISP82XX.
    Orabug: 18280331
    
    Signed-off-by: Saurav Kashyap <saurav.kashyap@qlogic.com>
    Acked-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>
    Acked-by: Guru Anbalagane <guru.anbalagane@oracle.com>

--- drivers/scsi/qla2xxx/qla_version.h
+++ drivers/scsi/qla2xxx/qla_version.h
@@ -7,7 +7,7 @@
 /*
  * Driver version
  */
-#define QLA2XXX_VERSION      "8.06.00.14.39.0-k"
+#define QLA2XXX_VERSION      "8.06.00.14.39.0-k1"
 
 #define QLA_DRIVER_MAJOR_VER	8
 #define QLA_DRIVER_MINOR_VER	6
