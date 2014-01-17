d32491089f308a775c15047ca2f03f2c4849c3d5
Ethan Zhao <ethan.zhao@oracle.com>
2014-01-17 09:36:53 -0500

    tg3: remove spin_lock_bh() in tg3_get_stats64() to fix dead lock
    when issue ifconfig eth1 mtu  1504 on tg3 device,hit dead lock
    BUG: scheduling while atomic: ifconfig/3866/0x10000200
    
    BUG: soft lockup - CPU#0 stuck for 22s! [ifconfig:3866]
    Call Trace:
     [<ffffffff815941ae>] _raw_spin_lock_bh+0x1e/0x30
     [<ffffffffa046614c>] tg3_get_stats64+0x3c/0x90 [tg3]
     [<ffffffff814c3b7d>] dev_get_stats+0x3d/0xa0
     [<ffffffff814d7d30>] rtnl_fill_ifinfo+0x400/0xb10
     [<ffffffff814b8aeb>] ? __kmalloc_reserve+0x3b/0xa0
     [<ffffffff8117c3b9>] ? ksize+0x19/0x60
     [<ffffffff814b8bf8>] ? __alloc_skb+0xa8/0x1e0
     [<ffffffff814d86a3>] rtmsg_ifinfo+0x83/0x100
     [<ffffffff814d8741>] rtnetlink_event+0x21/0x40
     [<ffffffff815982e5>] notifier_call_chain+0x55/0x80
     [<ffffffff810895c6>] raw_notifier_call_chain+0x16/0x20
     [<ffffffff814c5717>] call_netdevice_notifiers+0x37/0x70
     [<ffffffff814c6516>] netdev_features_change+0x16/0x20
     [<ffffffff814c6572>] netdev_update_features+0x22/0x30
     [<ffffffffa0463639>] tg3_set_mtu+0x69/0x90 [tg3]
     [<ffffffffa047c5c5>] tg3_change_mtu+0xa5/0x100 [tg3]
     [<ffffffff814c61c2>] dev_set_mtu+0x52/0x70
     [<ffffffff814c752d>] dev_ifsioc+0x10d/0x390
     [<ffffffff815917de>] ? mutex_lock+0x1e/0x50
     [<ffffffff814c971d>] dev_ioctl+0x36d/0x4b0
     [<ffffffff8115b0ae>] ? handle_mm_fault+0x17e/0x290
     [<ffffffff814aeafd>] sock_do_ioctl+0x5d/0x70
     [<ffffffff814aef29>] sock_ioctl+0x79/0x2f0
     [<ffffffff811a2a6c>] do_vfs_ioctl+0x8c/0x350
     [<ffffffff811a2dd1>] sys_ioctl+0xa1/0xb0
     [<ffffffff8159cb19>] system_call_fastpath+0x16/0x1b
    
    remove the spin_lock_bh() in tg3_get_stats64() to avoid it.
    
    Orabug: 18070676
    
    Signed-off-by: Ethan Zhao <ethan.zhao@oracle.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>

--- drivers/net/ethernet/broadcom/tg3.c
+++ drivers/net/ethernet/broadcom/tg3.c
@@ -15066,14 +15066,11 @@ static struct rtnl_link_stats64 *tg3_get_stats64(struct net_device *dev,
 {
 	struct tg3 *tp = netdev_priv(dev);
 
-	spin_lock_bh(&tp->lock);
 	if (!tp->hw_stats) {
-		spin_unlock_bh(&tp->lock);
 		return &tp->net_stats_prev;
 	}
 
 	tg3_get_nstats(tp, stats);
-	spin_unlock_bh(&tp->lock);
 
 	return stats;
 }
