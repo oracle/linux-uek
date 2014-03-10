c7039f56930d2f932b0bcfeea060dc6c1fe9fdb0
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2014-03-11 02:09:08 +0530

    arm64, hw_breakpoint.c: Fix CPU hotplug callback registration
    Subsystems that want to register CPU hotplug callbacks, as well as perform
    initialization for the CPUs that are already online, often do it as shown
    below:
    
    	get_online_cpus();
    
    	for_each_online_cpu(cpu)
    		init_cpu(cpu);
    
    	register_cpu_notifier(&foobar_cpu_notifier);
    
    	put_online_cpus();
    
    This is wrong, since it is prone to ABBA deadlocks involving the
    cpu_add_remove_lock and the cpu_hotplug.lock (when running concurrently
    with CPU hotplug operations).
    
    Instead, the correct and race-free way of performing the callback
    registration is:
    
    	cpu_notifier_register_begin();
    
    	for_each_online_cpu(cpu)
    		init_cpu(cpu);
    
    	/* Note the use of the double underscored version of the API */
    	__register_cpu_notifier(&foobar_cpu_notifier);
    
    	cpu_notifier_register_done();
    
    Fix the hw-breakpoint code in arm64 by using this latter form of callback
    registration.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Lorenzo Pieralisi <Lorenzo.Pieralisi@arm.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Orabug: 20800607
    (cherry picked from commit 3d0dc643f81ed64e2a7f839e8491728addbc51b9)
    Signed-off-by: Vaughan Cao <vaughan.cao@oracle.com>
    Reviewed-by: Ethan Zhao <ethan.zhao@oracle.com>
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    
    Conflicts:
    	arch/arm64/kernel/hw_breakpoint.c
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- arch/arm64/kernel/hw_breakpoint.c
+++ arch/arm64/kernel/hw_breakpoint.c
@@ -846,6 +846,8 @@ static int __init arch_hw_breakpoint_init(void)
 	pr_info("found %d breakpoint and %d watchpoint registers.\n",
 		core_num_brps, core_num_wrps);
 
+	cpu_notifier_register_begin();
+
 	/*
 	 * Reset the breakpoint resources. We assume that a halting
 	 * debugger will leave the world in a nice state for us.
@@ -860,7 +862,9 @@ static int __init arch_hw_breakpoint_init(void)
 			      TRAP_HWBKPT, "hw-watchpoint handler");
 
 	/* Register hotplug notifier. */
-	register_cpu_notifier(&hw_breakpoint_reset_nb);
+	__register_cpu_notifier(&hw_breakpoint_reset_nb);
+
+	cpu_notifier_register_done();
 
 	return 0;
 }
