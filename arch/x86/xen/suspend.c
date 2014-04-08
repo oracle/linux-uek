d4022337de045ed341c8e24becbb32fa6914cc53
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2014-04-08 11:03:23 -0400

    xen/pvhvm: Support more than 32 VCPUs when migrating.
    When Xen migrates an HVM guest, by default its shared_info can
    only hold up to 32 CPUs. As such the hypercall
    VCPUOP_register_vcpu_info was introduced which allowed us to
    setup per-page areas for VCPUs. This means we can boot PVHVM
    guest with more than 32 VCPUs. During migration the per-cpu
    structure is allocated freshly by the hypervisor (vcpu_info_mfn
    is set to INVALID_MFN) so that the newly migrated guest
    can make an VCPUOP_register_vcpu_info hypercall.
    
    Unfortunatly we end up triggering this condition in Xen:
    /* Run this command on yourself or on other offline VCPUS. */
     if ( (v != current) && !test_bit(_VPF_down, &v->pause_flags) )
    
    which means we are unable to setup the per-cpu VCPU structures
    for running vCPUS. The Linux PV code paths make this work by
    iterating over every vCPU with:
    
     1) is target CPU up (VCPUOP_is_up hypercall?)
     2) if yes, then VCPUOP_down to pause it.
     3) VCPUOP_register_vcpu_info
     4) if it was down, then VCPUOP_up to bring it back up
    
    But since VCPUOP_down, VCPUOP_is_up, and VCPUOP_up are
    not allowed on HVM guests we can't do this. However with the
    Xen commit "hvm: Support more than 32 VCPUS when migrating."
    we can do this. As such first check if VCPUOP_is_up is actually
    possible before trying this dance.
    
    As most of this dance code is done already in 'xen_setup_vcpu'
    lets make it callable on both PV and HVM. This means moving one
    of the checks out to 'xen_setup_runstate_info'.
    
    Orabug: 18552664
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Acked-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- arch/x86/xen/suspend.c
+++ arch/x86/xen/suspend.c
@@ -33,11 +33,7 @@ void xen_arch_hvm_post_suspend(int suspend_cancelled)
 	xen_hvm_init_shared_info();
 	xen_callback_vector();
 	xen_unplug_emulated_devices();
-	if (xen_feature(XENFEAT_hvm_safe_pvclock)) {
-		for_each_online_cpu(cpu) {
-			xen_setup_runstate_info(cpu);
-		}
-	}
+	xen_vcpu_restore();
 #endif
 }
 
