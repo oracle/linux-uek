d165178591d5fd50e7a347b26021e1b2ac9a0b61
Matthew Wilcox <matthew.r.wilcox@intel.com>
2014-04-11 11:58:45 -0400

    NVMe: Improve error messages
    Help people diagnose what is going wrong at initialisation time by
    printing out which command has gone wrong and what the device returned.
    Also fix the error message printed while waiting for reset.
    
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>
    Reviewed-by: Keith Busch <keith.busch@intel.com>
    (cherry picked from commit 27e8166c31656f7780e682eaf6bc9c3b8dd03253)
    
    Orabug: 18940591
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Acked-by: Jerry Snitselaar <jerry.snitselaar@oracle.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -1436,7 +1436,8 @@ static int nvme_wait_ready(struct nvme_dev *dev, u64 cap, bool enabled)
 			return -EINTR;
 		if (time_after(jiffies, timeout)) {
 			dev_err(&dev->pci_dev->dev,
-				"Device not ready; aborting initialisation\n");
+				"Device not ready; aborting %s\n", enabled ?
+						"initialisation" : "reset");
 			return -ENODEV;
 		}
 	}
@@ -2143,8 +2144,13 @@ static int set_queue_count(struct nvme_dev *dev, int count)
 
 	status = nvme_set_features(dev, NVME_FEAT_NUM_QUEUES, q_count, 0,
 								&result);
-	if (status)
-		return status < 0 ? -EIO : -EBUSY;
+	if (status < 0)
+		return status;
+	if (status > 0) {
+		dev_err(&dev->pci_dev->dev, "Could not set queue count (%d)\n",
+									status);
+		return -EBUSY;
+	}
 	return min(result & 0xffff, result >> 16) + 1;
 }
 
@@ -2280,6 +2286,7 @@ static int nvme_dev_add(struct nvme_dev *dev)
 
 	res = nvme_identify(dev, 0, 1, dma_addr);
 	if (res) {
+		dev_err(&pdev->dev, "Identify Controller failed (%d)\n", res);
 		res = -EIO;
 		goto out;
 	}
