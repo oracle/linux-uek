3eab6d40e048a3b34bf3dc1afe2a6df3c1081c1d
Chien-Hua Yen <chien.yen@oracle.com>
2014-05-27 12:35:25 -0700

    RDS: Use rds_local_wq for loopback connections in rds_conn_connect_if_down()
    This patch extends commit 0715fe8 "RDS: add workqueue for local
    loopback connections" to rds_conn_connect_if_down() and
    rds_connect_complete().
    
    Orabug: 18892380
    
    Signed-off-by: Chien-Hua Yen <chien.yen@oracle.com>
    Signed-off-by: Bang Nguyen <bang.nguyen@oracle.com>
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 50b92232deb0d9cb1b5491ce33252c6324511579)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 3964b0c6acf6919d3f90fe841c6ad53dd9136693)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 81797cbb0872669481fa2893dba32c5ea1b6b69e)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/connection.c
+++ net/rds/connection.c
@@ -656,8 +656,12 @@ EXPORT_SYMBOL_GPL(rds_conn_drop);
 void rds_conn_connect_if_down(struct rds_connection *conn)
 {
 	if (rds_conn_state(conn) == RDS_CONN_DOWN &&
-	    !test_and_set_bit(RDS_RECONNECT_PENDING, &conn->c_flags))
-		queue_delayed_work(rds_wq, &conn->c_conn_w, 0);
+	    !test_and_set_bit(RDS_RECONNECT_PENDING, &conn->c_flags)) {
+		if (conn->c_loopback)
+			queue_delayed_work(rds_local_wq, &conn->c_conn_w, 0);
+		else
+			queue_delayed_work(rds_wq, &conn->c_conn_w, 0);
+	}
 }
 EXPORT_SYMBOL_GPL(rds_conn_connect_if_down);
 
