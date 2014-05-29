0e17caeef2e56737d610f2fc38f6bdadd08aa5e0
Stephen M. Cameron <scameron@beardog.cce.hp.com>
2014-05-29 10:52:52 -0500

    hpsa: use gcc aligned attribute instead of manually padding structs
    Signed-off-by: Stephen M. Cameron <scameron@beardog.cce.hp.com>
    Reviewed-by: Webb Scales <webb.scales@hp.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 35d697c451678c3bf3f8a24f8e2b3918d1731bc5)
    
    Orabug: 19137821
    Signed-off-by: Vaughan Cao <vaughan.cao@oracle.com>
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -6939,7 +6939,6 @@ reinit_after_soft_reset:
 	 * the 5 lower bits of the address are used by the hardware. and by
 	 * the driver.  See comments in hpsa.h for more info.
 	 */
-#define COMMANDLIST_ALIGNMENT 128
 	BUILD_BUG_ON(sizeof(struct CommandList) % COMMANDLIST_ALIGNMENT);
 	h = kzalloc(sizeof(*h), GFP_KERNEL);
 	if (!h)
@@ -7412,7 +7411,6 @@ static int hpsa_alloc_ioaccel_cmd_and_bft(struct ctlr_info *h)
 	 * because the 7 lower bits of the address are used by the
 	 * hardware.
 	 */
-#define IOACCEL1_COMMANDLIST_ALIGNMENT 128
 	BUILD_BUG_ON(sizeof(struct io_accel1_cmd) %
 			IOACCEL1_COMMANDLIST_ALIGNMENT);
 	h->ioaccel_cmd_pool =
@@ -7450,7 +7448,6 @@ static int ioaccel2_alloc_cmds_and_bft(struct ctlr_info *h)
 	if (h->ioaccel_maxsg > IOACCEL2_MAXSGENTRIES)
 		h->ioaccel_maxsg = IOACCEL2_MAXSGENTRIES;
 
-#define IOACCEL2_COMMANDLIST_ALIGNMENT 128
 	BUILD_BUG_ON(sizeof(struct io_accel2_cmd) %
 			IOACCEL2_COMMANDLIST_ALIGNMENT);
 	h->ioaccel2_cmd_pool =
