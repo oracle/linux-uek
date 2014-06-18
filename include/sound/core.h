fb75874810b9d39ed719580f53711ea07a4efe15
Lars-Peter Clausen <lars@metafoo.de>
2014-06-18 13:32:31 +0200

    ALSA: control: Protect user controls against concurrent access
    The user-control put and get handlers as well as the tlv do not protect against
    concurrent access from multiple threads. Since the state of the control is not
    updated atomically it is possible that either two write operations or a write
    and a read operation race against each other. Both can lead to arbitrary memory
    disclosure. This patch introduces a new lock that protects user-controls from
    concurrent access. Since applications typically access controls sequentially
    than in parallel a single lock per card should be fine.
    
    Orabug: 20192542
    CVE: CVE-2014-4652
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Jaroslav Kysela <perex@perex.cz>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 07f4d9d74a04aa7c72c5dae0ef97565f28f17b92)
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>

--- include/sound/core.h
+++ include/sound/core.h
@@ -120,6 +120,10 @@ struct snd_card {
 	int user_ctl_count;		/* count of all user controls */
 	struct list_head controls;	/* all controls for this card */
 	struct list_head ctl_files;	/* active control files */
+#ifndef __GENKSYMS__
+	struct mutex user_ctl_lock;	/* protects user controls against
+					   concurrent access */
+#endif
 
 	struct snd_info_entry *proc_root;	/* root for soundcard specific files */
 	struct snd_info_entry *proc_id;	/* the card id */
