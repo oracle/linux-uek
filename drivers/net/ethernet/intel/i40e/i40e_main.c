fb0e003bbe62f4f312a834c5d3e5712c9f394eb8
Catherine Sullivan <catherine.sullivan@intel.com>
2014-07-09 07:46:17 +0000

    i40e: Fix firmware API version errors
    Orabug: 20147320
    
    Reword the error messages. Also add a major version check because
    We only want to warn on nvm_minor > expected_minor if
    nvm_major == expected_major. Lastly, change an if to an else if
    because the two statements will never evaluate to true at the same time.
    
    Change-ID: I6ddf9986f26b35f6879cbeac4fcef04a8497a383
    Signed-off-by: Catherine Sullivan <catherine.sullivan@intel.com>
    Tested-by: Jim Young <jamesx.m.young@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 7aa67613172734d5d6be99db019e57e453f76862)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>
    
    Conflicts:
    	drivers/net/ethernet/intel/i40e/i40e_main.c

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -7681,11 +7681,19 @@ static int i40e_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	dev_info(&pdev->dev, "%s\n", i40e_fw_version_str(hw));
 	if (err) {
 		dev_info(&pdev->dev,
-			 "init_adminq failed: %d expecting API %02x.%02x\n",
-			 err,
-			 I40E_FW_API_VERSION_MAJOR, I40E_FW_API_VERSION_MINOR);
+			 "The driver for the device stopped because the NVM image is newer than expected. You must install the most recent version of the network driver.\n");
 		goto err_pf_reset;
 	}
+
+	if (hw->aq.api_maj_ver == I40E_FW_API_VERSION_MAJOR &&
+	    hw->aq.api_min_ver > I40E_FW_API_VERSION_MINOR)
+		dev_info(&pdev->dev,
+			 "The driver for the device detected a newer version of the NVM image than expected. Please install the most recent version of the network driver.\n");
+	else if (hw->aq.api_maj_ver < I40E_FW_API_VERSION_MAJOR ||
+		 hw->aq.api_min_ver < (I40E_FW_API_VERSION_MINOR - 1))
+		dev_info(&pdev->dev,
+			 "The driver for the device detected an older version of the NVM image than expected. Please update the NVM image.\n");
+
 
 	i40e_verify_eeprom(pf);
 
