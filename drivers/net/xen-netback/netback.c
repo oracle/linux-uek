f452cdfcd32329392847909e977c6eadaea8522d
Zoltan Kiss <zoltan.kiss@citrix.com>
2014-07-18 19:08:05 +0100

    xen-netback: Fix pointer incrementation to avoid incorrect logging
    Orabug: 21976319
    
    Due to this pointer is increased prematurely, the error log contains rubbish.
    
    Signed-off-by: Zoltan Kiss <zoltan.kiss@citrix.com>
    Reported-by: Armin Zentai <armin.zentai@ezit.hu>
    Cc: netdev@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: xen-devel@lists.xenproject.org
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit d8cfbfc4660054150ca1b7c501a8edc0771022f9)
    Signed-off-by: Annie Li <annie.li@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/xen-netback/netback.c
+++ drivers/net/xen-netback/netback.c
@@ -963,7 +963,6 @@ static int xenvif_tx_check_gop(struct xenvif_queue *queue,
 
 	/* Check status of header. */
 	err = (*gopp_copy)->status;
-	(*gopp_copy)++;
 	if (unlikely(err)) {
 		if (net_ratelimit())
 			netdev_dbg(queue->vif->dev,
@@ -976,6 +975,7 @@ static int xenvif_tx_check_gop(struct xenvif_queue *queue,
 			xenvif_idx_release(queue, pending_idx,
 					   XEN_NETIF_RSP_ERROR);
 	}
+	(*gopp_copy)++;
 
 check_frags:
 	for (i = 0; i < nr_frags; i++, gop_map++) {
