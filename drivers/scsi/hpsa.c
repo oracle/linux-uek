74bd7617a2a4cf54e464977964bbaf72856d3989
Nicholas Bellinger <nab@linux-iscsi.org>
2014-11-14 17:27:19 -0600

    hpsa: Convert SCSI LLD ->queuecommand() for host_lock less operation
    There isn't anything in hpsa that requires the host lock to be held
    during queuecommand.
    
    Signed-off-by: Don Brace <don.brace@pmcs.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    Reviewed-by: Stephen M. Cameron <stephenmcameron@gmail.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    
    Orabug: 20363086
    (cherry picked from commit 763aadbf5015e86e93d209f10e34fd4daacc459b)
    Signed-off-by: Vaughan Cao <vaughan.cao@oracle.com>
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- drivers/scsi/hpsa.c
+++ drivers/scsi/hpsa.c
@@ -3885,8 +3885,11 @@ static int hpsa_scsi_ioaccel_raid_map(struct ctlr_info *h,
 						dev->scsi3addr);
 }
 
-static int hpsa_scsi_queue_command_lck(struct scsi_cmnd *cmd,
-	void (*done)(struct scsi_cmnd *))
+/*
+ * Running in struct Scsi_Host->host_lock less mode using LLD internal
+ * struct ctlr_info *h->lock w/ spin_lock_irqsave() protection.
+ */
+static int hpsa_scsi_queue_command(struct Scsi_Host *sh, struct scsi_cmnd *cmd)
 {
 	struct ctlr_info *h;
 	struct hpsa_scsi_dev_t *dev;
@@ -3899,14 +3902,14 @@ static int hpsa_scsi_queue_command_lck(struct scsi_cmnd *cmd,
 	dev = cmd->device->hostdata;
 	if (!dev) {
 		cmd->result = DID_NO_CONNECT << 16;
-		done(cmd);
+		cmd->scsi_done(cmd);
 		return 0;
 	}
 	memcpy(scsi3addr, dev->scsi3addr, sizeof(scsi3addr));
 
 	if (unlikely(lockup_detected(h))) {
 		cmd->result = DID_ERROR << 16;
-		done(cmd);
+		cmd->scsi_done(cmd);
 		return 0;
 	}
 	c = cmd_alloc(h);
@@ -3916,9 +3919,6 @@ static int hpsa_scsi_queue_command_lck(struct scsi_cmnd *cmd,
 	}
 
 	/* Fill in the command list header */
-
-	cmd->scsi_done = done;    /* save this for use by completion code */
-
 	/* save c in case we have to abort it  */
 	cmd->host_scribble = (unsigned char *) c;
 
@@ -4008,8 +4008,6 @@ static int hpsa_scsi_queue_command_lck(struct scsi_cmnd *cmd,
 	/* the cmd'll come back via intr handler in complete_scsi_command()  */
 	return 0;
 }
-
-static DEF_SCSI_QCMD(hpsa_scsi_queue_command)
 
 static int do_not_scan_if_controller_locked_up(struct ctlr_info *h)
 {
