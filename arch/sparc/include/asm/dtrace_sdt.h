cb83aff97b33ea5f47ab6a3d7e4a1d26940ed13e
Kris Van Hees <kris.van.hees@oracle.com>
2014-12-22 16:56:13 -0500

    dtrace: add support for sparc64 3of3
    This commit is the 3rd of 3 commits to add DTrace support to the sparc64
    kernel.  This particular commit covers the DTrace implementation code
    changes.  It provides an implementation of the DTrace core for sparc64,
    SDT, and syscall tracing.
    
    Note that the sparc64 implementation for SDT probe points utilizes a call
    to a trampoline function that in turn calls dtrace_probe() rather than
    using a trap-based mechanism.  This requires an additional data item to be
    associated with each module.  In order to facilitate this in a manner that
    is arch-dependent, DTrace specific data other than the list of SDT probes
    has been moved to a structure that is maintained by the DTrace module code
    and the module struct merely has a generic void *pdata member that can be
    populated with a pointer to the actual data.  This commit renames the
    num_dtrace_probes member of the module struct to be sdt_probec, since that
    is more consistent.
    
    Orabug: 19005031
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Acked-by: Nick Alcock <nick.alcock@oracle.com>

---
+++ arch/sparc/include/asm/dtrace_sdt.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2013,2014 Oracle, Inc. */
+
+#ifndef _SPARC_DTRACE_SDT_H
+#define _SPARC_DTRACE_SDT_H
+
+typedef uint32_t	sdt_instr_t;
+
+#define SDT_TRAMP_SIZE	11
+
+#endif /* _SPARC_DTRACE_SDT_H */
---
+++ arch/sparc/include/asm/dtrace_util.h
@@ -0,0 +1,8 @@
+/* Copyright (C) 2013,2014 Oracle, Inc. */
+
+#ifndef _SPARC_DTRACE_UTIL_H
+#define _SPARC_DTRACE_UTIL_H
+
+/* Nothing for now */
+
+#endif /* _SPARC_DTRACE_UTIL_H */
