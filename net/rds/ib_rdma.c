a543614af91c78a15e8c15cebc86587bfeaa54d7
Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
2015-01-22 18:27:30 -0800

    rds: port to UEK4, Linux-3.18*
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 28c178a324dffb593071305c3d7497afa2d39fb8)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/af_rds.c
    	net/rds/ib.c
    	net/rds/ib.h
    	net/rds/ib_cm.c
    	net/rds/ib_sysctl.c
    	net/rds/iw.c
    	net/rds/iw.h
    	net/rds/iw_cm.c
    	net/rds/iw_recv.c
    	net/rds/iw_sysctl.c
    	net/rds/rdma_transport.c
    	net/rds/rds.h
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- net/rds/ib_rdma.c
+++ net/rds/ib_rdma.c
@@ -544,7 +544,7 @@ static void __rds_ib_teardown_mr(struct rds_ib_mr *ibmr)
 
 			/* FIXME we need a way to tell a r/w MR
 			 * from a r/o MR */
-			WARN_ON_ONCE(page_mapping(page) && irqs_disabled());
+			WARN_ON_ONCE(!page->mapping && irqs_disabled());
 			set_page_dirty(page);
 			put_page(page);
 		}
