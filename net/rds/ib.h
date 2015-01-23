8e6e74444bb072568df29698e4ee877dbb822a36
Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
2015-01-22 18:27:30 -0800

    rds: port to UEK4, Linux-3.18*
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 28c178a324dffb593071305c3d7497afa2d39fb8)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/af_rds.c
    	net/rds/ib.c
    	net/rds/ib.h
    	net/rds/ib_cm.c
    	net/rds/ib_sysctl.c
    	net/rds/iw.c
    	net/rds/iw.h
    	net/rds/iw_cm.c
    	net/rds/iw_recv.c
    	net/rds/iw_sysctl.c
    	net/rds/rdma_transport.c
    	net/rds/rds.h
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit a543614af91c78a15e8c15cebc86587bfeaa54d7)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 2df84430f07c35dab933a521e548df7ead929f68)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -560,6 +560,5 @@ extern unsigned long rds_ib_sysctl_max_unsig_wrs;
 extern unsigned long rds_ib_sysctl_max_unsig_bytes;
 extern unsigned long rds_ib_sysctl_max_recv_allocation;
 extern unsigned int rds_ib_sysctl_flow_control;
-extern struct ctl_table rds_ib_sysctl_table[];
 
 #endif
