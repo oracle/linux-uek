ef64238d7dca31191cdf6aa8fcefc0cf16879bb9
rkennedy <rkennedy@emulex.com>
2015-02-13 10:02:25 -0800

    Update lpfc version for 10.6.61
    Orabug: 20539686
    
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- drivers/scsi/lpfc/lpfc_version.h
+++ drivers/scsi/lpfc/lpfc_version.h
@@ -18,7 +18,7 @@
  * included with this package.                                     *
  *******************************************************************/
 
-#define LPFC_DRIVER_VERSION "10.2.8061.0"
+#define LPFC_DRIVER_VERSION "10.6.61.0"
 #define LPFC_DRIVER_NAME		"lpfc"
 
 /* Used for SLI 2/3 */
