ba129efeb8f0027df28065f074c9416abddfd43e
Mitch Williams <mitch.a.williams@intel.com>
2015-02-24 06:58:51 +0000

    i40evf: ethtool RSS fixes
    Orabug: 20639907
    
    Add an extra check to make sure that the indirection table pointer is
    valid before dereferencing it.
    
    Change-ID: I698adbf3daff03081d01f489dc95a9f1ad8b12f1
    Reported-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit ed250ecc5546649e0d8a1b8f000594314a666547)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	drivers/net/ethernet/intel/i40evf/i40evf_ethtool.c
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- drivers/net/ethernet/intel/i40evf/i40evf_ethtool.c
+++ drivers/net/ethernet/intel/i40evf/i40evf_ethtool.c
@@ -636,12 +636,17 @@ static int i40evf_get_rxfh_indir(struct net_device *netdev, u32 *indir)
 	u32 hlut_val;
 	int i, j;
 
-	for (i = 0, j = 0; i <= I40E_VFQF_HLUT_MAX_INDEX; i++) {
-		hlut_val = rd32(hw, I40E_VFQF_HLUT(i));
-		indir[j++] = hlut_val & 0xff;
-		indir[j++] = (hlut_val >> 8) & 0xff;
-		indir[j++] = (hlut_val >> 16) & 0xff;
-		indir[j++] = (hlut_val >> 24) & 0xff;
+	if (!indir)
+		return 0;
+
+	if (indir) {
+		for (i = 0, j = 0; i <= I40E_VFQF_HLUT_MAX_INDEX; i++) {
+			hlut_val = rd32(hw, I40E_VFQF_HLUT(i));
+			indir[j++] = hlut_val & 0xff;
+			indir[j++] = (hlut_val >> 8) & 0xff;
+			indir[j++] = (hlut_val >> 16) & 0xff;
+			indir[j++] = (hlut_val >> 24) & 0xff;
+		}
 	}
 	return 0;
 }
