dd3b173baf5c8defe032cb5def11dfbca6147124
Guangyu Sun <guangyu.sun@oracle.com>
2015-03-11 10:01:44 -0700

    Revert "mm: Fix NULL pointer dereference in madvise(MADV_WILLNEED) support"
    Orabug: 20673281
    
    This reverts commit dd5a9a661056c96aecaf0bc48517ed5fad024dc6.
    
    The patch fixes a bug introduced in commit 1998cc048901 ("mm: make
    madvise(MADV_WILLNEED) support swap file prefetch"), which UEK2 does not
    have. So UEK2 is not vunerable to CVE-2014-8173.
    
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- include/asm-generic/pgtable.h
+++ include/asm-generic/pgtable.h
@@ -498,10 +498,11 @@ static inline int pmd_none_or_trans_huge_or_clear_bad(pmd_t *pmd)
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	barrier();
 #endif
-	if (pmd_none(pmdval) || pmd_trans_huge(pmdval))
+	if (pmd_none(pmdval))
 		return 1;
 	if (unlikely(pmd_bad(pmdval))) {
-		pmd_clear_bad(pmd);
+		if (!pmd_trans_huge(pmdval))
+			pmd_clear_bad(pmd);
 		return 1;
 	}
 	return 0;
