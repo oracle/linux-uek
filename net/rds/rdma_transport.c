27f2d6178057c9f4abe4833acf82d5c438b3b4dd
Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
2015-04-02 14:35:07 -0700

    rds: disable APM support
    The APM(Alternate Path Migration) feature is not used and its
    code is being disabled. (It can be re-enabled if/when APM support
    is enabled in rdma_cm.
    
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit f96850e3e3db30f29b6ced7e54e6a5d825aaf1ea)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/ib_cm.c
    	net/rds/rdma_transport.c
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit a676f2a5df3c7e6050c315d1328f25ec822d34f3)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 37347036
    
    UEK7 => LUCI => UEK8
    
    (cherry picked from commit c05e686d5f8dbe52385c28d581ce7a896e3085f7)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/rdma_transport.c
+++ net/rds/rdma_transport.c
@@ -82,10 +82,10 @@ int rds_rdma_cm_event_handler(struct rdma_cm_id *cm_id,
 	case RDMA_CM_EVENT_ADDR_RESOLVED:
 		rdma_set_service_type(cm_id, conn->c_tos);
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if RDMA_RDS_APM_SUPPORTED
 		if (rds_ib_apm_enabled)
 			rdma_set_timeout(cm_id, rds_ib_apm_timeout);
-#endif /* LINUX_VERSION < 4.12.0 */
+#endif
 
 		/* XXX do we need to clean up if this fails? */
 		ret = rdma_resolve_route(cm_id,
@@ -113,7 +113,7 @@ int rds_rdma_cm_event_handler(struct rdma_cm_id *cm_id,
 		ret = trans->cm_initiate_connect(cm_id);
 		break;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 12, 0)
+#if RDMA_RDS_APM_SUPPORTED
 	case RDMA_CM_EVENT_ALT_PATH_LOADED:
 		rdsdebug("RDS: alt path loaded\n");
 		if (conn)
@@ -127,7 +127,7 @@ int rds_rdma_cm_event_handler(struct rdma_cm_id *cm_id,
 	case RDMA_CM_EVENT_ALT_ROUTE_ERROR:
 		rdsdebug("RDS: alt route resolve error\n");
 		break;
-#endif /* LINUX_VERSION < 4.12.0 */
+#endif
 
 	case RDMA_CM_EVENT_ROUTE_ERROR:
 		/* IP might have been moved so flush the ARP entry and retry */
@@ -163,8 +163,13 @@ int rds_rdma_cm_event_handler(struct rdma_cm_id *cm_id,
 		break;
 
 	case RDMA_CM_EVENT_ADDR_CHANGE:
+#if RDMA_RDS_APM_SUPPORTED
 		if (conn && !rds_ib_apm_enabled)
 			rds_conn_drop(conn);
+#else
+		if (conn)
+			rds_conn_drop(conn);
+#endif
 		break;
 
 	case RDMA_CM_EVENT_DISCONNECTED:
