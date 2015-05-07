8a0309a7550f3dff22f33f0a4319c8108e47128d
Vitaly Kuznetsov <vkuznets@redhat.com>
2015-05-06 17:47:41 -0700

    Drivers: hv: vmbus: kill tasklets on module unload
    Explicitly kill tasklets we create on module unload.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 1959a28e2671004c1e9c30ccd2914b868f100742)
    
    Orabug: 21886720
    Signed-off-by: Jason Luo <zhangqing.luo@oracle.com>

--- drivers/hv/vmbus_drv.c
+++ drivers/hv/vmbus_drv.c
@@ -1098,11 +1098,14 @@ static void __exit vmbus_exit(void)
 	hv_synic_clockevents_cleanup();
 	vmbus_disconnect();
 	hv_remove_vmbus_irq();
+	tasklet_kill(&msg_dpc);
 	vmbus_free_channels();
 	bus_unregister(&hv_bus);
 	hv_cleanup();
-	for_each_online_cpu(cpu)
+	for_each_online_cpu(cpu) {
+		tasklet_kill(hv_context.event_dpc[cpu]);
 		smp_call_function_single(cpu, hv_synic_cleanup, NULL, 1);
+	}
 	acpi_bus_unregister_driver(&vmbus_acpi_driver);
 	hv_cpu_hotplug_quirk(false);
 }
