994cb52c0b9eb7cdae6eeb79aaa164b6a7efa11c
Jacob Keller <jacob.e.keller@intel.com>
2015-06-16 13:40:32 -0700

    fm10k: trivial fixup message style to include a colon
    Also use %d for error values, since printing in hexadecimal is probably
    not helpful.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    
    Orabug: 25394529
    (cherry picked from commit 0197cde62a0a39fdee606557b8cef6573a83e866)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_pci.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_pci.c
@@ -1797,7 +1797,7 @@ static int fm10k_probe(struct pci_dev *pdev,
 					   fm10k_driver_name);
 	if (err) {
 		dev_err(&pdev->dev,
-			"pci_request_selected_regions failed 0x%x\n", err);
+			"pci_request_selected_regions failed: %d\n", err);
 		goto err_pci_reg;
 	}
 
