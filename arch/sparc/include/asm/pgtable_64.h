9a8ab4f52d29d9d7efc86c84a86671e62f810db4
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2015-06-24 16:57:44 -0700

    mm: clarify that the function operates on hugepage pte
    We have confusing functions to clear pmd, pmd_clear_* and pmd_clear.  Add
    _huge_ to pmdp_clear functions so that we are clear that they operate on
    hugepage pte.
    
    We don't bother about other functions like pmdp_set_wrprotect,
    pmdp_clear_flush_young, because they operate on PTE bits and hence
    indicate they are operating on hugepage ptes
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Dan Duval <dan.duval@oracle.com>
    (cherry picked from commit 8809aa2d28d74111ff2f1928edaa4e9845c97a7d)
    
    Conflict:
    
    	arch/sparc/include/asm/pgtable_64.h

--- arch/sparc/include/asm/pgtable_64.h
+++ arch/sparc/include/asm/pgtable_64.h
@@ -907,10 +907,10 @@ static void maybe_tlb_batch_add(struct mm_struct *mm, unsigned long vaddr,
 		tlb_batch_add(mm, vaddr, ptep, orig, fullmm);
 }
 
-#define __HAVE_ARCH_PMDP_GET_AND_CLEAR
-static inline pmd_t pmdp_get_and_clear(struct mm_struct *mm,
-				       unsigned long addr,
-				       pmd_t *pmdp)
+#define __HAVE_ARCH_PMDP_HUGE_GET_AND_CLEAR
+static inline pmd_t pmdp_huge_get_and_clear(struct mm_struct *mm,
+					    unsigned long addr,
+					    pmd_t *pmdp)
 {
 	pmd_t pmd = *pmdp;
 	set_pmd_at(mm, addr, pmdp, __pmd(0UL));
