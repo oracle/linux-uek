fc05fa2c2095afc7de89b92141a1a4a382095b96
Kris Van Hees <kris.van.hees@oracle.com>
2015-07-07 23:37:14 -0400

    dtrace: do not allocate space for trampolines when probec = 0
    When it is known that a module does not contain any SDT probes, do
    not try to allocate a 0-sized memory block for trampolines.
    
    Signed-off-by: Kris Van Hees <kris.van.hees@oracle.com>
    Acked-by: Nick Alcock <nick.alcock@oracle.com>
    Acked-by: Allen Pais <allen.pais@oracle.com>

--- arch/sparc/kernel/module.c
+++ arch/sparc/kernel/module.c
@@ -15,10 +15,12 @@
 #include <linux/ctype.h>
 #include <linux/mm.h>
 
-#include <asm/dtrace_arch.h>
 #include <asm/processor.h>
 #include <asm/spitfire.h>
 #include <asm/cacheflush.h>
+#ifdef CONFIG_DTRACE
+# include <asm/dtrace_arch.h>
+#endif
 
 #include "entry.h"
 
@@ -224,18 +226,22 @@ int module_finalize(const Elf_Ehdr *hdr,
 		__asm__ __volatile__("flush %g6");
 	}
 
-#ifdef CONFIG_DTRACE
-	me->pdata = module_alloc(me->sdt_probec * SDT_TRAMP_SIZE *
-				 sizeof(asm_instr_t));
-#endif
+# ifdef CONFIG_DTRACE
+	if (me->sdt_probec > 0)
+		me->pdata = module_alloc(me->sdt_probec * SDT_TRAMP_SIZE *
+					 sizeof(asm_instr_t));
+	else
+		me->pdata = NULL;
+# endif
 
 	return 0;
 }
 
-#ifdef CONFIG_DTRACE
+# ifdef CONFIG_DTRACE
 void module_arch_cleanup(struct module *me)
 {
-	module_memfree(me->pdata);
+	if (me->pdata)
+		module_memfree(me->pdata);
 }
 #endif
 #endif /* CONFIG_SPARC64 */
