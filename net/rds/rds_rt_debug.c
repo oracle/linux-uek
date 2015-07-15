3dc9a46ccbff9f59fa00c00429e042610d0bb1ef
Qing Huang <qing.huang@oracle.com>
2015-07-14 18:36:43 -0700

    ib/rds: runtime debuggability enhancement
    This patch provides the ability to dynamically turn on or off various
    types of debug/diag prints inside the RDS module.
    The run-time debug prints are controlled by a rds module parameter,
    rds_rt_debug_bitmap.
    Here is the definition for different bits. We have implemented feature
    related bits, such as Connection Management, Active Bonding, Error prints,
    Send, Recv.
    in net/rds/rds_rt_debug.h
    ...
    enum {
            /* bit 0 ~ 19 are feature related bits */
            RDS_RTD_ERR                     = 1 << 0,       /* 0x1    */
            RDS_RTD_ERR_EXT                 = 1 << 1,       /* 0x2    */
            RDS_RTD_CM                      = 1 << 3,       /* 0x8    */
            RDS_RTD_CM_EXT                  = 1 << 4,       /* 0x10   */
            RDS_RTD_CM_EXT_P                = 1 << 5,       /* 0x20   */
            RDS_RTD_ACT_BND                 = 1 << 7,       /* 0x80   */
            RDS_RTD_ACT_BND_EXT             = 1 << 8,       /* 0x100  */
            RDS_RTD_RCV                     = 1 << 11,      /* 0x800  */
            RDS_RTD_RCV_EXT                 = 1 << 12,      /* 0x1000 */
            RDS_RTD_SND                     = 1 << 14,      /* 0x4000 */
            RDS_RTD_SND_EXT                 = 1 << 15,      /* 0x8000 */
    ...
    In general, *EXTRA bits mean that you will get extra information but
    possible flood prints as well. But every bit can be controlled by users
    so users can decide how much information they want to see/collect. The
    current embedded printk level used for this patch is KERN_INFO. Most
    likely all the msgs will only go to /var/log/messages without showing up
    on console if we use the default settings for /proc/sys/kernel/printk and
    /etc/rsyslog.conf in ol6 environment.
    E.g if we want to turn on RDS_RTD_ERR and RDS_RTD_CM bits. What we can
    do is
    echo 0x9 > /sys/module/rds/parameters/rds_rt_debug_bitmap
    To turn on RDS_RTD_ERR(0x1), RDS_RTD_CM(0x8), and RDS_RTD_RCV(0x800) bits
    echo 0x809 > /sys/module/rds/parameters/rds_rt_debug_bitmap
    Performance penalty: with all the debug flag bits set to 0, there
    should be no porformance impact in a kernel with this patch.
    Orabug 21314268
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Mukesh Kacker <mukesh.kacker@oracle.com>
    
    Reviewed-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    Reviewed-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
    Reviewed-by: Yuval Shaia <yuval.shaia@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit df7707e8c14e29a96e076fb08eabc7d9a9f39194)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/ib.c
    	net/rds/ib_cm.c
    	net/rds/rdma_transport.c
    	net/rds/recv.c
    	net/rds/threads.c
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 6716883d64ffb5a21daf16a2b20bab12a4a9bbd6)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit fa45128ff4fd8bd1eaae200bd6f6b9b231cd30d1)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

---
+++ net/rds/rds_rt_debug.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Oracle.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#include <linux/kernel.h>
+
+#include "rds.h"
+#include "rds_rt_debug.h"
+
+u32 kernel_rds_rt_debug_bitmap;
+EXPORT_SYMBOL(kernel_rds_rt_debug_bitmap);
+
+module_param_named(rds_rt_debug_bitmap, kernel_rds_rt_debug_bitmap, uint, 0644);
+MODULE_PARM_DESC(rds_rt_debug_bitmap, "RDS Runtime Debug Message Enabling Bitmap [default 0]");
+
