d087a64c4241404b04386fbd37f1c1b7d25e416c
Tariq Saeed <tariq.x.saeed@oracle.com>
2015-07-28 17:24:50 -0700

    chmod can cause hang in a cluster
    Orabug: 21496050
    
    While holding inode lock EX, ocfs2_setattr calls ocfs2_acl_chmod which calls
    ocfs2_get_acl which takes inode lock PR since it is a VFS entry point. This
    can lead to lead deadlock if another node is blocked on the lock with blocking
    level EX. Fix is to call ocfs2_get_acl_nolock in this code path.
    
    Signed-off-by: Tariq Saeed <tariq.x.saeed@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- fs/ocfs2/acl.c
+++ fs/ocfs2/acl.c
@@ -327,7 +327,7 @@ int ocfs2_check_acl(struct inode *inode, int mask, unsigned int flags)
 	return -EAGAIN;
 }
 
-int ocfs2_acl_chmod(struct inode *inode)
+int ocfs2_acl_chmod(struct inode *inode, struct buffer_head *bh)
 {
 	struct ocfs2_super *osb = OCFS2_SB(inode->i_sb);
 	struct posix_acl *acl, *clone;
@@ -339,7 +339,7 @@ int ocfs2_acl_chmod(struct inode *inode)
 	if (!(osb->s_mount_opt & OCFS2_MOUNT_POSIX_ACL))
 		return 0;
 
-	acl = ocfs2_get_acl(inode, ACL_TYPE_ACCESS);
+	acl = ocfs2_get_acl_nolock(inode, ACL_TYPE_ACCESS, bh);
 	if (IS_ERR(acl) || !acl)
 		return PTR_ERR(acl);
 	clone = posix_acl_clone(acl, GFP_KERNEL);
--- fs/ocfs2/acl.h
+++ fs/ocfs2/acl.h
@@ -27,7 +27,7 @@ struct ocfs2_acl_entry {
 };
 
 extern int ocfs2_check_acl(struct inode *, int, unsigned int);
-extern int ocfs2_acl_chmod(struct inode *);
+extern int ocfs2_acl_chmod(struct inode *, struct buffer_head *);
 extern int ocfs2_init_acl(handle_t *, struct inode *, struct inode *,
 			  struct buffer_head *, struct buffer_head *,
 			  struct ocfs2_alloc_context *,
