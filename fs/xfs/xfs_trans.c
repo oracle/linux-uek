1e1cb2c3ad69750812b74e8d9bca63cac45c8a7e
Brian Foster <bfoster@redhat.com>
2015-08-19 09:50:13 +1000

    xfs: return committed status from xfs_trans_roll()
    [ Upstream commit d43ac29be7a174f93a3d26cc1e68668fe86b782f ]
    
    Some callers need to make error handling decisions based on whether
    the current transaction successfully committed or not. Rename
    xfs_trans_roll(), add a new parameter and provide a wrapper to
    preserve existing callers.
    
    Orabug: 27091363
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: wen.gang.wang@oracle.com
    Reviewed-by: Allison Henderson <allison.henderson@oracle.com>

--- fs/xfs/xfs_trans.c
+++ fs/xfs/xfs_trans.c
@@ -1037,9 +1037,10 @@ xfs_trans_cancel(
  * chunk we've been working on and get a new transaction to continue.
  */
 int
-xfs_trans_roll(
+__xfs_trans_roll(
 	struct xfs_trans	**tpp,
-	struct xfs_inode	*dp)
+	struct xfs_inode	*dp,
+	int			*committed)
 {
 	struct xfs_trans	*trans;
 	struct xfs_trans_res	tres;
@@ -1070,6 +1071,7 @@ xfs_trans_roll(
 	if (error)
 		return error;
 
+	*committed = 1;
 	trans = *tpp;
 
 	/*
@@ -1098,4 +1100,13 @@ xfs_trans_roll(
 	if (dp)
 		xfs_trans_ijoin(trans, dp, 0);
 	return 0;
+}
+
+int
+xfs_trans_roll(
+	struct xfs_trans	**tpp,
+	struct xfs_inode	*dp)
+{
+	int			committed = 0;
+	return __xfs_trans_roll(tpp, dp, &committed);
 }
