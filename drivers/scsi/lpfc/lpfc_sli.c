459f37921e3eb0cfb7ed5b91ca5aa766a08490ab
James Smart <james.smart@avagotech.com>
2015-10-20 17:57:32 -0400

    lpfc: Make the function lpfc_sli4_mbox_completions_pending static
    Orabug: 21860804
    
    This makes the function lpfc_sli4_mbox_completion's definition
    static now in order to comply with its prototype being also
    declared as static too.
    
    Signed-off-by: Nicholas Krause <xerofoify@gmail.com>
    Signed-off-by: James Smart <james.smart@avagotech.com>
    ---
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/scsi/lpfc/lpfc_sli.c
+++ drivers/scsi/lpfc/lpfc_sli.c
@@ -6699,7 +6699,7 @@ lpfc_mbox_timeout(unsigned long ptr)
  * This function checks if any mailbox completions are present on the mailbox
  * completion queue.
  **/
-bool
+static bool
 lpfc_sli4_mbox_completions_pending(struct lpfc_hba *phba)
 {
 
