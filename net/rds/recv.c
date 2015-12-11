3f2c5339dfb52c4bdece7773ee4c952c8f97da8d
Santosh Shilimkar <santosh.shilimkar@oracle.com>
2015-12-11 12:01:56 -0800

    RDS: Add interface for receive MSG latency trace
    Socket option to tap receive path latency.
    SO_RDS: SO_RDS_MSG_RXPATH_LATENCY
    with parameter,
    struct rds_rx_trace_so {
    	u8 rx_traces;
           	u8 rx_trace_pos[RDS_MSG_RX_DGRAM_TRACE_MAX];
    }
    CMSG:
    RDS_CMSG_RXPATH_LATENCY(recvmsg)
    Returns rds message latencies in various stages of receive
    path in nS. Its set per socket using SO_RDS_MSG_RXPATH_LATENCY
    socket option. Legitimate points are defined in
    enum rds_message_rxpath_latency. More points can be added in
    future.
    CSMG format:
    struct rds_cmsg_rx_trace {
           	u8 rx_traces;
            u8 rx_trace_pos[RDS_MSG_RX_DGRAM_TRACE_MAX];
           	u64 rx_trace[RDS_MSG_RX_DGRAM_TRACE_MAX];
    }
    Receive MSG trace points: RDS message Receive Path Latency points
    enum rds_message_rxpath_latency {
    	RDS_MSG_RX_HDR_TO_DGRAM_START = 0,
    	RDS_MSG_RX_DGRAM_REASSEMBLE,
    	RDS_MSG_RX_DGRAM_DELIVERED,
    	RDS_MSG_RX_DGRAM_TRACE_MAX
    }
    
    Orabug: 22630180
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    
    Reviewed-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    Reviewed-by: Sowmini Varadhan <sowmini.varadhan@oracle.com>
    
    Tested-by: Namrata Jampani <namrata.jampani@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 67fb74441f1bf4645987c366d3e60b0c26305269)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 6246db85177e7fc45a62e8654962358841e7301a)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 1e206f5e7128503c4196f3de8b16c5e312d1e4d6)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/recv.c
+++ net/rds/recv.c
@@ -68,6 +68,8 @@ rds_recv_ok(struct net *net, struct sock *sk, struct sk_buff *skb)
 void rds_inc_init(struct rds_incoming *inc, struct rds_connection *conn,
 		  __be32 saddr)
 {
+	int i;
+
 	atomic_set(&inc->i_refcount, 1);
 	INIT_LIST_HEAD(&inc->i_item);
 	inc->i_conn = conn;
@@ -77,6 +79,9 @@ void rds_inc_init(struct rds_incoming *inc, struct rds_connection *conn,
 	inc->i_skb   = NULL;
 	inc->i_rx_tstamp.tv_sec = 0;
 	inc->i_rx_tstamp.tv_usec = 0;
+
+	for (i = 0; i < RDS_RX_MAX_TRACES; i++)
+		inc->i_rx_lat_trace[i] = 0;
 }
 EXPORT_SYMBOL_GPL(rds_inc_init);
 
@@ -558,6 +563,7 @@ rds_recv_local(struct rds_connection *conn, __be32 saddr, __be32 daddr,
 				do_gettimeofday(&inc->i_rx_tstamp);
 			rds_inc_addref(inc);
 			list_add_tail(&inc->i_item, &rs->rs_recv_queue);
+			inc->i_rx_lat_trace[RDS_MSG_RX_END] = local_clock();
 			__rds_wake_sk_sleep(sk);
 		}
 	} else {
@@ -730,7 +736,7 @@ static int rds_cmsg_recv(struct rds_incoming *inc, struct msghdr *msg,
 		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_RDMA_DEST,
 				sizeof(inc->i_rdma_cookie), &inc->i_rdma_cookie);
 		if (ret)
-			return ret;
+			goto out;
 	}
 
 	if ((inc->i_rx_tstamp.tv_sec != 0) &&
@@ -739,10 +745,30 @@ static int rds_cmsg_recv(struct rds_incoming *inc, struct msghdr *msg,
 			       sizeof(struct timeval),
 			       &inc->i_rx_tstamp);
 		if (ret)
-			return ret;
+			goto out;
 	}
 
-	return 0;
+	if (rs->rs_rx_traces) {
+		struct rds_cmsg_rx_trace t;
+		int i, j;
+
+		inc->i_rx_lat_trace[RDS_MSG_RX_CMSG] = local_clock();
+		t.rx_traces =  rs->rs_rx_traces;
+		for (i = 0; i < rs->rs_rx_traces; i++) {
+			j = rs->rs_rx_trace[i];
+			t.rx_trace_pos[j] = j;
+			t.rx_trace[j] = inc->i_rx_lat_trace[j + 1] -
+					  inc->i_rx_lat_trace[j];
+		}
+
+		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_RXPATH_LATENCY,
+				sizeof(t), &t);
+		if (ret)
+			goto out;
+	}
+
+out:
+	return ret;
 }
 
 int rds_recvmsg(struct socket *sock, struct msghdr *msg, size_t size,
