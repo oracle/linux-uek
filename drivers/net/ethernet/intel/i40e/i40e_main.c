6b74be75d4aaed036802ffdfac422278797e5bfa
Alexander Duyck <aduyck@mirantis.com>
2016-01-24 21:17:43 -0800

    i40e: Do not drop support for IPv6 VXLAN or GENEVE tunnels
    Orabug: 22342532
    
    All of the documentation in the datasheets for the XL710 do not call out
    any reason to exclude support for IPv6 based tunnels.  As such I am
    dropping the code that was excluding these tunnel types from having their
    port numbers recognized.  This way we can take advantage of things such as
    checksum offload for inner headers over IPv6 based VXLAN or GENEVE
    tunnels.
    
    Signed-off-by: Alexander Duyck <aduyck@mirantis.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 84d5946d49cf9552d0f1740ad62d0f126cb3b6a9)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -8571,9 +8571,6 @@ static void i40e_add_vxlan_port(struct net_device *netdev,
 	u8 next_idx;
 	u8 idx;
 
-	if (sa_family == AF_INET6)
-		return;
-
 	idx = i40e_get_udp_port_idx(pf, port);
 
 	/* Check if port already exists */
@@ -8613,9 +8610,6 @@ static void i40e_del_vxlan_port(struct net_device *netdev,
 	struct i40e_pf *pf = vsi->back;
 	u8 idx;
 
-	if (sa_family == AF_INET6)
-		return;
-
 	idx = i40e_get_udp_port_idx(pf, port);
 
 	/* Check if port already exists */
@@ -8650,9 +8644,6 @@ static void i40e_add_geneve_port(struct net_device *netdev,
 	u8 idx;
 
 	if (!(pf->flags & I40E_FLAG_GENEVE_OFFLOAD_CAPABLE))
-		return;
-
-	if (sa_family == AF_INET6)
 		return;
 
 	idx = i40e_get_udp_port_idx(pf, port);
@@ -8695,9 +8686,6 @@ static void i40e_del_geneve_port(struct net_device *netdev,
 	struct i40e_vsi *vsi = np->vsi;
 	struct i40e_pf *pf = vsi->back;
 	u8 idx;
-
-	if (sa_family == AF_INET6)
-		return;
 
 	if (!(pf->flags & I40E_FLAG_GENEVE_OFFLOAD_CAPABLE))
 		return;
