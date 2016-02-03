1faf5a3699621ff96b9604d25d40a9ae05a61a26
Borislav Petkov <bp@suse.de>
2016-02-03 12:33:33 +0100

    x86/microcode: Issue update message only once
    This is especially annoying on large boxes:
    
      x86: Booting SMP configuration:
      .... node  #0, CPUs:          #1
      microcode: CPU1 microcode updated early to revision 0x428, date = 2014-05-29
         #2
      microcode: CPU2 microcode updated early to revision 0x428, date = 2014-05-29
         #3
      ...
    
    so issue the update message only once.
    
    $ grep microcode /proc/cpuinfo
    
    shows whether every core got updated properly.
    
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Thomas Voegtle <tv@lio96.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1454499225-21544-6-git-send-email-bp@alien8.de
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit b7f500aedd4551a9bf29c617804c13f0ff18c879)
    
    Orabug: 30528904
    
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	arch/x86/kernel/cpu/microcode/amd.c
    	arch/x86/kernel/cpu/microcode/intel.c
    Different file names: {amd,intel}_early.c
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/microcode/intel_early.c
+++ arch/x86/kernel/cpu/microcode/intel_early.c
@@ -553,10 +553,7 @@ scan_microcode(struct mc_saved_data *mc_saved_data, unsigned long *initrd,
 static void
 print_ucode_info(struct ucode_cpu_info *uci, unsigned int date)
 {
-	int cpu = smp_processor_id();
-
-	pr_info("CPU%d microcode updated early to revision 0x%x, date = %04x-%02x-%02x\n",
-		cpu,
+	pr_info_once("microcode updated early to revision 0x%x, date = %04x-%02x-%02x\n",
 		uci->cpu_sig.rev,
 		date & 0xffff,
 		date >> 24,
