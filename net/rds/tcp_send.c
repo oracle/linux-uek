5eebd095233805e9e32cf083f9f396816372f5ab
Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
2016-03-03 19:18:28 -0800

    rds: find connection drop reason
    This patch attempts to find connection drop details.
    Rational for adding this type of patch is, there are too many
    places from where connection can get dropped.
    And, in some cases, we don't have any idea of the source of
    connection drop. This is especially painful for issues which
    are reproducible in customer environment only.
    Idea here is, we have tracker variable which keeps latest value
    of connection drop source.
    We can fetch that tracker variable as per our need.
    
    Orabug: 22631108
    
    Signed-off-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    
    Acked-by: Wengang Wang <wen.gang.wang@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit 147954a1cd39fb9178782ce14b474c43f8378ad4)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	net/rds/af_rds.c
    	net/rds/connection.c
    	net/rds/iw_cm.c
    	net/rds/iw_recv.c
    	net/rds/iw_send.c
    	net/rds/rdma_transport.c
    	net/rds/tcp_send.c
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 4815d5c20d79edb2e22d14b79945e58a48f1d470)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 9f5a18ced3d8d0176f62ea560cfb0058ed90cb51)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/tcp_send.c
+++ net/rds/tcp_send.c
@@ -152,6 +152,7 @@ out:
 			printk(KERN_WARNING "RDS/tcp: send to %pI4 "
 			       "returned %d, disconnecting and reconnecting\n",
 			       &conn->c_faddr, ret);
+			conn->c_drop_source = 141;
 			rds_conn_drop(conn);
 		}
 	}
