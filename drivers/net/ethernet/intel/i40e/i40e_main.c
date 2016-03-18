7727346f13da4d0541fe49ef611c886d90ffba6b
Shannon Nelson <shannon.nelson@intel.com>
2016-03-18 12:18:15 -0700

    i40e: Request PHY media event at reset time
    Orabug: 23176970
    
    Add the Media Not Available flag to the link event mask.  It seems
    that event comes first if you have a DA cable pulled out, but there's no
    follow-up event for Link Down; if you're not looking for MEDIA_NA you will
    get no event, even though there's now no Link.
    
    Change-ID: cb3340a2849805bb881f64f6f2ae810eef46eba7
    Signed-off-by: Shannon Nelson <shannon.nelson@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 867a79e37ed9a3a5a2051cc11df21a57a8a00bfe)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -6819,6 +6819,7 @@ static void i40e_reset_and_rebuild(struct i40e_pf *pf, bool reinit)
 	 */
 	ret = i40e_aq_set_phy_int_mask(&pf->hw,
 				       ~(I40E_AQ_EVENT_LINK_UPDOWN |
+					 I40E_AQ_EVENT_MEDIA_NA |
 					 I40E_AQ_EVENT_MODULE_QUAL_FAIL), NULL);
 	if (ret)
 		dev_info(&pf->pdev->dev, "set phy mask fail, err %s aq_err %s\n",
@@ -10940,6 +10941,7 @@ static int i40e_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	 */
 	err = i40e_aq_set_phy_int_mask(&pf->hw,
 				       ~(I40E_AQ_EVENT_LINK_UPDOWN |
+					 I40E_AQ_EVENT_MEDIA_NA |
 					 I40E_AQ_EVENT_MODULE_QUAL_FAIL), NULL);
 	if (err)
 		dev_info(&pf->pdev->dev, "set phy mask fail, err %s aq_err %s\n",
