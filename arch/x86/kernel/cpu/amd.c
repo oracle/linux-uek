43ec3737a9b1333b25bc15aa088f162e0152537d
Peter Zijlstra <peterz@infradead.org>
2016-03-25 15:52:34 +0100

    x86/topology: Fix AMD core count
    It turns out AMD gets x86_max_cores wrong when there are compute
    units.
    
    The issue is that Linux assumes:
    
    	nr_logical_cpus = nr_cores * nr_siblings
    
    But AMD reports its CU unit as 2 cores, but then sets num_smp_siblings
    to 2 as well.
    
    Boris: fixup ras/mce_amd_inj.c too, to compute the Node Base Core
    properly, according to the new nomenclature.
    
    Fixes: 1f12e32f4cd5 ("x86/topology: Create logical package id")
    Reported-by: Xiong Zhou <jencce.kernel@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Andreas Herrmann <aherrmann@suse.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Link: http://lkml.kernel.org/r/20160317095220.GO6344@twins.programming.kicks-ass.net
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    (cherry picked from commit ee6825c80e870fff1a370c718ec77022ade0889b)
    
    Orabug: 28783929
    
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	arch/x86/kernel/cpu/amd.c
    	arch/x86/ras/mce_amd_inj.c
    amd.c: contextual
    mce_amd_inj.c: does not exists
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -317,8 +317,9 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 		node_id = ecx & 7;
 
 		/* get compute unit information */
+		cores_per_cu = ((ebx >> 8) & 3) + 1;
+		c->x86_max_cores /= cores_per_cu;
 		c->compute_unit_id = ebx & 0xff;
-		cores_per_cu += ((ebx >> 8) & 3);
 	} else if (cpu_has(c, X86_FEATURE_NODEID_MSR)) {
 		u64 value;
 
@@ -333,8 +334,8 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 		u32 cus_per_node;
 
 		set_cpu_cap(c, X86_FEATURE_AMD_DCM);
-		cores_per_node = c->x86_max_cores / nodes_per_socket;
-		cus_per_node = cores_per_node / cores_per_cu;
+		cus_per_node = c->x86_max_cores / nodes_per_socket;
+		cores_per_node = cus_per_node * cores_per_cu;
 
 		/* store NodeID, use llc_shared_map to store sibling info */
 		per_cpu(cpu_llc_id, cpu) = node_id;
