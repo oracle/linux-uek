376751bc42d826e19b56de4fc314d530bfa2c824
Khalid Aziz <khalid.aziz@oracle.com>
2016-05-03 08:12:51 -0600

    Revert "OF/PCI: Add IORESOURCE_MEM_64 for 64-bit resource"
    This reverts commit 59072574902b6e44574c34a9989ec6ec6c494f99. This
    commit causes hotplug to break as documented in Orabug 22855133.
    
    Orabug: 22855133
    
    Signed-off-by: Khalid Aziz <khalid.aziz@oracle.com>
    (cherry picked from commit c0691adf4a28993c77371969ff7800901ff1a67b)
    Signed-off-by: Allen Pais <allen.pais@oracle.com>

--- drivers/of/address.c
+++ drivers/of/address.c
@@ -128,10 +128,8 @@ static unsigned int of_bus_pci_get_flags(const __be32 *addr)
 		flags |= IORESOURCE_IO;
 		break;
 	case 0x02: /* 32 bits */
-		flags |= IORESOURCE_MEM;
-		break;
 	case 0x03: /* 64 bits */
-		flags |= IORESOURCE_MEM | IORESOURCE_MEM_64;
+		flags |= IORESOURCE_MEM;
 		break;
 	}
 	if (w & 0x40000000)
