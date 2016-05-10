e2936cfa70ed9af228526267d1c359f780f6f6e2
Miklos Szeredi <mszeredi@redhat.com>
2016-05-11 01:16:37 +0200

    ovl: ignore permissions on underlying lookup
    Generally permission checking is not necessary when overlayfs looks up a
    dentry on one of the underlying layers, since search permission on base
    directory was already checked in ovl_permission().
    
    More specifically using lookup_one_len() causes a problem when the lower
    directory lacks search permission for a specific user while the upper
    directory does have search permission.  Since lookups are cached, this
    causes inconsistency in behavior: success depends on who did the first
    lookup.
    
    So instead use lookup_hash() which doesn't do the permission check.
    
    Reported-by: Ignacy Gawędzki <ignacy.gawedzki@green-communications.fr>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    
    Orabug: 26401569
    
    (backport upstream commit 38b78a5f18584db6fa7441e0f4531b283b0e6725)
    
    Signed-off-by: Shan Hai <shan.hai@oracle.com>
    Reviewed-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -324,9 +324,7 @@ static inline struct dentry *ovl_lookup_real(struct dentry *dir,
 {
 	struct dentry *dentry;
 
-	mutex_lock(&dir->d_inode->i_mutex);
-	dentry = lookup_one_len(name->name, dir, name->len);
-	mutex_unlock(&dir->d_inode->i_mutex);
+	dentry = lookup_hash(name, dir);
 
 	if (IS_ERR(dentry)) {
 		if (PTR_ERR(dentry) == -ENOENT)
