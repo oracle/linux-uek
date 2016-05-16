31ddafea31b8ed153f2a45df7e839a766c20e228
Mitch Williams <mitch.a.williams@intel.com>
2016-05-16 10:26:43 -0700

    i40e: set default VSI without a reset
    Orabug: 24568124
    
    Remove the need for a reset when the device enters limited promiscuous
    mode. This was causing heartburn for people who were using VFs and
    bridging, since this would require all of the VFs to undergo a reset
    each time the PF changed its promiscuity.
    
    Change-ID: I0a83495c5e4d68112bbc7a7a076d20fa8dd3b61c
    Signed-off-by: Mitch Williams <mitch.a.williams@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 5bc160319f8a1e7ea23d7136e725f9e6a4a7628a)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	drivers/net/ethernet/intel/i40e/i40e_main.c
    
    Signed-off-by: Dhaval Giani <dhaval.giani@oracle.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -9995,14 +9995,14 @@ void i40e_veb_release(struct i40e_veb *veb)
 static int i40e_add_veb(struct i40e_veb *veb, struct i40e_vsi *vsi)
 {
 	struct i40e_pf *pf = veb->pf;
-	bool is_default = false;
 	bool enable_stats = !!(pf->flags & I40E_FLAG_VEB_STATS_ENABLED);
 	int ret;
 
-	/* get a VEB from the hardware */
 	ret = i40e_aq_add_veb(&pf->hw, veb->uplink_seid, vsi->seid,
-			      veb->enabled_tc, is_default,
+			      veb->enabled_tc, false,
 			      &veb->seid, enable_stats, NULL);
+
+	/* get a VEB from the hardware */
 	if (ret) {
 		dev_info(&pf->pdev->dev,
 			 "couldn't add VEB, err %s aq_err %s\n",
