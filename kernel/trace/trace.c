6b2671432260f7809553882f48c88a1a36d12e6d
Avinash Repaka <avinash.repaka@oracle.com>
2016-06-02 11:56:16 -0700

    Revert "tracing: Print nasty banner when trace_printk() is in use"
    RDS uses trace_printk framework, hence disable trace_printk warning
    banner.
    
    Revert Details:
    
    Revert "tracing: Remove newline from trace_printk warning banner"
    This reverts commit 69a1c994cc540cc84469acf9952f72b899b38e8b.
    
    Revert "kernel: trace: fix printk message"
    This reverts commit eff264efeeb0898408e8c9df72d8a32621035bed.
    
    Revert "tracing: Print nasty banner when trace_printk() is in use"
    This reverts commit 2184db46e425c2b84a783eeead0e2b24ce67cd7f.
    
    Orabug: 23510486
    
    Signed-off-by: Avinash Repaka <avinash.repaka@oracle.com>
    Acked-by: Srinivas Eeda <srinivas.eeda@oracle.com>
    
    Orabug: 27199042
    
    (cherry picked from commit a7ce5d8e531270949726e096e9952309ee7003ef)
    cherry-pick-repo=linux-uek.git
    
    Conflicts:
    	kernel/trace/trace.c
    
    Reviewed-by: Avinash Repaka <avinash.repaka@oracle.com>
    Signed-off-by: Sudhakar Dindukurti <sudhakar.dindukurti@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -2838,22 +2838,7 @@ void trace_printk_init_buffers(void)
 	if (alloc_percpu_trace_buffer())
 		return;
 
-	/* trace_printk() is for debug use only. Don't use it in production. */
-
-	pr_warn("\n");
-	pr_warn("**********************************************************\n");
-	pr_warn("**   NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE   **\n");
-	pr_warn("**                                                      **\n");
-	pr_warn("** trace_printk() being used. Allocating extra memory.  **\n");
-	pr_warn("**                                                      **\n");
-	pr_warn("** This means that this is a DEBUG kernel and it is     **\n");
-	pr_warn("** unsafe for production use.                           **\n");
-	pr_warn("**                                                      **\n");
-	pr_warn("** If you see this message and you are not debugging    **\n");
-	pr_warn("** the kernel, report this immediately to your vendor!  **\n");
-	pr_warn("**                                                      **\n");
-	pr_warn("**   NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE   **\n");
-	pr_warn("**********************************************************\n");
+	pr_info("ftrace: Allocated trace_printk buffers\n");
 
 	/* Expand the buffers to set size */
 	tracing_update_buffers();
