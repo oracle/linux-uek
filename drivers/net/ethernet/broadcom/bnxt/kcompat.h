743d3abd039d0777fb9ce88c43fa9a3e1257dd41
Michael Chan <mchan@broadcom.com>
2016-06-16 21:54:41 -0400

    bnxt_en: New Broadcom ethernet driver.
    Orabug: 23221795
    
    Broadcom ethernet driver for the new family of NetXtreme-C/E
    ethernet devices.
    
    v5:
     - Removed empty blank lines at end of files (noted by David Miller).
     - Moved busy poll helper functions to bnxt.h to at least make the
     .c file look less cluttered with #ifdef (noted by Stephen Hemminger).
    
    v4:
     - Broke up 2 long message strings with "\n" (suggested by John Linville)
     - Constify an array of strings (suggested by Stephen Hemminger)
     - Improve bnxt_vf_pciid() (suggested by Stephen Hemminger)
     - Use PCI_VDEVICE() to populate pci_device_id table for more compact
       source.
    
    v3:
     - Fixed 2 more sparse warnings.
     - Removed some unused structures in .h files.
    
    v2:
     - Fixed all kbuild test robot reported warnings.
     - Fixed many of the checkpatch.pl errors and warnings.
     - Fixed the Kconfig description (noted by Dmitry Kravkov).
    
    Acked-by: Eddie Wai <eddie.wai@broadcom.com>
    Acked-by: Jeffrey Huang <huangjw@broadcom.com>
    Signed-off-by: Prashant Sreedharan <prashant@broadcom.com>
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [Upstream commit c0c050c58d840994ba842ad1c338a98e7c12b764]
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ drivers/net/ethernet/broadcom/bnxt/kcompat.h
@@ -0,0 +1,24 @@
+/* Broadcom NetXtreme-C/E network driver.
+ *
+ * Copyright (c) 2014-2015 Broadcom Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ */
+
+/* The forced speed, in units of 1Mb. All values 0 to INT_MAX are legal. */
+#define SPEED_10		10
+#define SPEED_100		100
+#define SPEED_1000		1000
+#define SPEED_2500		2500
+#define SPEED_5000		5000
+#define SPEED_10000		10000
+#define SPEED_20000		20000
+#define SPEED_25000		25000
+#define SPEED_40000		40000
+#define SPEED_50000		50000
+#define SPEED_56000		56000
+#define SPEED_100000		100000
+
+#define SPEED_UNKNOWN		-1
