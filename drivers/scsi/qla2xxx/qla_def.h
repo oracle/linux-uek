3e2ac4702c19b1eaf9eafcf4465c7d80e3c73af4
Joe Carnuccio <joe.carnuccio@qlogic.com>
2016-07-07 13:50:20 +0530

    qla2xxx: Let DPORT be enabled purely by nvram.
    Orabug: 23755773
    
    Signed-off-by: Joe Carnuccio <joe.carnuccio@qlogic.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@qlogic.com>
    Signed-off-by: Ethan Zhao <ethan.zhao@oracle.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -2998,6 +2998,7 @@ struct qla_hw_data {
 
 		uint32_t        mr_reset_hdlr_active:1;
 		uint32_t        mr_intr_valid:1;
+		uint32_t	dport_enabled:1;
 		uint32_t	fawwpn_enabled:1;
 		/* 35 bits */
 	} flags;
