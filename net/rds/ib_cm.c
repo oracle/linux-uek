67cfbd66b72797c9e34d4c5568783b5914056b28
Santosh Shilimkar <santosh.shilimkar@oracle.com>
2016-08-24 20:51:52 -0700

    RDS: IB: Avoid double reject on ACL failures
    We end up sending double reject on ACL failures. Fix it.
    
    Orabug: 22347191
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    
    Reviewed-by: Mukesh Kacker <mukesh.kacker@oracle.com>
    Reviewed-by: Ajaykumar Hotchandani <ajaykumar.hotchandani@oracle.com>
    
    Tested-by: Michael Nowak <michael.nowak@oracle.com>
    Tested-by: Rafael Alejandro Peralez <rafael.peralez@oracle.com>
    Tested-by: Liwen Huang <liwen.huang@oracle.com>
    Tested-by: Hong Liu <hong.x.liu@oracle.com>
    
    Orabug: 27364391
    
    (cherry picked from commit b51e3bdc48a58d3aaf069b658f21b83d2d87e3c9)
    cherry-pick-repo=linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 408ef14d882fd294b50f5232db513637238fa690)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 43ebeb403bd582dba560277dd6d5bf1b35059381)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib_cm.c
+++ net/rds/ib_cm.c
@@ -861,9 +861,7 @@ int rds_ib_cm_handle_connect(struct rdma_cm_id *cm_id,
 
 	acl_ret = rds_ib_match_acl(cm_id, dp->dp_saddr);
 	if (acl_ret < 0) {
-		int reject_reason = RDS_ACL_FAILURE;
-
-		rdma_reject(cm_id, &reject_reason, sizeof(int));
+		err = RDS_ACL_FAILURE;
 		rdsdebug("RDS: IB: passive: rds_ib_match_acl failed\n");
 		goto out;
 	}
