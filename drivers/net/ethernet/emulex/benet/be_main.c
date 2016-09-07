817eb2e0a729abe41f41477e69182e4ff652e00e
Somnath Kotur <somnath.kotur@broadcom.com>
2016-09-07 19:57:50 +0530

    be2net: Issue COMMON_RESET_FUNCTION cmd during driver unload
    Orabug: 25570957
    
    As per SLI guideline, drivers need to issue COMMON_RESET_FUNCTION SLI
    cmd during driver unload to clean up any non-persistent state
    information.
    Issue this cmd only if VFs are not assigned to VMs as it is possible
    for PF driver to unload while it\'s VF remains functional and assigned
    to a VM.
    
    Signed-off-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Signed-off-by: Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f72099e057c0b3ea3cfd16301cff9202c4db8ef4)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Dhaval Giani <dhaval.giani@oracle.com>

--- drivers/net/ethernet/emulex/benet/be_main.c
+++ drivers/net/ethernet/emulex/benet/be_main.c
@@ -5380,6 +5380,9 @@ static void be_remove(struct pci_dev *pdev)
 
 	be_clear(adapter);
 
+	if (!pci_vfs_assigned(adapter->pdev))
+		be_cmd_reset_function(adapter);
+
 	/* tell fw we're done with firing cmds */
 	be_cmd_fw_clean(adapter);
 
