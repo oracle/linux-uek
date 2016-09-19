33a47d9b076731122c66fa0e4366cf188208fa76
Jan Kara <jack@suse.cz>
2016-09-19 17:39:09 +0200

    posix_acl: Clear SGID bit when setting file permissions
    When file permissions are modified via chmod(2) and the user is not in
    the owning group or capable of CAP_FSETID, the setgid bit is cleared in
    inode_change_ok().  Setting a POSIX ACL via setxattr(2) sets the file
    permissions as well as the new ACL, but doesn't clear the setgid bit in
    a similar way; this allows to bypass the check in chmod(2).  Fix that.
    
    References: CVE-2016-7097
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit 073931017b49d9458aa351605b43a7e34598caef)
    
    Orabug: 25507344
    CVE: CVE-2016-7097
    
    Signed-off-by: Kirtikar Kashyap <kirtikar.kashyap@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    
     Conflicts:
    	fs/ceph/acl.c
    	fs/f2fs/acl.c
    	fs/gfs2/acl.c
    	fs/hfsplus/posix_acl.c
    	fs/jfs/acl.c
    	fs/ocfs2/acl.c
    	fs/orangefs/acl.c
    	fs/posix_acl.c
    	fs/xfs/xfs_acl.c
    	include/linux/posix_acl.h
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- fs/gfs2/acl.c
+++ fs/gfs2/acl.c
@@ -268,14 +268,10 @@ static int gfs2_xattr_system_set(struct dentry *dentry, const char *name,
 
 	if (type == ACL_TYPE_ACCESS) {
 		umode_t mode = inode->i_mode;
-		error = posix_acl_equiv_mode(acl, &mode);
+		error = posix_acl_update_mode(inode, &inode->i_mode, &acl);
 
-		if (error <= 0) {
-			posix_acl_release(acl);
-			acl = NULL;
-
-			if (error < 0)
-				return error;
+		if (error) {
+			return error;
 		}
 
 		error = gfs2_set_mode(inode, mode);
--- fs/xfs/xfs_acl.c
+++ fs/xfs/xfs_acl.c
@@ -383,17 +383,11 @@ xfs_xattr_acl_set(struct dentry *dentry, const char *name,
 		goto out_release;
 
 	if (type == ACL_TYPE_ACCESS) {
-		umode_t mode = inode->i_mode;
-		error = posix_acl_equiv_mode(acl, &mode);
+		umode_t mode;
 
-		if (error <= 0) {
-			posix_acl_release(acl);
-			acl = NULL;
-
-			if (error < 0)
-				return error;
-		}
-
+		error = posix_acl_update_mode(inode, &mode, &acl);
+		if (error)
+			return error;
 		error = xfs_set_mode(inode, mode);
 		if (error)
 			goto out_release;
