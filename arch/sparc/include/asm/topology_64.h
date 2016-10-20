7d50bc81b5a5171d85fa3125f0198b59b1c3a869
Atish Patra <atish.patra@oracle.com>
2016-10-19 18:33:29 -0600

    sparc64: Setup a scheduling domain for highest level cache.
    Individual scheduler domain should consist different hierarchy
    consisting of cores sharing similar property. Currently, no
    scheduler domain is defined separately for the cores that shares
    the last level cache. As a result, the scheduler fails to take
    advantage of cache locality while migrating tasks during load
    balancing.
    
    Here are the cpu masks currently present for sparc that are/can
    be used in scheduler domain construction.
    cpu_core_map : set based on the cores that shares l1 cache.
    core_core_sib_map : is set based on the socket id or max cache id.
    The prior SPARC notion of socket was defined as highest level of
    shared cache. However, the MD record on T7 platforms now describes
    the CPUs that share the physical socket and this is no longer tied
    to shared cache.
    
    That's why a separate cpu mask needs to be created that truly
    represent highest level of shared cache for all platforms.
    
    Modified after cherry picked from upstream commit.
    d624716b6c67e60681180786564b92ddb521148a
    The implementation is largely based on Chris's patches.
    
    Signed-off-by: Atish Patra <atish.patra@oracle.com>
    Reviewed-by: Chris Hyser <chris.hyser@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1e655ca52bb2727471f20cf4d8f62b4b9f69e6fc)
    Signed-off-by: Allen Pais <allen.pais@oracle.com>

--- arch/sparc/include/asm/topology_64.h
+++ arch/sparc/include/asm/topology_64.h
@@ -45,13 +45,19 @@ int __node_distance(int, int);
 #define topology_core_id(cpu)			(cpu_data(cpu).core_id)
 #define topology_core_cpumask(cpu)		(&cpu_core_sib_map[cpu])
 #define topology_thread_cpumask(cpu)		(&per_cpu(cpu_sibling_map, cpu))
+#define topology_core_cache_cpumask(cpu)	(&cpu_core_sib_cache_map[cpu])
 #endif /* CONFIG_SMP */
 
 extern cpumask_t cpu_core_map[NR_CPUS];
 extern cpumask_t cpu_core_sib_map[NR_CPUS];
+extern cpumask_t cpu_core_sib_cache_map[NR_CPUS];
+
+/**
+ * Return cores that shares the last level cache.
+ */
 static inline const struct cpumask *cpu_coregroup_mask(int cpu)
 {
-        return &cpu_core_map[cpu];
+	return &cpu_core_sib_cache_map[cpu];
 }
 
 #endif /* _ASM_SPARC64_TOPOLOGY_H */
