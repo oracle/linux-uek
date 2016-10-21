e16a81e65caa438ebc9b3e21bce31a6f7dd735a5
Don Skidmore <donald.c.skidmore@intel.com>
2016-10-20 21:42:00 -0400

    ixgbe: Add support to retrieve and store LED link active
    Orabug: 24568240
    
    This patch adds support to get the LED link active via the LEDCTL
    register.  If the LEDCTL register does not have LED link active
    (LED mode field = 0x0100) set then default LED link active returned.
    
    Signed-off-by: Don Skidmore <donald.c.skidmore@intel.com>
    Tested-by: Krishneil Singh <krishneil.k.singh@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 805cedd663549bfc74dbbc5dca73810df0589396)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_type.h
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_type.h
@@ -3345,6 +3345,7 @@ struct ixgbe_mac_operations {
 	s32 (*led_off)(struct ixgbe_hw *, u32);
 	s32 (*blink_led_start)(struct ixgbe_hw *, u32);
 	s32 (*blink_led_stop)(struct ixgbe_hw *, u32);
+	s32 (*init_led_link_act)(struct ixgbe_hw *);
 
 	/* RAR, Multicast, VLAN */
 	s32 (*set_rar)(struct ixgbe_hw *, u32, u8 *, u32, u32);
@@ -3455,6 +3456,7 @@ struct ixgbe_mac_info {
 	u8				san_mac_rar_index;
 	struct ixgbe_thermal_sensor_data  thermal_sensor_data;
 	bool				set_lben;
+	u8				led_link_act;
 };
 
 struct ixgbe_phy_info {
