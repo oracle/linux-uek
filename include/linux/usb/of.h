6d2ea2c08d125ae5465ef6b2a579dc053b40494f
Vijay Kumar <vijay.ac.kumar@oracle.com>
2016-10-28 13:59:57 -0700

    USB: core: let USB device know device node
    Although most of USB devices are hot-plug's, there are still some devices
    are hard wired on the board, eg, for HSIC and SSIC interface USB devices.
    If these kinds of USB devices are multiple functions, and they can supply
    other interfaces like i2c, gpios for other devices, we may need to
    describe these at device tree.
    
    In this commit, it uses "reg" in dts as physical port number to match
    the phyiscal port number decided by USB core, if they are the same,
    then the device node is for the device we are creating for USB core.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>
    Acked-by: Philipp Zabel <p.zabel@pengutronix.de>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Acked-by: Rob Herring <robh@kernel.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 69bec725985324e79b1c47ea287815ac4ddb0521)
    
    Conflicts:
    
    	include/linux/usb/of.h
    
    Signed-off-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Reviewed-by: Babu Moger <babu.moger@oracle.com>
    
    Orabug: 24785721
    Signed-off-by: Allen Pais <allen.pais@oracle.com>

--- include/linux/usb/of.h
+++ include/linux/usb/of.h
@@ -15,6 +15,8 @@
 enum usb_dr_mode of_usb_get_dr_mode(struct device_node *np);
 enum usb_device_speed of_usb_get_maximum_speed(struct device_node *np);
 bool of_usb_host_tpl_support(struct device_node *np);
+struct device_node *usb_of_get_child_node(struct device_node *parent,
+			int portnum);
 #else
 static inline enum usb_dr_mode of_usb_get_dr_mode(struct device_node *np)
 {
@@ -29,6 +31,11 @@ of_usb_get_maximum_speed(struct device_node *np)
 static inline bool of_usb_host_tpl_support(struct device_node *np)
 {
 	return false;
+}
+static inline struct device_node *usb_of_get_child_node
+		(struct device_node *parent, int portnum)
+{
+	return NULL;
 }
 #endif
 
