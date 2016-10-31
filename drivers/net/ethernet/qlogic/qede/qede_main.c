50f554dcaaf9d0ffdee4a031b3683430796d4f8b
Sudarsana Kalluru <Sudarsana.Kalluru@cavium.com>
2016-10-31 07:14:21 +0200

    qed*: Management firmware - notifications and defaults
    Orabug: 25477939
    
    Management firmware is interested in various tidbits about
    the driver - including the driver state & several configuration
    related fields [MTU, primtary MAC, etc.].
    This adds the necessray logic to update MFW with such configurations,
    some of which are passed directly via qed while for others APIs
    are provide so that qede would be able to later configure if needed.
    
    This also introduces a new default configuration for MTU which would
    replace the default inherited by being an ethernet device.
    
    Signed-off-by: Sudarsana Kalluru <Sudarsana.Kalluru@cavium.com>
    Signed-off-by: Yuval Mintz <Yuval.Mintz@cavium.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0fefbfbaad298162737d5418eb85065879f99b3e)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	drivers/net/ethernet/qlogic/qede/qede_main.c

--- drivers/net/ethernet/qlogic/qede/qede_main.c
+++ drivers/net/ethernet/qlogic/qede/qede_main.c
@@ -2358,6 +2358,8 @@ static void qede_init_ndev(struct qede_dev *edev)
 
 	/* Set network device HW mac */
 	ether_addr_copy(edev->ndev->dev_addr, edev->dev_info.common.hw_mac);
+
+	ndev->mtu = edev->dev_info.common.mtu;
 }
 
 /* This function converts from 32b param to two params of level and module
@@ -3705,6 +3707,8 @@ static int qede_open(struct net_device *ndev)
 #ifdef CONFIG_QEDE_VXLAN
 	vxlan_get_rx_port(ndev);
 #endif
+	edev->ops->common->update_drv_state(edev->cdev, true);
+
 	return 0;
 }
 
@@ -3713,6 +3717,8 @@ static int qede_close(struct net_device *ndev)
 	struct qede_dev *edev = netdev_priv(ndev);
 
 	qede_unload(edev, QEDE_UNLOAD_NORMAL);
+
+	edev->ops->common->update_drv_state(edev->cdev, false);
 
 	return 0;
 }
@@ -3773,6 +3779,8 @@ static int qede_set_mac_addr(struct net_device *ndev, void *p)
 				   edev->primary_mac);
 	if (rc)
 		return rc;
+
+	edev->ops->common->update_mac(edev->cdev, addr->sa_data);
 
 	/* Add MAC filter according to the new unicast HW MAC address */
 	ether_addr_copy(edev->primary_mac, ndev->dev_addr);
