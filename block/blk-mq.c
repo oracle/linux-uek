a076ce5fd490b86e9043f10ec16273a2e60f81aa
Keith Busch <keith.busch@intel.com>
2017-03-01 14:22:10 -0500

    blk-mq: Export blk_mq_freeze_queue_wait
    Drivers can start a freeze, so this provides a way to wait for frozen.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit 6bae363ee3057a14eec93440826813603559273a)
    
    Orabug: 26486098
    
    Signed-off-by: Ashok Vairavan <ashok.vairavan@oracle.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -89,10 +89,11 @@ void blk_mq_freeze_queue_start(struct request_queue *q)
 }
 EXPORT_SYMBOL_GPL(blk_mq_freeze_queue_start);
 
-static void blk_mq_freeze_queue_wait(struct request_queue *q)
+void blk_mq_freeze_queue_wait(struct request_queue *q)
 {
 	wait_event(q->mq_freeze_wq, percpu_ref_is_zero(&q->q_usage_counter));
 }
+EXPORT_SYMBOL_GPL(blk_mq_freeze_queue_wait);
 
 int blk_mq_freeze_queue_wait_timeout(struct request_queue *q,
 				     unsigned long timeout)
