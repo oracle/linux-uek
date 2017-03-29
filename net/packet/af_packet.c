ff3e7d00ba9bada7ca44ec2b63a0cf732af51eed
Andrey Konovalov <andreyknvl@google.com>
2017-03-29 16:11:22 +0200

    net/packet: fix overflow in check for tp_reserve
    Orabug: 26143563
    CVE: CVE-2017-7308
    
    When calculating po->tp_hdrlen + po->tp_reserve the result can overflow.
    
    Fix by checking that tp_reserve <= INT_MAX on assign.
    
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit bcc5364bdcfe131e6379363f089e7b4108d35b70)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	net/packet/af_packet.c

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -2035,6 +2035,8 @@ packet_setsockopt(struct socket *sock, int level, int optname, char __user *optv
 			return -EINVAL;
 		if (copy_from_user(&val, optval, sizeof(val)))
 			return -EFAULT;
+                if (val > INT_MAX)
+                        return -EINVAL;
 		lock_sock(sk);
 		if (po->rx_ring.pg_vec || po->tx_ring.pg_vec) {
 			ret = -EBUSY;
