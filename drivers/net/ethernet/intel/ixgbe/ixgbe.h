ae1d38d5fd5ea4445171fe62b4a62c68bcfa8069
Paul Greenwalt <paul.greenwalt@intel.com>
2017-04-13 08:07:07 -0400

    ixgbe: Add 1000Base-T device based on X550EM_X MAC
    Add support for new 1000Base-T device based on X550EM_X MAC
    type. All PHY operations are disabled as the PHY is controlled
    by FW.
    
    Signed-off-by: Paul Greenwalt <paul.greenwalt@intel.com>
    Tested-by: Krishneil Singh <krishneil.k.singh@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    
    Orabug: 26242766
    (cherry picked from commit 8dc963e1cd245e67d6a9ffb8447fc88fb6eaa370)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Ethan Zhao <ethan.zhao@oracle.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe.h
+++ drivers/net/ethernet/intel/ixgbe/ixgbe.h
@@ -748,6 +748,7 @@ enum ixgbe_boards {
 	board_X540,
 	board_X550,
 	board_X550EM_x,
+	board_x550em_x_fw,
 	board_x550em_a,
 	board_x550em_a_fw,
 };
@@ -757,6 +758,7 @@ extern const struct ixgbe_info ixgbe_82599_info;
 extern const struct ixgbe_info ixgbe_X540_info;
 extern const struct ixgbe_info ixgbe_X550_info;
 extern const struct ixgbe_info ixgbe_X550EM_x_info;
+extern const struct ixgbe_info ixgbe_x550em_x_fw_info;
 extern const struct ixgbe_info ixgbe_x550em_a_info;
 extern const struct ixgbe_info ixgbe_x550em_a_fw_info;
 #ifdef CONFIG_IXGBE_DCB
