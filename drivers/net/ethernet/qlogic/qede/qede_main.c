7801bb1b143273de93827ff8733de87294c237e7
sudarsana.kalluru@cavium.com <sudarsana.kalluru@cavium.com>
2017-04-26 09:00:51 -0700

    qede: Add support for PTP resource locking.
    Orabug: 26783820
    
    The patch adds necessary changes to the driver to use qed resource
    locking functionality. Currently the ptp initialization is spread
    between driver probe/open implementations, associated APIs are
    qede_ptp_register_phc()/qede_ptp_start(). Clubbed this functionality
    into single API qed_ptp_enable() to simplify the usage of qed resource
    locking implementation. The new API will be invoked in the probe path.
    Similarly the ptp clean-up code is moved to qede_ptp_disable() which
    gets invoked in the driver unload path.
    
    Signed-off-by: Sudarsana Reddy Kalluru <Sudarsana.Kalluru@cavium.com>
    Signed-off-by: Yuval Mintz <Yuval.Mintz@cavium.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [ Upstream commit 035744975aecf9b8e02920d93027a432c51062d1 ]
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/net/ethernet/qlogic/qede/qede_main.c
+++ drivers/net/ethernet/qlogic/qede/qede_main.c
@@ -963,13 +963,8 @@ static int __qede_probe(struct pci_dev *pdev, u32 dp_module, u8 dp_level,
 	edev->ops->common->set_id(cdev, edev->ndev->name, DRV_MODULE_VERSION);
 
 	/* PTP not supported on VFs */
-	if (!is_vf) {
-		rc = qede_ptp_register_phc(edev);
-		if (rc) {
-			DP_NOTICE(edev, "Cannot register PHC\n");
-			goto err5;
-		}
-	}
+	if (!is_vf)
+		qede_ptp_enable(edev, true);
 
 	edev->ops->register_ops(cdev, &qede_ll_ops, edev);
 
@@ -984,8 +979,6 @@ static int __qede_probe(struct pci_dev *pdev, u32 dp_module, u8 dp_level,
 
 	return 0;
 
-err5:
-	unregister_netdev(edev->ndev);
 err4:
 	qede_roce_dev_remove(edev);
 err3:
@@ -1036,7 +1029,7 @@ static void __qede_remove(struct pci_dev *pdev, enum qede_remove_mode mode)
 	unregister_netdev(ndev);
 	cancel_delayed_work_sync(&edev->sp_task);
 
-	qede_ptp_remove(edev);
+	qede_ptp_disable(edev);
 
 	qede_roce_dev_remove(edev);
 
@@ -1899,8 +1892,6 @@ static void qede_unload(struct qede_dev *edev, enum qede_unload_mode mode,
 	qede_roce_dev_event_close(edev);
 	edev->state = QEDE_STATE_CLOSED;
 
-	qede_ptp_stop(edev);
-
 	/* Close OS Tx */
 	netif_tx_disable(edev->ndev);
 	netif_carrier_off(edev->ndev);
@@ -2010,8 +2001,6 @@ static int qede_load(struct qede_dev *edev, enum qede_load_mode mode,
 	qede_roce_dev_event_open(edev);
 
 	edev->state = QEDE_STATE_OPEN;
-
-	qede_ptp_start(edev, (mode == QEDE_LOAD_NORMAL));
 
 	DP_INFO(edev, "Ending successfully qede load\n");
 
