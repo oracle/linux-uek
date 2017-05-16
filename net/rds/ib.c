29534200d0d9db2b47a41ac67950febde92944d3
Avinash Repaka <avinash.repaka@oracle.com>
2017-05-16 15:00:22 -0700

    RDS: IB: Fix cleanup during unregistering client
    As part of unregistering client, rds_ib_remove_one() is called, which
    queues rds_ib_dev_free() on rds_wq. This is why we need to flush rds_wq
    before returning rds_ib_unregister_client(). There is no reason to flush
    other wqs(rds_local_wq or rds_ip_wq) under rds_ib_unregister_client().
    
    Flushing of other work queued on c_wq(rds_wq or rds_local_wq) is done as
    part of rds_conn_destroy().
    
    This patch also fixes the crash that might arise if rds_ip_wq is flushed
    under rds_ib_unregister_client(), as rds_ip_wq might have been destroyed
    by then. Look rds_ib_init() error path; rds_ip_thread_exit() is called
    before rds_ib_unregister_client().
    
    Orabug: 27758801
    
    Signed-off-by: Avinash Repaka <avinash.repaka@oracle.com>
    Reviewed-by: Knut Omang <knut.omang@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 5524b97043e12478ccc0a70bb9dc7bdf065d13ab)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit f329e541dc7f2f1eb2cf7b79d97104422b572370)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.c
+++ net/rds/ib.c
@@ -2519,8 +2519,7 @@ static void rds_ib_unregister_client(void)
 {
 	ib_unregister_client(&rds_ib_client);
 	/* wait for rds_ib_dev_free() to complete */
-	flush_workqueue(rds_ip_wq);
-	flush_workqueue(rds_local_wq);
+	flush_workqueue(rds_wq);
 }
 
 static void rds_ib_update_ip_config(void)
