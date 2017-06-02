1a6ee6286e9bb456b0d488b7d27b259bb5647b39
Bart Van Assche <bart.vanassche@sandisk.com>
2017-06-02 14:21:57 -0700

    scsi: Make __scsi_remove_device go straight from BLOCKED to DEL
    If a device is blocked, make __scsi_remove_device() cause it to
    transition to the DEL state. This means that all the commands issued in
    .shutdown() will error in the mid-layer, thus making the removal proceed
    without being stopped.
    
    This patch is a slightly modified version of a patch from James
    Bottomley. This patch avoids that the following lockup occurs:
    
    Call Trace:
     schedule+0x35/0x80
     schedule_timeout+0x237/0x2d0
     io_schedule_timeout+0xa6/0x110
     wait_for_completion_io+0xa3/0x110
     blk_execute_rq+0xdf/0x120
     scsi_execute+0xce/0x150 [scsi_mod]
     scsi_execute_req_flags+0x8f/0xf0 [scsi_mod]
     sd_sync_cache+0xa9/0x190 [sd_mod]
     sd_shutdown+0x6a/0x100 [sd_mod]
     sd_remove+0x64/0xc0 [sd_mod]
     __device_release_driver+0x8d/0x120
     device_release_driver+0x1e/0x30
     bus_remove_device+0xf9/0x170
     device_del+0x127/0x240
     __scsi_remove_device+0xc1/0xd0 [scsi_mod]
     scsi_forget_host+0x57/0x60 [scsi_mod]
     scsi_remove_host+0x72/0x110 [scsi_mod]
     srp_remove_work+0x8b/0x200 [ib_srp]
    
    Reported-by: Israel Rukshin <israelr@mellanox.com>
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Cc: James Bottomley <James.Bottomley@HansenPartnership.com>
    Cc: Israel Rukshin <israelr@mellanox.com>
    Cc: Max Gurtovoy <maxg@mellanox.com>
    Cc: Benjamin Block <bblock@linux.vnet.ibm.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Orabug: 27339736
    
    (cherry picked from commit 255ee9320e5dc46173bb94dbcd68e32f11fc10a9)
    Signed-off-by: Kyle Fortin <kyle.fortin@oracle.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	drivers/scsi/scsi_sysfs.c
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -2640,7 +2640,6 @@ scsi_device_set_state(struct scsi_device *sdev, enum scsi_device_state state)
 		case SDEV_QUIESCE:
 		case SDEV_OFFLINE:
 		case SDEV_TRANSPORT_OFFLINE:
-		case SDEV_BLOCK:
 			break;
 		default:
 			goto illegal;
@@ -2654,6 +2653,7 @@ scsi_device_set_state(struct scsi_device *sdev, enum scsi_device_state state)
 		case SDEV_OFFLINE:
 		case SDEV_TRANSPORT_OFFLINE:
 		case SDEV_CANCEL:
+		case SDEV_BLOCK:
 		case SDEV_CREATED_BLOCK:
 			break;
 		default:
