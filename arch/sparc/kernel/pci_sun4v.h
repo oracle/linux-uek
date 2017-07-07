87bc7d0a590a46fae6f0e6421e8521a5245b8123
Allen Pais <allen.pais@oracle.com>
2017-07-07 13:24:25 +0530

    sparc64: Add ATU (new IOMMU) support
    ATU (Address Translation Unit) is a new IOMMU in SPARC supported with
    Hypervisor IOMMU v2 APIs.
    
    Current SPARC IOMMU supports only 32bit address ranges and one TSB
    per PCIe root complex that has a 2GB per root complex DVMA space
    limit. The limit has become a scalability bottleneck nowadays that
    a typical 10G/40G NIC can consume 300MB-500MB DVMA space per
    instance. When DVMA resource is exhausted, devices will not be usable
    since the driver can't allocate DVMA.
    
    ATU removes bottleneck by allowing guest os to create IOTSB of size
    32G (or more) with 64bit address ranges available in ATU HW. 32G is
    more than enough DVMA space to be shared by all PCIe devices under
    root complex contrast to 2G space provided by legacy IOMMU.
    
    ATU allows PCIe devices to use 64bit DMA addressing. Devices
    which choose to use 32bit DMA mask will continue to work with the
    existing legacy IOMMU.
    
    Orabug: 23239179
    
    Reviewed-by: chris hyser <chris.hyser@oracle.com>
    Signed-off-by: Tushar Dave <tushar.n.dave@oracle.com>
    Signed-off-by: Allen Pais <allen.pais@oracle.com>

--- arch/sparc/kernel/pci_sun4v.h
+++ arch/sparc/kernel/pci_sun4v.h
@@ -91,5 +91,12 @@ unsigned long pci_sun4v_msg_setvalid(unsigned long devhandle,
 unsigned long pci_sun4v_iov_dev_ready(unsigned long devhandle,
 				      unsigned long pci_device,
 				      unsigned long ready_flag);
+/* Sun4v HV IOMMU v2 APIs */
+unsigned long pci_sun4v_iotsb_conf(unsigned long devhandle,
+				   unsigned long ra,
+				   unsigned long table_size,
+				   unsigned long page_size,
+				   unsigned long dvma_base,
+				   u64 *iotsb_num);
 
 #endif /* !(_PCI_SUN4V_H) */
