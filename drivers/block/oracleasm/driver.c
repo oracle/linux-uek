8f805a649baa0de4c6312da7ab983789cac2c062
Martin K. Petersen <martin.petersen@oracle.com>
2017-08-16 21:13:37 -0400

    oracleasm: Copy the integrity descriptor
    The original code made assumptions about the oracleasm integrity
    descriptor hanging off of check_asm_ioc being mapped. Make sure we
    properly copy and validate the descriptor before use.
    
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Divya Indi <divya.indi@oracle.com>
    
    Orabug: 26649818
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/block/oracleasm/driver.c
+++ drivers/block/oracleasm/driver.c
@@ -1227,7 +1227,8 @@ static int asm_submit_io(struct file *file,
 	struct asm_disk_info *d;
 	struct inode *disk_inode;
 	struct block_device *bdev;
-	struct oracleasm_integrity_v2 *it;
+	struct oracleasm_integrity_v2 it;
+	bool integrity = false;
 
 	mlog_entry("(0x%p, 0x%p, 0x%p)\n", file, user_iocp, ioc);
 
@@ -1343,10 +1344,22 @@ static int asm_submit_io(struct file *file,
 	     "Request 0x%p (user_ioc 0x%p) passed validation checks\n",
 	     r, user_iocp);
 
-	if (bdev_get_integrity(bdev))
-		it = (struct oracleasm_integrity_v2 *)ioc->check_asm_ioc;
-	else
-		it = NULL;
+	if (bdev_get_integrity(bdev) && ioc->check_asm_ioc) {
+
+		if (copy_from_user(&it, (struct oracleasm_integrity_v2 *)ioc->check_asm_ioc, sizeof(it))) {
+			pr_err("%s: Failed to copy integrity descriptor\n",
+			       __func__);
+			ret = -EFAULT;
+			goto out_error;
+		}
+
+		if (asm_integrity_check(&it, bdev) < 0) {
+			ret = -EINVAL;
+			goto out_error;
+		}
+
+		integrity = true;
+	}
 
 	switch (ioc->operation_asm_ioc) {
 		default:
@@ -1355,18 +1368,10 @@ static int asm_submit_io(struct file *file,
 
 		case ASM_READ:
 			rw = READ;
-
-			if (it && asm_integrity_check(it, bdev) < 0)
-				goto out_error;
-
 			break;
 
 		case ASM_WRITE:
 			rw = WRITE;
-
-			if (it && asm_integrity_check(it, bdev) < 0)
-				goto out_error;
-
 			break;
 
 		case ASM_NOOP:
@@ -1405,8 +1410,8 @@ static int asm_submit_io(struct file *file,
 	 */
 	r->r_bio->bi_sector = ioc->first_asm_ioc * (asm_block_size(bdev) >> 9);
 
-	if (it) {
-		ret = asm_integrity_map(it, r, rw == READ);
+	if (integrity) {
+		ret = asm_integrity_map(&it, r, rw == READ);
 
 		if (ret < 0) {
 			mlog(ML_ERROR|ML_BIO,
