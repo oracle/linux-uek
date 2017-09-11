6117880d37743493ece7867543df16e921f2fdac
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2017-09-11 14:51:11 -0400

    x86/timers: Move simple_udelay_calibration() past kvmclock_init()
    simple_udelay_calibration() relies on x86_platform's calibration ops.
    For KVM these ops are set late in setup_arch() and so
    simple_udelay_calibration() ends up using native version.
    
    Besides being possibly incorrect, this significantly increases kernel
    boot time. For example, on my laptop executing start_kernel() by a guest
    takes ~10 times more than when KVM's ops are used.
    
    Since early_xdbc_setup_hardware() relies on calibration having been
    performed move it too.
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: baolu.lu@linux.intel.com
    Link: https://lkml.kernel.org/r/20170911185111.20636-1-boris.ostrovsky@oracle.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    (cherry picked from commit ccb64941f375a6eb21b1b20136730eb7d1716068)
    Signed-off-by: Jim Quigley <Jim.Quigley@oracle.com>
    
    Reviewed-by: Mihai Carabas <mihai.carabas@oracle.com>
    
    Orabug: 27786142
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1024,8 +1024,6 @@ void __init setup_arch(char **cmdline_p)
 	 */
 	init_hypervisor_platform();
 
-	tsc_early_delay_calibrate();
-
 	x86_init.resources.probe_roms();
 
 	/* after parse_early_param, so could debug it */
@@ -1109,9 +1107,6 @@ void __init setup_arch(char **cmdline_p)
 
 	memblock_set_current_limit(ISA_END_ADDRESS);
 	e820__memblock_setup();
-
-	if (!early_xdbc_setup_hardware())
-		early_xdbc_register_console();
 
 	reserve_bios_regions();
 
@@ -1203,6 +1198,10 @@ void __init setup_arch(char **cmdline_p)
 #ifdef CONFIG_KVM_GUEST
 	kvmclock_init();
 #endif
+
+	tsc_early_delay_calibrate();
+	if (!early_xdbc_setup_hardware())
+		early_xdbc_register_console();
 
 	x86_init.paging.pagetable_init();
 
