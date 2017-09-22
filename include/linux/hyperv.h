38ef531c1d4e560d667889234ae9c108464cb0d5
Stephen Hemminger <stephen@networkplumber.org>
2017-09-21 20:58:49 -0700

    vmbus: add per-channel sysfs info
    This extends existing vmbus related sysfs structure to provide per-channel
    state information. This is useful when diagnosing issues with multiple
    queues in networking and storage.
    
    The existing sysfs only displayed information about the primary
    channel. The one place it reported multiple channels was the
    channel_vp_mapping file which violated the sysfs convention
    of one value per file.
    
    Signed-off-by: Stephen Hemminger <sthemmin@microsoft.com>
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit c2e5df616e1ae6c2a074cb241ebb65a318ebaf7c)
    
    Orabug: 27747785
    
    Signed-off-by: Vijay Balakrishna <vijay.balakrishna@oracle.com>
    Reviewed-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -830,6 +830,11 @@ struct vmbus_channel {
 	struct rcu_head rcu;
 
 	/*
+	 * For sysfs per-channel properties.
+	 */
+	struct kobject			kobj;
+
+	/*
 	 * For performance critical channels (storage, networking
 	 * etc,), Hyper-V has a mechanism to enhance the throughput
 	 * at the expense of latency:
@@ -1090,6 +1095,7 @@ struct hv_device {
 	struct device device;
 
 	struct vmbus_channel *channel;
+	struct kset	     *channels_kset;
 };
 
 
