233116c7bd38aa6522115535195a485d01a7be1d
Guy Levi <guyle@mellanox.com>
2017-11-02 15:22:25 +0200

    IB/mlx4: Add contig support for control objects
    Taking advantage of the optimization which was introduced in previous
    commit ("IB/mlx4: Use optimal numbers of MTT entries") to optimize the
    MTT usage for QP and CQ.
    
    Signed-off-by: Guy Levi <guyle@mellanox.com>
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    
    Orabug: 27510588
    
    (cherry picked from commit ed8637d3615b38bd4d12ba5eb8ee6a0c3888e754)
    cherry-pick-repo=kernel/git/rdma/rdma.git
    
    This upstream backport supersedes UEK4 commit
    b1a086c311d3b41a3ac41d7637e7c73271f2bc4a from linux-uek.git.
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/infiniband/hw/mlx4/qp.c
+++ drivers/infiniband/hw/mlx4/qp.c
@@ -1041,6 +1041,8 @@ static int create_qp_common(struct mlx4_ib_dev *dev, struct ib_pd *pd,
 			struct mlx4_ib_create_wq wq;
 		} ucmd;
 		size_t copy_len;
+		int shift;
+		int n;
 
 		if (!udata) {
 			err = -EFAULT;
@@ -1108,8 +1110,10 @@ static int create_qp_common(struct mlx4_ib_dev *dev, struct ib_pd *pd,
 			goto err;
 		}
 
-		err = mlx4_mtt_init(dev->dev, ib_umem_page_count(qp->umem),
-				    qp->umem->page_shift, &qp->mtt);
+		n = ib_umem_page_count(qp->umem);
+		shift = mlx4_ib_umem_calc_optimal_mtt_size(qp->umem, 0, &n);
+		err = mlx4_mtt_init(dev->dev, n, shift, &qp->mtt);
+
 		if (err)
 			goto err_buf;
 
