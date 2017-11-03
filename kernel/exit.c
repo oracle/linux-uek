7fea017ffeb4da4c306965da3327b1dac6ec3d70
Tomas Jedlicka <tomas.jedlicka@oracle.com>
2017-11-03 11:26:30 -0400

    dtrace: Fix build time warnings
    Fixed buid-time warnings produced by various small coding errors:
    
    	- Violation of ISO C90 use after declaration
    	- Unused variables
    	- Not properly marked unused variables
    	- Missing casting between different types
    
    Signed-off-by: Tomas Jedlicka <tomas.jedlicka@oracle.com>
    Reviewed-by: Kris Van Hees <kris.van.hees@oracle.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -1687,12 +1687,13 @@ SYSCALL_DEFINE4(wait4, pid_t, upid, int __user *, stat_addr,
 		int, options, struct rusage __user *, ru)
 {
 	struct rusage r;
+	long err;
 
 	if (options & ~(WNOHANG|WUNTRACED|WCONTINUED|
 			__WNOTHREAD|__WCLONE|__WALL))
 		return -EINVAL;
 
-	long err = kernel_wait4(upid, stat_addr, options, ru ? &r : NULL);
+	err = kernel_wait4(upid, stat_addr, options, ru ? &r : NULL);
 
 	if (err > 0) {
 		if (ru && copy_to_user(ru, &r, sizeof(struct rusage)))
