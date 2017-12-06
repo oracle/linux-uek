bc798630b076306a4a89f3f19460ecbd9992ccfb
Quinn Tran <quinn.tran@cavium.com>
2017-12-06 14:37:37 +0530

    qla2xxx: Retry switch command on time out
    Orabug: 27235104
    
    Retry GID_PN & GPN_ID switch commands for time out case.
    
    Fixes: 726b85487067d ("qla2xxx: Add framework for async fabric discovery")
    Cc: <stable@vger.kernel.org> # 4.10+
    Signed-off-by: Quinn Tran <quinn.tran@cavium.com>
    Signed-off-by: Himanshu Madhani <himanshu.madhani@cavium.com>
    Reviewed-by: Hannes Reinecke <hare@suse.com>
    [ Upstream commit 25ad76b703d9ad536f3411b15b1070aeb059ab55 ]
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/scsi/qla2xxx/qla_gs.c
+++ drivers/scsi/qla2xxx/qla_gs.c
@@ -175,6 +175,9 @@ qla2x00_chk_ms_status(scsi_qla_host_t *vha, ms_iocb_entry_t *ms_pkt,
 				set_bit(LOCAL_LOOP_UPDATE, &vha->dpc_flags);
 			}
 			break;
+		case CS_TIMEOUT:
+			rval = QLA_FUNCTION_TIMEOUT;
+			/* drop through */
 		default:
 			ql_dbg(ql_dbg_disc, vha, 0x2033,
 			    "%s failed, completion status (%x) on port_id: "
@@ -2861,9 +2864,21 @@ static void qla2x00_async_gidpn_sp_done(void *s, int res)
 	ea.rc = res;
 	ea.event = FCME_GIDPN_DONE;
 
-	ql_dbg(ql_dbg_disc, vha, 0x204f,
-	    "Async done-%s res %x, WWPN %8phC ID %3phC \n",
-	    sp->name, res, fcport->port_name, id);
+	if (res == QLA_FUNCTION_TIMEOUT) {
+		ql_dbg(ql_dbg_disc, sp->vha, 0xffff,
+		    "Async done-%s WWPN %8phC timed out.\n",
+		    sp->name, fcport->port_name);
+		qla24xx_post_gidpn_work(sp->vha, fcport);
+		sp->free(sp);
+		return;
+	} else if (res)
+		ql_dbg(ql_dbg_disc, sp->vha, 0xffff,
+		    "Async done-%s fail res %x, WWPN %8phC \n",
+		    sp->name, res, fcport->port_name);
+	else
+		ql_dbg(ql_dbg_disc, vha, 0x204f,
+		    "Async done-%s good WWPN %8phC ID %3phC\n",
+		    sp->name, fcport->port_name, id);
 
 	qla2x00_fcport_event_handler(vha, &ea);
 
@@ -3197,6 +3212,14 @@ static void qla2x00_async_gpnid_sp_done(void *s, int res)
 	ea.id.b.al_pa = ct_req->req.port_id.port_id[2];
 	ea.rc = res;
 	ea.event = FCME_GPNID_DONE;
+
+	if (res) {
+		if (res == QLA_FUNCTION_TIMEOUT) {
+			qla24xx_post_gpnid_work(sp->vha, &ea.id);
+		}
+		sp->free(sp);
+		return;
+	}
 
 	qla2x00_fcport_event_handler(vha, &ea);
 
