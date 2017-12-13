8574668a22316d13a7e53e474351862caaa4e463
Sinan Kaya <okaya@codeaurora.org>
2017-12-13 02:20:51 -0500

    ACPI: properties: Implement get_match_data() callback
    Now that we have a get_match_data() callback as part of the firmware node,
    implement the ACPI specific piece for it.
    
    Signed-off-by: Sinan Kaya <okaya@codeaurora.org>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    
    (cherry picked from commit 146b4dbb0eef3695f35f5a80224d1f18c110ec85)
    
    Orabug: 31995830
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    
    Conflicts:
    	drivers/acpi/property.c    Contained partial implementation.
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/acpi/property.c
+++ drivers/acpi/property.c
@@ -1289,6 +1289,7 @@ acpi_fwnode_device_get_match_data(const struct fwnode_handle *fwnode,
 #define DECLARE_ACPI_FWNODE_OPS(ops) \
 	const struct fwnode_operations ops = {				\
 		.device_is_available = acpi_fwnode_device_is_available, \
+		.device_get_match_data = acpi_fwnode_device_get_match_data, \
 		.property_present = acpi_fwnode_property_present,	\
 		.property_read_int_array =				\
 			acpi_fwnode_property_read_int_array,		\
