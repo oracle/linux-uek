54418aa60ad49d1e87037855532b3b7ff1fbc903
Daniel Kiper <daniel.kiper@oracle.com>
2017-12-14 15:31:56 +0100

    x86/efi: Initialize and display UEFI secure boot state a bit later during init
    Otherwise Xen dom0 does not display "Secure boot enabled" message if it runs
    on secure boot enabled platform. This happens because boot_params.secure_boot
    is initialized too late. However, despite lack of message all features depending
    on UEFI secure boot are enabled properly.
    
    Orabug: 27258204
    
    Signed-off-by: Daniel Kiper <daniel.kiper@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1149,14 +1149,6 @@ void __init setup_arch(char **cmdline_p)
 	/* Allocate bigger log buffer */
 	setup_log_buf(1);
 
-#ifdef CONFIG_EFI_SECURE_BOOT_SECURELEVEL
-	if (boot_params.secure_boot) {
-		set_bit(EFI_SECURE_BOOT, &efi.flags);
-		set_securelevel(1);
-		pr_info("Secure boot enabled\n");
-	}
-#endif
-
 	reserve_initrd();
 
 #if defined(CONFIG_ACPI) && defined(CONFIG_BLK_DEV_INITRD)
