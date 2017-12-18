6a79d01eaf3790c256ab851125d5c9b9736c3801
Tim Chen <tim.c.chen@linux.intel.com>
2017-12-18 10:16:26 -0800

    x86: Move IBRS/IBPB feature detection to scattered.c
    Move IBRS/IBPB to scattered features for easier feature rescan.
    This help to rescan feature on microcode reload later.
    
    
    Orabug: 27344012
    CVE: CVE-2017-5715
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: John Haxby <john.haxby@oracle.com>
    Signed-off-by: Kirtikar Kashyap <kirtikar.kashyap@oracle.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -493,20 +493,6 @@ static void init_intel(struct cpuinfo_x86 *c)
 			wrmsrl(MSR_IA32_ENERGY_PERF_BIAS, epb);
 		}
 	}
-
-	if (!c->cpu_index) {
-		if (boot_cpu_has(X86_FEATURE_SPEC_CTRL)) {
-			printk(KERN_INFO "FEATURE SPEC_CTRL Present\n");
-			set_ibrs_supported();
-			set_ibpb_supported();
-			if (ibrs_inuse)
-				sysctl_ibrs_enabled = 1;
-			if (ibpb_inuse)
-				sysctl_ibpb_enabled = 1;
-		} else {
-			printk(KERN_INFO "FEATURE SPEC_CTRL Not Present\n");
-		}
-	}
 }
 
 #ifdef CONFIG_X86_32
