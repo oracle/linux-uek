61d400454f67e269ba23d507665212ef239c5a53
Shannon Nelson <shannon.nelson@oracle.com>
2017-12-19 15:59:59 -0800

    ixgbe: process the Rx ipsec offload
    If the chip sees and decrypts an ipsec offload, set up the skb
    sp pointer with the ralated SA info.  Since the chip is rude
    enough to keep to itself the table index it used for the
    decryption, we have to do our own table lookup, using the
    hash for speed.
    
    Signed-off-by: Shannon Nelson <shannon.nelson@oracle.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit 92103199f10a3c8b36c1232e96d5aaad1571d7ff)
    
    Orabug: 26719394
    
    Signed-off-by: Shannon Nelson <shannon.nelson@oracle.com>
    Reviewed-by: Tushar Dave <tushar.n.dave@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    
    Conflicts:
    	drivers/net/ethernet/intel/ixgbe/ixgbe_main.c

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -1753,7 +1753,8 @@ static void ixgbe_process_skb_fields(struct ixgbe_ring *rx_ring,
 		__vlan_hwaccel_put_tag(skb, htons(ETH_P_8021Q), vid);
 	}
 
-	skb_record_rx_queue(skb, rx_ring->queue_index);
+	if (ixgbe_test_staterr(rx_desc, IXGBE_RXDADV_STAT_SECP))
+		ixgbe_ipsec_rx(rx_ring, rx_desc, skb);
 
 	skb->protocol = eth_type_trans(skb, dev);
 }
