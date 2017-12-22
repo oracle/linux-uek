e3f093e4053e0f1a531c9d596541f4a60c5523fa
Tim Chen <tim.c.chen@linux.intel.com>
2017-12-21 19:27:59 -0500

    x86/feature: Report presence of IBPB and IBRS control
    Report presence of IBPB and IBRS.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: John Haxby <john.haxby@oracle.com>
    Signed-off-by: Kirtikar Kashyap <kirtikar.kashyap@oracle.com>
    
    (cherry-pick from cea5edba5689e032b17f513d6667b0e5d94e5f05 from UEK4)
    
    [Backport: patch "x86: Move IBRS/IBPB feature detection to scattered.c"
     moves this out.]
    
    Orabug: 27832367
    
    CVE: CVE-2017-5715
    
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: John Haxby <john.haxby@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/x86/kernel/cpu/intel.c
+++ arch/x86/kernel/cpu/intel.c
@@ -682,6 +682,11 @@ static void init_intel(struct cpuinfo_x86 *c)
 	init_intel_energy_perf(c);
 
 	init_intel_misc_features(c);
+
+	if (boot_cpu_has(X86_FEATURE_SPEC_CTRL))
+		printk_once(KERN_INFO "FEATURE SPEC_CTRL Present\n");
+	else
+		printk_once(KERN_INFO "FEATURE SPEC_CTRL Not Present\n");
 }
 
 #ifdef CONFIG_X86_32
