c0b56e3200f3cc048fefb4fe78ad915b28fac84a
Parav Pandit <parav@mellanox.com>
2018-01-04 17:25:39 +0200

    IB/mlx5: Change debugfs to have per port contents
    When there are multiple ports for single IB(RoCE) device, support
    debugfs entries to be available for each port.
    
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: Daniel Jurgens <danielj@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Orabug: 29249230
    
    (cherry picked from commit a9e546e73ace1ebfb80dc9b55b46ace306f684cd)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/infiniband/hw/mlx5/main.c
    	drivers/infiniband/hw/mlx5/mlx5_ib.h
    	(due to additional upstream patches 16c1975f10323, "IB/mlx5:
    	Create profile infrastructure to add and remove stages" after
    	v4.14)
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Acked-by: Parav Pandit <parav@mellanox.com>
    Tested-by: Anand Bibhuti <anand.bibhuti@oracle.com>
    Reviewed-by: Yuval Shaia<yuval.shaia@oracle.com>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -4183,6 +4183,8 @@ static void mlx5_ib_unbind_slave_port(struct mlx5_ib_dev *ibdev,
 	int err;
 	int i;
 
+	mlx5_ib_cleanup_cong_debugfs(ibdev, port_num);
+
 	spin_lock(&port->mp.mpi_lock);
 	if (!mpi->ibdev) {
 		spin_unlock(&port->mp.mpi_lock);
@@ -4259,6 +4261,10 @@ static bool mlx5_ib_bind_slave_port(struct mlx5_ib_dev *ibdev,
 			    port_num + 1);
 		goto unbind;
 	}
+
+	err = mlx5_ib_init_cong_debugfs(ibdev, port_num);
+	if (err)
+		goto unbind;
 
 	return true;
 
@@ -4666,7 +4672,7 @@ static void *mlx5_ib_add(struct mlx5_core_dev *mdev)
 			goto err_odp;
 	}
 
-	err = mlx5_ib_init_cong_debugfs(dev);
+	err = mlx5_ib_init_cong_debugfs(dev, port_num);
 	if (err)
 		goto err_cnt;
 
@@ -4725,7 +4731,7 @@ err_uar_page:
 	mlx5_put_uars_page(dev->mdev, dev->mdev->priv.uar);
 
 err_cong:
-	mlx5_ib_cleanup_cong_debugfs(dev);
+	mlx5_ib_cleanup_cong_debugfs(dev, port_num);
 err_cnt:
 	if (MLX5_CAP_GEN(dev->mdev, max_qp_cnt))
 		mlx5_ib_dealloc_counters(dev);
@@ -4781,7 +4787,7 @@ static void mlx5_ib_remove(struct mlx5_core_dev *mdev, void *context)
 	mlx5_free_bfreg(dev->mdev, &dev->fp_bfreg);
 	mlx5_free_bfreg(dev->mdev, &dev->bfreg);
 	mlx5_put_uars_page(dev->mdev, mdev->priv.uar);
-	mlx5_ib_cleanup_cong_debugfs(dev);
+	mlx5_ib_cleanup_cong_debugfs(dev, port_num);
 	if (MLX5_CAP_GEN(dev->mdev, max_qp_cnt))
 		mlx5_ib_dealloc_counters(dev);
 	mlx5_ib_cleanup_multiport_master(dev);
