4e0d3fdfd6da52d8f4afed21070d9f87b0904fff
Ben Hutchings <ben@decadent.org.uk>
2018-01-05 03:09:26 +0000

    x86: kvmclock: Disable use from vDSO if KPTI is enabled
    Orabug: 27333764
    CVE: CVE-2017-5754
    
    Currently the pvclock pages aren't being added to user-space page
    tables, and my attempt to fix this didn't work.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    (cherry picked from commit 082345aaa10a01ef140240a1d9f864a3cc17436e)
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    
    Conflicts:
      arch/x86/kernel/kvmclock.c

--- arch/x86/kernel/kvmclock.c
+++ arch/x86/kernel/kvmclock.c
@@ -24,6 +24,7 @@
 #include <linux/percpu.h>
 #include <linux/hardirq.h>
 #include <linux/memblock.h>
+#include <linux/kaiser.h>
 
 #include <asm/x86_init.h>
 #include <asm/reboot.h>
@@ -272,6 +273,13 @@ int __init kvm_setup_vsyscall_timeinfo(void)
 	u8 flags;
 	struct pvclock_vcpu_time_info *vcpu_time;
 	unsigned int size;
+
+	if (!hv_clock)
+		return 0;
+
+	/* FIXME: Need to add pvclock pages to user-space page tables */
+	if (kaiser_enabled)
+		return 0;
 
 	size = PAGE_ALIGN(sizeof(struct pvclock_vsyscall_time_info)*NR_CPUS);
 
