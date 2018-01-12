84766193fce2b0bee7bb222c7a5efe5b94a33b8e
Mike Kravetz <mike.kravetz@oracle.com>
2018-01-12 14:19:36 -0800

    x86/alternatives: add asm ALTERNATIVE macro
    Orabug: 27333764
    CVE: CVE-2017-5754
    
    This was pulled from upstream commit 4332195c5615 (x86/alternatives: Add
    instruction padding).  We can not use the padding part of that commit,
    but still want the ALTERNATIVE macro.
    
    The macro was updated as we do not have the passing as an argument.
    
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>

--- arch/x86/include/asm/alternative-asm.h
+++ arch/x86/include/asm/alternative-asm.h
@@ -26,6 +26,23 @@
 	.byte \alt_len
 .endm
 
+.macro ALTERNATIVE oldinstr, newinstr, feature
+140:
+	\oldinstr
+141:
+	.skip -(((144f-143f)-(141b-140b)) > 0) * ((144f-143f)-(141b-140b)),0x90
+142:
+	.pushsection .altinstructions,"a"
+	altinstruction_entry 140b,143f,\feature,142b-140b,144f-143f
+	.popsection
+
+	.pushsection .altinstr_replacement,"ax"
+143:
+	\newinstr
+144:
+	.popsection
+.endm
+
 #endif  /*  __ASSEMBLY__  */
 
 #endif /* _ASM_X86_ALTERNATIVE_ASM_H */
