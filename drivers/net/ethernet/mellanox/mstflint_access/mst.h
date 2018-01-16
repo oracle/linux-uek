c8fd3b64e5851ae0e0ecb96d8b61d16941a9ddd3
Qing Huang <qing.huang@oracle.com>
2018-01-16 11:27:32 -0800

    mlx4: add mstflint secure boot access kernel support
    Source files are copied from:
    https://github.com/Mellanox/mstflint.git master_devel
    (under kernel sub-directory - changes up to commit
    a8a84fae7459aba01ff6ad6cbc40622b7615b110)
    
    Due to recent security changes in UEK4, mstflint FW tool may
    lose the ability to access CX3 HCAs in Secure Boot enabled
    enviroment. This kernel patch in addition to an enhanced
    version of mstflint tool will let us regain the ability to
    manage CX3 FW when Secure Boot is enabled while running latest
    UEK4 kernels.
    
    Orabug: 27424392
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Rao Shoaib <rao.shoaib@oracle.com>

---
+++ drivers/net/ethernet/mellanox/mstflint_access/mst.h
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2011-2014 Mellanox Technologies, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _MST_H_
+#define _MST_H_
+
+#include <linux/ioctl.h>
+
+
+/****************************************************/
+#define MST_MEMORY_SIZE				(1024*1024)
+#define MST_BLOCK_SIZE				64
+
+#define MST_PARAMS_MAGIC			0xD0
+#define MST_BYTE_ACCESS_MAGIC		0xD1
+#define MST_BLOCK_ACCESS_MAGIC		0xD2
+#define MST_PCICONF_MAGIC			0xD3
+#define MST_PCIMEM_MAGIC			0xD4
+#define MST_CONNECTX_WA_MAGIC		0xD5
+#define MST_VPD_MAGIC				0xD6
+
+
+/****************************************************/
+/* GET PARAMS */
+#define MST_PARAMS _IOR(MST_PARAMS_MAGIC, 1, struct mst_params)
+
+struct mst_params {
+	unsigned int domain;
+	unsigned int bus;
+	unsigned int slot;
+	unsigned int func;
+	unsigned int bar;
+	unsigned int device;
+    unsigned int vendor;
+	unsigned int subsystem_device;
+    unsigned int subsystem_vendor;
+};
+
+/****************************************************/
+/* BYTE ACCESS */
+#define MST_READ4 _IOR(MST_BYTE_ACCESS_MAGIC, 1, struct mst_read4_st)
+
+struct mst_read4_st {
+	unsigned int offset;
+	u32 data;				/* OUT */
+};
+
+
+#define MST_WRITE4 _IOW(MST_BYTE_ACCESS_MAGIC, 2, struct mst_write4_st)
+
+struct mst_write4_st {
+	unsigned int offset;
+	u32 data;
+};
+
+
+#define PCICONF_READ4	MST_READ4
+#define PCICONF_WRITE4	MST_WRITE4
+#define PCIMEM_READ4	MST_READ4
+#define PCIMEM_WRITE4	MST_WRITE4
+
+
+/****************************************************/
+/* BLOCK ACCESS */
+#define PCIMEM_READ_BLOCK _IOR(MST_BLOCK_ACCESS_MAGIC, 1, struct mst_read_block_st)
+
+struct mst_read_block_st {
+	unsigned int offset;
+	unsigned int size;			/* in bytes */
+	u32 data[MST_BLOCK_SIZE];		/* OUT */
+};
+
+
+#define PCIMEM_WRITE_BLOCK _IOW(MST_BLOCK_ACCESS_MAGIC, 2, struct mst_write_block_st)
+
+struct mst_write_block_st {
+	unsigned int offset;
+	unsigned int size;			/* in bytes */
+	u32 data[MST_BLOCK_SIZE];
+};
+
+
+/****************************************************/
+/*
+ * INIT / STOP Conf Access
+ * Used to change conf registers on the fly,
+ * by default we set the conf register to default values
+ */
+#define PCICONF_INIT _IOC(_IOC_NONE, MST_PCICONF_MAGIC, 1, \
+			  sizeof(struct mst_pciconf_init_st))
+
+struct mst_pciconf_init_st {
+	unsigned int domain;
+	unsigned int bus;
+	unsigned int devfn;
+	/* Byte offsets in configuration space */
+	unsigned int addr_reg;
+	unsigned int data_reg;
+};
+
+
+#define PCICONF_STOP _IOC(_IOC_NONE, MST_PCICONF_MAGIC, 2, 0)
+
+
+/****************************************************/
+/*
+ * INIT / STOP Memory Access
+ * Used to change bar number and map the new bar on the fly,
+ * by default we set and map bar to default bar number per device
+ */
+#define PCIMEM_INIT _IOC(_IOC_NONE, MST_PCIMEM_MAGIC, 1, \
+			 sizeof(struct mst_mem_init_st))
+
+struct mst_mem_init_st {
+	unsigned int bar;
+};
+
+
+#define PCIMEM_STOP _IOC(_IOC_NONE, MST_PCIMEM_MAGIC, 2, 0)
+
+
+/****************************************************/
+/* CONNECTX ORDERING WA */
+#define CONNECTX_WA_BASE 0xf0384	/* SEM BASE ADDR. SEM 0xf0380 is reserved for external tools usage. */
+#define CONNECTX_WA_SIZE 3			/* Size in entries */
+
+#define PCI_CONNECTX_WA _IOR(MST_CONNECTX_WA_MAGIC, 1, u_int32_t)
+
+struct mst_connectx_wa {
+	u32 connectx_wa_slot_p1;	/* connectx used slot plus 1. zero means unused */
+};
+
+
+/****************************************************/
+/* VPD ACCESS */
+#define PCICONF_VPD_READ4 _IOR(MST_VPD_MAGIC, 1, struct mst_vpd_read4_st)
+struct mst_vpd_read4_st {
+	unsigned int offset;	/* IN - must be aligned to DWORD */
+	unsigned int timeout;	/* IN - timeout in milliseconds or zero for default timeout */
+	u32 data;				/* OUT */
+
+};
+
+#define PCICONF_VPD_WRITE4 _IOW(MST_VPD_MAGIC, 2, struct mst_vpd_write4_st)
+struct mst_vpd_write4_st {
+	unsigned int offset;	/* IN - must be aligned to DWORD */
+	unsigned int timeout;	/* IN - timeout in milliseconds or zero for default timeout */
+	u32 data;				/* IN */
+};
+
+
+#endif /* _MST_H_ */
---
+++ drivers/net/ethernet/mellanox/mstflint_access/mst_vpd.c
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2011-2014 Mellanox Technologies, Inc. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <linux/highmem.h>
+#include <linux/sched.h>
+#include "mst_kernel.h"
+
+
+/****************************************************/
+int pci_read4_vpd(struct mst_dev_data *dev, unsigned int timeout,
+		unsigned offset, u32 *buf)
+{
+	struct pci_dev *pci_dev = dev->pci_dev;
+	int vpd_cap = dev->vpd_cap_addr;
+	unsigned long end;
+	uint16_t addr;
+	int done, res = 0;
+
+
+	if (!vpd_cap) {
+		mst_err("device %s not support Vital Product Data\n", dev->name);
+		return -ENODEV;
+	}
+
+	if (!timeout) {
+		timeout = MST_VPD_DEFAULT_TOUT;
+	}
+
+	/* sets F bit to zero and write VPD addr */
+	addr = (0x7fff & offset);
+	res = pci_write_config_word(pci_dev, vpd_cap + PCI_VPD_ADDR, addr);
+	if (res) {
+		mst_err("pci_write_config_dword failed\n");
+		return res;
+	}
+
+	/* wait for data until F bit is set with one */
+	addr = 0x0;
+	done = 0;
+
+	end = msecs_to_jiffies(timeout) + jiffies;
+	while (time_before(jiffies, end)) {
+		res = pci_read_config_word(pci_dev, vpd_cap + PCI_VPD_ADDR, &addr);
+		if (res) {
+			mst_err("pci_read_config_word failed\n");
+			return res;
+		}
+
+		if (addr & 0x8000) {
+			done = 1;
+			break;
+		}
+
+		cond_resched();
+	}
+
+	if (!done) {
+		return -ETIMEDOUT;
+	}
+
+	/* read data */
+	res = pci_read_config_dword(pci_dev, vpd_cap + PCI_VPD_DATA, buf);
+	if (res) {
+		mst_err("pci_read_config_word failed\n");
+		return res;
+	}
+
+	return res;
+}
+EXPORT_SYMBOL(pci_read4_vpd);
+
+int pci_write4_vpd(struct mst_dev_data *dev, unsigned int timeout,
+		unsigned offset, u32 buf)
+{
+	struct pci_dev *pci_dev = dev->pci_dev;
+	int vpd_cap = dev->vpd_cap_addr;
+	unsigned long end;
+	uint16_t addr;
+	int done, res = 0;
+
+
+	if (!vpd_cap) {
+		mst_err("device %s not support Vital Product Data\n", dev->name);
+		return -ENODEV;
+	}
+
+	if (!timeout) {
+		timeout = MST_VPD_DEFAULT_TOUT;
+	}
+
+	/* write data */
+	res = pci_write_config_dword(pci_dev, vpd_cap + PCI_VPD_DATA, buf);
+	if (res) {
+		mst_err("pci_read_config_word failed\n");
+		return res;
+	}
+
+	/* sets F bit to one and write VPD addr */
+	addr = 0x8000 | (0x7ffff & offset);
+	res = pci_write_config_word(pci_dev, vpd_cap + PCI_VPD_ADDR, addr);
+	if (res) {
+		mst_err("pci_write_config_dword failed\n");
+		return res;
+	}
+
+	/* wait for data until F bit is set with zero */
+	addr = 0x0;
+	done = 0;
+
+	end = msecs_to_jiffies(timeout) + jiffies;
+	while (time_before(jiffies, end)) {
+		res = pci_read_config_word(pci_dev, vpd_cap + PCI_VPD_ADDR, &addr);
+		if (res) {
+			mst_err("pci_read_config_word failed\n");
+			return res;
+		}
+
+		if (!(addr & 0x8000)) {
+			done = 1;
+			break;
+		}
+
+		cond_resched();
+	}
+
+	if (!done) {
+		return -ETIMEDOUT;
+	}
+
+	return res;
+}
+EXPORT_SYMBOL(pci_write4_vpd);
