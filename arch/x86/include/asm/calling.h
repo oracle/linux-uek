75589ab12dcc35577bc7b925b86900fb7abd57d0
Tim Chen <tim.c.chen@linux.intel.com>
2018-01-17 02:29:54 -0500

    x86/entry: Stuff RSB for entry to kernel for non-SMEP platform
    Stuff RSB ensures that there will be no RSB underflow -- this could
    have lead to the CPU speculating using return addresses in control
    of user space.
    
    To do this we need a working stack to push return addresses to.
    We need to do this as early as possible before any schedule()
    happens. Call/ret should ordinarily be okay -- however, there
    are cases with SMM interrupts and such which could asynchronously
    empty the RSB.
    
    If SMEP is enabled, then this is a non issue because that stops the CPU from
    executing higher CPL instructions.
    
    Note on NMI: right now in case of NMI in the kernel space we might be
    unnecessarily stuffing the RSB. That's because we do not know if the NMI
    occured before or after STUFF_RSB had been executed.
    
    Conflicts:
      arch/x86/kernel/entry_64.S
    
    Orabug: 27376697
    CVE: CVE-2017-5715
    
    (Based on commit 589c20c50d8a1c83184accbce79ea075921bb13d.
     Incorporates commit 89c20c50d8a1c83184accbce79ea075921bb13d
     "x86/ia32: Move STUFF_RSB And ENABLE_IBRS")
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- arch/x86/include/asm/calling.h
+++ arch/x86/include/asm/calling.h
@@ -49,6 +49,7 @@ For 32-bit we have the following conventions - kernel is built with
 #include <asm/dwarf2.h>
 #include <asm/msr-index.h>
 #include <asm/cpufeature.h>
+#include <linux/stringify.h>
 
 /*
  * 64-bit system call stack frame layout defines and helpers,
@@ -199,6 +200,98 @@ For 32-bit we have the following conventions - kernel is built with
 /*
  * IBRS related macros
  */
+
+/*
+ * STUFF_RSB needs a working stack where we would push the return address.
+ * For the actual act of stuffing the RSB, we need "near CALL instructions
+ * with non-zero displacement" (thus the pause instruction.)
+ *
+ * Once we are done, we reclaim stack by moving the RSP.
+ *
+ * The primary side effect is to fill up the RSB.
+ *
+ * This ensures that there will be no RSB underflow -- which could have lead
+ * to the CPU speculating using return addresses in control of user space.
+ * That specific scenario could have arisen when scheduling from a shallow
+ * stack to a deep one.)
+ *
+ * If SMEP is enabled, then this is a non issue because that stops the CPU from
+ * executing higher CPL instructions.
+ *
+ * We want to do this as early as possible before any schedule() happens.
+ */
+#define __ASM_STUFF_RSB                        \
+       call    1f;                             \
+       pause;                                  \
+1:     call    2f;                             \
+       pause;                                  \
+2:     call    3f;                             \
+       pause;                                  \
+3:     call    4f;                             \
+       pause;                                  \
+4:     call    5f;                             \
+       pause;                                  \
+5:     call    6f;                             \
+       pause;                                  \
+6:     call    7f;                             \
+       pause;                                  \
+7:     call    8f;                             \
+       pause;                                  \
+8:     call    9f;                             \
+       pause;                                  \
+9:     call    10f;                            \
+       pause;                                  \
+10:    call    11f;                            \
+       pause;                                  \
+11:    call    12f;                            \
+       pause;                                  \
+12:    call    13f;                            \
+       pause;                                  \
+13:    call    14f;                            \
+       pause;                                  \
+14:    call    15f;                            \
+       pause;                                  \
+15:    call    16f;                            \
+       pause;                                  \
+16:    call    17f;                            \
+       pause;                                  \
+17:    call    18f;                            \
+       pause;                                  \
+18:    call    19f;                            \
+       pause;                                  \
+19:    call    20f;                            \
+       pause;                                  \
+20:    call    21f;                            \
+       pause;                                  \
+21:    call    22f;                            \
+       pause;                                  \
+22:    call    23f;                            \
+       pause;                                  \
+23:    call    24f;                            \
+       pause;                                  \
+24:    call    25f;                            \
+       pause;                                  \
+25:    call    26f;                            \
+       pause;                                  \
+26:    call    27f;                            \
+       pause;                                  \
+27:    call    28f;                            \
+       pause;                                  \
+28:    call    29f;                            \
+       pause;                                  \
+29:    call    30f;                            \
+       pause;                                  \
+30:    call    31f;                            \
+       pause;                                  \
+31:    call    32f;                            \
+       pause;                                  \
+32:                                            \
+       add $(32*8), %rsp;
+
+.macro STUFF_RSB
+	ALTERNATIVE __stringify(__ASM_STUFF_RSB), "", X86_FEATURE_SMEP
+.endm
+
 
 .macro PUSH_MSR_REGS
 	pushq	%rax
