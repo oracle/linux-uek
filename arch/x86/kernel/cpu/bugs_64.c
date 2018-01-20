c3b706a6da9ee67007bb16627fee0c03a7ce8e4b
Thomas Gleixner <tglx@linutronix.de>
2018-01-19 19:06:25 -0500

    x86/cpu: Implement CPU vulnerabilites sysfs functions
    Implement the CPU vulnerabilty show functions for meltdown, spectre_v1 and
    spectre_v2.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Linus Torvalds <torvalds@linuxfoundation.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: David Woodhouse <dwmw@amazon.co.uk>
    Link: https://lkml.kernel.org/r/20180107214913.177414879@linutronix.de
    
    (cherry picked from commit 61dc0f555b5c761cdafb0ba5bd41ecf22d68a4c4)
    
    Signed-off-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    Orabug: 27376697
    CVE: CVE-2017-5715
    CVE: CVE-2017-5753
    Signed-off-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    (cherry picked from commit af73f5915717ac610d75ac5868e220825c69b885)
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- arch/x86/kernel/cpu/bugs_64.c
+++ arch/x86/kernel/cpu/bugs_64.c
@@ -10,6 +10,8 @@
 #include <asm/processor.h>
 #include <asm/mtrr.h>
 #include <asm/cacheflush.h>
+#include <linux/device.h>
+#include <asm/spec_ctrl.h>
 
 void __init check_bugs(void)
 {
@@ -31,3 +33,32 @@ void __init check_bugs(void)
 	if (!direct_gbpages)
 		set_memory_4k((unsigned long)__va(0), 1);
 }
+
+#ifdef CONFIG_SYSFS
+ssize_t cpu_show_meltdown(struct device *dev,
+                         struct device_attribute *attr, char *buf)
+{
+	if (!boot_cpu_has(X86_BUG_CPU_MELTDOWN))
+		return sprintf(buf, "Not affected\n");
+	if (boot_cpu_has(X86_FEATURE_PTI))
+		return sprintf(buf, "Mitigation: PTI\n");
+	return sprintf(buf, "Vulnerable\n");
+}
+
+ssize_t cpu_show_spectre_v1(struct device *dev,
+                           struct device_attribute *attr, char *buf)
+{
+	if (!boot_cpu_has(X86_BUG_SPECTRE_V1)
+		return sprintf(buf, "Not affected\n");
+	return sprintf(buf, "Vulnerable\n");
+}
+
+ssize_t cpu_show_spectre_v2(struct device *dev,
+                           struct device_attribute *attr, char *buf)
+{
+	if (!boot_cpu_has(X86_BUG_SPECTRE_V2) ||
+	    (dynamic_ibrs))
+		return sprintf(buf, "Not affected\n");
+	return sprintf(buf, "Vulnerable\n");
+}
+#endif
