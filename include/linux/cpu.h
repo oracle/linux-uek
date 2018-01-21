80cfed0ba0a2bdf1fb94557e84db0878a99ea7fc
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2018-01-20 21:52:52 -0500

    x86: Fix kABI build breakage
    Remove externs added in commit cf3262703958ab338184ca238337d14b945c72e2
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Orabug: 27376697
    CVE: CVE-2017-5715
    Signed-off-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    (cherry picked from commit 26b3a12acfaf905b7c1061e1934c93c029552478)
    [added include <linux/device.h>]
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -17,6 +17,7 @@
 #include <linux/node.h>
 #include <linux/compiler.h>
 #include <linux/cpumask.h>
+#include <linux/device.h>
 
 struct device;
 
@@ -35,13 +36,6 @@ extern void cpu_remove_dev_attr(struct device_attribute *attr);
 
 extern int cpu_add_dev_attr_group(struct attribute_group *attrs);
 extern void cpu_remove_dev_attr_group(struct attribute_group *attrs);
-
-extern ssize_t cpu_show_meltdown(struct device *dev,
-				 struct device_attribute *attr, char *buf);
-extern ssize_t cpu_show_spectre_v1(struct device *dev,
-				   struct device_attribute *attr, char *buf);
-extern ssize_t cpu_show_spectre_v2(struct device *dev,
-				   struct device_attribute *attr, char *buf);
 
 #ifdef CONFIG_HOTPLUG_CPU
 extern void unregister_cpu(struct cpu *cpu);
