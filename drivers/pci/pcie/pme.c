3234077876bb4364b3dd2d4aeaa3fb7a66a3e7c8
Frederick Lawler <fred@fredlawl.com>
2018-02-13 21:52:18 -0600

    PCI/portdrv: Move pcieport_if.h to drivers/pci/pcie/
    Move pcieport_if.h from include/linux to drivers/pci/pcie/pcieport_if.h
    because the interfaces there are only used by the PCI core.
    
    Replace all uses of #include<linux/pcieport_if.h> with relative paths to
    the new file location, e.g., #include "../pcieport_if.h"
    
    Signed-off-by: Frederick Lawler <fred@fredlawl.com>
    Signed-off-by: Bjorn Helgaas <helgaas@kernel.org>
    
    (cherry picked from commit c37e627f9565368ed7bd1f3cf59a2d223ddba85a)
    
    Orabug: 31318631
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/pci/pcie/pme.c
+++ drivers/pci/pcie/pme.c
@@ -17,9 +17,9 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/device.h>
-#include <linux/pcieport_if.h>
 #include <linux/pm_runtime.h>
 
+#include "pcieport_if.h"
 #include "../pci.h"
 #include "portdrv.h"
 
