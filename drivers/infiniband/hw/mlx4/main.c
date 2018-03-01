d79193bab52f53622e8ab7c5e0c49860cb124a89
Steve Wise <swise@opengridcomputing.com>
2018-03-01 13:58:20 -0800

    mlx4_ib: zero out struct ib_pd when allocating
    Zero out the fields of the struct ib_pd for user mode pds so that
    users querying pds via nldev will not get garbage.  For simplicity,
    use kzalloc() to allocate the mlx4_ib_pd struct.
    
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>
    Reviewed-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    (cherry picked from upstream commit 5292443431fff5ba20f1b48985d382dd824016eb)
    
    Orabug: 27616702
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>

--- drivers/infiniband/hw/mlx4/main.c
+++ drivers/infiniband/hw/mlx4/main.c
@@ -1312,7 +1312,7 @@ static struct ib_pd *mlx4_ib_alloc_pd(struct ib_device *ibdev,
 	struct mlx4_ib_pd *pd;
 	int err;
 
-	pd = kmalloc(sizeof *pd, GFP_KERNEL);
+	pd = kzalloc(sizeof(*pd), GFP_KERNEL);
 	if (!pd)
 		return ERR_PTR(-ENOMEM);
 
@@ -1328,7 +1328,6 @@ static struct ib_pd *mlx4_ib_alloc_pd(struct ib_device *ibdev,
 			kfree(pd);
 			return ERR_PTR(-EFAULT);
 		}
-
 	return &pd->ibpd;
 }
 
