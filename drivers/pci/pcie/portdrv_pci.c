1525a06e1ba0926f29e3d678619952efc13ba97c
Bjorn Helgaas <bhelgaas@google.com>
2018-03-09 11:06:56 -0600

    PCI/portdrv: Disable port driver in compat mode
    The "pcie_ports=compat" kernel parameter sets pcie_ports_disabled, which is
    intended to disable the PCIe port driver.  But even when it was disabled,
    we registered pcie_portdriver so we could work around a BIOS PME issue (see
    fe31e69740ed ("PCI/PCIe: Clear Root PME Status bits early during system
    resume")).
    
    Registering the driver meant that the pcie_portdrv_probe() path called
    pci_enable_device(), pci_save_state(), pm_runtime_set_autosuspend_delay(),
    pm_runtime_use_autosuspend(), etc., even when the driver was disabled.
    
    We've since moved the BIOS PME workaround from the port driver to the core,
    so stop registering the PCIe port driver in compat mode.
    
    This means "pcie_ports=compat" will now be basically the same as turning
    off CONFIG_PCIEPORTBUS completely.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    (cherry picked from commit 79a011194b23302dc43f265ed4237054877768ff)
    
    Orabug: 31318631
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/pci/pcie/portdrv_pci.c
+++ drivers/pci/pcie/portdrv_pci.c
@@ -274,7 +274,7 @@ static int __init pcie_portdrv_init(void)
 	int retval;
 
 	if (pcie_ports_disabled)
-		return pci_register_driver(&pcie_portdriver);
+		return -EACCES;
 
 	dmi_check_system(pcie_portdrv_dmi_table);
 
