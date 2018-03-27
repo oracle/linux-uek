7e1b8ec54a8e1015340f3354b2c70cd950cb0264
Andy Lutomirski <luto@kernel.org>
2018-03-27 18:28:03 +0200

    x86/entry/64: Dont use IST entry for #BP stack
    There's nothing IST-worthy about #BP/int3.  We don't allow kprobes
    in the small handful of places in the kernel that run at CPL0 with
    an invalid stack, and 32-bit kernels have used normal interrupt
    gates for #BP forever.
    
    Furthermore, we don't allow kprobes in places that have usergs while
    in kernel mode, so "paranoid" is also unnecessary.
    
    OraBug: 27895351
    CVE: CVE-2018-8897
    
    Signed-off-by: Andy Lutomirski <luto@kernel.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit d8ba61ba58c88d5207c1ba2f7d9a2280e7d03be9)
    
    Reviewed-by: Jamie Iles <jamie.iles@oracle.com>
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -626,7 +626,7 @@ void __init early_trap_init(void)
 {
 	set_intr_gate_ist(1, &debug, DEBUG_STACK);
 	/* int3 can be called from all */
-	set_system_intr_gate_ist(3, &int3, DEBUG_STACK);
+	set_system_intr_gate(3, &int3);
 	set_intr_gate(14, &page_fault);
 	load_idt(&idt_descr);
 }
