fc673ecd81ab084991052555b19e6c720380cae6
Krish Sadhukhan <krish.sadhukhan@oracle.com>
2018-04-05 19:07:50 -0400

    x86/spec: Fix wrong output from sysfs
    Commit 60ce3575b0c9f18393ba03630c6bafcf2a53289d ("x86/spec: Also print IBRS
    if IBPB is disabled") used 'ibrs_inuse' and 'ibpb_inuse' as variables instead
    of calling them as functions. This caused wrong output from sysfs.
    
    Orabug: 27795350
    Suggested-by: Ankur Auroa <ankur.a.arora@oracle.com>
    Signed-of-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    Reviewed-by: Mark Kanda <mark.kanda@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/bugs_64.c
+++ arch/x86/kernel/cpu/bugs_64.c
@@ -119,10 +119,10 @@ ssize_t cpu_show_spectre_v2(struct device *dev,
 {
 	if (!boot_cpu_has(X86_BUG_SPECTRE_V2))
 		return sprintf(buf, "Not affected\n");
-	if (ibrs_inuse || ibpb_inuse)
+	if (ibrs_inuse() || ibpb_inuse())
 		return sprintf(buf, "Mitigation: %s%s\n",
-				ibrs_inuse ? "IBRS " : "",
-				ibpb_inuse ? "IBPB" : "");
+				ibrs_inuse() ? "IBRS " : "",
+				ibpb_inuse() ? "IBPB" : "");
 
 	return sprintf(buf, "Vulnerable\n");
 }
