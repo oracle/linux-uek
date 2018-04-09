8b1ed5179e55424c63c53f656e17fcf27f10b8df
Keith Busch <keith.busch@intel.com>
2018-04-09 16:04:41 -0600

    PCI/AER: Remove unused parameters
    Remove unused "struct pcie_device *" parameters to handle_error_source()
    and aer_process_err_devices().  No functional change intended.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    (cherry picked from commit 8f6fb677751f4da4b597120dbdcc7bee8e02647c)
    
    Orabug: 31318631
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/pci/pcie/aer/aerdrv_core.c
+++ drivers/pci/pcie/aer/aerdrv_core.c
@@ -555,15 +555,12 @@ failed:
 
 /**
  * handle_error_source - handle logging error into an event log
- * @aerdev: pointer to pcie_device data structure of the root port
  * @dev: pointer to pci_dev data structure of error source device
  * @info: comprehensive error information
  *
  * Invoked when an error being detected by Root Port.
  */
-static void handle_error_source(struct pcie_device *aerdev,
-	struct pci_dev *dev,
-	struct aer_err_info *info)
+static void handle_error_source(struct pci_dev *dev, struct aer_err_info *info)
 {
 	int pos;
 
@@ -709,8 +706,7 @@ static int get_device_error_info(struct pci_dev *dev, struct aer_err_info *info)
 	return 1;
 }
 
-static inline void aer_process_err_devices(struct pcie_device *p_device,
-			struct aer_err_info *e_info)
+static inline void aer_process_err_devices(struct aer_err_info *e_info)
 {
 	int i;
 
@@ -721,7 +717,7 @@ static inline void aer_process_err_devices(struct pcie_device *p_device,
 	}
 	for (i = 0; i < e_info->error_dev_num && e_info->dev[i]; i++) {
 		if (get_device_error_info(e_info->dev[i], e_info))
-			handle_error_source(p_device, e_info->dev[i], e_info);
+			handle_error_source(e_info->dev[i], e_info);
 	}
 }
 
@@ -752,7 +748,7 @@ static void aer_isr_one_error(struct pcie_device *p_device,
 		aer_print_port_info(p_device->port, e_info);
 
 		if (find_source_device(p_device->port, e_info))
-			aer_process_err_devices(p_device, e_info);
+			aer_process_err_devices(e_info);
 	}
 
 	if (e_src->status & PCI_ERR_ROOT_UNCOR_RCV) {
@@ -771,7 +767,7 @@ static void aer_isr_one_error(struct pcie_device *p_device,
 		aer_print_port_info(p_device->port, e_info);
 
 		if (find_source_device(p_device->port, e_info))
-			aer_process_err_devices(p_device, e_info);
+			aer_process_err_devices(e_info);
 	}
 }
 
