cb0cb355d72bd83af251af46813e2585ba4dc8ca
Jim Quigley <Jim.Quigley@oracle.com>
2018-04-16 13:47:25 +0100

    x86/CPU/AMD: Add the Secure Encrypted Virtualization CPU feature
    Update the CPU features to include identifying and reporting on the
    Secure Encrypted Virtualization (SEV) feature.  SEV is identified by
    CPUID 0x8000001f, but requires BIOS support to enable it (set bit 23 of
    MSR_K8_SYSCFG and set bit 0 of MSR_K7_HWCR).  Only show the SEV feature
    as available if reported by CPUID and enabled by BIOS.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: "Radim Krčmář" <rkrcmar@redhat.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: kvm@vger.kernel.org
    Cc: x86@kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    (cherry picked from commit 18c71ce9c8822d48d2b4c50242051535d46082ac)
    Signed-off-by: Jim Quigley <Jim.Quigley@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    
    Orabug: Orabug: 27786605
    
    Conflicts:
    	arch/x86/include/asm/cpufeatures.h
    	arch/x86/kernel/cpu/common.c
    
    Contextual diffs due to out of order commits of
    
    de791821c295cc61419a06fe5562288417d1bc58 x86/pti: Rename BUG_CPU_INSECURE to BUG_CPU_MELTDOWN
    99c6fa2511d8a683e61468be91b83f85452115fa x86/cpufeatures: Add X86_BUG_SPECTRE_V[12]
    
    Corresponding UEK5 commits :-
    
    commit 72a2beddcd3240047552de69ce45a28029c7e56c
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Mon Dec 4 15:07:33 2017 +0100
    
        x86/cpufeatures: Add X86_BUG_CPU_INSECURE
    
    commit 9298e868dddd820829f814cd25a0f28c92036af7
    Author: David Woodhouse <dwmw@amazon.co.uk>
    Date:   Sat Jan 6 11:49:23 2018 +0000
    
        x86/cpufeatures: Add X86_BUG_SPECTRE_V[12]

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -209,6 +209,7 @@
 
 #define X86_FEATURE_MBA			( 7*32+18) /* Memory Bandwidth Allocation */
 #define X86_FEATURE_RSB_CTXSW		( 7*32+19) /* "" Fill RSB on context switches */
+#define X86_FEATURE_SEV			( 7*32+20) /* AMD Secure Encrypted Virtualization */
 
 #define X86_FEATURE_USE_IBPB		( 7*32+21) /* "" Indirect Branch Prediction Barrier enabled */
 #define X86_FEATURE_USE_IBRS_FW		( 7*32+22) /* "" Use IBRS during runtime firmware calls */
