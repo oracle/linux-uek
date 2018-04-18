38a7e208d23bfaef257b980908f268212f5a3e87
Marc Zyngier <marc.zyngier@arm.com>
2018-04-18 10:39:04 +0100

    KVM: arm/arm64: vgic: Fix source vcpu issues for GICv2 SGI
    Now that we make sure we don't inject multiple instances of the
    same GICv2 SGI at the same time, we've made another bug more
    obvious:
    
    If we exit with an active SGI, we completely lose track of which
    vcpu it came from. On the next entry, we restore it with 0 as a
    source, and if that wasn't the right one, too bad. While this
    doesn't seem to trouble GIC-400, the architectural model gets
    offended and doesn't deactivate the interrupt on EOI.
    
    Another connected issue is that we will happilly make pending
    an interrupt from another vcpu, overriding the above zero with
    something that is just as inconsistent. Don't do that.
    
    The final issue is that we signal a maintenance interrupt when
    no pending interrupts are present in the LR. Assuming we've fixed
    the two issues above, we end-up in a situation where we keep
    exiting as soon as we've reached the active state, and not be
    able to inject the following pending.
    
    The fix comes in 3 parts:
    - GICv2 SGIs have their source vcpu saved if they are active on
      exit, and restored on entry
    - Multi-SGIs cannot go via the Pending+Active state, as this would
      corrupt the source field
    - Multi-SGIs are converted to using MI on EOI instead of NPIE
    
    Fixes: 16ca6a607d84bef0 ("KVM: arm/arm64: vgic: Don't populate multiple LRs with the same vintid")
    Reported-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    
    (cherry picked from commit 53692908b0f594285aba18ab848318262332ed25)
    
    Orabug: 27824321
    
    Signed-off-by: Liam Merwick <Liam.Merwick@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>

--- include/kvm/arm_vgic.h
+++ include/kvm/arm_vgic.h
@@ -127,6 +127,7 @@ struct vgic_irq {
 		u32 mpidr;			/* GICv3 target VCPU */
 	};
 	u8 source;			/* GICv2 SGIs only */
+	u8 active_source;		/* GICv2 SGIs only */
 	u8 priority;
 	enum vgic_irq_config config;	/* Level or edge */
 
