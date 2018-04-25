8c289b412df09eb130a17789a0973360cb19e093
Ankur Arora <ankur.a.arora@oracle.com>
2018-04-25 18:12:25 -0400

    x86/microcode: probe CPU features on microcode update
    Probe for updated CPUID features each time the microcode is
    loaded. Specifically this means when the sysfs cpu/microcode
    nodes are created (which is when the microcode is first loaded)
    or from a user trigger via the sysfs microcode/reload interface.
    
    Orabug: 27928096
    
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    
    (cherry-picked from commit c97a2bf2aa93390b23fbf9adb943e494fee18a18)
    
    conflicts:
      arch/x86/kernel/cpu/microcode/core.c
      arch/x86/include/asm/processor.h
    
    [Backport: call init_scattered_cpuid_features() instead of
     get_cpu_cap().]
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -229,11 +229,12 @@ static ssize_t microcode_write(struct file *file, const char __user *buf,
 	if (do_microcode_update(buf, len) == 0)
 		ret = (ssize_t)len;
 
-	if (ret > 0)
+	if (ret > 0) {
 		perf_check_microcode();
 
-	/* check spec_ctrl capabilities */
-	init_scattered_cpuid_features(&cpu_data(0), GET_CPU_CAP_FULL);
+		/* check spec_ctrl capabilities */
+		init_scattered_cpuid_features(&cpu_data(0), GET_CPU_CAP_FULL);
+	}
 
 	mutex_unlock(&microcode_mutex);
 	put_online_cpus();
@@ -327,8 +328,12 @@ static ssize_t reload_store(struct device *dev,
 		if (!ret)
 			ret = tmp_ret;
 	}
-	if (!ret)
+	if (!ret) {
 		perf_check_microcode();
+
+		init_scattered_cpuid_features(&cpu_data(0), GET_CPU_CAP_FULL);
+	}
+
 	mutex_unlock(&microcode_mutex);
 	put_online_cpus();
 
@@ -577,8 +582,12 @@ static int __init microcode_init(void)
 	mutex_lock(&microcode_mutex);
 
 	error = subsys_interface_register(&mc_cpu_interface);
-	if (!error)
+	if (!error) {
 		perf_check_microcode();
+
+		init_scattered_cpuid_features(&cpu_data(0), GET_CPU_CAP_FULL);
+	}
+
 	mutex_unlock(&microcode_mutex);
 	put_online_cpus();
 
