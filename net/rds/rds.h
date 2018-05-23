eee3ccce2eb43eff72bb7325ad9a1e5d070d32a6
Håkon Bugge <Haakon.Bugge@oracle.com>
2018-05-23 11:21:08 +0200

    Revert "Revert "net/rds: Revert "RDS: add reconnect retry scheme for stalled"
    This reverts commit fde8284e499b ("Revert "net/rds: Revert "RDS: add
    reconnect retry scheme for stalled connections""").
    
    Here is the commit message for the first revert:
    
    <quote>
    Commit 54312602fb26 ("RDS: add reconnect retry scheme for stalled
    connections") introduces the rds_reconnect_timeout to retry the
    connection establishment after sysctl_reconnect_retry_ms (default is
    1000ms). Nevertheless, this proactive mechanism is overkilled and it
    is causing long brownout time in the virtualized environment. In
    short, below are the justifications why said commit is reverted.
    
    a) The retry counter starts ticking after RDS received an ADDR_CHANGE
    event. After receiving an ADDR_CHANGE event, RDS needs to perform shutdown
    via shutdown_worker. Then, initiate a new connection via connect_worker.
    Eventually, a CM REQ is only sent out after RDS received
    RDMA_CM_EVENT_ADDR_RESOLVED and RDMA_CM_EVENT_ROUTE_RESOLVED events. If the
    retry is made to cater for stalled connection due to missing CM messages,
    the retry should only happen after a CM REQ is sent.  With the current
    retry scheme (and with the default 1000 ms) that happens after ADDR_CHANGE
    event, it introduces congestion in the single threaded workqueue.
    
    b) Assuming that we modify the retry counter to start ticking after a
    CM REQ message is sent out. By introducing another retry timeout, it
    complicates the system tuning. Why? First, the
    sysctl_reconnect_retry_ms relies on the underlying
    cma_response_timeout. Any modication of cma_response_timeout requires
    to tune sysctl_reconnect_retry_ms. Second, it is hard to find a
    universal timing that fits all configurations (bare-metal,
    virtualized, mixed environment, and homo/heterogenous system).
    </quote>
    
    Conflicts:
    	net/rds/ib_cm.c
    	net/rds/rdma_transport.c
    	net/rds/threads.c
    
    	Fixed checkpatch warnings.
    
    Orabug: 25521901
    
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Shannon Nelson <shannon.nelson@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 0963187c191b7c98792af0c04d4a7464f7c61042)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 93dd6e3f4b765f30e01fcbd97191f98fb8e278c8)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/rds.h
+++ net/rds/rds.h
@@ -150,8 +150,6 @@ enum {
 #define RDS_MPATH_WORKERS       8
 #define RDS_MPATH_HASH(rs, n) (jhash_1word((rs)->rs_bound_port, \
 			       (rs)->rs_hash_initval) & ((n) - 1))
-/* Bits for c_reconn_flags */
-#define RDS_RECONNECT_TIMEDOUT	0
 enum rds_conn_drop_src {
 	/* rds-core */
 	DR_DEFAULT,
@@ -303,6 +301,15 @@ struct rds_connection {
 	unsigned int		c_proposed_version;
 	unsigned int		c_version;
 	struct net		*c_net;
+
+	/* Re-connect stall diagnostics */
+	unsigned long           c_reconnect_start;
+	unsigned int            c_reconnect_drops;
+	int                     c_reconnect_warn;
+	int                     c_reconnect_err;
+	int			c_to_index;
+
+	unsigned int		c_reconnect;
 
 	/* Qos support */
 	u8                      c_tos;
@@ -1124,8 +1131,6 @@ extern unsigned long rds_sysctl_trace_flags;
 extern unsigned int  rds_sysctl_trace_level;
 extern unsigned int  rds_sysctl_shutdown_trace_start_time;
 extern unsigned int  rds_sysctl_shutdown_trace_end_time;
-extern unsigned long rds_sysctl_reconnect_retry_ms;
-extern unsigned int rds_sysctl_reconnect_max_retries;
 
 /* threads.c */
 int rds_threads_init(void);
