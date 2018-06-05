ce7f3678e9a01ef8af8b455d01dc6d3989f96a3b
Thomas Gleixner <tglx@linutronix.de>
2018-06-06 00:55:39 +0200

    x86/cpu/topology: Provide detect_extended_topology_early()
    To support force disabling of SMT it's required to know the number of
    thread siblings early. detect_extended_topology() cannot be called before
    the APIC driver is selected, so split out the part which initializes
    smp_num_siblings.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    
    Orabug: 28220674
    CVE: CVE-2018-3620
    
    (cherry picked from commit 95f3d39ccf7aaea79d1ffdac1c887c2e100ec1b6)
    
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    
    Conflicts:
    	arch/x86/kernel/cpu/cpu.h
    	arch/x86/kernel/cpu/topology.c
    Contextual

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -190,7 +190,7 @@ extern void init_scattered_cpuid_features(struct cpuinfo_x86 *c,
 extern unsigned int init_intel_cacheinfo(struct cpuinfo_x86 *c);
 extern void init_amd_cacheinfo(struct cpuinfo_x86 *c);
 
-extern void detect_extended_topology(struct cpuinfo_x86 *c);
+extern int detect_extended_topology(struct cpuinfo_x86 *c);
 extern void detect_ht(struct cpuinfo_x86 *c);
 
 #ifdef CONFIG_X86_32
