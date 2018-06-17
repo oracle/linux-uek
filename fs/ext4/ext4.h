76aa1172f69775856fde4e543b81126edfd24461
Theodore Ts'o <tytso@mit.edu>
2018-06-17 00:41:14 -0400

    ext4: add more inode number paranoia checks
    commit c37e9e013469521d9adb932d17a1795c139b36db upstream.
    
    If there is a directory entry pointing to a system inode (such as a
    journal inode), complain and declare the file system to be corrupted.
    
    Also, if the superblock's first inode number field is too small,
    refuse to mount the file system.
    
    This addresses CVE-2018-10882.
    
    https://bugzilla.kernel.org/show_bug.cgi?id=200069
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Cc: stable@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit ff6c96461be35381399466ad58f02b8d78ab480a)
    
    Orabug: 29545568
    CVE: CVE-2018-10882
    
    Signed-off-by: John Donnelly <John.P.Donnelly@oracle.com>
    Reviewed-by: Allen Pais <allen.pais@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	fs/ext4/ext4.h
    	fs/ext4/inode.c
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1236,8 +1236,6 @@ static inline struct timespec ext4_current_time(struct inode *inode)
 static inline int ext4_valid_inum(struct super_block *sb, unsigned long ino)
 {
 	return ino == EXT4_ROOT_INO ||
-		ino == EXT4_JOURNAL_INO ||
-		ino == EXT4_RESIZE_INO ||
 		(ino >= EXT4_FIRST_INO(sb) &&
 		 ino <= le32_to_cpu(EXT4_SB(sb)->s_es->s_inodes_count));
 }
