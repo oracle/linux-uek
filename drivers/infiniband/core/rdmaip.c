1d5c8be853352c7b540b5a84faeb66ee2dd90ff9
Håkon Bugge <Haakon.Bugge@oracle.com>
2018-06-18 11:36:13 +0200

    IB/rdmaip: Fix bug in failover_group parsing
    When supplying a module parameter value for the fail-over group as:
    
    ib0,ib1;ib2,ib3;ib4,ib5;ib6,ib7;ib8,ib9
    
    rdmaip / rds_rdma is unable to parse it correctly. Based on debug
    prints, it does:
    
    lab38 kernel: RDS/IB: ib0 is designated group  1
    lab38 kernel: RDS/IB: ib1 is designated group  1
    lab38 kernel: RDS/IB: ib2 is designated group  2
    (no more prints).
    
    This implies, that for Xn-8 systems, fail-over will not be distributed
    correctly. We see:
    
    lab38 kernel: RDS/IP: IP 192.2.23.100 migrated from ib0 to ib1:P01
    lab38 kernel: RDS/IP: IP 192.2.23.102 migrated from ib2 to ib1:P03
    lab38 kernel: RDS/IP: IP 192.2.23.104 migrated from ib4 to ib3:P05
    lab38 kernel: RDS/IP: IP 192.2.23.106 migrated from ib6 to ib3:P07
    lab38 kernel: RDS/IP: IP 192.2.23.108 migrated from ib8 to ib3:P09
    
    That is, all fail-overs except for ib0 are migrated to ib3.
    
    This commit fixes this. The following values for the module parameter
    have been tested (in user space):
    
    ib0
    ib0,ib1
    ib0,ib1,ib2
    ib0,ib1,ib2,ib3
    ib0;ib1
    ib0,ib1;ib2,ib3
    ib0,ib1,ib2;ib3,ib4,ib5
    ib0,ib1,ib2,ib3;ib4,ib5,ib6,ib7
    ib0;ib1;ib2
    ib0,ib1;ib2,ib3;ib4,ib5
    ib0,ib1,ib2;ib3,ib4,ib5;ib6,ib7,ib8
    ib0,ib1,ib2,ib3;ib4,ib5,ib6,ib7;ib8,ib9,ib10,ib11
    ib0;ib1;ib2;ib3
    ib0,ib1;ib2,ib3;ib4,ib5;ib6,ib7
    ib0,ib1,ib2;ib3,ib4,ib5;ib6,ib7,ib8;ib9,ib10,ib11
    ib0,ib1,ib2,ib3;ib4,ib5,ib6,ib7;ib8,ib9,ib10,ib11;ib12,ib13,ib14,ib15
    
    v1 -> v2:
       * Improved parsing to cover the above examples
    
    Orabug: 28198745
    
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Avinash Repaka <avinash.repaka@oracle.com>
    Reviewed-by: Shannon Nelson <shannon.nelson@oracle.com>
    Reviewed-by: Zhu Yanjun <yanjun.zhu@oracle.com>
    (cherry picked from commit bf8cd0080482fa23ee859cc6118c964693cb3a72
    repo uek-5-master)

--- drivers/infiniband/core/rdmaip.c
+++ drivers/infiniband/core/rdmaip.c
@@ -1893,7 +1893,8 @@ void rdmaip_ip_failover_groups_init(void)
 				}
 			}
 			tok = nxt_tok;
-			nxt_tok = strchr(str, ',');
+			if (nxt_tok)
+				nxt_tok = strchr(nxt_tok, ',');
 			if (nxt_tok) {
 				*nxt_tok = '\0';
 				nxt_tok++;
@@ -1901,7 +1902,8 @@ void rdmaip_ip_failover_groups_init(void)
 		}
 
 		grp = nxt_grp;
-		nxt_grp = strchr(str, ';');
+		if (nxt_grp)
+			nxt_grp = strchr(nxt_grp, ';');
 		if (nxt_grp) {
 			*nxt_grp = '\0';
 			nxt_grp++;
