bittencourtjulival/eclipse_kernel_xiaomi_stone

xfrm: Allow IPsec GSO with software crypto for local sockets.
With support of async crypto operations in the GSO codepath
we have everything in place to allow GSO for local sockets.
This patch enables the GSO codepath.

Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

include/net/xfrm.h
@@ -1910,6 +1910,8 @@ static inline bool xfrm_dst_offload_ok(struct dst_entry *dst)
 		return false;
 
 	xdst = (struct xfrm_dst *) dst;
+	if (!x->xso.offload_handle && !xdst->child->xfrm)
+		return true;
 	if (x->xso.offload_handle && (x->xso.dev == xfrm_dst_path(dst)->dev) &&
 	    !xdst->child->xfrm)
 		return true;

oracle/linux-uek

(cherry picked from commit 2271d5190ec60b06921c2e4e184fd1f4fad4e634)
Orabug: 26719394
Modified to not use xfrm_dst_path() from DaveM's recent struct changes.
Signed-off-by: Shannon Nelson <shannon.nelson@oracle.com>
Reviewed-by: Henry Willard <henry.willard@oracle.com>

include/net/xfrm.h
@@ -1881,6 +1881,8 @@ static inline bool xfrm_dst_offload_ok(struct dst_entry *dst)
 	if (!x || !x->type_offload)
 		return false;
 
+	if (!x->xso.offload_handle && !dst->child->xfrm)
+		return true;
 	if (x->xso.offload_handle && (x->xso.dev == dst->path->dev) &&
 	    !dst->child->xfrm)
 		return true;