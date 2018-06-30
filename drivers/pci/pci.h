6e541302ec00bea0f181ff9403d6ad87c2e433a9
Rajat Jain <rajatja@google.com>
2018-06-30 15:07:17 -0500

    PCI/AER: Define aer_stats structure for AER capable devices
    Define a structure to hold the AER statistics.  There are 2 groups of
    statistics: dev_* counters that are to be collected for all AER capable
    devices and rootport_* counters that are collected for all (AER capable)
    rootports only.  Allocate and free this structure when device is added or
    released (thus counters survive the lifetime of the device).
    
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    (cherry picked from commit db89ccbe52c7885644ba578c7771e57620f879b1)
    
    Orabug: 31318631
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -473,9 +473,11 @@ static inline bool cavium_t93_rev_b0(void)
 #ifdef CONFIG_PCIEAER
 void pci_no_aer(void);
 void pci_aer_init(struct pci_dev *dev);
+void pci_aer_exit(struct pci_dev *dev);
 #else
 static inline void pci_no_aer(void) { }
 static inline int pci_aer_init(struct pci_dev *d) { return -ENODEV; }
+static inline void pci_aer_exit(struct pci_dev *d) { }
 #endif
 
 #endif /* DRIVERS_PCI_H */
