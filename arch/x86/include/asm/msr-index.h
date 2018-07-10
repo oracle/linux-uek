4673d01b61edfe63d2d87856abec8fd0e25a37dd
David Woodhouse <dwmw@amazon.co.uk>
2018-07-10 12:20:42 -0400

    x86/msr: Add definitions for IA32_ARCH_CAPABILITIES MSR
    Add bit definitions for IA32_ARCH_CAPABILITIES MSR.
    
    See Intel's 336996-Speculative-Execution-Side-Channel-Mitigations.pdf
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: gnomes@lxorguk.ukuu.org.uk
    Cc: ak@linux.intel.com
    Cc: ashok.raj@intel.com
    Cc: dave.hansen@intel.com
    Cc: karahmed@amazon.de
    Cc: arjan@linux.intel.com
    Cc: torvalds@linux-foundation.org
    Cc: peterz@infradead.org
    Cc: bp@alien8.de
    Cc: pbonzini@redhat.com
    Cc: tim.c.chen@linux.intel.com
    Cc: gregkh@linux-foundation.org
    Link: https://lkml.kernel.org/r/1516896855-7642-5-git-send-email-dwmw@amazon.co.uk
    
    (cherry picked from commit 1e340c60d0dd3ae07b5bedc16a0469c14b9f3410)
    
    Orabug: 28001909
    
    Signed-off-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>
    Reviewed-by: Konrad Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Mihai Carabas <mihai.carabas@oracle.com>
    Reviewed-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    Signed-off-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    
    Partial cherry pick (only taking the definitions for ARCH_CAP_RDCL_NO
    and ARCH_CAP_IBRS_ALL).
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -42,7 +42,11 @@
 #define ATM_LNC_C6_AUTO_DEMOTE		(1UL << 25)
 
 #define MSR_MTRRcap			0x000000fe
+
 #define MSR_IA32_ARCH_CAPABILITIES	0x0000010a
+#define ARCH_CAP_RDCL_NO		(1 << 0) /* Not susceptible to Meltdown */
+#define ARCH_CAP_IBRS_ALL		(1 << 1) /* Enhanced IBRS support */
+
 #define MSR_IA32_BBL_CR_CTL		0x00000119
 #define MSR_IA32_BBL_CR_CTL3		0x0000011e
 
