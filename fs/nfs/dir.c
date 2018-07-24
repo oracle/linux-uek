c95f32c06f763fcc180cda9c123c6b4ff2302b1b
Trond Myklebust <trond.myklebust@hammerspace.com>
2018-07-24 14:27:11 -0400

    NFS: More excessive attribute revalidation in nfs_execute_ok()
    execute_ok() will only check the mode bits if the object is not a
    directory, so we don't need to revalidate the attributes in that case.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    Orabug: 31378833
    (cherry picked from commit 3825827ebf9973600347b16e848f3de52262ab6b)
    Signed-off-by: Calum Mackay <calum.mackay@oracle.com>
    Reviewed-by: John Donnelly <John.p.donnelly@oracle.com>
    
    The conflict is due to changed surrounding code: nfs_check_cache_invalid()
    checks for NFS_INO_INVALID_OTHER upstream. The patch content is the same:
    adding the return if it's a directory.
    
    Conflicts:
    	fs/nfs/dir.c
    
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -2531,6 +2531,8 @@ static int nfs_execute_ok(struct inode *inode, int mask)
 	struct nfs_server *server = NFS_SERVER(inode);
 	int ret = 0;
 
+	if (S_ISDIR(inode->i_mode))
+		return 0;
 	if (nfs_check_cache_invalid(inode, NFS_INO_INVALID_ACCESS)) {
 		if (mask & MAY_NOT_BLOCK)
 			return -ECHILD;
