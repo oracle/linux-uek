60abdb8c7b2d6b1a4784724271369b13e08dcaf0
Sunil Goutham <sgoutham@marvell.com>
2018-08-07 16:56:24 +0530

    net: octeontx2: Init mailbox communication with AF
    RVU PF and AF (admin function) shares 64KB of reserved memory region
    for communication. This patch initializes PF <=> AF mailbox IRQs,
    registers handlers for processing mbox messages received from AF.
    This patch adds support only to handle responses sent by AF
    for the mbox messages sent by PF.
    
    Mbox communication APIs and message formats are defined in AF driver
    (drivers/soc/marvell/soc/octeontx2), mbox.h from AF driver is included
    here to avoid duplication.
    
    Orabug: 30484033
    
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/otx2_common.h
@@ -11,6 +11,10 @@
 #ifndef OTX2_COMMON_H
 #define OTX2_COMMON_H
 
+#include <mbox.h>
+
+#include "otx2_reg.h"
+
 /* PCI device IDs */
 #define PCI_DEVID_OCTEONTX2_RVU_PF              0xA063
 
@@ -18,10 +22,26 @@
 #define PCI_CFG_REG_BAR_NUM                     2
 #define PCI_MBOX_BAR_NUM                        4
 
+#define NAME_SIZE                               32
+
+struct  mbox {
+	struct otx2_mbox	mbox;
+	struct work_struct	mbox_wrk;
+	struct otx2_mbox	mbox_up;
+	struct work_struct	mbox_up_wrk;
+	struct otx2_nic		*pfvf;
+};
+
 struct otx2_hw {
+	struct pci_dev		*pdev;
 	u16                     rx_queues;
 	u16                     tx_queues;
 	u16			max_queues;
+
+	/* MSI-X*/
+	u16			num_vec;
+	bool			*irq_allocated;
+	char			*irq_name;
 };
 
 struct otx2_nic {
@@ -30,6 +50,51 @@ struct otx2_nic {
 	struct device		*dev;
 	struct net_device	*netdev;
 
+	u16			pcifunc;
 	struct otx2_hw		hw;
+	struct mbox		mbox;
+	struct workqueue_struct *mbox_wq;
 };
+
+/* Register read/write APIs */
+static inline void otx2_write64(struct otx2_nic *nic, u64 offset, u64 val)
+{
+	writeq(val, nic->reg_base + offset);
+}
+
+static inline u64 otx2_read64(struct otx2_nic *nic, u64 offset)
+{
+	return readq(nic->reg_base + offset);
+}
+
+/* Mbox APIs */
+static inline int otx2_sync_mbox_msg(struct mbox *mbox)
+{
+	if (!otx2_mbox_nonempty(&mbox->mbox, 0))
+		return 0;
+	otx2_mbox_msg_send(&mbox->mbox, 0);
+	return otx2_mbox_wait_for_rsp(&mbox->mbox, 0);
+}
+
+#define M(_name, _id, _req_type, _rsp_type)				\
+static struct _req_type __maybe_unused					\
+*otx2_mbox_alloc_msg_ ## _name(struct mbox *mbox)			\
+{									\
+	struct _req_type *req;						\
+									\
+	req = (struct _req_type *)otx2_mbox_alloc_msg_rsp(		\
+		&mbox->mbox, 0, sizeof(struct _req_type),		\
+		sizeof(struct _rsp_type));				\
+	if (!req)							\
+		return NULL;						\
+	req->hdr.sig = OTX2_MBOX_REQ_SIG;				\
+	req->hdr.id = _id;						\
+	return req;							\
+}
+
+MBOX_MESSAGES
+#undef M
+
+int otx2_enable_msix(struct otx2_hw *hw);
+void otx2_disable_msix(struct otx2_nic *pfvf);
 #endif /* OTX2_COMMON_H */
