670075cf2d19352ea7c49ada406332a7b6f19688
subhra mazumdar <subhra.mazumdar@oracle.com>
2018-08-08 11:27:07 -0700

    sched: limit cpu search in select_idle_cpu
    Put upper and lower limit on cpu search of select_idle_cpu. The lower limit
    is amount of cpus in a core while upper limit is twice that. This ensures
    for any architecture we will usually search beyond a core. The upper limit
    also helps in keeping the search cost low and constant.
    
    Orabug: 28482695
    Signed-off-by: subhra mazumdar <subhra.mazumdar@oracle.com>
    Reviewed-by: Rohit Jain <rohit.k.jain@oracle.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5764,7 +5764,7 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 	u64 avg_cost, avg_idle;
 	u64 time, cost;
 	s64 delta;
-	int cpu, nr = INT_MAX;
+	int cpu, limit, floor, nr = INT_MAX;
 
 	this_sd = rcu_dereference(*this_cpu_ptr(&sd_llc));
 	if (!this_sd)
@@ -5782,10 +5782,17 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 
 	if (sched_feat(SIS_PROP)) {
 		u64 span_avg = sd->span_weight * avg_idle;
-		if (span_avg > 4*avg_cost)
+		floor = cpumask_weight(topology_sibling_cpumask(target));
+		if (floor < 2)
+			floor = 2;
+		limit = floor << 1;
+		if (span_avg > floor*avg_cost) {
 			nr = div_u64(span_avg, avg_cost);
-		else
-			nr = 4;
+			if (nr > limit)
+				nr = limit;
+		} else {
+			nr = floor;
+		}
 	}
 
 	time = local_clock();
