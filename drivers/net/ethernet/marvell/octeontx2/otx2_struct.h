fce351df635941ede4b92ca1d2a7f77c5a4fb9a0
Sunil Goutham <sgoutham@marvell.com>
2018-08-09 17:07:09 +0530

    net: octeontx2: Add receive packet handling support
    This patch adds support to process CQE_RX queued for
    a received packet, prepares an skb and handovers to stack.
    
    Orabug: 30484033
    
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/otx2_struct.h
+++ drivers/net/ethernet/marvell/octeontx2/otx2_struct.h
@@ -27,6 +27,19 @@ enum nix_xqe_type {
 	NIX_XQE_TYPE_SEND      = 0x8,
 };
 
+/* NIX CQE/SQE subdescriptor types */
+enum nix_subdc {
+	NIX_SUBDC_NOP  = 0x0,
+	NIX_SUBDC_EXT  = 0x1,
+	NIX_SUBDC_CRC  = 0x2,
+	NIX_SUBDC_IMM  = 0x3,
+	NIX_SUBDC_SG   = 0x4,
+	NIX_SUBDC_MEM  = 0x5,
+	NIX_SUBDC_JUMP = 0x6,
+	NIX_SUBDC_WORK = 0x7,
+	NIX_SUBDC_SOD  = 0xf,
+};
+
 /* NIX CQE header structure */
 struct nix_cqe_hdr_s {
 #if defined(__BIG_ENDIAN_BITFIELD)
@@ -41,6 +54,152 @@ struct nix_cqe_hdr_s {
 	u64 reserved_52_57        : 6;
 	u64 node                  : 2;
 	u64 cqe_type              : 4;
+#endif
+};
+
+/* NIX CQE RX parse structure */
+struct nix_rx_parse_s {
+#if defined(__BIG_ENDIAN_BITFIELD)  /* W0 */
+	u64 lhtype       : 4;
+	u64 lgtype       : 4;
+	u64 lftype       : 4;
+	u64 letype       : 4;
+	u64 ldtype       : 4;
+	u64 lctype       : 4;
+	u64 lbtype       : 4;
+	u64 latype       : 4;
+	u64 errcode      : 8;
+	u64 errlev       : 4;
+	u64 wqwd         : 1;
+	u64 express      : 1;
+	u64 rsvd_17      : 1;
+	u64 desc_sizem1  : 5;
+	u64 chan         : 12;
+#else
+	u64 chan         : 12;
+	u64 desc_sizem1  : 5;
+	u64 rsvd_17      : 1;
+	u64 express      : 1;
+	u64 wqwd         : 1;
+	u64 errlev       : 4;
+	u64 errcode      : 8;
+	u64 latype       : 4;
+	u64 lbtype       : 4;
+	u64 lctype       : 4;
+	u64 ldtype       : 4;
+	u64 letype       : 4;
+	u64 lftype       : 4;
+	u64 lgtype       : 4;
+	u64 lhtype       : 4;
+#endif
+#if defined(__BIG_ENDIAN_BITFIELD)  /* W1 */
+	u64 vtag1_tci    : 16;
+	u64 vtag0_tci    : 16;
+	u64 rsvd_95_94   : 2;
+	u64 pkind        : 6;
+	u64 vtag1_gone   : 1;
+	u64 vtag1_valid  : 1;
+	u64 vtag0_gone   : 1;
+	u64 vtag0_valid  : 1;
+	u64 l3b          : 1;
+	u64 l3m          : 1;
+	u64 l2b          : 1;
+	u64 l2m          : 1;
+	u64 pkt_lenm1    : 16;
+#else
+	u64 pkt_lenm1    : 16;
+	u64 l2m          : 1;
+	u64 l2b          : 1;
+	u64 l3m          : 1;
+	u64 l3b          : 1;
+	u64 vtag0_valid  : 1;
+	u64 vtag0_gone   : 1;
+	u64 vtag1_valid  : 1;
+	u64 vtag1_gone   : 1;
+	u64 pkind        : 6;
+	u64 rsvd_95_94   : 2;
+	u64 vtag0_tci    : 16;
+	u64 vtag1_tci    : 16;
+#endif
+#if defined(__BIG_ENDIAN_BITFIELD)  /* W2 */
+	u64 lhflags      : 8;
+	u64 lgflags      : 8;
+	u64 lfflags      : 8;
+	u64 leflags      : 8;
+	u64 ldflags      : 8;
+	u64 lcflags      : 8;
+	u64 lbflags      : 8;
+	u64 laflags      : 8;
+#else
+	u64 laflags      : 8;
+	u64 lbflags      : 8;
+	u64 lcflags      : 8;
+	u64 ldflags      : 8;
+	u64 leflags      : 8;
+	u64 lfflags      : 8;
+	u64 lgflags      : 8;
+	u64 lhflags      : 8;
+#endif
+#if defined(__BIG_ENDIAN_BITFIELD)  /* W3 */
+	u64 match_id     : 16;
+	u64 pb_aura      : 20;
+	u64 wqe_aura     : 20;
+	u64 eoh_ptr      : 8;
+#else
+	u64 eoh_ptr      : 8;
+	u64 wqe_aura     : 20;
+	u64 pb_aura      : 20;
+	u64 match_id     : 16;
+#endif
+#if defined(__BIG_ENDIAN_BITFIELD)  /* W4 */
+	u64 lhptr        : 8;
+	u64 lgptr        : 8;
+	u64 lfptr        : 8;
+	u64 leptr        : 8;
+	u64 ldptr        : 8;
+	u64 lcptr        : 8;
+	u64 lbptr        : 8;
+	u64 laptr        : 8;
+#else
+	u64 laptr        : 8;
+	u64 lbptr        : 8;
+	u64 lcptr        : 8;
+	u64 ldptr        : 8;
+	u64 leptr        : 8;
+	u64 lfptr        : 8;
+	u64 lgptr        : 8;
+	u64 lhptr        : 8;
+#endif
+#if defined(__BIG_ENDIAN_BITFIELD)	/* W5 */
+	u64 rsvd_383_341 : 43;
+	u64 flow_key_alg : 5;
+	u64 vtag1_ptr    : 8;
+	u64 vtag0_ptr    : 8;
+#else
+	u64 vtag0_ptr    : 8;
+	u64 vtag1_ptr    : 8;
+	u64 flow_key_alg : 5;
+	u64 rsvd_383_341 : 43;
+#endif
+	u64 rsvd_447_384;		/* W6 */
+};
+
+/* NIX CQE RX scatter/gather subdescriptor structure */
+struct nix_rx_sg_s {
+#if defined(__BIG_ENDIAN_BITFIELD)	/* W0 */
+	u64 subdc      : 4;
+	u64 rsvd_59_50 : 10;
+	u64 segs       : 2;
+	u64 seg3_size  : 16;
+	u64 seg2_size  : 16;
+	u64 seg1_size  : 16;
+#else
+	u64 seg1_size  : 16;
+	u64 seg2_size  : 16;
+	u64 seg3_size  : 16;
+	u64 segs       : 2;
+	u64 rsvd_59_50 : 10;
+	u64 subdc      : 4;
 #endif
 };
 
