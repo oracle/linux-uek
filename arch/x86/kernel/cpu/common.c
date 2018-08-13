45f03f080e2efda39b286f6afdddaadbeb567f36
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2018-08-12 20:53:26 -0400

    x86/speculation: Don't mark cpu_no_l1tf __initconst
    It is called from from identify_cpu() (by way of cpu_set_bug_bits()),
    which is not an __init function.
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    (cherry picked from commit 297fede6d1058d4576dbf11b8776839962684364)
    Orabug: 28505476
    CVE: CVE-2018-3620
    Signed-off-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Acked-by: Mihai Carabas <mihai.carabas@oracle.com>
    
      Conflicts:
     	arch/x86/kernel/cpu/common.c
    
    Cherrypicked from uek/uek-4.1-QU7 because this commit doesn't exist upstream.
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -777,7 +777,7 @@ static const struct x86_cpu_id cpu_no_meltdown[] = {
 	{}
 };
 
-static const __initconst struct x86_cpu_id cpu_no_l1tf[] = {
+static const struct x86_cpu_id cpu_no_l1tf[] = {
        /* in addition to cpu_no_speculation */
        { X86_VENDOR_INTEL,     6,      INTEL_FAM6_ATOM_SILVERMONT1     },
        { X86_VENDOR_INTEL,     6,      INTEL_FAM6_ATOM_SILVERMONT2     },
