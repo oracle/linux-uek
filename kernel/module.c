712135349dd5338c0622edc1413089a2d672c625
Eric Snowberg <eric.snowberg@oracle.com>
2018-08-15 10:33:24 -0600

    Revert "Enforce module signatures if the kernel is locked down"
    This reverts commit 4abc0be756cb957e5cddae9523f6a8802f8f81e2 from UEK5. An
    updated version commit 0efd412378ac ("Enforce module signatures if the kenrel
    is locked down") was submitted after this was applied.
    
    Orabug: 28702669
    Signed-off-by: Eric Snowberg <eric.snowberg@oracle.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>

--- kernel/module.c
+++ kernel/module.c
@@ -2793,8 +2793,7 @@ static int module_sig_check(struct load_info *info, int flags)
 	}
 
 	/* Not having a signature is only an error if we're strict. */
-	if (err == -ENOKEY && !sig_enforce &&
-	    !kernel_is_locked_down("Loading of unsigned modules"))
+	if (err == -ENOKEY && !sig_enforce)
 		err = 0;
 
 	return err;
