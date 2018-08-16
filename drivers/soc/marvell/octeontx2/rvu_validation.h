c1b113b1a8f1583c4694e394a56e55827a4402ed
Stanislaw Kardach <skardach@marvell.com>
2018-08-16 21:50:56 +0530

    soc: octeontx2: Add resource partitioning config
    This commit adds facilities for configuring and tracking resource
    partitioning between RVU PF devices (and indirectly their related
    RVU VFs).
    The AF keeps track of the PF level resource partitioning only because
    partitioning between VFs is a job of each RVU PF and only RVU PF driver
    knows the moment and number of VFs enabled.
    
    For each tracked resource (currently RVU LFs), user can set a limit
    for each RVU PF on how many resources may be attached to it via
    MBOX (MBOX_MSG_ATTACH_RESOURCES msg for LFs).
    This limit value K for PF X means that the sum of attach requests
    for that resource coming from all pcifunc belonging to PF x (called
    PF x's family) cannot exceed K.
    The sum of limit values for a particular resource always has to be lower
    or equal to the total amount of those resources on the system.
    
    The limits are modifiable for PFx until resource attach message is
    received. At that point they become read-only until all resources are
    detached from that device.
    
    The default values for the limits are defined as follows:
    * RVU AF (for its future VF handling)
      - NIX: pci_sriov_get_totalvfs(pdev)
      - NPA: pci_sriov_get_totalvfs(pdev)
    * networking RVU PF
      - NIX: 1 + pci_sriov_get_totalvfs(pdev)
      - NPA: 1 + pci_sriov_get_totalvfs(pdev)
      - rest: 0
    * SSO/TIM RVU PF
      - NPA: pci_sriov_get_totalvfs(pdev)
      - SSO: N(SSO_LFS) / N(SSO_PFS)
      - SSOW: N(SSOW_LFS) / N(SSO_PFS)
      - TIM: N(TIM_LFS) / N(SSO_PFS)
      - CPT: num_online_cpus() / N(SSO_PFS)
      - rest: 0
    * NPA RVU PF
      - NPA: pci_sriov_get_totalvfs(pdev)
      - rest: 0
    * CPT RVU PF
      - CPT: num_online_cpus()
      - NPA: 1
      - rest: 0
    
    Orabug: 30483967
    
    Signed-off-by: Stanislaw Kardach <skardach@marvell.com>
    Signed-off-by: Krzysztof Garczynski <kgarczynski@marvell.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

---
+++ drivers/soc/marvell/octeontx2/rvu_validation.h
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Marvell OcteonTx2 RVU Admin Function driver
+ *
+ * Copyright (C) 2018 Marvell International Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef RVU_VALIDATION_H
+#define RVU_VALIDATION_H
+
+struct rvu;
+struct rvu_quotas;
+
+struct rvu_quota {
+	struct kobj_attribute	sysfs;
+	/* Device to scope logs to */
+	struct device		*dev;
+	/* Kobject of the sysfs file */
+	struct kobject		*parent;
+	/* Pointer to base structure */
+	struct rvu_quotas	*base;
+	/* Argument passed to the quota_ops when this quota is modified */
+	void			*ops_arg;
+	/* Value of the quota */
+	int			val;
+};
+
+struct rvu_quota_ops {
+	/*
+	 * Called before sysfs store(). store() will proceed if returns 0.
+	 * It is called with struct rvu_quotas::lock taken.
+	 */
+	int (*pre_store)(void *arg, struct rvu_quota *quota, int new_val);
+	/** called after sysfs store(). */
+	void (*post_store)(void *arg, struct rvu_quota *quota, int old_val);
+};
+
+struct rvu_quotas {
+	struct rvu_quota_ops	ops;
+	struct mutex		*lock; /* lock taken for each sysfs operation */
+	u32			cnt; /* number of elements in arr */
+	u32			max; /* maximum value for a single quota */
+	u64			max_sum; /* maximum sum of all quotas */
+	struct rvu_quota	a[0]; /* array of quota assignments */
+};
+
+struct rvu_limits {
+	struct rvu_quotas	*sso;
+	struct rvu_quotas	*ssow;
+	struct rvu_quotas	*tim;
+	struct rvu_quotas	*cpt;
+	struct rvu_quotas	*npa;
+	struct rvu_quotas	*nix;
+};
+
+int rvu_policy_init(struct rvu *rvu);
+void rvu_policy_destroy(struct rvu *rvu);
+int rvu_check_rsrc_policy(struct rvu *rvu,
+			  struct rsrc_attach *req, u16 pcifunc);
+
+#endif /* RVU_VALIDATION_H */
