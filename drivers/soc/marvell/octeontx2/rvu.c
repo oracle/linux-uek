acad904af4e4ed9e3c10316d9e7df633b630e419
Sunil Goutham <sgoutham@marvell.com>
2018-08-18 14:39:31 +0530

    soc: octeontx2: Setup resource limits before enabling VFs
    Initialize resource limits before enabling AF's VFs.
    Otherwise after enabling if VFs immediately request for resource
    attach then it will fail.
    
    Orabug: 30484033
    
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/soc/marvell/octeontx2/rvu.c
+++ drivers/soc/marvell/octeontx2/rvu.c
@@ -2265,21 +2265,21 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (err)
 		goto err_flr;
 
-	/* Enable AF's VFs (if any) */
-	err = rvu_enable_sriov(rvu);
+	err = rvu_policy_init(rvu);
 	if (err)
 		goto err_irq;
 
-	err = rvu_policy_init(rvu);
+	/* Enable AF's VFs (if any) */
+	err = rvu_enable_sriov(rvu);
 	if (err)
-		goto err_sriov;
+		goto err_policy;
 
 	/* Initialize debugfs */
 	rvu_dbg_init(rvu);
 
 	return 0;
-err_sriov:
-	rvu_disable_sriov(rvu);
+err_policy:
+	rvu_policy_destroy(rvu);
 err_irq:
 	rvu_unregister_interrupts(rvu);
 err_flr:
