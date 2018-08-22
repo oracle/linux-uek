417872f0dcdf5f0a34d927b604baf0c76effc799
David Daney <david.daney@cavium.com>
2018-08-22 14:14:51 -0700

    arm64: Add MIDR values for Cavium cn83XX SoCs
    Tested-by: Alexander Graf <agraf@suse.de>
    Acked-by: David Daney <david.daney@cavium.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    
    Backported to 4.14
    
    Orabug: 30483967
    
    (upstream commit e982276d8f5c974b838fb22ba8d592feb039a544)
    
    Signed-off-by: Sergey Temerkhanov <stemerkhanov@cavium.com>
    Signed-off-by: Peter Swain <peter.swain@marvell.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -555,7 +555,7 @@ const struct arm64_cpu_capabilities arm64_errata[] = {
 	/* Cavium ThunderX, T83 pass 1.0 */
 		.desc = "Cavium erratum 27456",
 		.capability = ARM64_WORKAROUND_CAVIUM_27456,
-		MIDR_RANGE(MIDR_THUNDERX_83XX, 0x00, 0x00),
+		MIDR_RANGE(MIDR_OCTEON_T83, 0x00, 0x00),
 	},
 #endif
 #ifdef CONFIG_CAVIUM_ERRATUM_30115
@@ -576,7 +576,7 @@ const struct arm64_cpu_capabilities arm64_errata[] = {
 	/* Cavium ThunderX, T83 pass 1.0 */
 		.desc = "Cavium erratum 30115",
 		.capability = ARM64_WORKAROUND_CAVIUM_30115,
-		MIDR_RANGE(MIDR_THUNDERX_83XX, 0x00, 0x00),
+		MIDR_RANGE(MIDR_OCTEON_T83, 0x00, 0x00),
 	},
 #endif
 	{
