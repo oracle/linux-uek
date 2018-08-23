03d68d97535b986af77bf8c8a87731bd9a72f1ff
Jesper Dangaard Brouer <brouer@redhat.com>
2018-08-23 18:44:00 -0400

    bnxt_en: setup xdp_rxq_info
    Orabug: 28440609
    
    Driver hook points for xdp_rxq_info:
     * reg  : bnxt_alloc_rx_rings
     * unreg: bnxt_free_rx_rings
    
    This driver should be updated to re-register when changing
    allocation mode of RX rings.
    
    Tested on actual hardware.
    
    Cc: Andy Gospodarek <andy@greyhouse.net>
    Cc: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit 96a8604f95fa216b9ddfd15c687eed42a2f56901)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt_xdp.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt_xdp.c
@@ -95,6 +95,7 @@ bool bnxt_rx_xdp(struct bnxt *bp, struct bnxt_rx_ring_info *rxr, u16 cons,
 	xdp.data_hard_start = *data_ptr - offset;
 	xdp.data = *data_ptr;
 	xdp.data_end = *data_ptr + *len;
+	xdp.rxq = &rxr->xdp_rxq;
 	orig_data = xdp.data;
 	mapping = rx_buf->mapping - bp->rx_dma_offset;
 
