e435bf55d6909e3a610c6e515363fd1c87912ca9
Darren Kenny <darren.kenny@oracle.com>
2018-08-30 11:20:29 +0100

    kvm: fix possible spectre gadgets in arch/x86/kvm/mtrr.c
    Fix possible Spectre gadgets as found by smatch.
    
    Make use of array_index_nospec() to prevent speculation.
    
    To do this, it was necessary to add a new function to provide the size
    of the range for a given MSR. This should be slightly faster than using
    a barrier like LFENCE.
    
    Orabug: 29719693
    
    Signed-off-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>

--- arch/x86/kvm/mtrr.c
+++ arch/x86/kvm/mtrr.c
@@ -15,6 +15,7 @@
  */
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/nospec.h>
 #include <linux/kvm_host.h>
 #include <asm/mtrr.h>
 
@@ -268,6 +269,16 @@ static int fixed_msr_to_range_index(u32 msr)
 	return fixed_mtrr_seg_unit_range_index(seg, unit);
 }
 
+static int fixed_msr_end_range_index(u32 msr)
+{
+	int seg, unit;
+
+	if (!fixed_msr_to_seg_unit(msr, &seg, &unit))
+		return -1;
+
+	return fixed_mtrr_seg_end_range_index(seg);
+}
+
 static int fixed_mtrr_addr_to_seg(u64 addr)
 {
 	struct fixed_mtrr_segment *mtrr_seg;
@@ -383,9 +394,11 @@ int kvm_mtrr_set_msr(struct kvm_vcpu *vcpu, u32 msr, u64 data)
 		return 1;
 
 	index = fixed_msr_to_range_index(msr);
-	if (index >= 0)
+	if (index >= 0) {
+		index = array_index_nospec(index,
+		    fixed_msr_end_range_index(msr));
 		*(u64 *)&vcpu->arch.mtrr_state.fixed_ranges[index] = data;
-	else if (msr == MSR_MTRRdefType)
+	} else if (msr == MSR_MTRRdefType)
 		vcpu->arch.mtrr_state.deftype = data;
 	else
 		set_var_mtrr_msr(vcpu, msr, data);
@@ -415,6 +428,8 @@ int kvm_mtrr_get_msr(struct kvm_vcpu *vcpu, u32 msr, u64 *pdata)
 
 	index = fixed_msr_to_range_index(msr);
 	if (index >= 0) {
+		index = array_index_nospec(index,
+		    fixed_msr_end_range_index(msr));
 		*pdata = *(u64 *)&vcpu->arch.mtrr_state.fixed_ranges[index];
 	} else if (msr == MSR_MTRRdefType) {
 		*pdata = vcpu->arch.mtrr_state.deftype;
