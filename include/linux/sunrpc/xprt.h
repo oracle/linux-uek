48551fe4d271fc2ddf6464b0c7a2e002b8792877
Trond Myklebust <trond.myklebust@hammerspace.com>
2018-09-03 18:41:32 -0400

    SUNRPC: Turn off throttling of RPC slots for TCP sockets
    The theory was that we would need to grab the socket lock anyway, so we
    might as well use it to gate the allocation of RPC slots for a TCP
    socket.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    (cherry picked from commit 36bd7de949f41d586ef7794169af75462b67acbc)
    Orabug: 31226568
    Signed-off-by: Dai Ngo <dai.ngo@oracle.com>
    Reviewed-by: Calum Mackay <calum.mackay@oracle.com>
    
    Conflicts:
    	include/linux/sunrpc/xprt.h
    	net/sunrpc/xprt.c
    	net/sunrpc/xprtsock.c
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- include/linux/sunrpc/xprt.h
+++ include/linux/sunrpc/xprt.h
@@ -328,7 +328,6 @@ void			xprt_retry_reserve(struct rpc_task *task);
 int			xprt_reserve_xprt(struct rpc_xprt *xprt, struct rpc_task *task);
 int			xprt_reserve_xprt_cong(struct rpc_xprt *xprt, struct rpc_task *task);
 void			xprt_alloc_slot(struct rpc_xprt *xprt, struct rpc_task *task);
-void			xprt_lock_and_alloc_slot(struct rpc_xprt *xprt, struct rpc_task *task);
 bool			xprt_prepare_transmit(struct rpc_task *task);
 void			xprt_transmit(struct rpc_task *task);
 void			xprt_end_transmit(struct rpc_task *task);
