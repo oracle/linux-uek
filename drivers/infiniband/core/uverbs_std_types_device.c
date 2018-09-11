c9259fa0de65ec0e38a9466fb549e1c4b7a3051b
Jason Gunthorpe <jgg@mellanox.com>
2018-09-11 10:34:26 -0600

    RDMA/uverbs: Add a method to introspect handles in a context
    Introduce a helper function gather_objects_handle() to copy object handles
    under a spin lock.
    Expose these objects handles via uverbs ioctl.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    
    Orabug: 29249270
    
    (cherry picked from commit 149d3845f4a548dbc83932fab3491aeb0b070b3a)
    cherry-pick-repo=kernel/git/rdma/rdma.git
    
    Conflicts:
        See Backporting Notes
    
    Backporting Notes:
    
    This patch has re-designed to fit UEK-5 since we have huge gaps all over
    Mellanox drivers (v4.14 compared to v4.21).
    
    Added new file with the required ioctl functionality:
    drivers/infiniband/core/uverbs_std_types_device.c which originally was
    added by - 4785860 - ("RDMA/uverbs: Implement an ioctl that can call
    write and write_ex handlers").  The patch mentioned above has huge
    dependencies all over Mellanox drivers and the core with unrelated hunks
    and funtionality.
    
    Also, changed UVERBS_METHOD_INFO_HANDLES to be 1, same as upstream so
    the API will fit correctly.
    
    Signed-off-by: Erez Alfasi <ereza@mellanox.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Dag Moxnes <dag.moxnes@oracle.com>
    Reviewed-by: Ka-Cheong Poon <ka-cheong.poon@oracle.com>

---
+++ drivers/infiniband/core/uverbs_std_types_device.c
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB
+/*
+ * Copyright (c) 2018, Mellanox Technologies inc.  All rights reserved.
+ */
+#include <rdma/uverbs_ioctl.h>
+#include "uverbs.h"
+#include "rdma_core.h"
+
+static uint32_t *
+gather_objects_handle(struct ib_uverbs_file *ufile,
+		      const struct uverbs_api_object *uapi_object,
+		      ssize_t out_len,
+		      u64 *total)
+{
+	u64 max_count = out_len / sizeof(u32);
+	struct ib_uobject *obj;
+	u64 count = 0;
+	u32 *handles;
+
+	/* Allocated memory that cannot page out where we gather
+	 * all object ids under a spin_lock.
+	 */
+	handles = kvzalloc(out_len, GFP_KERNEL);
+	if (!handles)
+		return ERR_PTR(-ENOMEM);
+
+	spin_lock_irq(&ufile->uobjects_lock);
+	list_for_each_entry(obj, &ufile->uobjects, list) {
+		u32 obj_id = obj->id;
+
+		if (obj->uapi_object != uapi_object)
+			continue;
+
+		if (count >= max_count)
+			break;
+
+		handles[count] = obj_id;
+		count++;
+	}
+	spin_unlock_irq(&ufile->uobjects_lock);
+
+	*total = count;
+	return handles;
+}
+
+static int UVERBS_HANDLER(UVERBS_METHOD_INFO_HANDLES)(
+	struct ib_uverbs_file *ufile, struct uverbs_attr_bundle *attrs)
+{
+	const struct uverbs_api_object *uapi_object;
+	ssize_t out_len;
+	u64 total = 0;
+	u16 object_id;
+	u32 *handles;
+	int ret;
+
+	out_len = uverbs_attr_get_len(attrs, UVERBS_ATTR_INFO_HANDLES_LIST);
+	if (out_len <= 0 || (out_len % sizeof(u32) != 0))
+		return -EINVAL;
+
+	ret = uverbs_get_const(&object_id, attrs, UVERBS_ATTR_INFO_OBJECT_ID);
+	if (ret)
+		return ret;
+
+	uapi_object = uapi_get_object(ufile->device->uapi, object_id);
+	if (!uapi_object)
+		return -EINVAL;
+
+	handles = gather_objects_handle(ufile, uapi_object, out_len, &total);
+	if (IS_ERR(handles))
+		return PTR_ERR(handles);
+
+	ret = uverbs_copy_to(attrs, UVERBS_ATTR_INFO_HANDLES_LIST, handles,
+			     sizeof(u32) * total);
+	if (ret)
+		goto err;
+
+	ret = uverbs_copy_to(attrs, UVERBS_ATTR_INFO_TOTAL_HANDLES, &total,
+			     sizeof(total));
+err:
+	kvfree(handles);
+	return ret;
+}
+
+DECLARE_UVERBS_NAMED_METHOD(
+	UVERBS_METHOD_INFO_HANDLES,
+	/* Also includes any device specific object ids */
+	UVERBS_ATTR_CONST_IN(UVERBS_ATTR_INFO_OBJECT_ID,
+			     enum uverbs_default_objects, UA_MANDATORY),
+	UVERBS_ATTR_PTR_OUT(UVERBS_ATTR_INFO_TOTAL_HANDLES,
+			    UVERBS_ATTR_TYPE(u32), UA_OPTIONAL),
+	UVERBS_ATTR_PTR_OUT(UVERBS_ATTR_INFO_HANDLES_LIST,
+			    UVERBS_ATTR_MIN_SIZE(sizeof(u32)), UA_OPTIONAL));
+
+DECLARE_UVERBS_GLOBAL_METHODS(UVERBS_OBJECT_DEVICE,
+	&UVERBS_METHOD(UVERBS_METHOD_INFO_HANDLES),
+	);
