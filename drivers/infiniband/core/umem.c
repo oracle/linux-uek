de509678b535ab17710c92e9b8a8015817ff94f5
Jason Gunthorpe <jgg@mellanox.com>
2018-09-16 20:48:06 +0300

    RDMA/umem: Get rid of struct ib_umem.odp_data
    This no longer has any use, we can use container_of to get to the
    umem_odp, and a simple flag to indicate if this is an odp MR. Remove the
    few remaining references to it.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    
    Orabug: 29180371
    
    (cherry picked from commit 597ecc5a095406a668e53ab330495ddb65327f77)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 597ecc5a095406a668e53ab330495ddb65327f77
    
    Signed-off-by: Erez Alfasi <ereza@mellanox.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Tested-by: Gerald Gibson <gerald.gibson@oracle.com>
    Reviewed-by: Sharon Liu <sharon.s.liu@oracle.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -114,7 +114,7 @@ struct ib_umem *ib_umem_get(struct ib_ucontext *context, unsigned long addr,
 		umem = kzalloc(sizeof(struct ib_umem_odp), GFP_KERNEL);
 		if (!umem)
 			return ERR_PTR(-ENOMEM);
-		umem->odp_data = to_ib_umem_odp(umem);
+		umem->is_odp = 1;
 	} else {
 		umem = kzalloc(sizeof(*umem), GFP_KERNEL);
 		if (!umem)
@@ -250,7 +250,7 @@ EXPORT_SYMBOL(ib_umem_get);
 static void __ib_umem_release_tail(struct ib_umem *umem)
 {
 	mmdrop(umem->owning_mm);
-	if (umem->odp_data)
+	if (umem->is_odp)
 		kfree(to_ib_umem_odp(umem));
 	else
 		kfree(umem);
@@ -275,7 +275,7 @@ void ib_umem_release(struct ib_umem *umem)
 {
 	struct ib_ucontext *context = umem->context;
 
-	if (umem->odp_data) {
+	if (umem->is_odp) {
 		ib_umem_odp_release(to_ib_umem_odp(umem));
 		__ib_umem_release_tail(umem);
 		return;
@@ -313,7 +313,7 @@ int ib_umem_page_count(struct ib_umem *umem)
 	int n;
 	struct scatterlist *sg;
 
-	if (umem->odp_data)
+	if (umem->is_odp)
 		return ib_umem_num_pages(umem);
 
 	n = 0;
