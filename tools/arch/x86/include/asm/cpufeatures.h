fb70c644fe7aca519a701b8c2794715d40d4a3f8
Liam Merwick <Liam.Merwick@oracle.com>
2018-09-27 14:15:44 +0100

    tools/headers: Synchronize kernel ABI headers, v4.16-rc1
    Sync the following tooling headers with the latest kernel version:
    
      tools/arch/powerpc/include/uapi/asm/kvm.h
      tools/arch/x86/include/asm/cpufeatures.h
      tools/include/uapi/drm/i915_drm.h
      tools/include/uapi/linux/if_link.h
      tools/include/uapi/linux/kvm.h
    
    All the changes are new ABI additions which don't impact their use
    in existing tooling.
    
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit f091f1d6a2b4840c9b631d6138f5354401347863)
    
    Orabug: 28671425
    
    Signed-off-by: Liam Merwick <Liam.Merwick@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    
    Conflicts:
    	tools/arch/powerpc/include/uapi/asm/kvm.h
    	tools/include/uapi/linux/if_link.h
    	tools/include/uapi/linux/kvm.h
    
    [ Manual merge due to tools/headers commits being backported out-of-order
      (4.17 and 4.18 commits were backported before this). Also skipped change
      to if_link.h which is not in UEK5. ]

--- tools/arch/x86/include/asm/cpufeatures.h
+++ tools/arch/x86/include/asm/cpufeatures.h
@@ -210,6 +210,7 @@
 #define X86_FEATURE_SSBD		( 7*32+17) /* Speculative Store Bypass Disable */
 #define X86_FEATURE_MBA			( 7*32+18) /* Memory Bandwidth Allocation */
 #define X86_FEATURE_RSB_CTXSW		( 7*32+19) /* "" Fill RSB on context switches */
+#define X86_FEATURE_SEV			( 7*32+20) /* AMD Secure Encrypted Virtualization */
 
 #define X86_FEATURE_USE_IBPB		( 7*32+21) /* "" Indirect Branch Prediction Barrier enabled */
 #define X86_FEATURE_USE_IBRS_FW		( 7*32+22) /* "" Use IBRS during runtime firmware calls */
