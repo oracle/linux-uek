a54f37d3d5e5bcf8679c9f45e6caabf59adde659
Ross Philipson <ross.philipson@oracle.com>
2018-10-26 15:53:24 -0400

    x86: Secure Launch kernel early boot stub
    The Secure Launch (SL) stub provides the entry point for Intel TXT (and
    later AMD SKINIT) to vector to during the late launch. The symbol
    sl_stub_entry is that entry point and its offset into the kernel is
    conveyed to the launching code using the MLE (Measured Launch
    Environment) header in the structure named mle_header. The offset of the
    MLE header is set in the kernel_info. The routine sl_stub contains the
    very early late launch setup code responsible for setting up the basic
    environment to allow the normal kernel startup_32 code to proceed. It is
    also responsible for properly waking and handling the APs on Intel
    platforms. The routine sl_main which runs after entering 64b mode is
    responsible for measuring configuration and module information before
    it is used like the boot params, the kernel command line, the TXT heap,
    an external initramfs, etc.
    
    Orabug: 33297039
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

--- arch/x86/kernel/asm-offsets.c
+++ arch/x86/kernel/asm-offsets.c
@@ -13,6 +13,8 @@
 #include <linux/hardirq.h>
 #include <linux/suspend.h>
 #include <linux/kbuild.h>
+#include <linux/slr_table.h>
+#include <linux/slaunch.h>
 #include <asm/processor.h>
 #include <asm/thread_info.h>
 #include <asm/sigframe.h>
@@ -120,4 +122,22 @@ static void __used common(void)
 	OFFSET(ARIA_CTX_rounds, aria_ctx, rounds);
 #endif
 
+#ifdef CONFIG_SECURE_LAUNCH
+	BLANK();
+	OFFSET(SL_txt_info, txt_os_mle_data, txt_info);
+	OFFSET(SL_mle_scratch, txt_os_mle_data, mle_scratch);
+	OFFSET(SL_boot_params_addr, txt_os_mle_data, boot_params_addr);
+	OFFSET(SL_ap_wake_block, txt_os_mle_data, ap_wake_block);
+	OFFSET(SL_ap_wake_block_size, txt_os_mle_data, ap_wake_block_size);
+	OFFSET(SL_saved_misc_enable_msr, slr_entry_intel_info, saved_misc_enable_msr);
+	OFFSET(SL_saved_bsp_mtrrs, slr_entry_intel_info, saved_bsp_mtrrs);
+	OFFSET(SL_num_logical_procs, txt_bios_data, num_logical_procs);
+	OFFSET(SL_capabilities, txt_os_sinit_data, capabilities);
+	OFFSET(SL_mle_size, txt_os_sinit_data, mle_size);
+	OFFSET(SL_vtd_pmr_lo_base, txt_os_sinit_data, vtd_pmr_lo_base);
+	OFFSET(SL_vtd_pmr_lo_size, txt_os_sinit_data, vtd_pmr_lo_size);
+	OFFSET(SL_rlp_wakeup_addr, txt_sinit_mle_data, rlp_wakeup_addr);
+	OFFSET(SL_rlp_gdt_base, smx_rlp_mle_join, rlp_gdt_base);
+	OFFSET(SL_rlp_entry_point, smx_rlp_mle_join, rlp_entry_point);
+#endif
 }
