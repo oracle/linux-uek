1626cf1439fe1ea7bd242041bae6b687cb584ff7
Ross Philipson <ross.philipson@oracle.com>
2018-10-26 16:13:03 -0400

    x86: Secure Launch kernel late boot stub
    The routine slaunch_setup is called out of the x86 specific setup_arch()
    routine during early kernel boot. After determining what platform is
    present, various operations specific to that platform occur. This
    includes finalizing setting for the platform late launch and verifying
    that memory protections are in place.
    
    For TXT, this code also reserves the original compressed kernel setup
    area where the APs were left looping so that this memory cannot be used.
    
    Orabug: 33297039
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -21,6 +21,7 @@
 #include <linux/root_dev.h>
 #include <linux/hugetlb.h>
 #include <linux/tboot.h>
+#include <linux/slaunch.h>
 #include <linux/usb/xhci-dbgp.h>
 #include <linux/static_call.h>
 #include <linux/swiotlb.h>
@@ -937,6 +938,8 @@ void __init setup_arch(char **cmdline_p)
 #else
 	early_gart_iommu_check();
 #endif
+
+	slaunch_setup_txt();
 
 	/*
 	 * partially used pages are not usable - thus
