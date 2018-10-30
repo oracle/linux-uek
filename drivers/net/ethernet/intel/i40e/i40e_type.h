1fb067c3559bd7d49c88ec18aaa8197f17ce4b96
Krzysztof Galazka <krzysztof.galazka@intel.com>
2018-10-30 10:50:43 -0700

    i40e: Add capability flag for stopping FW LLDP
    Add HW capability flag to indicate that firmware supports stopping
    LLDP agent. This feature has been added in FW API 1.7 for XL710
    devices and 1.6 for X722. Also raise expected minor version number
    for X722 FW API to 6.
    
    Signed-off-by: Krzysztof Galazka <krzysztof.galazka@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit de10933e37039e1b9d67a3d21d4e0a14c734eaac)
    
    Orabug: 31051191
    CVE: CVE-2019-0140 CVE-2019-0139 CVE-2019-0144
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/intel/i40e/i40e_type.h
+++ drivers/net/ethernet/intel/i40e/i40e_type.h
@@ -598,6 +598,10 @@ struct i40e_hw {
 	struct i40e_dcbx_config desired_dcbx_config; /* CEE Desired Cfg */
 
 #define I40E_HW_FLAG_AQ_SRCTL_ACCESS_ENABLE BIT_ULL(0)
+#define I40E_HW_FLAG_802_1AD_CAPABLE        BIT_ULL(1)
+#define I40E_HW_FLAG_AQ_PHY_ACCESS_CAPABLE  BIT_ULL(2)
+#define I40E_HW_FLAG_NVM_READ_REQUIRES_LOCK BIT_ULL(3)
+#define I40E_HW_FLAG_FW_LLDP_STOPPABLE      BIT_ULL(4)
 	u64 flags;
 
 	/* debug mask */
