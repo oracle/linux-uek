3fb74802f9198e16070ce92ede3963167a2e47ee
Waiman Long <longman@redhat.com>
2018-11-29 13:07:46 -0500

    cpuset: Enable cpuset controller in default hierarchy
    Given the fact that thread mode had been merged into 4.14, it is now
    time to enable cpuset to be used in the default hierarchy (cgroup v2)
    as it is clearly threaded.
    
    The cpuset controller had experienced feature creep since its
    introduction more than a decade ago. Besides the core cpus and mems
    control files to limit cpus and memory nodes, there are a bunch of
    additional features that can be controlled from the userspace. Some of
    the features are of doubtful usefulness and may not be actively used.
    
    This patch enables cpuset controller in the default hierarchy with
    a minimal set of features, namely just the cpus and mems and their
    effective_* counterparts.  We can certainly add more features to the
    default hierarchy in the future if there is a real user need for them
    later on.
    
    Alternatively, with the unified hiearachy, it may make more sense
    to move some of those additional cpuset features, if desired, to
    memory controller or may be to the cpu controller instead of staying
    with cpuset.
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    
    (cherry picked from commit 4ec22e9c5a90e3809dd52014d5d239af8831a520)
    
    Orabug: 29013584
    
    Signed-off-by: chris hyser <chris.hyser@oracle.com>
    Reviewed-by: Tom Hromatka <tom.hromatka@oracle.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1828,12 +1828,11 @@ static s64 cpuset_read_s64(struct cgroup_subsys_state *css, struct cftype *cft)
 	return 0;
 }
 
-
 /*
  * for the common functions, 'private' gives the type of file
  */
 
-static struct cftype files[] = {
+static struct cftype legacy_files[] = {
 	{
 		.name = "cpus",
 		.seq_show = cpuset_common_seq_show,
@@ -1934,6 +1933,47 @@ static struct cftype files[] = {
 
 	{ }	/* terminate */
 };
+
+/*
+ * This is currently a minimal set for the default hierarchy. It can be
+ * expanded later on by migrating more features and control files from v1.
+ */
+static struct cftype dfl_files[] = {
+	{
+		.name = "cpus",
+		.seq_show = cpuset_common_seq_show,
+		.write = cpuset_write_resmask,
+		.max_write_len = (100U + 6 * NR_CPUS),
+		.private = FILE_CPULIST,
+		.flags = CFTYPE_NOT_ON_ROOT,
+	},
+
+	{
+		.name = "mems",
+		.seq_show = cpuset_common_seq_show,
+		.write = cpuset_write_resmask,
+		.max_write_len = (100U + 6 * MAX_NUMNODES),
+		.private = FILE_MEMLIST,
+		.flags = CFTYPE_NOT_ON_ROOT,
+	},
+
+	{
+		.name = "cpus.effective",
+		.seq_show = cpuset_common_seq_show,
+		.private = FILE_EFFECTIVE_CPULIST,
+		.flags = CFTYPE_NOT_ON_ROOT,
+	},
+
+	{
+		.name = "mems.effective",
+		.seq_show = cpuset_common_seq_show,
+		.private = FILE_EFFECTIVE_MEMLIST,
+		.flags = CFTYPE_NOT_ON_ROOT,
+	},
+
+	{ }	/* terminate */
+};
+
 
 /*
  *	cpuset_css_alloc - allocate a cpuset css
@@ -2109,8 +2149,10 @@ struct cgroup_subsys cpuset_cgrp_subsys = {
 	.post_attach	= cpuset_post_attach,
 	.bind		= cpuset_bind,
 	.fork		= cpuset_fork,
-	.legacy_cftypes	= files,
+	.legacy_cftypes	= legacy_files,
+	.dfl_cftypes	= dfl_files,
 	.early_init	= true,
+	.threaded	= true,
 };
 
 /**
