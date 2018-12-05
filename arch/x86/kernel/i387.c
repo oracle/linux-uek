2653a5757c163ab6f48ce77813cf80d71e13ef2a
Oleg Nesterov <oleg@redhat.com>
2018-12-05 15:45:08 -0800

    x86/fpu: Don't reset fpu_counter
    The "else" branch clears ->fpu_counter as a remnant of the lazy FPU
    usage counting:
    
      e07e23e ("[PATCH] non lazy "sleazy" fpu implementation")
    
    However, switch_fpu_prepare() does this now so that else branch is
    superfluous.
    
    If we do use_eager_fpu(), then this has no effect. Otherwise, if we
    actually wanted to prevent fpu preload after the context switch we would
    need to reset it unconditionally, even if __thread_has_fpu().
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/1423252925-14451-2-git-send-email-riel@redhat.com
    Signed-off-by: Borislav Petkov <bp@suse.de>
    (Cherry-picked from commit a9241ea5)
    
    Orabug: 29012034
    Signed-off-by: Mridula Shastry <mridula.c.shastry@oracle.com>
    Reviewed-by: Zhenzhong Duan <zhenzhong.duan@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/i387.c
+++ arch/x86/kernel/i387.c
@@ -122,8 +122,8 @@ void unlazy_fpu(struct task_struct *tsk)
 	if (__thread_has_fpu(tsk)) {
 		__save_init_fpu(tsk);
 		__thread_fpu_end(tsk);
-	} else
-		tsk->fpu_counter = 0;
+	}
+	
 	preempt_enable();
 }
 EXPORT_SYMBOL(unlazy_fpu);
