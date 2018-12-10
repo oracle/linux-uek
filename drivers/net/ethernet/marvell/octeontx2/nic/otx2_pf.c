2965b16b8ce08ce641e7cc8773562f8de885e067
Geetha sowjanya <gakula@marvell.com>
2018-12-10 16:22:12 +0530

    octeontx2-pf: Add mailbox bounce buffer
    When device is attached to a VM via VFIO, one stage mapping
    from intermediate physical address (IPA) to PA of mailbox region
    is marked as device memory. Even if driver in the VM does
    a VA to IPA mapping as normal memory, the whole region will be
    treated as device memory, so unaligned accesses won't be permitted.
    
    This patch adds a bounce buffer for mailbox region so that device
    can prepare mbox messages in the bounce buffer which will unaligned
    accesses. Before triggering notification to admin function, messages
    are copied to HW mbox region.
    
    Change-Id: Iab8e4b22d3232a3c10c54058973cfe9fd0150196
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 79c06519879875879c34c01d3995230ef796747c)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -108,6 +108,8 @@ static irqreturn_t otx2_pfaf_mbox_intr_handler(int irq, void *pf_irq)
 	/* Check for AF => PF response messages */
 	mbox = &pf->mbox.mbox;
 	mdev = &mbox->dev[0];
+	otx2_sync_mbox_bbuf(mbox, 0);
+
 	hdr = (struct mbox_hdr *)(mdev->mbase + mbox->rx_start);
 	if (hdr->num_msgs)
 		queue_work(pf->mbox_wq, &pf->mbox.mbox_wrk);
@@ -211,6 +213,10 @@ static int otx2_pfaf_mbox_init(struct otx2_nic *pf)
 
 	err = otx2_mbox_init(&mbox->mbox, hwbase, pf->pdev, pf->reg_base,
 			     MBOX_DIR_PFAF, 1);
+	if (err)
+		goto exit;
+
+	err = otx2_mbox_bbuf_init(mbox, pf->pdev);
 	if (err)
 		goto exit;
 
