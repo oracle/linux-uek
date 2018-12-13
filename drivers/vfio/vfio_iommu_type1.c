84d659a4f3d14f34b6371c78a51a00c2e9ef71a4
Daniel Jordan <daniel.m.jordan@oracle.com>
2018-12-13 13:18:27 -0500

    vfio: relieve mmap_sem reader cacheline bouncing by holding it longer
    Profiling shows significant time being spent on atomic ops in mmap_sem
    reader acquisition.  mmap_sem is taken and dropped for every single base
    page during pinning, so this is not surprising.
    
    Reduce the number of times mmap_sem is taken by holding for longer,
    which relieves atomic cacheline bouncing.
    
    Results for all VFIO page pinning patches
    -----------------------------------------
    
    The test measures the time from qemu invocation to the start of guest
    boot.  The guest uses kvm with 320G memory backed with THP.  320G fits
    in a node on the test machine used here, so there was no thrashing in
    reclaim because of __GFP_THISNODE in THP allocations[1].
    
    System:           X7-2
    CPU:              2 nodes * 24 cores/node * 2 threads/core = 96 CPUs
                      Intel(R) Xeon(R) Platinum 8160 CPU @ 2.10GHz
    Memory:           754G split evenly between nodes
    scaling_governor: performance
    
                           speedup vs    speedup vs
                 kernel      baseline    prev patch    avg sec ( CV )
               --------    ----------    ----------    --------------
               baseline                                   74.2 (.011)
                patch 6          3.6x          3.6x       20.9 (.021)
                patch 7          4.2x          1.2x       17.5 (.048)
                patch 8          7.4x          1.7x       10.0 (.006)
    
    At patch 6, lock_stat showed long reader wait time on mmap_sem writers,
    leading to patch 7.
    
    At patch 7, profiling revealed the issue with mmap_sem described above.
    
    [1] lkml.kernel.org/r/20180925120326.24392-1-mhocko@kernel.org
    
    CV = coefficient of variation = standard deviation / mean
    
    Signed-off-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>
    Reviewed-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    
    Performance results from this forward port (Orabug 29708084):
    
    System:           X6-2
    CPU:              2 nodes * 10 cores/node * 2 threads/core = 40 CPUs
                      Intel(R) Xeon(R) CPU E5-2630 v4 @ 2.20GHz
    Memory:           251G split evenly between nodes
    scaling_governor: performance
    
                       kernel       speedup    avg sec ( CV )
                     --------    ----------    --------------
                     baseline                    26.25 (.003)
                        ktask          5.5x        4.8 (.012)
    
    On a system with more cores and memory, the speedups are expected to be
    larger.
    
    Orabug: 29708084
    Signed-off-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -337,7 +337,7 @@ static int put_pfn(unsigned long pfn, int prot)
 }
 
 static int vaddr_get_pfn(struct mm_struct *mm, unsigned long vaddr,
-			 int prot, unsigned long *pfn)
+			 int prot, unsigned long *pfn, bool handle_mmap_sem)
 {
 	struct page *page[1];
 	struct vm_area_struct *vma;
@@ -348,7 +348,8 @@ static int vaddr_get_pfn(struct mm_struct *mm, unsigned long vaddr,
 	if (prot & IOMMU_WRITE)
 		flags |= FOLL_WRITE;
 
-	down_read(&mm->mmap_sem);
+	if (handle_mmap_sem)
+		down_read(&mm->mmap_sem);
 	if (mm == current->mm) {
 		ret = get_user_pages(vaddr, 1, flags | FOLL_LONGTERM, page,
 				     vmas);
@@ -367,14 +368,16 @@ static int vaddr_get_pfn(struct mm_struct *mm, unsigned long vaddr,
 			put_page(page[0]);
 		}
 	}
-	up_read(&mm->mmap_sem);
+	if (handle_mmap_sem)
+		up_read(&mm->mmap_sem);
 
 	if (ret == 1) {
 		*pfn = page_to_pfn(page[0]);
 		return 0;
 	}
 
-	down_read(&mm->mmap_sem);
+	if (handle_mmap_sem)
+		down_read(&mm->mmap_sem);
 
 	vaddr = untagged_addr(vaddr);
 
@@ -386,7 +389,8 @@ static int vaddr_get_pfn(struct mm_struct *mm, unsigned long vaddr,
 			ret = 0;
 	}
 
-	up_read(&mm->mmap_sem);
+	if (handle_mmap_sem)
+		up_read(&mm->mmap_sem);
 	return ret;
 }
 
@@ -409,9 +413,12 @@ static long vfio_pin_pages_remote(struct vfio_dma *dma, unsigned long vaddr,
 	if (!mm)
 		return -ENODEV;
 
-	ret = vaddr_get_pfn(mm, vaddr, dma->prot, pfn_base);
-	if (ret)
+	down_read(&mm->mmap_sem);
+	ret = vaddr_get_pfn(mm, vaddr, dma->prot, pfn_base, false);
+	if (ret) {
+		up_read(&mm->mmap_sem);
 		return ret;
+	}
 
 	pinned++;
 	rsvd = is_invalid_reserved_pfn(*pfn_base);
@@ -426,6 +433,7 @@ static long vfio_pin_pages_remote(struct vfio_dma *dma, unsigned long vaddr,
 			put_pfn(*pfn_base, dma->prot);
 			pr_warn("%s: RLIMIT_MEMLOCK (%ld) exceeded\n", __func__,
 					limit << PAGE_SHIFT);
+			up_read(&mm->mmap_sem);
 			return -ENOMEM;
 		}
 		if (*lock_cache <= 0)
@@ -440,7 +448,7 @@ static long vfio_pin_pages_remote(struct vfio_dma *dma, unsigned long vaddr,
 	/* Lock all the consecutive pages from pfn_base */
 	for (vaddr += PAGE_SIZE, iova += PAGE_SIZE; pinned < npage;
 	     pinned++, vaddr += PAGE_SIZE, iova += PAGE_SIZE) {
-		ret = vaddr_get_pfn(mm, vaddr, dma->prot, &pfn);
+		ret = vaddr_get_pfn(mm, vaddr, dma->prot, &pfn, false);
 		if (ret)
 			break;
 
@@ -457,6 +465,7 @@ static long vfio_pin_pages_remote(struct vfio_dma *dma, unsigned long vaddr,
 				pr_warn("%s: RLIMIT_MEMLOCK (%ld) exceeded\n",
 					__func__, limit << PAGE_SHIFT);
 				ret = -ENOMEM;
+				up_read(&mm->mmap_sem);
 				goto unpin_out;
 			}
 			if (*lock_cache <= 0)
@@ -467,6 +476,7 @@ static long vfio_pin_pages_remote(struct vfio_dma *dma, unsigned long vaddr,
 	}
 
 out:
+	up_read(&mm->mmap_sem);
 	ret = vfio_lock_acct(dma, lock_acct, false);
 
 unpin_out:
@@ -513,7 +523,7 @@ static int vfio_pin_page_external(struct vfio_dma *dma, unsigned long vaddr,
 	if (!mm)
 		return -ENODEV;
 
-	ret = vaddr_get_pfn(mm, vaddr, dma->prot, pfn_base);
+	ret = vaddr_get_pfn(mm, vaddr, dma->prot, pfn_base, true);
 	if (!ret && do_accounting && !is_invalid_reserved_pfn(*pfn_base)) {
 		ret = vfio_lock_acct(dma, 1, true);
 		if (ret) {
