7a50b5409bfc01845005bf2ebd4b0b4d34f0679f
Willem de Bruijn <willemb@google.com>
2018-12-30 17:24:36 -0500

    ip: validate header length on virtual device xmit
    [ Upstream commit cb9f1b783850b14cbd7f87d061d784a666dfba1f ]
    
    KMSAN detected read beyond end of buffer in vti and sit devices when
    passing truncated packets with PF_PACKET. The issue affects additional
    ip tunnel devices.
    
    Extend commit 76c0ddd8c3a6 ("ip6_tunnel: be careful when accessing the
    inner header") and commit ccfec9e5cb2d ("ip_tunnel: be careful when
    accessing the inner header").
    
    Move the check to a separate helper and call at the start of each
    ndo_start_xmit function in net/ipv4 and net/ipv6.
    
    This commit is a prerequest of commit 1ca1ba465e55 ("geneve: make sure
    to pull inner header in geneve_rx()") which is a fix for CVE-2024-26857.
    
    This commit also takes part of the 6c1c5097781f5 (“net: add
    atomic_long_t to  net_device_stats fields”). It seems more risky to backport
    the whole commit so we took that commit partially which add DEV_STATS_INC API
    and for that to work properly, we need to have the changes in the struct net_device_stats.
    
    Minor changes:
    - convert dev_kfree_skb to kfree_skb on error path,
      as dev_kfree_skb calls consume_skb which is not for error paths.
    - use pskb_network_may_pull even though that is pedantic here,
      as the same as pskb_may_pull for devices without llheaders.
    - do not cache ipv6 hdrs if used only once
      (unsafe across pskb_may_pull, was more relevant to earlier patch)
    
    Reported-by: syzbot <syzkaller@googlegroups.com>
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit cde81154f86e36df0fad21c23976cc8b80c01600)
    [vegard: drop changes to ip6erspan_tunnel_xmit(), which doesn't exist
     in 4.14 due to missing commit 5a963eb61b7c39e6c422b6e48619d19d04719358
     ("ip6_gre: Add ERSPAN native tunnel support") from 4.16]
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    (cherry picked from commit 683e08ff6ef0a279343ba4383c6e01e4a9143050)
    Signed-off-by: Yifei Liu <yifei.l.liu@oracle.com>
    
    Use UEK_KABI_RENAME to solve the kABI issues.

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -161,31 +161,38 @@ static inline bool dev_xmit_complete(int rc)
  *	(unsigned long) so they can be read and written atomically.
  */
 
+#define NET_DEV_STAT(FIELD)			\
+	union {					\
+		unsigned long FIELD;		\
+		atomic_long_t __##FIELD;	\
+	}
+
 struct net_device_stats {
-	unsigned long	rx_packets;
-	unsigned long	tx_packets;
-	unsigned long	rx_bytes;
-	unsigned long	tx_bytes;
-	unsigned long	rx_errors;
-	unsigned long	tx_errors;
-	unsigned long	rx_dropped;
-	unsigned long	tx_dropped;
-	unsigned long	multicast;
-	unsigned long	collisions;
-	unsigned long	rx_length_errors;
-	unsigned long	rx_over_errors;
-	unsigned long	rx_crc_errors;
-	unsigned long	rx_frame_errors;
-	unsigned long	rx_fifo_errors;
-	unsigned long	rx_missed_errors;
-	unsigned long	tx_aborted_errors;
-	unsigned long	tx_carrier_errors;
-	unsigned long	tx_fifo_errors;
-	unsigned long	tx_heartbeat_errors;
-	unsigned long	tx_window_errors;
-	unsigned long	rx_compressed;
-	unsigned long	tx_compressed;
+	UEK_KABI_RENAME(unsigned long rx_packets, NET_DEV_STAT(rx_packets));
+	UEK_KABI_RENAME(unsigned long tx_packets, NET_DEV_STAT(tx_packets));
+	UEK_KABI_RENAME(unsigned long rx_bytes, NET_DEV_STAT(rx_bytes));
+	UEK_KABI_RENAME(unsigned long tx_bytes, NET_DEV_STAT(tx_bytes));
+	UEK_KABI_RENAME(unsigned long rx_errors, NET_DEV_STAT(rx_errors));
+	UEK_KABI_RENAME(unsigned long tx_errors, NET_DEV_STAT(tx_errors));
+	UEK_KABI_RENAME(unsigned long rx_dropped, NET_DEV_STAT(rx_dropped));
+	UEK_KABI_RENAME(unsigned long tx_dropped, NET_DEV_STAT(tx_dropped));
+	UEK_KABI_RENAME(unsigned long multicast, NET_DEV_STAT(multicast));
+	UEK_KABI_RENAME(unsigned long collisions, NET_DEV_STAT(collisions));
+	UEK_KABI_RENAME(unsigned long rx_length_errors, NET_DEV_STAT(rx_length_errors));
+	UEK_KABI_RENAME(unsigned long rx_over_errors, NET_DEV_STAT(rx_over_errors));
+	UEK_KABI_RENAME(unsigned long rx_crc_errors, NET_DEV_STAT(rx_crc_errors));
+	UEK_KABI_RENAME(unsigned long rx_frame_errors, NET_DEV_STAT(rx_frame_errors));
+	UEK_KABI_RENAME(unsigned long rx_fifo_errors, NET_DEV_STAT(rx_fifo_errors));
+	UEK_KABI_RENAME(unsigned long rx_missed_errors, NET_DEV_STAT(rx_missed_errors));
+	UEK_KABI_RENAME(unsigned long tx_aborted_errors, NET_DEV_STAT(tx_aborted_errors));
+	UEK_KABI_RENAME(unsigned long tx_carrier_errors, NET_DEV_STAT(tx_carrier_errors));
+	UEK_KABI_RENAME(unsigned long tx_fifo_errors, NET_DEV_STAT(tx_fifo_errors));
+	UEK_KABI_RENAME(unsigned long tx_heartbeat_errors, NET_DEV_STAT(tx_heartbeat_errors));
+	UEK_KABI_RENAME(unsigned long tx_window_errors, NET_DEV_STAT(tx_window_errors));
+	UEK_KABI_RENAME(unsigned long rx_compressed, NET_DEV_STAT(rx_compressed));
+	UEK_KABI_RENAME(unsigned long tx_compressed, NET_DEV_STAT(tx_compressed));
 };
+#undef NET_DEV_STAT
 
 
 #include <linux/cache.h>
@@ -4550,5 +4557,10 @@ do {								\
  */
 #define PTYPE_HASH_SIZE	(16)
 #define PTYPE_HASH_MASK	(PTYPE_HASH_SIZE - 1)
+
+/* Note: Avoid these macros in fast path, prefer per-cpu or per-queue counters. */
+#define DEV_STATS_INC(DEV, FIELD) atomic_long_inc(&(DEV)->stats.__##FIELD)
+#define DEV_STATS_ADD(DEV, FIELD, VAL) 	\
+		atomic_long_add((VAL), &(DEV)->stats.__##FIELD)
 
 #endif	/* _LINUX_NETDEVICE_H */
