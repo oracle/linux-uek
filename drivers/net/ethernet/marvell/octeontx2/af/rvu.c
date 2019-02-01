1a4c7b34e6aadf7da12ee9cceed4254119a640c1
Sunil Goutham <sgoutham@marvell.com>
2019-02-01 19:16:35 +0530

    octeontx2-af: Setup resource limits before enabling VFs
    Initialize resource limits before enabling AF's VFs.
    Otherwise after enabling if VFs immediately request for resource
    attach then it will fail.
    
    Change-Id: I5f0869bdbce34a111a36861fa0539e48c40f1c96
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 1f06645685e8c30663687d07c68a53544d0a8e9e)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -2459,19 +2459,18 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (err)
 		goto err_flr;
 
-	/* Enable AF's VFs (if any) */
-	err = rvu_enable_sriov(rvu);
+	err = rvu_policy_init(rvu);
 	if (err)
 		goto err_irq;
 
-	err = rvu_policy_init(rvu);
+	/* Enable AF's VFs (if any) */
+	err = rvu_enable_sriov(rvu);
 	if (err)
-		goto err_sriov;
+		goto err_policy;
 
 	return 0;
-
-err_sriov:
-	rvu_disable_sriov(rvu);
+err_policy:
+	rvu_policy_destroy(rvu);
 err_irq:
 	rvu_unregister_interrupts(rvu);
 err_flr:
