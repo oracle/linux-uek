d9c56087f6e827a89a6eec6f5f59d7e18a9deb51
Aleksandr Loktionov <aleksandr.loktionov@intel.com>
2019-02-06 15:08:16 -0800

    i40e: Implement DDP support in i40e driver
    This patch introduces DDP (Dynamic Device Personalization) which allows
    loading profiles that change the way internal parser interprets processed
    frames. To load DDP profiles it utilizes ethtool flash feature. The files
    with recipes must be located in /var/lib/firmware directory. Afterwards
    the recipe can be loaded by invoking:
    
        ethtool -f <if_name> <file_name> 100
        ethtool -f <if_name> - 100
    
    See further details of this feature in the i40e documentation, or
    visit
    https://www.intel.com/content/www/us/en/architecture-and-technology/ethernet/dynamic-device-personalization-brief.html
    
    The driver shall verify DDP profile can be loaded in accordance with
    the rules:
    * Package with Group ID 0 are exclusive and can only be loaded the first.
    * Packages with Group ID 0x01-0xFE can only be loaded simultaneously
       with the packages from the same group.
    * Packages with Group ID 0xFF are compatible with all other packages.
    
    Signed-off-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    (cherry picked from commit cdc594e00370e153c323cf8aa9c43b66679e09a0)
    
    Orabug: 30939512
    
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/intel/i40e/i40e_ethtool.c
+++ drivers/net/ethernet/intel/i40e/i40e_ethtool.c
@@ -5163,6 +5163,7 @@ static const struct ethtool_ops i40e_ethtool_ops = {
 	.set_link_ksettings	= i40e_set_link_ksettings,
 	.get_fecparam = i40e_get_fec_param,
 	.set_fecparam = i40e_set_fec_param,
+	.flash_device = i40e_ddp_flash,
 };
 
 void i40e_set_ethtool_ops(struct net_device *netdev)
