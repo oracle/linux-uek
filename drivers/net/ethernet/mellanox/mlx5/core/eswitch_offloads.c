cd8b562bd13e207b370dfadd45c554e8de559723
Bodong Wang <bodong@mellanox.com>
2019-02-07 10:40:58 -0600

    net/mlx5: E-Switch, Refactor offloads flow steering init/cleanup
    E-switch offloads mode initialize/cleanup multiple steering related
    entities (flow table/group). Refactor these operations to internal
    helper functions for better block design.
    
    This patch doesn't change any functionality.
    
    Signed-off-by: Bodong Wang <bodong@mellanox.com>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Orabug: 30652299
    
    (cherry picked from commit eca8cc3895358b4c35a3ed439537b8c08d7dabb3)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    NOTE -
    This feature was re-implemented (not backported) on top of UEK-5.
    Therefore this patch is based on its upstream version, but can
    include additional code in order to fit this kernel version.
    
    Signed-off-by: Erez Alfasi <ereza@mellanox.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Tested-by: Gerald Gibson <gerald.gibson@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
@@ -1063,7 +1063,7 @@ static void esw_offloads_devcom_cleanup(struct mlx5_eswitch *esw)
 	mlx5_devcom_unregister_component(devcom, MLX5_DEVCOM_ESW_OFFLOADS);
 }
 
-int esw_offloads_init(struct mlx5_eswitch *esw, int nvports)
+static int esw_offloads_steering_init(struct mlx5_eswitch *esw, int nvports)
 {
 	int err;
 
@@ -1079,6 +1079,32 @@ int esw_offloads_init(struct mlx5_eswitch *esw, int nvports)
 	if (err)
 		goto create_fg_err;
 
+	return 0;
+
+create_fg_err:
+	esw_destroy_offloads_table(esw);
+
+create_ft_err:
+	esw_destroy_offloads_fdb_tables(esw);
+
+	return err;
+}
+
+static void esw_offloads_steering_cleanup(struct mlx5_eswitch *esw)
+{
+	esw_destroy_vport_rx_group(esw);
+	esw_destroy_offloads_table(esw);
+	esw_destroy_offloads_fdb_tables(esw);
+}
+
+int esw_offloads_init(struct mlx5_eswitch *esw, int nvports)
+{
+	int err;
+
+	err = esw_offloads_steering_init(esw, nvports);
+	if (err)
+		return err;
+
 	err = esw_offloads_load_reps(esw, nvports);
 	if (err)
 		goto err_reps;
@@ -1087,14 +1113,7 @@ int esw_offloads_init(struct mlx5_eswitch *esw, int nvports)
 	return 0;
 
 err_reps:
-	esw_destroy_vport_rx_group(esw);
-
-create_fg_err:
-	esw_destroy_offloads_table(esw);
-
-create_ft_err:
-	esw_destroy_offloads_fdb_tables(esw);
-
+	esw_offloads_steering_cleanup(esw);
 	return err;
 }
 
@@ -1121,9 +1140,7 @@ void esw_offloads_cleanup(struct mlx5_eswitch *esw, int nvports)
 {
 	esw_offloads_devcom_cleanup(esw);
 	esw_offloads_unload_reps(esw, nvports);
-	esw_destroy_vport_rx_group(esw);
-	esw_destroy_offloads_table(esw);
-	esw_destroy_offloads_fdb_tables(esw);
+	esw_offloads_steering_cleanup(esw);
 }
 
 static int esw_mode_from_devlink(u16 mode, u16 *mlx5_mode)
