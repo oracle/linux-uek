a638c486577f597c762950de775e89e4a93b4e01
Bill Kuzeja <William.Kuzeja@stratus.com>
2019-02-12 09:29:50 -0500

    scsi: qla2xxx: Fix panic from use after free in qla2x00_async_tm_cmd
    In qla2x00_async_tm_cmd, we reference off sp after it has been freed.  This
    caused a panic on a system running a slub debug kernel. Since fcport is
    passed in anyways, just use that instead.
    
    Signed-off-by: Bill Kuzeja <william.kuzeja@stratus.com>
    Acked-by: Giridhar Malavali <gmalavali@marvell.com>
    Acked-by: Himanshu Madhani <hmadhani@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Orabug: 31607567
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>

--- drivers/scsi/qla2xxx/qla_init.c
+++ drivers/scsi/qla2xxx/qla_init.c
@@ -1824,13 +1824,13 @@ qla2x00_async_tm_cmd(fc_port_t *fcport, uint32_t flags, uint32_t lun,
 
 		/* Issue Marker IOCB */
 		qla2x00_marker(vha, vha->hw->base_qpair,
-		    sp->fcport->loop_id, lun,
+		    fcport->loop_id, lun,
 		    flags == TCF_LUN_RESET ? MK_SYNC_ID_LUN : MK_SYNC_ID);
 	}
 
 done_free_sp:
 	sp->free(sp);
-	sp->fcport->flags &= ~FCF_ASYNC_SENT;
+	fcport->flags &= ~FCF_ASYNC_SENT;
 done:
 	return rval;
 }
