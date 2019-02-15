0efaa48c08b0af0072fdbadab4df3914b97ef590
Geetha sowjanya <gakula@marvell.com>
2019-02-15 15:16:18 +0530

    octeontx2-pf: Use OTX2_DATA_ALIGN() to align net SKB data
    SKB_DATA_ALIGN/NET_SKB_PAD depends on L1_CACHE_SHIFT macro for
    cache line size. This macro may changes for different kernel versions.
    Hence defining OTX2_DATA_ALIGN() to align SKB data to match OCTEONTX2
    cache line size of 128.
    
    Orabug: 30484033
    
    Change-Id: Ie5436eb51de2a89a3d8b2b04077ff25a3e4bbc9d
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/4359
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.h
@@ -20,8 +20,12 @@
 #define CGX_CHAN_BASE	0x800
 
 #define DMA_BUFFER_LEN	1536 /* In multiples of 128bytes */
-#define RCV_FRAG_LEN	(SKB_DATA_ALIGN(DMA_BUFFER_LEN + NET_SKB_PAD) + \
-			 SKB_DATA_ALIGN(sizeof(struct skb_shared_info)))
+#define OTX2_DATA_ALIGN(X)	ALIGN(X, OTX2_ALIGN)
+#define RCV_FRAG_LEN		\
+	((OTX2_DATA_ALIGN(DMA_BUFFER_LEN + NET_SKB_PAD)) + \
+	(OTX2_DATA_ALIGN(sizeof(struct skb_shared_info))))
+
+#define OTX2_HEAD_ROOM		OTX2_ALIGN
 
 #define	OTX2_ETH_HLEN		(VLAN_ETH_HLEN + VLAN_HLEN)
 #define OTX2_MIN_MTU		ETH_MIN_MTU
