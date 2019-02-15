ffd7e8164bfa59a1461760585b345b09668ae2bf
William Roche <william.roche@oracle.com>
2019-02-15 10:32:30 -0500

    x86/speculation: reformatting RSB overwrite macro
    Introducing the __ASM_RSB_STUFF code directly into the macro to prepare
    the possibility to do both half RSB stuffing and RSB overwriting with it.
    
    Signed-off-by: William Roche <william.roche@oracle.com>
    Co-developed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    
    (cherry picked from commit fe603bb0b80036523fb34b6c035d1ee66a26262f)
    
    Orabug: 29660924
    
    Signed-off-by: William Roche <william.roche@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Acked-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
    	arch/x86/include/asm/spec_ctrl.h
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/include/asm/spec_ctrl.h
+++ arch/x86/include/asm/spec_ctrl.h
@@ -58,74 +58,6 @@
 	popq %rcx;				\
 	popq %rax
 
-#define __ASM_STUFF_RSB				\
-	call	1f;				\
-	pause;					\
-1:	call	2f;				\
-	pause;					\
-2:	call	3f;				\
-	pause;					\
-3:	call	4f;				\
-	pause;					\
-4:	call	5f;				\
-	pause;					\
-5:	call	6f;				\
-	pause;					\
-6:	call	7f;				\
-	pause;					\
-7:	call	8f;				\
-	pause;					\
-8:	call	9f;				\
-	pause;					\
-9:	call	10f;				\
-	pause;					\
-10:	call	11f;				\
-	pause;					\
-11:	call	12f;				\
-	pause;					\
-12:	call	13f;				\
-	pause;					\
-13:	call	14f;				\
-	pause;					\
-14:	call	15f;				\
-	pause;					\
-15:	call	16f;				\
-	pause;					\
-16:	call	17f;				\
-	pause;					\
-17:	call	18f;				\
-	pause;					\
-18:	call	19f;				\
-	pause;					\
-19:	call	20f;				\
-	pause;					\
-20:	call	21f;				\
-	pause;					\
-21:	call	22f;				\
-	pause;					\
-22:	call	23f;				\
-	pause;					\
-23:	call	24f;				\
-	pause;					\
-24:	call	25f;				\
-	pause;					\
-25:	call	26f;				\
-	pause;					\
-26:	call	27f;				\
-	pause;					\
-27:	call	28f;				\
-	pause;					\
-28:	call	29f;				\
-	pause;					\
-29:	call	30f;				\
-	pause;					\
-30:	call	31f;				\
-	pause;					\
-31:	call	32f;				\
-	pause;					\
-32:						\
-	add $(32*8), %rsp;
-
 .macro ENABLE_IBRS
 	testl	$SPEC_CTRL_BASIC_IBRS_INUSE, PER_CPU_VAR(cpu_ibrs)
 	jz	7f
@@ -195,7 +127,39 @@
 	STATIC_JUMP_IF_TRUE .Lstuff_rsb_\@, rsb_overwrite_key, def=0
 	jmp	.Ldone_call_\@
 .Lstuff_rsb_\@:
-	__ASM_STUFF_RSB
+	call	1f;	pause
+1:	call	2f;	pause
+2:	call	3f;	pause
+3:	call	4f;	pause
+4:	call	5f;	pause
+5:	call	6f;	pause
+6:	call	7f;	pause
+7:	call	8f;	pause
+8:	call	9f;	pause
+9:	call	10f;	pause
+10:	call	11f;	pause
+11:	call	12f;	pause
+12:	call	13f;	pause
+13:	call	14f;	pause
+14:	call	15f;	pause
+15:	call	16f;	pause
+16:	call	17f;	pause
+17:	call	18f;	pause
+18:	call	19f;	pause
+19:	call	20f;	pause
+20:	call	21f;	pause
+21:	call	22f;	pause
+22:	call	23f;	pause
+23:	call	24f;	pause
+24:	call	25f;	pause
+25:	call	26f;	pause
+26:	call	27f;	pause
+27:	call	28f;	pause
+28:	call	29f;	pause
+29:	call	30f;	pause
+30:	call	31f;	pause
+31:	call	32f;	pause
+32:	add $(32*8), %rsp
 .Ldone_call_\@:
 .endm
 
