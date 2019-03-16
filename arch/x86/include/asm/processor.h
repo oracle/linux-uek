410991f202d6e68d76da3992d9e101f886cb221f
Thomas Gleixner <tglx@linutronix.de>
2019-03-15 20:36:48 -0400

    x86/speculation/mds: Conditionally clear CPU buffers on idle entry
    commit 131ddbba14bc156defba98235818f3fb0cd9537e upstream
    
    Add a static key which controls the invocation of the CPU buffer clear
    mechanism on idle entry. This is independent of other MDS mitigations
    because the idle entry invocation to mitigate the potential leakage due to
    store buffer repartitioning is only necessary on SMT systems.
    
    Add the actual invocations to the different halt/mwait variants which
    covers all usage sites. mwaitx is not patched as it's not available on
    Intel CPUs.
    
    The buffer clear is only invoked before entering the C-State to prevent
    that stale data from the idling CPU is spilled to the Hyper-Thread sibling
    after the Store buffer got repartitioned and all entries are available to
    the non idle sibling.
    
    When coming out of idle the store buffer is partitioned again so each
    sibling has half of it available. Now CPU which returned from idle could be
    speculatively exposed to contents of the sibling, but the buffers are
    flushed either on exit to user space or on VMENTER.
    
    When later on conditional buffer clearing is implemented on top of this,
    then there is no action required either because before returning to user
    space the context switch will set the condition flag which causes a flush
    on the return to user path.
    
    Note, that the buffer clearing on idle is only sensible on CPUs which are
    solely affected by MSBDS and not any other variant of MDS because the other
    MDS variants cannot be mitigated when SMT is enabled, so the buffer
    clearing on idle would be a window dressing exercise.
    
    This intentionally does not handle the case in the acpi/processor_idle
    driver which uses the legacy IO port interface for C-State transitions for
    two reasons:
    
     - The acpi/processor_idle driver was replaced by the intel_idle driver
       almost a decade ago. Anything Nehalem upwards supports it and defaults
       to that new driver.
    
     - The legacy IO port interface is likely to be used on older and therefore
       unaffected CPUs or on systems which do not receive microcode updates
       anymore, so there is no point in adding that.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit f59d302ca44d17a574051ea58b2024f9c6d20533)
    
    We're not using static keys, so unconditionally clear buffers on idle.
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
      Documentation/x86/mds.rst
      arch/x86/include/asm/irqflags.h
      arch/x86/include/asm/mwait.h
      arch/x86/include/asm/nospec-branch.h
      arch/x86/kernel/cpu/bugs.c
    
    Documentation/x86/mds.rst doesn't exist, nor does other spectre/spec
    documentation, so just drop these changes.
    
    The context in arch/x86/include/asm/irqflags.h is slightly different.
    
    The changes to arch/x86/include/asm/mwait.h instead need to be applied to
    arch/x86/include/asm/processor.h. Add an include for nospec-branch.h.
    The __mwaitx() function doesn't exist, so drop those changes.
    
    arch/x86/include/asm/nospec-branch.h and arch/x86/kernel/cpu/bugs.c drop
    the usage of static keys.
    
    Orabug: 29792061
    CVE: CVE-2018-12126
    CVE: CVE-2018-12130
    CVE: CVE-2018-12127
    CVE: CVE-2019-11091
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>
    Reviewed-by: Jamie Iles <jamie.iles@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -21,6 +21,7 @@ struct mm_struct;
 #include <asm/desc_defs.h>
 #include <asm/nops.h>
 #include <asm/special_insns.h>
+#include <asm/nospec-branch.h>
 
 #include <linux/personality.h>
 #include <linux/cpumask.h>
@@ -686,6 +687,8 @@ static inline void __monitor(const void *eax, unsigned long ecx,
 
 static inline void __mwait(unsigned long eax, unsigned long ecx)
 {
+	mds_idle_clear_cpu_buffers();
+
 	/* "mwait %eax, %ecx;" */
 	asm volatile(".byte 0x0f, 0x01, 0xc9;"
 		     :: "a" (eax), "c" (ecx));
@@ -693,6 +696,8 @@ static inline void __mwait(unsigned long eax, unsigned long ecx)
 
 static inline void __sti_mwait(unsigned long eax, unsigned long ecx)
 {
+	mds_idle_clear_cpu_buffers();
+
 	trace_hardirqs_on();
 	/* "mwait %eax, %ecx;" */
 	asm volatile("sti; .byte 0x0f, 0x01, 0xc9;"
