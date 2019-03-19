b02cbf7e01ee922b60b0a2f55b9f827dd4298487
Sunil Goutham <sgoutham@marvell.com>
2019-03-19 16:48:04 +0530

    octeontx2-af: Consolidate NIX HW issue workarounds
    This is a cleanup aimed to get rid of silicon variant
    checks spilled over multiple places. All NIX block
    related workarounds are moved to a single location.
    
    Orabug: 30483967
    
    Change-Id: I7933aa2ce6b807374466e9aa5fad457224050209
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/6265
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -619,10 +619,8 @@ void rvu_nix_update_link_credits(struct rvu *rvu, int blkaddr,
 void rvu_nix_update_sq_smq_mapping(struct rvu *rvu, int blkaddr, int nixlf,
 				   u16 sq, u16 smq);
 void rvu_nix_txsch_config_changed(struct nix_hw *nix_hw);
-int rvu_nix_tx_stall_workaround_init(struct rvu *rvu,
-				     struct nix_hw *nix_hw, int blkaddr);
-void rvu_nix_tx_stall_workaround_exit(struct rvu *rvu, struct nix_hw *nix_hw);
 ssize_t rvu_nix_get_tx_stall_counters(struct rvu *rvu,
 				      char __user *buffer, loff_t *ppos);
-void rvu_nix_enable_internal_bp(struct rvu *rvu, int blkaddr);
+int rvu_nix_fixes_init(struct rvu *rvu, struct nix_hw *nix_hw, int blkaddr);
+void rvu_nix_fixes_exit(struct rvu *rvu, struct nix_hw *nix_hw);
 #endif /* RVU_H */
