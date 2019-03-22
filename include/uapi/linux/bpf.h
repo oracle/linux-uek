ad61b02df917af5a206302accc318bea667adef0
Willem de Bruijn <willemb@google.com>
2019-03-22 14:32:55 -0400

    bpf: add bpf_skb_adjust_room flag BPF_F_ADJ_ROOM_FIXED_GSO
    bpf_skb_adjust_room adjusts gso_size of gso packets to account for the
    pushed or popped header room.
    
    This is not allowed with UDP, where gso_size delineates datagrams. Add
    an option to avoid these updates and allow this call for datagrams.
    
    It can also be used with TCP, when MSS is known to allow headroom,
    e.g., through MSS clamping or route MTU.
    
    Changes v1->v2:
      - document flag BPF_F_ADJ_ROOM_FIXED_GSO
      - do not expose BPF_F_ADJ_ROOM_MASK through uapi, as it may change.
    
    Conflicts arose from bpf helper description format changes in bpf.h
    and are confined to comments.  net/core/filter.c had to be slightly
    modified in the grow and shrink functions as UEK5 does not support
    UDP GSO - this simplifies the ENOTSUPP cases.
    
    Link: https://patchwork.ozlabs.org/patch/1052497/
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    (cherry picked from commit 2278f6cc151a8bef6ba0b3fe3009d14dc3c51c4a)
    
    Orabug: 29754923
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    
    Conflicts:
    	include/uapi/linux/bpf.h
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -567,7 +567,7 @@ union bpf_attr {
  *     @skb: pointer to skb
  *     @len_diff: (signed) amount of room to grow/shrink
  *     @mode: operation mode (enum bpf_adj_room_mode)
- *     @flags: reserved for future use
+ *     @flags: 0 or BPF_F_ADJ_ROOM_* combination
  *     Return: 0 on success or negative error code
  *
  * int bpf_sk_redirect_map(map, key, flags)
@@ -687,6 +687,9 @@ enum bpf_func_id {
 #define BPF_F_CURRENT_CPU		BPF_F_INDEX_MASK
 /* BPF_FUNC_perf_event_output for sk_buff input context. */
 #define BPF_F_CTXLEN_MASK		(0xfffffULL << 32)
+
+/* BPF_FUNC_skb_adjust_room flags. */
+#define BPF_F_ADJ_ROOM_FIXED_GSO	(1ULL << 0)
 
 /* Mode for BPF_FUNC_skb_adjust_room helper. */
 enum bpf_adj_room_mode {
