3164bc2734c1fe1310e32a6c5dfa34fd1f5e40f3
Jerin Jacob <jerinj@marvell.com>
2019-03-26 12:04:12 +0530

    octeontx2-af: add nix error af interrupt handlers
    Added debug messages for NIX_AF_RVU_INT, NIX_AF_ERR_INT and NIX_AF_RAS
    error AF interrupts.
    
    Change-Id: Ifccda17e7f77ae726bfdd2f4157d4a86bfec689d
    Signed-off-by: Jerin Jacob <jerinj@marvell.com>
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 49da0b7b62fb2281e4b9ac5ab79f3d70aeb55fc7)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -2178,6 +2178,7 @@ static void rvu_unregister_interrupts(struct rvu *rvu)
 	int irq;
 
 	rvu_npa_unregister_interrupts(rvu);
+	rvu_nix_unregister_interrupts(rvu);
 	rvu_cpt_unregister_interrupts(rvu);
 
 	/* Disable the Mbox interrupt */
@@ -2388,6 +2389,10 @@ static int rvu_register_interrupts(struct rvu *rvu)
 	rvu->irq_allocated[offset] = true;
 
 	ret = rvu_npa_register_interrupts(rvu);
+	if (ret)
+		goto fail;
+
+	ret = rvu_nix_register_interrupts(rvu);
 	if (ret)
 		goto fail;
 
@@ -2675,7 +2680,6 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		goto err_flr;
 
 	rvu_setup_rvum_blk_revid(rvu);
-
 	err = rvu_policy_init(rvu);
 	if (err)
 		goto err_irq;
