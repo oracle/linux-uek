eb8e6ff1a2c1a190c6430dfd9ad52a96fb0fb04a
Thomas Gleixner <tglx@linutronix.de>
2019-03-28 13:57:24 -0400

    x86/speculation/mds: Add mitigation mode VMWERV
    commit 22dd8365088b6403630b82423cf906491859b65e upstream
    
    In virtualized environments it can happen that the host has the microcode
    update which utilizes the VERW instruction to clear CPU buffers, but the
    hypervisor is not yet updated to expose the X86_FEATURE_MD_CLEAR CPUID bit
    to guests.
    
    Introduce an internal mitigation mode VWWERV which enables the invocation
    of the CPU buffer clearing even if X86_FEATURE_MD_CLEAR is not set. If the
    system has no updated microcode this results in a pointless execution of
    the VERW instruction wasting a few CPU cycles. If the microcode is updated,
    but not exposed to a guest then the CPU buffers will be cleared.
    
    That said: Virtual Machines Will Eventually Receive Vaccine
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Jon Masters <jcm@redhat.com>
    Tested-by: Jon Masters <jcm@redhat.com>
    
    (cherry picked from commit a2227b3f734fad5ace7c99103d6a7bc020c193fd)
    
    Conflicts:
      arch/x86/kernel/cpu/bugs.c
    
    The changes to  arch/x86/kernel/cpu/bugs.c instead need to be made to
    arch/x86/kernel/cpu/bugs_64.c. The context is also different, as static
    keys are not used.
    
    Orabug: 29641786
    CVE: CVE-2018-12126
    CVE: CVE-2018-12130
    CVE: CVE-2018-12127
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -1045,6 +1045,7 @@ extern bool cpu_has_amd_erratum(const int *);
 enum mds_mitigations {
 	MDS_MITIGATION_OFF,
 	MDS_MITIGATION_FULL,
+	MDS_MITIGATION_VMWERV,
 };
 
 #endif /* _ASM_X86_PROCESSOR_H */
