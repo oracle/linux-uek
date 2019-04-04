7c89cbc0785fbf0b24891fb50787a1a0b4f42e5d
Colin Ian King <colin.king@canonical.com>
2019-04-04 15:46:03 +0100

    tcp: remove redundant check on tskb
    [ Upstream commit d1edc085559744fbda7a55e97eeae8bd6135a11b ]
    
    The non-null check on tskb is always false because it is in an else
    path of a check on tskb and hence tskb is null in this code block.
    This is check is therefore redundant and can be removed as well
    as the label coalesc.
    
    if (tsbk) {
            ...
    } else {
            ...
            if (unlikely(!skb)) {
                    if (tskb)       /* can never be true, redundant code */
                            goto coalesc;
                    return;
            }
    }
    
    Addresses-Coverity: ("Logically dead code")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Reviewed-by: Mukesh Ojha <mojha@codeaurora.org>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: 94062790aedb ("tcp: defer shutdown(SEND_SHUTDOWN) for TCP_SYN_RECV sockets")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 6f7082e284ec3fa32694e5704da510194229ef12)
    [Vegard: fix conflicts in context due to missing commits
     e2080072ed2d98a55ae69d95dea60ff7a17cddd5 ("tcp: new list for sent but
     unacked skbs for RACK recovery") and
     75c119afe14f74b4dd967d75ed9f57ab6c0ef045 ("tcp: implement rb-tree based
     retransmit queue").]
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    (cherry picked from commit 22dc505fb63ec90f4f89fd3f6b58cad1c68e059c)
    Signed-off-by: Yifei Liu <yifei.l.liu@oracle.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -3155,7 +3155,6 @@ void tcp_send_fin(struct sock *sk)
 	 * as TCP stack thinks it has already been transmitted.
 	 */
 	if (tskb && (tcp_send_head(sk) || tcp_under_memory_pressure(sk))) {
-coalesce:
 		TCP_SKB_CB(tskb)->tcp_flags |= TCPHDR_FIN;
 		TCP_SKB_CB(tskb)->end_seq++;
 		tp->write_seq++;
@@ -3171,11 +3170,9 @@ coalesce:
 		}
 	} else {
 		skb = alloc_skb_fclone(MAX_TCP_HEADER, sk->sk_allocation);
-		if (unlikely(!skb)) {
-			if (tskb)
-				goto coalesce;
+		if (unlikely(!skb))
 			return;
-		}
+
 		skb_reserve(skb, MAX_TCP_HEADER);
 		sk_forced_mem_schedule(sk, skb->truesize);
 		/* FIN eats a sequence byte, write_seq advanced by tcp_queue_skb(). */
