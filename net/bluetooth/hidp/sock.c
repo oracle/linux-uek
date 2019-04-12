5e1d49cf249fd01c4b3aa46320f5948b502a0730
Young Xiao <YangX92@hotmail.com>
2019-04-12 15:24:30 +0800

    Bluetooth: hidp: fix buffer overflow
    Struct ca is copied from userspace. It is not checked whether the "name"
    field is NULL terminated, which allows local users to obtain potentially
    sensitive information from kernel stack memory, via a HIDPCONNADD command.
    
    This vulnerability is similar to CVE-2011-1079.
    
    Signed-off-by: Young Xiao <YangX92@hotmail.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Cc: stable@vger.kernel.org
    (cherry picked from commit a1616a5ac99ede5d605047a9012481ce7ff18b16)
    
    Orabug: 29786788
    CVE: CVE-2019-11884
    
    Reviewed-by: John Haxby <john.haxby@oracle.com>
    Signed-off-by: Allen Pais <allen.pais@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- net/bluetooth/hidp/sock.c
+++ net/bluetooth/hidp/sock.c
@@ -84,6 +84,7 @@ static int hidp_sock_ioctl(struct socket *sock, unsigned int cmd, unsigned long 
 			sockfd_put(csock);
 			return err;
 		}
+		ca.name[sizeof(ca.name)-1] = 0;
 
 		if (csock->sk->sk_state != BT_CONNECTED ||
 				isock->sk->sk_state != BT_CONNECTED) {
