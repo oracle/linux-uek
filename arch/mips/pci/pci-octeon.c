75395356f439917249b3a8f964b0c3deab605c2b
Venkat Subbiah <venkat.subbiah@cavium.com>
2019-05-02 17:51:27 +0200

    MIPS: OCTEON: Reorganize PCIe controller code.
    Orabug: 30483967
    
    Signed-off-by: Venkat Subbiah <venkat.subbiah@cavium.com>
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Leonid Rosenboim <lrosenboim@caviumnetworks.com>
    Signed-off-by: Carlos Munoz <carlos.munoz@caviumnetworks.com>
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- arch/mips/pci/pci-octeon.c
+++ arch/mips/pci/pci-octeon.c
@@ -583,13 +583,6 @@ static int __init octeon_pci_setup(void)
 	/* Point pcibios_map_irq() to the PCI version of it */
 	octeon_pcibios_map_irq = octeon_pci_pcibios_map_irq;
 
-	/* Only use the big bars on chips that support it */
-	if (OCTEON_IS_MODEL(OCTEON_CN31XX) ||
-	    OCTEON_IS_MODEL(OCTEON_CN38XX_PASS2))
-		octeon_dma_bar_type = OCTEON_DMA_BAR_TYPE_SMALL;
-	else
-		octeon_dma_bar_type = OCTEON_DMA_BAR_TYPE_BIG;
-
 	if (!octeon_is_pci_host()) {
 		pr_notice("Not in host mode, PCI Controller not initialized\n");
 		return 0;
