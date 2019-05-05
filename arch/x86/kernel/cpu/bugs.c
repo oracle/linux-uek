753131017d0e5f4824b987cb4011060da2ac9b0d
Kanth Ghatraju <kanth.ghatraju@oracle.com>
2019-05-05 10:00:53 -0400

    x86/speculation/mds: Make mds_mitigation mutable after init
    We provide a knob 'mds_user_clear' which the user can use
    to turn on/off MDS. The function (mds_user_clear_disable)
    does that but also updates mds_mitigation global variable.
    
    Except that said global variable is made read-only after boot
    so we get a #GP.
    
    (cherry picked from commit 5964a04f0227736649a3a2590711cfaa50217d75)
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    (cherry picked from commit 9059934d7e10dd8b2b2aa37ca6b5473b91c07931)
    
    Orabug: 30091537
    CVE: CVE-2019-11091
    CVE: CVE-2018-12126
    CVE: CVE-2018-12130
    CVE: CVE-2018-12127
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -440,7 +440,7 @@ static void x86_amd_ssb_disable(void)
 static void update_mds_branch_idle(void);
 
 /* Default mitigation for MDS-affected CPUs */
-static enum mds_mitigations mds_mitigation __ro_after_init = MDS_MITIGATION_FULL;
+static enum mds_mitigations mds_mitigation __read_mostly = MDS_MITIGATION_FULL;
 static bool mds_nosmt __ro_after_init = false;
 
 bool mds_user_clear_enabled(void)
