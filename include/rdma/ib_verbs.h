8732aed747355f9cd3f145f3dfcc1442bc898fbe
Shiraz Saleem <shiraz.saleem@intel.com>
2019-05-06 08:53:32 -0500

    RDMA/umem: Add API to find best driver supported page size in an MR
    This helper iterates through the SG list to find the best page size to use
    from a bitmap of HW supported page sizes. Drivers that support multiple
    page sizes, but not mixed sizes in an MR can use this API.
    
    Suggested-by: Jason Gunthorpe <jgg@ziepe.ca>
    Signed-off-by: Shiraz Saleem <shiraz.saleem@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Orabug: 30511634
    
    (cherry picked from commit 4a35339958f16d42a4ca06a8da9d4b5ab39ee8ea)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	include/rdma/ib_verbs.h
    
    Conflicts occurred due to a few context issue.
    
    Signed-off-by: Mohamad Heib <mohamadh@mellanox.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Tested-by: Gerald Gibson <gerald.gibson@oracle.com>
    Tested-by: Hong Liu <hong.x.liu@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -3164,6 +3164,30 @@ static inline bool rdma_cap_read_inv(struct ib_device *dev, u32 port_num)
 	return rdma_protocol_iwarp(dev, port_num);
 }
 
+/**
+ * rdma_find_pg_bit - Find page bit given address and HW supported page sizes
+ *
+ * @addr: address
+ * @pgsz_bitmap: bitmap of HW supported page sizes
+ */
+static inline unsigned int rdma_find_pg_bit(unsigned long addr,
+					    unsigned long pgsz_bitmap)
+{
+	unsigned long align;
+	unsigned long pgsz;
+
+	align = addr & -addr;
+
+	/* Find page bit such that addr is aligned to the highest supported
+	 * HW page size
+	 */
+	pgsz = pgsz_bitmap & ~(-align << 1);
+	if (!pgsz)
+		return __ffs(pgsz_bitmap);
+
+	return __fls(pgsz);
+}
+
 int ib_query_gid(struct ib_device *device,
 		 u8 port_num, int index, union ib_gid *gid,
 		 struct ib_gid_attr *attr);
