1b435361bad73d76a10d5464f58defb4bc2d190a
Carlos Munoz <cmunoz@caviumnetworks.com>
2019-05-07 17:43:49 +0200

    netdev: octeon: Move and update octeon network driver from staging.
    Orabug: 30483967
    
    Signed-off-by: Carlos Munoz <cmunoz@caviumnetworks.com>
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -618,6 +618,7 @@ void skb_release_head_state(struct sk_buff *skb)
 	nf_bridge_put(skb->nf_bridge);
 #endif
 }
+EXPORT_SYMBOL(skb_release_head_state);
 
 /* Free everything but the sk_buff shell. */
 static void skb_release_all(struct sk_buff *skb)
