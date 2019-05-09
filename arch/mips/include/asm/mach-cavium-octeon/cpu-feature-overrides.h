77e5bfbb9c2e02ebba8b0d2af36f38749e7293a2
David Daney <david.daney@cavium.com>
2019-05-09 13:46:22 +0200

    MIPS: OCTEON: Define cpu_has_local_ebase to 0.
    Also remove dead cpu_has_saa.
    
    Orabug: 30484033
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- arch/mips/include/asm/mach-cavium-octeon/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-cavium-octeon/cpu-feature-overrides.h
@@ -45,7 +45,6 @@
 #define cpu_has_ic_fills_f_dc	0
 #define cpu_has_64bits		1
 #define cpu_has_octeon_cache	1
-#define cpu_has_saa		octeon_has_saa()
 #ifdef CONFIG_CAVIUM_OCTEON2
 #define cpu_has_octeon2_isa     1
 #endif
@@ -76,17 +75,12 @@
 #define ARCH_HAS_USABLE_BUILTIN_POPCOUNT 1
 #endif
 
-static inline int octeon_has_saa(void)
-{
-	int id;
-	asm volatile ("mfc0 %0, $15,0" : "=r" (id));
-	return id >= 0x000d0300;
-}
-
 /*
  * The last 256MB are reserved for device to device mappings and the
  * BAR1 hole.
  */
 #define MAX_DMA32_PFN (((1ULL << 32) - (1ULL << 28)) >> PAGE_SHIFT)
+
+#define cpu_has_local_ebase	0
 
 #endif
