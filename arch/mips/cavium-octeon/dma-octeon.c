76d45d72cb5bfae33229d0b4ce1fd7184f30ae43
David Daney <david.daney@cavium.com>
2019-05-14 19:26:11 +0200

    MIPS: OCTEON: Fix plat_swiotlb_setup() for OCTEON3
    Orabug: 30483967
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- arch/mips/cavium-octeon/dma-octeon.c
+++ arch/mips/cavium-octeon/dma-octeon.c
@@ -264,7 +264,8 @@ void __init plat_swiotlb_setup(void)
 
 		/* These addresses map low for PCI. */
 		if (e->addr > 0x410000000ull &&
-		    current_cpu_type() != CPU_CAVIUM_OCTEON2)
+		    current_cpu_type() != CPU_CAVIUM_OCTEON2 &&
+		    current_cpu_type() != CPU_CAVIUM_OCTEON3)
 			continue;
 
 		addr_size += e->size;
