5d589370fa9fee223a5354dd2082feac06462136
Peter Swain <pswain@marvell.com>
2019-05-16 15:46:46 -0700

    mmc: cavium: avoid single-slot startup issues
    Previous code relied on particular state left by u-boot,
    or a previous _probe attempt aborted with EPROBE_DEFER,
    else it would program a clock-divisor of zero and hang MMC.
    
    Do not cache emm_switch setting if it has clock divisor zero.
    Do not use frequency params before they have been extracted
    from devtree.
    Do enforce lower frequency bound (400KHz) from mmc/core.
    
    Change-Id: I2d19084d1ab57d0f98e2ce1ffcd6fb918228f53c
    Signed-off-by: Peter Swain <pswain@marvell.com>
    Signed-off-by: Sujeet Baranwal <sbaranwal@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/8549
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/26911
    
    Orabug: 39052955
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>

--- drivers/mmc/host/cavium.c
+++ drivers/mmc/host/cavium.c
@@ -323,7 +323,7 @@ static void do_switch(struct cvm_mmc_host *host, u64 emm_switch)
 
 	check_switch_errors(host);
 
-	if (slot) {
+	if (slot && (emm_switch & MIO_EMM_SWITCH_CLK)) {
 		slot->cmd6_pending = false;
 		slot->cached_switch = emm_switch;
 	}
@@ -1315,6 +1315,8 @@ static void cvm_mmc_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 	clock = ios->clock;
 	max_f = max_supported_frequency(host);
 
+	if (clock < mmc->f_min)
+		clock = mmc->f_min;
 	if (clock > max_f)
 		clock = max_f;
 
@@ -1507,6 +1509,7 @@ static int cvm_mmc_init_lowlevel(struct cvm_mmc_slot *slot)
 	/* Make the changes take effect on this bus slot. */
 	set_bus_id(&emm_switch, slot->bus_id);
 	do_switch(host, emm_switch);
+	slot->cached_switch = emm_switch;
 	host->powered = true;
 
 	/*
@@ -1665,8 +1668,8 @@ int cvm_mmc_of_slot_probe(struct device *dev, struct cvm_mmc_host *host)
 
 	host->acquire_bus(host);
 	host->slot[id] = slot;
-	cvm_mmc_switch_to(slot);
 	cvm_mmc_init_lowlevel(slot);
+	cvm_mmc_switch_to(slot);
 	host->release_bus(host);
 
 	ret = mmc_add_host(mmc);
