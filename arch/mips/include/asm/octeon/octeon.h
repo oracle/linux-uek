c445a6eba1972d00e5848fc30997532b7517603d
David Daney <david.daney@cavium.com>
2019-05-16 12:12:45 +0200

    MIPS: Octeon: Add simple Octeon IPI infrastructure
    This will allow simple messages to be sent without taking any locks.
    
    Orabug: 30484033
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Carlos Munoz <carlos.munoz@caviumnetworks.com>
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- arch/mips/include/asm/octeon/octeon.h
+++ arch/mips/include/asm/octeon/octeon.h
@@ -411,4 +411,9 @@ int octeon_cpu_for_coreid(int coreid);
 void octeon_pci_console_init(const char *);
 void octeon_write_lcd(const char *s);
 
+typedef void (*octeon_message_fn_t)(void);
+int octeon_request_ipi_handler(octeon_message_fn_t fn);
+void octeon_send_ipi_single(int cpu, unsigned int action);
+void octeon_release_ipi_handler(int action);
+
 #endif /* __ASM_OCTEON_OCTEON_H */
