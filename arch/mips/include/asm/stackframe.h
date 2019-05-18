59b8dfbad936021943e7a752266d43d03750cf1e
David Daney <david.daney@cavium.com>
2019-05-18 12:01:04 +0200

    MIPS: Octeon: Fast access to the thread pointer
    If CONFIG_FAST_ACCESS_TO_THREAD_POINTER is enabled, then deploy the
    FAST_ACCESS_THREAD_OFFSET and the FAST_ACCESS_THREAD_REGISTER to
    bounce through the reserved hardware cache area for speed.
    
    Orabug: 30484033
    
    Signed-off-by: David Daney <david.daney@cavium.com>
    Signed-off-by: Carlos Munoz <cmunoz@caviumnetworks.com>
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- arch/mips/include/asm/stackframe.h
+++ arch/mips/include/asm/stackframe.h
@@ -428,6 +428,9 @@
 		eretnc
 #else
 		.set	arch=r4000
+#ifdef CONFIG_FAST_ACCESS_TO_THREAD_POINTER
+		LONG_L	k0, FAST_ACCESS_THREAD_OFFSET($0) /* K0 = thread pointer */
+#endif
 		eret
 		.set	mips0
 #endif
