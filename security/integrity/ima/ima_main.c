8b00ba33e7b329bc5564508ab399fd2bfc862499
Jeff Layton <jlayton@redhat.com>
2019-05-24 13:10:40 -0700

    IMA: switch IMA over to new i_version API
    Upstream commit 3b370b215f4b45c0be87d7a15b0d0e2e8b238aee
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Orabug: 29822843
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: John Donnelly <John.p.donnelly@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- security/integrity/ima/ima_main.c
+++ security/integrity/ima/ima_main.c
@@ -27,6 +27,7 @@
 #include <linux/slab.h>
 #include <linux/xattr.h>
 #include <linux/ima.h>
+#include <linux/iversion.h>
 
 #include "ima.h"
 
@@ -136,7 +137,8 @@ static void ima_check_last_writer(struct integrity_iint_cache *iint,
 	if (atomic_read(&inode->i_writecount) == 1) {
 		update = test_and_clear_bit(IMA_UPDATE_XATTR,
 					    &iint->atomic_flags);
-		if ((iint->version != inode->i_version) ||
+		if (!IS_I_VERSION(inode) ||
+		    inode_cmp_iversion(inode, iint->version) ||
 		    (iint->flags & IMA_NEW_FILE)) {
 			iint->flags &= ~(IMA_DONE_MASK | IMA_NEW_FILE);
 			iint->measured_pcrs = 0;
