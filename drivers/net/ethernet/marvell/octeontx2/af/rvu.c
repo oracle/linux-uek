0971b192ca4cf788f680e503f75ef8281631dff1
Sunil Goutham <sgoutham@marvell.com>
2019-06-07 11:31:04 +0530

    octeontx2-af: Update workarounds for hardware issues
    Few hardware issues from 96xx A0 are not fixed in
    95xx A0 silicon. This patch applies workarounds of
    non fixed 96xx A0's hw issues to 95xx A0.
    
    Orabug: 30484033
    
    Change-Id: I8b0a3483a158c9b3c02c8ba5aee8cfa3a37c9e66
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/10630
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -67,7 +67,7 @@ static void rvu_setup_hw_capabilities(struct rvu *rvu)
 	hw->cap.nix_shaping = true;
 	hw->cap.nix_tx_link_bp = true;
 
-	if ((is_rvu_9xxx_A0(rvu))) {
+	if (is_rvu_96xx_A0(rvu) || is_rvu_95xx_A0(rvu)) {
 		hw->cap.nix_fixed_txschq_mapping = true;
 		hw->cap.nix_txsch_per_cgx_lmac = 4;
 		hw->cap.nix_txsch_per_lbk_lmac = 132;
@@ -1050,7 +1050,7 @@ static int rvu_lookup_rsrc(struct rvu *rvu, struct rvu_block *block,
 {
 	u64 val;
 
-	if (block->type == BLKTYPE_TIM && is_rvu_9xxx_A0(rvu))
+	if (block->type == BLKTYPE_TIM && is_rvu_96xx_A0(rvu))
 		return rvu_lf_lookup_tim_errata(rvu, block, pcifunc, slot);
 
 	val = ((u64)pcifunc << 24) | (slot << 16) | (1ULL << 13);
