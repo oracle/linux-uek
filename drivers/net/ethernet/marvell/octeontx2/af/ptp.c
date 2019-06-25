82d783f6577e8fec4ddd9de63aac665f76db77ae
Harman Kalra <hkalra@marvell.com>
2019-06-25 13:26:37 +0530

    octeontx2-af: Sending tsc value to the userspace
    This patch updates the existing PTP_OP_GET_CLOCK mbox message to
    return timestamp counter value, tsc (cntvct_el0 or pmccntr_el0)
    along with the PTP HI clock value.
    
    In some debugging scenarios user might need to read the PTP HI
    clock value in the fastpath, so as to know how many ticks have
    been spent till point from the reception of the packet, as packet
    reception tick value is already appended to the packet by CGX.
    If directly PTP_OP_GET_CLOCK mbox message is sent ever time user
    wants to record PTP HI clock value, it will bring down the
    performance to a great extent as mbox is a very expensive process.
    
    To handle this PTP HI clock can be derived from timestamp counter
    (tsc, which could be running at 100MHz or system freq) using two
    parameters freq multiplier (ratio of frequencies of PTP HI clock
    and tsc) and clock delta (by how much tsc is lagging from PTP HI
    clock). During configuration stage these parameters are calculated
         freq_mult = (freq of PTP HI clock)/(freq of tsc counter)
         clk_delta = (PTP_HI clock value / freq_mult) - (tsc val)
       these PTP_HI val and tsc value are receieved by calling
       PTP_OP_GET_CLOCK. Purpose of returing tsc at the same time
       with PTP_HI value is to avoid mbox propogation delay.
    
    Now whenever user wants to know PTP HI clock, it can be derived
    from tsc counter and returned:
         PTP_HI val = (tsc value + clk_delta) * freq_mult
    
    Orabug: 30483967
    
    Change-Id: Ib93637f608d6aadb75e5e67e93c8285d7dfa96fb
    Signed-off-by: Harman Kalra <hkalra@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/12797
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/ptp.c
+++ drivers/net/ethernet/marvell/octeontx2/af/ptp.c
@@ -127,9 +127,23 @@ int ptp_adjfine(struct ptp *ptp, long scaled_ppm)
 	return 0;
 }
 
-int ptp_get_clock(struct ptp *ptp, u64 *clk)
+static inline u64 get_tsc(bool is_pmu)
 {
-	*clk = readq(ptp->reg_base + PTP_CLOCK_HI);
+	return is_pmu ? read_sysreg(pmccntr_el0) : read_sysreg(cntvct_el0);
+}
+
+int ptp_get_clock(struct ptp *ptp, bool is_pmu, u64 *clk, u64 *tsc)
+{
+	u64 end, start;
+	u8 retries = 0;
+
+	do {
+		start = get_tsc(0);
+		*tsc = get_tsc(is_pmu);
+		*clk = readq(ptp->reg_base + PTP_CLOCK_HI);
+		end = get_tsc(0);
+		retries++;
+	} while (((end - start) > 50) && retries < 5);
 
 	return 0;
 }
