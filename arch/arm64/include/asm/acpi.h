56223406049be0f8cfe738a4d0e9a2b98ce40717
Jeremy Linton <jeremy.linton@arm.com>
2019-06-26 16:37:17 -0500

    arm_pmu: acpi: spe: Add initial MADT/SPE probing
    ACPI 6.3 adds additional fields to the MADT GICC
    structure to describe SPE PPI's. We pick these out
    of the cached reference to the madt_gicc structure
    similarly to the core PMU code. We then create a platform
    device referring to the IRQ and let the user/module loader
    decide whether to load the SPE driver.
    
    Tested-by: Hanjun Guo <hanjun.guo@linaro.org>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Reviewed-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit d24a0c7099b32b6981d7f126c45348e381718350)
    
    Orabug: 27825982
    
    Signed-off-by: Russell King <russell.king@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/arm64/include/asm/acpi.h
+++ arch/arm64/include/asm/acpi.h
@@ -27,6 +27,9 @@
 	(!(entry) || (entry)->header.length != ACPI_MADT_GICC_LENGTH ||	\
 	(unsigned long)(entry) + ACPI_MADT_GICC_LENGTH > (end))
 
+#define ACPI_MADT_GICC_SPE  (ACPI_OFFSET(struct acpi_madt_generic_interrupt, \
+	spe_interrupt) + sizeof(u16))
+
 /* Basic configuration for ACPI */
 #ifdef	CONFIG_ACPI
 /* ACPI table mapping after acpi_permanent_mmap is set */
