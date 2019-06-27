679d3eb86f2dbeedea846e7a16df1e959486b58f
Marcelo Tosatti <mtosatti@redhat.com>
2019-06-27 18:05:47 -0400

    drivers/cpuidle: add cpuidle-haltpoll driver
    The cpuidle_haltpoll driver allows the guest vcpus to poll for a specified
    amount of time before halting. This provides the following benefits
    to host side polling:
    
           1) The POLL flag is set while polling is performed, which allows
              a remote vCPU to avoid sending an IPI (and the associated
              cost of handling the IPI) when performing a wakeup.
    
           2) The HLT VM-exit cost can be avoided.
    
    The downside of guest side polling is that polling is performed
    even with other runnable tasks in the host.
    
    Results comparing halt_poll_ns and server/client application
    where a small packet is ping-ponged:
    
    host                                        --> 31.33
    halt_poll_ns=300000 / no guest busy spin    --> 33.40   (93.8%)
    halt_poll_ns=0 / guest_halt_poll_ns=300000  --> 32.73   (95.7%)
    
    For the SAP HANA benchmarks (where idle_spin is a parameter
    of the previous version of the patch, results should be the
    same):
    
    hpns == halt_poll_ns
    
                             idle_spin=0/   idle_spin=800/   idle_spin=0/
                             hpns=200000    hpns=0 	  hpns=800000
    DeleteC06T03 (100 thread) 1.76           1.71 (-3%)       1.78 (+1%)
    InsertC16T02 (100 thread) 2.14           2.07 (-3%)       2.18 (+1.8%)
    DeleteC00T01 (1 thread)   1.34           1.28 (-4.5%)     1.29 (-3.7%)
    UpdateC00T03 (1 thread)   4.72           4.18 (-12%)      4.53 (-5%)
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    
    Orabug: 29967444
    
    Signed-off-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Reviewed-by: Ankur Arora <ankur.a.arora@oracle.com>
    
    ---
    NOTE: This patch is not yet accepted upstream. However, this is needed by
    Exadata team for their next release to satisfy RDS peformance requirements.
    https://patchwork.kernel.org/patch/10988259/
    ---

---
+++ drivers/cpuidle/cpuidle-haltpoll.c
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * cpuidle driver for halt polling.
+ *
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ * Authors: Marcelo Tosatti <mtosatti@redhat.com>
+ */
+
+#include <linux/init.h>
+#include <linux/cpuidle.h>
+#include <linux/module.h>
+#include <linux/sched/clock.h>
+#include <linux/sched/idle.h>
+
+static unsigned int guest_halt_poll_ns __read_mostly = 200000;
+module_param(guest_halt_poll_ns, uint, 0644);
+
+/* division factor to shrink halt_poll_ns */
+static unsigned int guest_halt_poll_shrink __read_mostly = 2;
+module_param(guest_halt_poll_shrink, uint, 0644);
+
+/* multiplication factor to grow per-cpu halt_poll_ns */
+static unsigned int guest_halt_poll_grow __read_mostly = 2;
+module_param(guest_halt_poll_grow, uint, 0644);
+
+/* value in ns to start growing per-cpu halt_poll_ns */
+static unsigned int guest_halt_poll_grow_start __read_mostly = 10000;
+module_param(guest_halt_poll_grow_start, uint, 0644);
+
+/* value in ns to start growing per-cpu halt_poll_ns */
+static bool guest_halt_poll_allow_shrink __read_mostly = true;
+module_param(guest_halt_poll_allow_shrink, bool, 0644);
+
+static DEFINE_PER_CPU(unsigned int, halt_poll_ns);
+
+static void adjust_haltpoll_ns(unsigned int block_ns,
+			       unsigned int *cpu_halt_poll_ns)
+{
+	unsigned int val;
+
+	/* Grow cpu_halt_poll_ns if
+	 * cpu_halt_poll_ns < block_ns < guest_halt_poll_ns
+	 */
+	if (block_ns > *cpu_halt_poll_ns && block_ns <= guest_halt_poll_ns) {
+		val = *cpu_halt_poll_ns * guest_halt_poll_grow;
+
+		if (val < guest_halt_poll_grow_start)
+			val = guest_halt_poll_grow_start;
+		if (val > guest_halt_poll_ns)
+			val = guest_halt_poll_ns;
+
+		*cpu_halt_poll_ns = val;
+	} else if (block_ns > guest_halt_poll_ns &&
+		   guest_halt_poll_allow_shrink) {
+		unsigned int shrink = guest_halt_poll_shrink;
+
+		val = *cpu_halt_poll_ns;
+		if (shrink == 0)
+			val = 0;
+		else
+			val /= shrink;
+		*cpu_halt_poll_ns = val;
+	}
+}
+
+static int haltpoll_enter_idle(struct cpuidle_device *dev,
+			       struct cpuidle_driver *drv, int index)
+{
+	unsigned int *cpu_halt_poll_ns;
+	unsigned long long start, now, block_ns;
+	int cpu = smp_processor_id();
+
+	cpu_halt_poll_ns = per_cpu_ptr(&halt_poll_ns, cpu);
+
+	if (current_set_polling_and_test()) {
+		local_irq_enable();
+		goto out;
+	}
+
+	start = sched_clock();
+	local_irq_enable();
+	for (;;) {
+		if (need_resched()) {
+			current_clr_polling();
+			goto out;
+		}
+
+		now = sched_clock();
+		if (now - start > *cpu_halt_poll_ns)
+			break;
+
+		cpu_relax();
+	}
+
+	local_irq_disable();
+	if (current_clr_polling_and_test()) {
+		local_irq_enable();
+		goto out;
+	}
+
+	default_idle();
+	block_ns = sched_clock() - start;
+	adjust_haltpoll_ns(block_ns, cpu_halt_poll_ns);
+
+out:
+	return index;
+}
+
+static struct cpuidle_driver haltpoll_driver = {
+	.name = "haltpoll_idle",
+	.owner = THIS_MODULE,
+	.states = {
+		{ /* entry 0 is for polling */ },
+		{
+			.enter			= haltpoll_enter_idle,
+			.exit_latency		= 0,
+			.target_residency	= 0,
+			.power_usage		= -1,
+			.name			= "Halt poll",
+			.desc			= "Halt poll idle",
+		},
+	},
+	.safe_state_index = 0,
+	.state_count = 2,
+};
+
+static int __init haltpoll_init(void)
+{
+	struct cpuidle_driver *drv = &haltpoll_driver;
+
+	cpuidle_poll_state_init(drv);
+
+	return cpuidle_register(drv, NULL);
+}
+
+static void __exit haltpoll_exit(void)
+{
+	cpuidle_unregister(&haltpoll_driver);
+}
+
+module_init(haltpoll_init);
+module_exit(haltpoll_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Marcelo Tosatti <mtosatti@redhat.com>");
+
