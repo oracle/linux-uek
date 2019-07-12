890f1b55ad1a4e8ccf2101ad480b45d7d25dc716
Ka-Cheong Poon <ka-cheong.poon@oracle.com>
2019-07-12 07:33:56 -0700

    rds: Not all huge pages are released after app shutdown
    In rds_ib_free_mr(), an ibmr is either added to the drop_list or
    free_list.  The conditions to be added to the drop_list are:
    
    FMR: ibmr->remap_count >= pool->fmr_attr.max_maps
    
    FRWR: ibmr->fr_state == MR_IS_STALE
    
    For FMR, remap_count is incremented for every mapping.  So the above
    conditions should eventually be met.  But the state is set to
    MR_IS_STABLE only if an error happens and this is an uncommon case.
    If the above conditions are not met, the ibmr is added to the
    free_list in rds_ib_free_mr().  Hence for FRWR, the ibmr is added to
    the free_list.
    
    Then in rds_ib_flush_mr_pool(), the free_list and drop_list are
    combined.  The code goes through the combined list and checks for the
    same conditions as above.  If rds_ib_flush_mr_pool() is not called
    with free_all set to 1, only those ibmr which meet the above
    conditions are flushed.  Those which do not meet the conditions are
    added to the clean_list.  The clean_list is for ibmr reuse.  So for
    FRWR, all those ibmr on the free_list are moved to clean_list to be
    reused.
    
    When an ibmr is reused, the associated MRs are invalidated and
    unmapped.  This means that if an ibmr is not reused, the associated
    MRs will never be unmapped.  This is the cause of the problem.
    
    To fix this, if an ibmr is not reused for a certain period of time,
    it will be flushed.  This garbage collection idle time and the
    frequency to do garbage collection are controlled by the new sysctl
    net.rds.ib.frwr_ibmr_gc_idle_time and net.rds.ib.frwr_gc_interval
    respectively.
    
    Orabug: 29890749
    
    Tested-by: Gerald Gibson <gerald.gibson@oracle.com>
    Tested-by: Ilya Listvinsky <ilya.listvinsky@oracle.com>
    Tested-by: Sajid Zia <sajid.zia@oracle.com>
    Signed-off-by: Ka-Cheong Poon <ka-cheong.poon@oracle.com>
    Reviewed-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 9a73dd3d4bcf1c2bd048d7634466e6afdd4656e6)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 6c7cba4ddf690d444a208bb23834e854322d0b9c)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -690,4 +690,7 @@ extern unsigned long rds_ib_sysctl_max_recv_allocation;
 extern unsigned int rds_ib_sysctl_flow_control;
 extern unsigned int rds_ib_sysctl_disable_unmap_fmr_cpu;
 extern int rds_ib_sysctl_local_ack_timeout;
+extern u32 rds_frwr_wake_intrvl;
+extern u32 rds_frwr_ibmr_gc_time;
+
 #endif
