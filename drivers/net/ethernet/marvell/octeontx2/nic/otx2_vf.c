1c70f2bf188573449e9b585af07f86b16c3a1717
Subbaraya Sundeep <sbhatta@marvell.com>
2019-07-17 14:43:30 +0530

    octeontx2-af: Transmit packets during SMQ flush
    NIX SMQ flush may not complete if no send queue is
    transmitting packets. Hence packets need to be
    transmitted from a send queue when SMQ flush is
    initiated. This patch adds a SMQVF driver which
    is a stripped down version of VF netdev driver.
    The driver exports a function which can be called
    by AF driver during SMQ flush to transmit packets.
    A new VF of AF with pcifunc 17 is used for this
    purpose. So 17th AF VF should not attached to
    userspace.
    
    Orabug: 30483967
    
    Change-Id: I6592f7615f33f423de2d464f7441864443f0dec4
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/14557
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_vf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_vf.c
@@ -538,6 +538,12 @@ static int otx2vf_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 	otx2_setup_dev_hw_settings(vf);
 
+	err = otx2smqvf_probe(vf);
+	if (!err)
+		return 0;
+	else if (err == -EINVAL)
+		goto err_detach_rsrc;
+
 	/* Assign default mac address */
 	otx2_get_mac_from_af(netdev);
 
@@ -601,7 +607,9 @@ static void otx2vf_remove(struct pci_dev *pdev)
 		return;
 
 	vf = netdev_priv(netdev);
-	unregister_netdev(netdev);
+
+	if (otx2smqvf_remove(vf))
+		unregister_netdev(netdev);
 
 	otx2vf_disable_mbox_intr(vf);
 
