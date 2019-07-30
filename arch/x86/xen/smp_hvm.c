1c7b63745d4da247f6e0c8f7249e77c89bb0f756
Dongli Zhang <dongli.zhang@oracle.com>
2019-07-30 22:36:10 +0800

    xen: delay xen_hvm_init_time_ops() if kdump is boot on vcpu>=32
    It seems sched_clock() can be used very early since upstream
    commit 857baa87b642 ("sched/clock: Enable sched clock early").
    
    With upstream commit 38669ba205d1 ("x86/xen/time: Output xen sched_clock
    time from 0"), kdump kernel in Xen HVM guest may panic at very early stage
    when accessing &__this_cpu_read(xen_vcpu)->time as in below:
    
    setup_arch()
     -> init_hypervisor_platform()
         -> x86_init.hyper.init_platform = xen_hvm_guest_init()
             -> xen_hvm_init_time_ops()
                 -> xen_clocksource_read()
                     -> src = &__this_cpu_read(xen_vcpu)->time;
    
    This is because Xen HVM supports at most MAX_VIRT_CPUS=32 'vcpu_info'
    embedded inside 'shared_info' during early stage until xen_vcpu_setup() is
    used to allocate/relocate 'vcpu_info' for boot cpu at arbitrary address.
    
    However, when Xen HVM guest panic on vcpu >= 32, since
    xen_vcpu_info_reset(0) would set per_cpu(xen_vcpu, cpu) = NULL when
    vcpu >= 32, xen_clocksource_read() on vcpu >= 32 would panic.
    
    This patch delays xen_hvm_init_time_ops() to later in
    xen_hvm_smp_prepare_boot_cpu() after the 'vcpu_info' for boot vcpu is
    registered when the boot vcpu is >= 32.
    
    This fix is not applicable to upstream but only to uek5 because I think
    early boot sched clock is not supported by uek5.
    
    Although we will work on fix for upstream, as this issue is urgent, this
    patch is temporarily used for exadata product release so that kdump would
    be able to run successfully when vcpu >= 32 in exadata.
    
    Orabug: 30068152
    
    Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
    Reviewed-by: Joe Jin <joe.jin@oracle.com>

--- arch/x86/xen/smp_hvm.c
+++ arch/x86/xen/smp_hvm.c
@@ -18,6 +18,9 @@ static void __init xen_hvm_smp_prepare_boot_cpu(void)
 	 */
 	xen_vcpu_setup(0);
 
+	if (xen_vcpu_nr(0) >= MAX_VIRT_CPUS)
+		xen_hvm_init_time_ops();
+
 	/*
 	 * The alternative logic (which patches the unlock/lock) runs before
 	 * the smp bootup up code is activated. Hence we need to set this up
