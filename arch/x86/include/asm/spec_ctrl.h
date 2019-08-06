44e2878f1ab6ba646071570b221d936ad353af29
Patrick Colp <patrick.colp@oracle.com>
2019-08-06 11:36:32 -0700

    x86/speculation: Add RSB stuffing to entry code
    Actually do the RSB stuffing on entry/exit to/from the kernel, based on
    the static key added in a previous commit.
    
    Orabug: 30090397
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/include/asm/spec_ctrl.h
+++ arch/x86/include/asm/spec_ctrl.h
@@ -112,6 +112,63 @@
 .Ldone_\@:
 .endm
 
+/*
+ * Define stuff and overwrite RSB stuffing macro.
+ * Similar to __FILL_RETURN_BUFFER without the need of an extra register.
+ *
+ * This macro will either:
+ * - do nothing, if rsb_stuff_key is disabled
+ * - do stuffing of 16 RSB entries, if rsb_stuff_key is enabled,
+ *   similar to using RSB_FILL_LOOPS with __FILL_RETURN_BUFFER.
+ * - do RSB overwrite, clearing 32 RSB entries, if both
+ *   rsb_stuff_key and rsb_overwrite_key are enabled,
+ *   similar to using RSB_CLEAR_LOOPS with __FILL_RETURN_BUFFER.
+ */
+.macro STUFF_RSB
+	STATIC_JUMP_IF_TRUE .Lstuff_rsb_\@, rsb_stuff_key, def=0
+	jmp	.Ldone_call_\@
+.Lstuff_rsb_\@:
+	call	1f;	pause
+1:	call	2f;	pause
+2:	call	3f;	pause
+3:	call	4f;	pause
+4:	call	5f;	pause
+5:	call	6f;	pause
+6:	call	7f;	pause
+7:	call	8f;	pause
+8:	call	9f;	pause
+9:	call	10f;	pause
+10:	call	11f;	pause
+11:	call	12f;	pause
+12:	call	13f;	pause
+13:	call	14f;	pause
+14:	call	15f;	pause
+15:	call	16f;	pause
+16:	STATIC_JUMP_IF_TRUE .Loverwrite_rsb_\@, rsb_overwrite_key, def=0
+	jmp	.Lset_stack_half_\@
+.Loverwrite_rsb_\@:
+	call	17f;	pause
+17:	call	18f;	pause
+18:	call	19f;	pause
+19:	call	20f;	pause
+20:	call	21f;	pause
+21:	call	22f;	pause
+22:	call	23f;	pause
+23:	call	24f;	pause
+24:	call	25f;	pause
+25:	call	26f;	pause
+26:	call	27f;	pause
+27:	call	28f;	pause
+28:	call	29f;	pause
+29:	call	30f;	pause
+30:	call	31f;	pause
+31:	call	32f;	pause
+32:	add $(16*8), %rsp
+.Lset_stack_half_\@:
+	add $(16*8), %rsp
+.Ldone_call_\@:
+.endm
+
 #else /* __ASSEMBLY__ */
 
 #include <linux/cpu.h>
