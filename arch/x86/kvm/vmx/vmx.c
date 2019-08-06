067500b1cbb3a6bd11d3b1e1bf787874fbdc08cf
Patrick Colp <patrick.colp@oracle.com>
2019-08-06 13:59:46 -0700

    x86/speculation: Plumb additional support into KVM
    When switching between host and guest ensure that the correct SPEC_CTRL
    value is being used for that context. Also add support for
    VMEXIT_RSB_FULL to stuff the RSB on VMEXIT.
    
    Orabug: 30090397
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -45,6 +45,7 @@
 #include <asm/mshyperv.h>
 #include <asm/mwait.h>
 #include <asm/spec-ctrl.h>
+#include <asm/spec_ctrl.h>
 #include <asm/virtext.h>
 #include <asm/vmx.h>
 
@@ -6669,8 +6670,10 @@ static fastpath_t vmx_vcpu_run(struct kvm_vcpu *vcpu)
 	 * If the L02 MSR bitmap does not intercept the MSR, then we need to
 	 * save it.
 	 */
-	if (unlikely(!msr_write_intercepted(vmx, MSR_IA32_SPEC_CTRL)))
-		vmx->spec_ctrl = native_read_msr(MSR_IA32_SPEC_CTRL);
+	if (ibrs_supported) {
+		if (unlikely(!msr_write_intercepted(vmx, MSR_IA32_SPEC_CTRL)))
+			vmx->spec_ctrl = native_read_msr(MSR_IA32_SPEC_CTRL);
+	}
 
 	x86_spec_ctrl_restore_host(vmx->spec_ctrl, 0);
 
