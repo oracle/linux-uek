2fbf173018ee977a1f30626f0430f25de2fe196c
hariprasad <hkelam@marvell.com>
2019-08-07 16:11:24 +0530

    octeontx2-pf: Add tx vlan offload support
    This patch adds below features on vlan tx side
            * vlan tag insertion by NIX
            * Make use of hardware tso feature for tagged packets.
    
    Change-Id: Ic5806bdc52e6d1b24833cfef2efa0fc32cea6fe5
    Signed-off-by: hariprasad <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/13974
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 50b4c37b20624d0259c6a052229327f2a512b70e)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.c
@@ -636,6 +636,19 @@ static void otx2_sqe_add_ext(struct otx2_nic *pfvf, struct otx2_snd_queue *sq,
 		ext->tstmp = 1;
 	}
 
+#define OTX2_VLAN_PTR_OFFSET     (ETH_HLEN - ETH_TLEN)
+	if (skb_vlan_tag_present(skb)) {
+		if (skb->vlan_proto == htons(ETH_P_8021Q)) {
+			ext->vlan1_ins_ena = 1;
+			ext->vlan1_ins_ptr = OTX2_VLAN_PTR_OFFSET;
+			ext->vlan1_ins_tci = skb_vlan_tag_get(skb);
+		} else if (skb->vlan_proto == htons(ETH_P_8021AD)) {
+			ext->vlan0_ins_ena = 1;
+			ext->vlan0_ins_ptr = OTX2_VLAN_PTR_OFFSET;
+			ext->vlan0_ins_tci = skb_vlan_tag_get(skb);
+		}
+	}
+
 	*offset += sizeof(*ext);
 }
 
@@ -674,6 +687,9 @@ static void otx2_sqe_add_hdr(struct otx2_nic *pfvf, struct otx2_snd_queue *sq,
 	if (skb->ip_summed == CHECKSUM_PARTIAL) {
 		sqe_hdr->ol3ptr = skb_network_offset(skb);
 		sqe_hdr->ol4ptr = skb_transport_offset(skb);
+		/* get vlan protocol Ethertype */
+		if (eth_type_vlan(skb->protocol))
+			skb->protocol = vlan_get_protocol(skb);
 
 		if (skb->protocol == htons(ETH_P_IP)) {
 			proto = ip_hdr(skb)->protocol;
@@ -939,6 +955,9 @@ bool otx2_sq_append_skb(struct net_device *netdev, struct otx2_snd_queue *sq,
 	}
 
 	if (skb_shinfo(skb)->gso_size && !is_hw_tso_supported(pfvf, skb)) {
+		/* Insert vlan tag before giving pkt to tso */
+		if (skb_vlan_tag_present(skb))
+			skb = __vlan_hwaccel_push_inside(skb);
 		otx2_sq_append_tso(pfvf, sq, skb, qidx);
 		return true;
 	}
