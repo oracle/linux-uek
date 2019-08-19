43b25aa91af5ed2cd6a8db17757a58f8dd83f31c
Jason Gunthorpe <jgg@mellanox.com>
2019-08-19 14:17:05 +0300

    RDMA/odp: Provide ib_umem_odp_release() to undo the allocs
    Now that there are allocator APIs that return the ib_umem_odp directly
    it should be freed through a umem_odp free'er as well.
    
    Link: https://lore.kernel.org/r/20190819111710.18440-8-leon@kernel.org
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Orabug: 30729404
    
    (cherry picked from commit 0446cad9ca385c3b8d6e5a1184e59650fa7a7a6d)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/infiniband/core/umem.c
    
    Some files has been changed files according to the following commit to
    fixing compiling issue that exposed by this patch:
    
        b95df5e3e459 "drivers/IB,core: reduce scope of mmap_sem"
    
    Signed-off-by: Mohamad Heib <mohamadh@mellanox.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Tested-by: Gerald Gibson <gerald.gibson@oracle.com>
    Tested-by: Hong Liu <hong.x.liu@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -332,63 +332,23 @@ umem_kfree:
 }
 EXPORT_SYMBOL(ib_umem_get);
 
-static void __ib_umem_release_tail(struct ib_umem *umem)
-{
-	mmdrop(umem->owning_mm);
-	if (umem->is_odp)
-		kfree(to_ib_umem_odp(umem));
-	else
-		kfree(umem);
-}
-
-static void ib_umem_release_defer(struct work_struct *work)
-{
-	struct ib_umem *umem = container_of(work, struct ib_umem, work);
-
-	down_write(&umem->owning_mm->mmap_sem);
-	umem->owning_mm->pinned_vm -= ib_umem_num_pages(umem);
-	up_write(&umem->owning_mm->mmap_sem);
-
-	__ib_umem_release_tail(umem);
-}
-
 /**
  * ib_umem_release - release memory pinned with ib_umem_get
  * @umem: umem struct to release
  */
 void ib_umem_release(struct ib_umem *umem)
 {
-	struct ib_ucontext *context = umem->context;
-
-	if (umem->is_odp) {
-		ib_umem_odp_release(to_ib_umem_odp(umem));
-		__ib_umem_release_tail(umem);
-		return;
-	}
+	atomic64_t cnt;
+	if (umem->is_odp)
+		return ib_umem_odp_release(to_ib_umem_odp(umem));
 
 	__ib_umem_release(umem->context->device, umem, 1);
 
-	/*
-	 * We may be called with the mm's mmap_sem already held.  This
-	 * can happen when a userspace munmap() is the call that drops
-	 * the last reference to our file and calls our release
-	 * method.  If there are memory regions to destroy, we'll end
-	 * up here and not be able to take the mmap_sem.  In that case
-	 * we defer the vm_locked accounting a workqueue.
-	 */
-	if (context->closing) {
-		if (!down_write_trylock(&umem->owning_mm->mmap_sem)) {
-			INIT_WORK(&umem->work, ib_umem_release_defer);
-			queue_work(ib_wq, &umem->work);
-			return;
-		}
-	} else {
-		down_write(&umem->owning_mm->mmap_sem);
-	}
-	umem->owning_mm->pinned_vm -= ib_umem_num_pages(umem);
-	up_write(&umem->owning_mm->mmap_sem);
+        atomic64_set(&cnt, umem->owning_mm->pinned_vm);
+        umem->owning_mm->pinned_vm = atomic64_sub_return(ib_umem_num_pages(umem), &cnt);
+	mmdrop(umem->owning_mm);
+	kfree(umem);
 
-	__ib_umem_release_tail(umem);
 }
 EXPORT_SYMBOL(ib_umem_release);
 
