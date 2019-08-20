848eede279edfcb75f9a43ec7fb56fd8decb54a5
Paolo Bonzini <pbonzini@redhat.com>
2019-08-20 10:51:15 -0400

    kvm: mmu: ITLB_MULTIHIT mitigation
    With some Intel processors, putting the same virtual address in the TLB
    as both a 4 KiB and 2 MiB page can confuse the instruction fetch unit
    and cause the processor to issue a machine check.  Unfortunately if EPT
    page tables use huge pages, it possible for a malicious guest to cause
    this situation.
    
    This patch adds a knob to mark huge pages as non-executable. When the
    nx_huge_pages parameter is enabled (and we are using EPT), all huge pages
    are marked as NX. If the guest attempts to execute in one of those pages,
    the page is broken down into 4K pages, which are then marked executable.
    
    This is not an issue for shadow paging (except nested EPT), because then
    the host is in control of TLB flushes and the problematic situation cannot
    happen.  With nested EPT, again the nested guest can cause problems so we
    treat shadow and direct EPT the same.
    
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    CVE: CVE-2018-12207
    Orabug: 30413524
    
    Signed-off-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -290,6 +290,8 @@ struct kvm_mmu_page {
 	u64 *spt;
 	/* hold the gfn of each spte inside spt */
 	gfn_t *gfns;
+	bool lpage_disallowed;	/* Can't be replaced by an equiv large page */
+
 	bool unsync;
 	int root_count;          /* Currently serving as active root */
 	unsigned int unsync_children;
@@ -871,6 +873,7 @@ struct kvm_vm_stat {
 	ulong mmu_unsync;
 	ulong remote_tlb_flush;
 	ulong lpages;
+	ulong nx_lpage_splits;
 	ulong max_mmu_page_hash_collisions;
 };
 
