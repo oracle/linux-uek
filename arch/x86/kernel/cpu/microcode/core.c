3afc172770c969975f6bff45c0146c6183dcd10d
Ashok Raj <ashok.raj@intel.com>
2019-08-22 23:43:47 +0300

    x86/microcode: Update late microcode in parallel
    Microcode update was changed to be serialized due to restrictions after
    Spectre days. Updating serially on a large multi-socket system can be
    painful since it is being done on one CPU at a time.
    
    Cloud customers have expressed discontent as services disappear for a prolonged
    time. Currently, only one core (or only one thread of a core in the case of an
    SMT system) goes through the update while other cores (or respectively, SMT
    threads) are quiesced.
    
    Do the microcode update only on the first thread of each core while
    other siblings simply wait for this to complete.
    
     [ bp: Simplify, massage, cleanup comments. ]
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jon Grimm <Jon.Grimm@amd.com>
    Cc: kanth.ghatraju@oracle.com
    Cc: konrad.wilk@oracle.com
    Cc: patrick.colp@oracle.com
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: x86-ml <x86@kernel.org>
    Link: https://urldefense.proofpoint.com/v2/url?u=https-3A__lkml.kernel.org_r_1566506627-2D16536-2D2-2Dgit-2Dsend-2Demail-2Dmihai.carabas-40oracle.com&d=DwIBaQ&c=RoP1YumCXCgaWHvlZYR8PZh8Bv7qIrMUB65eapI_JnE&r=IOMTUEJr06tE0LeEzvwr_907ba6u9S5iDf7M8ZYjbGY&m=HQnUvxzYcAft4TQU6i2cgOnDcqXRq5HNmQfXDCW0mCc&s=keMggB0XGOzj8kyM47xUQK9t3GboAzk3sKUx-FUXPBM&e=
    
    Orabug: 30302412
    JIRA: LINUX-1612
    
    Patch has not been committed to any tree yet so (1) there is no SHA1 to
    reference and (2) commit message will probably change.
    
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -120,11 +120,6 @@ module_param(dis_ucode_ldr, bool, 0);
  */
 static DEFINE_MUTEX(microcode_mutex);
 
-/*
- * Serialize late loading so that CPUs get updated one-by-one.
- */
-static DEFINE_SPINLOCK(update_lock);
-
 struct ucode_cpu_info		ucode_cpu_info[NR_CPUS];
 EXPORT_SYMBOL_GPL(ucode_cpu_info);
 
@@ -410,9 +405,17 @@ static int __reload_late(void *info)
 	if (__wait_for_cpus(&late_cpus_in, NSEC_PER_SEC))
 		return -1;
 
-	spin_lock(&update_lock);
-	apply_microcode_local(&err);
-	spin_unlock(&update_lock);
+	/*
+	 * On an SMT system, it suffices to load the microcode on one sibling of
+	 * the core because the microcode engine is shared between the threads.
+	 * Synchronization still needs to take place so that no concurrent
+	 * loading attempts happen on multiple threads of an SMT core. See
+	 * below.
+	 */
+	if (cpumask_first(cpu_sibling_mask(cpu)) == cpu)
+		apply_microcode_local(&err);
+	else
+		goto wait_for_siblings;
 
 	if (err < 0) {
 		pr_warn("Error reloading microcode on CPU %d\n", cpu);
@@ -421,14 +424,18 @@ static int __reload_late(void *info)
 		ret = 1;
 	}
 
-	/*
-	 * Increase the wait timeout to a safe value here since we're
-	 * serializing the microcode update and that could take a while on a
-	 * large number of CPUs. And that is fine as the *actual* timeout will
-	 * be determined by the last CPU finished updating and thus cut short.
-	 */
-	if (__wait_for_cpus(&late_cpus_out, NSEC_PER_SEC * num_online_cpus()))
+wait_for_siblings:
+	if (__wait_for_cpus(&late_cpus_out, NSEC_PER_SEC))
 		panic("Timeout during microcode update!\n");
+
+	/*
+	 * At least one thread has completed update on each core.
+	 * For others, simply call the update to make sure the
+	 * per-cpu cpuinfo can be updated with right microcode
+	 * revision.
+	 */
+	if (cpumask_first(cpu_sibling_mask(cpu)) != cpu)
+		apply_microcode_local(&err);
 
 	return ret;
 }
