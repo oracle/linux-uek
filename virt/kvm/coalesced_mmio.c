bc9400c9b2d126277208fec95743a78c077be5ff
Matt Delco <delco@chromium.org>
2019-09-12 16:07:58 -0700

    KVM: coalesced_mmio: add bounds checking
    The first/last indexes are typically shared with a user app.
    The app can change the 'last' index that the kernel uses
    to store the next result.  This change sanity checks the index
    before using it for writing to a potentially arbitrary address.
    
    Signed-off-by: Matt Delco <delco@chromium.org>
    
    Orabug: 30318042
    CVE: CVE-2019-14821
    [setje: This patch came to UEK while still under embargo.]
    Signed-off-by: Jan Setje-Eilers <jan.setjeeilers@oracle.com>
    Reviewed-by: Dhaval Giani <dhaval.giani@oracle.com>
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- virt/kvm/coalesced_mmio.c
+++ virt/kvm/coalesced_mmio.c
@@ -66,24 +66,27 @@ static int coalesced_mmio_write(struct kvm_vcpu *vcpu,
 {
 	struct kvm_coalesced_mmio_dev *dev = to_mmio(this);
 	struct kvm_coalesced_mmio_ring *ring = dev->kvm->coalesced_mmio_ring;
+	__u32 insert;
 
 	if (!coalesced_mmio_in_range(dev, addr, len))
 		return -EOPNOTSUPP;
 
 	spin_lock(&dev->kvm->ring_lock);
 
-	if (!coalesced_mmio_has_room(dev)) {
+	insert = ring->last; /* local copy to avoid check/use races */
+	if (!coalesced_mmio_has_room(dev) ||
+	    insert >= KVM_COALESCED_MMIO_MAX) {
 		spin_unlock(&dev->kvm->ring_lock);
 		return -EOPNOTSUPP;
 	}
 
 	/* copy data in first free entry of the ring */
 
-	ring->coalesced_mmio[ring->last].phys_addr = addr;
-	ring->coalesced_mmio[ring->last].len = len;
-	memcpy(ring->coalesced_mmio[ring->last].data, val, len);
+	ring->coalesced_mmio[insert].phys_addr = addr;
+	ring->coalesced_mmio[insert].len = len;
+	memcpy(ring->coalesced_mmio[insert].data, val, len);
 	smp_wmb();
-	ring->last = (ring->last + 1) % KVM_COALESCED_MMIO_MAX;
+	ring->last = (insert + 1) % KVM_COALESCED_MMIO_MAX;
 	spin_unlock(&dev->kvm->ring_lock);
 	return 0;
 }
