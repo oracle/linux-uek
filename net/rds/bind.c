d7223fd429e9012e0dc5c90d9a6ef64903f48b00
Ka-Cheong Poon <ka-cheong.poon@oracle.com>
2019-09-18 03:32:42 -0700

    net/rds: Check laddr_check before calling it
    In rds_bind(), laddr_check is called without checking if it is NULL or
    not.  But the loopback transport's laddr_check is NULL.  And
    rs_transport should be reset if rds_add_bound() fails.
    
    Orabug: 30313955
    
    Reported-by: syzbot+fae39afd2101a17ec624@syzkaller.appspotmail.com
    Signed-off-by: Ka-Cheong Poon <ka-cheong.poon@oracle.com>
    Acked-by: Santosh Shilimkar <santosh.shilimkar@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 9cbd4738c092c7aec0e78c30951b67c5d89167d8)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 36a29dd4de7679331b92c0cf072749a6dd4d2ee5)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/bind.c
+++ net/rds/bind.c
@@ -269,7 +269,8 @@ int rds_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
 	 */
 	if (rs->rs_transport) {
 		trans = rs->rs_transport;
-		if (trans->laddr_check(sock_net(sock->sk),
+		if (!trans->laddr_check ||
+		    trans->laddr_check(sock_net(sock->sk),
 				       binding_addr, scope_id) != 0) {
 			ret = -ENOPROTOOPT;
 			goto out;
@@ -287,6 +288,8 @@ int rds_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
 	}
 
 	ret = rds_add_bound(rs, binding_addr, &port, scope_id);
+	if (ret)
+		rs->rs_transport = NULL;
 
 out:
 	release_sock(sk);
