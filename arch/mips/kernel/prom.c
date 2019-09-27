20f8db49ef057e7f3d8c835a30bc45876a17c8e2
Henry Willard <henry.willard@oracle.com>
2019-09-27 13:04:00 -0600

    MIPS: octeon: Suppress early_init_dt_scan_memory damage.
    This patch fixes improper use of the fdt to initialize
    memory on the Octeon platform, which may cause loops or
    panics. More details are in the comments below.
    
    Jira: LINUX-3213 LINUX-3215
    Orabug: 34483890
    
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/mips/kernel/prom.c
+++ arch/mips/kernel/prom.c
@@ -38,6 +38,29 @@ char *mips_get_machine_name(void)
 #ifdef CONFIG_USE_OF
 void __init early_init_dt_add_memory_arch(u64 base, u64 size)
 {
+#ifdef CONFIG_CAVIUM_OCTEON_SOC
+	/*
+	 * This gets called from early_init_dt_scan_memory()
+	 * using the fdt as a reference for available memory.
+	 * On the Octeon MIPS platform this is the physical
+	 * memory on the system and does not reflect memory
+	 * used by u-boot and the other firmware.
+	 *
+	 * This gets called very early from relocate_kernel()
+	 * when CONFIG_RELOCATABLE is set. This will inhibit
+	 * later memory setup and the system will hang early
+	 * in the boot process. When CONFIG_RELOCATABLE is not
+	 * set, early_init_dt_scan() is called later, and it
+	 * may appear that everything is okay, but other users
+	 * of memblock may have problems and the kernel may
+	 * panic.
+	 *
+	 * Do nothing on Octeon. arch_mem_init() will initialize
+	 * memory correctly using the freelist pointer passed by
+	 * u-boot.
+	 */
+	return;
+#else
 	if (base >= PHYS_ADDR_MAX) {
 		pr_warn("Trying to add an invalid memory region, skipped\n");
 		return;
@@ -51,6 +74,7 @@ void __init early_init_dt_add_memory_arch(u64 base, u64 size)
 	}
 
 	add_memory_region(base, size, BOOT_MEM_RAM);
+#endif
 }
 
 int __init early_init_dt_reserve_memory_arch(phys_addr_t base,
