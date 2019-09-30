a5f63c02c870844762f5f67a590332ac0b233433
Jeremy Cline <jcline@redhat.com>
2019-09-30 21:22:47 +0000

    security: lockdown: expose a hook to lock the kernel down
    In order to automatically lock down kernels running on UEFI machines
    booted in Secure Boot mode, expose the lock_kernel_down() hook.
    
    Upstream Status: RHEL only
    Signed-off-by: Jeremy Cline <jcline@redhat.com>
    
    Orabug: 34304810
    Signed-off-by: Eric Snowberg <eric.snowberg@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -507,6 +507,7 @@ int security_inode_notifysecctx(struct inode *inode, void *ctx, u32 ctxlen);
 int security_inode_setsecctx(struct dentry *dentry, void *ctx, u32 ctxlen);
 int security_inode_getsecctx(struct inode *inode, void **ctx, u32 *ctxlen);
 int security_locked_down(enum lockdown_reason what);
+int security_lock_kernel_down(const char *where, enum lockdown_reason level);
 int lsm_fill_user_ctx(struct lsm_ctx __user *uctx, u32 *uctx_len,
 		      void *val, size_t val_len, u64 id, u64 flags);
 #else /* CONFIG_SECURITY */
@@ -1474,6 +1475,10 @@ static inline int security_inode_getsecctx(struct inode *inode, void **ctx, u32 
 	return -EOPNOTSUPP;
 }
 static inline int security_locked_down(enum lockdown_reason what)
+{
+	return 0;
+}
+static inline int security_lock_kernel_down(const char *where, enum lockdown_reason level)
 {
 	return 0;
 }
