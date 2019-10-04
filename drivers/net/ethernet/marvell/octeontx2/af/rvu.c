949dd451f69d1c93481a93768e834f4cbf0d5500
Rakesh Babu <rsaladi2@marvell.com>
2019-10-04 12:37:24 +0530

    octeontx2-af: Get MSIXTR_BASE from fw_data
    Get MSIX base address from firmware via fwdata structure
    instead of from CSR as the later will cause issue when kernel crashes
    and Kexec/Kdump loads secondary kernel and AF driver in the secondary
    kernel reads IOVA written by primary.
    
    Orabug: 30484033
    
    Change-Id: I5a54638e2909e208537c68d4725f8f159201fc9d
    Signed-off-by: Rakesh Babu <rsaladi2@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/16879
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -618,7 +618,10 @@ setup_vfmsix:
 	 */
 	cfg = rvu_read64(rvu, BLKADDR_RVUM, RVU_PRIV_CONST);
 	max_msix = cfg & 0xFFFFF;
-	phy_addr = rvu_read64(rvu, BLKADDR_RVUM, RVU_AF_MSIXTR_BASE);
+	if (rvu->fwdata && rvu->fwdata->msixtr_base)
+		phy_addr = rvu->fwdata->msixtr_base;
+	else
+		phy_addr = rvu_read64(rvu, BLKADDR_RVUM, RVU_AF_MSIXTR_BASE);
 	/* Register save */
 	rvu->msixtr_base_phy = phy_addr;
 	iova = dma_map_resource(rvu->dev, phy_addr,
@@ -905,6 +908,8 @@ init:
 
 	mutex_init(&rvu->rsrc_lock);
 
+	rvu_fwdata_init(rvu);
+
 	err = rvu_setup_msix_resources(rvu);
 	if (err)
 		return err;
@@ -927,8 +932,6 @@ init:
 		 */
 		rvu_scan_block(rvu, block);
 	}
-
-	rvu_fwdata_init(rvu);
 
 	err = rvu_npc_init(rvu);
 	if (err)
