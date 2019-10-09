1eeaaef1e65133493e052e0c3e5736b4ef9c8fc9
Jason Gunthorpe <jgg@mellanox.com>
2019-10-09 13:09:32 -0300

    RDMA/mlx5: Rework implicit ODP destroy
    Use SRCU in a sensible way by removing all MRs in the implicit tree from
    the two xarrays (the update operation), then a synchronize, followed by a
    normal single threaded teardown.
    
    This is only a little unusual from the normal pattern as there can still
    be some work pending in the unbound wq that may also require a workqueue
    flush. This is tracked with a single atomic, consolidating the redundant
    existing atomics and wait queue.
    
    For understand-ability the entire ODP implicit create/destroy flow now
    largely exists in a single pair of functions within odp.c, with a few
    support functions for tearing down an unused child.
    
    Link: https://lore.kernel.org/r/20191009160934.3143-13-jgg@ziepe.ca
    Reviewed-by: Artemy Kovalyov <artemyko@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    
    Orabug: 33038818
    
    (cherry picked from commit 5256edcb98a14b11409a2d323f56a70a8b366363)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/infiniband/hw/mlx5/mlx5_ib.h
    	drivers/infiniband/hw/mlx5/odp.c
    
    Minor context conflicts in mlx5_ib_alloc_implicit_mr and mlx5_ib_mr
    due to existing commits
    
    Signed-off-by: Amir Tzin <amirtz@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -625,12 +625,15 @@ struct mlx5_ib_mr {
 	u64			pi_iova;
 
 	/* For ODP and implicit */
-	atomic_t		num_leaf_free;
-	wait_queue_head_t       q_leaf_free;
-	atomic_t		num_pending_prefetch;
+	atomic_t		num_deferred_work;
 	struct ib_odp_counters	odp_stats;
 	bool			is_odp_implicit;
 	struct xarray		implicit_children;
+	union {
+		struct rcu_head rcu;
+		struct list_head elm;
+		struct work_struct work;
+	} odp_destroy;
 
 	struct mlx5_async_work  cb_work;
 };
