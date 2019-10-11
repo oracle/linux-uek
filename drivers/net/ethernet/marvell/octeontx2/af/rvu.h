fd01c7cc0fa226611c75a59aab11b2c4bb9b68b6
hariprasad <hkelam@marvell.com>
2019-10-11 19:59:39 +0530

    octeontx2-af: dont force enable default unicast entry
    Functions which install default mcam rules like unicast, enable
    entries as well. This patch modifies that behavior to enable
    only when they were enabled before. This simplifies logic such
    that traffic is received only after rvu_mbox_handler_nix_lf_start_rx
    is called.
    
    Orabug: 30484033
    
    Change-Id: I936097892c63f5210dd62e4328a2b89db45fc16f
    Signed-off-by: hariprasad <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/17492
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -643,11 +643,14 @@ void rvu_npc_get_mcam_counter_alloc_info(struct rvu *rvu, u16 pcifunc,
 					 int *enable_cnt);
 int npc_flow_steering_init(struct rvu *rvu, int blkaddr);
 const char *npc_get_field_name(u8 hdr);
-int rvu_npc_write_default_rule(struct rvu *rvu, int blkaddr, int nixlf,
-			       u16 pcifunc, u8 intf, struct mcam_entry *entry);
+bool rvu_npc_write_default_rule(struct rvu *rvu, int blkaddr, int nixlf,
+				u16 pcifunc, u8 intf, struct mcam_entry *entry,
+				int *entry_index);
 int npc_mcam_verify_channel(struct rvu *rvu, u16 pcifunc, u8 intf, u16 channel);
 int npc_get_bank(struct npc_mcam *mcam, int index);
 void npc_mcam_enable_flows(struct rvu *rvu, u16 target);
+void npc_enable_mcam_entry(struct rvu *rvu, struct npc_mcam *mcam,
+			   int blkaddr, int index, bool enable);
 
 /* CPT APIs */
 int rvu_cpt_init(struct rvu *rvu);
