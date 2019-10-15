55aaf12e09a1943c8be36304dc28a9d70e4ee5c0
Håkon Bugge <haakon.bugge@oracle.com>
2019-10-15 15:01:29 +0200

    rds: Fix and augment probe counters
    Prior to commit 041dc3e4d340 ("Backport multipath RDS from upstream to
    UEK4"), there was a function called rds_send_pong that incremented the
    statistics counter s_send_pong. After said commit, the function was
    re-factored and renamed to rds_send_probe. It was used as a helper
    function to send pongs, heartbeat pings, and heartbeat pongs. But it
    still, incorrectly now, incremented s_send_pong.
    
    Fixes the above in addition to introducing the full matrix of counters
    for {send,recv}_[{hb_,mprds_}]{ping,pong}.
    
    Orabug: 30418039
    
    Fixes: 041dc3e4d340 ("Backport multipath RDS from upstream to UEK4")
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Tested-by: Michael Nowak <michael.nowak@oracle.com>
    Reviewed-by: Ka-Cheong Poon <ka-cheong.poon@oracle.com>
    
    ---
    
    v1->v2:
    	* Removed MPRDS handshake probes from the statistics
    	* Moved increment of s_send_ping to rds_sendmsg() for
              simplicity, as suggested by Ka-Cheong
    	* Added Mike's t-b
    
    v2->v3:
    	* Added separate counters for MPRDS handshakes, as per
              discussion with Ka-Cheong
    	* Fixed a glitch where MPRDS handshakes would be logged as
              illegal probes, as detected by Ka-Cheong
    
    v3->v4:
    	* Re-factored the statistics counting in rds_send_probe() and
              moved them to the respective functions calling
              rds_send_probe()
    	* Added Ka-Cheong's r-b
    
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- net/rds/send.c
+++ net/rds/send.c
@@ -1628,6 +1628,9 @@ int rds_sendmsg(struct socket *sock, struct msghdr *msg, size_t payload_len)
 	 */
 	rds_stats_inc(s_send_queued);
 
+	if (!dport)
+		rds_stats_inc(s_send_ping);
+
 	ret = rds_send_xmit(cpath);
 	if (ret == -ENOMEM || ret == -EAGAIN)
 		rds_cond_queue_send_work(cpath, 1);
@@ -1850,7 +1853,6 @@ static int rds_send_probe(struct rds_conn_path *cp, __be16 sport,
 	spin_unlock_irqrestore(&cp->cp_lock, flags);
 
 	rds_stats_inc(s_send_queued);
-	rds_stats_inc(s_send_pong);
 
 	if (!test_bit(RDS_LL_SEND_FULL, &cp->cp_flags))
 		rds_cond_queue_send_work(cp, 0);
@@ -1872,10 +1874,13 @@ rds_send_hb(struct rds_connection *conn, int response)
 	if (conn->c_trans->t_type == RDS_TRANS_TCP)
 		return 0;
 
-	if (response)
+	if (response) {
 		flags |= RDS_FLAG_HB_PONG;
-	else
+		rds_stats_inc(s_send_hb_pong);
+	} else {
 		flags |= RDS_FLAG_HB_PING;
+		rds_stats_inc(s_send_hb_ping);
+	}
 	flags |= RDS_FLAG_ACK_REQUIRED;
 
 	rds_send_probe(&conn->c_path[0], 0, 0, flags);
@@ -1886,6 +1891,11 @@ rds_send_hb(struct rds_connection *conn, int response)
 int
 rds_send_pong(struct rds_conn_path *cp, __be16 dport)
 {
+	if (be16_to_cpu(dport) == RDS_FLAG_PROBE_PORT)
+		rds_stats_inc(s_send_mprds_pong);
+	else
+		rds_stats_inc(s_send_pong);
+
 	return rds_send_probe(cp, 0, dport, 0);
 }
 
@@ -1901,7 +1911,9 @@ rds_send_ping(struct rds_connection *conn, int cp_index)
 		return;
 	}
 	conn->c_ping_triggered = 1;
+
 	spin_unlock_irqrestore(&cp->cp_lock, flags);
+	rds_stats_inc(s_send_mprds_ping);
 	rds_send_probe(cp, cpu_to_be16(RDS_FLAG_PROBE_PORT), 0, 0);
 }
 EXPORT_SYMBOL_GPL(rds_send_ping);
