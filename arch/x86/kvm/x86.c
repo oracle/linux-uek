ad430115ca9c64025b318cd0c36473e9eea50339
Aaron Lewis <aaronlewis@google.com>
2019-10-21 16:30:22 -0700

    KVM: x86: Remove unneeded kvm_vcpu variable, guest_xcr0_loaded
    The kvm_vcpu variable, guest_xcr0_loaded, is a waste of an 'int'
    and a conditional branch.  VMX and SVM are the only users, and both
    unconditionally pair kvm_load_guest_xcr0() with kvm_put_guest_xcr0()
    making this check unnecessary. Without this variable, the predicates in
    kvm_load_guest_xcr0 and kvm_put_guest_xcr0 should match.
    
    Suggested-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Reviewed-by: Jim Mattson <jmattson@google.com>
    Signed-off-by: Aaron Lewis <aaronlewis@google.com>
    Change-Id: I7b1eb9b62969d7bbb2850f27e42f863421641b23
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 78958563d8023db0c6d03a2fe2a64d79b47b4349)
    
    Conflicts:
      arch/x86/kvm/x86.c
    
    Orabug: 31687958
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Acked-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -828,12 +828,8 @@ EXPORT_SYMBOL_GPL(kvm_lmsw);
 void kvm_load_guest_xcr0(struct kvm_vcpu *vcpu)
 {
 	if (kvm_read_cr4_bits(vcpu, X86_CR4_OSXSAVE) &&
-			!vcpu->guest_xcr0_loaded) {
-		/* kvm_set_xcr() also depends on this */
-		if (vcpu->arch.xcr0 != host_xcr0)
-			xsetbv(XCR_XFEATURE_ENABLED_MASK, vcpu->arch.xcr0);
-		vcpu->guest_xcr0_loaded = 1;
-	}
+	    vcpu->arch.xcr0 != host_xcr0)
+		xsetbv(XCR_XFEATURE_ENABLED_MASK, vcpu->arch.xcr0);
 
 	if (static_cpu_has(X86_FEATURE_PKU) &&
 	    (kvm_read_cr4_bits(vcpu, X86_CR4_PKE) ||
@@ -853,11 +849,9 @@ void kvm_put_guest_xcr0(struct kvm_vcpu *vcpu)
 			__write_pkru(vcpu->arch.host_pkru);
 	}
 
-	if (vcpu->guest_xcr0_loaded) {
-		if (vcpu->arch.xcr0 != host_xcr0)
-			xsetbv(XCR_XFEATURE_ENABLED_MASK, host_xcr0);
-		vcpu->guest_xcr0_loaded = 0;
-	}
+	if (kvm_read_cr4_bits(vcpu, X86_CR4_OSXSAVE) &&
+	    vcpu->arch.xcr0 != host_xcr0)
+		xsetbv(XCR_XFEATURE_ENABLED_MASK, host_xcr0);
 }
 EXPORT_SYMBOL_GPL(kvm_put_guest_xcr0);
 
