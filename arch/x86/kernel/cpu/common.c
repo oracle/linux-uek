aa48efdaaaded336ae52d56d304a740e4c6ae240
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2019-10-24 18:26:25 -0400

    x86/tsx: Add TSX cmdline option with TSX disabled by default
    Add kernel cmdline parameter "tsx" to control the Transactional
    Synchronization Extensions (TSX) feature.  On CPUs that support TSX
    control, use "tsx=on|off" to enable or disable TSX.  Not specifying this
    option is equivalent to "tsx=off". This is because on certain processors
    TSX may be used as a part of a speculative side channel attack.
    
    Carve out the TSX controlling functionality into a separate compilation
    unit because TSX is a CPU feature while the TSX async abort control
    machinery will go to cpu/bugs.c.
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Mark Gross <mgross@linux.intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Tested-by: Neelima Krishnan <neelima.krishnan@intel.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Babu Moger <Babu.Moger@amd.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Juergen Gross <jgross@suse.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: linux-doc@vger.kernel.org
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: "Rafael J. Wysocki" <rafael.j.wysocki@intel.com>
    Cc: Rahul Tanwar <rahul.tanwar@linux.intel.com>
    Cc: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Cc: Sean Christopherson <sean.j.christopherson@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: x86-ml <x86@kernel.org>
    Cc: Zhao Yakui <yakui.zhao@intel.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Conflicts:
      Documentation/admin-guide/kernel-parameters.txt
      arch/x86/kernel/cpu/cpu.h
      arch/x86/kernel/cpu/intel.c
      arch/x86/kernel/cpu/tsx.c
    
    The file Documentation/admin-guide/kernel-parameters.txt doesn't exits,
    instead make the changes to Documentation/kernel-parameters.txt.
    
    In arch/x86/kernel/cpu/cpu.h and arch/x86/kernel/cpu/intel.c, the context
    is a bit different.
    
    In arch/x86/kernel/cpu/tsx.c, the headers are found in the `asm`
    directory, not `linux`.
    
    __ro_after_init doesn't exist, so remove it.
    
    Orabug: 30419232
    CVE: CVE-2019-11135
    
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>
    Reviewed-by: Jan Setje-Eilers <jan.setjeeilers@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -1172,6 +1172,8 @@ void __init identify_boot_cpu(void)
 	vgetcpu_set_mode();
 #endif
 	cpu_detect_tlb(&boot_cpu_data);
+
+	tsx_init();
 }
 
 void __cpuinit identify_secondary_cpu(struct cpuinfo_x86 *c)
