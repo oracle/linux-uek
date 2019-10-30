fd2786fbc1df7bcae383fcb6b7f22c68eb867037
Junaid Shahid <junaids@google.com>
2019-10-30 19:06:14 -0400

    kvm: x86: mmu: Recovery of shattered NX large pages
    The page table pages corresponding to broken down large pages are
    zapped in FIFO order, so that the large page can potentially
    be recovered, if it is no longer being used for execution.  This removes
    the performance penalty for walking deeper EPT page tables.
    
    By default, one large page will last about one hour once the guest
    reaches a steady state.
    
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    CVE: CVE-2018-12207
    Orabug: 29967616
    
    Signed-off-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -318,8 +318,9 @@ struct kvm_rmap_head {
 struct kvm_mmu_page {
 	struct list_head link;
 	struct hlist_node hash_link;
-	bool unsync;
+	struct list_head lpage_disallowed_link;
 
+	bool unsync;
 	/*
 	 * The following two entries are used to key the shadow page in the
 	 * hash table.
@@ -858,6 +859,7 @@ struct kvm_arch {
 	 * Hash table of struct kvm_mmu_page.
 	 */
 	struct list_head active_mmu_pages;
+	struct list_head lpage_disallowed_mmu_pages;
 	struct list_head zapped_obsolete_pages;
 	struct kvm_page_track_notifier_node mmu_sp_tracker;
 	struct kvm_page_track_notifier_head track_notifier_head;
@@ -930,6 +932,7 @@ struct kvm_arch {
 
 	bool guest_can_read_msr_platform_info;
 	bool exception_payload_enabled;
+	struct task_struct *nx_lpage_recovery_thread;
 };
 
 struct kvm_vm_stat {
