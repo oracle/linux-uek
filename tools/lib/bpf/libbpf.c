35d17102f349cbbf0a2f6aec38b7e259afd9b0b5
Alexei Starovoitov <ast@kernel.org>
2019-10-30 15:32:11 -0700

    bpf: Replace prog_raw_tp+btf_id with prog_tracing
    The bpf program type raw_tp together with 'expected_attach_type'
    was the most appropriate api to indicate BTF-enabled raw_tp programs.
    But during development it became apparent that 'expected_attach_type'
    cannot be used and new 'attach_btf_id' field had to be introduced.
    Which means that the information is duplicated in two fields where
    one of them is ignored.
    Clean it up by introducing new program type where both
    'expected_attach_type' and 'attach_btf_id' fields have
    specific meaning.
    In the future 'expected_attach_type' will be extended
    with other attach points that have similar semantics to raw_tp.
    This patch is replacing BTF-enabled BPF_PROG_TYPE_RAW_TRACEPOINT with
    prog_type = BPF_RPOG_TYPE_TRACING
    expected_attach_type = BPF_TRACE_RAW_TP
    attach_btf_id = btf_id of raw tracepoint inside the kernel
    Future patches will add
    expected_attach_type = BPF_TRACE_FENTRY or BPF_TRACE_FEXIT
    where programs have the same input context and the same helpers,
    but different attach points.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20191030223212.953010-2-ast@kernel.org
    (cherry picked from commit f1b9509c2fb0ef4db8d22dac9aef8e856a5d81f6)
    
    Orabug: 35110384
    
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Reviewed-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Reviewed-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    
    Conflicts:
    	include/uapi/linux/bpf.h
    	kernel/trace/bpf_trace.c
    	tools/include/uapi/linux/bpf.h
    	tools/lib/bpf/libbpf.c
    	tools/lib/bpf/libbpf_probes.c
    
    Contextual and __GENKSYMS__ wrapping for prog/attach types.
    Needed to add BPF_PROG_TYPE_TRACING to switch() statememt
    in libbpf[_probes].c to ensure libbpf will still build
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -3583,6 +3583,7 @@ static bool bpf_prog_type__needs_kver(enum bpf_prog_type type)
 	case BPF_PROG_TYPE_SK_MSG:
 	case BPF_PROG_TYPE_CGROUP_SOCK_ADDR:
 	case BPF_PROG_TYPE_LIRC_MODE2:
+	case BPF_PROG_TYPE_TRACING:
 	case BPF_PROG_TYPE_SK_REUSEPORT:
 	case BPF_PROG_TYPE_FLOW_DISSECTOR:
 	case BPF_PROG_TYPE_UNSPEC:
