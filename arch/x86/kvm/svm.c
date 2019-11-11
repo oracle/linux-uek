658a203032e6b095b8bcf4518707a88d877d0849
Liran Alon <liran.alon@oracle.com>
2019-11-11 14:26:21 +0200

    KVM: SVM: Remove check if APICv enabled in SVM update_cr8_intercept() handler
    This check is unnecessary as x86 update_cr8_intercept() which calls
    this VMX/SVM specific callback already performs this check.
    
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Liran Alon <liran.alon@oracle.com>
    Reviewed-by: Jim Mattson <jmattson@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 49d654d85f857d9ca34fe2b4ac6d6cf34677e6c1)
    
    Orabug: 31631366
    
    Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -5161,8 +5161,7 @@ static void update_cr8_intercept(struct kvm_vcpu *vcpu, int tpr, int irr)
 {
 	struct vcpu_svm *svm = to_svm(vcpu);
 
-	if (svm_nested_virtualize_tpr(vcpu) ||
-	    kvm_vcpu_apicv_active(vcpu))
+	if (svm_nested_virtualize_tpr(vcpu))
 		return;
 
 	clr_cr_intercept(svm, INTERCEPT_CR8_WRITE);
