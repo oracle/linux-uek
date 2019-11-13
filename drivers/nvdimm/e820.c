7b3f6783c5ff1e998f95477b5387595cf85172ce
Dan Williams <dan.j.williams@intel.com>
2019-11-12 17:08:56 -0800

    libnvdimm: Move nvdimm_bus_attribute_group to device_type
    A 'struct device_type' instance can carry default attributes for the
    device. Use this facility to remove the export of
    nvdimm_bus_attribute_group and put the responsibility on the core rather
    than leaf implementations to define this attribute.
    
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: "Oliver O'Halloran" <oohall@gmail.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Link: https://lore.kernel.org/r/157309903815.1582359.6418211876315050283.stgit@dwillia2-desk3.amr.corp.intel.com
    Orabug: 32224144
    (cherry picked from commit e755799aefa9385469bec49b2c2ccf1aaa33829a)
    Conflicts:
    	arch/powerpc/platforms/pseries/papr_scm.c
    	drivers/nvdimm/bus.c
    Contextual change: due to missing both 87a30e1f05d7 and f517f7925b7b. The former
      is a debug hack that'll break kABI, the latter introduces NVDIMM_FAMILY_PAPR
      in nvdimm support for power architecture.
    Signed-off-by: Jane Chu <jane.chu@oracle.com>
    Reviewed-by: Joao Martins<joao.m.martins@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/nvdimm/e820.c
+++ drivers/nvdimm/e820.c
@@ -7,11 +7,6 @@
 #include <linux/libnvdimm.h>
 #include <linux/module.h>
 
-static const struct attribute_group *e820_pmem_attribute_groups[] = {
-	&nvdimm_bus_attribute_group,
-	NULL,
-};
-
 static int e820_pmem_remove(struct platform_device *pdev)
 {
 	struct nvdimm_bus *nvdimm_bus = platform_get_drvdata(pdev);
@@ -54,7 +49,6 @@ static int e820_pmem_probe(struct platform_device *pdev)
 	struct nvdimm_bus *nvdimm_bus;
 	int rc = -ENXIO;
 
-	nd_desc.attr_groups = e820_pmem_attribute_groups;
 	nd_desc.provider_name = "e820";
 	nd_desc.module = THIS_MODULE;
 	nvdimm_bus = nvdimm_bus_register(dev, &nd_desc);
