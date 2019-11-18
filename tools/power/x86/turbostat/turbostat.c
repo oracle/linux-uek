a68f7d21866241634d971c99871b0d8cbfbb1101
Len Brown <len.brown@intel.com>
2019-11-17 21:18:31 -0500

    tools/power turbostat: update version
    A stitch in time saves nine.
    
    Signed-off-by: Len Brown <len.brown@intel.com>
    (cherry picked from commit b95fffb9b4afa8b9aa4a389ec7a0c578811eaf42)
    Orabug: 34802779
    Signed-off-by: Harshvardhan Jha <harshvardhan.j.jha@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -5414,7 +5414,7 @@ int get_and_dump_counters(void)
 }
 
 void print_version() {
-	fprintf(outf, "turbostat version 19.08.31"
+	fprintf(outf, "turbostat version 20.03.20"
 		" - Len Brown <lenb@kernel.org>\n");
 }
 
