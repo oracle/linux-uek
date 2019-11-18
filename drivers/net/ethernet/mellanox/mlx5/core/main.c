0abf351431bd851e96bba9dd37fe7c1c46f0cdaa
Raed Salem <raeds@mellanox.com>
2019-11-18 14:30:20 +0200

    net/mlx5: Accel, Add core IPsec support for the Connect-X family
    This to set the base for downstream patches to support
    the new IPsec implementation of the Connect-X family.
    
    Following modifications made:
    - Remove accel layer dependency from MLX5_FPGA_IPSEC.
    - Introduce accel_ipsec_ops, each IPsec device will
      have to support these ops.
    
    Signed-off-by: Raed Salem <raeds@mellanox.com>
    Reviewed-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Orabug: 32936614
    
    (cherry picked from commit 9a6ad1ad71fbc5a52617e016a3608d71b91f62e8)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 9a6ad1ad71fbc5a52617e016a3608d71b91f62e8
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1139,11 +1139,7 @@ static int mlx5_load(struct mlx5_core_dev *dev)
 		goto err_fpga_start;
 	}
 
-	err = mlx5_accel_ipsec_init(dev);
-	if (err) {
-		mlx5_core_err(dev, "IPSec device start failed %d\n", err);
-		goto err_ipsec_start;
-	}
+	mlx5_accel_ipsec_init(dev);
 
 	err = mlx5_accel_tls_init(dev);
 	if (err) {
@@ -1198,7 +1194,6 @@ err_fs:
 	mlx5_accel_tls_cleanup(dev);
 err_tls_start:
 	mlx5_accel_ipsec_cleanup(dev);
-err_ipsec_start:
 	mlx5_fpga_device_stop(dev);
 err_fpga_start:
 	mlx5_rsc_dump_cleanup(dev);
@@ -1717,7 +1712,7 @@ static int __init init(void)
 	get_random_bytes(&sw_owner_id, sizeof(sw_owner_id));
 
 	mlx5_core_verify_params();
-	mlx5_accel_ipsec_build_fs_cmds();
+	mlx5_fpga_ipsec_build_fs_cmds();
 	mlx5_register_debugfs();
 
 	err = pci_register_driver(&mlx5_core_driver);
