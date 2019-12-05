0c11b6b75fd297ddbc6e9c83b475572207529629
Subbaraya Sundeep <sbhatta@marvell.com>
2019-12-05 18:04:30 +0530

    octeontx2-pf: Do not set num_msgs in mbox header
    A single interrupt line from VF to PF is used by
    VF to send messages to PF and for replying to UP messages
    sent by PF. Hence num_msgs in mbox header is cleared
    explicitly in PF interrupt handler to avoid queuing a
    work twice for an event. The same num_msgs which was
    cleared in interrupt handler was set again in work
    queue handler by mistake. This patch fixes it.
    Also
     - clearing interrupt in work queue handlers is not
       necessary hence removed it.
     - checking status in message response is not needed
       hence removed it.
    
    Change-Id: I1875d37816b61b4874eea1c7175c63dfea71bcb1
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/20142
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit f7af4ada60a923d7ae3e57865d9f2a0dec8a5c21)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_vf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_vf.c
@@ -132,10 +132,6 @@ static void otx2vf_vfaf_mbox_handler(struct work_struct *work)
 	}
 
 	otx2_mbox_reset(mbox, 0);
-
-	/* Clear the IRQ */
-	smp_wmb();
-	otx2_write64(af_mbox->pfvf, RVU_VF_INT, BIT_ULL(0));
 }
 
 static int otx2vf_process_mbox_msg_up(struct otx2_nic *vf,
