8b1d5ad351b79bed5c506454cfc273a1d719cd7a
Chen Yu <yu.c.chen@intel.com>
2020-01-14 12:09:45 +0800

    tools/power turbostat: Support Jasper Lake
    Jasper Lake, like Elkhart Lake, uses a Tremont CPU.
    So reuse the code.
    
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>
    (cherry picked from commit d7814c3098ddb2780bb66e787aa3949110dd4a41)
    Orabug: 34802779
    Signed-off-by: Harshvardhan Jha <harshvardhan.j.jha@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -4659,6 +4659,9 @@ unsigned int intel_model_duplicates(unsigned int model)
 	case INTEL_FAM6_ATOM_TREMONT_D:
 		return INTEL_FAM6_ATOM_GOLDMONT_D;
 
+	case INTEL_FAM6_ATOM_TREMONT_L:
+		return INTEL_FAM6_ATOM_TREMONT;
+
 	case INTEL_FAM6_ICELAKE_X:
 	case INTEL_FAM6_SAPPHIRERAPIDS_X:
 		return INTEL_FAM6_SKYLAKE_X;
