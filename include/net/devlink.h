ba1a881a0afee0e13dde4193c9c122f4003aff08
Amit Cohen <amitc@mellanox.com>
2020-01-19 15:00:58 +0200

    devlink: Add overlay source MAC is multicast trap
    Add packet trap that can report NVE packets that the device decided to
    drop because their overlay source MAC is multicast.
    
    Signed-off-by: Amit Cohen <amitc@mellanox.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c3cae4916e57d2f0364d5e7769218547fb1b7c60)
    Orabug: 33236075
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -646,6 +646,7 @@ enum devlink_trap_generic_id {
 	DEVLINK_TRAP_GENERIC_ID_IPV6_LPM_UNICAST_MISS,
 	DEVLINK_TRAP_GENERIC_ID_NON_ROUTABLE,
 	DEVLINK_TRAP_GENERIC_ID_DECAP_ERROR,
+	DEVLINK_TRAP_GENERIC_ID_OVERLAY_SMAC_MC,
 
 	/* Add new generic trap IDs above */
 	__DEVLINK_TRAP_GENERIC_ID_MAX,
@@ -719,6 +720,8 @@ enum devlink_trap_group_generic_id {
 	"non_routable_packet"
 #define DEVLINK_TRAP_GENERIC_NAME_DECAP_ERROR \
 	"decap_error"
+#define DEVLINK_TRAP_GENERIC_NAME_OVERLAY_SMAC_MC \
+	"overlay_smac_is_mc"
 
 #define DEVLINK_TRAP_GROUP_GENERIC_NAME_L2_DROPS \
 	"l2_drops"
