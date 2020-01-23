e2b2bd9edf69ab04a12cd40fa43d908a23701e3d
Robert Richter <rrichter@marvell.com>
2020-01-23 09:02:54 +0000

    EDAC: Store error type in struct edac_raw_error_desc
    Store the error type in struct edac_raw_error_desc. This makes the
    type parameter of edac_raw_mc_handle_error() obsolete.
    
    [ kernel-doc typo ]
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Robert Richter <rrichter@marvell.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    Acked-by: Aristeu Rozanski <aris@redhat.com>
    Link: https://lkml.kernel.org/r/20200123090210.26933-4-rrichter@marvell.com
    (cherry picked from commit 672ef0e5684048a00aeb923b10131275ea688543)
    
    Orabug: 33427594
    
    Signed-off-by: William Roche <william.roche@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/edac.h
+++ include/linux/edac.h
@@ -525,6 +525,7 @@ struct errcount_attribute_data {
  * struct edac_raw_error_desc - Raw error report structure
  * @grain:			minimum granularity for an error report, in bytes
  * @error_count:		number of errors of the same type
+ * @type:			severity of the error (CE/UE/Fatal)
  * @top_layer:			top layer of the error (layer[0])
  * @mid_layer:			middle layer of the error (layer[1])
  * @low_layer:			low layer of the error (layer[2])
@@ -550,6 +551,7 @@ struct edac_raw_error_desc {
 
 	/* the vars below and grain will be cleaned on every new error report */
 	u16 error_count;
+	enum hw_event_mc_err_type type;
 	int top_layer;
 	int mid_layer;
 	int low_layer;
