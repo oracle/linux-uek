110aa45cc20b76f87cdab15d8261be2aea810296
John Donnelly <john.p.donnelly@oracle.com>
2020-02-05 13:36:51 -0800

    iommu: Force iommu shutdown on panic
    This eliminates intermitten kdump hangs reported on AMD servers,
    Exadata servers, and possible other X86 servers. UEK5 has iommu
    enabled regardless if it is used or not, and can leave the machine
    in an indeterminate state on kdump restart.
    
    Derived from the upsteam commit: iommu/vt-d: Turn off translations at shutdown.
    
    Orabug: 31043947
    
    (cherry picked from commit 60afd4ae65bf709f33440550af83b3b9ca48d254)
    (cherry picked from commit 82572ad7d8413a2d13eb8a2bfad618a40cc5bab1)
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    
    Conflicts:  - Corrected minor conflicts between UEK6 and UEK5.
    	arch/x86/include/asm/x86_init.h
    	arch/x86/kernel/x86_init.c
    	kernel/panic.c
    
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -4657,7 +4657,7 @@ static void intel_disable_iommus(void)
 		iommu_disable_translation(iommu);
 }
 
-void intel_iommu_shutdown(void)
+void intel_iommu_shutdown(int panic)
 {
 	struct dmar_drhd_unit *drhd;
 	struct intel_iommu *iommu = NULL;
@@ -4665,7 +4665,8 @@ void intel_iommu_shutdown(void)
 	if (no_iommu || dmar_disabled)
 		return;
 
-	down_write(&dmar_global_lock);
+	if (!panic)
+		down_write(&dmar_global_lock);
 
 	/* Disable PMRs explicitly here. */
 	for_each_iommu(iommu, drhd)
@@ -4674,7 +4675,9 @@ void intel_iommu_shutdown(void)
 	/* Make sure the IOMMUs are switched off */
 	intel_disable_iommus();
 
-	up_write(&dmar_global_lock);
+	if (!panic)
+		up_write(&dmar_global_lock);
+	
 }
 
 static inline struct intel_iommu *dev_to_intel_iommu(struct device *dev)
