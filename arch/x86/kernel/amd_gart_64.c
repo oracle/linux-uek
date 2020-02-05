a4360cdd2255b18e5082a7de9443300aa0b77cc6
John Donnelly <john.p.donnelly@oracle.com>
2020-02-05 13:36:51 -0800

    iommu:  Force iommu shutdown on panic
    This eliminates intermitten kdump hangs reported on AMD servers, and
    possible other X86 servers. UEK6 has iommu enabled regardless if it is used
    or not, and can leave the machine in an indeterminate state on kdump restart.
    
    Derived from the upsteam commit: iommu/vt-d: Turn off translations at shutdown.
    
    Orabug: 30896552
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/x86/kernel/amd_gart_64.c
+++ arch/x86/kernel/amd_gart_64.c
@@ -683,7 +683,7 @@ static const struct dma_map_ops gart_dma_ops = {
 	.get_required_mask		= dma_direct_get_required_mask,
 };
 
-static void gart_iommu_shutdown(void)
+static void gart_iommu_shutdown(int panic)
 {
 	struct pci_dev *dev;
 	int i;
