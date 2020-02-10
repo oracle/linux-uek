06bae1d657e8f1f9eecf10d31726c082766cfb62
Brett Creeley <brett.creeley@intel.com>
2020-02-10 10:59:18 -0800

    i40e: Fix the conditional for i40e_vc_validate_vqs_bitmaps
    Commit d9d6a9aed3f6 ("i40e: Fix virtchnl_queue_select bitmap
    validation") introduced a necessary change for verifying how queue
    bitmaps from the iavf driver get validated. Unfortunately, the
    conditional was reversed. Fix this.
    
    Fixes: d9d6a9aed3f6 ("i40e: Fix virtchnl_queue_select bitmap validation")
    Signed-off-by: Brett Creeley <brett.creeley@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f27f37a04a69890ac85d9155f03ee2d23b678d8f)
    
    Orabug: 30939512
    
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/intel/i40e/i40e_virtchnl_pf.c
+++ drivers/net/ethernet/intel/i40e/i40e_virtchnl_pf.c
@@ -2365,7 +2365,7 @@ static int i40e_vc_enable_queues_msg(struct i40e_vf *vf, u8 *msg)
 		goto error_param;
 	}
 
-	if (i40e_vc_validate_vqs_bitmaps(vqs)) {
+	if (!i40e_vc_validate_vqs_bitmaps(vqs)) {
 		aq_ret = I40E_ERR_PARAM;
 		goto error_param;
 	}
@@ -2427,7 +2427,7 @@ static int i40e_vc_disable_queues_msg(struct i40e_vf *vf, u8 *msg)
 		goto error_param;
 	}
 
-	if (i40e_vc_validate_vqs_bitmaps(vqs)) {
+	if (!i40e_vc_validate_vqs_bitmaps(vqs)) {
 		aq_ret = I40E_ERR_PARAM;
 		goto error_param;
 	}
