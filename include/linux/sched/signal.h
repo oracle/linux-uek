de959dbd5a8397413f020d3482f120b0c70128f8
Dan Duval <dan.duval@oracle.com>
2020-02-12 11:38:07 -0500

    add kABI padding to include/linux/sched/signal.h
    Add room for future expansion, without breaking kABI,
    to include/linux/sched/signal.h
    
    Orabug: 30952503
    Signed-off-by: Dan Duval <dan.duval@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com

--- include/linux/sched/signal.h
+++ include/linux/sched/signal.h
@@ -2,6 +2,7 @@
 #ifndef _LINUX_SCHED_SIGNAL_H
 #define _LINUX_SCHED_SIGNAL_H
 
+#include <linux/uek_kabi.h>
 #include <linux/rculist.h>
 #include <linux/signal.h>
 #include <linux/sched.h>
@@ -225,6 +226,11 @@ struct signal_struct {
 	struct mutex cred_guard_mutex;	/* guard against foreign influences on
 					 * credential calculations
 					 * (notably. ptrace) */
+ 
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
 } __randomize_layout;
 
 /*
