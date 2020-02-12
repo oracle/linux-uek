d6b847710af7d042b00f35ac0bdc2231824cf320
Dan Duval <dan.duval@oracle.com>
2020-02-12 12:04:43 -0500

    add kABI padding to include/net/xfrm.h
    Add room for future expansion, without breaking kABI,
    to include/net/xfrm.h
    
    Orabug: 30952503
    Signed-off-by: Dan Duval <dan.duval@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com

--- include/net/xfrm.h
+++ include/net/xfrm.h
@@ -2,6 +2,7 @@
 #ifndef _NET_XFRM_H
 #define _NET_XFRM_H
 
+#include <linux/uek_kabi.h>
 #include <linux/compiler.h>
 #include <linux/xfrm.h>
 #include <linux/spinlock.h>
@@ -123,6 +124,8 @@ struct xfrm_state_walk {
 	u8			proto;
 	u32			seq;
 	struct xfrm_address_filter *filter;
+ 
+	UEK_KABI_RESERVE(1)
 };
 
 struct xfrm_state_offload {
@@ -130,6 +133,11 @@ struct xfrm_state_offload {
 	unsigned long		offload_handle;
 	unsigned int		num_exthdrs;
 	u8			flags;
+ 
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
 };
 
 struct xfrm_mode {
