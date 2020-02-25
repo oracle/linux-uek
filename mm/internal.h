f39dfdd5b1be280017ac95fccae741016a4f3512
Linus Torvalds <torvalds@linux-foundation.org>
2020-02-26 01:46:08 +0530

    mm: make page ref count overflow check tighter and more explicit
    commit f958d7b528b1b40c44cfda5eabe2d82760d868c3 upsteam.
    
    We have a VM_BUG_ON() to check that the page reference count doesn't
    underflow (or get close to overflow) by checking the sign of the count.
    
    That's all fine, but we actually want to allow people to use a "get page
    ref unless it's already very high" helper function, and we want that one
    to use the sign of the page ref (without triggering this VM_BUG_ON).
    
    Change the VM_BUG_ON to only check for small underflows (or _very_ close
    to overflowing), and ignore overflows which have strayed into negative
    territory.
    
    Acked-by: Matthew Wilcox <willy@infradead.org>
    Cc: Jann Horn <jannh@google.com>
    Cc: stable@kernel.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [ 4.4.y backport notes:
      Ajay: Open-coded atomic refcount access due to missing
      page_ref_count() helper in 4.4.y
      Srivatsa: Added overflow check to get_page_foll() and related code. ]
    Signed-off-by: Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu>
    Signed-off-by: Ajay Kaher <akaher@vmware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Orabug: 31351941
    CVE: CVE-2019-11487
    (cherry picked from commit 4057527a5e0428892f6bce5e0e78540e053bc136)
    Signed-off-by: Dan Duval <dan.duval@oracle.com>
    Reviewed-by: John Donnelly  <john.p.donnelly@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- mm/internal.h
+++ mm/internal.h
@@ -62,7 +62,8 @@ static inline void __get_page_tail_foll(struct page *page,
 	 * speculative page access (like in
 	 * page_cache_get_speculative()) on tail pages.
 	 */
-	VM_BUG_ON_PAGE(atomic_read(&page->first_page->_count) <= 0, page);
+	VM_BUG_ON_PAGE(page_ref_zero_or_close_to_overflow(compound_head(page)),
+		       page);
 	if (get_page_head)
 		atomic_inc(&page->first_page->_count);
 	get_huge_page_tail(page);
@@ -87,7 +88,7 @@ static inline void get_page_foll(struct page *page)
 		 * Getting a normal page or the head of a compound page
 		 * requires to already have an elevated page->_count.
 		 */
-		VM_BUG_ON_PAGE(atomic_read(&page->_count) <= 0, page);
+		VM_BUG_ON_PAGE(page_ref_zero_or_close_to_overflow(page), page);
 		atomic_inc(&page->_count);
 	}
 }
