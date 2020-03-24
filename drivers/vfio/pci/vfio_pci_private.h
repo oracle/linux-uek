fade60a63fbd564fef2925fe8ff33f5d07b5847d
Alex Williamson <alex.williamson@redhat.com>
2020-03-24 09:28:28 -0600

    vfio/pci: Add sriov_configure support
    With the VF Token interface we can now expect that a vfio userspace
    driver must be in collaboration with the PF driver, an unwitting
    userspace driver will not be able to get past the GET_DEVICE_FD step
    in accessing the device.  We can now move on to actually allowing
    SR-IOV to be enabled by vfio-pci on the PF.  Support for this is not
    enabled by default in this commit, but it does provide a module option
    for this to be enabled (enable_sriov=1).  Enabling VFs is rather
    straightforward, except we don't want to risk that a VF might get
    autoprobed and bound to other drivers, so a bus notifier is used to
    "capture" VFs to vfio-pci using the driver_override support.  We
    assume any later action to bind the device to other drivers is
    condoned by the system admin and allow it with a log warning.
    
    vfio-pci will disable SR-IOV on a PF before releasing the device,
    allowing a VF driver to be assured other drivers cannot take over the
    PF and that any other userspace driver must know the shared VF token.
    This support also does not provide a mechanism for the PF userspace
    driver itself to manipulate SR-IOV through the vfio API.  With this
    patch SR-IOV can only be enabled via the host sysfs interface and the
    PF driver user cannot create or remove VFs.
    
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    (cherry picked from commit 137e5531351db258eff58ea28f4dc8fdf7ca2990)
    
    Conflicts:
    	drivers/vfio/pci/vfio_pci_private.h
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    (cherry picked from commit cfd936100608ed0348bef334214653a7145ea36a)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/vfio/pci/vfio_pci_private.h
+++ drivers/vfio/pci/vfio_pci_private.h
@@ -13,6 +13,7 @@
 #include <linux/irqbypass.h>
 #include <linux/types.h>
 #include <linux/uuid.h>
+#include <linux/notifier.h>
 
 #ifndef VFIO_PCI_PRIVATE_H
 #define VFIO_PCI_PRIVATE_H
@@ -135,6 +136,7 @@ struct vfio_pci_device {
 	struct mutex		ioeventfds_lock;
 	struct list_head	ioeventfds_list;
 	struct vfio_pci_vf_token	*vf_token;
+	struct notifier_block	nb;
 	struct mutex		vma_lock;
 	struct list_head	vma_list;
 	struct rw_semaphore	memory_lock;
