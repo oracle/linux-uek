f4a82f6cff7a6bbee931982670f5fb545d0c1f8c
Linu Cherian <lcherian@marvell.com>
2020-04-13 18:15:47 +0530

    octeontx2-af: Provision to make kernel backward compatible with older firmware
    Firmware interface checks are now relaxed to major version.
    Major version would change only if there is structural change
    in existing commands that breaks functionality.
    
    This would make kernel backward compatible with older firmware
    as much as possible.
    
    Change-Id: Ibdb52cc62908f3dbfb84c4d97b98ec3118545f36
    Signed-off-by: Linu Cherian <lcherian@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/26749
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 246a03f1cc6ab1c992c23d3ff69755ed25adb0db)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.c
@@ -1362,8 +1362,7 @@ static int cgx_lmac_verify_fwi_version(struct cgx *cgx)
 	minor_ver = FIELD_GET(RESP_MINOR_VER, resp);
 	dev_dbg(dev, "Firmware command interface version = %d.%d\n",
 		major_ver, minor_ver);
-	if (major_ver != CGX_FIRMWARE_MAJOR_VER ||
-	    minor_ver != CGX_FIRMWARE_MINOR_VER)
+	if (major_ver != CGX_FIRMWARE_MAJOR_VER)
 		return -EIO;
 	else
 		return 0;
