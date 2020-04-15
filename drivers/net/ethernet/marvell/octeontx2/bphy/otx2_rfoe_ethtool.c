fd2fbcc7475846c9bce932097c526cb12720a5ea
Naveen Mamindlapalli <naveenm@marvell.com>
2020-04-15 21:50:47 +0530

    OcteonTX2 BPHY RFOE netdev driver initial version
    The BPHY RFOE netdev driver handles the packets such as eCPRI control
    data, PTP and other ethernet packets received from/sent to BPHY RFOE
    MHAB in Linux kernel. All other packets (ROE, eCPRI non-control)
    received/sent are handled by ODP application in user space. The ODP
    application initializes the JDT/MBT/PSM-queues to process the Rx/Tx
    packets in netdev and shares the information through driver ioctl.
    The Rx/TX notification will be sent to netdev using one of the PSM GPINT.
    
    Change-Id: Ie7e0bb952c313007e67f37f4eb59661511cb00d9
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/27014
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ drivers/net/ethernet/marvell/octeontx2/bphy/otx2_rfoe_ethtool.c
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Marvell OcteonTx2 RFOE Ethernet Driver
+ *
+ * Copyright (C) 2020 Marvell International Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/ethtool.h>
+#include <linux/net_tstamp.h>
+
+#include "otx2_rfoe.h"
+
+static const char ethtool_stat_strings[][ETH_GSTRING_LEN] = {
+	"oth_rx_packets",
+	"ptp_rx_packets",
+	"ecpri_rx_packets",
+	"rx_bytes",
+	"oth_rx_dropped",
+	"ptp_rx_dropped",
+	"ecpri_rx_dropped",
+	"oth_tx_packets",
+	"ptp_tx_packets",
+	"ecpri_tx_packets",
+	"tx_bytes",
+	"oth_tx_dropped",
+	"ptp_tx_dropped",
+	"ecpri_tx_dropped",
+};
+
+static void otx2_rfoe_get_strings(struct net_device *netdev, u32 sset, u8 *data)
+{
+	switch (sset) {
+	case ETH_SS_STATS:
+		memcpy(data, *ethtool_stat_strings,
+		       sizeof(ethtool_stat_strings));
+		break;
+	}
+}
+
+static int otx2_rfoe_get_sset_count(struct net_device *netdev, int sset)
+{
+	switch (sset) {
+	case ETH_SS_STATS:
+		return ARRAY_SIZE(ethtool_stat_strings);
+	default:
+		return -EOPNOTSUPP;
+	}
+}
+
+static void otx2_rfoe_get_ethtool_stats(struct net_device *netdev,
+					struct ethtool_stats *stats,
+					u64 *data)
+{
+	struct otx2_rfoe_ndev_priv *priv = netdev_priv(netdev);
+
+	spin_lock(&priv->stats.lock);
+	memcpy(data, &priv->stats,
+	       ARRAY_SIZE(ethtool_stat_strings) * sizeof(u64));
+	spin_unlock(&priv->stats.lock);
+}
+
+static void otx2_rfoe_get_drvinfo(struct net_device *netdev,
+				  struct ethtool_drvinfo *p)
+{
+	struct otx2_rfoe_ndev_priv *priv = netdev_priv(netdev);
+
+	snprintf(p->driver, sizeof(p->driver), "otx2_rfoe {rfoe%d lmac%d}",
+		 priv->rfoe_num, priv->lmac_id);
+	strlcpy(p->bus_info, "platform", sizeof(p->bus_info));
+}
+
+static int otx2_rfoe_get_ts_info(struct net_device *netdev,
+				 struct ethtool_ts_info *info)
+{
+	info->so_timestamping = SOF_TIMESTAMPING_TX_SOFTWARE |
+				SOF_TIMESTAMPING_RX_SOFTWARE |
+				SOF_TIMESTAMPING_SOFTWARE |
+				SOF_TIMESTAMPING_TX_HARDWARE |
+				SOF_TIMESTAMPING_RX_HARDWARE |
+				SOF_TIMESTAMPING_RAW_HARDWARE;
+
+	info->phc_index = -1;
+
+	info->tx_types = (1 << HWTSTAMP_TX_OFF) | (1 << HWTSTAMP_TX_ON);
+
+	info->rx_filters = (1 << HWTSTAMP_FILTER_NONE) |
+			   (1 << HWTSTAMP_FILTER_ALL);
+
+	return 0;
+}
+
+static u32 otx2_rfoe_get_msglevel(struct net_device *netdev)
+{
+	struct otx2_rfoe_ndev_priv *priv = netdev_priv(netdev);
+
+	return priv->msg_enable;
+}
+
+static void otx2_rfoe_set_msglevel(struct net_device *netdev, u32 level)
+{
+	struct otx2_rfoe_ndev_priv *priv = netdev_priv(netdev);
+
+	priv->msg_enable = level;
+}
+
+static const struct ethtool_ops otx2_rfoe_ethtool_ops = {
+	.get_drvinfo		= otx2_rfoe_get_drvinfo,
+	.get_link		= ethtool_op_get_link,
+	.get_ts_info		= otx2_rfoe_get_ts_info,
+	.get_strings		= otx2_rfoe_get_strings,
+	.get_sset_count		= otx2_rfoe_get_sset_count,
+	.get_ethtool_stats	= otx2_rfoe_get_ethtool_stats,
+	.get_msglevel		= otx2_rfoe_get_msglevel,
+	.set_msglevel		= otx2_rfoe_set_msglevel,
+};
+
+void otx2_rfoe_set_ethtool_ops(struct net_device *netdev)
+{
+	netdev->ethtool_ops = &otx2_rfoe_ethtool_ops;
+}
