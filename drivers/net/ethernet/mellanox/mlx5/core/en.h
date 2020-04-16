d98a6ca875a142068148c7d29af1e628eb0b6023
Maxim Mikityanskiy <maximmi@mellanox.com>
2020-04-16 11:32:42 +0300

    net/mlx5e: Rename ICOSQ WQE info struct and field
    Structs mlx5e_txqsq and mlx5e_xdpsq contain wqe_info arrays to store
    supplementary information corresponding to WQEs in the queue. Struct
    mlx5e_icosq also has such an array, but it's called differently -
    ico_wqe. This patch renames it to unify with the other SQs.
    
    In addition, rename the struct to emphasize its specific usage.
    
    Signed-off-by: Maxim Mikityanskiy <maximmi@mellanox.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    Orabug: 33519567
    
    (cherry picked from commit 7d42c8e9ab50091e4dda29066975fbb7aa0f1585)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 7d42c8e9ab50091e4dda29066975fbb7aa0f1585
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -334,7 +334,7 @@ enum {
 	MLX5E_SQ_STATE_PENDING_XSK_TX,
 };
 
-struct mlx5e_sq_wqe_info {
+struct mlx5e_icosq_wqe_info {
 	u8  opcode;
 	u8 num_wqebbs;
 
@@ -516,7 +516,7 @@ struct mlx5e_icosq {
 
 	/* write@xmit, read@completion */
 	struct {
-		struct mlx5e_sq_wqe_info *ico_wqe;
+		struct mlx5e_icosq_wqe_info *wqe_info;
 	} db;
 
 	/* read only */
