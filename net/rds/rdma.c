f4aa0b626eb2bf451196c1ce09c724b86830ace6
Gerd Rausch <gerd.rausch@oracle.com>
2020-04-29 14:19:48 -0700

    net/rds: Don't sleep inside worker threads
    It blocks other work items waiting in line.
    Change wait_event_* calls into asynchronous event handlers instead.
    
    Orabug: 32373816
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
    (cherry picked from commit 5a205fbd3dd6813bdbc9b58807d9f682f7e19d0d)
    Port to U3
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 8c8240700aa46832f5a8a297e41abeb44ce06ed3)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit f75b96c41ce9ae70a12c6188681afb9abaa5a8cd)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/rdma.c
+++ net/rds/rdma.c
@@ -778,9 +778,12 @@ static void inc_rdma_map_pending(struct rds_conn_path *cp)
 
 static void dec_rdma_map_pending(struct rds_conn_path *cp)
 {
-	if (atomic_dec_and_test(&cp->cp_rdma_map_pending))
+	if (atomic_dec_and_test(&cp->cp_rdma_map_pending)) {
 		if (waitqueue_active(&cp->cp_waitq))
 			wake_up_all(&cp->cp_waitq);
+		if (test_bit(RDS_SHUTDOWN_WAITING, &cp->cp_flags))
+			mod_delayed_work(cp->cp_wq, &cp->cp_down_wait_w, 0);
+	}
 }
 
 /*
