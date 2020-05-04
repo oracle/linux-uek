c295d5141518ffdcc623932bc856221a4c73c979
Christoph Hellwig <hch@lst.de>
2020-05-04 19:15:26 +0200

    x86/platform/uv: Unexport uv_apicid_hibits
    This variable is not used by modular code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20200504171527.2845224-11-hch@lst.de
    
    Orabug: 32527679
    (cherry picked from commit 479d6d904557b774c1bd5b0338d8111dd2b322ee)
    Signed-off-by: Dan Duval <dan.duval@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>

--- arch/x86/kernel/apic/x2apic_uv_x.c
+++ arch/x86/kernel/apic/x2apic_uv_x.c
@@ -50,7 +50,6 @@ int uv_min_hub_revision_id;
 EXPORT_SYMBOL_GPL(uv_min_hub_revision_id);
 
 unsigned int uv_apicid_hibits;
-EXPORT_SYMBOL_GPL(uv_apicid_hibits);
 
 static struct apic apic_x2apic_uv_x;
 static struct uv_hub_info_s uv_hub_info_node0;
