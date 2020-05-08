27f91665b217cf4d4480d66c8c39fe7e2e1dfcb7
Shivasharan S <shivasharan.srikanteshwara@broadcom.com>
2020-05-08 14:21:30 +0530

    scsi: megaraid_sas: Replace undefined MFI_BIG_ENDIAN macro with __BIG_ENDIAN_BITFIELD macro
    MFI_BIG_ENDIAN macro used in drivers structure bitfield to check the CPU
    big endianness is undefined which would break the code on big endian
    machine. __BIG_ENDIAN_BITFIELD kernel macro should be used in places of
    MFI_BIG_ENDIAN macro.
    
    Link: https://lore.kernel.org/r/20200508085130.23339-1-chandrakanth.patil@broadcom.com
    Fixes: a7faf81d7858 ("scsi: megaraid_sas: Set no_write_same only for Virtual Disk")
    Cc: <stable@vger.kernel.org> # v5.6+
    Signed-off-by: Shivasharan S <shivasharan.srikanteshwara@broadcom.com>
    Signed-off-by: Chandrakanth Patil <chandrakanth.patil@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    Orabug: 31454512
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/scsi/megaraid/megaraid_sas.h
+++ drivers/scsi/megaraid/megaraid_sas.h
@@ -523,7 +523,7 @@ union MR_PROGRESS {
  */
 struct MR_PD_PROGRESS {
 	struct {
-#ifndef MFI_BIG_ENDIAN
+#ifndef __BIG_ENDIAN_BITFIELD
 		u32     rbld:1;
 		u32     patrol:1;
 		u32     clear:1;
@@ -549,7 +549,7 @@ struct MR_PD_PROGRESS {
 	};
 
 	struct {
-#ifndef MFI_BIG_ENDIAN
+#ifndef __BIG_ENDIAN_BITFIELD
 		u32     rbld:1;
 		u32     patrol:1;
 		u32     clear:1;
