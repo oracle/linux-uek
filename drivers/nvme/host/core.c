85f58b9b6c49ef27019defa38571abce581d927b
John Donnelly <john.p.donnelly@oracle.com>
2020-05-11 11:58:24 -0700

    nvme: Fix device removal of nvme client driver causing sysfs_warn_dup() warning.
    This change allows removal and reloading a NVME client driver (Qlogic qla2xxx FC driver)
    with NVME targets. After a teardown of the NVME client driver, the special sysfs devices are
    still present:
    
    	ls /sys/devices/virtual/nvme-subsystem/
    	nvme-subsys0
    	nvme-subsys1
    
    causing a driver reload failure.
    
    Fix provided by: qutran@marvell.com.
    
    Orabug: 30634740
    
    Fixes: 9af59b41ce73 nvme: Avoid flush dependency in delete controller flow
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -392,6 +392,7 @@ static void nvme_free_ns_head(struct kref *ref)
 	ida_simple_remove(&head->subsys->ns_ida, head->instance);
 	list_del_init(&head->entry);
 	cleanup_srcu_struct_quiesced(&head->srcu);
+	nvme_put_subsystem(head->subsys);
 	kfree(head);
 }
 
