a2fc16dbbd91a16a9b8387adc6621d576ddf3c64
Quentin Casasnovas <quentin.casasnovas@oracle.com>
2020-05-11 14:51:58 +0200

    KSPLICE: mips: clear the stack before going in the freezer.
    We were only cleaning the stack for ARM64 and amd64; this also adds it
    for MIPS.
    
    [ Vegard: See 2301153b43f7 ("KSPLICE: clear the stack before going in the
      freezer.") corresponding x86/aarch64 patch. ]
    
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    Reported-by: Vegard Nossum <vegard.nossum@oracle.com>
    
    [ Vegard: current_stack_pointer is not defined on MIPS ]
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Reviewed-by: Rob Gardner <rob.gardner@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 36725623
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/ksplice_clear_stack.h
+++ include/linux/ksplice_clear_stack.h
@@ -11,6 +11,10 @@
  */
 static inline void ksplice_clear_stack(void)
 {
+#ifdef CONFIG_MIPS
+	unsigned long current_stack_pointer =
+		(unsigned long)__builtin_frame_address(0);
+#endif
 	memset((void*)(current_stack_pointer - KSPLICE_CLEAR_STACK_BYTES),
 	       0x0, KSPLICE_CLEAR_STACK_BYTES);
 }
