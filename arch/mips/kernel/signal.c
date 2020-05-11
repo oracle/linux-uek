e5a50c8cda1765c95ea2a7441fd96cab87e9bbde
Quentin Casasnovas <quentin.casasnovas@oracle.com>
2020-05-11 14:51:58 +0200

    KSPLICE: mips: clear the stack before going in the freezer.
    We were only cleaning the stack for ARM64 and amd64; this also adds it
    for MIPS.
    
    [ Vegard: See 2301153b43f7 ("KSPLICE: clear the stack before going in the
      freezer.") corresponding x86/aarch64 patch. ]
    
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    Reported-by: Vegard Nossum <vegard.nossum@oracle.com>
    
    [ Vegard: current_stack_pointer is not defined on MIPS ]
    
    Orabug: 31353008
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Reviewed-by: Rob Gardner <rob.gardner@oracle.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/mips/kernel/signal.c
+++ arch/mips/kernel/signal.c
@@ -42,6 +42,7 @@
 #include <asm/msa.h>
 
 #include "signal-common.h"
+#include  <linux/ksplice_clear_stack.h>
 
 static int (*save_fp_context)(void __user *sc);
 static int (*restore_fp_context)(void __user *sc);
@@ -882,6 +883,8 @@ asmlinkage void do_notify_resume(struct pt_regs *regs, void *unused,
 	/* deal with pending signal delivery */
 	if (thread_info_flags & _TIF_SIGPENDING) {
 		set_thread_flag(TIF_KSPLICE_FROM_ENTRY_CODE);
+		if (thread_info_flags & _TIF_KSPLICE_FREEZING)
+			ksplice_clear_stack();
 		do_signal(regs);
 		clear_thread_flag(TIF_KSPLICE_FROM_ENTRY_CODE);
 	}
