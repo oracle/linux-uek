436383fba1dc07dd123dcec87659687e02cf30cf
Gerd Rausch <gerd.rausch@oracle.com>
2020-05-14 11:50:02 -0700

    Revert "RDS: IB: fix panic with handlers running post teardown"
    This reverts commit c0f72c8d665ed07fb86022873ce1a1dbe1ab9fd1.
    
    "tasklet_kill" only makes sure that the tasklet isn't scheduled,
    or the tasklet that was already scheduled is finished executing.
    
    After it's finished, future calls to "tasklet_schedule"
    are expected to continue having their callbacks executed.
    
    Preventing that is the job of "tasklet_disable".
    
    By returning early in functions "rds_ib_tasklet_fn_send"
    and "rds_ib_rx", the CQ no longer gets armed,
    a calls to "ib_req_notify_cq" are no longer done.
    That leads to those completions piling up and eventually
    the shutdown-path gets stuck also, waiting for
    "ic->i_signaled_sends" to go down to zero,
    which of course can no longer happen.
    
    The fact that this wasn't observed before tells us
    that the original problem for which this fix was introduced
    most likely wasn't reproduced in testing.
    
    Also, since "ic->i_cq_quiesce" was only reset
    in "rds_ib_cm_connect_complete", the shutdown-path
    would have gotten stuck even if no CQ notifications
    were missed before.
    
    If RDS wants to make sure no future tasklets are scheduled,
    it ought to use "tasklet_disable" instead.
    
    Orabug: 32373816
    
    Fixes: f3d0627f33a6 ("RDS: IB: fix panic with handlers running post teardown")
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Ka-cheong Poon <ka-cheong.poon@oracle.com>
    (cherry picked from commit 069628e191a962fe544f0b0f482ec3261feac00c)
    Port to U3
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit f017457aac3544f996dd698111d02e2dc6a3d40a)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 1ef9639c776c1b4185b58f203c6e24e91076e8e4)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -335,7 +335,6 @@ struct rds_ib_connection {
 	spinlock_t              i_rx_lock;
 	unsigned int            i_rx_wait_for_handler;
 	atomic_t                i_worker_has_rx;
-	atomic_t		i_cq_quiesce;
 	uint			i_irq_local_cpu;
 
 	/* For handling delayed release of device related resource. */
