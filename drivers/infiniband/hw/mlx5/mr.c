e258d4fe2adeb818cbc156dc964bfb85251dfe61
Nikhil Krishna <nikhilk@mellanox.com>
2020-05-15 09:34:02 -0700

    RDMA/mlx5: Set MR cache limit for both PF and VF
    MR cache limit is used for PF but not set for VF causing better latency
    on the PF. Issue is also in upstream and not limited to UEK.
    
    This change is not upstream and Mellanox cannot commit at this time it
    will be accepted upstream.
    
    Orabug: 33630950
    
    UEK6 => UEK7
    
    (cherry picked from commit 23e2964e1a1673d0cd4e3cda496cc87063263940)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Conflicts:
    	drivers/infiniband/hw/mlx5/mr.c
    
    Small contextual difference caused a conflict.
    
    Signed-off-by: Nikhil Krishna <nikhilk@mellanox.com>
    Tested-by: Ria Mirchandani <ria.mirchandani@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>

--- drivers/infiniband/hw/mlx5/mr.c
+++ drivers/infiniband/hw/mlx5/mr.c
@@ -736,12 +736,16 @@ int mlx5_mr_cache_init(struct mlx5_ib_dev *dev)
 		ent->xlt = (1 << ent->order) * sizeof(struct mlx5_mtt) /
 			   MLX5_IB_UMR_OCTOWORD;
 		ent->access_mode = MLX5_MKC_ACCESS_MODE_MTT;
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 		if ((dev->mdev->profile.mask & MLX5_PROF_MASK_MR_CACHE) &&
 		    !dev->is_rep && mlx5_core_is_pf(dev->mdev) &&
 		    mlx5_ib_can_load_pas_with_umr(dev, 0))
 			ent->limit = dev->mdev->profile.mr_cache[i].limit;
 		else
 			ent->limit = 0;
+#else
+		ent->limit = dev->mdev->profile.mr_cache[i].limit;
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 		spin_lock_irq(&ent->lock);
 		queue_adjust_cache_locked(ent);
 		spin_unlock_irq(&ent->lock);
