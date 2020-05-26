a65604b9272faccbb0ac3c7fa8e9aaf187497f71
Daniel Jordan <daniel.m.jordan@oracle.com>
2020-05-26 15:38:15 -0400

    padata: return first error code from a multithreaded job
    Return the first error code encountered from a multithreaded job.
    
    Threads in a job can fail for different reasons, which may need tracking
    in the future, but returning the first will do for VFIO page pinning
    because the kernel unwinds the same way no matter the error and the one
    current user of the optimization, qemu, only uses the code to decide
    whether to retry in one case and otherwise just logs error messages.
    
    Orabug: 31600516
    Signed-off-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1763,7 +1763,7 @@ deferred_init_maxorder(u64 *i, struct zone *zone, unsigned long *start_pfn,
 	return nr_pages;
 }
 
-static void __init
+static int __init
 deferred_init_memmap_chunk(unsigned long start_pfn, unsigned long end_pfn,
 			   void *arg)
 {
@@ -1781,6 +1781,8 @@ deferred_init_memmap_chunk(unsigned long start_pfn, unsigned long end_pfn,
 		deferred_init_maxorder(&i, zone, &spfn, &epfn);
 		cond_resched();
 	}
+
+	return 0;
 }
 
 /* An arch may override for more concurrency. */
