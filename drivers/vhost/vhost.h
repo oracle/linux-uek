c946870ba0bbdcb0554b55788642f2fbc84dc747
Jason Wang <jasowang@redhat.com>
2020-05-29 16:02:58 +0800

    vhost: allow device that does not depend on vhost worker
    vDPA device currently relays the eventfd via vhost worker. This is
    inefficient due the latency of wakeup and scheduling, so this patch
    tries to introduce a use_worker attribute for the vhost device. When
    use_worker is not set with vhost_dev_init(), vhost won't try to
    allocate a worker thread and the vhost_poll will be processed directly
    in the wakeup function.
    
    This help for vDPA since it reduces the latency caused by vhost worker.
    
    In my testing, it saves 0.2 ms in pings between VMs on a mutual host.
    
    Signed-off-by: Zhu Lingshan <lingshan.zhu@intel.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Link: https://lore.kernel.org/r/20200529080303.15449-2-jasowang@redhat.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit 01fcb1cbc88effb3493c6197efc96b69b9f4823a)
    
    Conflicts:
    	drivers/vhost/vdpa.c
    	drivers/vhost/vhost.c
    	drivers/vhost/vhost.h
    
    vDPA isn't backported.
    We don't have commit 8f6a7f96dc ("vhost, kcov: collect coverage from
    vhost_worker").
    
    Orabug: 31555511
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Si-Wei Liu <si-wei.liu@oracle.com>

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -153,6 +153,7 @@ struct vhost_dev {
 	int iov_limit;
 	int weight;
 	int byte_weight;
+	bool use_worker;
 	int (*msg_handler)(struct vhost_dev *dev,
 			   struct vhost_iotlb_msg *msg);
 };
@@ -160,6 +161,7 @@ struct vhost_dev {
 bool vhost_exceeds_weight(struct vhost_virtqueue *vq, int pkts, int total_len);
 void vhost_dev_init(struct vhost_dev *, struct vhost_virtqueue **vqs,
 		    int nvqs, int iov_limit, int weight, int byte_weight,
+		    bool use_worker,
 		    int (*msg_handler)(struct vhost_dev *dev,
 				       struct vhost_iotlb_msg *msg));
 long vhost_dev_set_owner(struct vhost_dev *dev);
