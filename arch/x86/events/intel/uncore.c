b6f416c725916acfd3fb686e53bcf685779adfbe
Roman Sudarikov <roman.sudarikov@linux.intel.com>
2020-06-01 11:35:41 +0300

    perf/x86/intel/uncore: Expose an Uncore unit to PMON mapping
    Each Uncore unit type, by its nature, can be mapped to its own context -
    which platform component each PMON block of that type is supposed to
    monitor.
    
    Intel® Xeon® Scalable processor family (code name Skylake-SP) makes
    significant changes in the integrated I/O (IIO) architecture. The new
    solution introduces IIO stacks which are responsible for managing traffic
    between the PCIe domain and the Mesh domain. Each IIO stack has its own
    PMON block and can handle either DMI port, x16 PCIe root port, MCP-Link
    or various built-in accelerators. IIO PMON blocks allow concurrent
    monitoring of I/O flows up to 4 x4 bifurcation within each IIO stack.
    
    Software is supposed to program required perf counters within each IIO
    stack and gather performance data. The tricky thing here is that IIO PMON
    reports data per IIO stack but users have no idea what IIO stacks are -
    they only know devices which are connected to the platform.
    
    Understanding IIO stack concept to find which IIO stack that particular
    IO device is connected to, or to identify an IIO PMON block to program
    for monitoring specific IIO stack assumes a lot of implicit knowledge
    about given Intel server platform architecture.
    
    Usage example:
        ls /sys/devices/uncore_<type>_<pmu_idx>/die*
    
    Signed-off-by: Alexander Antonov <alexander.antonov@linux.intel.com>
    Signed-off-by: Roman Sudarikov <roman.sudarikov@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Kan Liang <kan.liang@linux.intel.com>
    Reviewed-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Link: https://lkml.kernel.org/r/20200601083543.30011-2-alexander.antonov@linux.intel.com
    (cherry picked from commit 19a39819818dee57e363bd44bd096e2e940a456b)
    
    Orabug: 34561741
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: James Puthukattukaran <james.puthukattukaran@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/x86/events/intel/uncore.c
+++ arch/x86/events/intel/uncore.c
@@ -877,10 +877,12 @@ static int uncore_pmu_register(struct intel_uncore_pmu *pmu)
 			.read		= uncore_pmu_event_read,
 			.module		= THIS_MODULE,
 			.capabilities	= PERF_PMU_CAP_NO_EXCLUDE,
+			.attr_update	= pmu->type->attr_update,
 		};
 	} else {
 		pmu->pmu = *pmu->type->pmu;
 		pmu->pmu.attr_groups = pmu->type->attr_groups;
+		pmu->pmu.attr_update = pmu->type->attr_update;
 	}
 
 	uncore_get_pmu_name(pmu);
@@ -912,6 +914,9 @@ static void uncore_type_exit(struct intel_uncore_type *type)
 {
 	struct intel_uncore_pmu *pmu = type->pmus;
 	int i;
+
+	if (type->cleanup_mapping)
+		type->cleanup_mapping(type);
 
 	if (pmu) {
 		for (i = 0; i < type->num_boxes; i++, pmu++) {
@@ -983,6 +988,9 @@ static int __init uncore_type_init(struct intel_uncore_type *type, bool setid)
 	}
 
 	type->pmu_group = &uncore_pmu_attr_group;
+
+	if (type->set_mapping)
+		type->set_mapping(type);
 
 	return 0;
 
