29293faf4f388eab82fab638f426b4db0662024d
Christoph Hellwig <hch@lst.de>
2020-06-10 18:41:59 -0700

    kernel: add kthread_use_mm/kthread_unuse_mm helpers
    This is a merge of the following 3 upstream patches:
    
    1) 9bf5b9eb232b kernel: move use_mm/unuse_mm to kthread.c
    
       "This series improves the use_mm / unuse_mm interface by better documenting
       the assumptions, and my taking the set_fs manipulations spread over the
       callers into the core API."
    
       "These helpers are only for use with kernel threads, and I will tie them
       more into the kthread infrastructure going forward.  Also move the
       prototypes to kthread.h - mmu_context.h was a little weird to start with
       as it otherwise contains very low-level MM bits."
    
    2) 9bf5b9eb232b kernel: move use_mm/unuse_mm to kthread.c
    
       "cover the newly merged use_mm/unuse_mm caller in vfio"
    
    3) f5678e7f2ac3 kernel: better document the use_mm/unuse_mm API contract
    
       "Switch the function documentation to kerneldoc comments, and add
       WARN_ON_ONCE asserts that the calling thread is a kernel thread and does
       not have ->mm set (or has ->mm set in the case of unuse_mm)."
    
       "Also give the functions a kthread_ prefix to better document the use case."
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit 9bf5b9eb232b34738800868e30bea3bad4a6a1ba)
    (cherry picked from commit 4dbe59a6ae358f30b6a9a50406249d54cc954dc1)
    (cherry picked from commit f5678e7f2ac31c270334b936352f0ef2fe7dd2b3)
    
    Conflicts resolved:
    drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v10.c
    drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v7.c
    drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v8.c
    drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gfx_v9.c
       NOTE: The include file list context is different due to code base
       differences. No real conflicts.
    mm/mmu_context.c
       NOTE: To keep UEK-KABI compatibility fixes, old interfaces use_mm
       and unuse_mm are retained in mmu_context.c so this file is retained.
       Rest of the kernel will be using the new helpers kthread_use_mm and
       kthread_unuse_mm defined in kthread.c.
    drivers/vfio/vfio_iommu_type1.c
       NOTE: The conflict is due to code base being different but
       no real conflict. Patch just replaces the include of mmu_context.h
       with kthread.h.
    arch/powerpc/platforms/powernv/vas-fault.c
       NOTE: This file is not present in uek6 code base so the
       patch is not applicable.
    fs/io-wq.c
    fs/io_uring.c
       NOTE: These files are already at a higher code base version
       so the interface name changes are applied manually.
    
    UEK-KABI compatibility fixes:
       Wrapping the include <linux/mmu_context.h> with #ifndef __GENKSYMS__
       to fix UEK-KABI compatibility check errors on the symbols
       kthread_create_on_node and kthread_stop.
       Keeping the old interfaces use_mm() and unuse_mm() in the same location
       as before but the rest of the kernel will be using the new interfaces
       kthread_use_mm() and kthread_unuse_mm().
    
    Orabug: 33042559
    
    Signed-off-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Reviewed-by: Alan Adamson <alan.adamson@oracle.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_arcturus.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_arcturus.c
@@ -26,7 +26,6 @@
 #include <linux/module.h>
 #include <linux/fdtable.h>
 #include <linux/uaccess.h>
-#include <linux/mmu_context.h>
 #include <linux/firmware.h>
 #include "amdgpu.h"
 #include "amdgpu_amdkfd.h"
