e92fe5f9c9991d55e301bdd8267523885eec0523
Parav Pandit <parav@mellanox.com>
2020-06-19 03:32:52 +0000

    net/mlx5: Move helper to eswitch layer
    To use port number to port index conversion at eswitch level, move it to
    eswitch header.
    
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    Reviewed-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Orabug: 32176425
    
    (cherry picked from commit 443bf36eb543238cbd6399d658839e2e17c8dab2)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
    
    Minor context in a context due to Oracle specific:
    commit 786ff264c8c0 ("devlink: Introduce controller number")
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
@@ -1143,12 +1143,6 @@ is_devlink_port_supported(const struct mlx5_core_dev *dev,
 	       mlx5_eswitch_is_vf_vport(dev->priv.eswitch, rpriv->rep->vport);
 }
 
-static unsigned int
-vport_to_devlink_port_index(const struct mlx5_core_dev *dev, u16 vport_num)
-{
-	return (MLX5_CAP_GEN(dev, vhca_id) << 16) | vport_num;
-}
-
 static int register_devlink_port(struct mlx5_core_dev *dev,
 				 struct mlx5e_rep_priv *rpriv)
 {
@@ -1168,7 +1162,7 @@ static int register_devlink_port(struct mlx5_core_dev *dev,
 	 * TODO: load controller_num from offloads->host_number
 	 */
 	mlx5e_rep_get_port_parent_id(rpriv->netdev, &ppid);
-	dl_port_index = vport_to_devlink_port_index(dev, rep->vport);
+	dl_port_index = mlx5_esw_vport_to_devlink_port_index(dev, rep->vport);
 	pfnum = PCI_FUNC(dev->pdev->devfn);
 
 	if (rep->vport == MLX5_VPORT_UPLINK)
