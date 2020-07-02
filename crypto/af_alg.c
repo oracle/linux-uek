ffb8a48d3df75c3db064d2074f9925570bfba5b9
Herbert Xu <herbert@gondor.apana.org.au>
2020-07-02 13:32:21 +1000

    crypto: af_alg - Fix regression on empty requests
    commit 662bb52f50bca16a74fe92b487a14d7dccb85e1a upstream.
    
    Some user-space programs rely on crypto requests that have no
    control metadata.  This broke when a check was added to require
    the presence of control metadata with the ctx->init flag.
    
    This patch fixes the regression by setting ctx->init as long as
    one sendmsg(2) has been made, with or without a control message.
    
    Reported-by: Sachin Sant <sachinp@linux.vnet.ibm.com>
    Reported-by: Naresh Kamboju <naresh.kamboju@linaro.org>
    Fixes: f3c802a1f300 ("crypto: algif_aead - Only wake up when...")
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 546e3961b5d4c6db82cfb441fabb4353940c8f95)
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    (cherry picked from commit 2939b898498d9da54f5ea4c0b3d698a7644843fa)
    Signed-off-by: Yifei Liu <yifei.l.liu@oracle.com>

--- crypto/af_alg.c
+++ crypto/af_alg.c
@@ -892,6 +892,7 @@ int af_alg_sendmsg(struct socket *sock, struct msghdr *msg, size_t size,
 		err = -EINVAL;
 		goto unlock;
 	}
+	ctx->init = true;
 
 	if (init) {
 		ctx->enc = enc;
@@ -899,7 +900,6 @@ int af_alg_sendmsg(struct socket *sock, struct msghdr *msg, size_t size,
 			memcpy(ctx->iv, con.iv->iv, ivsize);
 
 		ctx->aead_assoclen = con.aead_assoclen;
-		ctx->init = true;
 	}
 
 	while (size) {
