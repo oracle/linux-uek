27acabf72e91264ed7e33f666708e575c900a870
Bjorn Helgaas <bhelgaas@google.com>
2020-07-16 17:34:30 -0500

    PCI/ERR: Rename pci_aer_clear_device_status() to pcie_clear_device_status()
    pci_aer_clear_device_status() clears the error bits in the PCIe Device
    Status Register (PCI_EXP_DEVSTA).  Every PCIe device has this register,
    regardless of whether it supports AER.
    
    Rename pci_aer_clear_device_status() to pcie_clear_device_status() to make
    clear that it is PCIe-specific but not AER-specific.  Move it to
    drivers/pci/pci.c, again since it's not AER-specific.  No functional change
    intended.
    
    Link: https://lore.kernel.org/r/20200717195619.766662-1-helgaas@kernel.org
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    (cherry picked from commit 600a5b4fc8e8f6dad098cd50e0e727cb2a16be46)
    
    Orabug: 32961050
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    (cherry picked from commit 0d308f0c848de56b09506931d16909c3df5af368)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -93,6 +93,7 @@ void pci_refresh_power_state(struct pci_dev *dev);
 void pci_power_up(struct pci_dev *dev);
 void pci_disable_enabled_device(struct pci_dev *dev);
 int pci_finish_runtime_suspend(struct pci_dev *dev);
+void pcie_clear_device_status(struct pci_dev *dev);
 void pcie_clear_root_pme_status(struct pci_dev *dev);
 bool pci_check_pme_status(struct pci_dev *dev);
 void pci_pme_wakeup_bus(struct pci_bus *bus);
@@ -673,7 +674,6 @@ void pci_aer_init(struct pci_dev *dev);
 void pci_aer_exit(struct pci_dev *dev);
 extern const struct attribute_group aer_stats_attr_group;
 void pci_aer_clear_fatal_status(struct pci_dev *dev);
-void pci_aer_clear_device_status(struct pci_dev *dev);
 int pci_aer_clear_status(struct pci_dev *dev);
 int pci_aer_raw_clear_status(struct pci_dev *dev);
 #else
@@ -681,7 +681,6 @@ static inline void pci_no_aer(void) { }
 static inline void pci_aer_init(struct pci_dev *d) { }
 static inline void pci_aer_exit(struct pci_dev *d) { }
 static inline void pci_aer_clear_fatal_status(struct pci_dev *dev) { }
-static inline void pci_aer_clear_device_status(struct pci_dev *dev) { }
 static inline int pci_aer_clear_status(struct pci_dev *dev) { return -EINVAL; }
 static inline int pci_aer_raw_clear_status(struct pci_dev *dev) { return -EINVAL; }
 #endif
