0e69ee70c172c7eeafc95f384cbe1eeaf68bccf6
Christoph Hellwig <hch@lst.de>
2020-07-21 10:48:15 +0200

    fs: push the getname from do_rmdir into the callers
    This mirrors do_unlinkat and will make life a little easier for
    the init code to reuse the whole function with a kernel filename.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    (cherry picked from commit e24ab0ef689de43649327f54cd1088f3dad25bb3)
    
    Orabug: 35495339
    
    Signed-off-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Reviewed-by: Alan Adamson <alan.adamson@oracle.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- fs/namei.c
+++ fs/namei.c
@@ -4021,17 +4021,16 @@ out:
 }
 EXPORT_SYMBOL(vfs_rmdir);
 
-long do_rmdir(int dfd, const char __user *pathname)
+long do_rmdir(int dfd, struct filename *name)
 {
 	int error = 0;
-	struct filename *name;
 	struct dentry *dentry;
 	struct path path;
 	struct qstr last;
 	int type;
 	unsigned int lookup_flags = 0;
 retry:
-	name = filename_parentat(dfd, getname(pathname), lookup_flags,
+	name = filename_parentat(dfd, name, lookup_flags,
 				&path, &last, &type);
 	if (IS_ERR(name))
 		return PTR_ERR(name);
@@ -4082,7 +4081,7 @@ exit1:
 
 SYSCALL_DEFINE1(rmdir, const char __user *, pathname)
 {
-	return do_rmdir(AT_FDCWD, pathname);
+	return do_rmdir(AT_FDCWD, getname(pathname));
 }
 
 /**
@@ -4228,8 +4227,7 @@ SYSCALL_DEFINE3(unlinkat, int, dfd, const char __user *, pathname, int, flag)
 		return -EINVAL;
 
 	if (flag & AT_REMOVEDIR)
-		return do_rmdir(dfd, pathname);
-
+		return do_rmdir(dfd, getname(pathname));
 	return do_unlinkat(dfd, getname(pathname));
 }
 
