574ff9f49f36f139ff67fb0556ad1c4c72d434fb
Håkon Bugge <haakon.bugge@oracle.com>
2020-07-21 14:15:07 +0200

    rds: ib: Revert "net/rds: Avoid stalled connection due to CM REQ retries"
    This reverts commit 637d07858a21cecb22c9d903e896a732cf3f7327.
    
    Commit 637d07858a21 ("net/rds: Avoid stalled connection due to CM REQ
    retries") introduced a generation scheme allegedly to detect duplicate
    RDMA CM REQs.
    
    However, duplicate CM packets are filtered by the CM layer and should
    not reach the client (RDS). Further, this commit introduced a bug when
    a node being the active connector rebooted. Once rebooted, the peer
    would classify the rebooted node's connection requests as duplicates.
    
    The signature of this bug is:
    
    rds_ib_cm_handle_connect: 1151: duplicated REQ in UP state. cm_id x conn y reject! ic->i_last_rej_seq 1 cm_req_seq 1
    
    Orabug: 31648137
    
    Tested-by: Aiman Al-Khammash <aiman.al-khammash@oracle.com>
    Tested-by: Gerald Gibson <gerald.gibson@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    
    Conflicts in:
    	net/rds/ib.h
    	net/rds/ib_cm.c
    
    	Due to context differences
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 4eea395a414d5bebd0b19461b52bd7abe26e243b)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 1b14fa074f1b418b4d7df20cc60f414d40446c07)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -56,8 +56,6 @@
 #define	RDS_IB_CQ_ERR		2
 
 #define RDS_IB_DEFAULT_FREG_PORT_NUM	1
-#define RDS_CM_RETRY_SEQ_EN	BIT(7)
-#define RDS_CM_REQ_SEQ_SZ	(RDS_CM_RETRY_SEQ_EN - 1)
 
 #define RDS_RDMA_RESOLVE_ADDR_TIMEOUT_MS(c) ((c)->c_loopback ? 1000 : 4000)
 
@@ -103,7 +101,7 @@ struct rds_ib_conn_priv_cmn {
 	u8			ricpc_protocol_minor;
 	__be16			ricpc_protocol_minor_mask;	/* bitmask */
 	u8			ricpc_tos;
-	u8			ricpc_cm_seq;
+	u8			ricpc_reserved1;
 	__be16			ricpc_frag_sz;
 	__be64			ricpc_ack_seq;
 	__be32			ricpc_credit;	/* non-zero enables flow ctl */
@@ -127,7 +125,7 @@ struct rds6_ib_connect_private {
 #define dp_protocol_minor	dp_cmn.ricpc_protocol_minor
 #define dp_protocol_minor_mask	dp_cmn.ricpc_protocol_minor_mask
 #define dp_tos			dp_cmn.ricpc_tos
-#define dp_cm_seq		dp_cmn.ricpc_cm_seq
+#define dp_reserved1		dp_cmn.ricpc_reserved1
 #define dp_frag_sz		dp_cmn.ricpc_frag_sz
 #define dp_ack_seq		dp_cmn.ricpc_ack_seq
 #define dp_credit		dp_cmn.ricpc_credit
@@ -297,9 +295,6 @@ struct rds_ib_connection {
 	unsigned int            i_rx_wait_for_handler;
 	atomic_t                i_worker_has_rx;
 	atomic_t		i_cq_quiesce;
-	u8			i_req_sequence;
-	u8			i_prev_seq;
-	u8			i_last_rej_seq;
 	uint			i_irq_local_cpu;
 
 	/* For handling delayed release of device related resource. */
@@ -314,7 +309,6 @@ struct rds_ib_connection {
 #define IB_GET_POST_CREDITS(v)	((v) >> 16)
 #define IB_SET_SEND_CREDITS(v)	((v) & 0xffff)
 #define IB_SET_POST_CREDITS(v)	((v) << 16)
-#define IB_GET_CM_SEQ_NUM(v)	((v) & RDS_CM_REQ_SEQ_SZ)
 
 struct rds_ib_ipaddr {
 	struct list_head	list;
