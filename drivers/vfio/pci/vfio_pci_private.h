7fb8d361ecc6a0b6723323a19612fb3696c846a0
Ankur Arora <ankur.a.arora@oracle.com>
2020-07-23 14:02:09 +0000

    vfio-pci: protect remap_pfn_range() from simultaneous calls
    A fix for CVE-2020-12888 change the mmap to be dynamic so it gets zapped out
    and faulted back in based on the state of the PCI_COMMAND register).
    
    The original code flow was this:
    
      vfio_iommu_type1::vfio_device_mmap()
        vfio_pci::vfio_pci_mmap()
          remap_pfn_range(vma->vm_start .. vma->vm_end);
    
      vfio_iommu_type1::vfio_iommu_type1_ioctl(VFIO_PIN_MAP_DMA)
        get_user_pages_remote()
        iommu_map()
    
    Which got changed to this:
    
      vfio_iommu_type1::vfio_device_mmap()
        vfio_pci::vfio_pci_mmap()
         /* Setup vm->vm_ops */
    
      vfio_iommu_type1::vfio_iommu_type1_ioctl(VFIO_PIN_MAP_DMA)
        get_user_pages_remote()
          follow_fault_pfn(vma, vaddr); /* For missing pages */
            fixup_user_fault()
    	  handle_mm_fault()
    	    vfio_pci::vfio_pci_mmap_fault()
    	      remap_pfn_range(vma->vm_start .. vma->vm_end);
        iommu_map()
    
    We use ktask to parallelize IOMMU mappings in vfio_iommu_type1 with a
    chunking unit of KTASK_PTE_MINCHUNK (128MB). When pinning DMA memory to
    export a CX-5 VF (2GB BAR0), this means potentially multiple calls to
    remap_pfn_range() -- each of which try to remap the full extent.
    
    This ends up hitting BUG_ON(!pte_none(*pte)) in remap_pte_range()
    because we are mapping an already mapped pte.
    
    The fix is to elide calls to remap_pfn_range() if the VMA is already
    mapped.
    
    Fixes: a2b07532ab02 ("vfio-pci: Invalidate mmaps and block MMIO access
    on disabled memory")
    
    CVE: CVE-2020-12888
    Orabug: 31663627
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    (cherry picked from commit e7d5f4f48b7ef250e64882f61cba949bc660ea33)

--- drivers/vfio/pci/vfio_pci_private.h
+++ drivers/vfio/pci/vfio_pci_private.h
@@ -130,6 +130,8 @@ struct vfio_pci_device {
 	struct mutex		vma_lock;
 	struct list_head	vma_list;
 	struct rw_semaphore	memory_lock;
+	/* Protects VMA against simultaneous remaps. */
+	struct mutex		map_lock;
 };
 
 #define is_intx(vdev) (vdev->irq_type == VFIO_PCI_INTX_IRQ_INDEX)
