ce11ed8543b43c03d37bce7d8ac9090acb5401c7
Liam R. Howlett <Liam.Howlett@Oracle.com>
2020-07-24 13:02:52 -0400

    Maple Tree: Add new data structure
    The Maple Tree is a new data structure optimised for storing
    ranges.
    
    Signed-off-by: Liam Howlett <Liam.Howlett@oracle.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

---
+++ tools/testing/radix-tree/trace/events/maple_tree.h
@@ -0,0 +1,4 @@
+#define trace_mas_split(a) do {} while(0);
+#define trace_mas_spanning_store(a) do {} while(0);
+#define trace_mas_rebalance(a) do {} while(0);
+#define trace_mas_spanning_rebalance(a) do {} while(0);
