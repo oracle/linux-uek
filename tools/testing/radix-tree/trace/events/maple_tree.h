1e3d5141e7e03452656a621660d0d950be096970
Liam R. Howlett <Liam.Howlett@Oracle.com>
2020-07-24 13:02:52 -0400

    Maple Tree: Add new data structure
    The maple tree is an RCU-safe range based B-tree designed to use modern
    processor cache efficiently.  There are a number of places in the kernel
    that a non-overlapping range-based tree would be beneficial, especially
    one with a simple interface.  The first user that is covered in this
    patch set is the vm_area_struct, where three data structures are
    replaced by the maple tree: the augmented rbtree, the vma cache, and the
    linked list of VMAs in the mm_struct.  The long term goal is to reduce
    or remove the mmap_sem contention.
    
    The tree has a branching factor of 10 for non-leaf nodes and 16 for leaf
    nodes.  With the increased branching factor, it is significantly shorter than
    the rbtree so it has fewer cache misses.  The removal of the linked list
    between subsequent entries also reduces the cache misses and the need to pull
    in the previous and next VMA during many tree alterations.
    
    Signed-off-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

---
+++ tools/testing/radix-tree/trace/events/maple_tree.h
@@ -0,0 +1,8 @@
+#define trace_mas_split(a) do {} while(0);
+#define trace_mas_spanning_store(a) do {} while(0);
+#define trace_mas_rebalance(a) do {} while(0);
+#define trace_mas_spanning_rebalance(a) do {} while(0);
+#define trace_mtree_load(a) do {} while(0);
+#define trace_mtree_erase(a) do {} while(0);
+#define trace_mtree_store_range(a, b) do {} while(0);
+#define trace_mas_is_span_wr(a, b, c) do {} while(0);
