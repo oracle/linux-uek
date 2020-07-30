be69439efc5c29c37c31e05d3a5d35c69296fa47
Sagi Grimberg <sagi@grimberg.me>
2020-07-30 11:51:00 -0700

    nvmet: fix a memory leak
    We forgot to free new_model_number
    
    Fixes: 013b7ebe5a0d ("nvmet: make ctrl model configurable")
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 382fee1a8b623e2546a3e15e80517389e0e0673e)
    Orabug: 32969554
    
    Signed-off-by: Alan Adamson <alan.adamson@oracle.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>

--- drivers/nvme/target/configfs.c
+++ drivers/nvme/target/configfs.c
@@ -1110,6 +1110,7 @@ static ssize_t nvmet_subsys_attr_model_store(struct config_item *item,
 	up_write(&nvmet_config_sem);
 
 	kfree_rcu(new_model, rcuhead);
+	kfree(new_model_number);
 
 	return count;
 }
