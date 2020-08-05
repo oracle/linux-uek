2cc5192eed080df337ae298a10d7000d59c15dbd
Dan Duval <dan.duval@oracle.com>
2020-08-05 14:23:16 -0400

    fix kABI breakage from "netns: provide pure entropy for net_hash_mix()"
    Commit adbb8bdd392db14dc80ad1ac29f8f1d37ab57a62 added element "hash_mix"
    to "struct net" in include/net/net_namespace.h.  As it happens, there
    is a four-byte hole at that location into which hash_mix will fit.
    
    Hide the addition of hash_mix from the kABI checker.
    
    Orabug: 31351903
    CVE: CVE-2019-10638
    CVE: CVE-2019-10639
    Signed-off-by: Dan Duval <dan.duval@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -56,7 +56,7 @@ struct net {
 						 */
 	spinlock_t		rules_mod_lock;
 
-	u32			hash_mix;
+	UEK_KABI_FILL_HOLE(u32			hash_mix)
 	atomic64_t		cookie_gen;
 
 	struct list_head	list;		/* list of network namespaces */
