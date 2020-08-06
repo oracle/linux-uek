55da5aa2a1a7b619b6d7d944ccdf7a0ff52d88e1
Arun Easi <aeasi@marvell.com>
2020-08-06 04:10:10 -0700

    scsi: qla2xxx: Fix WARN_ON in qla_nvme_register_hba
    qla_nvme_register_hba() puts out a warning when there are not enough queue
    pairs available for FC-NVME.  Just fail the NVME registration rather than a
    WARNING + call Trace.
    
    Link: https://lore.kernel.org/r/20200806111014.28434-8-njavali@marvell.com
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Signed-off-by: Arun Easi <aeasi@marvell.com>
    Signed-off-by: Nilesh Javali <njavali@marvell.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    (cherry picked from commit 897d68eb816bfae5ad9e870f68350dbb599d6e0e)
    
    Orabug: 31798223
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -3851,6 +3851,7 @@ struct qla_hw_data {
 		uint32_t        n2n_bigger:1;
 		uint32_t	secure_adapter:1;
 		uint32_t	secure_fw:1;
+		uint32_t	max_req_queue_warned:1;
 	} flags;
 
 	uint16_t max_exchg;
