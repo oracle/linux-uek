2cadb6740b33d1cd40f15a1493589d5be6ae490c
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2020-08-06 14:03:55 +0200

    cpufreq: intel_pstate: Implement passive mode with HWP enabled
    Allow intel_pstate to work in the passive mode with HWP enabled and
    make it set the HWP minimum performance limit (HWP floor) to the
    P-state value given by the target frequency supplied by the cpufreq
    governor, so as to prevent the HWP algorithm and the CPU scheduler
    from working against each other, at least when the schedutil governor
    is in use, and update the intel_pstate documentation accordingly.
    
    Among other things, this allows utilization clamps to be taken
    into account, at least to a certain extent, when intel_pstate is
    in use and makes it more likely that sufficient capacity for
    deadline tasks will be provided.
    
    After this change, the resulting behavior of an HWP system with
    intel_pstate in the passive mode should be close to the behavior
    of the analogous non-HWP system with intel_pstate in the passive
    mode, except that the HWP algorithm is generally allowed to make the
    CPU run at a frequency above the floor P-state set by intel_pstate in
    the entire available range of P-states, while without HWP a CPU can
    run in a P-state above the requested one if the latter falls into the
    range of turbo P-states (referred to as the turbo range) or if the
    P-states of all CPUs in one package are coordinated with each other
    at the hardware level.
    
    [Note that in principle the HWP floor may not be taken into account
     by the processor if it falls into the turbo range, in which case the
     processor has a license to choose any P-state, either below or above
     the HWP floor, just like a non-HWP processor in the case when the
     target P-state falls into the turbo range.]
    
    With this change applied, intel_pstate in the passive mode assumes
    complete control over the HWP request MSR and concurrent changes of
    that MSR (eg. via the direct MSR access interface) are overridden by
    it.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>
    (cherry picked from commit f6ebbcf08f37b01827c51309a188e85165e498e7)
    
    Orabug: 35524050
    
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -574,6 +574,8 @@ unsigned int cpufreq_driver_resolve_freq(struct cpufreq_policy *policy,
 unsigned int cpufreq_policy_transition_delay_us(struct cpufreq_policy *policy);
 int cpufreq_register_governor(struct cpufreq_governor *governor);
 void cpufreq_unregister_governor(struct cpufreq_governor *governor);
+int cpufreq_start_governor(struct cpufreq_policy *policy);
+void cpufreq_stop_governor(struct cpufreq_policy *policy);
 
 struct cpufreq_governor *cpufreq_default_governor(void);
 struct cpufreq_governor *cpufreq_fallback_governor(void);
