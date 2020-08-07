5dd031160050e1f7724ea1b84b9715bf712489f8
Johannes Weiner <hannes@cmpxchg.org>
2020-08-06 23:20:45 -0700

    mm: memcontrol: decouple reference counting from page accounting
    The reference counting of a memcg is currently coupled directly to how
    many 4k pages are charged to it.  This doesn't work well with Roman's new
    slab controller, which maintains pools of objects and doesn't want to keep
    an extra balance sheet for the pages backing those objects.
    
    This unusual refcounting design (reference counts usually track pointers
    to an object) is only for historical reasons: memcg used to not take any
    css references and simply stalled offlining until all charges had been
    reparented and the page counters had dropped to zero.  When we got rid of
    the reparenting requirement, the simple mechanical translation was to take
    a reference for every charge.
    
    More historical context can be found in commit e8ea14cc6ead ("mm:
    memcontrol: take a css reference for each charged page"), commit
    64f219938941 ("mm: memcontrol: remove obsolete kmemcg pinning tricks") and
    commit b2052564e66d ("mm: memcontrol: continue cache reclaim from offlined
    groups").
    
    The new slab controller exposes the limitations in this scheme, so let's
    switch it to a more idiomatic reference counting model based on actual
    kernel pointers to the memcg:
    
    - The per-cpu stock holds a reference to the memcg its caching
    
    - User pages hold a reference for their page->mem_cgroup. Transparent
      huge pages will no longer acquire tail references in advance, we'll
      get them if needed during the split.
    
    - Kernel pages hold a reference for their page->mem_cgroup
    
    - Pages allocated in the root cgroup will acquire and release css
      references for simplicity. css_get() and css_put() optimize that.
    
    - The current memcg_charge_slab() already hacked around the per-charge
      references; this change gets rid of that as well.
    
    - tcp accounting will handle reference in mem_cgroup_sk_{alloc,free}
    
    Roman:
    1) Rebased on top of the current mm tree: added css_get() in
       mem_cgroup_charge(), dropped mem_cgroup_try_charge() part
    2) I've reformatted commit references in the commit log to make
       checkpatch.pl happy.
    
    [hughd@google.com: remove css_put_many() from __mem_cgroup_clear_mc()]
      Link: http://lkml.kernel.org/r/alpine.LSU.2.11.2007302011450.2347@eggly.anvils
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Acked-by: Roman Gushchin <guro@fb.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Link: http://lkml.kernel.org/r/20200623174037.3951353-6-guro@fb.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    orabug: 32424163
    reference: https://lkml.kernel.org/r/20200422204708.2176080-6-guro@fb.com/
    since we still use try/commit/cancel charge API
    (cherry picked from commit 1a3e1f40962c445b997151a542314f3c6097f8c3)
    Signed-off-by: Libo Chen <libo.chen@oracle.com>
    Reviewed-by: Tom Hromatka <tom.hromatka@oracle.com>
    Tested-by: Tom Hromatka <tom.hromatka@oracle.com>
    
    Conflicts:
    	mm/memcontrol.c
    	mm/slab.h
    
    Conflicts due to not having commit f0e45fb4da29
    (mm: memcontrol: drop unused try/commit/cancel charge API) because we still have
    many users of try/commit/cancel charge API and having commit
    (mm: memcg/slab: charge individual slab objects instead of pages) that removed
    memcg_charge_slab()

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2340,13 +2340,17 @@ static void drain_stock(struct memcg_stock_pcp *stock)
 {
 	struct mem_cgroup *old = stock->cached;
 
+	if (!old)
+		return;
+
 	if (stock->nr_pages) {
 		page_counter_uncharge(&old->memory, stock->nr_pages);
 		if (do_memsw_account())
 			page_counter_uncharge(&old->memsw, stock->nr_pages);
-		css_put_many(&old->css, stock->nr_pages);
 		stock->nr_pages = 0;
 	}
+
+	css_put(&old->css);
 	stock->cached = NULL;
 }
 
@@ -2383,6 +2387,7 @@ static void refill_stock(struct mem_cgroup *memcg, unsigned int nr_pages)
 	stock = this_cpu_ptr(&memcg_stock);
 	if (stock->cached != memcg) { /* reset if necessary */
 		drain_stock(stock);
+		css_get(&memcg->css);
 		stock->cached = memcg;
 	}
 	stock->nr_pages += nr_pages;
@@ -2814,12 +2819,10 @@ force:
 	page_counter_charge(&memcg->memory, nr_pages);
 	if (do_memsw_account())
 		page_counter_charge(&memcg->memsw, nr_pages);
-	css_get_many(&memcg->css, nr_pages);
 
 	return 0;
 
 done_restock:
-	css_get_many(&memcg->css, batch);
 	if (batch > nr_pages)
 		refill_stock(memcg, batch - nr_pages);
 
@@ -2856,8 +2859,6 @@ static void cancel_charge(struct mem_cgroup *memcg, unsigned int nr_pages)
 	page_counter_uncharge(&memcg->memory, nr_pages);
 	if (do_memsw_account())
 		page_counter_uncharge(&memcg->memsw, nr_pages);
-
-	css_put_many(&memcg->css, nr_pages);
 }
 
 static void lock_page_lru(struct page *page, int *isolated)
@@ -3133,6 +3134,7 @@ int __memcg_kmem_charge_page(struct page *page, gfp_t gfp, int order)
 		if (!ret) {
 			page->mem_cgroup = memcg;
 			__SetPageKmemcg(page);
+			return 0;
 		}
 	}
 	css_put(&memcg->css);
@@ -3155,12 +3157,11 @@ void __memcg_kmem_uncharge_page(struct page *page, int order)
 	VM_BUG_ON_PAGE(mem_cgroup_is_root(memcg), page);
 	__memcg_kmem_uncharge(memcg, nr_pages);
 	page->mem_cgroup = NULL;
+	css_put(&memcg->css);
 
 	/* slab pages do not have PageKmemcg flag set */
 	if (PageKmemcg(page))
 		__ClearPageKmemcg(page);
-
-	css_put_many(&memcg->css, nr_pages);
 }
 
 static bool consume_obj_stock(struct obj_cgroup *objcg, unsigned int nr_bytes)
@@ -3306,13 +3307,16 @@ void obj_cgroup_uncharge(struct obj_cgroup *objcg, size_t size)
  */
 void mem_cgroup_split_huge_fixup(struct page *head)
 {
+	struct mem_cgroup *memcg = head->mem_cgroup;
 	int i;
 
 	if (mem_cgroup_disabled())
 		return;
 
-	for (i = 1; i < HPAGE_PMD_NR; i++)
-		head[i].mem_cgroup = head->mem_cgroup;
+	for (i = 1; i < HPAGE_PMD_NR; i++) {
+		css_get(&memcg->css);
+		head[i].mem_cgroup = memcg;
+	}
 
 	__mod_memcg_state(head->mem_cgroup, MEMCG_RSS_HUGE, -HPAGE_PMD_NR);
 }
@@ -5734,7 +5738,10 @@ static int mem_cgroup_move_account(struct page *page,
 	 */
 	smp_mb();
 
-	page->mem_cgroup = to; 	/* caller should have done css_get */
+	css_get(&to->css);
+	css_put(&from->css);
+
+	page->mem_cgroup = to;
 
 	__unlock_page_memcg(from);
 
@@ -5954,8 +5961,6 @@ static void __mem_cgroup_clear_mc(void)
 		 */
 		if (!mem_cgroup_is_root(mc.to))
 			page_counter_uncharge(&mc.to->memory, mc.moved_swap);
-
-		css_put_many(&mc.to->css, mc.moved_swap);
 
 		mc.moved_swap = 0;
 	}
@@ -6756,8 +6761,11 @@ int mem_cgroup_try_charge(struct page *page, struct mm_struct *mm,
 		memcg = get_mem_cgroup_from_mm(mm);
 
 	ret = try_charge(memcg, gfp_mask, nr_pages);
+	if (ret) {
+		css_put(&memcg->css);
+		memcg = NULL;
+	}
 
-	css_put(&memcg->css);
 out:
 	*memcgp = memcg;
 	return ret;
@@ -6853,6 +6861,8 @@ void mem_cgroup_cancel_charge(struct page *page, struct mem_cgroup *memcg,
 		return;
 
 	cancel_charge(memcg, nr_pages);
+
+	css_put(&memcg->css);
 }
 
 struct uncharge_gather {
@@ -6894,9 +6904,6 @@ static void uncharge_batch(const struct uncharge_gather *ug)
 	__this_cpu_add(ug->memcg->vmstats_percpu->nr_page_events, nr_pages);
 	memcg_check_events(ug->memcg, ug->dummy_page);
 	local_irq_restore(flags);
-
-	if (!mem_cgroup_is_root(ug->memcg))
-		css_put_many(&ug->memcg->css, nr_pages);
 }
 
 static void uncharge_page(struct page *page, struct uncharge_gather *ug)
@@ -6944,6 +6951,7 @@ static void uncharge_page(struct page *page, struct uncharge_gather *ug)
 
 	ug->dummy_page = page;
 	page->mem_cgroup = NULL;
+	css_put(&ug->memcg->css);
 }
 
 static void uncharge_list(struct list_head *page_list)
@@ -7052,8 +7060,8 @@ void mem_cgroup_migrate(struct page *oldpage, struct page *newpage)
 	page_counter_charge(&memcg->memory, nr_pages);
 	if (do_memsw_account())
 		page_counter_charge(&memcg->memsw, nr_pages);
-	css_get_many(&memcg->css, nr_pages);
 
+	css_get(&memcg->css);
 	commit_charge(newpage, memcg, false);
 
 	local_irq_save(flags);
@@ -7280,8 +7288,7 @@ void mem_cgroup_swapout(struct page *page, swp_entry_t entry)
 				     -nr_entries);
 	memcg_check_events(memcg, page);
 
-	if (!mem_cgroup_is_root(memcg))
-		css_put_many(&memcg->css, nr_entries);
+	css_put(&memcg->css);
 }
 
 /**
