26a88f13e37d8120fad066d0ffb58dd4ca20f05d
Roman Gushchin <guro@fb.com>
2020-08-06 23:21:20 -0700

    mm: memcg/slab: deprecate slab_root_caches
    Currently there are two lists of kmem_caches:
    1) slab_caches, which contains all kmem_caches,
    2) slab_root_caches, which contains only root kmem_caches.
    
    And there is some preprocessor magic to have a single list if
    CONFIG_MEMCG_KMEM isn't enabled.
    
    It was required earlier because the number of non-root kmem_caches was
    proportional to the number of memory cgroups and could reach really big
    values.  Now, when it cannot exceed the number of root kmem_caches, there
    is really no reason to maintain two lists.
    
    We never iterate over the slab_root_caches list on any hot paths, so it's
    perfectly fine to iterate over slab_caches and filter out non-root
    kmem_caches.
    
    It allows to remove a lot of config-dependent code and two pointers from
    the kmem_cache structure.
    
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Shakeel Butt <shakeelb@google.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Tejun Heo <tj@kernel.org>
    Link: http://lkml.kernel.org/r/20200623174037.3951353-16-guro@fb.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    UPSTREAM BUG ALERT: crash on boot, do not bisect on it, fixed by later
    patch (“mm: memcg/slab: use a single set of kmem_caches for all
    allocations”)
    
    Jira: LINUX-8152
    Orabug: 32119767
    (cherry picked from commit c7094406fcb7cdf4fe1de8893f0613b75349773d)
    Signed-off-by: Libo Chen <libo.chen@oracle.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Tom Hromatka <tom.hromatka@oracle.com>

--- mm/slab.c
+++ mm/slab.c
@@ -1239,7 +1239,6 @@ void __init kmem_cache_init(void)
 				  nr_node_ids * sizeof(struct kmem_cache_node *),
 				  SLAB_HWCACHE_ALIGN, 0, 0);
 	list_add(&kmem_cache->list, &slab_caches);
-	memcg_link_cache(kmem_cache);
 	slab_state = PARTIAL;
 
 	/*
