d5c6c6eb972b5ba97f92d09de10e6b62fd29a2fb
Christoph Hellwig <hch@lst.de>
2020-08-23 11:10:42 +0200

    block: fix locking for struct block_device size updates
    Two different callers use two different mutexes for updating the
    block device size, which obviously doesn't help to actually protect
    against concurrent updates from the different callers.  In addition
    one of the locks, bd_mutex is rather prone to deadlocks with other
    parts of the block stack that use it for high level synchronization.
    
    Switch to using a new spinlock protecting just the size updates, as
    that is all we need, and make sure everyone does the update through
    the proper helper.
    
    This fixes a bug reported with the nvme revalidating disks during a
    hot removal operation, which can currently deadlock on bd_mutex.
    
    Reported-by: Xianting Tian <xianting_tian@126.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit c2b4bb8cb3741c0bacf3683e4c1ecd04c977ada3)
    
    Conflicts:
    - include/linux/blk_types.h does not contain the definition of struct
      block_device, instead apply changes to include/linux/fs.h
    - block/partitions/core.c: bdev_resize_partition() does not exist in
      UEK6. It was refactored out of blkpg_do_ioctl() (block/ioctl.c) in
      fa9156ae597c244df4e12891dc8329f649970d9a ("block: refactor
      blkpg_ioctl"). Apply the changes to this function instead. The only
      difference is that in this version, "length" is a long long rather
      than a sector_t, so the use of casts was adjusted to match the
      existing code.
    
    Orabug: 33005793
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Reviewed-by: Alan Adamson <alan.adamson@oracle.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -502,6 +502,7 @@ struct block_device {
 	unsigned		bd_part_count;
 	int			bd_invalidated;
 	struct gendisk *	bd_disk;
+	spinlock_t		bd_size_lock; /* for bd_inode->i_size updates */
 	struct request_queue *  bd_queue;
 	struct backing_dev_info *bd_bdi;
 	struct list_head	bd_list;
