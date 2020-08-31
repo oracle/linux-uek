a9060380459767c96b104dec087ca6cbb6088f68
Anushka Singh <anushka.singh1@wipro.com>
2020-08-31 20:55:39 +0530

    Octeon: Fix build error in cvmx-qlm.c
    Fix build error in arch/mips/cavium-octeon/executive/cvmx-qlm.c
    introduced in previous commit: c14a2a449c
    
    Signed-off-by: Anushka Singh <anushka.singh1@wipro.com>
    
    Orabug: 34483890
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- arch/mips/cavium-octeon/executive/cvmx-qlm.c
+++ arch/mips/cavium-octeon/executive/cvmx-qlm.c
@@ -1780,7 +1780,6 @@ enum cvmx_qlm_mode __cvmx_qlm_get_mode_cn73xx(int qlm)
 			if (mux == 2) {
 				qlm_mode[qlm] = CVMX_QLM_MODE_RXAUI_1X2;
 				bgx_cmr_rx_lmacs.s.lmacs = 4;
-			} else
 			}
 			/* fall through */
 		case 0x205:
