071d1471cd09799bbc7fe56780781acabe148007
Naveen Mamindlapalli <naveenm@marvell.com>
2020-09-01 17:45:40 +0530

    octeontx2-af: Added mbox message to retrieve VF's base steering rule
    Unlike kernel VF netdev driver, userspace application drivers support
    installing MCAM rules from VFs as well. This could cause conflicting
    behavior when both PF and VFs install similar rules. To avoid that,
    VF would have to install the traffic rules using the base steering
    rule fields installed by PF as minimum matching creteria. To facilitate
    this requirement, a new mailbox message has been added that returns
    the base steering rule installed by its parent PF when VF requests.
    If there is no base steering rule installed by PF then VF's default
    unicast entry will be returned. It is the responsibility of VF userspace
    driver to do the concatenation of it's rules with base steering rule.
    
    When PF sends this mailbox message then the base channel number
    of the port to which the PF is mapped to, will be returned.
    
    Change-Id: Ie2d8954c4f365d50c72e4b893ae1a021bfb04661
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/35781
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 9c0820cf2871bb0ef1682d745c616581e2989fdd)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
@@ -2949,3 +2949,49 @@ int rvu_mbox_handler_npc_set_pkind(struct rvu *rvu,
 	return rvu_npc_set_parse_mode(rvu, req->hdr.pcifunc, req->mode,
 				      req->dir, req->pkind);
 }
+
+int rvu_mbox_handler_npc_read_base_steer_rule(struct rvu *rvu,
+					      struct msg_req *req,
+					      struct npc_mcam_read_base_rule_rsp *rsp)
+{
+	struct npc_mcam *mcam = &rvu->hw->mcam;
+	int index, blkaddr, nixlf, rc = 0;
+	u16 pcifunc = req->hdr.pcifunc;
+	struct rvu_pfvf *pfvf;
+	u8 intf, enable;
+
+	blkaddr = rvu_get_blkaddr(rvu, BLKTYPE_NPC, 0);
+	if (blkaddr < 0)
+		return NPC_MCAM_INVALID_REQ;
+
+	/* Return the channel number in case of PF */
+	if (!(pcifunc & RVU_PFVF_FUNC_MASK)) {
+		pfvf = rvu_get_pfvf(rvu, pcifunc);
+		rsp->entry.kw[0] = pfvf->rx_chan_base;
+		rsp->entry.kw_mask[0] = 0xFFFULL;
+		goto out;
+	}
+
+	/* Find the pkt steering rule installed by PF to this VF */
+	mutex_lock(&mcam->lock);
+	for (index = 0; index < mcam->bmap_entries; index++) {
+		if (mcam->entry2target_pffunc[index] == pcifunc)
+			goto read_entry;
+	}
+
+	rc = nix_get_nixlf(rvu, pcifunc, &nixlf, NULL);
+	if (rc < 0) {
+		mutex_unlock(&mcam->lock);
+		goto out;
+	}
+	/* Read the default ucast entry if there is no pkt steering rule */
+	index = npc_get_nixlf_mcam_index(mcam, pcifunc, nixlf,
+					 NIXLF_UCAST_ENTRY);
+read_entry:
+	/* Read the mcam entry */
+	npc_read_mcam_entry(rvu, mcam, blkaddr, index, &rsp->entry, &intf,
+			    &enable);
+	mutex_unlock(&mcam->lock);
+out:
+	return rc;
+}
