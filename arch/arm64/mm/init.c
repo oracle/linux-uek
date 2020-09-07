7fc8a3e62df23f3344c2d047e96bdf4e606969cb
Chen Zhou <chenzhou10@huawei.com>
2020-09-07 21:47:41 +0800

    arm64: kdump: introduce some macroes for crash kernel reservation
    Introduce macro CRASH_ALIGN for alignment, macro CRASH_ADDR_LOW_MAX
    for upper bound of low crash memory, macro CRASH_ADDR_HIGH_MAX for
    upper bound of high crash memory, use macroes instead.
    
    Besides, keep consistent with x86, use CRASH_ALIGN as the lower bound
    of crash kernel reservation.
    
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    
    Orabug: 31554906
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -66,7 +66,7 @@ EXPORT_SYMBOL(vmemmap);
  * bit addressable memory area.
  */
 phys_addr_t arm64_dma_phys_limit __ro_after_init;
-static phys_addr_t arm64_dma32_phys_limit __ro_after_init;
+phys_addr_t arm64_dma32_phys_limit __ro_after_init;
 
 #ifdef CONFIG_KEXEC_CORE
 /*
@@ -91,8 +91,8 @@ static void __init reserve_crashkernel(void)
 
 	if (crash_base == 0) {
 		/* Current arm64 boot protocol requires 2MB alignment */
-		crash_base = memblock_find_in_range(0, arm64_dma32_phys_limit,
-				crash_size, SZ_2M);
+		crash_base = memblock_find_in_range(CRASH_ALIGN, CRASH_ADDR_LOW_MAX,
+				crash_size, CRASH_ALIGN);
 		if (crash_base == 0) {
 			pr_warn("cannot allocate crashkernel (size:0x%llx)\n",
 				crash_size);
@@ -110,7 +110,7 @@ static void __init reserve_crashkernel(void)
 			return;
 		}
 
-		if (!IS_ALIGNED(crash_base, SZ_2M)) {
+		if (!IS_ALIGNED(crash_base, CRASH_ALIGN)) {
 			pr_warn("cannot reserve crashkernel: base address is not 2MB aligned\n");
 			return;
 		}
