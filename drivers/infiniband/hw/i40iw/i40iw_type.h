bdcf2efbf95eaf8474c301f0c0a06c6dc4d40d75
Parav Pandit <parav@nvidia.com>
2020-09-14 15:35:28 +0300

    RDMA/i40iw: Avoid typecast from void to pci_dev
    hw_context stores a pci_device pointer. Use the correct type instead of
    void * and avoid typecasts.
    
    Link: https://lore.kernel.org/r/20200914123528.270382-1-parav@mellanox.com
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Acked-by: Shiraz Saleem <shiraz.saleem@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Orabug: 32176425
    
    (cherry picked from commit 1d7c995820277944b7aafca1d05ce0c32775145c)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 1d7c995820277944b7aafca1d05ce0c32775145c
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharon Liu <sharon.s.liu@oracle.com>

--- drivers/infiniband/hw/i40iw/i40iw_type.h
+++ drivers/infiniband/hw/i40iw/i40iw_type.h
@@ -73,6 +73,7 @@ struct i40iw_pd_ops;
 struct i40iw_priv_qp_ops;
 struct i40iw_priv_cq_ops;
 struct i40iw_hmc_ops;
+struct pci_dev;
 
 enum i40iw_page_size {
 	I40IW_PAGE_SIZE_4K,
@@ -256,7 +257,7 @@ struct i40iw_vsi_pestat {
 
 struct i40iw_hw {
 	u8 __iomem *hw_addr;
-	void *dev_context;
+	struct pci_dev *pcidev;
 	struct i40iw_hmc_info hmc;
 };
 
