7703f6f32351f08fba340b1cedaaf575123bab2f
Roi Dayan <roid@nvidia.com>
2020-09-16 10:11:33 +0300

    net/mlx5e: Do not reload ethernet ports when changing eswitch mode
    When switching modes between legacy and switchdev and back, do not
    reload ethernet interfaces. just change the profile from nic profile
    to uplink rep profile in switchdev mode.
    
    Signed-off-by: Roi Dayan <roid@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 33519567
    
    (cherry picked from commit 7a9fb35e8c3a67145fca262c304de65cb2f83abf)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/en/reporter_rx.c
    	drivers/net/ethernet/mellanox/mlx5/core/en/reporter_tx.c
    
    Small conflicts in a context caused by having
    devlink_health_reporter_destroy instead of
    devlink_port_health_reporter_destroy in UEK6.
    
    Since Orabug 33247746, a mlx5_core module parameter
    expose_pf_phys_port_name had been introduced to control the naming
    for the PF interface. See commit b232deb38fa3 ("net/mlx5: Implement
    Oracle-only solution for mlx device names"). As this commit requires
    phys_port_name sysfs attribute to be kept as same across switchdev
    mode change, the expected naming behaviour has to align with this
    requirement.
    
    The new interface naming after this commit (e.g. on OL8) is:
    
    * by default, i.e. on expose_pf_phys_port_name=0 - mlx5 interface names
      are like enp8s0f0 in both legacy and switchdev mode.
    
      Reading of phys_port_name sysfs attribute would result in:
    
               |                       |
               | legacy(non-switchdev) |    switchdev
    -----------+-----------------------+------------------
       SR-IOV  |                       |
               |     -EOPNOTSUPP       |   -EOPNOTSUPP
      Disabled |                       |
    -----------+-----------------------+------------------
       SR-IOV  |                       |
               |     -EOPNOTSUPP       |   -EOPNOTSUPP
      Enabled  |                       |
    
    * on expose_pf_phys_port_name=1 - mlx5 interface names are like
      enp8s0f0np0 in both legacy and switchdev mode, like in upstream.
    
      Reading of phys_port_name sysfs attribute would result in:
    
               |                       |
               | legacy(non-switchdev) |    switchdev
    -----------+-----------------------+------------------
       SR-IOV  |                       |
               |   "p<port-number>"    | "p<port-number>"
      Disabled |                       |
    -----------+-----------------------+------------------
       SR-IOV  |                       |
               |   "p<port-number>"    | "p<port-number>"
      Enabled  |                       |
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Reviewed-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Signed-off-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -5569,6 +5569,11 @@ rollback:
 	return err;
 }
 
+void mlx5e_netdev_attach_nic_profile(struct mlx5e_priv *priv)
+{
+	mlx5e_netdev_change_profile(priv, &mlx5e_nic_profile, NULL);
+}
+
 void mlx5e_destroy_netdev(struct mlx5e_priv *priv)
 {
 	struct net_device *netdev = priv->netdev;
@@ -5676,6 +5681,7 @@ static int mlx5e_probe(struct auxiliary_device *adev,
 	mlx5e_devlink_port_type_eth_set(priv);
 
 	mlx5e_dcbnl_init_app(priv);
+	mlx5_uplink_netdev_set(mdev, netdev);
 	return 0;
 
 err_resume:
