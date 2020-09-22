ac32c4490d85c423bf848da74ae43d75d5ba8374
Eric Snowberg <eric.snowberg@oracle.com>
2020-09-22 17:16:33 -0400

    kexec: Validate pe files against the system_lacklist_keyring
    When Secure Boot is enabled, X.509 certificates in the dbx are loaded
    into the .system_blacklist_keyring.  These certificates are used
    to revoke loading kernel modules but they are not used to revoke
    loading kernels thru kexec.
    
    Add the ability to use certificates in the .system_blacklist_keyring
    to revoke loading a kernel thru kexec.
    
    Orabug: 31961121
    CVE: CVE-2020-26541
    Signed-off-by: Eric Snowberg <eric.snowberg@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: David Edmondson <david.edmondson@oracle.com>
    (cherry picked from commit 084ad8729c9957172c55760019af5cdcfab57736)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- arch/x86/kernel/kexec-bzimage64.c
+++ arch/x86/kernel/kexec-bzimage64.c
@@ -534,6 +534,11 @@ int bzImage64_verify_sig(const char *kernel, unsigned long kernel_len)
 	int ret;
 
 	ret = verify_pefile_signature(kernel, kernel_len,
+				      system_blacklist_keyring, &trusted);
+	if (trusted)
+		return -EKEYREJECTED;
+
+	ret = verify_pefile_signature(kernel, kernel_len,
 				      system_trusted_keyring, &trusted);
 	if (ret < 0)
 		return ret;
