79fafb2c8711a4b4659ebb1608a3a0484e28a898
Sean Christopherson <sean.j.christopherson@intel.com>
2020-09-23 15:14:05 -0700

    KVM: x86/mmu: Move flush logic from mmu_page_zap_pte() to FNAME(invlpg)
    Move the logic that controls whether or not FNAME(invlpg) needs to flush
    fully into FNAME(invlpg) so that mmu_page_zap_pte() doesn't return a
    value.  This allows a future patch to redefine the return semantics for
    mmu_page_zap_pte() so that it can recursively zap orphaned child shadow
    pages for nested TDP MMUs.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20200923221406.16297-2-sean.j.christopherson@intel.com>
    Reviewed-by: Ben Gardon <bgardon@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    [gregory.herrero@oracle.com: Resolve the FNAME(invlpg) conflict by using UEK6's
    page_header(__pa(sptep)) helper instead of upstream's sptep_to_sp().]
    
    (cherry picked from commit ace569e0154a8c7a686bab8e791876d27b42d9b2)
    
    Orabug: 39673871
    
    Signed-off-by: Gregory Herrero <gregory.herrero@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Dongli Zhang <dongli.zhang@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -2783,7 +2783,7 @@ static void validate_direct_spte(struct kvm_vcpu *vcpu, u64 *sptep,
 	}
 }
 
-static bool mmu_page_zap_pte(struct kvm *kvm, struct kvm_mmu_page *sp,
+static void mmu_page_zap_pte(struct kvm *kvm, struct kvm_mmu_page *sp,
 			     u64 *spte)
 {
 	u64 pte;
@@ -2799,13 +2799,9 @@ static bool mmu_page_zap_pte(struct kvm *kvm, struct kvm_mmu_page *sp,
 			child = page_header(pte & PT64_BASE_ADDR_MASK);
 			drop_parent_pte(child, spte);
 		}
-		return true;
-	}
-
-	if (is_mmio_spte(pte))
+	} else if (is_mmio_spte(pte)) {
 		mmu_spte_clear_no_track(spte);
-
-	return false;
+	}
 }
 
 static void kvm_mmu_page_unlink_children(struct kvm *kvm,
