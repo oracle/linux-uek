137c183ae86b812052868eada093614b74fb849b
Avihai Horon <avihaih@nvidia.com>
2020-09-23 19:50:14 +0300

    RDMA/core: Introduce new GID table query API
    Introduce rdma_query_gid_table which enables querying all the GID tables
    of a given device and copying the attributes of all valid GID entries to a
    provided buffer.
    
    This API provides a faster way to query a GID table using single call and
    will be used in libibverbs to improve current approach that requires
    multiple calls to open, close and read multiple sysfs files for a single
    GID table entry.
    
    Link: https://lore.kernel.org/r/20200923165015.2491894-4-leon@kernel.org
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Orabug: 33065697
    
    (cherry picked from commit c4b4d548fabc15b9c5db9f61204dd0c608414d2d)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Signed-off-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    
    Conflicts:
    	include/uapi/rdma/ib_user_ioctl_verbs.h
    
    Conflicts due to minor differences in the context, resolved manually.

--- include/uapi/rdma/ib_user_ioctl_verbs.h
+++ include/uapi/rdma/ib_user_ioctl_verbs.h
@@ -173,4 +173,12 @@ struct ib_uverbs_query_port_resp_ex {
 	__u8  reserved[6];
 };
 
+struct ib_uverbs_gid_entry {
+	__aligned_u64 gid[2];
+	__u32 gid_index;
+	__u32 port_num;
+	__u32 gid_type;
+	__u32 netdev_ifindex; /* It is 0 if there is no netdev associated with it */
+};
+
 #endif
