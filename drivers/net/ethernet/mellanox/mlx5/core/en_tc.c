9658ce8ab43e04d2128cbc1f49b06215a4123f96
Maor Dickman <maord@nvidia.com>
2020-09-30 16:31:11 +0300

    net/mlx5e: Fix modify header actions memory leak
    Modify header actions are allocated during parse tc actions and only
    freed during the flow creation, however, on error flow the allocated
    memory is wrongly unfreed.
    
    Fix this by calling dealloc_mod_hdr_actions in __mlx5e_add_fdb_flow
    and mlx5e_add_nic_flow error flow.
    
    Fixes: d7e75a325cb2 ("net/mlx5e: Add offloading of E-Switch TC pedit (header re-write) actions")
    Fixes: 2f4fe4cab073 ("net/mlx5e: Add offloading of NIC TC pedit (header re-write) actions")
    Signed-off-by: Maor Dickman <maord@nvidia.com>
    Reviewed-by: Paul Blakey <paulb@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 33842440
    
    (cherry picked from commit e68e28b4a9d71261e3f8fd05a72d6cf0b443a493)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: e68e28b4a9d71261e3f8fd05a72d6cf0b443a493
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -4671,6 +4671,7 @@ __mlx5e_add_fdb_flow(struct mlx5e_priv *priv,
 	return flow;
 
 err_free:
+	dealloc_mod_hdr_actions(&parse_attr->mod_hdr_acts);
 	mlx5e_flow_put(priv, flow);
 out:
 	return ERR_PTR(err);
@@ -4815,6 +4816,7 @@ mlx5e_add_nic_flow(struct mlx5e_priv *priv,
 	return 0;
 
 err_free:
+	dealloc_mod_hdr_actions(&parse_attr->mod_hdr_acts);
 	mlx5e_flow_put(priv, flow);
 out:
 	return err;
