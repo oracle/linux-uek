494eb21fb8c60c07466be6501e11bf21cc401fbe
Victor Erminpour <victor.erminpour@oracle.com>
2020-10-01 15:36:24 -0700

    ksplice: add sysctls for determining Ksplice features.
    This adds a new set of sysctls that the Ksplice user-space can use for
    feature tests.  The first sysctl added is ksplice.init_trace_safe which
    means that it is safe to trace a child_subreaper processes without
    making them killable which was fixed in 2d39b3cd34 (signal: protect
    SIGNAL_UNKILLABLE from unintentional clearing.).
    
    Ksplice can now test this sysctl and use that to determine whether it is
    safe to trace init or not.  Future sysctls can be added for other
    features, as well as kernel internal features for kernel Ksplice.
    
    Orabug: 26537364
    
    Signed-off-by: Jamie Iles <jamie.iles@oracle.com>
    Signed-off-by: Victor Erminpour <victor.erminpour@oracle.com>
    
    [Victor: Port to LUCI]
    [Victor: Port to LUCI v5.9]
    [Victor: Port to LUCI v5.17]
    
    Orabug: 29961704
    
    Signed-off-by: Victor Erminpour <victor.erminpour@oracle.com>
    Reviewed-by: John Donnelly  <john.p.donnelly@oracle.com>
    
    [Victor: LUCI port to v5.19]
    
    Orabug: 29961704
    
    Signed-off-by: Victor Erminpour <victor.erminpour@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    
    [Alex: LUCI port to v6.1]
    
    Orabug: 29961704
    
    Signed-off-by: Alex Thorlton <alex.thorlton@oracle.com>
    Reviewed-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    
    [Alex: LUCI port to v6.5]
    
    Orabug: 29961704
    
    Signed-off-by: Alex Thorlton <alex.thorlton@oracle.com>
    Reviewed-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    
    [Julian: Register ksplice sysctls in an __init function.]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>
    
    [Julian: Remove sentinel element from sysctl registration table, fixes warnings due to acc154691fc7]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>

---
+++ include/linux/ksplice.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2017 Oracle Corporation
+ */
+#ifndef __LINUX_KSPLICE_H__
+#define __LINUX_KSPLICE_H__
+
+#include <linux/sysctl.h>
+
+extern struct ctl_table ksplice_sysctls[];
+
+#endif /* __LINUX_KSPLICE_H__ */
