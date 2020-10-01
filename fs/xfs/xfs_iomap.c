f83e4de18f62c26a51695b4136a3bc74f74bc761
Eric Sandeen <sandeen@sandeen.net>
2020-10-01 08:34:48 -0500

    xfs: trim IO to found COW extent limit
    A bug existed in the XFS reflink code between v5.1 and v5.5 in which
    the mapping for a COW IO was not trimmed to the mapping of the COW
    extent that was found.  This resulted in a too-short copy, and
    corruption of other files which shared the original extent.
    
    (This happened only when extent size hints were set, which bypasses
    delalloc and led to this code path.)
    
    This was (inadvertently) fixed upstream with
    
    36adcbace24e "xfs: fill out the srcmap in iomap_begin"
    
    and related patches which moved lots of this functionality to
    the iomap subsystem.
    
    Hence, this is a -stable only patch, targeted to fix this
    corruption vector without other major code changes.
    
    Orabug: 34765284
    
    Fixes: 78f0cc9d55cb ("xfs: don't use delalloc extents for COW on files with extsize hints")
    Cc: <stable@vger.kernel.org> # 5.4.x
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit aee38af574a136425d89cf4760ece0e0c2fe0fb3)
    Signed-off-by: Wengang Wang <wen.gang.wang@oracle.com>
    Reviewed-by: Junxiao Bi <junxiao.bi@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -1044,13 +1044,20 @@ xfs_file_iomap_begin(
 		/*
 		 * For buffered writes we need to report the address of the
 		 * previous block (if there was any) so that the higher level
-		 * write code can perform read-modify-write operations.  For
-		 * direct I/O code, which must be block aligned we need to
-		 * report the newly allocated address.
+		 * write code can perform read-modify-write operations; we
+		 * won't need the CoW fork mapping until writeback.  For direct
+		 * I/O, which must be block aligned, we need to report the
+		 * newly allocated address.  If the data fork has a hole, copy
+		 * the COW fork mapping to avoid allocating to the data fork.
+		 *
+		 * Otherwise, ensure that the imap range does not extend past
+		 * the range allocated/found in CoW fork.
 		 */
 		if (!(flags & IOMAP_DIRECT) &&
-		    orig.br_startblock != HOLESTARTBLOCK)
+		    orig.br_startblock != HOLESTARTBLOCK) {
+			xfs_trim_extent(&orig, imap.br_startoff, imap.br_blockcount);
 			imap = orig;
+		}
 
 		end_fsb = imap.br_startoff + imap.br_blockcount;
 		length = XFS_FSB_TO_B(mp, end_fsb) - offset;
