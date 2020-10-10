517c4fbab7a779cb3198512060ed89153bdde776
Leon Romanovsky <leonro@nvidia.com>
2020-10-10 11:57:26 +0300

    net/mlx5: Simplify eswitch mode check
    Provide mlx5_core device instead of "priv" pointer while checking
    eswith mode.
    
    Reviewed-by: Roi Dayan <roid@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    
    Remove is_mdev_switchdev_mode from drivers/infiniband/hw/mlx5/main.c that
    was done in upstream commit 64825827ae3a ("RDMA/mlx5: Separate counters
    from main.c").
    
    Orabug: 32879638
    
    (cherry picked from commit e87114022e1de734de0552e6b4f2dc5309efa27a)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/infiniband/hw/mlx5/ib_rep.h
    	drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
    	include/linux/mlx5/eswitch.h
    
    Conflicts arose due to missing code. Chunks in en_tc.c were skipped as NA
    due to missing upstream commit aedd133d17bc ("net/mlx5e: Support CT
    offload for tc nic flows").
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Reviewed-by: Si-Wei Liu <si-wei.liu@oracle.com>
    (cherry picked from commit eec0d4299614e9d5420839d3bfe50a9aca0589d3)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -3094,7 +3094,7 @@ static void mlx5e_modify_admin_state(struct mlx5_core_dev *mdev,
 
 	mlx5_set_port_admin_status(mdev, state);
 
-	if (!MLX5_ESWITCH_MANAGER(mdev) ||  mlx5_eswitch_mode(esw) == MLX5_ESWITCH_OFFLOADS ||
+	if (mlx5_eswitch_mode(mdev) == MLX5_ESWITCH_OFFLOADS ||
 	    !MLX5_CAP_GEN(mdev, uplink_follow))
 		return;
 
