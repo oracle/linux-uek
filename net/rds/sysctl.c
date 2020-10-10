0a297b1cb9416cd091b2787ed86bd105cf0d1202
Gerd Rausch <gerd.rausch@oracle.com>
2020-10-10 12:32:40 -0700

    net/rds: Delay reconnects from passive side by 3 seconds
    This is expected to cut the MAD/CM load by half.
    We know that the active side will insist on the right-of-way,
    and therefore the passive side has no chance of success.
    Unless the active side doesn't retry (e.g. after a reboot).
    
    Orabug: 32373816
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Ka-cheong Poon <ka-cheong.poon@oracle.com>
    (cherry picked from commit 56099375ce5b4523fa11e16a1cff32df67db302e)
    Port to U3
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 2610bf0ec94d395f1ee45b97f6993db6f0616d89)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 702db6c84ce6565bb7a16217ffdc08fad50843d2)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/sysctl.c
+++ net/rds/sysctl.c
@@ -43,6 +43,7 @@ static unsigned long rds_sysctl_reconnect_max = ~0UL;
 
 unsigned long rds_sysctl_reconnect_min_jiffies;
 unsigned long rds_sysctl_reconnect_max_jiffies = HZ;
+unsigned long rds_sysctl_reconnect_passive_min_jiffies;
 
 unsigned int  rds_sysctl_max_unacked_packets = 8;
 unsigned int  rds_sysctl_max_unacked_bytes = (16 << 20);
@@ -98,6 +99,13 @@ static struct ctl_table rds_sysctl_rds_table[] = {
 		.proc_handler   = proc_doulongvec_ms_jiffies_minmax,
 		.extra1		= &rds_sysctl_reconnect_min_jiffies,
 		.extra2		= &rds_sysctl_reconnect_max,
+	},
+	{
+		.procname       = "reconnect_passive_min_delay_ms",
+		.data		= &rds_sysctl_reconnect_passive_min_jiffies,
+		.maxlen         = sizeof(unsigned long),
+		.mode           = 0644,
+		.proc_handler   = proc_doulongvec_ms_jiffies_minmax,
 	},
 	{
 		.procname       = "pf_rds",
@@ -187,6 +195,7 @@ int rds_sysctl_init(void)
 {
 	rds_sysctl_reconnect_min = msecs_to_jiffies(1);
 	rds_sysctl_reconnect_min_jiffies = rds_sysctl_reconnect_min;
+	rds_sysctl_reconnect_passive_min_jiffies = msecs_to_jiffies(3000);
 
 	rds_sysctl_reg_table = register_net_sysctl(&init_net, "net/rds", rds_sysctl_rds_table);
 	if (!rds_sysctl_reg_table)
