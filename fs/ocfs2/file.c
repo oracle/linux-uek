448b4f3108567476e4ae6df642f9a8bebf7b245a
John Donnelly <john.p.donnelly@oracle.com>
2020-10-13 04:14:03 -0700

    ocfs2: remove unused ocfs2_prepare_inode_for_refcount()
    Orabug: 32007790
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- fs/ocfs2/file.c
+++ fs/ocfs2/file.c
@@ -2104,33 +2104,6 @@ static int ocfs2_is_io_unaligned(struct inode *inode, size_t count, loff_t pos)
 	return 0;
 }
 
-static int ocfs2_prepare_inode_for_refcount(struct inode *inode,
-					    struct file *file,
-					    loff_t pos, size_t count,
-					    int *meta_level)
-{
-	int ret;
-	struct buffer_head *di_bh = NULL;
-	u32 cpos = pos >> OCFS2_SB(inode->i_sb)->s_clustersize_bits;
-	u32 clusters =
-		ocfs2_clusters_for_bytes(inode->i_sb, pos + count) - cpos;
-
-	ret = ocfs2_inode_lock(inode, &di_bh, 1);
-	if (ret) {
-		mlog_errno(ret);
-		goto out;
-	}
-
-	*meta_level = 1;
-
-	ret = ocfs2_refcount_cow(inode, di_bh, cpos, clusters, UINT_MAX);
-	if (ret)
-		mlog_errno(ret);
-out:
-	brelse(di_bh);
-	return ret;
-}
-
 static int ocfs2_prepare_inode_for_write(struct file *file,
 					 loff_t pos,
 					 size_t count)
