9a425e2a2d35597665e9a98aeed68537b50019e4
Håkon Bugge <haakon.bugge@oracle.com>
2020-10-14 13:50:21 +0200

    Revert "UEK6 compiler warning for /net/rds/send.c"
    This reverts commit 6ea14730c595a7ec2897856e8cec72dcdcd1dc41.
    
    Commit ad76fa64554d ("RDS: Drop the connection as part of cancel to
    avoid hangs") was a mitigation for the fix in commit c947b62005de
    ("net/rds: Fix endless RNR situation"). Hence, revert ad76fa64554d
    ("RDS: Drop the connection as part of cancel to avoid hangs") and its
    descendants.
    
    Orabug: 31505749
    
    Fixes: ad76fa64554d ("RDS: Drop the connection as part of cancel to avoid hangs")
    Tested-by: Roopa Agrawal <roopa.agrawal@oracle.com>
    Tested-by: Jun Yang <jun.yang@oracle.com>
    Tested-by: Hong Liu <hong.x.liu@oracle.com>
    Reviewed-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 8df45138fa5051aa1536c7d4ce862f5ee1be584f)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit ce055fac796da7bef6b5ec0600c0f5a4c18e70db)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/send.c
+++ net/rds/send.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2006, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -893,9 +893,7 @@ void rds_send_drop_to(struct rds_sock *rs, struct sockaddr_in6 *dest)
 	struct rds_message *rm, *tmp;
 	struct rds_connection *conn = NULL;
 	struct rds_conn_path *cp;
-	enum {
-		nbits = BIT(BITS_PER_BYTE * sizeof(conn->c_tos)),
-	};
+	unsigned int nbits = BIT(BITS_PER_BYTE * sizeof(conn->c_tos));
 	DECLARE_BITMAP(conn_dropped, nbits);
 	unsigned long flags;
 	LIST_HEAD(list);
