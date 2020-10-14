8b8e9d1477ca7a8e902c1a1fdf1eddd773393833
Ankur Arora <ankur.a.arora@oracle.com>
2020-10-14 01:32:56 -0700

    x86/clear_page: add clear_page_uncached()
    Define clear_page_uncached() as an alternative_call() to clear_page_nt()
    if the CPU sets X86_FEATURE_NT_GOOD and fallback to clear_page() if it
    doesn't.
    
    Similarly define clear_page_uncached_flush() which provides an SFENCE
    if the CPU sets X86_FEATURE_NT_GOOD.
    
    Also, add the glue interface clear_user_highpage_uncached().
    
    Orabug: 32124427
    
    Conflicts:
       Context conflict because commit 0d3db1f14abb "x86/alternatives,
       kvm: Fix a couple of CALLs without a frame pointer" re-did the
       alternatives_call to pass the current_stack_pointer.
    
       So, instead of using "0" to force the input value of the page pointer
       in the same register as the output value (which uses "=D") explicitly
       specify the input constraint as "D".
    
       This is same as the resolution chosen in clear_page() in the listed
       commit.
    
       Also add the missing kmsan annotation that cam in via b073d7f8aee4
       "mm: kmsan: maintain KMSAN metadata for page operations"
    
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- arch/x86/include/asm/page.h
+++ arch/x86/include/asm/page.h
@@ -28,6 +28,12 @@ static inline void clear_user_page(void *page, unsigned long vaddr,
 	clear_page(page);
 }
 
+static inline void clear_user_page_uncached(void *page, unsigned long vaddr,
+					    struct page *pg)
+{
+	clear_page_uncached(page);
+}
+
 static inline void copy_user_page(void *to, void *from, unsigned long vaddr,
 				  struct page *topage)
 {
