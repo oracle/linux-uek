9a4b8fe7fd715b473f91435901528405d6e697f6
Ankur Arora <ankur.a.arora@oracle.com>
2020-10-14 01:32:54 -0700

    perf bench: add memset_movnti()
    Clone memset_movnti() from arch/x86/lib/memset_64.S.
    
    perf bench mem memset on -f x86-64-movnt on Intel Broadwellx, Skylakex
    and AMD Rome:
    
    Intel Broadwellx:
    $ for i in 2 8 32 128 512; do
    	perf bench mem memset -f x86-64-movnt -s ${i}MB
      done
    
      # Output pruned.
      # Running 'mem/memset' benchmark:
      # function 'x86-64-movnt' (movnt-based memset() in arch/x86/lib/memset_64.S)
      # Copying 2MB bytes ...
            11.837121 GB/sec
      # Copying 8MB bytes ...
            11.783560 GB/sec
      # Copying 32MB bytes ...
            11.868591 GB/sec
      # Copying 128MB bytes ...
            11.865211 GB/sec
      # Copying 512MB bytes ...
            11.864085 GB/sec
    
    Intel Skylakex:
    $ for i in 2 8 32 128 512; do
    	perf bench mem memset -f x86-64-movnt -s ${i}MB
      done
      # Running 'mem/memset' benchmark:
      # function 'x86-64-movnt' (movnt-based memset() in arch/x86/lib/memset_64.S)
      # Copying 2MB bytes ...
             6.361971 GB/sec
      # Copying 8MB bytes ...
             6.300403 GB/sec
      # Copying 32MB bytes ...
             6.288992 GB/sec
      # Copying 128MB bytes ...
             6.328793 GB/sec
      # Copying 512MB bytes ...
             6.324471 GB/sec
    
    AMD Rome:
    $ for i in 2 8 32 128 512; do
    	perf bench mem memset -f x86-64-movnt -s ${i}MB
      done
      # Running 'mem/memset' benchmark:
      # function 'x86-64-movnt' (movnt-based memset() in arch/x86/lib/memset_64.S)
      # Copying 2MB bytes ...
            10.993199 GB/sec
      # Copying 8MB bytes ...
            14.221784 GB/sec
      # Copying 32MB bytes ...
            14.293337 GB/sec
      # Copying 128MB bytes ...
            15.238947 GB/sec
      # Copying 512MB bytes ...
            16.476093 GB/sec
    
    Orabug: 32124427
    
    Conflict:
      tools/arch/x86/lib/memset_64.S
    
      Switch to SYM_FUNC_START_LOCAL entry/exit macros.
    
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- tools/perf/bench/mem-memset-x86-64-asm-def.h
+++ tools/perf/bench/mem-memset-x86-64-asm-def.h
@@ -1,9 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-MEMSET_FN(memset_orig,
+MEMSET_FN(memset_movq,
 	"x86-64-unrolled",
 	"unrolled memset() in arch/x86/lib/memset_64.S")
 
 MEMSET_FN(__memset,
 	"x86-64-stosq",
 	"movsq-based memset() in arch/x86/lib/memset_64.S")
+
+MEMSET_FN(memset_movnti,
+	"x86-64-movnt",
+	"movnt-based memset() in arch/x86/lib/memset_64.S")
