b2aa064f0b80376248b0bb2d1e4d9067d9e5a0d3
Ankur Arora <ankur.a.arora@oracle.com>
2020-10-14 01:32:57 -0700

    mm, clear_huge_page: use clear_page_uncached() for gigantic pages
    Uncached writes are suitable for circumstances where the region written to
    is not expected to be read again soon, or the region written to is large
    enough that there's no expectation that we will find the writes in the
    cache.
    
    Accordingly switch to using clear_page_uncached() for gigantic pages.
    
    Orabug: 32124427
    
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- mm/memory.c
+++ mm/memory.c
@@ -6309,7 +6309,7 @@ static void clear_gigantic_page(struct page *page,
 	for (i = 0; i < pages_per_huge_page; i++) {
 		p = nth_page(page, i);
 		cond_resched();
-		clear_user_highpage(p, addr + i * PAGE_SIZE);
+		clear_user_highpage_uncached(p, addr + i * PAGE_SIZE);
 	}
 }
 
@@ -6329,6 +6329,7 @@ void clear_huge_page(struct page *page,
 
 	if (unlikely(pages_per_huge_page > MAX_ORDER_NR_PAGES)) {
 		clear_gigantic_page(page, addr, pages_per_huge_page);
+		clear_page_uncached_flush();
 		return;
 	}
 
