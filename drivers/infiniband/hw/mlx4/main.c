3cf01bfea4fa57c29d69023e5ce0ceb8dbc106c8
aru kolappan <aru.kolappan@oracle.com>
2020-10-20 17:30:34 -0700

    IB/mlx4: disable CQ time stamping
    When CQ time stamping feature is enabled, it inadvertently exposes
    entire 4K block from the device which has some sensitive addresses
    for the HCA including a semaphore. When one of this address is
    read by a userspace process, it locks semaphore in the HCA and
    halts command processing. This causes commands to timeout, and
    results in a system crash. For now, since we do not use time
    stamping feature we will disable it to protect HCA from getting
    locked. Mellanox is working upstream to fix this in a different
    way. Once that change is done we will revisit this patch.
    
    Orabug: 32042517
    
    Proposed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Hakon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Srinivas Eeda <srinivas.eeda@oracle.com>
    Reviewed-by: Rama Nichanamatlu <rama.nichanamatlu@oracle.com>
    Signed-off-by: aru kolappan <aru.kolappan@oracle.com>

--- drivers/infiniband/hw/mlx4/main.c
+++ drivers/infiniband/hw/mlx4/main.c
@@ -582,10 +582,8 @@ static int mlx4_ib_query_device(struct ib_device *ibdev,
 
 	if (uhw->outlen >= resp.response_length + sizeof(resp.hca_core_clock_offset)) {
 		resp.response_length += sizeof(resp.hca_core_clock_offset);
-		if (!err && !mlx4_is_slave(dev->dev)) {
-			resp.comp_mask |= MLX4_IB_QUERY_DEV_RESP_MASK_CORE_CLOCK_OFFSET;
+		if (!err && !mlx4_is_slave(dev->dev))
 			resp.hca_core_clock_offset = clock_params.offset % PAGE_SIZE;
-		}
 	}
 
 	if (uhw->outlen >= resp.response_length +
