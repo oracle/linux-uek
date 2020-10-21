677ec88895a5aa0d9883d5118c8c5aa8a2550e1d
John Donnelly <john.p.donnelly@oracle.com>
2020-10-21 05:40:33 -0700

    qed: Corrects warning: 'qed_iwarp_ll2_slowpath' defined but not used
    Fixes: 3ffa9a222a6a3e ("qed: fix build breakage")
    
    Orabug: 32052282
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/net/ethernet/qlogic/qed/qed_iwarp.c
+++ drivers/net/ethernet/qlogic/qed/qed_iwarp.c
@@ -2508,29 +2508,6 @@ static void qed_iwarp_ll2_rel_tx_pkt(void *cxt, u8 connection_handle,
 	kfree(buffer);
 }
 
-/* The only slowpath for iwarp ll2 is unalign flush. When this completion
- * is received, need to reset the FPDU.
- */
-static void
-qed_iwarp_ll2_slowpath(void *cxt,
-		       u8 connection_handle,
-		       u32 opaque_data_0, u32 opaque_data_1)
-{
-	struct unaligned_opaque_data unalign_data;
-	struct qed_hwfn *p_hwfn = cxt;
-	struct qed_iwarp_fpdu *fpdu;
-
-	qed_iwarp_mpa_get_data(p_hwfn, &unalign_data,
-			       opaque_data_0, opaque_data_1);
-
-	DP_VERBOSE(p_hwfn, QED_MSG_RDMA, "(0x%x) Flush fpdu\n",
-		   unalign_data.cid);
-
-	fpdu = qed_iwarp_get_curr_fpdu(p_hwfn, (u16)unalign_data.cid);
-	if (fpdu)
-		memset(fpdu, 0, sizeof(*fpdu));
-}
-
 static int qed_iwarp_ll2_stop(struct qed_hwfn *p_hwfn, struct qed_ptt *p_ptt)
 {
 	struct qed_iwarp_info *iwarp_info = &p_hwfn->p_rdma_info->iwarp;
