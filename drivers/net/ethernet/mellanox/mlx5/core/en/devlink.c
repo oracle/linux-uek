734d8e56093b35114c331b04d4256ef79adde7c2
Roi Dayan <roid@nvidia.com>
2020-10-21 10:28:50 +0300

    net/mlx5e: Register nic devlink port with switch id
    We will re-use the native NIC port net device instance for the Uplink
    representor. Since the netdev will be kept registered while we engage
    switchdev mode also the devlink will be kept registered.
    Register the nic devlink port with switch id so it will be available
    when changing profiles.
    
    Signed-off-by: Roi Dayan <roid@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 32176425
    
    (cherry picked from commit 5a65d85dc7f4532755750aab645c46a47219fd1a)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/en/devlink.c
    	drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
    
    Reconciled API usages in mlx5e_devlink_port_register():
    	devlink_port_attrs_set()
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/devlink.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/devlink.c
@@ -2,23 +2,42 @@
 /* Copyright (c) 2020, Mellanox Technologies inc.  All rights reserved. */
 
 #include "en/devlink.h"
+#include "eswitch.h"
+
+static void
+mlx5e_devlink_get_port_parent_id(struct mlx5_core_dev *dev, struct netdev_phys_item_id *ppid)
+{
+	u64 parent_id;
+
+	parent_id = mlx5_query_nic_system_image_guid(dev);
+	ppid->id_len = sizeof(parent_id);
+	memcpy(ppid->id, &parent_id, sizeof(parent_id));
+}
 
 int mlx5e_devlink_port_register(struct mlx5e_priv *priv)
 {
 	struct devlink *devlink = priv_to_devlink(priv->mdev);
+	struct netdev_phys_item_id ppid = {};
+	unsigned int dl_port_index;
 
-	if (mlx5_core_is_pf(priv->mdev))
+	if (mlx5_core_is_pf(priv->mdev)) {
+		if (MLX5_ESWITCH_MANAGER(priv->mdev))
+			mlx5e_devlink_get_port_parent_id(priv->mdev, &ppid);
+		dl_port_index = mlx5_esw_vport_to_devlink_port_index(priv->mdev,
+								     MLX5_VPORT_UPLINK);
 		devlink_port_attrs_set(&priv->dl_port,
 				       DEVLINK_PORT_FLAVOUR_PHYSICAL,
 				       PCI_FUNC(priv->mdev->pdev->devfn),
 				       false, 0,
-				       NULL, 0);
-	else
+				       &ppid.id[0], ppid.id_len);
+	} else {
+		dl_port_index = mlx5_esw_vport_to_devlink_port_index(priv->mdev, 0);
 		devlink_port_attrs_set(&priv->dl_port,
 				       DEVLINK_PORT_FLAVOUR_VIRTUAL,
 				       0, false, 0, NULL, 0);
+	}
 
-	return devlink_port_register(devlink, &priv->dl_port, 1);
+	return devlink_port_register(devlink, &priv->dl_port, dl_port_index);
 }
 
 void mlx5e_devlink_port_type_eth_set(struct mlx5e_priv *priv)
