3ba6a6d1cf4fedc238b866370b69314be6dc1a41
Steve Sistare <steven.sistare@oracle.com>
2020-10-27 16:46:45 -0700

    mm: introduce VM_EXEC_KEEP
    A vma with the VM_EXEC_KEEP flag is preserved across exec.  The flag
    is supported for anonymous vmas only and is cleared automatically after
    exec and for forked child processes.  For safety, overlap with fixed
    address VMAs created in the new mm during exec (e.g. the stack and elf
    load segments) is not permitted and will cause the exec to fail.  The
    possibility of a collision can be made extremely small by only applying
    the flag to VMAs representing ranges mapped with system-selected
    addresses in the mmap region.
    
    Orabug: 32387875
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>
    Signed-off-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Reviewed-by: Vishal Moola <vishal.moola@oracle.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2011,6 +2011,80 @@ int insert_vm_struct(struct mm_struct *mm, struct vm_area_struct *vma)
 }
 
 /*
+ * Duplicate an anonymous VMA into a new mm as part of preserving
+ * it across exec.
+ */
+int vma_dup(struct vm_area_struct *old_vma, struct mm_struct *mm)
+{
+	struct vm_area_struct *vma;
+	unsigned long npages;
+	int ret = -ENOMEM;
+
+	if (WARN_ON(!vma_is_anonymous(old_vma)))
+		return -EINVAL;
+
+	if (find_vma_intersection(mm, old_vma->vm_start, old_vma->vm_end))
+		return -EEXIST;
+
+	npages = vma_pages(old_vma);
+	vm_stat_account(mm, old_vma->vm_flags, npages);
+
+	vma = vm_area_dup(old_vma);
+	if (!vma)
+		goto fail_nomem;
+
+	ret = vma_dup_policy(old_vma, vma);
+	if (ret)
+		goto fail_nomem_policy;
+
+	vma->vm_mm = mm;
+	ret = anon_vma_fork(vma, old_vma);
+	if (ret)
+		goto fail_nomem_anon_vma_fork;
+
+	/*
+	 * Clear functionality that should not carry over to the new
+	 * process. Note that VM_EXEC_KEEP is cleared later to allow
+	 * code called by copy_page_range to infer that the copying is
+	 * for preserving over exec and not for process forking.
+	 */
+	vm_flags_clear(vma, VM_LOCKED|VM_LOCKONFAULT|VM_UFFD_MISSING|VM_UFFD_WP|VM_UFFD_MINOR);
+	vma->vm_userfaultfd_ctx = NULL_VM_UFFD_CTX;
+
+	ret = vma_link(mm, vma);
+	if (ret)
+		goto fail_nomem_vma_link;
+
+	/*
+	 * Now that the dup vma is inserted into the mm, clear VM_ACCOUNT
+	 * from old_vma.  Since vma_dup() is only called during exec to
+	 * duplicate a vma from the outgoing mm into the mm of the new
+	 * process, this effectively transfers the accounting from the old
+	 * vma to new one.
+	 * exec holds the mmap read lock on the outgoing mm at this point
+	 * so __vm_flags_mod() is used here to avoid an assert that the
+	 * mmap write lock is held which is not needed in this instance..
+	 */
+	__vm_flags_mod(old_vma, 0, VM_ACCOUNT);
+
+	ret = copy_page_range(vma, old_vma);
+
+	vm_flags_clear(vma, VM_EXEC_KEEP);
+
+	return ret;
+
+fail_nomem_vma_link:
+	unlink_anon_vmas(vma);
+fail_nomem_anon_vma_fork:
+	mpol_put(vma_policy(vma));
+fail_nomem_policy:
+	vm_area_free(vma);
+fail_nomem:
+	vm_stat_account(mm, old_vma->vm_flags, -npages);
+	return -ENOMEM;
+}
+
+/*
  * Return true if the calling process may expand its vm space by the passed
  * number of pages
  */
