0ac50e991d1df50007f2a9ea68e992ebf8c445d5
Steve Sistare <steven.sistare@oracle.com>
2020-10-27 16:48:37 -0700

    mm: madv_doexec_flag sysctl
    Export the value of MADV_DOEXEC in /proc/sys/vm/madv_doexec_flag in case
    it must change due to a conflict with upstream.  Applications must use the
    sysctl until the flag is upstreamed, and UEK will provide the sysctl until
    the apps are updated to use the final value.
    
    Orabug: 32387875
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3918,6 +3918,7 @@ extern bool process_shares_mm(struct task_struct *p, struct mm_struct *mm);
 extern int sysctl_drop_caches;
 int drop_caches_sysctl_handler(struct ctl_table *, int, void *, size_t *,
 		loff_t *);
+extern int madv_doexec_flag;
 #endif
 
 void drop_slab(void);
