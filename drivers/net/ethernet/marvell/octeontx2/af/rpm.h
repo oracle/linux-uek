c81fbf8950451122e31c90f2c8215ee796ab5d38
Geetha sowjanya <gakula@marvell.com>
2020-11-16 15:47:33 +0530

    octeontx2-pf: cn10k: Update Marvell license header
    This patch updates the license header in all cn10k files.
    
    Change-Id: Ifdad63edf8c821f1c7234ea8069c6394cdf88660
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/40128
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit bf82475e8bd00f703918318f1de53b9c91f7c272)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rpm.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rpm.h
@@ -1,11 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*  Marvell OcteonTx2 RPM driver
  *
- * Copyright (C) 2020 Marvell International Ltd.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * Copyright (C) 2020 Marvell.
  */
 
 #ifndef RPM_H
