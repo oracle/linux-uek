4062e3786663d55d243c14720e52c8afacd841ff
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2020-11-17 20:49:52 +0800

    EDAC: Add DDR5 new memory type
    Add a new entry to 'enum mem_type' and a new string to
    'edac_mem_types[]' for DDR5 new memory type.
    
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    
    (cherry picked from commit bc1c99a5971aa7571e8b9731c28fa32abe12cab8)
    
    JIRA: LINUX-9929
    Orabug: 32367830
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- include/linux/edac.h
+++ include/linux/edac.h
@@ -189,6 +189,7 @@ static inline char *mc_event_error_type(const unsigned int err_type)
  *			This is a variant of the DDR4 memories.
  * @MEM_LRDDR4:		Load-Reduced DDR4 memory.
  * @MEM_LPDDR4:		Low-Power DDR4 memory.
+ * @MEM_DDR5:		Unbuffered DDR5 RAM
  * @MEM_NVDIMM:		Non-volatile RAM
  * @MEM_WIO2:		Wide I/O 2.
  */
@@ -216,6 +217,7 @@ enum mem_type {
 	MEM_RDDR4,
 	MEM_LRDDR4,
 	MEM_LPDDR4,
+	MEM_DDR5,
 	MEM_NVDIMM,
 	MEM_WIO2,
 };
@@ -242,6 +244,7 @@ enum mem_type {
 #define MEM_FLAG_RDDR4          BIT(MEM_RDDR4)
 #define MEM_FLAG_LRDDR4         BIT(MEM_LRDDR4)
 #define MEM_FLAG_LPDDR4         BIT(MEM_LPDDR4)
+#define MEM_FLAG_DDR5           BIT(MEM_DDR5)
 #define MEM_FLAG_NVDIMM         BIT(MEM_NVDIMM)
 #define MEM_FLAG_WIO2		BIT(MEM_WIO2)
 
