fdcbd49d7bbcc2bfbde786c23f30202044d7d45f
Karan Tilak Kumar <kartilak@cisco.com>
2020-11-20 14:07:12 -0800

    scsi: fnic: Change shost_printk() to FNIC_FCS_DBG()
    Replacing shost_printk with FNIC_FCS_DBG() so that these log messages are
    controlled by fnic_log_level flag in fnic_fip_handler_timer.
    
    Bumping up version number from 47 to 49 to maintain same level as internal
    version.
    
    Link: https://lore.kernel.org/r/20201120220712.16708-1-kartilak@cisco.com
    Reviewed-by: Arulprabhu Ponnusamy <arulponn@cisco.com>
    Co-developed-by: Satish Kharat <satishkh@cisco.com>
    Signed-off-by: Satish Kharat <satishkh@cisco.com>
    Signed-off-by: Karan Tilak Kumar <kartilak@cisco.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 90b3a938031fd2da2000f84e58223971ad8f6df3)
    
    Orabug: 32625115
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/scsi/fnic/fnic_fcs.c
+++ drivers/scsi/fnic/fnic_fcs.c
@@ -1354,7 +1354,7 @@ void fnic_handle_fip_timer(struct fnic *fnic)
 	}
 
 	vlan = list_first_entry(&fnic->vlans, struct fcoe_vlan, list);
-	shost_printk(KERN_DEBUG, fnic->lport->host,
+	FNIC_FCS_DBG(KERN_DEBUG, fnic->lport->host,
 		  "fip_timer: vlan %d state %d sol_count %d\n",
 		  vlan->vid, vlan->state, vlan->sol_count);
 	switch (vlan->state) {
@@ -1377,7 +1377,7 @@ void fnic_handle_fip_timer(struct fnic *fnic)
 			 * no response on this vlan, remove  from the list.
 			 * Try the next vlan
 			 */
-			shost_printk(KERN_INFO, fnic->lport->host,
+			FNIC_FCS_DBG(KERN_INFO, fnic->lport->host,
 				  "Dequeue this VLAN ID %d from list\n",
 				  vlan->vid);
 			list_del(&vlan->list);
@@ -1387,7 +1387,7 @@ void fnic_handle_fip_timer(struct fnic *fnic)
 				/* we exhausted all vlans, restart vlan disc */
 				spin_unlock_irqrestore(&fnic->vlans_lock,
 							flags);
-				shost_printk(KERN_INFO, fnic->lport->host,
+				FNIC_FCS_DBG(KERN_INFO, fnic->lport->host,
 					  "fip_timer: vlan list empty, "
 					  "trigger vlan disc\n");
 				fnic_event_enq(fnic, FNIC_EVT_START_VLAN_DISC);
