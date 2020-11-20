303b4e0e518c38325750bd5ef77c21b21a9e36af
Ka-Cheong Poon <ka-cheong.poon@oracle.com>
2020-11-19 20:24:25 -0800

    net/rds: rds_ib_remove_one() accesses freed memory
    rds_ib_dev_free() calls complete(rds_ibdev->rid_dev_rem_complete) and
    then kfree(rds_ibdev).  If this kfree() is finished before the
    wait_for_completion(&rds_ibdev->rid_dev_rem_complete) completes in
    rds_ib_remove_one(), wait_for_completion() code can access freed
    memory.
    
    Orabug: 31973422
    
    Fixes: e233963403f6 ("net/rds: Spread out the rds_rdma module shutdown work")
    Signed-off-by: Ka-Cheong Poon <ka-cheong.poon@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit d438ecb0948f87cac9de33673314c4dff87b1e36)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 780a13a16122b88a23e02e93c9bc51b842f3ca1f)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -429,7 +429,7 @@ struct rds_ib_device {
 	bool			rid_dev_rem;
 
 	struct work_struct	rid_dev_rem_work;
-	struct completion	rid_dev_rem_complete;
+	struct completion	*rid_dev_rem_complete;
 };
 
 static inline int ibdev_to_node(struct ib_device *ibdev)
