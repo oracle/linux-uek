79d3c90e67cb58a0fe3627ca7539bbfd23ccd88a
Alan Maguire <alan.maguire@oracle.com>
2020-11-20 14:55:27 +0100

    rds: fix out-of-tree build broken by tracepoints
    Out-of-tree builds of RDS need to work along with in-kernel builds.
    Tracepoint definitions in include/trace/events/rds.h need to be
    moved into net/rds.  TRACE_INCLUDE_PATH needs to be adjusted
    and cflags need to add current path to include paths searched
    so that include/trace/define_trace.h #include of RDS trace.h
    finds it in net/rds.
    
    Orabug: 32180549
    Reported-by: Haakon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- net/rds/connection.c
+++ net/rds/connection.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2020 Oracle and/or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -38,7 +38,7 @@
 #include "rds.h"
 #include "loop.h"
 
-#include <trace/events/rds.h>
+#include "trace.h"
 
 #define RDS_CONNECTION_HASH_BITS 12
 #define RDS_CONNECTION_HASH_ENTRIES (1 << RDS_CONNECTION_HASH_BITS)
