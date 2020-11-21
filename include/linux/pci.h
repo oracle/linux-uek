df66d1cb64c2a390461ba4a5195dac18d08c7130
Sean V Kelley <sean.v.kelley@intel.com>
2020-11-20 16:10:24 -0800

    PCI/ERR: Cache RCEC EA Capability offset in pci_init_capabilities()
    Extend support for Root Complex Event Collectors by decoding and caching
    the RCEC Endpoint Association Extended Capabilities when enumerating. Use
    that cached information for later error source reporting. See PCIe r5.0,
    sec 7.9.10.
    
    Co-developed-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Link: https://lore.kernel.org/r/20201121001036.8560-4-sean.v.kelley@intel.com
    Tested-by: Jonathan Cameron <Jonathan.Cameron@huawei.com> # non-native/no RCEC
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Sean V Kelley <sean.v.kelley@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    
    (cherry picked from commit 90655631988f8f501529e6de5f13614389717ead)
    
    Orabug: 32961050
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    (cherry picked from commit 9bd1fa4d169eaa7b681758906403c2be31523494)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -287,6 +287,7 @@ struct pci_vpd;
 struct pci_sriov;
 struct pci_ats;
 struct pci_p2pdma;
+struct rcec_ea;
 
 /* The pci_dev structure describes PCI devices */
 struct pci_dev {
@@ -309,6 +310,9 @@ struct pci_dev {
 #ifdef CONFIG_PCIEAER
 	u16		aer_cap;	/* AER capability offset */
 	struct aer_stats *aer_stats;	/* AER stats for this device */
+#endif
+#ifdef CONFIG_PCIEPORTBUS
+	struct rcec_ea	*rcec_ea;	/* RCEC cached endpoint association */
 #endif
 	u8		pcie_cap;	/* PCIe capability offset */
 	u8		msi_cap;	/* MSI capability offset */
