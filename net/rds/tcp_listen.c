5a039eecda4423ddc9c815cc8dcd3436f69b551b
Alan Maguire <alan.maguire@oracle.com>
2020-11-23 11:09:45 +0100

    rds: fix out-of-tree build broken by tracepoints
    Out-of-tree builds of RDS need to work along with in-kernel builds.
    Tracepoint definitions in include/trace/events/rds.h need to be
    moved into net/rds.  TRACE_INCLUDE_PATH needs to be adjusted
    and cflags need to add current path to include paths searched
    so that include/trace/define_trace.h #include of RDS trace.h
    finds it in net/rds.
    
    Orabug: 32180857
    Reported-by: Haakon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 4af26a361223313c5104ad7d8c603b14c54ccb49)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit bd098caccaaa711bd8aa7220152bed04c6b2c781)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/tcp_listen.c
+++ net/rds/tcp_listen.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2020 Oracle and/or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -34,7 +34,7 @@
 #include <linux/in.h>
 #include <net/tcp.h>
 
-#include <trace/events/rds.h>
+#include "trace.h"
 
 #include "rds.h"
 #include "tcp.h"
