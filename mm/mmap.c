f635713adcd8d104db9658930100f1b537ea11ee
Liam R. Howlett <Liam.Howlett@Oracle.com>
2020-11-30 21:30:04 -0500

    mm/mmap: Change do_brk_munmap() to use do_mas_align_munmap()
    do_brk_munmap() has already aligned the address and has a maple tree
    state to be used.  Use the new do_mas_align_munmap() to avoid
    unnecessary alignment and error checks.
    
    Signed-off-by: Liam R. Howlett <Liam.Howlett@Oracle.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2972,14 +2972,15 @@ static int do_brk_munmap(struct ma_state *mas, struct vm_area_struct *vma,
 	struct mm_struct *mm = vma->vm_mm;
 	struct vm_area_struct unmap;
 	unsigned long unmap_pages;
-	int ret = 1;
+	int ret;
 
 	arch_unmap(mm, newbrk, oldbrk);
 
 	if (likely((vma->vm_end < oldbrk) ||
 		   ((vma->vm_start == newbrk) && (vma->vm_end == oldbrk)))) {
 		/* remove entire mapping(s) */
-		ret = do_mas_munmap(mas, mm, newbrk, oldbrk-newbrk, uf, true);
+		ret = do_mas_align_munmap(mas, vma, mm, newbrk, oldbrk, uf,
+					  true);
 		goto munmap_full_vma;
 	}
 
