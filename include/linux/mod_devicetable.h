c26c6d51300ff19ac0a8a8a145dad6df5a393409
Dave Ertman <david.m.ertman@intel.com>
2020-12-02 16:54:24 -0800

    Add auxiliary bus support
    Add support for the Auxiliary Bus, auxiliary_device and auxiliary_driver.
    It enables drivers to create an auxiliary_device and bind an
    auxiliary_driver to it.
    
    The bus supports probe/remove shutdown and suspend/resume callbacks.
    Each auxiliary_device has a unique string based id; driver binds to
    an auxiliary_device based on this id through the bus.
    
    Co-developed-by: Kiran Patil <kiran.patil@intel.com>
    Co-developed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Co-developed-by: Fred Oh <fred.oh@linux.intel.com>
    Co-developed-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Kiran Patil <kiran.patil@intel.com>
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Fred Oh <fred.oh@linux.intel.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Dave Ertman <david.m.ertman@intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Shiraz Saleem <shiraz.saleem@intel.com>
    Reviewed-by: Parav Pandit <parav@mellanox.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Reviewed-by: Martin Habets <mhabets@solarflare.com>
    Link: https://lore.kernel.org/r/20201113161859.1775473-2-david.m.ertman@intel.com
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://lore.kernel.org/r/160695681289.505290.8978295443574440604.stgit@dwillia2-desk3.amr.corp.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 7de3697e9cbd4bd3d62bafa249d57990e1b8f294)
    
    Orabug: 32461425
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    
    Conflicts:
    	drivers/base/Makefile
    	include/linux/mod_devicetable.h
    	scripts/mod/devicetable-offsets.c
    	scripts/mod/file2alias.c
    
    Changes to be committed:
     	new file:   Documentation/driver-api/auxiliary_bus.rst
     	modified:   Documentation/driver-api/index.rst
     	modified:   drivers/base/Kconfig
     	modified:   drivers/base/Makefile
     	new file:   drivers/base/auxiliary.c
     	new file:   include/linux/auxiliary_bus.h
     	modified:   include/linux/mod_devicetable.h
     	modified:   scripts/mod/devicetable-offsets.c
     	modified:   scripts/mod/file2alias.c
    
    (cherry picked from commit 17874b0bd9a73481058c56bb6dba6de329c78944)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -825,4 +825,25 @@ struct wmi_device_id {
 	const void *context;
 };
 
+#define MHI_DEVICE_MODALIAS_FMT "mhi:%s"
+#define MHI_NAME_SIZE 32
+
+/**
+ * struct mhi_device_id - MHI device identification
+ * @chan: MHI channel name
+ * @driver_data: driver data;
+ */
+struct mhi_device_id {
+	const char chan[MHI_NAME_SIZE];
+	kernel_ulong_t driver_data;
+};
+
+#define AUXILIARY_NAME_SIZE 32
+#define AUXILIARY_MODULE_PREFIX "auxiliary:"
+
+struct auxiliary_device_id {
+	char name[AUXILIARY_NAME_SIZE];
+	kernel_ulong_t driver_data;
+};
+
 #endif /* LINUX_MOD_DEVICETABLE_H */
