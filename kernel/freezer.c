8c9dc2dd42335b34feb89dc81295c23db084c919
Victor Erminpour <victor.erminpour@oracle.com>
2020-12-08 14:01:57 -0800

    ksplice: Provide an interface to freeze tasks
    Provides an alternative method for freezing selected tasks using a flag
    in struct task_struct. This is usefull starting with 3.3 due to
    freezer changes.
    
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    
    [Dave: aarch64: KSPLICE: Provide an interface to freeze tasks]
    
    Signed-off-by: Dave Aldridge <david.j.aldridge@oracle.com>
    
    [Victor: LUCI port]
    
    Orabug: 29961704
    
    Signed-off-by: Victor Erminpour <victor.erminpour@oracle.com>
    Reviewed-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    
    [Victor: LUCI port to v5.19]
    
    Orabug: 29961704
    
    Signed-off-by: Victor Erminpour <victor.erminpour@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    
    [Alex: LUCI port to v6.1]
    
    Orabug: 29961704
    
    Signed-off-by: Alex Thorlton <alex.thorlton@oracle.com>
    Reviewed-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    
    [Alex: LUCI port to v6.5]
    
    Orabug: 29961704
    
    Signed-off-by: Alex Thorlton <alex.thorlton@oracle.com>
    Reviewed-by: Quentin Casasnovas <quentin.casasnovas@oracle.com>
    
    [Julian: LUCI port to v6.8]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>
    
    [Julian: LUCI port to v6.12]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>
    
    [Julian: LUCI port to v6.16]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>
    
    [Julian: LUCI port to v6.17]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>
    
    [Julian: LUCI port to v6.18]
    
    Orabug: 29961704
    
    Signed-off-by: Julian Pidancet <julian.pidancet@oracle.com>
    Reviewed-by: Gregory Herrero <gregory.herrero@oracle.com>

--- kernel/freezer.c
+++ kernel/freezer.c
@@ -44,6 +44,9 @@ bool freezing_slow_path(struct task_struct *p)
 	if (tsk_is_oom_victim(p))
 		return false;
 
+	if (test_tsk_thread_flag(p, TIF_KSPLICE_FREEZING))
+		return true;
+
 	if (pm_nosig_freezing || cgroup_freezing(p))
 		return true;
 
