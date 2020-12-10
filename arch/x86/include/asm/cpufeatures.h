48a579709f055c26433e0332e0ea771e57a2cb84
Tom Lendacky <thomas.lendacky@amd.com>
2020-12-10 11:09:36 -0600

    x86/cpu: Add VM page flush MSR availablility as a CPUID feature
    commit 69372cf01290b9587d2cee8fbe161d75d55c3adc upstream.
    
    On systems that do not have hardware enforced cache coherency between
    encrypted and unencrypted mappings of the same physical page, the
    hypervisor can use the VM page flush MSR (0xc001011e) to flush the cache
    contents of an SEV guest page. When a small number of pages are being
    flushed, this can be used in place of issuing a WBINVD across all CPUs.
    
    CPUID 0x8000001f_eax[2] is used to determine if the VM page flush MSR is
    available. Add a CPUID feature to indicate it is supported and define the
    MSR.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Message-Id: <f1966379e31f9b208db5257509c4a089a87d33d0.1607620209.git.thomas.lendacky@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 4fa849d4af687540e1c4d8309bea15ec613279d1)
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Conflicts:
    	arch/x86/include/asm/msr-index.h
    Notes: conflict was caused by we backported 728cd1b93334 ("KVM: x86: SVM:
    move avic definitions from AMD's spec to svm.h") from higher release,
    which made context change. Ignore context change.

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -237,6 +237,7 @@
 #define X86_FEATURE_VMCALL		( 8*32+18) /* "" Hypervisor supports the VMCALL instruction */
 #define X86_FEATURE_VMW_VMMCALL		( 8*32+19) /* "" VMware prefers VMMCALL hypercall instruction */
 #define X86_FEATURE_SEV_ES		( 8*32+20) /* AMD Secure Encrypted Virtualization - Encrypted State */
+#define X86_FEATURE_VM_PAGE_FLUSH	( 8*32+21) /* "" VM Page Flush MSR is supported */
 
 #define X86_FEATURE_VMEXIT_RSB_FULL	( 8*32+27) /* "" Whether to stuff the RSB on VMEXIT. */
 
