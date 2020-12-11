c51c6ef843ef7a99ea7d3f88c755ee39fd1d19ce
George Cherian <george.cherian@marvell.com>
2020-12-11 11:55:24 +0530

    octeontx2-af: Add devlink suppoort to af driver
    Add devlink support to AF driver. Basic devlink support is added.
    Currently info_get is the only supported devlink ops.
    
    devlink ouptput looks like this
     # devlink dev
     pci/0002:01:00.0
     # devlink dev info
     pci/0002:01:00.0:
      driver octeontx2-af
     #
    
    Change-Id: Ib063815c6f7a6e1edca825cd5915da1849eea301
    Signed-off-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Jerin Jacob <jerinj@marvell.com>
    Signed-off-by: George Cherian <george.cherian@marvell.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/44942
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 1682607309ed3824075ed4c3edd241b7814c29c7)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -3384,11 +3384,15 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (err)
 		goto err_flr;
 
+	err = rvu_register_dl(rvu);
+	if (err)
+		goto err_irq;
+
 	rvu_setup_rvum_blk_revid(rvu);
 
 	err = rvu_policy_init(rvu);
 	if (err)
-		goto err_irq;
+		goto err_dl;
 
 	/* Enable AF's VFs (if any) */
 	err = rvu_enable_sriov(rvu);
@@ -3399,8 +3403,11 @@ static int rvu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	rvu_dbg_init(rvu);
 
 	return 0;
+
 err_policy:
 	rvu_policy_destroy(rvu);
+err_dl:
+	rvu_unregister_dl(rvu);
 err_irq:
 	rvu_unregister_interrupts(rvu);
 err_flr:
@@ -3433,6 +3440,7 @@ static void rvu_remove(struct pci_dev *pdev)
 	rvu_dbg_exit(rvu);
 	rvu_policy_destroy(rvu);
 	rvu_unregister_interrupts(rvu);
+	rvu_unregister_dl(rvu);
 	rvu_flr_wq_destroy(rvu);
 	rvu_cgx_exit(rvu);
 	rvu_fwdata_exit(rvu);
