1fbaa4add5277fff795c905a44f524a26ec8d015
Parav Pandit <parav@nvidia.com>
2020-12-11 22:12:17 -0800

    net/mlx5: SF, Add auxiliary device support
    Introduce API to add and delete an auxiliary device for an SF.
    Each SF has its own dedicated window in the PCI BAR 2.
    
    SF device is similar to PCI PF and VF that supports multiple class of
    devices such as net, rdma and vdpa.
    
    SF device will be added or removed in subsequent patch during SF
    devlink port function state change command.
    
    A subfunction device exposes user supplied subfunction number which will
    be further used by systemd/udev to have deterministic name for its
    netdevice and rdma device.
    
    An mlx5 subfunction auxiliary device example:
    
    $ devlink dev eswitch set pci/0000:06:00.0 mode switchdev
    
    $ devlink port show
    pci/0000:06:00.0/65535: type eth netdev ens2f0np0 flavour physical port 0 splittable false
    
    $ devlink port add pci/0000:06:00.0 flavour pcisf pfnum 0 sfnum 88
    pci/0000:08:00.0/32768: type eth netdev eth6 flavour pcisf controller 0 pfnum 0 sfnum 88 external false splittable false
      function:
        hw_addr 00:00:00:00:00:00 state inactive opstate detached
    
    $ devlink port show ens2f0npf0sf88
    pci/0000:06:00.0/32768: type eth netdev ens2f0npf0sf88 flavour pcisf controller 0 pfnum 0 sfnum 88 external false splittable false
      function:
        hw_addr 00:00:00:00:88:88 state inactive opstate detached
    
    $ devlink port function set ens2f0npf0sf88 hw_addr 00:00:00:00:88:88 state active
    
    On activation,
    
    $ ls -l /sys/bus/auxiliary/devices/
    mlx5_core.sf.4 -> ../../../devices/pci0000:00/0000:00:03.0/0000:06:00.0/mlx5_core.sf.4
    
    $ cat /sys/bus/auxiliary/devices/mlx5_core.sf.4/sfnum
    88
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Reviewed-by: Vu Pham <vuhuong@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 32176425
    
    (cherry picked from commit 90d010b8634b89a97ca3b7aa6a88fd566fc77717)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/main.c
    	Documentation/networking/device_drivers/ethernet/mellanox/mlx5.rst
    
    Minor conflict in a context caused by a missing code.
    
    Additionally, Documentation updates are maintained in the old path
    without ethernet/ subdirectory.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -520,6 +520,7 @@ struct mlx5_devcom;
 struct mlx5_eq_table;
 struct mlx5_irq_table;
 struct mlx5_vhca_state_notifier;
+struct mlx5_sf_dev_table;
 
 struct mlx5_rate_limit {
 	u32			rate;
@@ -620,6 +621,7 @@ struct mlx5_priv {
 	struct mlx5_uars_page	       *uar;
 #ifdef CONFIG_MLX5_SF
 	struct mlx5_vhca_state_notifier *vhca_state_notifier;
+	struct mlx5_sf_dev_table *sf_dev_table;
 #endif
 };
 
