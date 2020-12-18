3e1b2585bea018d9ee26debe0c585aae437f44de
Josh Poimboeuf <jpoimboe@redhat.com>
2020-12-18 14:19:32 -0600

    objtool: Add 'alt_group' struct
    Create a new struct associated with each group of alternatives
    instructions.  This will help with the removal of fake jumps, and more
    importantly with adding support for stack layout changes in
    alternatives.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    
    (cherry picked from commit b23cc71c62747f2e4c3e56138872cf47e1294f8a)
    
    Orabug: 34335632
    CVE: CVE-2022-29901
    CVE: CVE-2022-23816
    
    [Alexandre: conflict in check.c because commit is not backported:
     7117f16bf460 ("objtool: Fix ORC vs alternatives")]
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -747,20 +747,28 @@ static int handle_group_alt(struct objtool_file *file,
 			    struct instruction *orig_insn,
 			    struct instruction **new_insn)
 {
-	static unsigned int alt_group_next_index = 1;
 	struct instruction *last_orig_insn, *last_new_insn, *insn, *fake_jump = NULL;
-	unsigned int alt_group = alt_group_next_index++;
+	struct alt_group *orig_alt_group, *new_alt_group;
 	unsigned long dest_off;
 
+
+	orig_alt_group = malloc(sizeof(*orig_alt_group));
+	if (!orig_alt_group) {
+		WARN("malloc failed");
+		return -1;
+	}
 	last_orig_insn = NULL;
 	insn = orig_insn;
 	sec_for_each_insn_from(file, insn) {
 		if (insn->offset >= special_alt->orig_off + special_alt->orig_len)
 			break;
 
-		insn->alt_group = alt_group;
+		insn->alt_group = orig_alt_group;
 		last_orig_insn = insn;
 	}
+	orig_alt_group->orig_group = NULL;
+	orig_alt_group->first_insn = orig_insn;
+	orig_alt_group->last_insn = last_orig_insn;
 
 	if (next_insn_same_sec(file, last_orig_insn)) {
 		fake_jump = malloc(sizeof(*fake_jump));
@@ -791,8 +799,13 @@ static int handle_group_alt(struct objtool_file *file,
 		return 0;
 	}
 
+	new_alt_group = malloc(sizeof(*new_alt_group));
+	if (!new_alt_group) {
+		WARN("malloc failed");
+		return -1;
+	}
+
 	last_new_insn = NULL;
-	alt_group = alt_group_next_index++;
 	insn = *new_insn;
 	sec_for_each_insn_from(file, insn) {
 		if (insn->offset >= special_alt->new_off + special_alt->new_len)
@@ -802,7 +815,7 @@ static int handle_group_alt(struct objtool_file *file,
 
 		insn->ignore = orig_insn->ignore_alts;
 		insn->func = orig_insn->func;
-		insn->alt_group = alt_group;
+		insn->alt_group = new_alt_group;
 
 		/*
 		 * Since alternative replacement code is copy/pasted by the
@@ -853,6 +866,10 @@ static int handle_group_alt(struct objtool_file *file,
 			  special_alt->new_sec, special_alt->new_off);
 		return -1;
 	}
+
+	new_alt_group->orig_group = orig_alt_group;
+	new_alt_group->first_insn = *new_insn;
+	new_alt_group->last_insn = last_new_insn;
 
 	if (fake_jump)
 		list_add(&fake_jump->list, &last_new_insn->list);
