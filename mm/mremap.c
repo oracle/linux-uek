5b2543a0005e66c6c5dcc1daca8ce0a496ce1e7e
Liam R. Howlett <Liam.Howlett@Oracle.com>
2021-01-04 15:03:49 -0500

    mm/mremap: Use vma_next() instead of vma linked list
    Signed-off-by: Liam R. Howlett <Liam.Howlett@Oracle.com>

--- mm/mremap.c
+++ mm/mremap.c
@@ -623,7 +623,7 @@ static unsigned long move_vma(struct vm_area_struct *vma,
 	if (excess) {
 		vma->vm_flags |= VM_ACCOUNT;
 		if (split)
-			vma->vm_next->vm_flags |= VM_ACCOUNT;
+			vma_next(mm, vma)->vm_flags |= VM_ACCOUNT;
 	}
 
 	return new_addr;
@@ -796,9 +796,11 @@ out:
 static int vma_expandable(struct vm_area_struct *vma, unsigned long delta)
 {
 	unsigned long end = vma->vm_end + delta;
+	struct vm_area_struct *next;
 	if (end < vma->vm_end) /* overflow */
 		return 0;
-	if (vma->vm_next && vma->vm_next->vm_start < end) /* intersection */
+	next = vma_next(vma->vm_mm, vma);
+	if (next && next->vm_start < end) /* intersection */
 		return 0;
 	if (get_unmapped_area(NULL, vma->vm_start, end - vma->vm_start,
 			      0, MAP_FIXED) & ~PAGE_MASK)
