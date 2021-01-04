5cebb446f33d97842ca0cd2299607eadd79bce00
Liam R. Howlett <Liam.Howlett@Oracle.com>
2021-01-04 14:56:20 -0500

    mm/gup: Use maple tree navigation instead of linked list
    Signed-off-by: Liam R. Howlett <Liam.Howlett@Oracle.com>

--- mm/gup.c
+++ mm/gup.c
@@ -1570,7 +1570,6 @@ int __mm_populate(unsigned long start, unsigned long len, int ignore_errors)
 	long ret = 0;
 
 	end = start + len;
-
 	for (nstart = start; nstart < end; nstart = nend) {
 		/*
 		 * We want to fault in pages for [nstart; end) address range.
@@ -1579,10 +1578,10 @@ int __mm_populate(unsigned long start, unsigned long len, int ignore_errors)
 		if (!locked) {
 			locked = 1;
 			mmap_read_lock(mm);
-			vma = find_vma(mm, nstart);
+			vma = find_vma_intersection(mm, start, end);
 		} else if (nstart >= vma->vm_end)
-			vma = vma->vm_next;
-		if (!vma || vma->vm_start >= end)
+			vma = find_vma(mm, vma->vm_end);
+		if (!vma)
 			break;
 		/*
 		 * Set [nstart; nend) to intersection of desired address
