ecc964dd409556cc832e27d06c4e0dbad7e78ad5
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2021-01-18 13:32:35 +0100

    firmware: raspberrypi: Introduce devm_rpi_firmware_get()
    [ Upstream commit f663204c9a1f8d6fcc590667d9d7a9f44e064644 ]
    
    It'll simplify the firmware handling for most consumers.
    
    Suggested-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Stable-dep-of: 5bca3688bdbc ("Input: raspberrypi-ts - fix refcount leak in rpi_ts_probe")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit de85be00a8f30c98aa10fff13580271a59da2be5)
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Conflicts:
    	drivers/firmware/raspberrypi.c
    	include/soc/bcm2835/raspberrypi-firmware.h
    Notes: conflict is caused by we backported the commit b2e8d7227ba7
    ("firmware: raspberrypi: Introduce vl805 init routine"), which made
    context change, keep both code.

--- include/soc/bcm2835/raspberrypi-firmware.h
+++ include/soc/bcm2835/raspberrypi-firmware.h
@@ -146,6 +146,8 @@ int rpi_firmware_property_list(struct rpi_firmware *fw,
 void rpi_firmware_put(struct rpi_firmware *fw);
 struct rpi_firmware *rpi_firmware_get(struct device_node *firmware_node);
 int rpi_firmware_init_vl805(struct pci_dev *pdev);
+struct rpi_firmware *devm_rpi_firmware_get(struct device *dev,
+					   struct device_node *firmware_node);
 #else
 static inline int rpi_firmware_property(struct rpi_firmware *fw, u32 tag,
 					void *data, size_t len)
@@ -168,6 +170,12 @@ static inline struct rpi_firmware *rpi_firmware_get(struct device_node *firmware
 static inline int rpi_firmware_init_vl805(struct pci_dev *pdev)
 {
 	return 0;
+}
+
+static inline struct rpi_firmware *devm_rpi_firmware_get(struct device *dev,
+					struct device_node *firmware_node)
+{
+	return NULL;
 }
 #endif
 
