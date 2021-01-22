5406c8baa7d79430269bc44ed6ae7df111ff9352
Yang Li <abaci-bugfix@linux.alibaba.com>
2021-01-22 17:02:53 +0800

    scsi: qla2xxx: Remove redundant NULL check
    Fix below warnings reported by coccicheck:
    ./drivers/scsi/qla2xxx/qla_init.c:3371:2-7: WARNING: NULL check before
    some freeing functions is not needed.
    ./drivers/scsi/qla2xxx/qla_init.c:7855:5-10: WARNING: NULL check before
    some freeing functions is not needed.
    ./drivers/scsi/qla2xxx/qla_init.c:7916:2-7: WARNING: NULL check before
    some freeing functions is not needed.
    ./drivers/scsi/qla2xxx/qla_init.c:8113:4-18: WARNING: NULL check before
    some freeing functions is not needed.
    ./drivers/scsi/qla2xxx/qla_init.c:8174:2-7: WARNING: NULL check before
    some freeing functions is not needed.
    
    Link: https://lore.kernel.org/r/1611306174-92627-1-git-send-email-abaci-bugfix@linux.alibaba.com
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Signed-off-by: Yang Li <abaci-bugfix@linux.alibaba.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 18c05faf8ab14913c8144108661efa8f17b10b5b)
    
    Orabug: 33116624
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/scsi/qla2xxx/qla_init.c
+++ drivers/scsi/qla2xxx/qla_init.c
@@ -3384,8 +3384,7 @@ qla2x00_alloc_fw_dump(scsi_qla_host_t *vha)
 				    "Re-Allocated (%d KB) and save firmware dump.\n",
 				    dump_size / 1024);
 			} else {
-				if (ha->fw_dump)
-					vfree(ha->fw_dump);
+				vfree(ha->fw_dump);
 				ha->fw_dump = fw_dump;
 
 				ha->fw_dump_len = ha->fw_dump_alloc_len =
@@ -7877,8 +7876,7 @@ qla24xx_load_risc_flash(scsi_qla_host_t *vha, uint32_t *srisc_addr,
 	templates = (risc_attr & BIT_9) ? 2 : 1;
 	ql_dbg(ql_dbg_init, vha, 0x0160, "-> templates = %u\n", templates);
 	for (j = 0; j < templates; j++, fwdt++) {
-		if (fwdt->template)
-			vfree(fwdt->template);
+		vfree(fwdt->template);
 		fwdt->template = NULL;
 		fwdt->length = 0;
 
@@ -7938,8 +7936,7 @@ qla24xx_load_risc_flash(scsi_qla_host_t *vha, uint32_t *srisc_addr,
 	return QLA_SUCCESS;
 
 failed:
-	if (fwdt->template)
-		vfree(fwdt->template);
+	vfree(fwdt->template);
 	fwdt->template = NULL;
 	fwdt->length = 0;
 
@@ -8135,8 +8132,7 @@ qla24xx_load_risc_blob(scsi_qla_host_t *vha, uint32_t *srisc_addr)
 	templates = (risc_attr & BIT_9) ? 2 : 1;
 	ql_dbg(ql_dbg_init, vha, 0x0170, "-> templates = %u\n", templates);
 	for (j = 0; j < templates; j++, fwdt++) {
-		if (fwdt->template)
-			vfree(fwdt->template);
+		vfree(fwdt->template);
 		fwdt->template = NULL;
 		fwdt->length = 0;
 
@@ -8196,8 +8192,7 @@ qla24xx_load_risc_blob(scsi_qla_host_t *vha, uint32_t *srisc_addr)
 	return QLA_SUCCESS;
 
 failed:
-	if (fwdt->template)
-		vfree(fwdt->template);
+	vfree(fwdt->template);
 	fwdt->template = NULL;
 	fwdt->length = 0;
 
