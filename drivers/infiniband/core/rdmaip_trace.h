6727b72bfa9e7bf677ed0f1ded546d7d967219fe
Alan Maguire <alan.maguire@oracle.com>
2021-01-28 15:52:55 +0000

    A/A Bonding: remove use of trace_printk(), replacing with tracepoints
    A/A Bonding uses trace_printk() directly, leading to warning banner
    messages about allocation of trace buffers.  Instead it should
    use tracepoints to show debug messages, allowing specification
    of which tracepoints to activate via tracefs while still dumping
    data to /sys/kernel/debug/tracing/trace_pipe
    
    3 tracepoints are added: rdmaip_debug_[1-3], and arguments consist
    of the level (1-3), function name and varargs.  Function name
    and string are printed as the trace message.  Tracepoints 1-2
    are enabled by default and sysctl modifications of debug level
    result in the appropriate tracepoint enabling/disabling.
    
    Orabug: 32969529
    
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
    (cherry picked from commit e7a26725d4ba0ff0c7f3e6d08e3ecf3cc35c847c)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

---
+++ drivers/infiniband/core/rdmaip_trace.h
@@ -0,0 +1,57 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Trace point definitions for resilient rdma events.
+ *
+ * Copyright (c) 2020, Oracle and/or its affiliates.
+ */
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM rdmaip
+#if !defined(__RDMAIP_TRACE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define __RDMAIP_TRACE_H
+
+#include <linux/tracepoint.h>
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM	rdmaip
+
+#define MAX_MSG_LEN	100
+
+DECLARE_EVENT_CLASS(rdmaip_debug,
+	TP_PROTO(int level, const char *func, struct va_format *vaf),
+	TP_ARGS(level, func, vaf),
+	TP_STRUCT__entry(
+		__field(int, level)
+		__string(func, func)
+		__dynamic_array(char, msg, MAX_MSG_LEN)
+	),
+	TP_fast_assign(
+		__entry->level = level;
+		__assign_str(func, func);
+		WARN_ON_ONCE(vsnprintf(__get_dynamic_array(msg),
+				       MAX_MSG_LEN, vaf->fmt,
+				       *vaf->va) >= MAX_MSG_LEN);
+	),
+	TP_printk("%s: %s", __get_str(func), __get_str(msg))
+);
+
+DEFINE_EVENT(rdmaip_debug, rdmaip_debug_1,
+	TP_PROTO(int level, const char *func, struct va_format *vaf),
+	TP_ARGS(level, func, vaf)
+);
+
+DEFINE_EVENT(rdmaip_debug, rdmaip_debug_2,
+	TP_PROTO(int level, const char *func, struct va_format *vaf),
+	TP_ARGS(level, func, vaf)
+);
+
+DEFINE_EVENT(rdmaip_debug, rdmaip_debug_3,
+	TP_PROTO(int level, const char *func, struct va_format *vaf),
+	TP_ARGS(level, func, vaf)
+);
+
+#endif /* __RDMAIP_TRACE_H */
+
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE rdmaip_trace
+#include <trace/define_trace.h>
