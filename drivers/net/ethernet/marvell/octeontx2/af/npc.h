03c1b14f156758064a0b9c6c1152e4b3c9507dde
Harman Kalra <hkalra@marvell.com>
2021-02-02 00:39:14 +0530

    octeontx2-af: adding new lt def registers support
    CN10k introduces following new LT DEF registers:
    1. APAD (alignment padding) LT DEF registers are
    enhancement to existing apad calculation algorithm
    where not just ipv4 and ipv6 but also other protocols
    can be matched and required alignment can be added by NIX.
    
    2. ET LT DEF register defines layer information in NPC_RESULT_S
    to identify the Ethertype location in L2 header. Used for
    Ethertype overwriting in inline IPsec flow.
    
    This patch adds required structures and some header changes. Also
    strict version check (based on minor field) is imposed to highlight
    version mismatch between the kernel headers and KPU profile.
    
    Change-Id: I77969a855ea09d1873fb2e32b8997129380dd464
    Signed-off-by: Harman Kalra <hkalra@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/45262
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    Reviewed-by: Kiran Kumar Kokkilagadda <kirankumark@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit d6b693205a5d12231d5780cb5b67e0e7ba7ae5e7)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/npc.h
+++ drivers/net/ethernet/marvell/octeontx2/af/npc.h
@@ -490,6 +490,29 @@ struct npc_lt_def_ipsec {
 	u8	spi_nz;
 } __packed;
 
+struct npc_lt_def_apad {
+	u8	ltype_mask;
+	u8	ltype_match;
+	u8	lid;
+	u8	valid;
+} __packed;
+
+struct npc_lt_def_color {
+	u8	ltype_mask;
+	u8	ltype_match;
+	u8	lid;
+	u8	noffset;
+	u8	offset;
+} __packed;
+
+struct npc_lt_def_et {
+	u8	ltype_mask;
+	u8	ltype_match;
+	u8	lid;
+	u8	valid;
+	u8	offset;
+} __packed;
+
 struct npc_lt_def_cfg {
 	struct npc_lt_def	rx_ol2;
 	struct npc_lt_def	rx_oip4;
@@ -507,6 +530,13 @@ struct npc_lt_def_cfg {
 	struct npc_lt_def	pck_oip4;
 	struct npc_lt_def	pck_oip6;
 	struct npc_lt_def	pck_iip4;
+	struct npc_lt_def_apad	rx_apad0;
+	struct npc_lt_def_apad	rx_apad1;
+	struct npc_lt_def_color	rx_ovlan;
+	struct npc_lt_def_color	rx_ivlan;
+	struct npc_lt_def_color	rx_gen0_color;
+	struct npc_lt_def_color	rx_gen1_color;
+	struct npc_lt_def_et	rx_et[2];
 } __packed;
 
 /* Loadable KPU profile firmware data */
