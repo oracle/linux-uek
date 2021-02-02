18040d19b0aa72d066a87c31c72a94cc4ddb6279
Kan Liang <kan.liang@linux.intel.com>
2021-02-02 12:09:10 -0800

    perf report: Support instruction latency
    The instruction latency information can be recorded on some platforms,
    e.g., the Intel Sapphire Rapids server. With both memory latency
    (weight) and the new instruction latency information, users can easily
    locate the expensive load instructions, and also understand the time
    spent in different stages. The users can optimize their applications in
    different pipeline stages.
    
    The 'weight' field is shared among different architectures. Reusing the
    'weight' field may impacts other architectures. Add a new field to store
    the instruction latency.
    
    Like the 'weight' support, introduce a 'ins_lat' for the global
    instruction latency, and a 'local_ins_lat' for the local instruction
    latency version.
    
    Add new sort functions, INSTR Latency and Local INSTR Latency,
    accordingly.
    
    Add local_ins_lat to the default_mem_sort_order[].
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Jin Yao <yao.jin@linux.intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lore.kernel.org/lkml/1612296553-21962-7-git-send-email-kan.liang@linux.intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    
    (cherry picked from commit 590db42de068a1d11e51bd0796a9044621aeed2e)
    
    Conflicts:
    	tools/perf/Documentation/perf-report.txt
    	tools/perf/util/event.h
    	tools/perf/util/sort.c
    	tools/perf/util/sort.h
    UEK does not support code_page_size, manually add documentation.
    UEK does not support no_hw_idx perf_sample, manually add code fragment.
    
    Orabug: 32986807
    
    Signed-off-by: Thomas Tai <thomas.tai@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- tools/perf/util/sort.h
+++ tools/perf/util/sort.h
@@ -48,6 +48,7 @@ struct he_stat {
 	u64			period_guest_sys;
 	u64			period_guest_us;
 	u64			weight;
+	u64			ins_lat;
 	u32			nr_events;
 };
 
@@ -221,6 +222,8 @@ enum sort_type {
 	SORT_CGROUP_ID,
 	SORT_SYM_IPC_NULL,
 	SORT_TIME,
+	SORT_LOCAL_INS_LAT,
+	SORT_GLOBAL_INS_LAT,
 
 	/* branch stack specific sort keys */
 	__SORT_BRANCH_STACK,
