f1de14e638f053d790ecf5d0dc84762dd786244f
Dongli Zhang <dongli.zhang@oracle.com>
2021-02-05 10:07:12 -0800

    vhost scsi: alloc vhost_scsi with kvzalloc() to avoid delay
    The size of 'struct vhost_scsi' is order-10 (~2.3MB). It may take long time
    delay by kzalloc() to compact memory pages by retrying multiple times when
    there is a lack of high-order pages. As a result, there is latency to
    create a VM (with vhost-scsi) or to hotadd vhost-scsi-based storage.
    
    The prior commit 595cb754983d ("vhost/scsi: use vmalloc for order-10
    allocation") prefers to fallback only when really needed, while this patch
    allocates with kvzalloc() with __GFP_NORETRY implicitly set to avoid
    retrying memory pages compact for multiple times.
    
    The __GFP_NORETRY is implicitly set if the size to allocate is more than
    PAGE_SZIE and when __GFP_RETRY_MAYFAIL is not explicitly set.
    
    Orabug: 32471695
    
    The upstream commit id is not available. This patch was sent to upstream
    and acked by Jason Wang and Pankaj Gupta.
    Link: https://lore.kernel.org/virtualization/20210123080853.4214-1-dongli.zhang@oracle.com/T/#u
    
    Cc: Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
    Cc: Joe Jin <joe.jin@oracle.com>
    Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
    Reviewed-by: Joe Jin <joe.jin@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/vhost/scsi.c
+++ drivers/vhost/scsi.c
@@ -1855,12 +1855,9 @@ static int vhost_scsi_open(struct inode *inode, struct file *f)
 	struct vhost_virtqueue **vqs;
 	int r = -ENOMEM, i;
 
-	vs = kzalloc(sizeof(*vs), GFP_KERNEL | __GFP_NOWARN | __GFP_RETRY_MAYFAIL);
-	if (!vs) {
-		vs = vzalloc(sizeof(*vs));
-		if (!vs)
-			goto err_vs;
-	}
+	vs = kvzalloc(sizeof(*vs), GFP_KERNEL);
+	if (!vs)
+		goto err_vs;
 
 	vqs = kmalloc(VHOST_SCSI_MAX_VQ * sizeof(*vqs), GFP_KERNEL);
 	if (!vqs)
