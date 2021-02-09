74cdaca80e56195f360da95db463415b7b9a558e
Henry Willard <henry.willard@oracle.com>
2021-02-09 15:00:53 -0800

    arm64: kexec: add support for kexec with spin-table
    kexec is not supported on systems that use spin-table as the
    cpu enablement method. The spin-table implementation lacks
    cpu_die() and several other methods needed by the hotplug
    framework used by kexec on Arm64.
    
    The spin-table mechanism is setup before Linux kernel is entered
    with details provided in the device tree. The "release-address"
    variable provides the address of a word the secondary CPUs are
    polling. The boot CPU will store the real address of
    secondary_holding_pen() and the secondary CPUs will branch to
    that address. secondary_holding_pen() is another loop where the
    secondary CPUs wait to be called up by the boot CPU.
    
    This patch uses that mechanism to implement cpu_die(). In modern
    versions of u-boot that implement spin-table, the address of the
    loop in protected memory can be derived from the "release-address"
    value. The patch validates the existence of the loop before
    proceeding. smp_spin_table_cpu_die() uses cpu_soft_restart() to
    branch to the loop with the MMU and caching turned off where the
    CPU waits until released by the new kernel.
    
    Special Oracle notes:
    
    Because of the difficulty of modifying the kernel command line,
    this patch forces the use of fast_kexec if it is available. Use of
    fast kexec can be over-ridden by writing a 0 to
    /proc/sys/kernel/fast_kexec. The hotplug route to a kexec_shutdown
    takes several seconds just to stop all the CPUs on ortano, so it
    probably won't satisfy fast-boot requirements.
    
    Also due to the difficulty in changing the kernel command line,
    the pensando version automatically adds additional arguments via
    
    CONFIG_CMDLINE="crashkernel=512M softdog.soft_panic=1"
    CONFIG_CMDLINE_EXTEND=y
    
    If you invoke kexec -l with --reuse-cmdline, the CONFIG_CMDLINE
    value will be appended again, and the kernel command line will get
    longer and longer. There is no practical way for the kernel to know
    how it was started, so there is no way to suppress this at the moment.
    This needs to be addressed one way or another before production.
    
    Orabug: 32549966
    
    Signed-off-by: Henry Willard <henry.willard@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    (cherry picked from commit 20fae5507191d5dce28f9a003dc3a2ed603a3be3)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/arm64/kernel/smp_spin_table.c
+++ arch/arm64/kernel/smp_spin_table.c
@@ -13,17 +13,26 @@
 #include <linux/mm.h>
 
 #include <asm/cacheflush.h>
+#include <asm/daifflags.h>
 #include <asm/cpu_ops.h>
 #include <asm/cputype.h>
 #include <asm/io.h>
 #include <asm/smp_plat.h>
+#include <asm/mmu_context.h>
+#include <asm/kexec.h>
 
+#include "cpu-reset.h"
 extern void secondary_holding_pen(void);
 volatile unsigned long __section(.mmuoff.data.read)
 secondary_holding_pen_release = INVALID_HWID;
 
 static phys_addr_t cpu_release_addr[NR_CPUS];
-
+static unsigned int spin_table_loop[4] = {
+	0xd503205f,        /* wfe */
+	0x58000060,        /* ldr  x0, spin_table_cpu_release_addr */
+	0xb4ffffc0,        /* cbnz x0, 0b */
+	0xd61f0000         /* br   x0 */
+};
 /*
  * Write secondary_holding_pen_release in a way that is guaranteed to be
  * visible to all observers, irrespective of whether they're taking part
@@ -59,6 +68,10 @@ static int smp_spin_table_cpu_init(unsigned int cpu)
 		       cpu);
 
 	of_node_put(dn);
+
+#ifdef CONFIG_FAST_KEXEC
+	fast_kexec = 1;
+#endif
 
 	return ret;
 }
@@ -117,9 +130,109 @@ static int smp_spin_table_cpu_boot(unsigned int cpu)
 	return 0;
 }
 
+
+
+/*
+ * There is a four instruction loop set aside in protected
+ * memory by u-boot where secondary CPUs wait for the kernel to
+ * start.
+ *
+ * 0:       wfe
+ *          ldr    x0, spin_table_cpu_release_addr
+ *          cbz    x0, 0b
+ *          br     x0
+ * spin_table_cpu_release_addr:
+ *          .quad  0
+ *
+ * The address of spin_table_cpu_release_addr is passed in the
+ * "release-address" property in the device table.
+ * smp_spin_table_cpu_prepare() stores the real address of
+ * secondary_holding_pen() where the secondary CPUs loop
+ * until they are released one at a time by smp_spin_table_cpu_boot().
+ * We reuse the spin-table loop by clearing spin_table_cpu_release_addr,
+ * and branching to the beginning of the loop via cpu_soft_restart(),
+ * which turns off the MMU and caching.
+ */
+static void smp_spin_table_cpu_die(unsigned int cpu)
+{
+	__le64 __iomem *release_addr;
+	unsigned int *spin_table_inst;
+	unsigned long spin_table_start;
+
+	if (!cpu_release_addr[cpu])
+		goto spin;
+
+	spin_table_start = (cpu_release_addr[cpu] - sizeof(spin_table_loop));
+
+	/*
+	 * The cpu-release-addr may or may not be inside the linear mapping.
+	 * As ioremap_cache will either give us a new mapping or reuse the
+	 * existing linear mapping, we can use it to cover both cases. In
+	 * either case the memory will be MT_NORMAL.
+	 */
+	release_addr = ioremap_cache(spin_table_start,
+				sizeof(*release_addr) +
+				sizeof(spin_table_loop));
+
+	if (!release_addr)
+		goto spin;
+
+	spin_table_inst = (unsigned int *)release_addr;
+	if (spin_table_inst[0] != spin_table_loop[0] ||
+		spin_table_inst[1] != spin_table_loop[1] ||
+		spin_table_inst[2] != spin_table_loop[2] ||
+		spin_table_inst[3] != spin_table_loop[3])
+		goto spin;
+
+	/*
+	 * Clear the release address, so that we can use it again
+	 */
+	writeq_relaxed(0, release_addr + 2);
+	__flush_dcache_area((__force void *)(release_addr + 2),
+			    sizeof(*release_addr));
+
+	iounmap(release_addr);
+
+	local_daif_mask();
+	cpu_soft_restart(spin_table_start, 0, 0, cpu);
+
+	BUG();
+
+spin:
+	cpu_park_loop();
+
+}
+
+static int smp_spin_table_cpu_kill(unsigned int cpu)
+{
+	unsigned long start, end;
+
+	start = jiffies;
+	end = start + msecs_to_jiffies(100);
+
+	do {
+		if (!cpu_online(cpu)) {
+			pr_info("CPU%d killed\n", cpu);
+			return 0;
+		}
+	} while (time_before(jiffies, end));
+	pr_warn("CPU%d may not have shut down cleanly\n", cpu);
+	return -ETIMEDOUT;
+
+}
+
+/* Nothing to do here */
+static int smp_spin_table_cpu_disable(unsigned int cpu)
+{
+	return 0;
+}
+
 const struct cpu_operations smp_spin_table_ops = {
 	.name		= "spin-table",
 	.cpu_init	= smp_spin_table_cpu_init,
 	.cpu_prepare	= smp_spin_table_cpu_prepare,
 	.cpu_boot	= smp_spin_table_cpu_boot,
+	.cpu_die	= smp_spin_table_cpu_die,
+	.cpu_kill	= smp_spin_table_cpu_kill,
+	.cpu_disable	= smp_spin_table_cpu_disable,
 };
