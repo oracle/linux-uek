f17bf6944101a3cd68ff8fe4b47227cb0d9adbf0
Mike Kravetz <mike.kravetz@oracle.com>
2021-02-24 12:08:56 -0800

    hugetlb: convert page_huge_active() HPageMigratable flag
    Use the new hugetlb page specific flag HPageMigratable to replace the
    page_huge_active interfaces.  By it's name, page_huge_active implied that
    a huge page was on the active list.  However, that is not really what code
    checking the flag wanted to know.  It really wanted to determine if the
    huge page could be migrated.  This happens when the page is actually added
    to the page cache and/or task page table.  This is the reasoning behind
    the name change.
    
    The VM_BUG_ON_PAGE() calls in the *_huge_active() interfaces are not
    really necessary as we KNOW the page is a hugetlb page.  Therefore, they
    are removed.
    
    The routine page_huge_active checked for PageHeadHuge before testing the
    active bit.  This is unnecessary in the case where we hold a reference or
    lock and know it is a hugetlb head page.  page_huge_active is also called
    without holding a reference or lock (scan_movable_pages), and can race
    with code freeing the page.  The extra check in page_huge_active shortened
    the race window, but did not prevent the race.  Offline code calling
    scan_movable_pages already deals with these races, so removing the check
    is acceptable.  Add comment to racy code.
    
    [songmuchun@bytedance.com: remove set_page_huge_active() declaration from include/linux/hugetlb.h]
      Link: https://lkml.kernel.org/r/CAMZfGtUda+KoAZscU0718TN61cSFwp4zy=y2oZ=+6Z2TAZZwng@mail.gmail.com
    
    Link: https://lkml.kernel.org/r/20210122195231.324857-3-mike.kravetz@oracle.com
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Orabug: 33032173
    
    (cherry picked from commit 8f251a3d5ce3bdea73bd045ed35db64f32e0d0d9)
    
    Conflicts:
    	mm/memory_hotplug.c
    
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1236,30 +1236,6 @@ struct hstate *size_to_hstate(unsigned long size)
 }
 
 /*
- * Test to determine whether the hugepage is "active/in-use" (i.e. being linked
- * to hstate->hugepage_activelist.)
- *
- * This function can be called for tail pages, but never returns true for them.
- */
-bool page_huge_active(struct page *page)
-{
-	return PageHeadHuge(page) && PagePrivate(&page[1]);
-}
-
-/* never called for tail page */
-void set_page_huge_active(struct page *page)
-{
-	VM_BUG_ON_PAGE(!PageHeadHuge(page), page);
-	SetPagePrivate(&page[1]);
-}
-
-static void clear_page_huge_active(struct page *page)
-{
-	VM_BUG_ON_PAGE(!PageHeadHuge(page), page);
-	ClearPagePrivate(&page[1]);
-}
-
-/*
  * Internal hugetlb specific page flag. Do not use outside of the hugetlb
  * code
  */
@@ -1320,7 +1296,7 @@ static void __free_huge_page(struct page *page)
 	}
 
 	spin_lock(&hugetlb_lock);
-	clear_page_huge_active(page);
+	ClearHPageMigratable(page);
 	hugetlb_cgroup_uncharge_page(hstate_index(h),
 				     pages_per_huge_page(h), page);
 	if (restore_reserve)
@@ -3894,7 +3870,7 @@ retry_avoidcopy:
 				make_huge_pte(vma, new_page, 1));
 		page_remove_rmap(old_page, true);
 		hugepage_add_new_anon_rmap(new_page, vma, haddr);
-		set_page_huge_active(new_page);
+		SetHPageMigratable(new_page);
 		/* Make the old page be freed below */
 		new_page = old_page;
 	}
@@ -4137,12 +4113,12 @@ retry:
 	spin_unlock(ptl);
 
 	/*
-	 * Only make newly allocated pages active.  Existing pages found
-	 * in the pagecache could be !page_huge_active() if they have been
-	 * isolated for migration.
+	 * Only set HPageMigratable in newly allocated pages.  Existing pages
+	 * found in the pagecache may not have HPageMigratableset if they have
+	 * been isolated for migration.
 	 */
 	if (new_page)
-		set_page_huge_active(page);
+		SetHPageMigratable(page);
 
 	unlock_page(page);
 out:
@@ -4463,7 +4439,7 @@ int hugetlb_mcopy_atomic_pte(struct mm_struct *dst_mm,
 	update_mmu_cache(dst_vma, dst_addr, dst_pte);
 
 	spin_unlock(ptl);
-	set_page_huge_active(page);
+	SetHPageMigratable(page);
 	if (vm_shared)
 		unlock_page(page);
 	ret = 0;
@@ -5219,12 +5195,13 @@ bool isolate_huge_page(struct page *page, struct list_head *list)
 	bool ret = true;
 
 	spin_lock(&hugetlb_lock);
-	if (!PageHeadHuge(page) || !page_huge_active(page) ||
+	if (!PageHeadHuge(page) ||
+	    !HPageMigratable(page) ||
 	    !get_page_unless_zero(page)) {
 		ret = false;
 		goto unlock;
 	}
-	clear_page_huge_active(page);
+	ClearHPageMigratable(page);
 	list_move_tail(&page->lru, list);
 unlock:
 	spin_unlock(&hugetlb_lock);
@@ -5235,7 +5212,7 @@ void putback_active_hugepage(struct page *page)
 {
 	VM_BUG_ON_PAGE(!PageHead(page), page);
 	spin_lock(&hugetlb_lock);
-	set_page_huge_active(page);
+	SetHPageMigratable(page);
 	list_move_tail(&page->lru, &(page_hstate(page))->hugepage_activelist);
 	spin_unlock(&hugetlb_lock);
 	put_page(page);
