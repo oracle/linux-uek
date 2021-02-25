5e3fdb52c93a96b4d93f6b21775578c1dd77b844
Steve Sistare <steven.sistare@oracle.com>
2021-02-25 11:25:02 -0800

    vfio/type1: fix unmap all on ILP32
    Some ILP32 architectures support mapping a 32-bit vaddr within a 64-bit
    iova space.  The unmap-all code uses 32-bit SIZE_MAX as an upper bound on
    the extent of the mappings within iova space, so mappings above 4G cannot
    be found and unmapped.  Use U64_MAX instead, and use u64 for size variables.
    This also fixes a static analysis bug found by the kernel test robot running
    smatch for ILP32.
    
    Fixes: 0f53afa12bae ("vfio/type1: unmap cleanup")
    Fixes: c19650995374 ("vfio/type1: implement unmap all")
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>
    Message-Id: <1614281102-230747-1-git-send-email-steven.sistare@oracle.com>
    Link: https://lore.kernel.org/linux-mm/20210222141043.GW2222@kadam
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    
    Orabug: 32967882
    
    (cherry picked from commit 7dc4b2fdb27242faf40fc20ef83372b7033af050)
    Signed-off-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    
    Conflicts:
    	drivers/vfio/vfio_iommu_type1.c
    
    	Contextual due to missing upstream commit:
    	331e33d2 vfio iommu: Update UNMAP_DMA ioctl to get dirty bitmap before unmap

--- drivers/vfio/vfio_iommu_type1.c
+++ drivers/vfio/vfio_iommu_type1.c
@@ -160,7 +160,7 @@ static struct vfio_dma *vfio_find_dma(struct vfio_iommu *iommu,
 }
 
 static struct rb_node *vfio_find_dma_first_node(struct vfio_iommu *iommu,
-						dma_addr_t start, size_t size)
+						dma_addr_t start, u64 size)
 {
 	struct rb_node *res = NULL;
 	struct rb_node *node = iommu->dma_list.rb_node;
@@ -1045,7 +1045,7 @@ static int vfio_dma_do_unmap(struct vfio_iommu *iommu,
 	size_t unmapped = 0;
 	int ret = -EINVAL, retries = 0;
 	dma_addr_t iova = unmap->iova;
-	unsigned long size = unmap->size;
+	u64 size = unmap->size;
 	bool unmap_all = unmap->flags & VFIO_DMA_UNMAP_FLAG_ALL;
 	bool invalidate_vaddr = unmap->flags & VFIO_DMA_UNMAP_FLAG_VADDR;
 	struct rb_node *n, *first_n;
@@ -1058,13 +1058,11 @@ static int vfio_dma_do_unmap(struct vfio_iommu *iommu,
 	if (unmap_all) {
 		if (iova || size)
 			goto unlock;
-		size = SIZE_MAX;
-	} else if (!size || size & mask) {
+		size = U64_MAX;
+	} else if (!size || size & mask ||
+		   iova + size - 1 < iova || size > SIZE_MAX) {
 		goto unlock;
 	}
-
-	if (iova + size - 1 < iova || size > SIZE_MAX)
-		goto unlock;
 
 	WARN_ON(mask & PAGE_MASK);
 again:
