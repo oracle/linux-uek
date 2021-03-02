2e0670ae3059aec7f8334de2f9ad15bea6d008ca
Benita Bose <benita.bose@intel.com>
2021-03-02 10:12:02 -0800

    ice: Add Support for XPS
    Enable and configure XPS. The driver code implemented sets up the Transmit
    Packet Steering Map, which in turn will be used by the kernel in queue
    selection during Tx.
    
    Signed-off-by: Benita Bose <benita.bose@intel.com>
    Tested-by: Tony Brelinski <tonyx.brelinski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit 634da4c118434cf8a0c5eabce9eb58502ef1521c)
    Orabug: 33048702
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>

--- drivers/net/ethernet/intel/ice/ice_txrx.h
+++ drivers/net/ethernet/intel/ice/ice_txrx.h
@@ -197,6 +197,11 @@ struct ice_rxq_stats {
 	u64 gro_dropped; /* GRO returned dropped */
 };
 
+enum ice_ring_state_t {
+	ICE_TX_XPS_INIT_DONE,
+	ICE_TX_NBITS,
+};
+
 /* this enum matches hardware bits and is meant to be used by DYN_CTLN
  * registers and QINT registers or more generally anywhere in the manual
  * mentioning ITR_INDX, ITR_NONE cannot be used as an index 'n' into any
@@ -294,6 +299,7 @@ struct ice_ring {
 	};
 
 	struct rcu_head rcu;		/* to avoid race on free */
+	DECLARE_BITMAP(xps_state, ICE_TX_NBITS);	/* XPS Config State */
 	struct bpf_prog *xdp_prog;
 	struct xdp_umem *xsk_umem;
 	struct zero_copy_allocator zca;
