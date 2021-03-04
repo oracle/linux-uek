6c168695cd184078c479a855dcf8111517f8f88c
Jens Axboe <axboe@kernel.dk>
2021-03-04 12:21:05 -0700

    kernel: provide create_io_thread() helper
    [ Upstream commit cc440e8738e5c875297ac0e90316745093be7e28 ]
    
    Provide a generic helper for setting up an io_uring worker. Returns a
    task_struct so that the caller can do whatever setup is needed, then call
    wake_up_new_task() to kick it into gear.
    
    Add a kernel_clone_args member, io_thread, which tells copy_process() to
    mark the task with PF_IO_WORKER.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 1500fed00878fd59b2d6a1832b8d3f7c261a5671)
    
    Orabug: 35495339
    
    Signed-off-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Reviewed-by: Alan Adamson <alan.adamson@oracle.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    
    Conflicts:
    	include/linux/sched/task.h
    	Conflict is due to code base difference and no real changes.
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/sched/task.h
+++ include/linux/sched/task.h
@@ -38,6 +38,7 @@ struct kernel_clone_args {
 	/* Number of elements in *set_tid */
 	size_t set_tid_size;
 	int cgroup;
+	int io_thread;
 	struct cgroup *cgrp;
 	struct css_set *cset;
 };
@@ -106,6 +107,7 @@ extern void exit_itimers(struct signal_struct *);
 extern long _do_fork(struct kernel_clone_args *kargs);
 extern bool legacy_clone_args_valid(const struct kernel_clone_args *kargs);
 extern long do_fork(unsigned long, unsigned long, unsigned long, int __user *, int __user *);
+struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node);
 struct task_struct *fork_idle(int);
 struct mm_struct *copy_init_mm(void);
 extern pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags);
