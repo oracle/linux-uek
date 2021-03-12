01553564b4813a94abdc16c33227ffa4dacf1af3
Lee Jones <lee.jones@linaro.org>
2021-03-12 09:47:18 +0000

    scsi: qla2xxx: Fix incorrectly named function qla8044_check_temp()
    Fixes the following W=1 kernel build warning(s):
    
     drivers/scsi/qla2xxx/qla_nx2.c:2038: warning: expecting prototype for qla4_8xxx_check_temp(). Prototype was for qla8044_check_temp() instead
    
    Link: https://lore.kernel.org/r/20210312094738.2207817-11-lee.jones@linaro.org
    Cc: Nilesh Javali <njavali@marvell.com>
    Cc: GR-QLogic-Storage-Upstream@marvell.com
    Cc: "James E.J. Bottomley" <jejb@linux.ibm.com>
    Cc: "Martin K. Petersen" <martin.petersen@oracle.com>
    Cc: linux-scsi@vger.kernel.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit a736e44904422ade7af4ac50c64f58617ae4b7ba)
    
    Orabug: 33116624
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/scsi/qla2xxx/qla_nx2.c
+++ drivers/scsi/qla2xxx/qla_nx2.c
@@ -2029,7 +2029,7 @@ exit_error:
 }
 
 /**
- * qla4_8xxx_check_temp - Check the ISP82XX temperature.
+ * qla8044_check_temp - Check the ISP82XX temperature.
  * @vha: adapter block pointer.
  *
  * Note: The caller should not hold the idc lock.
