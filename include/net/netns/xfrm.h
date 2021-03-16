e40e86f36e5b9624f0b02659e968e667549bf4e5
Ahmed S. Darwish <a.darwish@linutronix.de>
2021-03-16 11:56:29 +0100

    net: xfrm: Localize sequence counter per network namespace
    [ Upstream commit e88add19f68191448427a6e4eb059664650a837f ]
    
    A sequence counter write section must be serialized or its internal
    state can get corrupted. The "xfrm_state_hash_generation" seqcount is
    global, but its write serialization lock (net->xfrm.xfrm_state_lock) is
    instantiated per network namespace. The write protection is thus
    insufficient.
    
    To provide full protection, localize the sequence counter per network
    namespace instead. This should be safe as both the seqcount read and
    write sections access data exclusively within the network namespace. It
    also lays the foundation for transforming "xfrm_state_hash_generation"
    data type from seqcount_t to seqcount_LOCKNAME_t in further commits.
    
    Fixes: b65e3d7be06f ("xfrm: state: add sequence count to detect hash resizes")
    Signed-off-by: Ahmed S. Darwish <a.darwish@linutronix.de>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit a128e07b472ba254dc98e85b6c718802343e7e19)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    
        KABI violation
                include/net/netns/xfrm.h -use EXTEND to add new member.

--- include/net/netns/xfrm.h
+++ include/net/netns/xfrm.h
@@ -72,9 +72,10 @@ struct netns_xfrm {
 #if IS_ENABLED(CONFIG_IPV6)
 	struct dst_ops		xfrm6_dst_ops;
 #endif
-	spinlock_t xfrm_state_lock;
-	spinlock_t xfrm_policy_lock;
-	struct mutex xfrm_cfg_mutex;
+	spinlock_t		xfrm_state_lock;
+	spinlock_t 		xfrm_policy_lock;
+	struct mutex 		xfrm_cfg_mutex;
+	UEK_KABI_EXTEND(seqcount_t xfrm_state_hash_generation)
 };
 
 #endif
