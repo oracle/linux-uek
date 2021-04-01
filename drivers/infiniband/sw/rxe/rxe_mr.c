d10fc2c144f7e88bf5ef2dc8cbaeaeabf3376abd
Rao Shoaib <rao.shoaib@oracle.com>
2021-04-01 12:13:24 -0700

    RDMA/rxe: ipc_bench fails with SoftRoCE plus shpd
    The failure was due to incorrect PD's being used
    for permission check
    
    Orabug: 32716141
    
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    Signed-off-by: Rao Shoaib <rao.shoaib@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    
    Orabug: 32716152
    Fixes: 64545c693214 ("RDMA/rxe: Enhance Soft Roce to support Oracle proprietary shared PD extension")
    
    UEK6 ==> LUCI
    
    The real_rxepd value was erroneously being checked against itself.
    
    Signed-off-by: William Kucharski <william.kucharski@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- drivers/infiniband/sw/rxe/rxe_mr.c
+++ drivers/infiniband/sw/rxe/rxe_mr.c
@@ -627,7 +627,7 @@ struct rxe_mr *lookup_mr(struct rxe_pd *pd, int access, u32 key,
 	if (unlikely((type == RXE_LOOKUP_LOCAL && mr->lkey != key) ||
 		     (type == RXE_LOOKUP_REMOTE && mr->rkey != key) ||
 #ifndef WITHOUT_ORACLE_EXTENSIONS
-		     (mr_pd(mr)->real_rxepd != mr_pd(mr)->real_rxepd) ||
+		     (mr_pd(mr)->real_rxepd != pd->real_rxepd) ||
 #else
 		     (mr_pd(mr) != pd) ||
 #endif
