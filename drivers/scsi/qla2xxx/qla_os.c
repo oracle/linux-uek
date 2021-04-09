7d05c12fe28a180b5d6e3c1c813921d9bd12e5d6
Qiheng Lin <linqiheng@huawei.com>
2021-04-09 20:09:25 +0800

    scsi: qla2xxx: Remove unneeded if-null-free check
    Eliminate the following coccicheck warning:
    
    drivers/scsi/qla2xxx/qla_os.c:4622:2-7:
     WARNING: NULL check before some freeing functions is not needed.
    drivers/scsi/qla2xxx/qla_os.c:4637:3-8:
     WARNING: NULL check before some freeing functions is not needed.
    
    Link: https://lore.kernel.org/r/20210409120925.7122-1-linqiheng@huawei.com
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Signed-off-by: Qiheng Lin <linqiheng@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit efd2617100d9c6a49c8cd27160a2353b04844d78)
    
    Orabug: 33131554
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -4619,8 +4619,7 @@ qla2x00_free_fw_dump(struct qla_hw_data *ha)
 		dma_free_coherent(&ha->pdev->dev,
 		    EFT_SIZE, ha->eft, ha->eft_dma);
 
-	if (ha->fw_dump)
-		vfree(ha->fw_dump);
+	vfree(ha->fw_dump);
 
 	ha->fce = NULL;
 	ha->fce_dma = 0;
@@ -4634,8 +4633,7 @@ qla2x00_free_fw_dump(struct qla_hw_data *ha)
 	ha->fw_dump_len = 0;
 
 	for (j = 0; j < 2; j++, fwdt++) {
-		if (fwdt->template)
-			vfree(fwdt->template);
+		vfree(fwdt->template);
 		fwdt->template = NULL;
 		fwdt->length = 0;
 	}
