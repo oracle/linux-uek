3e40563e4395c427771811617e4105e6ec9bc2af
James Morse <james.morse@arm.com>
2021-04-30 12:52:48 +0100

    irqchip/gic-v3: Print DT partitions in the same way as APCI
    DT systems print out the details of each CPU in a partition as it
    is discovered. e.g.
    | GICv3: GIC: PPI partition interrupt-partition-1[1] { /cpus/cpu@100[4] /cpus/cpu@101[5] }
    
    Its going to be tricky to do this with ACPI as the PPTT helper returns
    a cpumask, the DSDT parsing that would produce paths is not available
    early enough. The simplest thing to do is print the set of CPUs.
    
    Printing slightly different debug information depending on how the
    platform booted is a bit naff. Change DT to just pring the set of
    CPUs too. The above example becomes:
    | GICv3: GIC: PPI partition: interrupt-partition-1 [1] { CPUs:<4-5> }
    
    Signed-off-by: James Morse <james.morse@arm.com>
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -2008,10 +2008,6 @@ static void __init gic_populate_ppi_partitions(struct device_node *gic_node)
 		part = &parts[part_idx];
 
 		part->partition_id = of_node_to_fwnode(child_part);
-
-		pr_info("GIC: PPI partition %pOFn[%d] { ",
-			child_part, part_idx);
-
 		n = of_property_count_elems_of_size(child_part, "affinity",
 						    sizeof(u32));
 		WARN_ON(n <= 0);
@@ -2036,13 +2032,12 @@ static void __init gic_populate_ppi_partitions(struct device_node *gic_node)
 				continue;
 			}
 
-			pr_cont("%pOF[%d] ", cpu_node, cpu);
-
 			cpumask_set_cpu(cpu, &part->mask);
 			of_node_put(cpu_node);
 		}
 
-		pr_cont("}\n");
+		pr_info("GIC: PPI partition:%pOFn [%d] { CPUs:<%*pbl> }",
+			child_part, part_idx, cpumask_pr_args(&part->mask));
 		part_idx++;
 	}
 
