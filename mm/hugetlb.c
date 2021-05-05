849554ac9fd47fbfbaaad1e792046be7b259de24
Mike Kravetz <mike.kravetz@oracle.com>
2021-05-04 18:34:52 -0700

    hugetlb: add per-hstate mutex to synchronize user adjustments
    The helper routine hstate_next_node_to_alloc accesses and modifies the
    hstate variable next_nid_to_alloc.  The helper is used by the routines
    alloc_pool_huge_page and adjust_pool_surplus.  adjust_pool_surplus is
    called with hugetlb_lock held.  However, alloc_pool_huge_page can not be
    called with the hugetlb lock held as it will call the page allocator.
    Two instances of alloc_pool_huge_page could be run in parallel or
    alloc_pool_huge_page could run in parallel with adjust_pool_surplus
    which may result in the variable next_nid_to_alloc becoming invalid for
    the caller and pages being allocated on the wrong node.
    
    Both alloc_pool_huge_page and adjust_pool_surplus are only called from
    the routine set_max_huge_pages after boot.  set_max_huge_pages is only
    called as the reusult of a user writing to the proc/sysfs nr_hugepages,
    or nr_hugepages_mempolicy file to adjust the number of hugetlb pages.
    
    It makes little sense to allow multiple adjustment to the number of
    hugetlb pages in parallel.  Add a mutex to the hstate and use it to only
    allow one hugetlb page adjustment at a time.  This will synchronize
    modifications to the next_nid_to_alloc variable.
    
    Link: https://lkml.kernel.org/r/20210409205254.242291-4-mike.kravetz@oracle.com
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Reviewed-by: Muchun Song <songmuchun@bytedance.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Cc: "Aneesh Kumar K . V" <aneesh.kumar@linux.ibm.com>
    Cc: Barry Song <song.bao.hua@hisilicon.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Hillf Danton <hdanton@sina.com>
    Cc: HORIGUCHI NAOYA <naoya.horiguchi@nec.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Roman Gushchin <guro@fb.com>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Orabug: 33032173
    
    (cherry picked from commit 2938396771c8fd0870b5284319f9e78b4b552a79)
    
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2420,6 +2420,11 @@ static int set_max_huge_pages(struct hstate *h, unsigned long count, int nid,
 	else
 		return -ENOMEM;
 
+	/*
+	 * resize_lock mutex prevents concurrent adjustments to number of
+	 * pages in hstate via the proc/sysfs interfaces.
+	 */
+	mutex_lock(&h->resize_lock);
 	spin_lock(&hugetlb_lock);
 
 	/*
@@ -2452,6 +2457,7 @@ static int set_max_huge_pages(struct hstate *h, unsigned long count, int nid,
 	if (hstate_is_gigantic(h) && !IS_ENABLED(CONFIG_CONTIG_ALLOC)) {
 		if (count > persistent_huge_pages(h)) {
 			spin_unlock(&hugetlb_lock);
+			mutex_unlock(&h->resize_lock);
 			NODEMASK_FREE(node_alloc_noretry);
 			return -EINVAL;
 		}
@@ -2526,6 +2532,7 @@ static int set_max_huge_pages(struct hstate *h, unsigned long count, int nid,
 out:
 	h->max_huge_pages = persistent_huge_pages(h);
 	spin_unlock(&hugetlb_lock);
+	mutex_unlock(&h->resize_lock);
 
 	NODEMASK_FREE(node_alloc_noretry);
 
@@ -2994,6 +3001,7 @@ void __init hugetlb_add_hstate(unsigned int order)
 	BUG_ON(hugetlb_max_hstate >= HUGE_MAX_HSTATE);
 	BUG_ON(order == 0);
 	h = &hstates[hugetlb_max_hstate++];
+	mutex_init(&h->resize_lock);
 	h->order = order;
 	h->mask = ~((1ULL << (order + PAGE_SHIFT)) - 1);
 	h->nr_huge_pages = 0;
