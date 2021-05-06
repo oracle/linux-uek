ff8ba7939aa3bcb6161763602e8e00865a622edf
James Morse <james.morse@arm.com>
2021-05-06 13:52:51 +0100

    ACPI / PPTT: Add a helper to fill a cpumask from a processor container
    The ACPI table for MPAM describes a set of CPUs with the UID of a
    processor container. These exist both in the namespace and the PPTT.
    
    Using the existing for-each helpers, provide a helper to find the
    specified processor container in the PPTT, and fill a cpumask
    with the CPUs that belong to it.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/acpi/pptt.c
+++ drivers/acpi/pptt.c
@@ -388,6 +388,40 @@ u32 acpi_pptt_count_containers(void)
 	return count;
 }
 
+struct __cpus_from_container_arg {
+	u32 acpi_cpu_id;
+	cpumask_t *cpus;
+};
+
+static int __cpus_from_container(struct acpi_pptt_processor *container, void *arg)
+{
+	struct __cpus_from_container_arg *params = arg;
+
+	if (container->acpi_processor_id == params->acpi_cpu_id)
+		acpi_pptt_get_child_cpus(container, params->cpus);
+
+	return 0;
+}
+
+/**
+ * acpi_pptt_get_cpus_from_container() - Populate a cpumask with all CPUs in a
+ * 					 processor containers
+ *
+ * Find the specified Processor Container, and fill cpus with all the cpus
+ * below it.
+ *
+ * Return: 0 for a complete walk, or an error if the mask is incomplete.
+ */
+int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus)
+{
+	struct __cpus_from_container_arg params;
+
+	params.acpi_cpu_id = acpi_cpu_id;
+	params.cpus = cpus;
+
+	cpumask_clear(cpus);
+	return acpi_pptt_for_each_container(&__cpus_from_container, &params);
+}
 
 static int acpi_find_cache_levels(struct acpi_table_header *table_hdr,
 				  u32 acpi_cpu_id)
