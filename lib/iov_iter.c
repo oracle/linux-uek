f8524fce6a88dd79509e0108e9e53f6d6330b723
Andreas Gruenbacher <agruenba@redhat.com>
2021-05-13 17:04:54 +0200

    iov_iter: Add iov_iter_fault_in_writeable()
    Add the equivalent of iov_iter_fault_in_readable(), but for pages that will be
    written to.
    
    While at it, fix an indentation error in iov_iter_fault_in_readable().
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- lib/iov_iter.c
+++ lib/iov_iter.c
@@ -480,12 +480,30 @@ int iov_iter_fault_in_readable(struct iov_iter *i, size_t bytes)
 		iterate_iovec(i, bytes, v, iov, skip, ({
 			err = fault_in_pages_readable(v.iov_base, v.iov_len);
 			if (unlikely(err))
-			return err;
+				return err;
 		0;}))
 	}
 	return 0;
 }
 EXPORT_SYMBOL(iov_iter_fault_in_readable);
+
+int iov_iter_fault_in_writeable(struct iov_iter *i, size_t bytes)
+{
+	size_t skip = i->iov_offset;
+	const struct iovec *iov;
+	int err;
+	struct iovec v;
+
+	if (!(i->type & (ITER_BVEC|ITER_KVEC))) {
+		iterate_iovec(i, bytes, v, iov, skip, ({
+			err = fault_in_pages_writeable(v.iov_base, v.iov_len);
+			if (unlikely(err))
+				return err;
+		0;}))
+	}
+	return 0;
+}
+EXPORT_SYMBOL(iov_iter_fault_in_writeable);
 
 void iov_iter_init(struct iov_iter *i, unsigned int direction,
 			const struct iovec *iov, unsigned long nr_segs,
