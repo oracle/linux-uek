668aa45c0fb04a517754351d2a2bcd4845f33946
Matthew Wilcox (Oracle) <willy@infradead.org>
2021-05-14 17:27:24 -0700

    mm: fix struct page layout on 32-bit systems
    commit 9ddb3c14afba8bc5950ed297f02d4ae05ff35cd1 upstream.
    
    32-bit architectures which expect 8-byte alignment for 8-byte integers and
    need 64-bit DMA addresses (arm, mips, ppc) had their struct page
    inadvertently expanded in 2019.  When the dma_addr_t was added, it forced
    the alignment of the union to 8 bytes, which inserted a 4 byte gap between
    'flags' and the union.
    
    Fix this by storing the dma_addr_t in one or two adjacent unsigned longs.
    This restores the alignment to that of an unsigned long.  We always
    store the low bits in the first word to prevent the PageTail bit from
    being inadvertently set on a big endian platform.  If that happened,
    get_user_pages_fast() racing against a page which was freed and
    reallocated to the page_pool could dereference a bogus compound_head(),
    which would be hard to trace back to this cause.
    
    Link: https://lkml.kernel.org/r/20210510153211.1504886-1-willy@infradead.org
    Fixes: c25fff7171be ("mm: add dma_addr_t to struct page")
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Acked-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Acked-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Tested-by: Matteo Croce <mcroce@linux.microsoft.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit ecdf893c5aefa26f4d46ced6e17513fc8fa23d2a)
    
    Fix KABI violation in include/linux/mm_types.h
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -96,10 +96,10 @@ struct page {
 		};
 		struct {	/* page_pool used by netstack */
 			/**
-			 * @dma_addr: might require a 64-bit value even on
+			 * @dma_addr: might require a 64-bit value on
 			 * 32-bit architectures.
 			 */
-			dma_addr_t dma_addr;
+			UEK_KABI_REPLACE(dma_addr_t dma_addr, unsigned long dma_addr[2])
 		};
 		struct {	/* slab, slob and slub */
 			union {
