dc5fa5e2232c8d6081e742153a820aae56a09a77
Xie Yongji <xieyongji@bytedance.com>
2021-05-31 21:58:52 +0800

    virtio-net: Add validation for used length
    commit ad993a95c508417acdeb15244109e009e50d8758 upstream.
    
    This adds validation for used length (might come
    from an untrusted device) to avoid data corruption
    or loss.
    
    Signed-off-by: Xie Yongji <xieyongji@bytedance.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Link: https://lore.kernel.org/r/20210531135852.113-1-xieyongji@bytedance.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ Larry: backport to 5.4.y. Minor conflict resolved due to missing commit 9ce6146ec7b50
      virtio_net: Add XDP frame size in two code paths ]
    Signed-off-by: Larry Bassel <larry.bassel@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit c1b40d1959517ff2ea473d40eeab4691d6d62462)
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -720,6 +720,12 @@ static struct sk_buff *receive_small(struct net_device *dev,
 	len -= vi->hdr_len;
 	stats->bytes += len;
 
+	if (unlikely(len > GOOD_PACKET_LEN)) {
+		pr_debug("%s: rx error: len %u exceeds max size %d\n",
+			 dev->name, len, GOOD_PACKET_LEN);
+		dev->stats.rx_length_errors++;
+		goto err_len;
+	}
 	rcu_read_lock();
 	xdp_prog = rcu_dereference(rq->xdp_prog);
 	if (xdp_prog) {
@@ -822,6 +828,7 @@ err:
 err_xdp:
 	rcu_read_unlock();
 	stats->xdp_drops++;
+err_len:
 	stats->drops++;
 	put_page(page);
 xdp_xmit:
@@ -874,6 +881,13 @@ static struct sk_buff *receive_mergeable(struct net_device *dev,
 	head_skb = NULL;
 	stats->bytes += len - vi->hdr_len;
 
+	truesize = mergeable_ctx_to_truesize(ctx);
+	if (unlikely(len > truesize)) {
+		pr_debug("%s: rx error: len %u exceeds truesize %lu\n",
+			 dev->name, len, (unsigned long)ctx);
+		dev->stats.rx_length_errors++;
+		goto err_skb;
+	}
 	rcu_read_lock();
 	xdp_prog = rcu_dereference(rq->xdp_prog);
 	if (xdp_prog) {
@@ -992,14 +1006,6 @@ static struct sk_buff *receive_mergeable(struct net_device *dev,
 		}
 	}
 	rcu_read_unlock();
-
-	truesize = mergeable_ctx_to_truesize(ctx);
-	if (unlikely(len > truesize)) {
-		pr_debug("%s: rx error: len %u exceeds truesize %lu\n",
-			 dev->name, len, (unsigned long)ctx);
-		dev->stats.rx_length_errors++;
-		goto err_skb;
-	}
 
 	head_skb = page_to_skb(vi, rq, page, offset, len, truesize, !xdp_prog,
 			       metasize);
