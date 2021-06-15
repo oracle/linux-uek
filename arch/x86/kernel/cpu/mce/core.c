f49aae0806878ba121c61e9ed7a7afab8eb95c2b
William Roche <william.roche@oracle.com>
2021-06-15 08:42:56 +0000

    Revert "Allow mce to reset instead of panic on UE"
    This reverts commit b4044e4199b50cd94e7b2de77b15f1633a211290.
    
    There are 2 main reasons to revert this UEK specific code:
    . The misleading stack problem described in 24745271/27199035
    no longer exists.
    . A printk() message from an NMI handler may not reach the
    console before UEK resets the machine, losing the MCE reset
    reason information. So, as it's done upstream, we need to panic()
    in the case of a fatal MCE in order to take advantage of the
    panic reason reporting either on the console or from the kdump
    generated crashdump data.
    
    In the same time, we also have to revert the complementary
    fix for 31026187.
    Revert "x86/mce: Restart the system when LMCE UE error occurs"
    This reverts commit 9c9eb4869d2b1d3b4516da13c0558b05842a6dd1.
    
    Orabug: 32820277
    
    Signed-off-by: William Roche <william.roche@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- arch/x86/kernel/cpu/mce/core.c
+++ arch/x86/kernel/cpu/mce/core.c
@@ -42,7 +42,6 @@
 #include <linux/export.h>
 #include <linux/jump_label.h>
 #include <linux/set_memory.h>
-#include <linux/reboot.h>
 
 #include <asm/intel-family.h>
 #include <asm/processor.h>
@@ -278,7 +277,6 @@ static atomic_t mce_panicked;
 
 static int fake_panic;
 static atomic_t mce_fake_panicked;
-static atomic_t mce_reboot;
 
 /* Panic in progress. Enable interrupts and wait for final IPI */
 static void wait_for_panic(void)
@@ -287,29 +285,8 @@ static void wait_for_panic(void)
 
 	preempt_disable();
 	local_irq_enable();
-
-	/* 
-	 * If two cpus get into mce_panic() very close together,
-	 * the second CPU will wait for mce_reboot flag. The lead CPU 
-	 * should set the flag within PANIC_TIMEOUT seconds.
-	 */
 	while (timeout-- > 0)
-	{
-		/* 
-		 * The intent of this patch is to ensure that the mce stack 
-		 * is not put in the panic stack trace when the kernel reboots 
-		 * due to the Uncorrectable Error. The mce stack in the panic trace 
-		 * confuses the administrator and falsely implicates mce module
-		 * as a culprit.
-		 */ 
-		if(atomic_read(&mce_reboot))
-			machine_emergency_restart();
 		udelay(1);
-	}
-	/* 
-	 * If the mce_reboot flag is not set within PANIC_TIMEOUT secs
-	 * call kernel panic.
-	 */
 	if (panic_timeout == 0)
 		panic_timeout = mca_cfg.panic_timeout;
 	panic("Panicing machine check CPU died");
@@ -320,7 +297,6 @@ static void mce_panic(const char *msg, struct mce *final, char *exp)
 	int apei_err = 0;
 	struct llist_node *pending;
 	struct mce_evt_llist *l;
-	bool uncorrectable = false;
 
 	if (!fake_panic) {
 		/*
@@ -357,18 +333,9 @@ static void mce_panic(const char *msg, struct mce *final, char *exp)
 			if (!apei_err)
 				apei_err = apei_write_mce(m);
 		}
-		/*
-		 * Set the flag for any uncorreactable errors. But turn on 
-		 * mce_reboot flag only after printing MCE errors. Otherwise,
-		 * we may end up restarting the system without printing mce 
-		 * logs.
-		 */
-		uncorrectable = true;
 	}
 	if (final) {
 		print_mce(final);
-		if (final->status & MCI_STATUS_UC)
-			uncorrectable = true;
 		if (!apei_err)
 			apei_err = apei_write_mce(final);
 	}
@@ -376,11 +343,11 @@ static void mce_panic(const char *msg, struct mce *final, char *exp)
 		pr_emerg(HW_ERR "Some CPUs didn't answer in synchronization\n");
 	if (exp)
 		pr_emerg(HW_ERR "Machine check: %s\n", exp);
-	if(uncorrectable)
-		atomic_inc(&mce_reboot);
-	if (!fake_panic) 
-		wait_for_panic();
-	else
+	if (!fake_panic) {
+		if (panic_timeout == 0)
+			panic_timeout = mca_cfg.panic_timeout;
+		panic(msg);
+	} else
 		pr_emerg(HW_ERR "Fake kernel panic: %s\n", msg);
 }
 
@@ -2425,7 +2392,6 @@ static void mce_reset(void)
 {
 	cpu_missing = 0;
 	atomic_set(&mce_fake_panicked, 0);
-	atomic_set(&mce_reboot, 0);
 	atomic_set(&mce_executing, 0);
 	atomic_set(&mce_callin, 0);
 	atomic_set(&global_nwo, 0);
