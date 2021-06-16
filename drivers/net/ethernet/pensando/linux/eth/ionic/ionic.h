affc69bfd676bdd7ffd0d649949ac7cb157fe223
Vijay Kumar <vijay.ac.kumar@oracle.com>
2021-06-16 15:54:19 -0500

    net/ethernet/pensando: Fix Pensando driver build
    Removed files from  Pensando dir:
    -----------------------------------
    * Removed pensando/linux/Makefile
    * Removed pensando/linux/eth/ionic/Kconfig
    * Removed pensando/linux/linux_ver.mk
    
    Added following in Pensando dir files:
    -------------------------------------------
    * Added drivers/net/ethernet/pensando/Kconfig
    * Added drivers/net/ethernet/pensando/Makefile
    * Updated drivers/net/ethernet/Kconfig for "pensando"
    * Updated drivers/net/ethernet/Makefile for CONFIG_NET_VENDOR_PENSANDO
    * Updated IONIC_DRV_VERSION in drivers/net/ethernet//pensando/linux/Makefile with  pensando/linux/Makefile's DVER.
        See this: github.com/pensando/dsc-drivers/drivers/linux/Makefile
        VER = $(shell echo $$SW_VERSION)
        ifeq ($(DVER),)
           DVER = $(shell git describe --tags 2>/dev/null)
           ifeq ($(DVER),)
             DVER = "1.15.8.12"          <============
           endif
        endif
    
    Orabug: 32569849
    
    Signed-off-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

--- drivers/net/ethernet/pensando/linux/eth/ionic/ionic.h
+++ drivers/net/ethernet/pensando/linux/eth/ionic/ionic.h
@@ -14,7 +14,7 @@ struct ionic_lif;
 
 #define IONIC_DRV_NAME		"ionic"
 #define IONIC_DRV_DESCRIPTION	"Pensando Ethernet NIC Driver"
-#define IONIC_DRV_VERSION	drv_ver
+#define IONIC_DRV_VERSION	"1.15.8.12"
 
 #define PCI_VENDOR_ID_PENSANDO			0x1dd8
 
