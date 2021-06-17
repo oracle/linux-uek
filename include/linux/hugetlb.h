eae0df7ce9286ac5766b5988576b70af2eded74f
Muchun Song <songmuchun@bytedance.com>
2021-06-17 15:18:02 +1000

    mm: sparsemem: use huge PMD mapping for vmemmap pages
    The preparation of splitting huge PMD mapping of vmemmap pages is ready,
    so switch the mapping from PTE to PMD.
    
    Link: https://lkml.kernel.org/r/20210616094915.34432-3-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Chen Huang <chenhuang5@huawei.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Xiongchun Duan <duanxiongchun@bytedance.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    
    Orabug: 33032173
    
    (cherry picked from commit 7d41d8046c5ad3b50c4752e23807e7e4b24d33b7)
    cherry-pick repo linux-next
    
    Conflicts:
    	arch/x86/mm/init_64.c
    	mm/memory_hotplug.c
    	  UEK does not have commit a08a2ae3461 ("mm,memory_hotplug:
    	  allocate memmap from the added memory range") and commit
    	  2a13468d36677 ("mm: memory_hotplug: disable memmap_on_memory
    	  when hugetlb_free_vmemmap enabled") from linux-next
    	  therefore no need to adjust mhp_supports_memmap_on_memory().
    
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -697,20 +697,6 @@ static inline void huge_ptep_modify_prot_commit(struct vm_area_struct *vma,
 }
 #endif
 
-#ifdef CONFIG_HUGETLB_PAGE_FREE_VMEMMAP
-extern bool hugetlb_free_vmemmap_enabled;
-
-static inline bool is_hugetlb_free_vmemmap_enabled(void)
-{
-	return hugetlb_free_vmemmap_enabled;
-}
-#else
-static inline bool is_hugetlb_free_vmemmap_enabled(void)
-{
-	return false;
-}
-#endif
-
 #else	/* CONFIG_HUGETLB_PAGE */
 struct hstate {};
 
@@ -864,12 +850,13 @@ static inline void set_huge_swap_pte_at(struct mm_struct *mm, unsigned long addr
 					pte_t *ptep, pte_t pte, unsigned long sz)
 {
 }
-
-static inline bool is_hugetlb_free_vmemmap_enabled(void)
-{
-	return false;
-}
 #endif	/* CONFIG_HUGETLB_PAGE */
+
+#ifdef CONFIG_HUGETLB_PAGE_FREE_VMEMMAP
+extern bool hugetlb_free_vmemmap_enabled;
+#else
+#define hugetlb_free_vmemmap_enabled	false
+#endif
 
 static inline spinlock_t *huge_pte_lock(struct hstate *h,
 					struct mm_struct *mm, pte_t *pte)
