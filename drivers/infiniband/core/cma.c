f02175ac2181805b93ce6448b120ccbe860e68e3
Leon Romanovsky <leonro@nvidia.com>
2021-06-29 09:49:33 +0300

    RDMA/core: Always release restrack object
    Change location of rdma_restrack_del() to fix the bug where
    task_struct was acquired but not released, causing to resource leak.
    
      ucma_create_id() {
        ucma_alloc_ctx();
        rdma_create_user_id() {
          rdma_restrack_new();
          rdma_restrack_set_name() {
            rdma_restrack_attach_task.part.0(); <--- task_struct was gotten
          }
        }
        ucma_destroy_private_ctx() {
          ucma_put_ctx();
          rdma_destroy_id() {
            _destroy_id()                       <--- id_priv was freed
          }
        }
      }
    
    Fixes: 889d916b6f8a ("RDMA/core: Don't access cm_id after its destruction")
    Link: https://lore.kernel.org/r/073ec27acb943ca8b6961663c47c5abe78a5c8cc.1624948948.git.leonro@nvidia.com
    Reported-by: Pavel Skripkin <paskripkin@gmail.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Orabug: 34704742
    
    (cherry picked from commit 3d8287544223a3d2f37981c1f9ffd94d0b5e9ffc)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/infiniband/core/cma.c
                Minor conflict in the context of rdma_destroy_id() was
                resolved manually.
    
    Suggested-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -2033,6 +2033,7 @@ void rdma_destroy_id(struct rdma_cm_id *id)
 	mutex_lock(&id_priv->handler_mutex);
 	mutex_unlock(&id_priv->handler_mutex);
 
+	rdma_restrack_del(&id_priv->res);
 	if (id_priv->cma_dev) {
 		if (rdma_cap_ib_cm(id_priv->id.device, 1)) {
 			if (id_priv->cm_id.ib)
@@ -2042,7 +2043,6 @@ void rdma_destroy_id(struct rdma_cm_id *id)
 				iw_destroy_cm_id(id_priv->cm_id.iw);
 		}
 		cma_leave_mc_groups(id_priv);
-		rdma_restrack_del(&id_priv->res);
 		cma_release_dev(id_priv);
 	}
 
@@ -2057,7 +2057,6 @@ void rdma_destroy_id(struct rdma_cm_id *id)
 	kfree(id_priv->id.route.path_rec);
 
 	put_net(id_priv->id.route.addr.dev_addr.net);
-	rdma_restrack_del(&id_priv->res);
 	kfree(id_priv);
 #else
 	INIT_WORK(&id_priv->work, __rdma_free);
