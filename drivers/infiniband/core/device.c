6d71e7b93a297d7959d7afa567362bfdce430dee
Anand Khoje <anand.a.khoje@oracle.com>
2021-07-26 04:59:39 +0000

    IB/core: Read subnet_prefix in ib_query_port via cache.
    ib_query_port() calls device->ops.query_port() to get the port
    attributes. The method of querying is device driver specific.
    The same function calls device->ops.query_gid() to get the GID and
    extract the subnet_prefix (gid_prefix).
    
    The GID and subnet_prefix are stored in a cache. But they do not get
    read from the cache if the device is an Infiniband device. The
    following change takes advantage of the cached subnet_prefix.
    Testing with RDBMS has shown a significant improvement in performance
    with this change.
    
    Fixes: fad61ad ("IB/core: Add subnet prefix to port info")
    Link: https://lore.kernel.org/r/20210712122625.1147-4-anand.a.khoje@oracle.com
    Signed-off-by: Anand Khoje <anand.a.khoje@oracle.com>
    Signed-off-by: Haakon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    (cherry picked from commit 21bfee9c0c7754408b1f311bffe304caf3e62250)
    cherry-pick-repo=kernel/git/rdma/rdma.git
    branch=origin/wip/jgg-for-next
    
    Orabug: 33134286
    
    Signed-off-by: Anand Khoje <anand.a.khoje@oracle.com>
    Reviewed-by: Praveen Kumar Kannoju <praveen.kannoju@oracle.com>
    
    Conflicts:
    	drivers/infiniband/core/device.c
    	* Upstream has different line numbers.

--- drivers/infiniband/core/device.c
+++ drivers/infiniband/core/device.c
@@ -2023,7 +2023,6 @@ static int __ib_query_port(struct ib_device *device,
 			   u8 port_num,
 			   struct ib_port_attr *port_attr)
 {
-	union ib_gid gid = {};
 	int err;
 
 	memset(port_attr, 0, sizeof(*port_attr));
@@ -2036,11 +2035,8 @@ static int __ib_query_port(struct ib_device *device,
 	    IB_LINK_LAYER_INFINIBAND)
 		return 0;
 
-	err = device->ops.query_gid(device, port_num, 0, &gid);
-	if (err)
-		return err;
-
-	port_attr->subnet_prefix = be64_to_cpu(gid.global.subnet_prefix);
+	ib_get_cached_subnet_prefix(device, port_num,
+		&port_attr->subnet_prefix);
 	return 0;
 }
 
