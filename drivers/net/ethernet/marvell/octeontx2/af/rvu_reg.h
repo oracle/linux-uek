723fe2a6df2c1e95ba9c467150de768980b09f8f
Sunil Goutham <sgoutham@marvell.com>
2021-08-05 12:04:32 +0530

    octeontx2-af: cn10k: DWRR MTU configuration
    On OcteonTx2 DWRR quantum is directly configured into each of
    the transmit scheduler queues. And PF/VF drivers were free to
    config any value upto 2^24.
    
    On CN10K, HW is modified, the quantum configuration at scheduler
    queues is in terms of weight. And SW needs to setup a base DWRR MTU
    at NIX_AF_DWRR_RPM_MTU / NIX_AF_DWRR_SDP_MTU. HW will do
    'DWRR MTU * weight' to get the quantum. For LBK traffic, value
    programmed into NIX_AF_DWRR_RPM_MTU register is considered as
    DWRR MTU.
    
    This patch programs a default DWRR MTU of 8192 into HW and also
    provides a way to change this via devlink params.
    
    Change-Id: I49e96df48b9db2d7fc807f6979c97faabbff2380
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/57983
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit fac05b9d748a3d3f11e9ce0f9d0143ce22a0bf4b)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_reg.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_reg.h
@@ -282,6 +282,8 @@
 #define NIX_AF_SMQX_CFG(a)                      (0x700 | (a) << 16)
 #define NIX_AF_SMQX_STATUS(a)			(0x730 | (a) << 16)
 #define NIX_AF_SQM_DBG_CTL_STATUS		(0x750)
+#define NIX_AF_DWRR_SDP_MTU                     (0x790)
+#define NIX_AF_DWRR_RPM_MTU                     (0x7A0)
 #define NIX_AF_PSE_CHANNEL_LEVEL                (0x800)
 #define NIX_AF_PSE_SHAPER_CFG                   (0x810)
 #define NIX_AF_MARK_FORMATX_CTL(a)              (0x900 | (a) << 18)
