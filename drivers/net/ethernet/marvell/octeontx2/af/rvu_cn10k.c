3050c0ea118130bbc16f381abcbbc3055226aa16
Pavan Nikhilesh <pbhagavatula@marvell.com>
2021-08-06 16:27:27 +0530

    octeontx2-af: cn10K: modify default vWQE timer interval
    Set the NIX AF vWQE timer interval to 0x3F which translates to 6400ns
    This allows NIX LFs to configure vWQE timeout between 6.4us to 1.6ms.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: I8fcd306e000bdf4109114b4c712d4b8fcb37bcfc
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/58087
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit da90cf172b4877cfe1d58cb658f7db28fc592fe3)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
@@ -552,8 +552,10 @@ void rvu_nix_block_cn10k_init(struct rvu *rvu, struct nix_hw *nix_hw)
 {
 	int blkaddr = nix_hw->blkaddr;
 
-	/* Set vWQE timer interval to max possible value i.e. 102us. */
-	rvu_write64(rvu, blkaddr, NIX_AF_VWQE_TIMER, 0x3FFULL);
+	/* Set AF vWQE timer interval to a LF configurable range of
+	 * 6.4us to 1.632ms.
+	 */
+	rvu_write64(rvu, blkaddr, NIX_AF_VWQE_TIMER, 0x3FULL);
 }
 
 void rvu_apr_block_cn10k_init(struct rvu *rvu)
