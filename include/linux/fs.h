4a64ef5eaf21e5210706acbb617f3032582a1a4c
Prakash Sangappa <prakash.sangappa@oracle.com>
2021-08-10 17:18:06 -0700

    Fix kabi issue in struct address_space.
    Use UEK_KABI_USE() for the new member 'nr_thps'.
    
    Orabug: 33190199
    
    Signed-off-by: Prakash Sangappa <prakash.sangappa@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Victor Erminpour <victor.erminpour@oracle.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -451,7 +451,6 @@ int pagecache_write_end(struct file *, struct address_space *mapping,
  * @i_pages: Cached pages.
  * @gfp_mask: Memory allocation flags to use for allocating pages.
  * @i_mmap_writable: Number of VM_SHARED mappings.
- * @nr_thps: Number of THPs in the pagecache (non-shmem only).
  * @i_mmap: Tree of private and shared mappings.
  * @i_mmap_rwsem: Protects @i_mmap and @i_mmap_writable.
  * @nrpages: Number of page entries, protected by the i_pages lock.
@@ -463,16 +462,13 @@ int pagecache_write_end(struct file *, struct address_space *mapping,
  * @private_lock: For use by the owner of the address_space.
  * @private_list: For use by the owner of the address_space.
  * @private_data: For use by the owner of the address_space.
+ * @nr_thps: Number of THPs in the pagecache (non-shmem only).
  */
 struct address_space {
 	struct inode		*host;
 	struct xarray		i_pages;
 	gfp_t			gfp_mask;
 	atomic_t		i_mmap_writable;
-#ifdef CONFIG_READ_ONLY_THP_FOR_FS
-	/* number of thp, only for non-shmem files */
-	atomic_t		nr_thps;
-#endif
 	struct rb_root_cached	i_mmap;
 	struct rw_semaphore	i_mmap_rwsem;
 	unsigned long		nrpages;
@@ -484,8 +480,13 @@ struct address_space {
 	spinlock_t		private_lock;
 	struct list_head	private_list;
 	void			*private_data;
-
+#ifdef CONFIG_READ_ONLY_THP_FOR_FS
+	/* number of thp, only for non-shmem files */
+	UEK_KABI_USE(1, atomic_t        nr_thps)
+#else
 	UEK_KABI_RESERVE(1)
+#endif
+
 	UEK_KABI_RESERVE(2)
 	UEK_KABI_RESERVE(3)
 	UEK_KABI_RESERVE(4)
