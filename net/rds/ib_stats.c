409f0f7dea5ac9e4f81ce5ed23b2037ec566da2f
Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
2021-08-12 08:25:31 +0200

    rds: fix statistics counters and check for memory leak
    The statistics counters for memory-allocation and cache-usage are not
    correctly updated in current codebase.  A check for memory leak is
    added when freeing devices; a warning will printed if the
    allocation-counter is nonzero.
    
    Orabug: 31372378
    Signed-off-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 31fc754b1f14532530a5a35ff3ea95c8e7138342)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 9f850676e4c120ceaa8cbde1cd8cc49ed982395a)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/ib_stats.c
+++ net/rds/ib_stats.c
@@ -129,3 +129,26 @@ unsigned int rds_ib_stats_info_copy(struct rds_info_iterator *iter,
 out:
 	return ARRAY_SIZE(rds_ib_stat_names);
 }
+
+void rds_ib_stats_print(const char *where)
+{
+	struct rds_ib_statistics ibstats = {};
+	uint64_t *src;
+	uint64_t *sum;
+	size_t i;
+	int cpu;
+	size_t nibstats = sizeof(ibstats) / sizeof(uint64_t);
+
+	for_each_online_cpu(cpu) {
+		src = (uint64_t *)&(per_cpu(rds_ib_stats, cpu));
+		sum = (uint64_t *)&ibstats;
+		for (i = 0; i < nibstats; i++)
+			*(sum++) += *(src++);
+	}
+
+	sum = (uint64_t *)&ibstats;
+	for (i = 0; i < nibstats; i++)
+		if (sum[i])
+			pr_info("%s %s %lld\n", where, rds_ib_stat_names[i], sum[i]);
+}
+EXPORT_SYMBOL_GPL(rds_ib_stats_print);
