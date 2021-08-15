9817547a9ba6b6bfe8086bcff9a567b41fee470f
Han Wang <zjuwanghan@outlook.com>
2021-08-15 21:07:29 +0800

    cpufreq: remove useless INIT_LIST_HEAD()
    list cpu_data_list has been inited staticly through LIST_HEAD,
    so there's no need to call another INIT_LIST_HEAD. Simply remove
    it from cppc_cpufreq_init.
    
    Signed-off-by: Han Wang <zjuwanghan@outlook.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    (cherry picked from commit 6065a672679f0502ede024a03db82c03534a5e00)
    
    Orabug: 38471683
    
    Signed-off-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/cpufreq/cppc_cpufreq.c
+++ drivers/cpufreq/cppc_cpufreq.c
@@ -870,8 +870,6 @@ static int __init cppc_cpufreq_init(void)
 	if (!acpi_cpc_valid())
 		return -ENODEV;
 
-	INIT_LIST_HEAD(&cpu_data_list);
-
 	cppc_check_hisi_workaround();
 	cppc_freq_invariance_init();
 	populate_efficiency_class();
