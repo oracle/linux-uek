1818e6f8ffc34b4befc4728ce5e2e489d9f999ae
Anand Khoje <anand.a.khoje@oracle.com>
2021-08-26 06:33:16 +0000

    IB/core: Updating cache for subnet_prefix in config_non_roce_gid_cache()
    Currently, cache for subnet_prefix was getting updated by reading port
    attributes via ib_query_port. ib_query_port() calls ops.query_gid()
    to get subnet_prefix and returns it via port_attr.
    
    In ib_cache_update(), config_non_roce_gid_cache() obtains GIDs
    by calling ops.query_gid(). We utilize this to store subnet_prefix
    in cache.
    
    Link: https://lore.kernel.org/r/20210712122625.1147-2-anand.a.khoje@oracle.com
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Suggested-by: Aru Kolappan <aru.kolappan@oracle.com>
    Signed-off-by: Anand Khoje <anand.a.khoje@oracle.com>
    Signed-off-by: Haakon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    (cherry picked from commit 0bc0602ae8275aadc7288692331589350987cef7)
    cherry-pick-repo=kernel/git/rdma/rdma.git
    branch=origin/wip/jgg-for-next
    
    Orabug: 33283556
    
    Signed-off-by: Anand Khoje <anand.a.khoje@oracle.com>
    Reviewed-by: Praveen Kumar Kannoju <praveen.kannoju@oracle.com>

--- drivers/infiniband/core/cache.c
+++ drivers/infiniband/core/cache.c
@@ -1424,7 +1424,7 @@ int rdma_read_gid_l2_fields(const struct ib_gid_attr *attr,
 EXPORT_SYMBOL(rdma_read_gid_l2_fields);
 
 static int config_non_roce_gid_cache(struct ib_device *device,
-				     u32 port, int gid_tbl_len)
+				     u32 port, struct ib_port_attr *tprops)
 {
 	struct ib_gid_attr gid_attr = {};
 	struct ib_gid_table *table;
@@ -1436,7 +1436,7 @@ static int config_non_roce_gid_cache(struct ib_device *device,
 	table = rdma_gid_table(device, port);
 
 	mutex_lock(&table->lock);
-	for (i = 0; i < gid_tbl_len; ++i) {
+	for (i = 0; i < tprops->gid_tbl_len; ++i) {
 		if (!device->ops.query_gid)
 			continue;
 		ret = device->ops.query_gid(device, port, i, &gid_attr.gid);
@@ -1447,6 +1447,8 @@ static int config_non_roce_gid_cache(struct ib_device *device,
 			goto err;
 		}
 		gid_attr.index = i;
+		tprops->subnet_prefix =
+			be64_to_cpu(gid_attr.gid.global.subnet_prefix);
 		add_modify_gid(table, &gid_attr);
 	}
 err:
@@ -1479,7 +1481,7 @@ ib_cache_update(struct ib_device *device, u8 port, bool update_gids,
 
 	if (!rdma_protocol_roce(device, port) && update_gids) {
 		ret = config_non_roce_gid_cache(device, port,
-						tprops->gid_tbl_len);
+						tprops);
 		if (ret)
 			goto err;
 	}
