4b875ab3d391f8f6aa4b82da9e9d7a07991f8240
Mark Haywood <mark.haywood@oracle.com>
2021-09-10 21:08:31 +0200

    net/rds: Add basic support for network namespaces
    Add basic support for network namespaces to RDS by implementing per-net
    namespace operations and converting the global RDS socket list to
    per-namespace lists.
    
    Orabug: 29822296
    
    Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
    Signed-off-by: Greg Jumper <greg.jumper@oracle.com>
    Reviewed-by: Alan Maguire <alan.maguire@oracle.com>

---
+++ net/rds/rds_ns.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2007, 2021 Oracle and/or its affiliates.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#include <net/net_namespace.h>
+#include <net/netns/generic.h>
+#include "rds.h"
+
+static int rds_netid;
+
+struct rds_net *rds_ns(struct net *net)
+{
+	struct rds_net *rns = net_generic(net, rds_netid);
+
+	return rns;
+}
+EXPORT_SYMBOL(rds_ns);
+
+static __net_init int rds_init_net(struct net *net)
+{
+	struct rds_net *rns;
+
+	rns = rds_ns(net);
+
+	memset(rns, 0, sizeof(*rns));
+
+	mutex_init(&rns->rns_sock_lock);
+	INIT_LIST_HEAD(&rns->rns_sock_list);
+
+	return 0;
+}
+
+static void rds_exit_net(struct net *net)
+{
+	struct rds_net *rns = rds_ns(net);
+
+	mutex_destroy(&rns->rns_sock_lock);
+}
+
+static struct pernet_operations rds_net_ops = {
+	.init = rds_init_net,
+	.exit = rds_exit_net,
+	.id = &rds_netid,
+	.size = sizeof(struct rds_net),
+};
+
+int rds_reg_pernet(void)
+{
+	return register_pernet_device(&rds_net_ops);
+}
+
+void rds_unreg_pernet(void)
+{
+	unregister_pernet_device(&rds_net_ops);
+}
