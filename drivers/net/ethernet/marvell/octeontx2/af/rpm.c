a0a1d9d0de5f6abbbbe05534d7b0cdf57129a0c8
Hariprasad Kelam <hkelam@marvell.com>
2021-09-15 22:35:50 +0530

    octeontx2-af: cn10k: rpm : code placement
    Ensure codeplacement inline with upstream kernel.
    
    Change-Id: I71b0d4675bce8aeb93c7f02cecb36f343d2e77e9
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/61387
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit cdaaee4263750e15fafff71d8e1f741b1e8c264f)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rpm.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rpm.c
@@ -54,99 +54,6 @@ int rpm_get_nr_lmacs(void *rpmd)
 	return hweight8(rpm_read(rpm, 0, CGXX_CMRX_RX_LMACS) & 0xFULL);
 }
 
-u8 rpm_get_lmac_type(void *rpmd, int lmac_id)
-{
-	rpm_t *rpm = rpmd;
-	u64 req = 0, resp;
-	int err;
-
-	req = FIELD_SET(CMDREG_ID, CGX_CMD_GET_LINK_STS, req);
-	err = cgx_fwi_cmd_generic(req, &resp, rpm, 0);
-	if (!err)
-		return FIELD_GET(RESP_LINKSTAT_LMAC_TYPE, resp);
-	return err;
-}
-
-int rpm_lmac_internal_loopback(void *rpmd, int lmac_id, bool enable)
-{
-	rpm_t *rpm = rpmd;
-	u8 lmac_type;
-	u64 cfg;
-
-	if (!is_lmac_valid(rpm, lmac_id))
-		return -ENODEV;
-	lmac_type = rpm->mac_ops->get_lmac_type(rpm, lmac_id);
-	if (lmac_type == LMAC_MODE_100G_R) {
-		cfg = rpm_read(rpm, lmac_id, RPMX_MTI_PCS100X_CONTROL1);
-
-		if (enable)
-			cfg |= RPMX_MTI_PCS_LBK;
-		else
-			cfg &= ~RPMX_MTI_PCS_LBK;
-		rpm_write(rpm, lmac_id, RPMX_MTI_PCS100X_CONTROL1, cfg);
-	} else {
-		cfg = rpm_read(rpm, 0, RPMX_MTI_LPCSX_CONTROL(lmac_id));
-		if (enable)
-			cfg |= RPMX_MTI_PCS_LBK;
-		else
-			cfg &= ~RPMX_MTI_PCS_LBK;
-		rpm_write(rpm, 0, RPMX_MTI_LPCSX_CONTROL(lmac_id), cfg);
-	}
-
-	return 0;
-}
-
-int rpm_get_rx_stats(void *rpmd, int lmac_id, int idx, u64 *rx_stat)
-{
-	rpm_t *rpm = rpmd;
-	u64 val_lo, val_hi;
-
-	if (!is_lmac_valid(rpm, lmac_id))
-		return -ENODEV;
-
-	mutex_lock(&rpm->lock);
-
-	/* Update idx to point per lmac Rx statistics page */
-	idx += lmac_id * rpm->mac_ops->rx_stats_cnt;
-
-	/* Read lower 32 bits of counter */
-	val_lo = rpm_read(rpm, 0, RPMX_MTI_STAT_RX_STAT_PAGES_COUNTERX +
-			  (idx * 8));
-
-	/* upon read of lower 32 bits, higher 32 bits are written
-	 * to RPMX_MTI_STAT_DATA_HI_CDC
-	 */
-	val_hi = rpm_read(rpm, 0, RPMX_MTI_STAT_DATA_HI_CDC);
-
-	*rx_stat = (val_hi << 32 | val_lo);
-
-	mutex_unlock(&rpm->lock);
-	return 0;
-}
-
-int rpm_get_tx_stats(void *rpmd, int lmac_id, int idx, u64 *tx_stat)
-{
-	rpm_t *rpm = rpmd;
-	u64 val_lo, val_hi;
-
-	if (!is_lmac_valid(rpm, lmac_id))
-		return -ENODEV;
-
-	mutex_lock(&rpm->lock);
-
-	/* Update idx to point per lmac Tx statistics page */
-	idx += lmac_id * rpm->mac_ops->tx_stats_cnt;
-
-	val_lo = rpm_read(rpm, 0, RPMX_MTI_STAT_TX_STAT_PAGES_COUNTERX +
-			    (idx * 8));
-	val_hi = rpm_read(rpm, 0, RPMX_MTI_STAT_DATA_HI_CDC);
-
-	*tx_stat = (val_hi << 32 | val_lo);
-
-	mutex_unlock(&rpm->lock);
-	return 0;
-}
-
 void rpm_lmac_enadis_rx_pause_fwding(void *rpmd, int lmac_id, bool enable)
 {
 	rpm_t *rpm = rpmd;
@@ -183,11 +90,14 @@ int rpm_lmac_get_pause_frm_status(void *rpmd, int lmac_id,
 	return 0;
 }
 
-static int rpm_lmac_enadis_8023_pause_frm(void *rpmd, int lmac_id, u8 tx_pause,
-					  u8 rx_pause)
+int rpm_lmac_enadis_pause_frm(void *rpmd, int lmac_id, u8 tx_pause,
+			      u8 rx_pause)
 {
 	rpm_t *rpm = rpmd;
 	u64 cfg;
+
+	if (!is_lmac_valid(rpm, lmac_id))
+		return -ENODEV;
 
 	cfg = rpm_read(rpm, lmac_id, RPMX_MTI_MAC100X_COMMAND_CONFIG);
 	cfg &= ~RPMX_MTI_MAC100X_COMMAND_CONFIG_RX_P_DISABLE;
@@ -210,18 +120,6 @@ static int rpm_lmac_enadis_8023_pause_frm(void *rpmd, int lmac_id, u8 tx_pause,
 	}
 	rpm_write(rpm, 0, RPMX_CMR_RX_OVR_BP, cfg);
 	return 0;
-}
-
-int rpm_lmac_enadis_pause_frm(void *rpmd, int lmac_id,
-			      u8 tx_pause, u8 rx_pause)
-{
-	rpm_t *rpm = rpmd;
-
-	if (!is_lmac_valid(rpm, lmac_id))
-		return -ENODEV;
-
-	return  rpm_lmac_enadis_8023_pause_frm(rpmd, lmac_id,
-					       tx_pause, rx_pause);
 }
 
 void rpm_lmac_pause_frm_config(void *rpmd, int lmac_id, bool enable)
@@ -279,6 +177,99 @@ void rpm_lmac_pause_frm_config(void *rpmd, int lmac_id, bool enable)
 		cfg |= RPMX_MTI_MAC100X_COMMAND_CONFIG_TX_P_DISABLE;
 		rpm_write(rpm, lmac_id, RPMX_MTI_MAC100X_COMMAND_CONFIG, cfg);
 	}
+}
+
+int rpm_get_rx_stats(void *rpmd, int lmac_id, int idx, u64 *rx_stat)
+{
+	rpm_t *rpm = rpmd;
+	u64 val_lo, val_hi;
+
+	if (!is_lmac_valid(rpm, lmac_id))
+		return -ENODEV;
+
+	mutex_lock(&rpm->lock);
+
+	/* Update idx to point per lmac Rx statistics page */
+	idx += lmac_id * rpm->mac_ops->rx_stats_cnt;
+
+	/* Read lower 32 bits of counter */
+	val_lo = rpm_read(rpm, 0, RPMX_MTI_STAT_RX_STAT_PAGES_COUNTERX +
+			  (idx * 8));
+
+	/* upon read of lower 32 bits, higher 32 bits are written
+	 * to RPMX_MTI_STAT_DATA_HI_CDC
+	 */
+	val_hi = rpm_read(rpm, 0, RPMX_MTI_STAT_DATA_HI_CDC);
+
+	*rx_stat = (val_hi << 32 | val_lo);
+
+	mutex_unlock(&rpm->lock);
+	return 0;
+}
+
+int rpm_get_tx_stats(void *rpmd, int lmac_id, int idx, u64 *tx_stat)
+{
+	rpm_t *rpm = rpmd;
+	u64 val_lo, val_hi;
+
+	if (!is_lmac_valid(rpm, lmac_id))
+		return -ENODEV;
+
+	mutex_lock(&rpm->lock);
+
+	/* Update idx to point per lmac Tx statistics page */
+	idx += lmac_id * rpm->mac_ops->tx_stats_cnt;
+
+	val_lo = rpm_read(rpm, 0, RPMX_MTI_STAT_TX_STAT_PAGES_COUNTERX +
+			    (idx * 8));
+	val_hi = rpm_read(rpm, 0, RPMX_MTI_STAT_DATA_HI_CDC);
+
+	*tx_stat = (val_hi << 32 | val_lo);
+
+	mutex_unlock(&rpm->lock);
+	return 0;
+}
+
+u8 rpm_get_lmac_type(void *rpmd, int lmac_id)
+{
+	rpm_t *rpm = rpmd;
+	u64 req = 0, resp;
+	int err;
+
+	req = FIELD_SET(CMDREG_ID, CGX_CMD_GET_LINK_STS, req);
+	err = cgx_fwi_cmd_generic(req, &resp, rpm, 0);
+	if (!err)
+		return FIELD_GET(RESP_LINKSTAT_LMAC_TYPE, resp);
+	return err;
+}
+
+int rpm_lmac_internal_loopback(void *rpmd, int lmac_id, bool enable)
+{
+	rpm_t *rpm = rpmd;
+	u8 lmac_type;
+	u64 cfg;
+
+	if (!is_lmac_valid(rpm, lmac_id))
+		return -ENODEV;
+	lmac_type = rpm->mac_ops->get_lmac_type(rpm, lmac_id);
+	if (lmac_type == LMAC_MODE_100G_R) {
+		cfg = rpm_read(rpm, lmac_id, RPMX_MTI_PCS100X_CONTROL1);
+
+		if (enable)
+			cfg |= RPMX_MTI_PCS_LBK;
+		else
+			cfg &= ~RPMX_MTI_PCS_LBK;
+		rpm_write(rpm, lmac_id, RPMX_MTI_PCS100X_CONTROL1, cfg);
+	} else {
+		cfg = rpm_read(rpm, 0, RPMX_MTI_LPCSX_CONTROL(lmac_id));
+		if (enable)
+			cfg |= RPMX_MTI_PCS_LBK;
+		else
+			cfg &= ~RPMX_MTI_PCS_LBK;
+		rpm_write(rpm, 0, RPMX_MTI_LPCSX_CONTROL(lmac_id), cfg);
+	}
+
+	return 0;
 }
 
 void rpm_lmac_ptp_config(void *rpmd, int lmac_id, bool enable)
