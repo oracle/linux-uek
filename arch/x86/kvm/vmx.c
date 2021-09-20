e72e75b5d2e6698683626e99b2930b072ef5569f
John Donnelly <john.p.donnelly@oracle.com>
2021-09-20 12:22:30 -0700

    kvm:vmx Fix build error in kvm/vmx.c
    Corrects: "warning: 'vmx_refresh_apicv_exec_ctrl' defined but not used "
    error.
    
    Fixes: 6ec7474d183f ("kvm:vmx: more complete state update on APICv on/off")
    
    Orabug: 33375485
    
    Signed-off-by: John Donnelly <john.p.donnelly@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    (cherry picked from commit 3d51a7d8f7e3303532f1939741f341cd241cbe19)
    Signed-off-by: aloktiw <alok.a.tiwari@oracle.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -4804,26 +4804,6 @@ static u32 vmx_pin_based_exec_ctrl(struct vcpu_vmx *vmx)
 	return pin_based_exec_ctrl;
 }
 
-static void vmx_refresh_apicv_exec_ctrl(struct kvm_vcpu *vcpu)
-{
-	struct vcpu_vmx *vmx = to_vmx(vcpu);
-
-	vmcs_write32(PIN_BASED_VM_EXEC_CONTROL, vmx_pin_based_exec_ctrl(vmx));
-	if (cpu_has_secondary_exec_ctrls()) {
-		if (kvm_vcpu_apicv_active(vcpu))
-			vmcs_set_bits(SECONDARY_VM_EXEC_CONTROL,
-				      SECONDARY_EXEC_APIC_REGISTER_VIRT |
-				      SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
-		else
-			vmcs_clear_bits(SECONDARY_VM_EXEC_CONTROL,
-					SECONDARY_EXEC_APIC_REGISTER_VIRT |
-					SECONDARY_EXEC_VIRTUAL_INTR_DELIVERY);
-	}
-
-	if (cpu_has_vmx_msr_bitmap())
-		vmx_set_msr_bitmap(vcpu);
-}
-
 static u32 vmx_exec_control(struct vcpu_vmx *vmx)
 {
 	u32 exec_control = vmcs_config.cpu_based_exec_ctrl;
