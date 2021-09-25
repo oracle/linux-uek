55fcbcf8c474dbb1e28a2593b0c68ef07098e4a5
Leon Romanovsky <leonro@nvidia.com>
2021-09-25 14:22:47 +0300

    octeontx2: Move devlink registration to be last devlink command
    This change prevents from users to access device before devlink is fully
    configured. This change allows us to delete call to devlink_params_publish()
    and impossible check during unregister flow.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1d264db405cb02c7a225be8e244d171156f6e668)
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_devlink.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_devlink.c
@@ -93,7 +93,6 @@ int otx2_register_dl(struct otx2_nic *pfvf)
 		return -ENOMEM;
 	}
 
-	devlink_register(dl);
 	otx2_dl = devlink_priv(dl);
 	otx2_dl->dl = dl;
 	otx2_dl->pfvf = pfvf;
@@ -107,12 +106,10 @@ int otx2_register_dl(struct otx2_nic *pfvf)
 		goto err_dl;
 	}
 
-	devlink_params_publish(dl);
-
+	devlink_register(dl);
 	return 0;
 
 err_dl:
-	devlink_unregister(dl);
 	devlink_free(dl);
 	return err;
 }
@@ -120,16 +117,10 @@ err_dl:
 void otx2_unregister_dl(struct otx2_nic *pfvf)
 {
 	struct otx2_devlink *otx2_dl = pfvf->dl;
-	struct devlink *dl;
-
-	if (!otx2_dl || !otx2_dl->dl)
-		return;
-
-	dl = otx2_dl->dl;
-
-	devlink_params_unregister(dl, otx2_dl_params,
-				  ARRAY_SIZE(otx2_dl_params));
+	struct devlink *dl = otx2_dl->dl;
 
 	devlink_unregister(dl);
+	devlink_params_unregister(dl, otx2_dl_params,
+				  ARRAY_SIZE(otx2_dl_params));
 	devlink_free(dl);
 }
