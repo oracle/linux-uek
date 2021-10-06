d9543c73d641b7b887d936725b2afc78d27e8817
Rob Herring <robh@kernel.org>
2021-10-06 11:43:21 -0500

    of: Add of_get_cpu_hwid() to read hardware ID from CPU nodes
    There are various open coded implementions parsing the CPU node 'reg'
    property which contains the CPU's hardware ID. Introduce a new function,
    of_get_cpu_hwid(), to read the hardware ID.
    
    All the callers should be DT only code, so no need for an empty
    function.
    
    Cc: Frank Rowand <frowand.list@gmail.com>
    Signed-off-by: Rob Herring <robh@kernel.org>
    Tested-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Link: https://lore.kernel.org/r/20211006164332.1981454-2-robh@kernel.org
    (cherry picked from commit 795e92ec5fd79027648bd7f779d34bad5b6f2f55)
    
    Conflicts:
    	include/linux/of.h
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 8919ea26872b815e8dde6e63ee5fcd5ea58549c3)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/of.h
+++ include/linux/of.h
@@ -351,6 +351,7 @@ extern const void *of_get_property(const struct device_node *node,
 				int *lenp);
 extern struct device_node *of_get_cpu_node(int cpu, unsigned int *thread);
 extern struct device_node *of_get_next_cpu_node(struct device_node *prev);
+extern u64 of_get_cpu_hwid(struct device_node *cpun, unsigned int thread);
 
 #define for_each_property_of_node(dn, pp) \
 	for (pp = dn->properties; pp != NULL; pp = pp->next)
