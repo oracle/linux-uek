a07affc5bbdaa1a0607c917b9e73d45220ef22a4
Ahmed S. Darwish <a.darwish@linutronix.de>
2021-10-16 10:49:08 +0200

    net: sched: Use _bstats_update/set() instead of raw writes
    The Qdisc::running sequence counter, used to protect Qdisc::bstats reads
    from parallel writes, is in the process of being removed. Qdisc::bstats
    read/writes will synchronize using an internal u64_stats sync point
    instead.
    
    Modify all bstats writes to use _bstats_update(). This ensures that
    the internal u64_stats sync point is always acquired and released as
    appropriate.
    
    Signed-off-by: Ahmed S. Darwish <a.darwish@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Orabug: 35290200
    
    (cherry picked from commit f56940daa5a74fb20b5f5487535549949f2d8d0c)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: f56940daa5a74fb20b5f5487535549949f2d8d0c
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>

--- net/sched/sch_qfq.c
+++ net/sched/sch_qfq.c
@@ -1245,8 +1245,7 @@ static int qfq_enqueue(struct sk_buff *skb, struct Qdisc *sch,
 		return err;
 	}
 
-	cl->bstats.bytes += len;
-	cl->bstats.packets += gso_segs;
+	_bstats_update(&cl->bstats, len, gso_segs);
 	sch->qstats.backlog += len;
 	++sch->q.qlen;
 
