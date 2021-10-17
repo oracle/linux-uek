4e61f0cc890b29616d0d5c06a28749f17477c23c
Gautham Ananthakrishna <gautham.ananthakrishna@oracle.com>
2021-10-17 17:13:59 +0000

    fs/dcache: correct the usage of static_key for negative dentry
    The current usage of static_key_enabled() is incorrect. In this way
    it is used no differently than a global integer which controls
    whether to execute a branch.
    
    The thing that makes static keys special is the "self-modifying code"
    aspect. We need to use this feature to reduce overheads
    
    Orabug: 33468829
    
    Signed-off-by: Gautham Ananthakrishna <gautham.ananthakrishna@oracle.com>
    Reviewed-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -158,7 +158,7 @@ struct dentry_stat_t {
 #define NEG_IS_SB_UMOUNTING(sb)	\
 	unlikely(!(sb)->s_root || !((sb)->s_flags & SB_ACTIVE))
 
-static struct static_key limit_neg_key = STATIC_KEY_INIT_FALSE;
+DEFINE_STATIC_KEY_FALSE(limit_neg_key);
 static int neg_dentry_pc_old;
 static int neg_dentry_pc;
 
@@ -313,7 +313,7 @@ static inline int dentry_string_cmp(const unsigned char *cs, const unsigned char
  */
 static inline void __neg_dentry_dec(struct dentry *dentry)
 {
-	if (!static_key_enabled(&limit_neg_key)) {
+	if (!static_branch_unlikely(&limit_neg_key)) {
 		this_cpu_dec(nr_dentry_negative);
 		return;
 	}
@@ -358,7 +358,7 @@ static inline void __neg_dentry_inc(struct dentry *dentry)
 {
 	long cnt = 0, *pcnt;
 
-	if (!static_key_enabled(&limit_neg_key)) {
+	if (!static_branch_unlikely(&limit_neg_key)) {
 		this_cpu_inc(nr_dentry_negative);
 		return;
 	}
@@ -436,7 +436,7 @@ static int proc_neg_dentry_pc(const struct ctl_table *table, int write,
 	 * neg_dentry_nfree_init and return.
 	 */
 	if (!neg_dentry_pc && neg_dentry_pc_old) {
-		static_key_slow_dec(&limit_neg_key);
+		static_key_slow_dec(&limit_neg_key.key);
 		goto out;
 	}
 
@@ -463,7 +463,7 @@ static int proc_neg_dentry_pc(const struct ctl_table *table, int write,
 		neg_dentry_percpu_limit, neg_dentry_nfree_init);
 
 	if (!neg_dentry_pc_old)
-		static_key_slow_inc(&limit_neg_key);
+		static_key_slow_inc(&limit_neg_key.key);
 out:
 	neg_dentry_pc_old = neg_dentry_pc;
 	return 0;
