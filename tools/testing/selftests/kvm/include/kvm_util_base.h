151af6f7eb9c5fee77aeffc6bb1f169f6f712c9b
Peter Gonda <pgonda@google.com>
2021-10-21 10:43:02 -0700

    selftest: KVM: Add open sev dev helper
    Refactors out open path support from open_kvm_dev_path_or_exit() and
    adds new helper for SEV device path.
    
    Signed-off-by: Peter Gonda <pgonda@google.com>
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Cc: Marc Orr <marcorr@google.com>
    Cc: Sean Christopherson <seanjc@google.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Brijesh Singh <brijesh.singh@amd.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: kvm@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Message-Id: <20211021174303.385706-5-pgonda@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 7a6ab3cf398a11233845ed248aea7141dc9d510d)
    Orabug: 37070016
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Ian Chin Wang <ian.chin.wang@oracle.com>
    [ pre-req for 6a58150859fd ("selftest: KVM: Add intra host migration tests") ]
    Conflicts:
    	tools/testing/selftests/kvm/include/kvm_util.h
    [ Contextual conflict due to out-of-order UEK7U1 backport of v5.17 commit
      96c1a6285568 ("kvm: selftests: move ucall declarations into ucall_common.h")
      and manually add declaration of open_path_or_exit() to kvm_util_base.h ]
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- tools/testing/selftests/kvm/include/kvm_util_base.h
+++ tools/testing/selftests/kvm/include/kvm_util_base.h
@@ -80,6 +80,7 @@ struct vm_guest_mode_params {
 };
 extern const struct vm_guest_mode_params vm_guest_mode_params[];
 
+int open_path_or_exit(const char *path, int flags);
 int open_kvm_dev_path_or_exit(void);
 int kvm_check_cap(long cap);
 int vm_check_cap(struct kvm_vm *vm, long cap);
