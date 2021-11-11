05a3b71059e40a6fa154f17d363e5c8d892954be
Gerd Rausch <gerd.rausch@oracle.com>
2021-11-11 09:30:31 -0800

    Revert "net/rds: Allocate pages on HCA NUMA nodeid"
    This reverts commit 7692fc72b66f97feb3d00d34f3fcd1a24605c18c.
    
    Orabug: 33590962
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Orabug: 33590097
    
    UEK6 => UEK7
    
    (cherry picked from commit 967447026abe3f01febea058bcb42d1809ed0321)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit 6a62c9279360e982a4d5830cbc1fc6d3de374142)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/message.c
+++ net/rds/message.c
@@ -314,8 +314,7 @@ int rds_message_copy_from_user(struct rds_message *rm, struct iov_iter *from,
 		if (!sg_page(sg)) {
 			ret = rds_page_remainder_alloc(sg, iov_iter_count(from),
 						       GFP_ATOMIC == gfp ?
-						       gfp : GFP_HIGHUSER,
-						       NUMA_NO_NODE);
+						       gfp : GFP_HIGHUSER);
 
 			if (ret)
 				return ret;
