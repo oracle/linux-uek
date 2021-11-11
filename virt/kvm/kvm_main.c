d6426bd6bb7e9c051e243188cb6ce8d1bcbefac0
Paolo Bonzini <pbonzini@redhat.com>
2021-11-11 10:13:38 -0500

    KVM: generalize "bugged" VM to "dead" VM
    Generalize KVM_REQ_VM_BUGGED so that it can be called even in cases
    where it is by design that the VM cannot be operated upon.  In this
    case any KVM_BUG_ON should still warn, so introduce a new flag
    kvm->vm_dead that is separate from kvm->vm_bugged.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit f4d316537059b274452727e86f46ff3bdefdde4d)
    [ dependency pre-req for b56639318bb2 ("KVM: SEV: Add support for SEV intra host migration") ]
    Orabug: 37070016
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Ian Chin Wang <ian.chin.wang@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -3965,7 +3965,7 @@ static long kvm_vcpu_ioctl(struct file *filp,
 	struct kvm_fpu *fpu = NULL;
 	struct kvm_sregs *kvm_sregs = NULL;
 
-	if (vcpu->kvm->mm != current->mm || vcpu->kvm->vm_bugged)
+	if (vcpu->kvm->mm != current->mm || vcpu->kvm->vm_dead)
 		return -EIO;
 
 	if (unlikely(_IOC_TYPE(ioctl) != KVMIO))
@@ -4175,7 +4175,7 @@ static long kvm_vcpu_compat_ioctl(struct file *filp,
 	void __user *argp = compat_ptr(arg);
 	int r;
 
-	if (vcpu->kvm->mm != current->mm || vcpu->kvm->vm_bugged)
+	if (vcpu->kvm->mm != current->mm || vcpu->kvm->vm_dead)
 		return -EIO;
 
 	switch (ioctl) {
@@ -4241,7 +4241,7 @@ static long kvm_device_ioctl(struct file *filp, unsigned int ioctl,
 {
 	struct kvm_device *dev = filp->private_data;
 
-	if (dev->kvm->mm != current->mm || dev->kvm->vm_bugged)
+	if (dev->kvm->mm != current->mm || dev->kvm->vm_dead)
 		return -EIO;
 
 	switch (ioctl) {
@@ -4578,7 +4578,7 @@ static long kvm_vm_ioctl(struct file *filp,
 	void __user *argp = (void __user *)arg;
 	int r;
 
-	if (kvm->mm != current->mm || kvm->vm_bugged)
+	if (kvm->mm != current->mm || kvm->vm_dead)
 		return -EIO;
 	switch (ioctl) {
 	case KVM_CREATE_VCPU:
@@ -4795,7 +4795,7 @@ static long kvm_vm_compat_ioctl(struct file *filp,
 	struct kvm *kvm = filp->private_data;
 	int r;
 
-	if (kvm->mm != current->mm || kvm->vm_bugged)
+	if (kvm->mm != current->mm || kvm->vm_dead)
 		return -EIO;
 
 	r = kvm_arch_vm_compat_ioctl(filp, ioctl, arg);
