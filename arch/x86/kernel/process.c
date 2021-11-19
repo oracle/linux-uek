8ea2c1897df6fd34c341adc26aad31c3d8113936
Peter Zijlstra <peterz@infradead.org>
2021-11-19 10:29:47 +0100

    x86: Pin task-stack in __get_wchan()
    When commit 5d1ceb3969b6 ("x86: Fix __get_wchan() for !STACKTRACE")
    moved from stacktrace to native unwind_*() usage, the
    try_get_task_stack() got lost, leading to use-after-free issues for
    dying tasks.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Fixes: 5d1ceb3969b6 ("x86: Fix __get_wchan() for !STACKTRACE")
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=215031
    Link: https://lore.kernel.org/stable/YZV02RCRVHIa144u@fedora64.linuxtx.org/
    Reported-by: Justin Forbes <jmforbes@linuxtx.org>
    Reported-by: Holger Hoffstätte <holger@applied-asynchrony.com>
    Cc: Qi Zheng <zhengqi.arch@bytedance.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 0dc636b3b757a6b747a156de613275f9d74a4a66)
    
    Orabug: 38161988
    
    Signed-off-by: Siddhi Katage <siddhi.katage@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Libo Chen <libo.chen@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -991,6 +991,9 @@ unsigned long __get_wchan(struct task_struct *p)
 	struct unwind_state state;
 	unsigned long addr = 0;
 
+	if (!try_get_task_stack(p))
+		return 0;
+
 	for (unwind_start(&state, p, NULL, NULL); !unwind_done(&state);
 	     unwind_next_frame(&state)) {
 		addr = unwind_get_return_address(&state);
@@ -1000,6 +1003,8 @@ unsigned long __get_wchan(struct task_struct *p)
 			continue;
 		break;
 	}
+
+	put_task_stack(p);
 
 	return addr;
 }
