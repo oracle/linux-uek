da90377a6b36876979444976d78817373118b56a
Heiko Carstens <hca@linux.ibm.com>
2021-11-29 14:03:08 +0100

    topology/sysfs: export cluster attributes only if an architectures has support
    The cluster_id and cluster_cpus topology sysfs attributes have been
    added with commit c5e22feffdd7 ("topology: Represent clusters of CPUs
    within a die").
    
    They are currently only used for x86, arm64, and riscv (via generic
    arch topology), however they are still present with bogus default
    values for all other architectures. Instead of enforcing such new
    sysfs attributes to all architectures, make them only optional visible
    if an architecture opts in by defining both the topology_cluster_id
    and topology_cluster_cpumask attributes.
    
    This is similar to what was done when the book and drawer topology
    levels were introduced: avoid useless and therefore confusing sysfs
    attributes for architectures which cannot make use of them.
    
    This should not break any existing applications, since this is a
    new interface introduced with the v5.16 merge window.
    
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Link: https://lore.kernel.org/r/20211129130309.3256168-3-hca@linux.ibm.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit e795707703b32fecdd7467afcc33ff1e92416c05)
    
    Orabug: 36473714
    
    Conflicts:
        Documentation/admin-guide/cputopology.rst
        drivers/base/topology.c
        include/linux/topology.h
    Due to backports: c28e351e228ca, 01d23badcb731 and a418df06fb566
    as well as the absence of upstream patch 2c4dcd7fd57b20a2
    
    Signed-off-by: Libo Chen <libo.chen@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/base/topology.c
+++ drivers/base/topology.c
@@ -48,8 +48,10 @@ static DEVICE_ATTR_RO(physical_package_id);
 define_id_show_func(die_id, "%d");
 static DEVICE_ATTR_RO(die_id);
 
+#ifdef TOPOLOGY_CLUSTER_SYSFS
 define_id_show_func(cluster_id, "%d");
 static DEVICE_ATTR_RO(cluster_id);
+#endif
 
 define_id_show_func(core_id, "%d");
 static DEVICE_ATTR_RO(core_id);
@@ -69,9 +71,11 @@ define_siblings_read_func(core_siblings, core_cpumask);
 static BIN_ATTR_RO(core_siblings, CPUMAP_FILE_MAX_BYTES);
 static BIN_ATTR_RO(core_siblings_list, CPULIST_FILE_MAX_BYTES);
 
+#ifdef TOPOLOGY_CLUSTER_SYSFS
 define_siblings_read_func(cluster_cpus, cluster_cpumask);
 static BIN_ATTR_RO(cluster_cpus, CPUMAP_FILE_MAX_BYTES);
 static BIN_ATTR_RO(cluster_cpus_list, CPULIST_FILE_MAX_BYTES);
+#endif
 
 define_siblings_read_func(die_cpus, die_cpumask);
 static BIN_ATTR_RO(die_cpus, CPUMAP_FILE_MAX_BYTES);
@@ -104,8 +108,10 @@ static struct bin_attribute *bin_attrs[] = {
 	&bin_attr_thread_siblings_list,
 	&bin_attr_core_siblings,
 	&bin_attr_core_siblings_list,
+#ifdef TOPOLOGY_CLUSTER_SYSFS
 	&bin_attr_cluster_cpus,
 	&bin_attr_cluster_cpus_list,
+#endif
 	&bin_attr_die_cpus,
 	&bin_attr_die_cpus_list,
 	&bin_attr_package_cpus,
@@ -124,7 +130,9 @@ static struct bin_attribute *bin_attrs[] = {
 static struct attribute *default_attrs[] = {
 	&dev_attr_physical_package_id.attr,
 	&dev_attr_die_id.attr,
+#ifdef TOPOLOGY_CLUSTER_SYSFS
 	&dev_attr_cluster_id.attr,
+#endif
 	&dev_attr_core_id.attr,
 #ifdef TOPOLOGY_BOOK_SYSFS
 	&dev_attr_book_id.attr,
