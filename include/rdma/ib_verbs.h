b18872c4a05e6f100bb87956c90fba11e8cd41ce
Håkon Bugge <haakon.bugge@oracle.com>
2021-11-29 11:53:16 +0100

    IB/core: Introduce IB_CQ_FORCE_ZERO_CV
    rds_rdma maintains its own least-used completion vector scheme.
    
    The mlx{4,5} drivers also maintain a least-used EQ scheme, but only when
    the supplied completion vector is zero and we are not executing on CPU
    zero. Yes, weird.
    
    But both mechanisms are needed.
    
    RDS uses its own (with the exception of the non-determinism when
    completion vector zero is supplied).
    
    Other ULPs depend on the least-used weighting by the mlx{4,5} device
    driver, e.g., IPoIB, and so does the CQs used by QPs sending MAD
    SMI/GSI packets.
    
    And, user-space has established the convention that they supply
    completion vector zero and relies on the mlx{4,5} driver to get them
    spread on different EQs.
    
    Fixed by introducing an ib_verbs enum IB_CQ_FORCE_ZERO_CV. When this
    value is supplied, it signals to the device drivers that this
    literally means completion vector number zero, and not zero as
    interpreted as: use the least loaded completion vector from the device
    driver centric perspective.
    
    Orabug: 33679626
    
    Fixes: c6d27569bb24 ("{IB,net}/mlx5: Spread IB CQs more evenly over EQs")
    Fixes: 8b78aa300db7 ("IB/mlx4: Scatter CQs to different EQs")
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    
    Orabug: 33615757
    
    UEK7 => LUCI
    
    (cherry picked from commit 3dae60c98db6cb04448157b376171e5059871caf)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    
    Orabug: 37347034
    
    LUCI-v6.5 => LUCI-v6.6 => UEK8
    
    Changed commit order to address the issue of commit
    
    c3a4c52f32834 ("net/mlx{4,5},rds_rdma: fix non-determinism when comp_vector is zero")
    
    that used to touch code both inside and outside of net/rds.
    
    This leads to problems for LUCI, as the rewind-mechanism assumes that all
    changes to net/rds can be applied cleanly, because the rewind-base is
    unchanged.
    
    However, if the code that's attempted to be merged touches files outside
    net/rds, it may run into conflicts.
    There is no such guarantee for non-RDS code.
    
    We move the IB+MLX5+MLX4 portions of the Orabug 33615757
    to this earlier point in the history *here*, so that symbol "IB_CQ_FORCE_ZERO_CV"
    becomes defined and we can make RDS changes RDS-only changes.
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -378,6 +378,10 @@ enum ib_cq_attr_mask {
 	IB_CQ_MODERATE = 1 << 0,
 };
 
+enum ib_cq_comp_vector_zero {
+	IB_CQ_FORCE_ZERO_CV = U32_MAX,
+};
+
 struct ib_cq_caps {
 	u16     max_cq_moderation_count;
 	u16     max_cq_moderation_period;
