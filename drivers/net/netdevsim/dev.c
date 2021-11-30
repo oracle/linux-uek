d0d4716a173efe13b8ad4636d23d67a8c8733852
Leon Romanovsky <leonro@nvidia.com>
2021-11-30 12:16:20 +0200

    devlink: Simplify devlink resources unregister call
    The devlink_resources_unregister() used second parameter as an
    entry point for the recursive removal of devlink resources. None
    of the callers outside of devlink core needed to use this field,
    so let's remove it.
    
    As part of this removal, the "struct devlink_resource" was moved
    from .h to .c file as it is not possible to use in any place in
    the code except devlink.c.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Orabug: 34481188
    
    (cherry picked from commit 4c897cfc46a554a523343fc3296333c473a2fc52)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 4c897cfc46a554a523343fc3296333c473a2fc52
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    (cherry picked from commit 70d7c332085e2139ac8d9cbbd800e88cd813a12d)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/net/netdevsim/dev.c
+++ drivers/net/netdevsim/dev.c
@@ -1550,7 +1550,7 @@ err_params_unregister:
 	devlink_params_unregister(devlink, nsim_devlink_params,
 				  ARRAY_SIZE(nsim_devlink_params));
 err_dl_unregister:
-	devlink_resources_unregister(devlink, NULL);
+	devlink_resources_unregister(devlink);
 err_vfc_free:
 	kfree(nsim_dev->vfconfigs);
 err_devlink_free:
@@ -1593,7 +1593,7 @@ void nsim_dev_remove(struct nsim_bus_dev *nsim_bus_dev)
 	nsim_dev_debugfs_exit(nsim_dev);
 	devlink_params_unregister(devlink, nsim_devlink_params,
 				  ARRAY_SIZE(nsim_devlink_params));
-	devlink_resources_unregister(devlink, NULL);
+	devlink_resources_unregister(devlink);
 	kfree(nsim_dev->vfconfigs);
 	devlink_free(devlink);
 	dev_set_drvdata(&nsim_bus_dev->dev, NULL);
