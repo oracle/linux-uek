fef25fa760c2b4a29c7a65f3b29d1cd3bfc388cd
Gerd Rausch <gerd.rausch@oracle.com>
2021-12-01 11:50:31 -0800

    net/rds: Switch to the Linux-5.15 API
    All of the RDS commits starting with
    "uek-rpm: Disable CONFIG_RDS for UEK7"
    all the way to the commit preceeding
    this one are based on the Linux-5.4 API.
    
    As such they won't even compile on Linux-5.15.
    
    With this commit we adjust the RDS code-base
    to compile and work on Linux-5.15.
    
    Changes:
      * FMR support has been removed through a number of upstream-commits, e.g.:
        >3a578152a920 ("RDMA/core: Remove FMR device ops")
        So we follow the upstream-lead and get rid of FMR from UEK/RDS as well.
        Please note that after this change, there's some dead
        leftover code that needs to be cleaned up, e.g.
        "fmr_max_remaps".
    
      * Change function prototypes and calls to "copy_from_user"
        to "copy_from_sockptr" to adhere to the changes introduced by:
        >a7b75c5a8c41 ("net: pass a sockptr_t into ->setsockopt")
    
      * Replaced "get_seconds" by "ktime_get_real_seconds",
        since "get_seconds" was removed by:
        >aba428a0c612 ("timekeeping: Remove unused get_seconds()")
    
      * Function "ib_check_mr_access" now needs to know the IB device:
        >adac4cb3c1ff ("RDMA/uverbs: Check ODP in ib_check_mr_access() as well")
    
      * The "add" callback of "struct rds_ib_client" now returns an error:
        >11a0ae4c4bff9 ("RDMA: Allow ib_client's to fail when add() is called")
        Function "rds_ib_add_one" changed accordingly.
    
      * Function "ibdev_to_node" now has become a part of the RDMA API:
        >8ecfca68dc4c ("RDMA: Lift ibdev_to_node from rds to common code")
    
      * Function "ib_dma_alloc_coherent" disappeared:
        >2af29468e3b3 ("RDMA/core: Remove ib_dma_{alloc,free}_coherent")
        Replaced by "kzalloc_node" + "ib_dma_map_single"
    
      * Function "rdma_reject" now requires a reason:
        >8094ba0ace7f ("RDMA/cma: Provide ECE reject reason")
    
      * Without FMR support, there is no "struct ib_fmr_attr".
        All we care about for now (until cleanup) is "max_pages".
    
      * Function "ib_destroy_srq" no longer has a return value:
        >119181d1d432 ("RDMA: Restore ability to fail on SRQ destroy")
    
      * For RDMA, "pin_user_pages" is the new method to lock pages:
        >dbfe7d74376e ("rds: convert get_user_pages() --> pin_user_pages()")
    
      * Replace "__kernel_time_t" with "__s64" because "__kernel_time_t"
        has now become "__kernel_old_time_t", as of
        >2a785996cc5e2 ("y2038: uapi: change __kernel_time_t to __kernel_old_time_t"
        and "__kernel_old_time_t" is not available in user-land.
        Luckily, incompatibility with user-land (uapi) is not permitted
        and we know that "time64_t" has been defined as "__s64" since:
        >361a3bf005824 ("time64: Add time64.h header and define struct timespec64")
    
      * Use new(er) sock/TCP helpers:
        >ce3d9544ceca ("net: add sock_set_keepalive")
        >c433594c0745 ("net: add sock_no_linger")
        >12abc5ee7873 ("tcp: add tcp_sock_set_nodelay")
        >480aeb9639d6 ("tcp: add tcp_sock_set_keepcnt")
        >71c48eb81c9e ("tcp: add tcp_sock_set_keepidle")
        >d41ecaac903c ("tcp: add tcp_sock_set_keepintvl")
        >db10538a4b99 ("tcp: add tcp_sock_set_cork")
    
      * Support for "---help---" in Kconfig has been removed with:
        >f70f74d15ca8 kconfig: remove '---help---' support
        So we use "help" instead.
    
    Orabug: 33590097
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    
    Orabug: 33590087
    
    UEK7 => LUCI
    
    (cherry picked from commit d2374113f573fe7faa44cd5cfd7a228906029b71)
    cherry-pick-repo=UEK/production/linux-uek.git
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/tcp_send.c
+++ net/rds/tcp_send.c
@@ -39,29 +39,18 @@
 #include "rds.h"
 #include "tcp.h"
 
-static void rds_tcp_cork(struct socket *sock, int val)
-{
-	mm_segment_t oldfs;
-
-	oldfs = get_fs();
-	set_fs(KERNEL_DS);
-	sock->ops->setsockopt(sock, SOL_TCP, TCP_CORK, (char __user *)&val,
-			      sizeof(val));
-	set_fs(oldfs);
-}
-
 void rds_tcp_xmit_path_prepare(struct rds_conn_path *cp)
 {
 	struct rds_tcp_connection *tc = cp->cp_transport_data;
 
-	rds_tcp_cork(tc->t_sock, 1);
+	tcp_sock_set_cork(tc->t_sock->sk, true);
 }
 
 void rds_tcp_xmit_path_complete(struct rds_conn_path *cp)
 {
 	struct rds_tcp_connection *tc = cp->cp_transport_data;
 
-	rds_tcp_cork(tc->t_sock, 0);
+	tcp_sock_set_cork(tc->t_sock->sk, false);
 }
 
 /* the core send_sem serializes this with other xmit and shutdown */
