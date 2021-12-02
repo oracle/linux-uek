a7e67a59506640b4c21f61711e15acbfd1027f13
Sharath Srinivasan <sharath.srinivasan@oracle.com>
2021-12-02 03:35:49 -0800

    mstflint_access: Update driver code to v4.18.0-1 from Github
    Orabug: 33186470
    
    (cherry picked from commit 858809d850a17738b05fd7df21e99c2c71c84865)
    cherry-pick-repo=github.com/Mellanox/mstflint.git
    
    We upgraded the mstflint_access driver code from v4.17.0-1 to v4.18.0-1.
    
    The kernel module source was fetched from:
    https://github.com/Mellanox/mstflint.git tag v4.18.0-1 in the "kernel"
    sub-directory.
    
    We skipped 1 patch which made changes to "mstflint_kernel.spec" et.al.,
    since these files are needed for a standalone rpm build, not needed for
    UEK builds.
    
    We include one patch that updates licenses of mstflint_access kernel
    files. We also ignore license updates made to files that are not part
    of the mstflint_access kernel files.
    
    Code change to mst_init() is to fix build warning/error triggered
    by [-Werror=misleading-indentation]: "note: ...this statement, but the
    latter is misleadingly indented as if it were guarded by the 'if'"
    
    v4.17.0-1 => v4.18.0-1
    
    858809d850a1 update licenses
    
    Signed-off-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>

--- drivers/net/ethernet/mellanox/mstflint_access/mst.h
+++ drivers/net/ethernet/mellanox/mstflint_access/mst.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 OR Linux-OpenIB */
 /*
- * Copyright (c) 2018 Mellanox Technologies. All rights reserved.
+ * Copyright (c) 2018-2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  */
 
 #ifndef _MST_H_
