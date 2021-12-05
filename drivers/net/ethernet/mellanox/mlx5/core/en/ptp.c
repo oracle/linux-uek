0a256e5b8ab8e93a014bf9dd4a23093305148e82
Tariq Toukan <tariqt@nvidia.com>
2021-12-05 12:31:47 +0200

    net/mlx5e: Use bitmap field for profile features
    Use a features bitmap field in mlx5e_profile to declare profile support
    state of the different features.  Let it replace the existing
    rx_ptp_support boolean. It will be extended to cover more features in a
    downstream patch.
    
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 33842440
    
    (cherry picked from commit 6c72cb05d4b877300cb23df32f7451bdc55d2940)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 6c72cb05d4b877300cb23df32f7451bdc55d2940
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/ptp.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en/ptp.c
@@ -760,7 +760,7 @@ int mlx5e_ptp_alloc_rx_fs(struct mlx5e_priv *priv)
 {
 	struct mlx5e_ptp_fs *ptp_fs;
 
-	if (!priv->profile->rx_ptp_support)
+	if (!mlx5e_profile_feature_cap(priv->profile, PTP_RX))
 		return 0;
 
 	ptp_fs = kzalloc(sizeof(*ptp_fs), GFP_KERNEL);
@@ -775,7 +775,7 @@ void mlx5e_ptp_free_rx_fs(struct mlx5e_priv *priv)
 {
 	struct mlx5e_ptp_fs *ptp_fs = priv->fs.ptp_fs;
 
-	if (!priv->profile->rx_ptp_support)
+	if (!mlx5e_profile_feature_cap(priv->profile, PTP_RX))
 		return;
 
 	mlx5e_ptp_rx_unset_fs(priv);
@@ -786,7 +786,7 @@ int mlx5e_ptp_rx_manage_fs(struct mlx5e_priv *priv, bool set)
 {
 	struct mlx5e_ptp *c = priv->channels.ptp;
 
-	if (!priv->profile->rx_ptp_support)
+	if (!mlx5e_profile_feature_cap(priv->profile, PTP_RX))
 		return 0;
 
 	if (!test_bit(MLX5E_STATE_OPENED, &priv->state))
