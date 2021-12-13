1038658f29f2169951120c60d7f93b021070efe4
Kees Cook <keescook@chromium.org>
2021-12-13 14:33:23 -0800

    RDMA/mlx5: Use memset_after() to zero struct mlx5_ib_mr
    In preparation for FORTIFY_SOURCE performing compile-time and run-time
    field bounds checking for memset(), avoid intentionally writing across
    neighboring fields.
    
    Use memset_after() to zero the end of struct mlx5_ib_mr that should be
    initialized.
    
    Link: https://lore.kernel.org/r/20211213223331.135412-10-keescook@chromium.org
    Acked-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Orabug: 35074598
    
    (cherry picked from commit e517f76a3cb24b2a4749e0348390aece24cf9925)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/infiniband/hw/mlx5/mlx5_ib.h
    
    Small conflict in a context caused by already applied fix.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- drivers/infiniband/hw/mlx5/mlx5_ib.h
+++ drivers/infiniband/hw/mlx5/mlx5_ib.h
@@ -671,9 +671,9 @@ struct mlx5_ib_mr {
 
 	/* User MR data */
 	struct mlx5_cache_ent *cache_ent;
+	/* Everything after cache_ent is zero'd when MR allocated */
 	struct ib_umem *umem;
 
-	/* This is zero'd when the MR is allocated */
 	union {
 		/* Used only while the MR is in the cache */
 		struct {
@@ -725,7 +725,7 @@ struct mlx5_ib_mr {
 /* Zero the fields in the mr that are variant depending on usage */
 static inline void mlx5_clear_mr(struct mlx5_ib_mr *mr)
 {
-	memset(mr->out, 0, sizeof(*mr) - offsetof(struct mlx5_ib_mr, out));
+	memset_after(mr, 0, cache_ent);
 }
 
 static inline bool is_odp_mr(struct mlx5_ib_mr *mr)
