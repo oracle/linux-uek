f1221ccf9557f59eeaa91db4fef84b5ed616dce5
Rao Shoaib <rao.shoaib@oracle.com>
2022-01-06 17:20:20 -0800

    DMA/rxe: Update default value of RXE_MAX_PDN
    Bug 33615343 bumped up default values of RXE via
    a clean cherry-pick from upstream. However,
    Oracle has added proprietary support for shared PD
    which introduced RXE_MIN_PDN, RXE_MAX_PDN. These
    values needed to be adjusted when RXE_MAX_PD value
    was changed, but were not and broke RXE.
    This patch adjusts the value of RXE_MAX_PDN.
    
    Orabug: 33676596
    
    Fixes: 09b17ac992017c820b61ec37b92cefc175878702
    Signed-off-by: Rao Shoaib <rao.shoaib@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    
    Signed-off-by: Rao Shoaib <rao.shoaib@oracle.com>

--- drivers/infiniband/sw/rxe/rxe_param.h
+++ drivers/infiniband/sw/rxe/rxe_param.h
@@ -105,7 +105,7 @@ enum rxe_device_param {
 
 #ifndef WITHOUT_ORACLE_EXTENSIONS
 	RXE_MIN_PDN			= 0x00000001,
-	RXE_MAX_PDN			= 0x000FFFFF,
+	RXE_MAX_PDN			= 0x00100000,
 #endif
 	/* Delay before calling arbiter timer */
 	RXE_NSEC_ARB_TIMER_DELAY	= 200,
