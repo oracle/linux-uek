b30494534d3f81f0855599102da2e820ed6a7cc4
Peter Zijlstra <peterz@infradead.org>
2022-01-11 12:11:14 +0100

    x86/entry_32: Fix segment exceptions
    commit 9cdbeec4096804083944d05da96bbaf59a1eb4f9 upstream.
    
    The LKP robot reported that commit in Fixes: caused a failure. Turns out
    the ldt_gdt_32 selftest turns into an infinite loop trying to clear the
    segment.
    
    As discovered by Sean, what happens is that PARANOID_EXIT_TO_KERNEL_MODE
    in the handle_exception_return path overwrites the entry stack data with
    the task stack data, restoring the "bad" segment value.
    
    Instead of having the exception retry the instruction, have it emulate
    the full instruction. Replace EX_TYPE_POP_ZERO with EX_TYPE_POP_REG
    which will do the equivalent of: POP %reg; MOV $imm, %reg.
    
    In order to encode the segment registers, add them as registers 8-11 for
    32-bit.
    
    By setting regs->[defg]s the (nested) RESTORE_REGS will pop this value
    at the end of the exception handler and by increasing regs->sp, it will
    have skipped the stack slot.
    
    This was debugged by Sean Christopherson <seanjc@google.com>.
    
     [ bp: Add EX_REG_GS too. ]
    
    Fixes: aa93e2ad7464 ("x86/entry_32: Remove .fixup usage")
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lore.kernel.org/r/Yd1l0gInc4zRcnt/@hirez.programming.kicks-ass.net
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 198a6f40822eda39f713e345cebdb8523645d3ce)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    
    Conflicts:
    	arch/x86/include/asm/extable_fixup_types.h
    	arch/x86/mm/extable.c

--- arch/x86/include/asm/extable_fixup_types.h
+++ arch/x86/include/asm/extable_fixup_types.h
@@ -16,8 +16,15 @@
 #define EX_DATA_FLAG_SHIFT		12
 #define EX_DATA_IMM_SHIFT		16
 
+#define EX_DATA_REG(reg)		((reg) << EX_DATA_REG_SHIFT)
 #define EX_DATA_FLAG(flag)		((flag) << EX_DATA_FLAG_SHIFT)
 #define EX_DATA_IMM(imm)		((imm) << EX_DATA_IMM_SHIFT)
+
+/* segment regs */
+#define EX_REG_DS			EX_DATA_REG(8)
+#define EX_REG_ES			EX_DATA_REG(9)
+#define EX_REG_FS			EX_DATA_REG(10)
+#define EX_REG_GS			EX_DATA_REG(11)
 
 /* flags */
 #define EX_FLAG_CLEAR_AX		EX_DATA_FLAG(1)
@@ -32,17 +39,20 @@
 #define	EX_TYPE_COPY			 4
 #define	EX_TYPE_CLEAR_FS		 5
 #define	EX_TYPE_FPU_RESTORE		 6
-#define	EX_TYPE_WRMSR			 7
-#define	EX_TYPE_RDMSR			 8
-#define	EX_TYPE_BPF			 9
+#define	EX_TYPE_BPF			 7
+#define	EX_TYPE_WRMSR			 8
+#define	EX_TYPE_RDMSR			 9
+#define	EX_TYPE_WRMSR_SAFE		10 /* reg := -EIO */
+#define	EX_TYPE_RDMSR_SAFE		11 /* reg := -EIO */
+#define	EX_TYPE_WRMSR_IN_MCE		12
+#define	EX_TYPE_RDMSR_IN_MCE		13
+#define	EX_TYPE_DEFAULT_MCE_SAFE	14
+#define	EX_TYPE_FAULT_MCE_SAFE		15
 
-#define	EX_TYPE_WRMSR_IN_MCE		10
-#define	EX_TYPE_RDMSR_IN_MCE		11
+#define	EX_TYPE_POP_REG			16 /* sp += sizeof(long) */
+#define EX_TYPE_POP_ZERO		(EX_TYPE_POP_REG | EX_DATA_IMM(0))
 
-#define	EX_TYPE_DEFAULT_MCE_SAFE	12
-#define	EX_TYPE_FAULT_MCE_SAFE		13
-
-#define	EX_TYPE_POP_ZERO		14
-#define	EX_TYPE_IMM_REG			15 /* reg := (long)imm */
+#define	EX_TYPE_IMM_REG			17 /* reg := (long)imm */
+#define	EX_TYPE_EFAULT_REG		(EX_TYPE_IMM_REG | EX_DATA_IMM(-EFAULT))
 
 #endif
