50630799e7e5a49f93de83a893a2a7e5ae0c6979
Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
2022-01-19 14:53:32 -0800

    uek: kabi: add kABI padding to arch/x86/events/perf_event.h
    Add room for future expansion, without breaking kABI,
    to arch/x86/events/perf_event.h.
    
    Orabug: 33403351
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -912,6 +912,11 @@ struct x86_pmu {
 	int				num_hybrid_pmus;
 	struct x86_hybrid_pmu		*hybrid_pmu;
 	u8 (*get_hybrid_cpu_type)	(void);
+
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
 };
 
 struct x86_perf_task_context_opt {
