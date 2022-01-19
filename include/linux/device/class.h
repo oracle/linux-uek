a79fb7faac1b5d8e9065e703ad954f5780fa2a44
Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
2022-01-19 14:52:07 -0800

    uek: kabi: add kABI padding to include/linux/device/class.h
    Add room for future expansion, without breaking kABI,
    to include/linux/device/class.h.
    
    Orabug: 33403351
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/device/class.h
+++ include/linux/device/class.h
@@ -14,6 +14,7 @@
 #ifndef _DEVICE_CLASS_H_
 #define _DEVICE_CLASS_H_
 
+#include <linux/uek_kabi.h>
 #include <linux/kobject.h>
 #include <linux/klist.h>
 #include <linux/pm.h>
@@ -75,6 +76,11 @@ struct class {
 	const struct dev_pm_ops *pm;
 
 	struct subsys_private *p;
+
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
 };
 
 struct class_dev_iter {
