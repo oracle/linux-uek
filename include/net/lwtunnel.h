43cfe78464f527b1c070b4cf474f698719e4418b
Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
2022-01-19 14:52:19 -0800

    uek: kabi: add kABI padding to include/net/lwtunnel.h
    Add room for future expansion, without breaking kABI,
    to include/net/lwtunnel.h.
    
    Orabug: 33403351
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- include/net/lwtunnel.h
+++ include/net/lwtunnel.h
@@ -2,6 +2,7 @@
 #ifndef __NET_LWTUNNEL_H
 #define __NET_LWTUNNEL_H 1
 
+#include <linux/uek_kabi.h>
 #include <linux/lwtunnel.h>
 #include <linux/netdevice.h>
 #include <linux/skbuff.h>
@@ -30,6 +31,12 @@ struct lwtunnel_state {
 	int		(*orig_output)(struct net *net, struct sock *sk, struct sk_buff *skb);
 	int		(*orig_input)(struct sk_buff *);
 	struct		rcu_head rcu;
+
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
+
 	__u8            data[];
 };
 
