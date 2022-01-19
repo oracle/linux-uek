f67a20401d638a95a343869fc7594d21d70009d2
Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
2022-01-19 14:52:14 -0800

    uek: kabi: add kABI padding to include/linux/sched.h
    Add room for future expansion, without breaking kABI,
    to include/linux/sched.h.
    
    Orabug: 33403351
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -7,6 +7,7 @@
  * APIs (schedule(), wakeup variants, etc.)
  */
 
+#include <linux/uek_kabi.h>
 #include <uapi/linux/sched.h>
 
 #include <asm/current.h>
@@ -560,6 +561,11 @@ struct sched_entity {
 	 */
 	struct sched_avg		avg;
 #endif
+
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
 };
 
 struct sched_rt_entity {
@@ -1462,6 +1468,15 @@ struct task_struct {
 	unsigned long			lowest_stack;
 	unsigned long			prev_lowest_stack;
 #endif
+
+	UEK_KABI_RESERVE(1)
+	UEK_KABI_RESERVE(2)
+	UEK_KABI_RESERVE(3)
+	UEK_KABI_RESERVE(4)
+	UEK_KABI_RESERVE(5)
+	UEK_KABI_RESERVE(6)
+	UEK_KABI_RESERVE(7)
+	UEK_KABI_RESERVE(8)
 
 #ifdef CONFIG_X86_MCE
 	void __user			*mce_vaddr;
