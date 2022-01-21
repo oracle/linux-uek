c229bc4c6f343e8f0021003602a22092b191b195
Eelco Chaudron <echaudro@redhat.com>
2022-01-21 11:09:55 +0100

    bpf: add frags support to the bpf_xdp_adjust_tail() API
    This change adds support for tail growing and shrinking for XDP frags.
    
    When called on a non-linear packet with a grow request, it will work
    on the last fragment of the packet. So the maximum grow size is the
    last fragments tailroom, i.e. no new buffer will be allocated.
    A XDP frags capable driver is expected to set frag_size in xdp_rxq_info
    data structure to notify the XDP core the fragment size.
    frag_size set to 0 is interpreted by the XDP core as tail growing is
    not allowed.
    Introduce __xdp_rxq_info_reg utility routine to initialize frag_size field.
    
    When shrinking, it will work from the last fragment, all the way down to
    the base buffer depending on the shrinking size. It's important to mention
    that once you shrink down the fragment(s) are freed, so you can not grow
    again to the original size.
    
    Acked-by: Toke Hoiland-Jorgensen <toke@redhat.com>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Co-developed-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>
    Link: https://lore.kernel.org/r/eabda3485dda4f2f158b477729337327e609461d.1642758637.git.lorenzo@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    
    Orabug: 35383105
    
    (cherry picked from commit bf25146a5595269810b1f47d048f114c5ff9f544)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	include/net/xdp.h
    
    Solve kABI issue by adding frag_size to struct xdp_rxq_info using
    UEK_KABI_USE.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/net/xdp.h
+++ include/net/xdp.h
@@ -65,7 +65,7 @@ struct xdp_rxq_info {
 	struct xdp_mem_info mem;
 	unsigned int napi_id;
 
-	UEK_KABI_RESERVE(1)
+	UEK_KABI_USE(1, u32 frag_size)
 	UEK_KABI_RESERVE(2)
 	UEK_KABI_RESERVE(3)
 	UEK_KABI_RESERVE(4)
@@ -347,6 +347,8 @@ struct xdp_frame *xdp_convert_buff_to_frame(struct xdp_buff *xdp)
 	return xdp_frame;
 }
 
+void __xdp_return(void *data, struct xdp_mem_info *mem, bool napi_direct,
+		  struct xdp_buff *xdp);
 void xdp_return_frame(struct xdp_frame *xdpf);
 void xdp_return_frame_rx_napi(struct xdp_frame *xdpf);
 void xdp_return_buff(struct xdp_buff *xdp);
@@ -383,8 +385,17 @@ out:
 	__xdp_release_frame(xdpf->data, mem);
 }
 
-int xdp_rxq_info_reg(struct xdp_rxq_info *xdp_rxq,
-		     struct net_device *dev, u32 queue_index, unsigned int napi_id);
+int __xdp_rxq_info_reg(struct xdp_rxq_info *xdp_rxq,
+		       struct net_device *dev, u32 queue_index,
+		       unsigned int napi_id, u32 frag_size);
+static inline int
+xdp_rxq_info_reg(struct xdp_rxq_info *xdp_rxq,
+		 struct net_device *dev, u32 queue_index,
+		 unsigned int napi_id)
+{
+	return __xdp_rxq_info_reg(xdp_rxq, dev, queue_index, napi_id, 0);
+}
+
 void xdp_rxq_info_unreg(struct xdp_rxq_info *xdp_rxq);
 void xdp_rxq_info_unused(struct xdp_rxq_info *xdp_rxq);
 bool xdp_rxq_info_is_reg(struct xdp_rxq_info *xdp_rxq);
