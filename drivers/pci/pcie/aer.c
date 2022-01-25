7c74d65a96e8635e5dd59c77cb2014041873c7b7
Stefan Roese <sr@denx.de>
2022-01-25 08:18:18 +0100

    PCI/AER: Configure ECRC for every device
    Move pcie_set_ecrc_checking() to pci_aer_init() to make sure that
    pcie_set_ecrc_checking() is called for each PCIe device, including
    hot-added devices.
    
    Link: https://lore.kernel.org/r/20220125071820.2247260-2-sr@denx.de
    Signed-off-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Pali Rohár <pali@kernel.org>
    Cc: Bharat Kumar Gogada <bharat.kumar.gogada@xilinx.com>
    Cc: Michal Simek <michal.simek@xilinx.com>
    Cc: Yao Hongbo <yaohongbo@linux.alibaba.com>
    Cc: Naveen Naidu <naveennaidu479@gmail.com>
    (cherry picked from commit 9ffb98f144ebdc9345f3b34fac35c4ff12ae4e50)
    
    Orabug: 37778293
    
    Signed-off-by: Samasth Norway Ananda <samasth.norway.ananda@oracle.com>
    Reviewed-by: Karolina Stolarek <karolina.stolarek@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/pci/pcie/aer.c
+++ drivers/pci/pcie/aer.c
@@ -436,6 +436,8 @@ void pci_aer_init(struct pci_dev *dev)
 	pci_add_ext_cap_save_buffer(dev, PCI_EXT_CAP_ID_ERR, sizeof(u32) * n);
 
 	pci_aer_clear_status(dev);
+
+	pcie_set_ecrc_checking(dev);
 }
 
 void pci_aer_exit(struct pci_dev *dev)
@@ -1445,9 +1447,6 @@ static int set_device_error_reporting(struct pci_dev *dev, void *data)
 		else
 			pci_disable_pcie_error_reporting(dev);
 	}
-
-	if (enable)
-		pcie_set_ecrc_checking(dev);
 
 	return 0;
 }
