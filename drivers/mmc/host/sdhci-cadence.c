b449ee1f2dc835739f50b55334af80cbbe161630
David Clear <dac2@pensando.io>
2022-01-28 19:23:47 -0800

    drivers/mmc/host: Pensando Elba support in the Cadence EMMC host controller
    This patch adds Elba SoC support to the Cadence EMMC controller.
    The Elba SoC has particular accessor requirements for non-word-size
    registers.
    
    Signed-off-by: David Clear <dac2@pensando.io>
    
    Heavily modified by Dave Kleikamp to conform to non-upstream changes in
    UEK6.
    
    Orabug: 34091165
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/mmc/host/sdhci-cadence.c
+++ drivers/mmc/host/sdhci-cadence.c
@@ -174,9 +174,13 @@ struct sdhci_cdns_sd4_phy {
 
 struct sdhci_cdns_priv {
 	void __iomem *hrs_addr;
+	void __iomem *ctl_addr;	/* write control */
+	spinlock_t wrlock;	/* write lock */
 	bool enhanced_strobe;
 	const struct sdhci_cdns_data *cdns_data;
 	void *phy;
+	void (*priv_write_l)(struct sdhci_cdns_priv *priv, u32 val,
+			     void __iomem *reg);
 };
 
 struct sdhci_cdns_sd4_phy_cfg {
@@ -834,6 +838,15 @@ static void sdhci_cdns_sd6_phy_calc_settings(struct sdhci_cdns_sd6_phy *phy)
 	sdhci_cdns_sd6_phy_calc_io(phy);
 }
 
+static inline void sdhci_cdns_priv_writel(struct sdhci_cdns_priv *priv,
+					  u32 val, void __iomem *reg)
+{
+	if (unlikely(priv->priv_write_l))
+		priv->priv_write_l(priv, val, reg);
+	else
+		writel(val, reg);
+}
+
 static int sdhci_cdns_sd4_write_phy_reg(struct sdhci_cdns_priv *priv,
 					u8 addr, u8 data)
 {
@@ -851,14 +864,14 @@ static int sdhci_cdns_sd4_write_phy_reg(struct sdhci_cdns_priv *priv,
 	writel(tmp, reg);
 
 	tmp |= SDHCI_CDNS_SD4_HRS04_WR;
-	writel(tmp, reg);
+	sdhci_cdns_priv_writel(priv, tmp, reg);
 
 	ret = readl_poll_timeout(reg, tmp, tmp & SDHCI_CDNS_SD4_HRS04_ACK, 0, 10);
 	if (ret)
 		return ret;
 
 	tmp &= ~SDHCI_CDNS_SD4_HRS04_WR;
-	writel(tmp, reg);
+	sdhci_cdns_priv_writel(priv, tmp, reg);
 
 	ret = readl_poll_timeout(reg, tmp, !(tmp & SDHCI_CDNS_SD4_HRS04_ACK),
 				 0, 10);
@@ -1295,7 +1308,7 @@ static void sdhci_cdns_set_emmc_mode(struct sdhci_cdns_priv *priv, u32 mode)
 	tmp = readl(priv->hrs_addr + SDHCI_CDNS_HRS06);
 	tmp &= ~SDHCI_CDNS_HRS06_MODE;
 	tmp |= FIELD_PREP(SDHCI_CDNS_HRS06_MODE, mode);
-	writel(tmp, priv->hrs_addr + SDHCI_CDNS_HRS06);
+	sdhci_cdns_priv_writel(priv, tmp, priv->hrs_addr + SDHCI_CDNS_HRS06);
 }
 
 static u32 sdhci_cdns_get_emmc_mode(struct sdhci_cdns_priv *priv)
@@ -1648,7 +1661,7 @@ static int sdhci_cdns_sd4_set_tune_val(struct sdhci_host *host, unsigned int val
 	 */
 	for (i = 0; i < 2; i++) {
 		tmp |= SDHCI_CDNS_HRS06_TUNE_UP;
-		writel(tmp, reg);
+		sdhci_cdns_priv_writel(priv, tmp, reg);
 
 		ret = readl_poll_timeout(reg, tmp,
 					 !(tmp & SDHCI_CDNS_HRS06_TUNE_UP),
@@ -1700,6 +1713,89 @@ static int sdhci_cdns_execute_tuning(struct sdhci_host *host, u32 opcode)
 	}
 
 	return priv->cdns_data->set_tune_val(host, end_of_streak - max_streak / 2);
+}
+
+/*
+ * The Pensando Elba SoC explicitly controls byte-lane enables on writes
+ * which includes writes to the HRS registers.
+ */
+static void elba_priv_write_l(struct sdhci_cdns_priv *priv, u32 val,
+			      void __iomem *reg)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(&priv->wrlock, flags);
+	writel(0x78, priv->ctl_addr);
+	writel(val, reg);
+	spin_unlock_irqrestore(&priv->wrlock, flags);
+}
+
+static void elba_write_l(struct sdhci_host *host, u32 val, int reg)
+{
+	elba_priv_write_l(sdhci_cdns_priv(host), val, host->ioaddr + reg);
+}
+
+static void elba_write_w(struct sdhci_host *host, u16 val, int reg)
+{
+	struct sdhci_cdns_priv *priv = sdhci_cdns_priv(host);
+	unsigned long flags;
+	u32 m = (reg & 0x3);
+	u32 msk = (0x3 << (m));
+
+	spin_lock_irqsave(&priv->wrlock, flags);
+	writel(msk << 3, priv->ctl_addr);
+	writew(val, host->ioaddr + reg);
+	spin_unlock_irqrestore(&priv->wrlock, flags);
+}
+
+static void elba_write_b(struct sdhci_host *host, u8 val, int reg)
+{
+	struct sdhci_cdns_priv *priv = sdhci_cdns_priv(host);
+	unsigned long flags;
+	u32 m = (reg & 0x3);
+	u32 msk = (0x1 << (m));
+
+	spin_lock_irqsave(&priv->wrlock, flags);
+	writel(msk << 3, priv->ctl_addr);
+	writeb(val, host->ioaddr + reg);
+	spin_unlock_irqrestore(&priv->wrlock, flags);
+}
+
+static const struct sdhci_ops sdhci_elba_ops = {
+	.write_l = elba_write_l,
+	.write_w = elba_write_w,
+	.write_b = elba_write_b,
+	.set_clock = sdhci_set_clock,
+	.get_timeout_clock = sdhci_cdns_get_timeout_clock,
+	.set_bus_width = sdhci_set_bus_width,
+	.reset = sdhci_reset,
+	.set_uhs_signaling = sdhci_cdns_set_uhs_signaling,
+};
+
+static int elba_phy_probe(struct platform_device *pdev, struct sdhci_cdns_priv *priv)
+{
+	struct sdhci_host *host = platform_get_drvdata(pdev);
+	struct resource *iomem;
+	void __iomem *ioaddr;
+
+	host->mmc->caps |= (MMC_CAP_1_8V_DDR | MMC_CAP_8_BIT_DATA);
+
+	host->quirks &= ~SDHCI_QUIRK_CAP_CLOCK_BASE_BROKEN;
+
+	iomem = platform_get_resource(pdev, IORESOURCE_MEM, 1);
+	if (!iomem)
+		return -ENOMEM;
+
+	ioaddr = devm_platform_ioremap_resource(pdev, 1);
+	if (IS_ERR(ioaddr))
+		return PTR_ERR(ioaddr);
+
+	priv->ctl_addr = ioaddr;
+	priv->priv_write_l = elba_priv_write_l;
+	spin_lock_init(&priv->wrlock);
+	writel(0x78, priv->ctl_addr);
+
+	return 0;
 }
 
 static const struct sdhci_ops sdhci_cdns_sd4_ops = {
@@ -1764,6 +1860,14 @@ static const struct sdhci_cdns_of_data sdhci_cdns_sd6_of_data = {
 	.phy_probe = sdhci_cdns_sd6_phy_probe,
 };
 
+static const struct sdhci_pltfm_data sdhci_elba_pltfm_data = {
+	.ops = &sdhci_elba_ops,
+};
+
+static const struct sdhci_cdns_of_data sdhci_elba_of_data = {
+	.phy_probe = elba_phy_probe,
+	.pltfm_data = &sdhci_elba_pltfm_data,
+};
 
 static void sdhci_cdns_hs400_enhanced_strobe(struct mmc_host *mmc,
 					     struct mmc_ios *ios)
@@ -1810,6 +1914,7 @@ static int sdhci_cdns_probe(struct platform_device *pdev)
 	}
 
 	host = sdhci_pltfm_init(pdev, data->pltfm_data, sizeof(*priv));
+
 	if (IS_ERR(host)) {
 		ret = PTR_ERR(host);
 		goto disable_clk;
@@ -1843,9 +1948,11 @@ static int sdhci_cdns_probe(struct platform_device *pdev)
 	if (ret)
 		goto free;
 
-	ret = priv->cdns_data->phy_init(priv);
-	if (ret)
-		goto free;
+	if (priv->cdns_data) {
+		ret = priv->cdns_data->phy_init(priv);
+		if (ret)
+			goto free;
+	}
 
 	sdhci_enable_v4_mode(host);
 	ret = sdhci_add_host(host);
@@ -1898,6 +2005,10 @@ static const struct of_device_id sdhci_cdns_match[] = {
 	{
 		.compatible = "socionext,uniphier-sd4hc",
 		.data = &sdhci_cdns_uniphier_pltfm_data,
+	},
+	{
+		.compatible = "pensando,elba-emmc",
+		.data = &sdhci_elba_of_data
 	},
 	{
 		.compatible = "cdns,sd4hc",
