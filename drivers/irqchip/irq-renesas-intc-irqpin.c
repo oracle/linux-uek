f8b29546cfd0e81140bf44a1f7b7dee3229d7161
Marc Zyngier <maz@kernel.org>
2022-02-01 12:03:01 +0000

    irqchip/renesas-intc-irqpin: Move PM device over to irq domain
    Move the reference to the device over to the irq domain.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Bartosz Golaszewski <brgl@bgdev.pl>
    Link: https://lore.kernel.org/r/20220201120310.878267-4-maz@kernel.org
    
    Orabug: 35383105
    
    (cherry picked from commit c2ea6b9b03c1acde89c6100aff894e64386e72cc)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: c2ea6b9b03c1acde89c6100aff894e64386e72cc
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/irqchip/irq-renesas-intc-irqpin.c
+++ drivers/irqchip/irq-renesas-intc-irqpin.c
@@ -507,7 +507,6 @@ static int intc_irqpin_probe(struct platform_device *pdev)
 
 	irq_chip = &p->irq_chip;
 	irq_chip->name = "intc-irqpin";
-	irq_chip->parent_device = dev;
 	irq_chip->irq_mask = disable_fn;
 	irq_chip->irq_unmask = enable_fn;
 	irq_chip->irq_set_type = intc_irqpin_irq_set_type;
@@ -521,6 +520,8 @@ static int intc_irqpin_probe(struct platform_device *pdev)
 		dev_err(dev, "cannot initialize irq domain\n");
 		goto err0;
 	}
+
+	irq_domain_set_pm_device(p->irq_domain, dev);
 
 	if (p->shared_irqs) {
 		/* request one shared interrupt */
