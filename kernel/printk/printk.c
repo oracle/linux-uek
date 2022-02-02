ca33b5e743484ad28f4550cfbbddbcc30588aae1
Stephen Brennan <stephen.s.brennan@oracle.com>
2022-02-02 09:18:20 -0800

    printk: Avoid livelock with heavy printk during panic
    During panic(), if another CPU is writing heavily the kernel log (e.g.
    via /dev/kmsg), then the panic CPU may livelock writing out its messages
    to the console. Note when too many messages are dropped during panic and
    suppress further printk, except from the panic CPU. This could result in
    some important messages being dropped. However, messages are already
    being dropped, so this approach at least prevents a livelock.
    
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20220202171821.179394-4-stephen.s.brennan@oracle.com
    (cherry picked from commit 13fb0f74d7029df3b8137f11ef955e578a4a4a60)
    
    Conflicts:
      kernel/printk/printk.c - changes due to lockless printk ringbuffer
    
    Orabug: 33740420
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Reviewed-by: Junxiao Bi <junxiao.bi@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -93,6 +93,12 @@ EXPORT_SYMBOL_GPL(console_drivers);
  */
 int __read_mostly suppress_printk;
 
+/*
+ * During panic, heavy printk by other CPUs can delay the
+ * panic and risk deadlock on console resources.
+ */
+int __read_mostly suppress_panic_printk;
+
 #ifdef CONFIG_LOCKDEP
 static struct lockdep_map console_lock_dep_map = {
 	.name = "console_lock"
@@ -2007,6 +2013,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 	if (unlikely(suppress_printk))
 		return 0;
 
+	if (unlikely(suppress_panic_printk) &&
+	    atomic_read(&panic_cpu) != raw_smp_processor_id())
+		return 0;
+
 	if (level == LOGLEVEL_SCHED) {
 		level = LOGLEVEL_DEFAULT;
 		in_sched = true;
@@ -2421,6 +2431,7 @@ void console_unlock(void)
 {
 	static char ext_text[CONSOLE_EXT_LOG_MAX];
 	static char text[LOG_LINE_MAX + PREFIX_MAX];
+	static int panic_console_dropped;
 	unsigned long flags;
 	bool do_cond_resched, retry;
 
@@ -2473,6 +2484,10 @@ again:
 			/* messages are gone, move to first one */
 			console_seq = log_first_seq;
 			console_idx = log_first_idx;
+			if (panic_in_progress() && panic_console_dropped++ > 10) {
+				suppress_panic_printk = 1;
+				pr_warn_once("Too many dropped messages. Suppress messages on non-panic CPUs to prevent livelock.\n");
+			}
 		} else {
 			len = 0;
 		}
