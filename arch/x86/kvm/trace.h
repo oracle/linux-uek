d74c33d8291a531d03b0aafcafcb8a41fcdcad44
Sean Christopherson <seanjc@google.com>
2022-02-04 21:42:04 +0000

    KVM: x86: Treat x2APIC's ICR as a 64-bit register, not two 32-bit regs
    Emulate the x2APIC ICR as a single 64-bit register, as opposed to forking
    it across ICR and ICR2 as two 32-bit registers.  This mirrors hardware
    behavior for Intel's upcoming IPI virtualization support, which does not
    split the access.
    
    Previous versions of Intel's SDM and AMD's APM don't explicitly state
    exactly how ICR is reflected in the vAPIC page for x2APIC, KVM just
    happened to speculate incorrectly.
    
    Handling the upcoming behavior is necessary in order to maintain
    backwards compatibility with KVM_{G,S}ET_LAPIC, e.g. failure to shuffle
    the 64-bit ICR to ICR+ICR2 and vice versa would break live migration if
    IPI virtualization support isn't symmetrical across the source and dest.
    
    Cc: Zeng Guang <guang.zeng@intel.com>
    Cc: Chao Gao <chao.gao@intel.com>
    Cc: Maxim Levitsky <mlevitsk@redhat.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20220204214205.3306634-10-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit a57a31684d7be2d4427292db21ef622ea9591f5b)
    
    Orabug: 34160612
    
    Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    
    Conflicts:
    	arch/x86/kvm/lapic.c
    This commit was merged before:
    b9964ee36bdf KVM: x86: Make kvm_lapic_set_reg() a "private" xAPIC helper
    even though it uses functions like __kvm_lapic_get_reg() which where introduced
    by b9964ee36bdf.
    
    Squash commit:
    b9964ee36bdf KVM: x86: Make kvm_lapic_set_reg() a "private" xAPIC helper
    (cherry picked from commit b9964ee36bdf2ffcbe9084510874c2ff97b3cb46)
    to preserve bisectability of the repo. It cannot be simply rebased to appear
    before the current commit because they both use functions that the other
    provides (__kvm_lapic_get_reg() and kvm_x2apic_icr_write() ).
    
    (cherry picked from commit 7d4c841993d5f1bd169cb74d2f23bdd2dff704cd)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/x86/kvm/trace.h
+++ arch/x86/kvm/trace.h
@@ -251,13 +251,13 @@ TRACE_EVENT(kvm_cpuid,
  * Tracepoint for apic access.
  */
 TRACE_EVENT(kvm_apic,
-	TP_PROTO(unsigned int rw, unsigned int reg, unsigned int val),
+	TP_PROTO(unsigned int rw, unsigned int reg, u64 val),
 	TP_ARGS(rw, reg, val),
 
 	TP_STRUCT__entry(
 		__field(	unsigned int,	rw		)
 		__field(	unsigned int,	reg		)
-		__field(	unsigned int,	val		)
+		__field(	u64,		val		)
 	),
 
 	TP_fast_assign(
@@ -266,7 +266,7 @@ TRACE_EVENT(kvm_apic,
 		__entry->val		= val;
 	),
 
-	TP_printk("apic_%s %s = 0x%x",
+	TP_printk("apic_%s %s = 0x%llx",
 		  __entry->rw ? "write" : "read",
 		  __print_symbolic(__entry->reg, kvm_trace_symbol_apic),
 		  __entry->val)
