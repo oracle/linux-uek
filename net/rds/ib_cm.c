2dacfd873ea7c64706b05e6afc8fc3489cf55cf6
Nagappan Ramasamy Palaniappan <nagappan.ramasamy.palaniappan@oracle.com>
2022-02-04 04:14:52 +0000

    rds/ib: handle posted ACK during connection shutdown
    We need to check and if needed poll for ack work request posted by rds
    during connection shutdown. This is part of reaping of all tx
    completions when rds connection is shutting down.
    
    Orabug: 33820747
    
    Fixes: db4cdc1358b0 (Revert "rds/ib: handle posted ACK during connection
    shutdown")
    
    Suggested-by: Håkon Bugge <haakon.bugge@oracle.com>
    Signed-off-by: Nagappan Ramasamy Palaniappan <nagappan.ramasamy.palaniappan@oracle.com>
    Reviewed-by: Arumugam Kolappan <aru.kolappan@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- net/rds/ib_cm.c
+++ net/rds/ib_cm.c
@@ -1983,6 +1983,7 @@ unsigned long rds_ib_conn_path_shutdown_check_wait(struct rds_conn_path *cp)
 		test_bit(RDS_IB_CQ_ERR, &ic->i_flags) ||
 		(rds_ib_ring_empty(&ic->i_recv_ring) &&
 		 rds_ib_ring_empty(&ic->i_send_ring) &&
+		 (!test_bit(IB_ACK_IN_FLIGHT, &ic->i_ack_flags)) &&
 		 (atomic_read(&ic->i_fastreg_wrs) ==
 		  RDS_IB_DEFAULT_FREG_WR))) ? 0
 		: msecs_to_jiffies(1000);
@@ -1994,7 +1995,8 @@ void rds_ib_conn_path_shutdown_tidy_up(struct rds_conn_path *cp)
 	struct rds_ib_connection *ic = conn->c_transport_data;
 
 	/* Try to reap pending TX completions every second */
-	if (!rds_ib_ring_empty(&ic->i_send_ring))
+	if (!rds_ib_ring_empty(&ic->i_send_ring) ||
+	    test_bit(IB_ACK_IN_FLIGHT, &ic->i_ack_flags))
 		rds_ib_tx_poll(ic);
 
 	/* Try to reap pending RX completions every second */
