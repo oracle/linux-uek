ae4b6b29ff35151876767c71a5abee6a34a3c3d0
Subbaraya Sundeep <sbhatta@marvell.com>
2022-02-10 17:21:44 +0530

    octeontx2-pf: Add TC feature for VFs
    This patch adds TC feature for VFs also. When MCAM
    rules are allocated for a VF then either TC or ntuple
    filters can be used. Below are the commands to use
    TC feature for a VF(say lbk0):
    
    devlink dev param set pci/0002:01:00.1 name mcam_count value 16 \
     cmode runtime
    ethtool -K lbk0 hw-tc-offload on
    ifconfig lbk0 up
    tc qdisc add dev lbk0 ingress
    tc filter add dev lbk0 parent ffff: protocol ip flower skip_sw \
     dst_mac 98:03:9b:83:aa:12 action police rate 100Mbit burst 5000
    
    Also to modify any fields of the hardware context with
    NIX_AQ_INSTOP_WRITE command then corresponding masks of those
    fields must be set as per hardware. This was missing in
    ingress ratelimiting context. This patch sets those masks also.
    
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4b0385bc8e6a52797602196714f9a77f62cd540d)
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_tc.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_tc.c
@@ -64,7 +64,7 @@ int otx2_tc_alloc_ent_bitmap(struct otx2_nic *nic)
 {
 	struct otx2_tc_info *tc = &nic->tc_info;
 
-	if (!nic->flow_cfg->max_flows || is_otx2_vf(nic->pcifunc))
+	if (!nic->flow_cfg->max_flows)
 		return 0;
 
 	/* Max flows changed, free the existing bitmap */
@@ -1121,6 +1121,7 @@ int otx2_setup_tc(struct net_device *netdev, enum tc_setup_type type,
 		return -EOPNOTSUPP;
 	}
 }
+EXPORT_SYMBOL(otx2_setup_tc);
 
 static const struct rhashtable_params tc_flow_ht_params = {
 	.head_offset = offsetof(struct otx2_tc_flow, node),
@@ -1155,6 +1156,7 @@ int otx2_init_tc(struct otx2_nic *nic)
 	}
 	return err;
 }
+EXPORT_SYMBOL(otx2_init_tc);
 
 void otx2_shutdown_tc(struct otx2_nic *nic)
 {
@@ -1163,3 +1165,4 @@ void otx2_shutdown_tc(struct otx2_nic *nic)
 	kfree(tc->tc_entries_bitmap);
 	rhashtable_destroy(&tc->flow_table);
 }
+EXPORT_SYMBOL(otx2_shutdown_tc);
