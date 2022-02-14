b3724257e95d45b0d4aa8a1c5bf8c4a225f9b3df
Rayan Dasoriya <dasoriya@google.com>
2022-02-14 20:06:51 +0000

    x86/quirks: Scan all busses for early PCI quirks
    Recently was noticed in an HP GEN9 system that kdump couldn't succeed
    due to an irq storm coming from an Intel NIC, narrowed down to be lack
    of clearing the MSI/MSI-X enable bits during the kdump kernel boot.
    For that, we need an early quirk to manually turn off MSI/MSI-X for
    PCI devices - this was worked as an optional boot parameter in a
    subsequent patch.
    
    Problem is that in our test system, the Intel NICs were not present in
    any secondary bus under the first PCIe root complex, so they couldn't
    be reached by the recursion in check_dev_quirk(). Modern systems,
    specially with multi-processors and multiple NUMA nodes expose multiple
    root complexes, describing more than one PCI hierarchy domain. Currently
    the simple recursion present in the early-quirks code from x86 starts a
    descending recursion from bus 0000:00, and reach many other busses by
    navigating this hierarchy walking through the bridges. This is not
    enough in systems with more than one root complex/host bridge, since
    the recursion won't "traverse" to other root complexes by starting
    statically in 0000:00 (for more details, see [0]).
    
    This patch hence implements the full bus/device/function scan in
    early_quirks(), by checking all possible busses instead of using a
    recursion based on the first root bus or limiting the search scope to
    the first 32 busses (like it was done in the beginning [1]).
    
    [0] https://bugs.launchpad.net/bugs/1797990
    
    [1] From historical perspective, early PCI scan dates back
    to BitKeeper, added by Andi Kleen's "[PATCH] APIC fixes for x86-64",
    on October/2003. It initially restricted the search to the first
    32 busses and slots.
    
    Due to a potential bug found in Nvidia chipsets, the scan
    was changed to run only in the first root bus: see
    commit 8659c406ade3 ("x86: only scan the root bus in early PCI quirks")
    
    Finally, secondary busses reachable from the 1st bus were re-added back by:
    commit 850c321027c2 ("x86/quirks: Reintroduce scanning of secondary buses")
    
    Reported-by: Dan Streetman <ddstreet@canonical.com>
    Signed-off-by: Guilherme G. Piccoli <gpiccoli@canonical.com>
    
    BUG=b/160734277
    TEST=presubmit and validation
    RELEASE_NOTE=x86/quirks: Scan all busses for early PCI quirks
    SOURCE=FROMLIST(https://lore.kernel.org/linux-pci/20181018183721.27467-1-gpiccoli@canonical.com/)
    
    Signed-off-by: Guilherme G. Piccoli <gpiccoli@canonical.com>
    Change-Id: Ie6204b709a1760331bc5fa069a2a4b12f4655566
    Reviewed-on: https://cos-review.googlesource.com/c/third_party/kernel/+/29401
    Tested-by: Cusky Presubmit Bot <presubmit@cos-infra-prod.iam.gserviceaccount.com>
    Reviewed-by: Oleksandr Tymoshenko <ovt@google.com>
    Reviewed-on: https://cos-review.googlesource.com/c/third_party/kernel/+/41521
    Tested-by: Varsha Teratipally <teratipally@google.com>
    (cherry picked from commit 32afca47eaeb9d9bf5e562be7e28194c198781f6)
    From COS kernel repo at:
    https://cos.googlesource.com/third_party/kernel/+/release-R117-cos-6.6
    Orabug: 37383446
    
    Conflicts:
    	arch/x86/kernel/early-quirks.c
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>

--- arch/x86/kernel/early-quirks.c
+++ arch/x86/kernel/early-quirks.c
@@ -781,7 +781,6 @@ static int __init check_dev_quirk(int num, int slot, int func)
 	u16 vendor;
 	u16 device;
 	u8 type;
-	u8 sec;
 	int i;
 
 	class = read_pci_config_16(num, slot, func, PCI_CLASS_DEVICE);
@@ -810,11 +809,8 @@ static int __init check_dev_quirk(int num, int slot, int func)
 	type = read_pci_config_byte(num, slot, func,
 				    PCI_HEADER_TYPE);
 
-	if ((type & PCI_HEADER_TYPE_MASK) == PCI_HEADER_TYPE_BRIDGE) {
-		sec = read_pci_config_byte(num, slot, func, PCI_SECONDARY_BUS);
-		if (sec > num)
-			early_pci_scan_bus(sec);
-	}
+	if ((type & 0x7f) == PCI_HEADER_TYPE_BRIDGE)
+		return -1;
 
 	if (!(type & PCI_HEADER_TYPE_MFD))
 		return -1;
@@ -837,8 +833,10 @@ static void __init early_pci_scan_bus(int bus)
 
 void __init early_quirks(void)
 {
+	int bus;
 	if (!early_pci_allowed())
 		return;
 
-	early_pci_scan_bus(0);
+	for (bus = 0; bus < 256; bus++)
+		early_pci_scan_bus(bus);
 }
