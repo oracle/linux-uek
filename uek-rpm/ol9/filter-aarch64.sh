158a89e1aeab08d6894ccee431302ce5f8c2c067
Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
2022-02-17 11:26:57 -0800

    uek-rpm: Adjust rpm modules list
    Orabug: 33816737
    
    Modules are moved between core, modules and extra rpms to keep
     - kernel-uek-core with minimal number of modules
     - all needed modules in core to support Oracle Servers.
    
    From modules-extra to core rpm.
    	net/rds
    	fs/ocfs2
    
    From modules to core rpm.
    	drivers/net/ethernet/broadcom
    	drivers/scsi/aic7xxx
    	drivers/scsi/bfa
    	drivers/scsi/bnx2i
    	drivers/scsi/bnx2fc
    	drivers/scsi/fcoe
    	drivers/scsi/libsas
    	drivers/scsi/lpfc
    	drivers/scsi/megaraid
    	drivers/scsi/mpt3sas
    	drivers/scsi/pm8001
    	drivers/scsi/qla2xxx
    	drivers/scsi/qla4xxx
    	drivers/scsi/iscsi_boot_sysfs.ko
    	drivers/uio
    	drivers/infiniband/core/ib_umad.ko
    	drivers/infiniband/core/ib_uverbs.ko
    	drivers/infiniband/core/rdma_ucm.ko
    	drivers/infiniband/core/rdmaip_notify.ko
    	drivers/infiniband/hw/mlx4/mlx4_ib.ko
    	drivers/infiniband/hw/mlx5/mlx5_ib.ko
    	drivers/infiniband/ulp/ipoib/ib_ipoib.ko
    
    From core to modules. These modules are not required in Exadata and OCI systems.
    	drivers/gpio
    	drivers/gpu
    	drivers/hid
    	drivers/hwtracing
    	drivers/pinctrl
    	drivers/pps
    
    Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- uek-rpm/ol9/filter-aarch64.sh
+++ uek-rpm/ol9/filter-aarch64.sh
@@ -11,10 +11,10 @@
 
 overrides="cec rvu_mbox ib_core ib_cm iw_cm rdma_cm"
 
-driverdirs="atm auxdisplay bcma bluetooth firewire fmc infiniband isdn leds media memstick message mmc mtd mwave nfc ntb pcmcia platform power ssb staging tty uio uwb w1"
+driverdirs="atm auxdisplay bcma bluetooth firewire fmc infiniband isdn leds media memstick message mmc mtd mwave nfc ntb pcmcia platform power ssb staging tty uwb w1"
 
-ethdrvs="3com adaptec arc alteon atheros broadcom cadence calxeda chelsio cisco dec dlink emulex icplus marvell micrel myricom neterion nvidia oki-semi packetengines qlogic rdc renesas sfc silan sis smsc stmicro sun tehuti ti via wiznet xircom"
+ethdrvs="3com adaptec arc alteon atheros cadence calxeda chelsio cisco dec dlink emulex icplus marvell micrel myricom neterion nvidia oki-semi packetengines qlogic rdc renesas sfc silan sis smsc stmicro sun tehuti ti via wiznet xircom"
 
 drmdrvs="amd arm bridge ast exynos hisilicon i2c imx mgag200 meson msm nouveau panel radeon rcar-du rockchip tegra sun4i tinydrm vc4"
 
-singlemods="ntb_netdev iscsi_ibft iscsi_boot_sysfs megaraid pmcraid qedi qla1280 9pnet_rdma rpcrdma nvmet-rdma nvme-rdma hid-picolcd hid-prodikeys hwa-hc hwpoison-inject target_core_user sbp_target cxgbit iw_cxgb3 iw_cxgb4 cxgb3i cxgb3i cxgb3i_ddp cxgb4i chcr chtls cros_ec_sensors_core cros_ec_sensors cros_ec_baro cros_ec_light_prox cros_ec_keyb cros_ec_dev regmap-sdw regmap-sdw-mbq"
+singlemods="ntb_netdev iscsi_ibft megaraid pmcraid qedi qla1280 9pnet_rdma rpcrdma nvmet-rdma nvme-rdma hid-picolcd hid-prodikeys hwa-hc hwpoison-inject target_core_user sbp_target cxgbit iw_cxgb3 iw_cxgb4 cxgb3i cxgb3i cxgb3i_ddp cxgb4i chcr chtls cros_ec_sensors_core cros_ec_sensors cros_ec_baro cros_ec_light_prox cros_ec_keyb cros_ec_dev regmap-sdw regmap-sdw-mbq b44 tg3"
