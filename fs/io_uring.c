1e7f7c92b036691228795117c4f14a6644cd3ef4
Jens Axboe <axboe@kernel.dk>
2022-02-21 05:49:30 -0700

    io_uring: don't convert to jiffies for waiting on timeouts
    commit 228339662b398a59b3560cd571deb8b25b253c7e upstream.
    
    If an application calls io_uring_enter(2) with a timespec passed in,
    convert that timespec to ktime_t rather than jiffies. The latter does
    not provide the granularity the application may expect, and may in
    fact provided different granularity on different systems, depending
    on what the HZ value is configured at.
    
    Turn the timespec into an absolute ktime_t, and use that with
    schedule_hrtimeout() instead.
    
    Link: https://github.com/axboe/liburing/issues/531
    Cc: stable@vger.kernel.org
    Reported-by: Bob Chen <chenbo.chen@alibaba-inc.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 7c83437fb3aeda489638304edf7c386b16e7cc5b)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -7591,7 +7591,7 @@ static int io_run_task_work_sig(void)
 /* when returns >0, the caller should retry */
 static inline int io_cqring_wait_schedule(struct io_ring_ctx *ctx,
 					  struct io_wait_queue *iowq,
-					  signed long *timeout)
+					  ktime_t timeout)
 {
 	int ret;
 
@@ -7603,8 +7603,9 @@ static inline int io_cqring_wait_schedule(struct io_ring_ctx *ctx,
 	if (test_bit(0, &ctx->check_cq_overflow))
 		return 1;
 
-	*timeout = schedule_timeout(*timeout);
-	return !*timeout ? -ETIME : 1;
+	if (!schedule_hrtimeout(&timeout, HRTIMER_MODE_ABS))
+		return -ETIME;
+	return 1;
 }
 
 /*
@@ -7617,7 +7618,7 @@ static int io_cqring_wait(struct io_ring_ctx *ctx, int min_events,
 {
 	struct io_wait_queue iowq;
 	struct io_rings *rings = ctx->rings;
-	signed long timeout = MAX_SCHEDULE_TIMEOUT;
+	ktime_t timeout = KTIME_MAX;
 	int ret;
 
 	do {
@@ -7633,7 +7634,7 @@ static int io_cqring_wait(struct io_ring_ctx *ctx, int min_events,
 
 		if (get_timespec64(&ts, uts))
 			return -EFAULT;
-		timeout = timespec64_to_jiffies(&ts);
+		timeout = ktime_add_ns(timespec64_to_ktime(ts), ktime_get_ns());
 	}
 
 	if (sig) {
@@ -7665,7 +7666,7 @@ static int io_cqring_wait(struct io_ring_ctx *ctx, int min_events,
 		}
 		prepare_to_wait_exclusive(&ctx->cq_wait, &iowq.wq,
 						TASK_INTERRUPTIBLE);
-		ret = io_cqring_wait_schedule(ctx, &iowq, &timeout);
+		ret = io_cqring_wait_schedule(ctx, &iowq, timeout);
 		finish_wait(&ctx->cq_wait, &iowq.wq);
 		cond_resched();
 	} while (ret > 0);
