788a30dedf8bae48f2caa5159e2db1a45f64bfe6
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2022-02-22 21:57:40 +0300

    x86/coco: Add API to handle encryption mask
    AMD SME/SEV uses a bit in the page table entries to indicate that the
    page is encrypted and not accessible to the VMM.
    
    TDX uses a similar approach, but the polarity of the mask is opposite to
    AMD: if the bit is set the page is accessible to VMM.
    
    Provide vendor-neutral API to deal with the mask: cc_mkenc() and
    cc_mkdec() modify given address to make it encrypted/decrypted. It can
    be applied to phys_addr_t, pgprotval_t or page table entry value.
    
    pgprot_encrypted() and pgprot_decrypted() reimplemented using new
    helpers.
    
    The implementation will be extended to cover TDX.
    
    pgprot_decrypted() is used by drivers (i915, virtio_gpu, vfio).
    cc_mkdec() called by pgprot_decrypted(). Export cc_mkdec().
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Link: https://lore.kernel.org/r/20220222185740.26228-5-kirill.shutemov@linux.intel.com
    (cherry picked from commit b577f542f93cbba57f8d6185ef1fb13a41ddf162)
    Orabug: 37070016
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Ian Chin Wang <ian.chin.wang@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -15,16 +15,11 @@
 		     cachemode2protval(_PAGE_CACHE_MODE_UC_MINUS)))	\
 	 : (prot))
 
-/*
- * Macros to add or remove encryption attribute
- */
-#define pgprot_encrypted(prot)	__pgprot(__sme_set(pgprot_val(prot)))
-#define pgprot_decrypted(prot)	__pgprot(__sme_clr(pgprot_val(prot)))
-
 #ifndef __ASSEMBLY__
 #include <asm/x86_init.h>
 #include <asm/pkru.h>
 #include <asm/fpu/api.h>
+#include <asm/coco.h>
 #include <asm-generic/pgtable_uffd.h>
 
 extern pgd_t early_top_pgt[PTRS_PER_PGD];
@@ -35,6 +30,12 @@ void ptdump_walk_pgd_level_debugfs(struct seq_file *m, struct mm_struct *mm,
 				   bool user);
 void ptdump_walk_pgd_level_checkwx(void);
 void ptdump_walk_user_pgd_level_checkwx(void);
+
+/*
+ * Macros to add or remove encryption attribute
+ */
+#define pgprot_encrypted(prot)	__pgprot(cc_mkenc(pgprot_val(prot)))
+#define pgprot_decrypted(prot)	__pgprot(cc_mkdec(pgprot_val(prot)))
 
 #ifdef CONFIG_DEBUG_WX
 #define debug_checkwx()		ptdump_walk_pgd_level_checkwx()
