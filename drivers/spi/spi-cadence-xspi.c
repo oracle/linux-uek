229decc5c4b6eb49a3af536303cb94c2e30dcd14
George Cherian <george.cherian@marvell.com>
2022-02-28 23:33:02 +0530

    drivers:spi:cadence-xspi: Switch to polling mode when in panic
    The kernel mtd-oops logger after a panic will work only in polling mode.
    Interrupts won't be delivered after a panic. Also make sure the driver
    doesn't enter any sleep/schedule calls.
    
    Add additional checks to make sure we are not in oops write path.
    
    Change-Id: I7e4cda66e9822d8cedd1e4e2cd1476ea16dd4b82
    Signed-off-by: George Cherian <george.cherian@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/71874
    Reviewed-by: Witold Sadowski <wsadowski@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    
    Orabug: 33889959
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    (cherry picked from commit 3851739bb923c5963afbff0b85da4af70ba6c9ce)
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/spi/spi-cadence-xspi.c
+++ drivers/spi/spi-cadence-xspi.c
@@ -638,7 +638,7 @@ static int cdns_xspi_send_stig_command(struct cdns_xspi_dev *cdns_xspi,
 
 		cdns_xspi_trigger_command(cdns_xspi, cmd_regs);
 
-		if (cdns_xspi->irq) {
+		if (cdns_xspi->irq && pstore_sleep) {
 			wait_for_completion(&cdns_xspi->sdma_complete);
 			if (cdns_xspi->sdma_error) {
 				cdns_xspi_set_interrupts(cdns_xspi, false);
@@ -651,7 +651,7 @@ static int cdns_xspi_send_stig_command(struct cdns_xspi_dev *cdns_xspi,
 		cdns_xspi_sdma_handle(cdns_xspi);
 	}
 
-	if (cdns_xspi->irq) {
+	if (cdns_xspi->irq && pstore_sleep) {
 		wait_for_completion(&cdns_xspi->cmd_complete);
 		cdns_xspi_set_interrupts(cdns_xspi, false);
 	} else {
