f25f2f5cde2b43200615f5eb6d0fcb14bf380f0b
Wojciech Drewek <wojciech.drewek@intel.com>
2022-03-04 17:40:45 +0100

    net/sched: Allow flower to match on GTP options
    Options are as follows: PDU_TYPE:QFI and they refernce to
    the fields from the  PDU Session Protocol. PDU Session data
    is conveyed in GTP-U Extension Header.
    
    GTP-U Extension Header is described in 3GPP TS 29.281.
    PDU Session Protocol is described in 3GPP TS 38.415.
    
    PDU_TYPE -  indicates the type of the PDU Session Information (4 bits)
    QFI      -  QoS Flow Identifier (6 bits)
    
      # ip link add gtp_dev type gtp role sgsn
      # tc qdisc add dev gtp_dev ingress
      # tc filter add dev gtp_dev protocol ip parent ffff: \
          flower \
            enc_key_id 11 \
            gtp_opts 1:8/ff:ff \
          action mirred egress redirect dev eth0
    
    Signed-off-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit e3acda7ade0a36c5cbebc2b54d30b7f08a4ba29b)
    
    Conflicts:
    	include/net/gtp.h
    
    Orabug: 35365171
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>

--- include/net/gtp.h
+++ include/net/gtp.h
@@ -27,6 +27,37 @@ struct gtp1_header {	/* According to 3GPP TS 29.060. */
 	__be32	tid;
 } __attribute__ ((packed));
 
+struct gtp1_header_long {	/* According to 3GPP TS 29.060. */
+	__u8	flags;
+	__u8	type;
+	__be16	length;
+	__be32	tid;
+	__be16	seq;
+	__u8	npdu;
+	__u8	next;
+} __packed;
+
+/* GTP Information Element */
+struct gtp_ie {
+	__u8	tag;
+	__u8	val;
+} __packed;
+
+struct gtp0_packet {
+	struct gtp0_header gtp0_h;
+	struct gtp_ie ie;
+} __packed;
+
+struct gtp1u_packet {
+	struct gtp1_header_long gtp1u_h;
+	struct gtp_ie ie;
+} __packed;
+
+struct gtp_pdu_session_info {	/* According to 3GPP TS 38.415. */
+	u8	pdu_type;
+	u8	qfi;
+};
+
 #define GTP1_F_NPDU	0x01
 #define GTP1_F_SEQ	0x02
 #define GTP1_F_EXTHDR	0x04
