bcd20aa65e98e826ca5ae5de9434ff683ab21e0c
Eric Dumazet <edumazet@google.com>
2022-03-10 21:14:20 -0800

    net: add per-cpu storage and net->core_stats
    Before adding yet another possibly contended atomic_long_t,
    it is time to add per-cpu storage for existing ones:
     dev->tx_dropped, dev->rx_dropped, and dev->rx_nohandler
    
    Because many devices do not have to increment such counters,
    allocate the per-cpu storage on demand, so that dev_get_stats()
    does not have to spend considerable time folding zero counters.
    
    Note that some drivers have abused these counters which
    were supposed to be only used by core networking stack.
    
    v4: should use per_cpu_ptr() in dev_get_stats() (Jakub)
    v3: added a READ_ONCE() in netdev_core_stats_alloc() (Paolo)
    v2: add a missing include (reported by kernel test robot <lkp@intel.com>)
        Change in netdev_core_stats_alloc() (Jakub)
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: jeffreyji <jeffreyji@google.com>
    Reviewed-by: Brian Vazquez <brianvv@google.com>
    Reviewed-by: Jakub Kicinski <kuba@kernel.org>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Link: https://lore.kernel.org/r/20220311051420.2608812-1-eric.dumazet@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 35290196
    
    (cherry picked from commit 625788b5844511cf4c30cffa7fa0bc3a69cebc82)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/net/tun.c
    	net/core/gro_cells.c
    	net/hsr/hsr_device.c
    
    Small conflicts in a context caused by already applied fixes.
    
    Conflicts:
    	include/linux/netdevice.h
    
    Small conflicts in a context caused by kABI solutions.
    Keep rx_dropped, tx_dropped, and rx_nohandler; use UEK_KABI_USE() to add
    core_stats to avoid kABI breakages.
    
    Conflicts:
    	drivers/net/vxlan/vxlan_core.c
    
    Apply the change to file drivers/net/vxlan.c instead.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Jack Vogel  <jack.vogel@oracle.com>

--- drivers/net/vxlan/vxlan_core.c
+++ drivers/net/vxlan/vxlan_core.c
@@ -1937,7 +1937,7 @@ static int vxlan_rcv(struct sock *sk, struct sk_buff *skb)
 
 	if (unlikely(!(vxlan->dev->flags & IFF_UP))) {
 		rcu_read_unlock();
-		atomic_long_inc(&vxlan->dev->rx_dropped);
+		dev_core_stats_rx_dropped_inc(vxlan->dev);
 		goto drop;
 	}
 
