e56bec0147f3a8fb57c77837e247e8ec8521636e
Sean Christopherson <seanjc@google.com>
2022-03-11 04:35:15 +0000

    KVM: x86: Make APICv inhibit reasons an enum and cleanup naming
    Use an enum for the APICv inhibit reasons, there is no meaning behind
    their values and they most definitely are not "unsigned longs".  Rename
    the various params to "reason" for consistency and clarity (inhibit may
    be confused as a command, i.e. inhibit APICv, instead of the reason that
    is getting toggled/checked).
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20220311043517.17027-2-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 7491b7b2e1c57990dcd0f60ed2f3f1c92a145486)
    
    Orabug: 34160612
    
    Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    
    Conflicts:
    	arch/x86/kvm/svm/svm.h
    UEK is missing:
    abb6d479e226 KVM: x86: make several APIC virtualization callbacks optional
    which removes the declaration for avic_load_eoi_exitmap()
    
    	arch/x86/kvm/x86.c
    7446cfebe8be KVM: x86: Drop NULL check on kvm_x86_ops.check_apicv_inhibit_reasons
    which removes a line from __kvm_request_apicv_update()
    
    (cherry picked from commit 35f64f8dbeda7ce731a786108caa16fe6a0a7b33)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/x86/kvm/svm/avic.c
+++ arch/x86/kvm/svm/avic.c
@@ -853,7 +853,7 @@ out:
 	return ret;
 }
 
-bool avic_check_apicv_inhibit_reasons(ulong bit)
+bool avic_check_apicv_inhibit_reasons(enum kvm_apicv_inhibit reason)
 {
 	ulong supported = BIT(APICV_INHIBIT_REASON_DISABLE) |
 			  BIT(APICV_INHIBIT_REASON_ABSENT) |
@@ -864,7 +864,7 @@ bool avic_check_apicv_inhibit_reasons(ulong bit)
 			  BIT(APICV_INHIBIT_REASON_X2APIC) |
 			  BIT(APICV_INHIBIT_REASON_BLOCKIRQ);
 
-	return supported & BIT(bit);
+	return supported & BIT(reason);
 }
 
 
