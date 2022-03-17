b76cfbfb3043cd57215a538d1713bae1ae548f01
Pablo Neira Ayuso <pablo@netfilter.org>
2022-03-17 12:04:42 +0100

    netfilter: nf_tables: initialize registers in nft_do_chain()
    commit 4c905f6740a365464e91467aa50916555b28213d upstream.
    
    Initialize registers to avoid stack leak into userspace.
    
    Fixes: 96518518cc41 ("netfilter: add nftables")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit 4d28522acd1c4415c85f6b33463713a268f68965)
    Orabug: 34012926
    CVE: CVE-2022-1016
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Reviewed-by: John Donnelly  <john.p.donnelly@oracle.com>
    Signed-off-by: aloktiw <alok.a.tiwari@oracle.com>

--- net/netfilter/nf_tables_core.c
+++ net/netfilter/nf_tables_core.c
@@ -118,7 +118,7 @@ nft_do_chain(struct nft_pktinfo *pkt, const struct nf_hook_ops *ops)
 	const struct net *net = dev_net(pkt->in ? pkt->in : pkt->out);
 	const struct nft_rule *rule;
 	const struct nft_expr *expr, *last;
-	struct nft_regs regs;
+	struct nft_regs regs = {};
 	unsigned int stackptr = 0;
 	struct nft_jumpstack jumpstack[NFT_JUMP_STACK_SIZE];
 	struct nft_stats *stats;
