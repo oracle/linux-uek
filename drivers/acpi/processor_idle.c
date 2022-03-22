748d4ebad50c220e3ca0bf9a2865b3eadb7f41d2
Stephane Eranian <eranian@google.com>
2022-03-22 15:15:12 -0700

    ACPI: Add perf low power callback
    Add an optional callback needed by some PMU features, e.g., AMD
    BRS, to give a chance to the perf_events code to change its state before
    a CPU goes to low power and after it comes back.
    
    The callback is void when the PERF_NEEDS_LOPWR_CB flag is not set.
    This flag must be set in arch specific perf_event.h header whenever needed.
    When not set, there is no impact on the ACPI code.
    
    Signed-off-by: Stephane Eranian <eranian@google.com>
    [peterz: build fix]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20220322221517.2510440-9-eranian@google.com
    (cherry picked from commit 2a606a18cd672a16343d146a126721b34cc6adbd)
    
    Conflicts:
    	drivers/acpi/processor_idle.c
    	include/linux/perf_event.h
    
    Orabug: 34530245
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>

--- drivers/acpi/processor_idle.c
+++ drivers/acpi/processor_idle.c
@@ -20,6 +20,7 @@
 #include <linux/tick.h>
 #include <linux/cpuidle.h>
 #include <linux/cpu.h>
+#include <linux/perf_event.h>
 #include <acpi/processor.h>
 
 /*
@@ -546,6 +547,8 @@ static void wait_for_freeze(void)
  */
 static void __cpuidle acpi_idle_do_entry(struct acpi_processor_cx *cx)
 {
+	perf_lopwr_cb(true);
+
 	if (cx->entry_method == ACPI_CSTATE_FFH) {
 		/* Call into architectural FFH based C-state */
 		acpi_processor_ffh_cstate_enter(cx);
@@ -556,6 +559,8 @@ static void __cpuidle acpi_idle_do_entry(struct acpi_processor_cx *cx)
 		inb(cx->address);
 		wait_for_freeze();
 	}
+
+	perf_lopwr_cb(false);
 }
 
 /**
