86fcdfd1277c6c09a590a87430e82f41249799c2
Rohit Nair <rohit.sajan.kumar@oracle.com>
2022-04-05 12:31:01 -0700

    Revert "rds/ib: recover rds connection from stuck rx path"
    A regression was found on a earlier patch. We aim to revert the original
    change that introduced the regression with this patch.
    
    This reverts commit a1125e4ecbbf9fc48bf392e0e979b1ee69462eb1.
    
    Orabug: 34039269
    Signed-off-by: Rohit Nair <rohit.sajan.kumar@oracle.com>
    Reviewed-by: Manjunath Patil <manjunath.b.patil@oracle.com>

--- net/rds/ib.h
+++ net/rds/ib.h
@@ -670,7 +670,6 @@ void rds_ib_conn_ha_changed(struct rds_connection *conn,
 			    const unsigned char *ha,
 			    unsigned ha_len);
 void rds_ib_init_frag(unsigned int version);
-void rds_ib_rx_schedule_work(struct rds_ib_connection *ic);
 void rds_ib_conn_destroy_init(struct rds_connection *conn);
 void rds_ib_destroy_fastreg(struct rds_ib_device *rds_ibdev);
 int rds_ib_setup_fastreg(struct rds_ib_device *rds_ibdev);
@@ -715,7 +714,6 @@ struct rds_ib_device *rds_rdma_rs_get_device(const struct in6_addr *addr,
 extern struct kmem_cache *rds_ib_incoming_slab;
 extern struct kmem_cache *rds_ib_frag_slab;
 extern atomic_t rds_ib_allocation;
-extern struct delayed_work rds_ib_rx_poll_check_w;
 int rds_ib_recv_init(void);
 void rds_ib_recv_exit(void);
 int rds_ib_recv_path(struct rds_conn_path *cp);
@@ -804,7 +802,6 @@ extern unsigned int rds_ib_srq_max_wr;
 extern unsigned int rds_ib_srq_hwm_refill;
 extern unsigned int rds_ib_srq_lwm_refill;
 extern unsigned int rds_ib_srq_enabled;
-extern unsigned int rds_ib_rx_poll_intrvl;
 
 /* ib_sysctl.c */
 int rds_ib_sysctl_init(void);
@@ -825,23 +822,5 @@ extern u32 rds_frwr_ibmr_qrtn_time;
 extern unsigned rds_ib_sysctl_yield_after_ms;
 extern unsigned rds_ib_sysctl_cm_watchdog_ms;
 extern unsigned int rds_ib_tx_stall_threshold;
-extern unsigned int rds_ib_rx_stall_threshold;
-extern unsigned int rds_ib_stall_check_enable;
-
-/* Schedule RDS Rx poll check on all connection */
-static inline void rds_ib_schedule_rx_poll_check(void)
-{
-	if (!rds_ib_stall_check_enable)
-		return;
-
-	schedule_delayed_work(&rds_ib_rx_poll_check_w,
-			      msecs_to_jiffies(rds_ib_rx_poll_intrvl));
-}
-
-/* Cancel RDS Rx poll check work */
-static inline void rds_ib_cancel_rx_poll_check(void)
-{
-	cancel_delayed_work_sync(&rds_ib_rx_poll_check_w);
-}
 
 #endif
