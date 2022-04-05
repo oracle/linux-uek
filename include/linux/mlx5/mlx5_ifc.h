aea76b179ff0476cc1a959bef78567f9496ad12e
Yishai Hadas <yishaih@nvidia.com>
2022-04-05 16:55:33 +0300

    net/mlx5: Introduce ifc bits for migratable
    Introduce ifc related stuff to enable set VF as migratable.
    
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    
    Orabug: 34778256
    
    There is an effort to submit this commit upstream.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    (cherry picked from commit 396845d11d32d8bd82d5a67e30e9ea4ea1e9811c)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -75,6 +75,7 @@ enum {
 	MLX5_SET_HCA_CAP_OP_MOD_ODP                   = 0x2,
 	MLX5_SET_HCA_CAP_OP_MOD_ATOMIC                = 0x3,
 	MLX5_SET_HCA_CAP_OP_MOD_ROCE                  = 0x4,
+	MLX5_SET_HCA_CAP_OP_MOD_GENERAL_DEVICE2       = 0x20,
 };
 
 enum {
@@ -1802,7 +1803,10 @@ struct mlx5_ifc_cmd_hca_cap_bits {
 };
 
 struct mlx5_ifc_cmd_hca_cap_2_bits {
-	u8	   reserved_at_0[0xa0];
+	u8	   reserved_at_0[0x80];
+
+	u8         migratable[0x1];
+	u8         reserved_at_81[0x1f];
 
 	u8	   max_reformat_insert_size[0x8];
 	u8	   max_reformat_insert_offset[0x8];
