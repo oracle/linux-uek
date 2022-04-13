50f674e293af61c9d6f056508d5a6da80612c0a8
Witold Sadowski <wsadowski@marvell.com>
2022-04-13 09:39:26 -0700

    swup: Sync update structures
    Sync smc_update_descriptor structure to
    new version in ATF.
    
    Signed-off-by: Witold Sadowski <wsadowski@marvell.com>
    Change-Id: Ie507e9e535fb7246de9da61c93a5aa0928e42ee2
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/74829
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    
    Orabug: 34229054
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/soc/marvell/cn10ka-swup/mrvl_swup.h
+++ drivers/soc/marvell/cn10ka-swup/mrvl_swup.h
@@ -208,7 +208,7 @@ struct smc_update_obj_info {
 
 #define UPDATE_MAGIC		0x55504454	/* UPDT */
 /** Current smc_update_descriptor version */
-#define UPDATE_VERSION		0x0001
+#define UPDATE_VERSION		0x0100
 
 #define UPDATE_FLAG_BACKUP	0x0001	/** Set to update secondary location */
 #define UPDATE_FLAG_EMMC	0x0002	/** Set to update eMMC instead of SPI */
@@ -230,13 +230,17 @@ struct smc_update_descriptor {
 	uint64_t	image_size;	/** Size of image (CPIO file) */
 	uint32_t	bus;		/** SPI BUS number */
 	uint32_t	cs;		/** SPI chip select number */
-	uint32_t	async_spi;	/** Async SPI operations */
+	uint32_t	async_spi;      /** Async SPI operations */
 	uint32_t	reserved;	/** Space to add stuff */
 	uint64_t	user_addr;	/** Passed to customer function */
 	uint64_t	user_size;	/** Passed to customer function */
 	uint64_t	user_flags;	/** Passed to customer function */
 	uintptr_t	work_buffer;	/** Used for compressed objects */
 	uint64_t	work_buffer_size;/** Size of work buffer */
+	uintptr_t	output_console;	/** Text output console for update info */
+	uint32_t	output_console_size;/** Console buffer size in bytes */
+	uint32_t	output_console_end;/** Not used yet */
+	uint64_t	reserved2[8];
 	struct smc_update_obj_info object_retinfo[SMC_MAX_OBJECTS];
 };
 
