248cb6c9e3f702a4bd138bcd910510d7c7129300
Wei Yang <richard.weiyang@gmail.com>
2022-04-13 23:07:05 -0700

    mm/memcg: set pos explicitly for reclaim and !reclaim
    During mem_cgroup_iter, there are two ways to get iteration position:
    reclaim vs non-reclaim mode.
    
    Let's do it explicitly for reclaim vs non-reclaim mode.
    
    Link: https://lkml.kernel.org/r/20220330234719.18340-3-richard.weiyang@gmail.com
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    Reviewed-by: Roman Gushchin <roman.gushchin@linux.dev>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1014,9 +1014,6 @@ struct mem_cgroup *mem_cgroup_iter(struct mem_cgroup *root,
 	if (!root)
 		root = root_mem_cgroup;
 
-	if (prev && !reclaim)
-		pos = prev;
-
 	rcu_read_lock();
 
 	if (reclaim) {
@@ -1042,6 +1039,8 @@ struct mem_cgroup *mem_cgroup_iter(struct mem_cgroup *root,
 			 */
 			(void)cmpxchg(&iter->position, pos, NULL);
 		}
+	} else if (prev) {
+		pos = prev;
 	}
 
 	if (pos)
