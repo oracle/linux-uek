cb7fcab624ee094bfd668e80fbeaf286f54b43d9
Jayanthi Annadurai <jannadurai@marvell.com>
2022-04-22 13:57:48 -0700

    soc: marvell: Fix chip revision check
    This change fixes the cn10k revision check.
    
    Change-Id: I3736f7493f376173464466816434181e8c4c78c2
    Signed-off-by: Jayanthi Annadurai <jannadurai@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/75499
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    
    Orabug: 34229054
    
    Signed-off-by: Austin Sehnert <austin.sehnert@oracle.com>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/soc/marvell/octeontx/octeontx_smc.h
+++ include/soc/marvell/octeontx/octeontx_smc.h
@@ -96,7 +96,7 @@ static inline bool is_soc_cn10ka_ax(void)
 		if (rev == NULL)
 			return true;
 
-		if ((rev[1] == 'A') || (rev[1] == 'a'))
+		if ((rev[0] == 'A') || (rev[0] == 'a'))
 			return true;
 	}
 
@@ -116,7 +116,7 @@ static inline bool is_soc_cnf10ka_ax(void)
 		if (rev == NULL)
 			return true;
 
-		if ((rev[1] == 'A') || (rev[1] == 'a'))
+		if ((rev[0] == 'A') || (rev[0] == 'a'))
 			return true;
 	}
 
