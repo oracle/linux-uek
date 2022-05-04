WindRiver-Labs/linux-yocto-5.10

gpio: Change GPIO level interrupt handler to handle_level_irq
commit ce614ec4b82e759b18824d945c1f2f74b070626d from
git@git.assembla.com:cavium/WindRiver.linux.git

The current level interrupt handler configured is masking GPIO
interrupt and never unmasking it.
This is incorrect, as level IRQ will be handled only once.
Fix it with use of handle_level_irq, which will unmask interrupt and
thus allow the next interrupt to be handled.”

Signed-off-by: Witold Sadowski <wsadowski@marvell.com>
Change-Id: Ibff0f36597d158830f0fb3c182a378d98677ed65
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/61297
Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
(cherry picked from commit 6f5c1fd022d9304fa510626341f5502e0e6a73aa)
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/61684
Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

drivers/gpio/gpio-thunderx.c
@@ -552,7 +552,7 @@ static int thunderx_gpio_irq_set_type(struct irq_data *d,
 		irq_set_handler_locked(d, handle_fasteoi_ack_irq);
 		bit_cfg |= GPIO_BIT_CFG_INT_TYPE;
 	} else {
-		irq_set_handler_locked(d, handle_fasteoi_mask_irq);
+		irq_set_handler_locked(d, handle_level_irq);
 	}
 
 	raw_spin_lock_irqsave(&txgpio->lock, flags);

oracle/linux-uek

Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/61299
Orabug: 33889959
Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
Reviewed-by: Henry Willard <henry.willard@oracle.com>
(cherry picked from commit a55bcfce7521bcee7048317b8f9a46dc3b2aa761)
Signed-off-by: Brian Maly <brian.maly@oracle.com>

drivers/gpio/gpio-thunderx.c
@@ -687,7 +687,7 @@ static int thunderx_gpio_irq_set_type(struct irq_data *data,
 		irq_set_handler_locked(data, handle_fasteoi_ack_irq);
 		bit_cfg |= GPIO_BIT_CFG_INT_TYPE;
 	} else {
-		irq_set_handler_locked(data, handle_fasteoi_mask_irq);
+		irq_set_handler_locked(data, handle_level_irq);
 	}
 
 	if (flow_type & (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_LEVEL_LOW)) {