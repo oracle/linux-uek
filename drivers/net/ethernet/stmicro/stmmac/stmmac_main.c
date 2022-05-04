cbbb9d1ee883da40a87e166d14977f7fa6c489ff
Jakub Kicinski <kuba@kernel.org>
2022-05-04 09:37:24 -0700

    net: switch to netif_napi_add_tx()
    Switch net callers to the new API not requiring
    the NAPI_POLL_WEIGHT argument.
    
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Acked-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Acked-by: Alexandra Winter <wintera@linux.ibm.com>
    Link: https://lore.kernel.org/r/20220504163725.550782-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 34988287
    
    (cherry picked from commit 16d083e28f1a4f6deef82be92d6a0f5aa2fe7e08)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/net/ethernet/fungible/funeth/funeth_main.c
    
    "funeth_main.c" doesn't exist in uek7, so skipped this hunk.
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -6858,9 +6858,8 @@ static void stmmac_napi_add(struct net_device *dev)
 				       NAPI_POLL_WEIGHT);
 		}
 		if (queue < priv->plat->tx_queues_to_use) {
-			netif_tx_napi_add(dev, &ch->tx_napi,
-					  stmmac_napi_poll_tx,
-					  NAPI_POLL_WEIGHT);
+			netif_napi_add_tx(dev, &ch->tx_napi,
+					  stmmac_napi_poll_tx);
 		}
 		if (queue < priv->plat->rx_queues_to_use &&
 		    queue < priv->plat->tx_queues_to_use) {
