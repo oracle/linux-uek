482963e307104e1d0eb9f60f6f86c59a9ccc85f3
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2022-05-12 22:40:24 -0400

    bnxt_en: parse and report result field when NVRAM package install fails
    Instead of always returning -ENOPKG, decode the firmware error
    code further when the HWRM_NVM_INSTALL_UPDATE firmware call fails.
    Return a more suitable error code to userspace and log an error
    in dmesg.
    
    This is version 2 of the earlier patch that was reverted:
    
    02acd399533e ("bnxt_en: parse result field when NVRAM package install fails")
    
    In this new version, if the call is made through devlink instead of
    ethtool, we'll also set the error message in extack.
    
    Link: https://lore.kernel.org/netdev/20220307141358.4d52462e@kicinski-fedora-pc1c0hjn.dhcp.thefacebook.com/
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Reviewed-by: Pavan Chebbi <pavan.chebbi@broadcom.com>
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit ab0bed4bf6fae8a42cf3b08b38e1fffb1a79193a)
    Orabug: 35431038
    
    Signed-off-by: Samasth Norway Ananda <samasth.norway.ananda@oracle.com>
    Reviewed-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    
    Conflicts:
    	drivers/net/ethernet/broadcom/bnxt/bnxt_ethtool.h
            Introduced function declaration for bnxt_get_pkginfo()
            resolved by removing that line.

--- drivers/net/ethernet/broadcom/bnxt/bnxt_ethtool.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt_ethtool.h
@@ -52,7 +52,7 @@ u16 bnxt_get_fw_auto_link_speeds(u32);
 int bnxt_hwrm_nvm_get_dev_info(struct bnxt *bp,
 			       struct hwrm_nvm_get_dev_info_output *nvm_dev_info);
 int bnxt_flash_package_from_fw_obj(struct net_device *dev, const struct firmware *fw,
-				   u32 install_type);
+				   u32 install_type, struct netlink_ext_ack *extack);
 void bnxt_ethtool_init(struct bnxt *bp);
 void bnxt_ethtool_free(struct bnxt *bp);
 
