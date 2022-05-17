f9636b3e4dee7ed2b096172fefb18e906c797dbb
Stephen Brennan <stephen.s.brennan@oracle.com>
2022-05-16 17:05:08 -0700

    vmcoreinfo: include kallsyms symbols
    The internal kallsyms tables contain information which could be quite
    useful to a debugging tool in the absence of other debuginfo.  If kallsyms
    is enabled, then a debugging tool could parse it and use it as a fallback
    symbol table.  Combined with BTF data, live & post-mortem debuggers can
    support basic operations without needing a large DWARF debuginfo file
    available.  As many as five symbols are necessary to properly parse
    kallsyms names and addresses.  Add these to the vmcoreinfo note.
    
    CONFIG_KALLSYMS_ABSOLUTE_PERCPU does impact the computation of symbol
    addresses.  However, a debugger can infer this configuration value by
    comparing the address of _stext in the vmcoreinfo with the address
    computed via kallsyms.  So there's no need to include information about
    this config value in the vmcoreinfo note.
    
    To verify that we're still well below the maximum of 4096 bytes, I created
    a script[1] to compute a rough upper bound on the possible size of
    vmcoreinfo.  On v5.18-rc7, the script reports 3106 bytes, and with this
    patch, the maximum become 3370 bytes.
    
    [1]: https://github.com/brenns10/kernel_stuff/blob/master/vmcoreinfosize/
    
    Link: https://lkml.kernel.org/r/20220517000508.777145-3-stephen.s.brennan@oracle.com
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Acked-by: Baoquan He <bhe@redhat.com>
    Cc: Bixuan Cui <cuibixuan@huawei.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: David Vernet <void@manifault.com>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Stephen Boyd <swboyd@chromium.org>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 5fd8fea935a1091083506d0b982fcc5d35062f06)
    
    Orabug: 34475880
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Reviewed-by: John Donnelly <john.p.donnelly@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- kernel/crash_core.c
+++ kernel/crash_core.c
@@ -14,6 +14,8 @@
 #include <asm/page.h>
 #include <asm/sections.h>
 
+#include "kallsyms_internal.h"
+
 /* vmcoreinfo stuff */
 static unsigned char *vmcoreinfo_data;
 static size_t vmcoreinfo_size;
@@ -519,6 +521,18 @@ static int __init crash_save_vmcoreinfo_init(void)
 #ifdef CONFIG_HUGETLB_PAGE
 	VMCOREINFO_NUMBER(HUGETLB_PAGE_DTOR);
 #endif
+
+#ifdef CONFIG_KALLSYMS
+	VMCOREINFO_SYMBOL(kallsyms_names);
+	VMCOREINFO_SYMBOL(kallsyms_token_table);
+	VMCOREINFO_SYMBOL(kallsyms_token_index);
+#ifdef CONFIG_KALLSYMS_BASE_RELATIVE
+	VMCOREINFO_SYMBOL(kallsyms_offsets);
+	VMCOREINFO_SYMBOL(kallsyms_relative_base);
+#else
+	VMCOREINFO_SYMBOL(kallsyms_addresses);
+#endif /* CONFIG_KALLSYMS_BASE_RELATIVE */
+#endif /* CONFIG_KALLSYMS */
 
 	arch_crash_save_vmcoreinfo();
 	update_vmcoreinfo_note();
