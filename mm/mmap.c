7176a3ea28029422a3915af9b4ad0691dd539550
Andrew Morton <akpm@linux-foundation.org>
2022-05-18 19:37:00 -0700

    mm-mmap-use-advanced-maple-tree-api-for-mmap_region-fix-checkpatch-fixes
    reflow comments, fix comment leadin
    
    Cc: "Liam R. Howlett" <Liam.Howlett@Oracle.com>
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1879,16 +1879,20 @@ cannot_expand:
 		addr = vma->vm_start;
 		mas_reset(&mas);
 
-		/* If vm_flags changed after call_mmap(), we should try merge vma again
-		 * as we may succeed this time.
+		/*
+		 * If vm_flags changed after call_mmap(), we should try merge
+		 * vma again as we may succeed this time.
 		 */
 		if (unlikely(vm_flags != vma->vm_flags && prev)) {
 			merge = vma_merge(mm, prev, vma->vm_start, vma->vm_end, vma->vm_flags,
 				NULL, vma->vm_file, vma->vm_pgoff, NULL, NULL_VM_UFFD_CTX, NULL);
 			if (merge) {
-				/* ->mmap() can change vma->vm_file and fput the original file. So
-				 * fput the vma->vm_file here or we would add an extra fput for file
-				 * and cause general protection fault ultimately.
+				/*
+				 * ->mmap() can change vma->vm_file and fput
+				 * the original file. So fput the vma->vm_file
+				 * here or we would add an extra fput for file
+				 * and cause general protection fault
+				 * ultimately.
 				 */
 				fput(vma->vm_file);
 				vm_area_free(vma);
