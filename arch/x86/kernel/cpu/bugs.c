b850d1ca8022a91c7fcd4832de904b2522bda8eb
Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
2022-05-19 20:28:10 -0700

    x86/speculation: Add a common function for MD_CLEAR mitigation update
    Processor MMIO Stale Data mitigation uses similar mitigation as MDS and
    TAA. In preparation for adding its mitigation, add a common function to
    update all mitigations that depend on MD_CLEAR.
    
      [ bp: Add a newline in md_clear_update_mitigation() to separate
        statements better. ]
    
    Signed-off-by: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    (Cherry picked from commit f52ea6c26953fed339aa4eae717ee5c2133c7ff2)
    
    Orabug: 34202259
    CVE: CVE-2022-21123
    CVE: CVE-2022-21127
    CVE: CVE-2022-21125
    CVE: CVE-2022-21166
    	Conflicts:
    		- arch/x86/kernel/cpu/bugs.c
    
    Signed-off-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>
    Reviewed-by: Patrick Colp <patrick.colp@oracle.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -146,7 +146,7 @@ static enum ssb_mitigation ssb_select_mitigation(void);
 static void ssb_init(void);
 static void l1tf_select_mitigation(void);
 static void mds_select_mitigation(void);
-static void mds_print_mitigation(void);
+static void md_clear_update_mitigation(void);
 static void taa_select_mitigation(void);
 static void srbds_select_mitigation(void);
 
@@ -300,10 +300,10 @@ void __ref check_bugs(void)
 		return;
 
 	/*
-	 * As MDS and TAA mitigations are inter-related, print MDS
-	 * mitigation until after TAA mitigation selection is done.
+	 * As MDS and TAA mitigations are inter-related, update and print their
+	 * mitigation after TAA mitigation selection is done.
 	 */
-	mds_print_mitigation();
+	md_clear_update_mitigation();
 
 	arch_smt_update();
 
@@ -554,14 +554,6 @@ static void mds_select_mitigation(void)
 		update_mds_branch_idle();
 }
 
-static void mds_print_mitigation(void)
-{
-	if (!boot_cpu_has_bug(X86_BUG_MDS) || cpu_mitigations_off())
-		return;
-
-	pr_info("%s\n", mds_strings[mds_mitigation]);
-}
-
 static int __init mds_cmdline(char *str)
 {
 	if (!boot_cpu_has_bug(X86_BUG_MDS))
@@ -612,7 +604,7 @@ static void taa_select_mitigation(void)
 	/* TSX previously disabled by tsx=off */
 	if (!boot_cpu_has(X86_FEATURE_RTM)) {
 		taa_mitigation = TAA_MITIGATION_TSX_DISABLED;
-		goto out;
+		return;
 	}
 
 	if (cpu_mitigations_off()) {
@@ -626,7 +618,7 @@ static void taa_select_mitigation(void)
 	 */
 	if (taa_mitigation == TAA_MITIGATION_OFF &&
 	    mds_mitigation == MDS_MITIGATION_OFF)
-		goto out;
+		return;
 
 	ia32_cap = x86_read_arch_cap_msr();
 
@@ -642,7 +634,7 @@ static void taa_select_mitigation(void)
 				break;
 			case MDS_MITIGATION_OFF:
 				taa_mitigation = TAA_MITIGATION_OFF;
-				goto out;
+				return;
 			};
 		}
 	} else
@@ -675,18 +667,6 @@ static void taa_select_mitigation(void)
 
 	if (taa_nosmt || cpu_mitigations_auto_nosmt())
 		cpu_smt_disable(false);
-
-	/*
-	 * Update MDS mitigation, if necessary, as the mds_user_clear is
-	 * now enabled for TAA mitigation.
-	 */
-	if (mds_mitigation == MDS_MITIGATION_OFF &&
-	    boot_cpu_has_bug(X86_BUG_MDS)) {
-		mds_mitigation = MDS_MITIGATION_FULL;
-		mds_select_mitigation();
-	}
-out:
-	pr_info("%s\n", taa_strings[taa_mitigation]);
 }
 
 static int __init tsx_async_abort_parse_cmdline(char *str)
@@ -711,6 +691,33 @@ static int __init tsx_async_abort_parse_cmdline(char *str)
 	return 0;
 }
 early_param("tsx_async_abort", tsx_async_abort_parse_cmdline);
+
+#undef pr_fmt
+#define pr_fmt(fmt)     "" fmt
+
+static void __init md_clear_update_mitigation(void)
+{
+	if (cpu_mitigations_off())
+		return;
+
+	if (!static_key_enabled(&mds_user_clear))
+		goto out;
+
+	/*
+	 * mds_user_clear is now enabled. Update MDS mitigation, if
+	 * necessary.
+	 */
+	if (mds_mitigation == MDS_MITIGATION_OFF &&
+	    boot_cpu_has_bug(X86_BUG_MDS)) {
+		mds_mitigation = MDS_MITIGATION_FULL;
+		mds_select_mitigation();
+	}
+out:
+	if (boot_cpu_has_bug(X86_BUG_MDS))
+		pr_info("MDS: %s\n", mds_strings[mds_mitigation]);
+	if (boot_cpu_has_bug(X86_BUG_TAA))
+		pr_info("TAA: %s\n", taa_strings[taa_mitigation]);
+}
 
 #undef pr_fmt
 #define pr_fmt(fmt)	"SRBDS: " fmt
