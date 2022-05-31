95210442eac5e13939287a8ac6ae10b907c44cd4
Sarthak Kukreti <sarthakkukreti@google.com>
2022-05-31 15:56:40 -0400

    dm verity: set DM_TARGET_IMMUTABLE feature flag
    commit 4caae58406f8ceb741603eee460d79bacca9b1b5 upstream.
    
    The device-mapper framework provides a mechanism to mark targets as
    immutable (and hence fail table reloads that try to change the target
    type). Add the DM_TARGET_IMMUTABLE flag to the dm-verity target's
    feature flags to prevent switching the verity target with a different
    target type.
    
    Fixes: a4ffc152198e ("dm: add verity target")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sarthak Kukreti <sarthakkukreti@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 27798cca4e54fe9c390396c4cc655480f827bbd5)
    
    Orabug: 34555434
    CVE: CVE-2022-2503
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    
    Conflicts:
    	drivers/md/dm-verity.c
    Conflict occured as UEK4 has an old version number, conflict resolved by
    keeping the version as it is.
    
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- drivers/md/dm-verity.c
+++ drivers/md/dm-verity.c
@@ -988,6 +988,7 @@ bad:
 
 static struct target_type verity_target = {
 	.name		= "verity",
+	.features	= DM_TARGET_IMMUTABLE,
 	.version	= {1, 2, 0},
 	.module		= THIS_MODULE,
 	.ctr		= verity_ctr,
