312ce6cd587d1e4c78d6333640e980aa96d974f3
Saravana Kannan <saravanak@google.com>
2022-06-01 20:56:52 -0700

    module: Fix prefix for module.sig_enforce module param
    Commit cfc1d277891e ("module: Move all into module/") changed the prefix
    of the module param by moving/renaming files.  A later commit also moves
    the module_param() into a different file, thereby changing the prefix
    yet again.
    
    This would break kernel cmdline compatibility and also userspace
    compatibility at /sys/module/module/parameters/sig_enforce.
    
    So, set the prefix back to "module.".
    
    Fixes: cfc1d277891e ("module: Move all into module/")
    Link: https://lore.kernel.org/lkml/20220602034111.4163292-1-saravanak@google.com/
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Aaron Tomlin <atomlin@redhat.com>
    Acked-by: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 73503963b715a64a44aa2b1c486114b917a17c73)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Orabug: 36651773
    
    Signed-off-by: Khalid Aziz <khalid.aziz@oracle.com>
    
    Conflicts: Add the fix to kernel/module/main.c instead of signing.c as
    we don't have commit: 0c1e42805c25 ("module: Move extra signature
    support out of core code") in UEK7U2 code.
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Khalid Aziz <khalid.aziz@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -268,6 +268,9 @@ static void module_assert_mutex_or_preempt(void)
 }
 
 #ifdef CONFIG_MODULE_SIG
+#undef MODULE_PARAM_PREFIX
+#define MODULE_PARAM_PREFIX "module."
+
 static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
 module_param(sig_enforce, bool_enable_only, 0644);
 
