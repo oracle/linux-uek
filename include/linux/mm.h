58f35f5fb8cdbe412d04c5430349ca80893a0a37
Naoya Horiguchi <naoya.horiguchi@nec.com>
2022-06-02 14:06:28 +0900

    mm,hwpoison: set PG_hwpoison for busy hugetlb pages
    If memory_failure() fails to grab page refcount on a hugetlb page because
    it's busy, it returns without setting PG_hwpoison on it.  This not only
    loses a chance of error containment, but breaks the rule that
    action_result() should be called only when memory_failure() do any of
    handling work (even if that's just setting PG_hwpoison).  This
    inconsistency could harm code maintainability.
    
    So set PG_hwpoison and call hugetlb_set_page_hwpoison() for such a case.
    
    Link: https://lkml.kernel.org/r/20220602050631.771414-3-naoya.horiguchi@linux.dev
    Fixes: 405ce051236c ("mm/hwpoison: fix race between hugetlb free/demotion and memory_failure_hugetlb()")
    Signed-off-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Liu Shixin <liushixin2@huawei.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Yang Shi <shy828301@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3250,6 +3250,7 @@ enum mf_flags {
 	MF_SOFT_OFFLINE = 1 << 3,
 	MF_UNPOISON = 1 << 4,
 	MF_SW_SIMULATED = 1 << 5,
+	MF_NO_RETRY = 1 << 6,
 };
 extern int memory_failure(unsigned long pfn, int flags);
 extern void memory_failure_queue(unsigned long pfn, int flags);
