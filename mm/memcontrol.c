c70baaa055c0ef3de47d9ae58502aa543136e4be
Muchun Song <songmuchun@bytedance.com>
2022-06-21 20:56:50 +0800

    mm: memcontrol: prepare objcg API for non-kmem usage
    Pagecache pages are charged at the allocation time and holding a
    reference to the original memory cgroup until being reclaimed.
    Depending on the memory pressure, specific patterns of the page
    sharing between different cgroups and the cgroup creation and
    destruction rates, a large number of dying memory cgroups can be
    pinned by pagecache pages. It makes the page reclaim less efficient
    and wastes memory.
    
    We can convert LRU pages and most other raw memcg pins to the objcg
    direction to fix this problem, and then the page->memcg will always
    point to an object cgroup pointer.
    
    Therefore, the infrastructure of objcg no longer only serves
    CONFIG_MEMCG_KMEM. In this patch, we move the infrastructure of the
    objcg out of the scope of the CONFIG_MEMCG_KMEM so that the LRU pages
    can reuse it to charge pages.
    
    We know that the LRU pages are not accounted at the root level. But
    the page->memcg_data points to the root_mem_cgroup. So the
    page->memcg_data of the LRU pages always points to a valid pointer.
    But the root_mem_cgroup dose not have an object cgroup. If we use
    obj_cgroup APIs to charge the LRU pages, we should set the
    page->memcg_data to a root object cgroup. So we also allocate an
    object cgroup for the root_mem_cgroup.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: Michal Koutný <mkoutny@suse.com>
    Acked-by: Roman Gushchin <roman.gushchin@linux.dev>
    
    Link: https://lore.kernel.org/all/20220621125658.64935-4-songmuchun@bytedance.com/
    
    Orabug: 37405594
    
     Conflicts:
    	mm/memcontrol.c
    	(Due to presence of following commits in UEK-8:
    	  i. 'commit e4dde56cd208 mm: multi-gen LRU: per-node lru_gen_folio
    	       lists'
    	  ii. 'commit 9c94bef9c912 mm/memcontrol.c: replace cgroup_memory_nokmem
    		with mem_cgroup_kmem_disabled()'
    	  iii. 'commit 675d6c9b59e3 mm: kmem: make memcg keep a reference to the original objcg'
    	include/linux/memcontrol.h
    	(Due to presence of 'commit 3a3b7fec3974 mm: remove CONFIG_MEMCG_KMEM' - which removed the
    	 #ifdef from struct mem_cgroup, in UEK-8)
    
    Signed-off-by: Imran Khan <imran.f.khan@oracle.com>
    Reviewed-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -128,12 +128,10 @@ bool mem_cgroup_kmem_disabled(void)
 static void obj_cgroup_uncharge_pages(struct obj_cgroup *objcg,
 				      unsigned int nr_pages);
 
-static void obj_cgroup_release(struct percpu_ref *ref)
+static void obj_cgroup_release_bytes(struct obj_cgroup *objcg)
 {
-	struct obj_cgroup *objcg = container_of(ref, struct obj_cgroup, refcnt);
 	unsigned int nr_bytes;
 	unsigned int nr_pages;
-	unsigned long flags;
 
 	/*
 	 * At this point all allocated objects are freed, and
@@ -147,9 +145,9 @@ static void obj_cgroup_release(struct percpu_ref *ref)
 	 * 3) CPU1: a process from another memcg is allocating something,
 	 *          the stock if flushed,
 	 *          objcg->nr_charged_bytes = PAGE_SIZE - 92
-	 * 5) CPU0: we do release this object,
+	 * 4) CPU0: we do release this object,
 	 *          92 bytes are added to stock->nr_bytes
-	 * 6) CPU0: stock is flushed,
+	 * 5) CPU0: stock is flushed,
 	 *          92 bytes are added to objcg->nr_charged_bytes
 	 *
 	 * In the result, nr_charged_bytes == PAGE_SIZE.
@@ -161,6 +159,14 @@ static void obj_cgroup_release(struct percpu_ref *ref)
 
 	if (nr_pages)
 		obj_cgroup_uncharge_pages(objcg, nr_pages);
+}
+
+static void obj_cgroup_release(struct percpu_ref *ref)
+{
+	struct obj_cgroup *objcg = container_of(ref, struct obj_cgroup, refcnt);
+	unsigned long flags;
+
+	obj_cgroup_release_bytes(objcg);
 
 	spin_lock_irqsave(&objcg_lock, flags);
 	list_del(&objcg->list);
@@ -189,10 +195,10 @@ static struct obj_cgroup *obj_cgroup_alloc(void)
 	return objcg;
 }
 
-static void memcg_reparent_objcgs(struct mem_cgroup *memcg,
-				  struct mem_cgroup *parent)
+static void memcg_reparent_objcgs(struct mem_cgroup *memcg)
 {
 	struct obj_cgroup *objcg, *iter;
+	struct mem_cgroup *parent = parent_mem_cgroup(memcg);
 
 	objcg = rcu_replace_pointer(memcg->objcg, NULL, true);
 
@@ -3081,22 +3087,11 @@ unsigned long mem_cgroup_usage(struct mem_cgroup *memcg, bool swap)
 
 static int memcg_online_kmem(struct mem_cgroup *memcg)
 {
-	struct obj_cgroup *objcg;
-
 	if (mem_cgroup_kmem_disabled())
 		return 0;
 
 	if (unlikely(mem_cgroup_is_root(memcg)))
 		return 0;
-
-	objcg = obj_cgroup_alloc();
-	if (!objcg)
-		return -ENOMEM;
-
-	objcg->memcg = memcg;
-	rcu_assign_pointer(memcg->objcg, objcg);
-	obj_cgroup_get(objcg);
-	memcg->orig_objcg = objcg;
 
 	static_branch_enable(&memcg_kmem_online_key);
 
@@ -3107,17 +3102,13 @@ static int memcg_online_kmem(struct mem_cgroup *memcg)
 
 static void memcg_offline_kmem(struct mem_cgroup *memcg)
 {
-	struct mem_cgroup *parent;
-
 	if (mem_cgroup_kmem_disabled())
 		return;
 
 	if (unlikely(mem_cgroup_is_root(memcg)))
 		return;
 
-	parent = parent_mem_cgroup(memcg);
-	memcg_reparent_objcgs(memcg, parent);
-	memcg_reparent_list_lrus(memcg, parent);
+	memcg_reparent_list_lrus(memcg, parent_mem_cgroup(memcg));
 }
 
 #ifdef CONFIG_CGROUP_WRITEBACK
@@ -3614,6 +3605,7 @@ mem_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 static int mem_cgroup_css_online(struct cgroup_subsys_state *css)
 {
 	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
+	struct obj_cgroup *objcg;
 
 	if (memcg_online_kmem(memcg))
 		goto remove_id;
@@ -3630,6 +3622,15 @@ static int mem_cgroup_css_online(struct cgroup_subsys_state *css)
 		queue_delayed_work(system_unbound_wq, &stats_flush_dwork,
 				   FLUSH_TIME);
 	lru_gen_online_memcg(memcg);
+
+	objcg = obj_cgroup_alloc();
+	if (!objcg)
+		goto free_shrinker;
+
+	objcg->memcg = memcg;
+	rcu_assign_pointer(memcg->objcg, objcg);
+	obj_cgroup_get(objcg);
+	memcg->orig_objcg = objcg;
 
 	/* Online state pins memcg ID, memcg ID pins CSS */
 	refcount_set(&memcg->id.ref, 1);
@@ -3648,6 +3649,8 @@ static int mem_cgroup_css_online(struct cgroup_subsys_state *css)
 	xa_store(&mem_cgroup_ids, memcg->id.id, memcg, GFP_KERNEL);
 
 	return 0;
+free_shrinker:
+	free_shrinker_info(memcg);
 offline_kmem:
 	memcg_offline_kmem(memcg);
 remove_id:
@@ -3666,6 +3669,7 @@ static void mem_cgroup_css_offline(struct cgroup_subsys_state *css)
 
 	zswap_memcg_offline_cleanup(memcg);
 
+	memcg_reparent_objcgs(memcg);
 	memcg_offline_kmem(memcg);
 	reparent_shrinker_deferred(memcg);
 	wb_memcg_offline(memcg);
