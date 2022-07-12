0day-ci/linux

mm/gup: add a range variant of unpin_user_pages_dirty_lock()
Add an unpin_user_page_range_dirty_lock() API which takes a starting page
and how many consecutive pages we want to unpin and optionally dirty.

To that end, define another iterator for_each_compound_range() that
operates in page ranges as opposed to page array.

For users (like RDMA mr_dereg) where each sg represents a contiguous set
of pages, we're able to more efficiently unpin pages without having to
supply an array of pages much of what happens today with
unpin_user_pages().

Link: https://lkml.kernel.org/r/20210212130843.13865-4-joao.m.martins@oracle.com
Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
Reviewed-by: John Hubbard <jhubbard@nvidia.com>
Cc: Christoph Hellwig <hch@infradead.org>
Cc: Doug Ledford <dledford@redhat.com>
Cc: Matthew Wilcox <willy@infradead.org>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

include/linux/mm.h
@@ -1265,6 +1265,8 @@ static inline void put_page(struct page *page)
 void unpin_user_page(struct page *page);
 void unpin_user_pages_dirty_lock(struct page **pages, unsigned long npages,
 				 bool make_dirty);
+void unpin_user_page_range_dirty_lock(struct page *page, unsigned long npages,
+				      bool make_dirty);
 void unpin_user_pages(struct page **pages, unsigned long npages);
 
 /**

oracle/linux-uek

Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
(cherry picked from commit 458a4f788f8602e5701b3d8c2fb6b021310a7301)
Conflicts:
	include/linux/mm.h
	mm/gup.c
Contextual changes due to the lack of unpin_user_page*() family
of functions, particularly commit
eddb1c228f79("mm/gup: introduce pin_user_pages*() and FOLL_PIN") and
f1f6a7dd9b53("mm, tree-wide: rename put_user_page*() to unpin_user_page*()").
Additionally, partially backport put_compound_head() from commit
commit 3faa52c03f44 ("mm/gup: track FOLL_PIN pages"). Note that we
do not include the FOLL_PIN portion of put_compound_head() given its
non-existence in UEK6.
Orabug: 34314763
Tested-by: Ria Mirchandani <ria.mirchandani@oracle.com>
Reviewed-by: Jane Chu <jane.chu@oracle.com>
(cherry picked from commit 94030946368ad0174305dc311c019e0f63f6deab)
Signed-off-by: Brian Maly <brian.maly@oracle.com>

include/linux/mm.h
@@ -1102,6 +1102,9 @@ static inline void put_user_page(struct page *page)
 void put_user_pages_dirty_lock(struct page **pages, unsigned long npages,
 			       bool make_dirty);
 
+void put_user_page_range_dirty_lock(struct page *page, unsigned long npages,
+				    bool make_dirty);
+
 void put_user_pages(struct page **pages, unsigned long npages);
 
 #if defined(CONFIG_SPARSEMEM) && !defined(CONFIG_SPARSEMEM_VMEMMAP)