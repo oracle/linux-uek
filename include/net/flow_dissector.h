111a70703e7a1e2ea385bdf6271b83ddac6cb76b
Wojciech Drewek <wojciech.drewek@intel.com>
2022-07-18 14:18:10 +0200

    flow_dissector: Add PPPoE dissectors
    Allow to dissect PPPoE specific fields which are:
    - session ID (16 bits)
    - ppp protocol (16 bits)
    - type (16 bits) - this is PPPoE ethertype, for now only
      ETH_P_PPP_SES is supported, possible ETH_P_PPP_DISC
      in the future
    
    The goal is to make the following TC command possible:
    
      # tc filter add dev ens6f0 ingress prio 1 protocol ppp_ses \
          flower \
            pppoe_sid 12 \
            ppp_proto ip \
          action drop
    
    Note that only PPPoE Session is supported.
    
    Signed-off-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Acked-by: Guillaume Nault <gnault@redhat.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    (cherry picked from commit 46126db9c86110e5fc1e369b9bb89735ddefdae4)
    Conflicts:
    	include/net/flow_dissector.h
    	net/core/flow_dissector.c
    Orabug: 35365171
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>

--- include/net/flow_dissector.h
+++ include/net/flow_dissector.h
@@ -269,6 +269,26 @@ struct flow_dissector_key_hash {
 	u32 hash;
 };
 
+/**
+ * struct flow_dissector_key_num_of_vlans:
+ * @num_of_vlans: num_of_vlans value
+ */
+struct flow_dissector_key_num_of_vlans {
+	u8 num_of_vlans;
+};
+
+/**
+ * struct flow_dissector_key_pppoe:
+ * @session_id: pppoe session id
+ * @ppp_proto: ppp protocol
+ * @type: pppoe eth type
+ */
+struct flow_dissector_key_pppoe {
+	__be16 session_id;
+	__be16 ppp_proto;
+	__be16 type;
+};
+
 enum flow_dissector_key_id {
 	FLOW_DISSECTOR_KEY_CONTROL, /* struct flow_dissector_key_control */
 	FLOW_DISSECTOR_KEY_BASIC, /* struct flow_dissector_key_basic */
@@ -298,6 +318,8 @@ enum flow_dissector_key_id {
 	FLOW_DISSECTOR_KEY_META, /* struct flow_dissector_key_meta */
 	FLOW_DISSECTOR_KEY_CT, /* struct flow_dissector_key_ct */
 	FLOW_DISSECTOR_KEY_HASH, /* struct flow_dissector_key_hash */
+	FLOW_DISSECTOR_KEY_NUM_OF_VLANS, /* struct flow_dissector_key_num_of_vlans */
+	FLOW_DISSECTOR_KEY_PPPOE, /* struct flow_dissector_key_pppoe */
 
 	FLOW_DISSECTOR_KEY_MAX,
 };
