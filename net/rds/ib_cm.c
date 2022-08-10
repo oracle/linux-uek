oracle/linux-uek

commit 654ee1b3bf4ce482f41db80990a9589500ef0221

net/rds: Extract dest qp num for displaying in rds-info
This patch extracts the source and destination qp number during the
completion of an rds connection establishment and are saved in newly
defined variables. These values are being passed to the user-space rds-info
application when needed, for display in its output.

Orabug: 31880139

Suggested-by: Rama Nichanamatlu <rama.nichanamatlu@oracle.com>
Signed-off-by: Praveen Kumar Kannoju <praveen.kannoju@oracle.com>
Reviewed-by: Alan Maguire <alan.maguire@oracle.com>
Signed-off-by: Somasundaram Krishnasamy <somasundaram.krishnasamy@oracle.com>

net/rds/ib_cm.c
@@ -375,6 +375,10 @@ void rds_ib_cm_connect_complete(struct rds_connection *conn, struct rdma_cm_even
 					    NULL);
 	}
 
+	/* Save the qp number information in the connection details. */
+	ic->i_qp_num = ic->i_cm_id->qp->qp_num;
+	ic->i_dst_qp_num = event->param.conn.qp_num;
+
 	rds_connect_complete(conn);
 }
 

include/uapi/linux/rds.h
@@ -291,9 +291,10 @@ struct rds_info_rdma_connection {
 	uint32_t	frag;
 	uint16_t        flow_ctl_post_credit;
 	uint16_t        flow_ctl_send_credit;
-	uint32_t        qp_num;
+	int32_t		qp_num;
 	uint32_t        w_alloc_ctr;
 	uint32_t        w_free_ctr;
+	int32_t		dst_qp_num;
 
 };
 
@@ -314,9 +315,10 @@ struct rds6_info_rdma_connection {
 	uint32_t	frag;
 	uint16_t        flow_ctl_post_credit;
 	uint16_t        flow_ctl_send_credit;
-	uint32_t	qp_num;
+	int32_t		qp_num;
 	uint32_t	w_alloc_ctr;
 	uint32_t	w_free_ctr;
+	int32_t		dst_qp_num;
 };
 
 /*

net/rds/ib.c
@@ -769,6 +769,9 @@ static int rds_ib_conn_info_visitor(struct rds_connection *conn,
 	memset(&iinfo->src_gid, 0, sizeof(iinfo->src_gid));
 	memset(&iinfo->dst_gid, 0, sizeof(iinfo->dst_gid));
 
+	iinfo->qp_num = -1;
+	iinfo->dst_qp_num = -1;
+
 	if (ic) {
 		iinfo->tos = conn->c_tos;
 		iinfo->sl = ic->i_sl;
@@ -785,7 +788,8 @@ static int rds_ib_conn_info_visitor(struct rds_connection *conn,
 		iinfo->max_send_wr = ic->i_send_ring.w_nr;
 		iinfo->max_recv_wr = ic->i_recv_ring.w_nr;
 		iinfo->max_send_sge = rds_ibdev->max_sge;
-		iinfo->qp_num = ic->i_cm_id->qp->qp_num;
+		iinfo->qp_num = ic->i_qp_num;
+		iinfo->dst_qp_num = ic->i_dst_qp_num;
 		iinfo->w_alloc_ctr = ic->i_recv_ring.w_alloc_ctr;
 		iinfo->w_free_ctr =
 			(u32)atomic_read(&ic->i_recv_ring.w_free_ctr);
@@ -817,6 +821,9 @@ static int rds6_ib_conn_info_visitor(struct rds_connection *conn,
 	memset(&iinfo6->src_gid, 0, sizeof(iinfo6->src_gid));
 	memset(&iinfo6->dst_gid, 0, sizeof(iinfo6->dst_gid));
 
+	iinfo6->qp_num = -1;
+	iinfo6->dst_qp_num = -1;
+
 	if (ic) {
 		iinfo6->tos = conn->c_tos;
 		iinfo6->sl = ic->i_sl;
@@ -832,7 +839,8 @@ static int rds6_ib_conn_info_visitor(struct rds_connection *conn,
 		iinfo6->max_send_wr = ic->i_send_ring.w_nr;
 		iinfo6->max_recv_wr = ic->i_recv_ring.w_nr;
 		iinfo6->max_send_sge = rds_ibdev->max_sge;
-		iinfo6->qp_num = ic->i_cm_id->qp->qp_num;
+		iinfo6->qp_num = ic->i_qp_num;
+		iinfo6->dst_qp_num = ic->i_dst_qp_num;
 		iinfo6->w_alloc_ctr = ic->i_recv_ring.w_alloc_ctr;
 		iinfo6->w_free_ctr =
 			(u32)atomic_read(&ic->i_recv_ring.w_free_ctr);

net/rds/ib.h
@@ -302,6 +302,10 @@ struct rds_ib_connection {
 	struct delayed_work	i_delayed_free_work;
 	struct list_head	i_delayed_free_work_node;
 	struct rds_ib_device	*i_saved_rds_ibdev;
+
+	/* qp number info */
+	s32			i_qp_num;
+	s32			i_dst_qp_num;
 };
 
 /* This assumes that atomic_t is at least 32 bits */

commit e46868ce8be1d08849e65e0054de2028168b7290

rds/rdma: correctly assign the dest qp num in rds ib connection
Commit 654ee1b3bf4c ("net/rds: Extract dest qp num for displaying in
rds-info") assigns the dest qp num in rds ib connection during
RDMA_CM_EVENT_ESTABLISHED event handling. This works good for active
side of the connection. For passive side, the dest qp num is passed in
the RDMA_CM_EVENT_CONNECT_REQUEST event. This patch adds the change that
sets dest qp num in rds ib connection during
RDMA_CM_EVENT_CONNECT_REQUEST event handling.

Without this patch, the dest qp num on passive side is set to 0. We can
observe this behavior in rds-info -I output.

Fixes: 654ee1b3bf4c ("net/rds: Extract dest qp num for displaying in
rds-info")

v1->v2: Addressed review comments on where to set dest qp num.

Orabug: 34429477
Signed-off-by: Rohit Nair <rohit.sajan.kumar@oracle.com>
Reviewed-by: Arumugam Kolappan <aru.kolappan@oracle.com>
Signed-off-by: Brian Maly <brian.maly@oracle.com>

net/rds/ib_cm.c
@@ -424,7 +424,8 @@ void rds_ib_cm_connect_complete(struct rds_connection *conn, struct rdma_cm_even
 
 	/* Save the qp number information in the connection details. */
 	ic->i_qp_num = ic->i_cm_id->qp->qp_num;
-	ic->i_dst_qp_num = event->param.conn.qp_num;
+	if (event->param.conn.qp_num)
+		ic->i_dst_qp_num = event->param.conn.qp_num;
 
 	rds_ib_cancel_cm_watchdog(ic, "connect complete");
 
@@ -1871,6 +1872,8 @@ int rds_ib_cm_handle_connect(struct rdma_cm_id *cm_id,
 				       event->param.conn.initiator_depth);
 		if (err)
 			reason = "rds_ib_cm_accept failed";
+		else if (event->param.conn.qp_num)
+			ic->i_dst_qp_num = event->param.conn.qp_num;
 	}
 
 out: