b07327d5c8c87064487ef5f2a1cdec7e57511389
Hans de Goede <hdegoede@redhat.com>
2022-08-10 16:23:28 +0200

    wifi: brcmfmac: Use ISO3166 country code and rev 0 as fallback on 43430
    Many devices ship with a nvram ccode value of X2/XT/XU/XV/ALL which are
    all special world-wide compatibility ccode-s. Most of these world-wide
    ccode-s allow passive scan mode only for 2.4GHz channels 12-14,
    only enabling them when an AP is seen on them.
    
    Since linux-firmware has moved to the new cyfmac43430-sdio.bin +
    cyfmac43430-sdio.clm_blob firmware files this no longer works and
    43430 devices using e.g. an X2 ccode fail to connect to an AP on
    channel 13.
    
    Add the 43430 chip-id to the list of chips for which to use the ISO3166
    country code + rev 0 as fallback in brcmf_translate_country_code() to
    fix this.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://lore.kernel.org/r/20220810142328.141030-1-hdegoede@redhat.com
    (cherry picked from commit d173d0207bda8173b27a38cae439a2f904d4f7c8)
    
    Orabug: 35435347
    Signed-off-by: Vijay Kumar <Vijay.ac.kumar@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -7484,6 +7484,7 @@ int brcmf_cfg80211_wait_vif_event(struct brcmf_cfg80211_info *cfg,
 static bool brmcf_use_iso3166_ccode_fallback(struct brcmf_pub *drvr)
 {
 	switch (drvr->bus_if->chip) {
+	case BRCM_CC_43430_CHIP_ID:
 	case BRCM_CC_4345_CHIP_ID:
 		return true;
 	default:
