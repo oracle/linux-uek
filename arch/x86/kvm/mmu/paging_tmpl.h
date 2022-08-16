2b565465ee0a608f44fec41d5c68b90764e77879
Chao Peng <chao.p.peng@linux.intel.com>
2022-08-16 20:53:22 +0800

    KVM: Rename mmu_notifier_* to mmu_invalidate_*
    The motivation of this renaming is to make these variables and related
    helper functions less mmu_notifier bound and can also be used for non
    mmu_notifier based page invalidation. mmu_invalidate_* was chosen to
    better describe the purpose of 'invalidating' a page that those
    variables are used for.
    
      - mmu_notifier_seq/range_start/range_end are renamed to
        mmu_invalidate_seq/range_start/range_end.
    
      - mmu_notifier_retry{_hva} helper functions are renamed to
        mmu_invalidate_retry{_hva}.
    
      - mmu_notifier_count is renamed to mmu_invalidate_in_progress to
        avoid confusion with mn_active_invalidate_count.
    
      - While here, also update kvm_inc/dec_notifier_count() to
        kvm_mmu_invalidate_begin/end() to match the change for
        mmu_notifier_count.
    
    No functional change intended.
    
    Signed-off-by: Chao Peng <chao.p.peng@linux.intel.com>
    Message-Id: <20220816125322.1110439-3-chao.p.peng@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 20ec3ebd707c77fb9b11b37193449193d4649f33)
    
    Orabug: 39832946
    
    Conflicts:
    	arch/arm64/kvm/mmu.c - minor conflicts
    	arch/riscv/kvm/mmu.c - no file
    	virt/kvm/kvm_main.c - minor conflicts
    	virt/kvm/pfncache.c - no file
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- arch/x86/kvm/mmu/paging_tmpl.h
+++ arch/x86/kvm/mmu/paging_tmpl.h
@@ -634,7 +634,7 @@ static void FNAME(pte_prefetch)(struct kvm_vcpu *vcpu, struct guest_walker *gw,
 	 * If addresses are being invalidated, skip prefetching to avoid
 	 * accidentally prefetching those addresses.
 	 */
-	if (unlikely(vcpu->kvm->mmu_notifier_count))
+	if (unlikely(vcpu->kvm->mmu_invalidate_in_progress))
 		return;
 
 	if (sp->role.direct)
@@ -884,7 +884,7 @@ static int FNAME(page_fault)(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	else
 		fault->max_level = walker.level;
 
-	mmu_seq = vcpu->kvm->mmu_notifier_seq;
+	mmu_seq = vcpu->kvm->mmu_invalidate_seq;
 	smp_rmb();
 
 	if (kvm_faultin_pfn(vcpu, fault, &r))
