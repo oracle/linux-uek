67370a9a7a327141042f343c455c9c77dec89b17
Vasyl Gomonovych <vgomonovych@marvell.com>
2022-08-17 07:27:54 -0700

    driver: edac: octeontx: Refactor EDAC driver
    OcteonTX and CN10K EDAC driver currently support only EDAC
    framework for hardware error sources register
    and error report.
    
    Signed-off-by: Vasyl Gomonovych <vgomonovych@marvell.com>
    Change-Id: I21b24d59701b01070ad4df93245cffe0d3f6fc14
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/84137
    Reviewed-by: Jayanthi Annadurai <jannadurai@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    
    Orabug: 34671021
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/edac/octeontx_edac.h
+++ drivers/edac/octeontx_edac.h
@@ -10,25 +10,17 @@
 #ifndef __OCTEONTX_EDAC_H__
 #define __OCTEONTX_EDAC_H__
 
+#define OCTEONTX_GHES_ERR_RING_SIG ((int)'M' << 24 | 'R' << 16 | 'V' << 8 | 'L')
+
 #define CN10K_CPU_MODEL			(0xd49)
-#define PCI_DEVICE_ID_OCTEONTX2_LMC	(0xa022)
-#define PCI_DEVICE_ID_OCTEONTX2_MCC	(0xa070)
-#define PCI_DEVICE_ID_OCTEONTX2_MDC	(0xa073)
-
-#define OCTEONTX_RAS_MDC_SDEI_EVENT	(0x40000000)
-#define OCTEONTX_RAS_MCC_SDEI_EVENT	(0x40000001)
-#define OCTEONTX_RAS_LMC_SDEI_EVENT	(0x40000002)
-#define OCTEONTX_RAS_DSS_SDEI_EVENT	(0x40000001)
-#define OCTEONTX_RAS_TAD_SDEI_EVENT	(0x40000002)
-
-#define OCTEONTX_LMC	"lmc"
-#define OCTEONTX_MCC	"mcc"
-#define OCTEONTX_MDC	"mdc"
-#define OCTEONTX_DSS	"dss"
-#define OCTEONTX_TAD	"tad"
 
 #define OCTEONTX2_EDAC_INJECT		(0xc2000c0b)
-#define OCTEON10_EDAC_INJECT		(0xc2000b10)
+#define CN10K_EDAC_INJECT		(0xc2000b10)
+
+#define OCTEONTX_MDC_EINJ_CAP	(0x10000000)
+#define OCTEONTX_MCC_EINJ_CAP	(0x40000000)
+
+#define CN10KA_DSS_EINJ_CAP	(0x20000000)
 
 #define OCTEONTX_EDAC_F_BITMASK		0x007 /* single bit to corrupt */
 #define OCTEONTX_EDAC_F_MULTI		0x008 /* corrupt multiple bits */
@@ -37,98 +29,59 @@
 #define OCTEONTX_EDAC_F_REREAD		0x100 /* read-back in EL3 */
 #define OCTEONTX_EDAC_F_PHYS		0x200 /* target is EL3-physical, not EL012 */
 
-#define OCTEONTX_GHES_NAME_MAX_LEN 16
-#define OCTEONTX_GHES_FRU_TEXT_LEN 32
-
-#define OCTEONTX_GHES_ERR_RING_SIG ((int)'M' << 24 | 'R' << 16 | 'V' << 8 | 'L')
-
-#define IS_NOT_MC_SDEI_EVENT(id) ((id != OCTEONTX_RAS_MDC_SDEI_EVENT) && \
-	(id != OCTEONTX_RAS_MCC_SDEI_EVENT) && \
-	(id != OCTEONTX_RAS_LMC_SDEI_EVENT))
+#define SIZE	256
+#define EDAC_FW_REC_SIZE	32
+#define NAME_SZ	8
 
 struct processor_error {
 	struct cper_sec_proc_arm desc;
 	struct cper_arm_err_info info;
 };
 
-union octeontx_edac_cper {
+union octeontx_ghes_cper {
 	struct cper_sec_mem_err mem;
-	struct processor_error  core;
+	struct processor_error core;
 };
 
-struct octeontx_edac_mc_record {
-	struct acpi_hest_generic_status estatus;
-	struct acpi_hest_generic_data   gdata;
-	union octeontx_edac_cper        cper;
+struct octeontx_ghes_ring_record {
+	union octeontx_ghes_cper cper;
+	uint32_t severity;
+	char msg[EDAC_FW_REC_SIZE];
 };
 
-/*
- * Describes an error source per ACPI (Generic Hardware Error Source).
- * This produces GHES-compliant error records from data forwarded by the [ATF]
- * firmware.
- * There exists one of these for each error source.
- *
- * @name:               ghes source name
- * @id:                 sdei event id
- * @esa_pa              physical address of Error Status Address register/iomem
- * @esb_pa:             phys address of Error Status Block follow Error Status Data
- * @ring_pa:            physical address of Ring of Error Status Blocks
- * @esa_va:             mapped to Error Status Address point on Error Status Block
- * @esb_va:             mapped to Error Status Block
- * @ring:               mapped to Ring of Error Status Blocks
- * @ring_sz:            ring size
- * @esb_sz:             esb size
- * @dev:                memory controller owner device
- * @mci                 corresponding memory controller
- * @mc_work             worker for sdei callback
- */
-struct octeontx_edac_ghes {
-	phys_addr_t                     esa_pa;
-	phys_addr_t                     esb_pa;
-	phys_addr_t                     ring_pa;
-	phys_addr_t                     *esa_va;
-	struct octeontx_edac_mc_record  *esb_va;
-	struct octeontx_edac_ghes_ring  *ring;
-	size_t                          ring_sz;
-	size_t                          esb_sz;
-	struct device                   dev;
-	struct mem_ctl_info             *mci;
-	struct work_struct              mc_work;
-	u32                             id;
-	char                            name[OCTEONTX_GHES_NAME_MAX_LEN];
-};
-
-struct octeontx_edac_driver {
-	struct device             *dev;
-	struct octeontx_edac_ghes *source_list;
-	size_t                    source_count;
-};
-
-struct octeontx_edac_ghes_ring_record {
-	union {
-		struct cper_sec_mem_err mcc;
-		struct cper_sec_mem_err mdc;
-		struct cper_sec_mem_err lmc;
-		struct cper_sec_mem_err tad;
-		struct cper_sec_mem_err dss;
-	} u;
-	uint32_t error_severity;
-	char fru_text[OCTEONTX_GHES_FRU_TEXT_LEN];
-};
-
-struct octeontx_edac_ghes_ring {
+struct octeontx_ghes_ring {
 	uint32_t head;
 	uint32_t tail;
 	uint32_t size;
 	uint32_t sig;
 	uint32_t reg;
-	struct octeontx_edac_ghes_ring_record records[0] __aligned(8);
+	struct octeontx_ghes_ring_record records[0] __aligned(8);
+};
+
+struct octeontx_ghes {
+	union {
+		struct mem_ctl_info *mci;
+		struct edac_device_ctl_info *edac_dev;
+	};
+	phys_addr_t ring_pa;
+	struct octeontx_ghes_ring __iomem *ring;
+	size_t ring_sz;
+	u32 sdei_num;
+	u32 ecc_cap;
+	struct mutex lock;
+	char name[NAME_SZ];
+};
+
+struct octeontx_ghes_list {
+	struct octeontx_ghes *ghes;
+	size_t count;
 };
 
 struct octeontx_edac_pvt {
 	unsigned long inject;
 	unsigned long error_type;
 	unsigned long address;
+	struct octeontx_ghes *ghes;
 };
 
 #endif // __OCTEONTX_EDAC_H__
