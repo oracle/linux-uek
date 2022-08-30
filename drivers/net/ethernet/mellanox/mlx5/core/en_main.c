1aa84746983faeff09a9599075dc4527d6e5f76e
Maxim Mikityanskiy <maximmi@nvidia.com>
2022-08-30 19:39:49 +0300

    net/mlx5e: Trigger NAPI after activating an SQ
    If an SQ is deactivated and reactivated again, some packets could be
    sent after MLX5E_SQ_STATE_ENABLED is cleared, but before
    netif_tx_stop_queue, meaning that NAPI might miss some completions. In
    order to handle them, make sure to trigger NAPI after SQ activation in
    all cases where it can be relevant. Regular SQs, XDP SQs and XSK SQs are
    good. Missing cases added: after recovery, after activating HTB SQs and
    after activating PTP SQs.
    
    Signed-off-by: Maxim Mikityanskiy <maximmi@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 35290200
    
    (cherry picked from commit 79efecb41f58430454cbb5809fa7b3135150da6d)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 79efecb41f58430454cbb5809fa7b3135150da6d
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Aron Silverton <aron.silverton@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -1446,6 +1446,7 @@ static int mlx5e_alloc_txqsq(struct mlx5e_channel *c,
 	sq->mkey_be   = c->mkey_be;
 	sq->netdev    = c->netdev;
 	sq->mdev      = c->mdev;
+	sq->channel   = c;
 	sq->priv      = c->priv;
 	sq->ch_ix     = c->ix;
 	sq->txq_ix    = txq_ix;
@@ -2458,8 +2459,6 @@ static void mlx5e_activate_channel(struct mlx5e_channel *c)
 		mlx5e_activate_xsk(c);
 	else
 		mlx5e_activate_rq(&c->rq);
-
-	mlx5e_trigger_napi_icosq(c);
 }
 
 static void mlx5e_deactivate_channel(struct mlx5e_channel *c)
@@ -2551,12 +2550,18 @@ err_free:
 	return err;
 }
 
-static void mlx5e_activate_channels(struct mlx5e_channels *chs)
+static void mlx5e_activate_channels(struct mlx5e_priv *priv, struct mlx5e_channels *chs)
 {
 	int i;
 
 	for (i = 0; i < chs->num; i++)
 		mlx5e_activate_channel(chs->c[i]);
+
+	if (priv->htb)
+		mlx5e_qos_activate_queues(priv);
+
+	for (i = 0; i < chs->num; i++)
+		mlx5e_trigger_napi_icosq(chs->c[i]);
 
 	if (chs->ptp)
 		mlx5e_ptp_activate_channel(chs->ptp);
@@ -2864,9 +2869,7 @@ out:
 void mlx5e_activate_priv_channels(struct mlx5e_priv *priv)
 {
 	mlx5e_build_txq_maps(priv);
-	mlx5e_activate_channels(&priv->channels);
-	if (priv->htb)
-		mlx5e_qos_activate_queues(priv);
+	mlx5e_activate_channels(priv, &priv->channels);
 	mlx5e_xdp_tx_enable(priv);
 
 	/* dev_watchdog() wants all TX queues to be started when the carrier is
