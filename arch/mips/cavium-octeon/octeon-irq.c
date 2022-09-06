9ddb719d04ea9c4fdb273e38f4d532f4fc3c567d
Alexander Sverdlin <alexander.sverdlin@nokia.com>
2022-09-06 11:59:43 +0200

    MIPS: OCTEON: irq: Fix octeon_irq_force_ciu_mapping()
    [ Upstream commit ba912afbd611d3a5f22af247721a071ad1d5b9e0 ]
    
    For irq_domain_associate() to work the virq descriptor has to be
    pre-allocated in advance. Otherwise the following happens:
    
    WARNING: CPU: 0 PID: 0 at .../kernel/irq/irqdomain.c:527 irq_domain_associate+0x298/0x2e8
    error: virq128 is not allocated
    Modules linked in:
    CPU: 0 PID: 0 Comm: swapper/0 Not tainted 4.19.78-... #1
            ...
    Call Trace:
    [<ffffffff801344c4>] show_stack+0x9c/0x130
    [<ffffffff80769550>] dump_stack+0x90/0xd0
    [<ffffffff801576d0>] __warn+0x118/0x130
    [<ffffffff80157734>] warn_slowpath_fmt+0x4c/0x70
    [<ffffffff801b83c0>] irq_domain_associate+0x298/0x2e8
    [<ffffffff80a43bb8>] octeon_irq_init_ciu+0x4c8/0x53c
    [<ffffffff80a76cbc>] of_irq_init+0x1e0/0x388
    [<ffffffff80a452cc>] init_IRQ+0x4c/0xf4
    [<ffffffff80a3cc00>] start_kernel+0x404/0x698
    
    Use irq_alloc_desc_at() to avoid the above problem.
    
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@nokia.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit f0ebdfc10bd1bb4f461949485510cb255478cbdb)
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Conflicts:
    	arch/mips/cavium-octeon/octeon-irq.c
    Keep code to match stable v5.4
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

--- arch/mips/cavium-octeon/octeon-irq.c
+++ arch/mips/cavium-octeon/octeon-irq.c
@@ -121,10 +121,15 @@ void octeon_irq_free_cd(struct irq_domain *d, unsigned int irq)
 static int octeon_irq_force_ciu_mapping(struct irq_domain *domain,
 					int irq, int line, int bit)
 {
-	int r;
+	struct device_node *of_node;
+	int ret;
 
-	r = irq_alloc_desc_at(irq, 0);
-	WARN_ON(r < 0);
+	of_node = irq_domain_get_of_node(domain);
+	if (!of_node)
+		return -EINVAL;
+	ret = irq_alloc_desc_at(irq, of_node_to_nid(of_node));
+	if (ret < 0)
+		return ret;
 
 	return irq_domain_associate(domain, irq, line << 6 | bit);
 }
