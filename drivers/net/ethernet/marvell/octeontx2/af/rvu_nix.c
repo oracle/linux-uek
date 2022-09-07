f8ef57cdc65072ef81ca36eb4fc3e0ccbf6a954e
Naveen Mamindlapalli <naveenm@marvell.com>
2022-09-07 18:26:02 +0530

    octeontx2-af: Re-enable mac tx in otx2_stop routine
    While testing qos scheduling with multiple flows, netdev watchdog is
    triggered when a low priority flow never gets a chance to transmit
    the packets because other high priority flows are over subscribing
    the link. The netdev watchdog invokes driver tx_timeout routine, which
    calls otx2_stop followed by otx2_start. During this sequence, it appears
    that some packets were still in NIX pipeline resulting NIX generating
    error interrupts which appears abnormal. Along with error interrupts,
    there are other errors seen w.r.t SQ initialization and as a result
    traffic from certain flows stops. This patch fixes the issue by re-enabling
    mac tx in ndo_stop() ensuring that packets in NIX pipeline gets processed
    and flushed properly.
    
    Also fix put_page ref count issue.
    
    Change-Id: Id9268b66ef881054a95162bf70de8810372322d1
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/85657
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    
    Orabug: 34671021
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
@@ -4797,7 +4797,8 @@ int rvu_mbox_handler_nix_lf_stop_rx(struct rvu *rvu, struct msg_req *req,
 	pfvf = rvu_get_pfvf(rvu, pcifunc);
 	clear_bit(NIXLF_INITIALIZED, &pfvf->flags);
 
-	return rvu_cgx_start_stop_io(rvu, pcifunc, false);
+	rvu_cgx_start_stop_io(rvu, pcifunc, false);
+	return rvu_cgx_tx_enable(rvu, pcifunc, true);
 }
 
 #define RX_SA_BASE  GENMASK_ULL(52, 7)
