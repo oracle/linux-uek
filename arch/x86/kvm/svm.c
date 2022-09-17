3443f94033cef4a5ffdc98641a3a6ddffdcd58e9
Babu Moger <babu.moger@amd.com>
2022-09-16 17:04:39 -0700

    KVM: SVM: Clear the CR4 register on reset
    This problem was reported on a SVM guest while executing kexec.
    Kexec fails to load the new kernel when the PCID feature is enabled.
    
    When kexec starts loading the new kernel, it starts the process by
    resetting the vCPU's and then bringing each vCPU online one by one.
    The vCPU reset is supposed to reset all the register states before the
    vCPUs are brought online. However, the CR4 register is not reset during
    this process. If this register is already setup during the last boot,
    all the flags can remain intact. The X86_CR4_PCIDE bit can only be
    enabled in long mode. So, it must be enabled much later in SMP
    initialization.  Having the X86_CR4_PCIDE bit set during SMP boot can
    cause a boot failures.
    
    Fix the issue by resetting the CR4 register in init_vmcb().
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Message-Id: <161471109108.30811.6392805173629704166.stgit@bmoger-ubuntu>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit 9e46f6c6c959d9bb45445c2e8f04a75324a0dfd0)
    Conflicts:
      - arch/x86/kvm/svm.c vs. arch/x86/kvm/svm/svm.c
      - svm_set_cr4() is static and declared after init_vmcb()
    Orabug: 34617675
    Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
    Reviewed-by: Joe Jin <joe.jin@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -375,6 +375,7 @@ static u8 rsm_ins_bytes[] = "\x0f\xaa";
 static void svm_set_cr0(struct kvm_vcpu *vcpu, unsigned long cr0);
 static void svm_flush_tlb(struct kvm_vcpu *vcpu, bool invalidate_gpa);
 static void svm_complete_interrupts(struct vcpu_svm *svm);
+static int svm_set_cr4(struct kvm_vcpu *vcpu, unsigned long cr4);
 
 static int nested_svm_exit_handled(struct vcpu_svm *svm);
 static int nested_svm_intercept(struct vcpu_svm *svm);
@@ -1591,6 +1592,7 @@ static void init_vmcb(struct vcpu_svm *svm)
 	init_sys_seg(&save->ldtr, SEG_TYPE_LDT);
 	init_sys_seg(&save->tr, SEG_TYPE_BUSY_TSS16);
 
+	svm_set_cr4(&svm->vcpu, 0);
 	svm_set_efer(&svm->vcpu, 0);
 	save->dr6 = 0xffff0ff0;
 	kvm_set_rflags(&svm->vcpu, 2);
