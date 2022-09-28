5a59b6f812158c7ab333b09dd1bca5eefbadac34
Jason A. Donenfeld <Jason@zx2c4.com>
2022-09-28 18:47:30 +0200

    random: add 8-bit and 16-bit batches
    There are numerous places in the kernel that would be sped up by having
    smaller batches. Currently those callsites do `get_random_u32() & 0xff`
    or similar. Since these are pretty spread out, and will require patches
    to multiple different trees, let's get ahead of the curve and lay the
    foundation for `get_random_u8()` and `get_random_u16()`, so that it's
    then possible to start submitting conversion patches leisurely.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    (cherry picked from commit 585cd5fe9f7378601b1d4915ad6e9088333b5e5e)
    
    Orabug: 35543716
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    
    Conflicts: Minor conflicts due to a different surrounding code in
    	   include/linux/random.h

--- include/linux/random.h
+++ include/linux/random.h
@@ -34,6 +34,8 @@ void get_random_bytes(void *buf, int nbytes); /* Satisfy KABI */
 void get_random_bytes(void *buf, size_t len);
 #endif
 size_t __must_check get_random_bytes_arch(void *buf, size_t len);
+u8 get_random_u8(void);
+u16 get_random_u16(void);
 u32 get_random_u32(void);
 u64 get_random_u64(void);
 static inline unsigned int get_random_int(void)
