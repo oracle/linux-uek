bfb2f6e0e10b20e6b65bed982a0f302bbd7f9365
Christoph Hellwig <hch@lst.de>
2022-10-26 11:58:40 +0530

    xfs: factor out a new xfs_log_force_inode helper
    commit 54fbdd1035e3a4e4f4082c335b095426cdefd092 upstream.
    
    Create a new helper to force the log up to the last LSN touching an
    inode.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Acked-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 05e2b279ead434521da2e9304944111f865e26b7)
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    
    Conflicts:
    	fs/xfs/xfs_inode.c
    The conflict is caused by we have extra commit ae8c6ba8b7cc ("xfs: refactor
    the predicate part of xfs_free_eofblocks") which introduced another function
    int xfs_has_eofblocks() in uek6/u3, so context was changed, keep both functions.

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -4068,3 +4068,22 @@ xfs_has_eofblocks(
 	*has = imap.br_startblock != HOLESTARTBLOCK || ip->i_delayed_blks;
 	return 0;
 }
+
+/*
+ * Ensure all commited transactions touching the inode are written to the log.
+ */
+int
+xfs_log_force_inode(
+	struct xfs_inode	*ip)
+{
+	xfs_lsn_t		lsn = 0;
+
+	xfs_ilock(ip, XFS_ILOCK_SHARED);
+	if (xfs_ipincount(ip))
+		lsn = ip->i_itemp->ili_last_lsn;
+	xfs_iunlock(ip, XFS_ILOCK_SHARED);
+
+	if (!lsn)
+		return 0;
+	return xfs_log_force_lsn(ip->i_mount, lsn, XFS_LOG_SYNC, NULL);
+}
