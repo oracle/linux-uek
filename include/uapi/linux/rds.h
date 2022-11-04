oracle/linux-uek

commit 1ff7b301948199085b881afa88a4556464e644af

net/rds : Adding support to print SCQ and RCQ completion vectors in rds-info.
This change adds support to display completion vectors associated with
Send CQ and Receive CQ in rds-info.

We can use 'rds-info -Iv' to get these fields.

Orabug: 34398210

Signed-off-by: Anand Khoje <anand.a.khoje@oracle.com>
Reviewed-by: Arumugam Kolappan <aru.kolappan@oracle.com>
Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

include/uapi/linux/rds.h
@@ -313,7 +313,8 @@ struct rds_info_rdma_connection {
 	uint64_t	rx_poll_ts;
 	uint64_t	tx_poll_cnt;
 	uint64_t	rx_poll_cnt;
-
+	int32_t         scq_vector;
+	int32_t         rcq_vector;
 };
 
 struct rds6_info_rdma_connection {
@@ -347,6 +348,8 @@ struct rds6_info_rdma_connection {
 	uint64_t	rx_poll_ts;
 	uint64_t	tx_poll_cnt;
 	uint64_t	rx_poll_cnt;
+	int32_t         scq_vector;
+	int32_t         rcq_vector;
 };
 
 struct rds_path_info {

net/rds/ib.c
@@ -708,6 +708,8 @@ static int rds_ib_conn_info_visitor(struct rds_connection *conn,
 			(uint64_t)atomic64_read(&ic->i_tx_poll_cnt);
 		iinfo->rx_poll_cnt =
 			(uint64_t)atomic64_read(&ic->i_rx_poll_cnt);
+		iinfo->scq_vector = ic->i_scq_vector;
+		iinfo->rcq_vector = ic->i_rcq_vector;
 	}
 	return 1;
 }
@@ -779,6 +781,8 @@ static int rds6_ib_conn_info_visitor(struct rds_connection *conn,
 			(uint64_t)atomic64_read(&ic->i_tx_poll_cnt);
 		iinfo6->rx_poll_cnt =
 			(uint64_t)atomic64_read(&ic->i_rx_poll_cnt);
+		iinfo6->scq_vector = ic->i_scq_vector;
+		iinfo6->rcq_vector = ic->i_rcq_vector;
 	}
 	return 1;
 }

commit 10c977e7fe3205e36082b4a53f1b5bd2a7674629

uapi: Fix [rs]cq_vector data types in rds[6]_info_rdma_connection
Consistently use types from linux/types.h.

Orabug: 34732042

Fixes: 1ff7b3019481 ("Adding support to print SCQ and RCQ completion vectors in rds-info.")
Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
Reviewed-by: Alan Maguire <alan.maguire@oracle.com>
Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

include/uapi/linux/rds.h
@@ -310,8 +310,8 @@ struct rds_info_rdma_connection {
 	__u64		rx_poll_ts;
 	__u64		tx_poll_cnt;
 	__u64		rx_poll_cnt;
-	int32_t         scq_vector;
-	int32_t         rcq_vector;
+	__s32		scq_vector;
+	__s32		rcq_vector;
 };
 
 struct rds6_info_rdma_connection {
@@ -345,8 +345,8 @@ struct rds6_info_rdma_connection {
 	__u64		rx_poll_ts;
 	__u64		tx_poll_cnt;
 	__u64		rx_poll_cnt;
-	int32_t         scq_vector;
-	int32_t         rcq_vector;
+	__s32		scq_vector;
+	__s32		rcq_vector;
 };
 
 struct rds_path_info {