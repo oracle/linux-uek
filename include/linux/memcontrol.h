2eed6e35c5de00b37b68f33849c5a48142765ea7
Muchun Song <songmuchun@bytedance.com>
2022-11-06 17:52:10 +0530

    mm: list_lru: replace linear array with xarray
    If we run 10k containers in the system, the size of the
    list_lru_memcg->lrus can be ~96KB per list_lru.  When we decrease the
    number containers, the size of the array will not be shrinked.  It is
    not scalable.  The xarray is a good choice for this case.  We can save a
    lot of memory when there are tens of thousands continers in the system.
    If we use xarray, we also can remove the logic code of resizing array,
    which can simplify the code.
    
    [akpm@linux-foundation.org: remove unused local]
    
    Link: https://lkml.kernel.org/r/20220228122126.37293-13-songmuchun@bytedance.com
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Cc: Alex Shi <alexs@kernel.org>
    Cc: Anna Schumaker <Anna.Schumaker@Netapp.com>
    Cc: Chao Yu <chao@kernel.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Fam Zheng <fam.zheng@bytedance.com>
    Cc: Jaegeuk Kim <jaegeuk@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Kari Argillander <kari.argillander@gmail.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Qi Zheng <zhengqi.arch@bytedance.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Cc: Trond Myklebust <trond.myklebust@hammerspace.com>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Wei Yang <richard.weiyang@gmail.com>
    Cc: Xiongchun Duan <duanxiongchun@bytedance.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from bbca91cca9a902de2e9907370e9c1e0a3d1aab0f)
    Orabug: 34717841
    
    Signed-off-by: Kamalesh Babulal <kamalesh.babulal@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com> (KABI changes)
    Reviewed-by: Tom Hromatka <tom.hromatka@oracle.com>
    
    Conflicts:
    - KABI - add new structure member
    	 struct list_lru_node::list_lru_ext::struct xarray xa
      and remove previously introduced structure members in previous patches
    	struct list_lru_node::spinlock_t lock
    	struct list_lru_node::struct list_lru_memcg   __rcu *mlrus
    - KABI - retain struct list_lru_per_memcg
      Retaining it for the original structure using it:
      struct list_lru_node::struct list_lru_memcg __rcu * memcg_lrus
      this structure member is deprecated using UEK_KABI_DEPRECATE().
    - Propagate the changes introduced to list_lru_walk_node() to
      list_lru_walk_one_node() (non-upstream commit 31489778fedb
      "fs/dcache: Estimate the number of negative dentries to prune per
      filesystem")
    
    (cherry picked from commit 735b76da9a6a86df39a1c6e3809b5f9cdcc54dff)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1693,18 +1693,6 @@ void obj_cgroup_uncharge(struct obj_cgroup *objcg, size_t size);
 
 extern struct static_key_false memcg_kmem_enabled_key;
 
-extern int memcg_nr_cache_ids;
-void memcg_get_cache_ids(void);
-void memcg_put_cache_ids(void);
-
-/*
- * Helper macro to loop through all memcg-specific caches. Callers must still
- * check if the cache is valid (it is either valid or NULL).
- * the slab_mutex must be held when looping through those caches
- */
-#define for_each_memcg_cache_index(_idx)	\
-	for ((_idx) = 0; (_idx) < memcg_nr_cache_ids; (_idx)++)
-
 static inline bool memcg_kmem_enabled(void)
 {
 	return static_branch_likely(&memcg_kmem_enabled_key);
@@ -1761,9 +1749,6 @@ static inline void __memcg_kmem_uncharge_page(struct page *page, int order)
 {
 }
 
-#define for_each_memcg_cache_index(_idx)	\
-	for (; NULL; )
-
 static inline bool memcg_kmem_enabled(void)
 {
 	return false;
@@ -1772,14 +1757,6 @@ static inline bool memcg_kmem_enabled(void)
 static inline int memcg_cache_id(struct mem_cgroup *memcg)
 {
 	return -1;
-}
-
-static inline void memcg_get_cache_ids(void)
-{
-}
-
-static inline void memcg_put_cache_ids(void)
-{
 }
 
 static inline struct mem_cgroup *mem_cgroup_from_obj(void *p)
