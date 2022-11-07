dc306056233eb95d700a590ac217dc01ef7264b4
Kees Cook <keescook@chromium.org>
2022-11-06 19:46:35 -0800

    tty: Move TIOCSTI toggle variable before kerndoc
    The variable "tty_legacy_tiocsti" should be defined before the kerndoc
    for the tiocsti() function. The new variable was breaking the "htmldocs"
    build target:
    
    drivers/tty/tty_io.c:2271: warning: cannot understand function prototype: 'bool tty_legacy_tiocsti __read_mostly = IS_ENABLED(CONFIG_LEGACY_TIOCSTI); '
    
    Fixes: 83efeeeb3d04 ("tty: Allow TIOCSTI to be disabled")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Link: https://lore.kernel.org/lkml/20221107143434.66f7be35@canb.auug.org.au
    Cc: Jiri Slaby <jirislaby@kernel.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>
    Link: https://lore.kernel.org/r/20221107034631.never.637-kees@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 5c30f3e4a6e67c88c979ad30554bf4ef9b24fbd0)
    
    Orabug: 38255504
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Todd Vierling <todd.vierling@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -2259,6 +2259,7 @@ static int tty_fasync(int fd, struct file *filp, int on)
 	return retval;
 }
 
+static bool tty_legacy_tiocsti __read_mostly = IS_ENABLED(CONFIG_LEGACY_TIOCSTI);
 /**
  * tiocsti		-	fake input character
  * @tty: tty to fake input into
@@ -2272,7 +2273,6 @@ static int tty_fasync(int fd, struct file *filp, int on)
  *  * Called functions take tty_ldiscs_lock
  *  * current->signal->tty check is safe without locks
  */
-static bool tty_legacy_tiocsti __read_mostly = IS_ENABLED(CONFIG_LEGACY_TIOCSTI);
 static int tiocsti(struct tty_struct *tty, char __user *p)
 {
 	char ch, mbz = 0;
