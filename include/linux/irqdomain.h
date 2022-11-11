c76ecb12182d6b5d7aa321249a289525e92064b7
Thomas Gleixner <tglx@linutronix.de>
2022-11-11 14:54:32 +0100

    genirq/irqdomain: Move bus token enum into a seperate header
    Split the bus token defines out into a seperate header file to avoid
    inclusion of irqdomain.h in msi.h.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Ashok Raj <ashok.raj@intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20221111122014.237221143@linutronix.de
    
    Orabug: 35383105
    
    (cherry picked from commit aeef20527c87fed37c6f159d9eafd063a099f6ed)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: aeef20527c87fed37c6f159d9eafd063a099f6ed
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/irqdomain.h
+++ include/linux/irqdomain.h
@@ -32,6 +32,7 @@
 
 #include <linux/uek_kabi.h>
 #include <linux/types.h>
+#include <linux/irqdomain_defs.h>
 #include <linux/irqhandler.h>
 #include <linux/of.h>
 #include <linux/mutex.h>
@@ -68,27 +69,6 @@ struct irq_fwspec {
 /* Conversion function from of_phandle_args fields to fwspec  */
 void of_phandle_args_to_fwspec(struct device_node *np, const u32 *args,
 			       unsigned int count, struct irq_fwspec *fwspec);
-
-/*
- * Should several domains have the same device node, but serve
- * different purposes (for example one domain is for PCI/MSI, and the
- * other for wired IRQs), they can be distinguished using a
- * bus-specific token. Most domains are expected to only carry
- * DOMAIN_BUS_ANY.
- */
-enum irq_domain_bus_token {
-	DOMAIN_BUS_ANY		= 0,
-	DOMAIN_BUS_WIRED,
-	DOMAIN_BUS_GENERIC_MSI,
-	DOMAIN_BUS_PCI_MSI,
-	DOMAIN_BUS_PLATFORM_MSI,
-	DOMAIN_BUS_NEXUS,
-	DOMAIN_BUS_IPI,
-	DOMAIN_BUS_FSL_MC_MSI,
-	DOMAIN_BUS_TI_SCI_INTA_MSI,
-	DOMAIN_BUS_WAKEUP,
-	DOMAIN_BUS_VMD_MSI,
-};
 
 /**
  * struct irq_domain_ops - Methods for irq_domain objects
