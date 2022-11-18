oracle/linux-uek

commit 4e805861b70bb3637736c76235bba461d5ab347f

net/mlx5: Add support for configuring max device MTU
Allow an admin creating a vdpa device to specify the max MTU for the
net device.

For example, to create a device with max MTU of 1000, the following
command can be used:

$ vdpa dev add name vdpa-a mgmtdev auxiliary/mlx5_core.sf.1 mtu 1000

This configuration mechanism assumes that vdpa is the sole real user of
the function. mlx5_core could theoretically change the mtu of the
function using the ip command on the mlx5_core net device but this
should not be done.

Reviewed-by: Si-Wei Liu<si-wei.liu@oracle.com>
Signed-off-by: Eli Cohen <elic@nvidia.com>
Link: https://lore.kernel.org/r/20220221121927.194728-1-elic@nvidia.com
Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
Acked-by: Jason Wang <jasowang@redhat.com>

Orabug: 33842440

(cherry picked from commit 1e00e821e4ca6355c7fff9f69a4c5ecd78e348a0)
cherry-pick-repo=kernel/git/torvalds/linux.git
unmodified-from-upstream: 1e00e821e4ca6355c7fff9f69a4c5ecd78e348a0

Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
Signed-off-by: Qing Huang <qing.huang@oracle.com>
Reviewed-by: Aron Silverton <aron.silverton@oracle.com>
Signed-off-by: Brian Maly <brian.maly@oracle.com>

drivers/vdpa/mlx5/net/mlx5_vnet.c
@@ -2536,6 +2536,28 @@ static int event_handler(struct notifier_block *nb, unsigned long event, void *p
 	return ret;
 }
 
+static int config_func_mtu(struct mlx5_core_dev *mdev, u16 mtu)
+{
+	int inlen = MLX5_ST_SZ_BYTES(modify_nic_vport_context_in);
+	void *in;
+	int err;
+
+	in = kvzalloc(inlen, GFP_KERNEL);
+	if (!in)
+		return -ENOMEM;
+
+	MLX5_SET(modify_nic_vport_context_in, in, field_select.mtu, 1);
+	MLX5_SET(modify_nic_vport_context_in, in, nic_vport_context.mtu,
+		 mtu + MLX5V_ETH_HARD_MTU);
+	MLX5_SET(modify_nic_vport_context_in, in, opcode,
+		 MLX5_CMD_OP_MODIFY_NIC_VPORT_CONTEXT);
+
+	err = mlx5_cmd_exec_in(mdev, modify_nic_vport_context, in);
+
+	kvfree(in);
+	return err;
+}
+
 static int mlx5_vdpa_dev_add(struct vdpa_mgmt_dev *v_mdev, const char *name,
 			     const struct vdpa_dev_set_config *add_config)
 {
@@ -2596,6 +2618,13 @@ static int mlx5_vdpa_dev_add(struct vdpa_mgmt_dev *v_mdev, const char *name,
 	init_mvqs(ndev);
 	mutex_init(&ndev->reslock);
 	config = &ndev->config;
+
+	if (add_config->mask & BIT_ULL(VDPA_ATTR_DEV_NET_CFG_MTU)) {
+		err = config_func_mtu(mdev, add_config->net.mtu);
+		if (err)
+			goto err_mtu;
+	}
+
 	err = query_mtu(mdev, &mtu);
 	if (err)
 		goto err_mtu;
@@ -2726,7 +2755,8 @@ static int mlx5v_probe(struct auxiliary_device *adev,
 	mgtdev->mgtdev.device = mdev->device;
 	mgtdev->mgtdev.id_table = id_table;
 	mgtdev->mgtdev.config_attr_mask = BIT_ULL(VDPA_ATTR_DEV_NET_CFG_MACADDR) |
-					  BIT_ULL(VDPA_ATTR_DEV_NET_CFG_MAX_VQP);
+					  BIT_ULL(VDPA_ATTR_DEV_NET_CFG_MAX_VQP) |
+					  BIT_ULL(VDPA_ATTR_DEV_NET_CFG_MTU);
 	mgtdev->mgtdev.max_supported_vqs =
 		MLX5_CAP_DEV_VDPA_EMULATION(mdev, max_num_virtio_queues) + 1;
 	mgtdev->mgtdev.supported_features = get_supported_features(mdev);

commit 01766ad0ae1b8de3839004ce16d45157f079f54a

vdpa/mlx5: default_mtu should not override mtu from vdpa tool
The UEK6 specific mlx5_vdpa module param, default_mtu, predated
the per-vdpa mtu attribute introduced through commit 4e805861b70b
("net/mlx5: Add support for configuring max device MTU"). The
expected right behaviour if both mtu configs are present, is to
have per-vdpa instance mtu override the default_mtu. However,
the citing commit did not deal with the interaction properly
when the patch is backported and merged to UEK. Let's fix it.

Orabug: 34756655
Fixes: 4e805861b70b ("net/mlx5: Add support for configuring max device MTU")
Signed-off-by: Si-Wei Liu <si-wei.liu@oracle.com>
Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
Signed-off-by: Brian Maly <brian.maly@oracle.com>

drivers/vdpa/mlx5/net/mlx5_vnet.c
@@ -2701,8 +2701,9 @@ static int mlx5_vdpa_dev_add(struct vdpa_mgmt_dev *v_mdev, const char *name,
 	if (err)
 		goto err_mtu;
 
-	if (unlikely(default_mtu) && default_mtu != mtu) {
-		err = mlx5_modify_nic_vport_mtu(mdev, default_mtu + MLX5V_ETH_HARD_MTU);
+	if (unlikely(default_mtu) && default_mtu != mtu &&
+	    !add_config->mask & BIT_ULL(VDPA_ATTR_DEV_NET_CFG_MTU)) {
+		err = config_func_mtu(mdev, default_mtu);
 		if (err) {
 			mlx5_vdpa_warn(&ndev->mvdev,
 				       "changed to default mtu %d failed\n",
@@ -2783,7 +2784,7 @@ err_mpfs:
 		mlx5_mpfs_del_mac(pfmdev, config->mac);
 err_mtu:
 	if (saved_mtu)
-		mlx5_modify_nic_vport_mtu(mdev, saved_mtu + MLX5V_ETH_HARD_MTU);
+		config_func_mtu(mdev, saved_mtu);
 	mutex_destroy(&ndev->reslock);
 err_alloc:
 	put_device(&mvdev->vdev.dev);