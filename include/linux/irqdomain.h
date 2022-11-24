20669bb6941641cd62c5a7348e5abdca8836ca24
Thomas Gleixner <tglx@linutronix.de>
2022-11-25 00:24:15 +0100

    genirq/irqdomain: Provide IRQ_DOMAIN_FLAG_MSI_PARENT
    The new PCI/IMS (Interrupt Message Store) functionality is allowing
    hardware vendors to provide implementation specific storage for the MSI
    messages. This can be device memory and also host/guest memory, e.g. in
    queue memory which is shared with the hardware.
    
    This requires device specific MSI interrupt domains, which cannot be
    achieved by expanding the existing PCI/MSI interrupt domain concept which is
    a global interrupt domain shared by all PCI devices on a particular (IOMMU)
    segment:
    
                                             |--- device 1
         [Vector]---[Remapping]---[PCI/MSI]--|...
                                             |--- device N
    
    This works because the PCI/MSI[-X] space is uniform, but falls apart with
    PCI/IMS which is implementation defined and must be available along with
    PCI/MSI[-X] on the same device.
    
    To support PCI/MSI[-X] plus PCI/IMS on the same device it is required to
    rework the PCI/MSI interrupt domain hierarchy concept in the following way:
    
                                  |--- [PCI/MSI] device 1
         [Vector]---[Remapping]---|...
                                  |--- [PCI/MSI] device N
    
    That allows in the next step to create multiple interrupt domains per device:
    
                                  |--- [PCI/MSI] device 1
                                  |--- [PCI/IMS] device 1
         [Vector]---[Remapping]---|...
                                  |--- [PCI/MSI] device N
                                  |--- [PCI/IMS] device N
    
    So the domain which previously created the global PCI/MSI domain must now
    act as parent domain for the per device domains.
    
    The hierarchy depth is the same as before, but the PCI/MSI domains are then
    device specific and not longer global.
    
    Provide IRQ_DOMAIN_FLAG_MSI_PARENT, which allows to identify these parent
    domains, along with helpers to query it.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20221124230313.690038274@linutronix.de
    
    Orabug: 35383105
    
    (cherry picked from commit b749e6d31c88cff2202b968aaba246e5d7379038)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: b749e6d31c88cff2202b968aaba246e5d7379038
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/irqdomain.h
+++ include/linux/irqdomain.h
@@ -194,6 +194,9 @@ enum {
 	/* Irq domain doesn't translate anything */
 	IRQ_DOMAIN_FLAG_NO_MAP		= (1 << 6),
 
+	/* Irq domain is a MSI parent domain */
+	IRQ_DOMAIN_FLAG_MSI_PARENT	= (1 << 8),
+
 	/*
 	 * Flags starting from IRQ_DOMAIN_FLAG_NONCORE are reserved
 	 * for implementation specific purposes and ignored by the
@@ -556,6 +559,11 @@ static inline bool irq_domain_is_msi_remap(struct irq_domain *domain)
 
 extern bool irq_domain_hierarchical_is_msi_remap(struct irq_domain *domain);
 
+static inline bool irq_domain_is_msi_parent(struct irq_domain *domain)
+{
+	return domain->flags & IRQ_DOMAIN_FLAG_MSI_PARENT;
+}
+
 #else	/* CONFIG_IRQ_DOMAIN_HIERARCHY */
 static inline int irq_domain_alloc_irqs(struct irq_domain *domain,
 			unsigned int nr_irqs, int node, void *arg)
@@ -601,6 +609,12 @@ irq_domain_hierarchical_is_msi_remap(struct irq_domain *domain)
 {
 	return false;
 }
+
+static inline bool irq_domain_is_msi_parent(struct irq_domain *domain)
+{
+	return false;
+}
+
 #endif	/* CONFIG_IRQ_DOMAIN_HIERARCHY */
 
 #else /* CONFIG_IRQ_DOMAIN */
