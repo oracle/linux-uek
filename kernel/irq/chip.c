fb170e1d893a755290b1320b142c20a6adfb2302
Thomas Gleixner <tglx@linutronix.de>
2022-11-25 00:24:12 +0100

    genirq/irqdomain: Rename irq_domain::dev to irq_domain:: Pm_dev
    irq_domain::dev is a misnomer as it's usually the rule that a device
    pointer points to something which is directly related to the instance.
    
    irq_domain::dev can point to some other device for power management to
    ensure that this underlying device is not powered down when an interrupt is
    allocated.
    
    The upcoming per device MSI domains really require a pointer to the device
    which instantiated the irq domain and not to some random other device which
    is required for power management down the chain.
    
    Rename irq_domain::dev to irq_domain::pm_dev and fixup the few sites which
    use that pointer.
    
    Conversion was done with the help of coccinelle.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20221124230313.574541683@linutronix.de
    
    Orabug: 35383105
    
    (cherry picked from commit 6a9fc4190ca23fcf327de666e1a98dbdcdd2d210)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    
    Conflicts:
    	include/linux/irqdomain.h
    
    A small contextual conflict caused by prior kABI workaround code.
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -1559,13 +1559,13 @@ int irq_chip_compose_msi_msg(struct irq_data *data, struct msi_msg *msg)
 	return 0;
 }
 
-static struct device *irq_get_parent_device(struct irq_data *data)
+static struct device *irq_get_pm_device(struct irq_data *data)
 {
 	if (data->chip->parent_device)
 		return data->chip->parent_device;
 
 	if (data->domain)
-		return data->domain->dev;
+		return data->domain->pm_dev;
 
 	return NULL;
 }
@@ -1579,7 +1579,7 @@ static struct device *irq_get_parent_device(struct irq_data *data)
  */
 int irq_chip_pm_get(struct irq_data *data)
 {
-	struct device *dev = irq_get_parent_device(data);
+	struct device *dev = irq_get_pm_device(data);
 	int retval = 0;
 
 	if (IS_ENABLED(CONFIG_PM) && dev)
@@ -1598,7 +1598,7 @@ int irq_chip_pm_get(struct irq_data *data)
  */
 int irq_chip_pm_put(struct irq_data *data)
 {
-	struct device *dev = irq_get_parent_device(data);
+	struct device *dev = irq_get_pm_device(data);
 	int retval = 0;
 
 	if (IS_ENABLED(CONFIG_PM) && dev)
