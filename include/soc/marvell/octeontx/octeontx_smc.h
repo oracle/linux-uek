4a931cf98e1beb25ecf63c1f1c10003a85c6f913
sdonelli <sdonelli@marvell.com>
2022-11-24 16:06:18 +0000

    drivers: soc: Adds common Marvell OcteonTX header for SMC calls
    This patch unifies handling of SMC call check code
    for OcteonTX and CN10K platforms.
    Previously drivers have used different approach to check
    compatibilit with the ATF istance running on secure side.
    Now common code is available.
    OcteonTX2 and CN10K shares the same UUID for ATF services.
    Using soc_id framework to get and match the soc id and revision
    instead of reading from FDT node.
    
    Signed-off-by: sdonelli <sdonelli@marvell.com>
    Change-Id: I272ad240896feacbbb55f0dd03b14a985bbea37c
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ include/soc/marvell/octeontx/octeontx_smc.h
@@ -0,0 +1,112 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * Copyright (C) 2021 Marvell
+ *
+ */
+
+#ifndef _SOC_MRVL_OCTEONTX_SMC_H
+#define _SOC_MRVL_OCTEONTX_SMC_H
+
+#include <linux/errno.h>
+#include <linux/arm-smccc.h>
+#include <asm/cputype.h>
+#include <linux/of.h>
+#include <linux/sys_soc.h>
+
+/* Data and defines for SMC call */
+#define OCTEONTX_ARM_SMC_SVC_UID			0xc200ff01
+
+/* This is expected OCTEONTX response for SVC UID command */
+/** Check software version and compatibility of ATF
+ *
+ * The call verifies ATF instance running on the system.
+ *
+ * @return
+ *	0 (T9x) and 2 (cn10k) on success
+ *	error code on failure
+ *
+ */
+static inline int octeontx_soc_check_smc(void)
+{
+#define CPU_MODEL_CN10KX_PART	0xd49
+
+	const int octeontx_svc_uuid[] = {
+		0x6ff498cf,
+		0x5a4e9cfa,
+		0x2f2a3aa4,
+		0x5945b105,
+	};
+
+	struct arm_smccc_res res;
+
+	/* Is it OCTEONTX on the other side of SMC monitor? */
+	arm_smccc_smc(OCTEONTX_ARM_SMC_SVC_UID, 0, 0, 0, 0, 0, 0, 0, &res);
+	if (res.a0 != octeontx_svc_uuid[0] || res.a1 != octeontx_svc_uuid[1] ||
+	    res.a2 != octeontx_svc_uuid[2] || res.a3 != octeontx_svc_uuid[3])
+		return -EPERM;
+
+	if (MIDR_PARTNUM(read_cpuid_id()) == CPU_MODEL_CN10KX_PART)
+		return 2;
+
+	return 0;
+}
+
+static inline bool is_soc_cn10kx(void)
+{
+	if (MIDR_PARTNUM(read_cpuid_id()) == CPU_MODEL_CN10KX_PART)
+		return 1;
+	return 0;
+}
+
+static inline bool is_soc_cnf10kb(void)
+{
+	static const struct soc_device_attribute cnf10kb_socinfo[] = {
+		{.soc_id = "jep106:0369:00bc", },
+		{},
+	};
+	const struct soc_device_attribute *attr;
+
+	attr = soc_device_match(cnf10kb_socinfo);
+
+	if (attr)
+		return true;
+
+	return false;
+}
+
+static inline bool is_soc_cn10ka_ax(void)
+{
+	static const struct soc_device_attribute cn10ka_socinfo[] = {
+		{.soc_id = "jep106:0369:00b9", .revision = "0x00000000", },
+		{.soc_id = "jep106:0369:00b9", .revision = "0x00000001", },
+		{},
+	};
+	const struct soc_device_attribute *attr;
+
+	attr = soc_device_match(cn10ka_socinfo);
+
+	if (attr)
+		return true;
+
+	return false;
+}
+
+static inline bool is_soc_cnf10ka_ax(void)
+{
+	static const struct soc_device_attribute cnf10ka_socinfo[] = {
+		{.soc_id = "jep106:0369:00ba", .revision = "0x00000000", },
+		{.soc_id = "jep106:0369:00ba", .revision = "0x00000001", },
+		{},
+	};
+	const struct soc_device_attribute *attr;
+
+	attr = soc_device_match(cnf10ka_socinfo);
+
+	if (attr)
+		return true;
+
+	return false;
+}
+
+#endif /* _SOC_MRVL_OCTEONTX_SMC_H */
