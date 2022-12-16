e98ba1422ed21a8e8f6f4d3b2df3a39c7878426b
Jakub Kicinski <kuba@kernel.org>
2022-12-15 20:41:22 -0800

    devlink: protect devlink dump by the instance lock
    Take the instance lock around devlink_nl_fill() when dumping,
    doit takes it already.
    
    We are only dumping basic info so in the worst case we were risking
    data races around the reload statistics. Until the big devlink mutex
    was removed all relevant code was protected by it, so the missing
    instance lock was not exposed.
    
    Fixes: d3efc2a6a6d8 ("net: devlink: remove devlink_mutex")
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Link: https://lore.kernel.org/r/20221216044122.1863550-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 35290196
    
    (cherry picked from commit 214964a13ab56a9757d146b79b468a7ca190fbfb)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: 214964a13ab56a9757d146b79b468a7ca190fbfb
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>

--- net/core/devlink.c
+++ net/core/devlink.c
@@ -1660,10 +1660,13 @@ static int devlink_nl_cmd_get_dumpit(struct sk_buff *msg,
 			continue;
 		}
 
+		devl_lock(devlink);
 		err = devlink_nl_fill(msg, devlink, DEVLINK_CMD_NEW,
 				      NETLINK_CB(cb->skb).portid,
 				      cb->nlh->nlmsg_seq, NLM_F_MULTI);
+		devl_unlock(devlink);
 		devlink_put(devlink);
+
 		if (err)
 			goto out;
 		idx++;
