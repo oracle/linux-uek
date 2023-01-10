edc5d1dff4faedd622d1521436ac2381f25663e2
Rohit Nair <rohit.sajan.kumar@oracle.com>
2023-01-09 16:14:14 -0800

    IB/mlx5: Add a signature check to received EQEs and CQEs
    As PRM defines, an EQE is valid if byte-wise XOR of the entire EQE
    (including signature field) and the EQE index is 0xff. Otherwise, we can
    assume we have a corrupt EQE. The same is applicable to CQE as well.
    
    Adding a check to verify the EQE and CQE is valid in that aspect and if
    not, dump the CQE and EQE to dmesg to be inspected.
    
    This patch was sent upstream, but was rejected due to performce
    concerns. Thus this patch is being added with oracle extensions and is
    disabled by default.
    
    Orabug: 34821074
    
    Signed-off-by: Rohit Nair <rohit.sajan.kumar@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/infiniband/hw/mlx5/cq.c
+++ drivers/infiniband/hw/mlx5/cq.c
@@ -39,6 +39,7 @@
 
 #ifndef WITHOUT_ORACLE_EXTENSIONS
 #include <linux/mlx5/driver.h>
+#include "oracle_ext.h"
 #endif /* WITHOUT_ORACLE_EXTENSIONS */
 
 static void mlx5_ib_cq_comp(struct mlx5_core_cq *cq, struct mlx5_eqe *eqe)
@@ -468,6 +469,11 @@ repoll:
 		return -EAGAIN;
 
 	cqe64 = (cq->mcq.cqe_sz == 64) ? cqe : cqe + 64;
+
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (unlikely(mlx5_ib_verify_cqe_flag))
+		verify_cqe(cqe64, cq);
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 	++cq->mcq.cons_index;
 
