cfc7368745f6f6579afab04ee9c53b9d48003bc1
Kim Phillips <kim.phillips@amd.com>
2023-01-24 10:33:16 -0600

    x86/cpu, kvm: Add the Null Selector Clears Base feature
    The Null Selector Clears Base feature was being open-coded for KVM.
    Add it to its newly added native CPUID leaf 0x80000021 EAX proper.
    
    Also drop the bit description comments now it's more self-describing.
    
      [ bp: Convert test in check_null_seg_clears_base() too. ]
    
    Signed-off-by: Kim Phillips <kim.phillips@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Acked-by: Sean Christopherson <seanjc@google.com>
    Link: https://lore.kernel.org/r/20230124163319.2277355-6-kim.phillips@amd.com
    (cherry picked from commit 5b909d4ae59aedc711b7a432da021be0e82c95a0)
    
    Orabug: 37184800
    
    Signed-off-by: Simran Singh <simran.p.singh@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -431,6 +431,7 @@
 /* AMD-defined Extended Feature 2 EAX, CPUID level 0x80000021 (EAX), word 20 */
 #define X86_FEATURE_NO_NESTED_DATA_BP	(20*32+ 0) /* "" No Nested Data Breakpoints */
 #define X86_FEATURE_LFENCE_RDTSC	(20*32+ 2) /* "" LFENCE always serializing / synchronizes RDTSC */
+#define X86_FEATURE_NULL_SEL_CLR_BASE	(20*32+ 6) /* "" Null Selector Clears Base */
 #define X86_FEATURE_AUTOIBRS		(20*32+ 8) /* "" Automatic IBRS */
 #define X86_FEATURE_SBPB		(20*32+27) /* "" Selective Branch Prediction Barrier */
 #define X86_FEATURE_IBPB_BRTYPE		(20*32+28) /* "" MSR_PRED_CMD[IBPB] flushes all branch type predictions */
