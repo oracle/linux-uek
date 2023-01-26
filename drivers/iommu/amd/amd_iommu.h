d0dce0616bf711f5fbe93599ee48cfecba6d2424
Joao Martins <joao.m.martins@oracle.com>
2023-01-26 11:35:23 -0500

    iommu/amd: Disable AVIC on certain systems BIOS
    E4-2C machine BIOS may mis-advertise the IOMMU feature GA_vAPIC
    which essentially enables IOMMU AVIC. However this feature have
    problems and should not be used in E4-2C machines, thus we should
    ignore what goes over the EFR IOMMU registers obtained via MMIO.
    
    Orabug: 35018577
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -172,6 +172,7 @@ static inline struct protection_domain *to_pdomain(struct iommu_domain *dom)
 
 bool translation_pre_enabled(struct amd_iommu *iommu);
 int __init add_special_device(u8 type, u8 id, u32 *devid, bool cmd_line);
+void __init disable_vapic(void);
 
 int amd_iommu_pdom_id_alloc(void);
 int amd_iommu_pdom_id_reserve(u16 id, gfp_t gfp);
