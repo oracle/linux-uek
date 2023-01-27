368d769fec9b502749d88281c5927d34314adcfd
Muralidhara M K <muralidhara.mk@amd.com>
2023-01-27 17:04:10 +0000

    EDAC/amd64: Split read_base_mask() into dct/umc functions
    Call them from their respective hw_info_get() paths.
    
    Call the new functions after the setting the chip select base and mask
    counts, since those are need to read the correct number of chip select
    base and mask registers. And call the new functions before the remaining
    set up, because the base and mask register values will be needed later.
    
      [Yazen: Rebased/reworked patch and reworded commit message. ]
    
    Signed-off-by: Muralidhara M K <muralidhara.mk@amd.com>
    Co-developed-by: Naveen Krishna Chatradhi <naveenkrishna.chatradhi@amd.com>
    Signed-off-by: Naveen Krishna Chatradhi <naveenkrishna.chatradhi@amd.com>
    Co-developed-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20230127170419.1824692-14-yazen.ghannam@amd.com
    (cherry picked from commit b29dad9bf3ce13e7a37f7fc75ee2c95ac48b9f08)
    
    Orabug: 36092305
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Henry Willard <henry.willard@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -1357,7 +1357,7 @@ static void umc_prep_chip_selects(struct amd64_pvt *pvt)
 	}
 }
 
-static void read_umc_base_mask(struct amd64_pvt *pvt)
+static void umc_read_base_mask(struct amd64_pvt *pvt)
 {
 	u32 umc_base_reg, umc_base_reg_sec;
 	u32 umc_mask_reg, umc_mask_reg_sec;
@@ -1411,12 +1411,9 @@ static void read_umc_base_mask(struct amd64_pvt *pvt)
 /*
  * Function 2 Offset F10_DCSB0; read in the DCS Base and DCS Mask registers
  */
-static void read_dct_base_mask(struct amd64_pvt *pvt)
+static void dct_read_base_mask(struct amd64_pvt *pvt)
 {
 	int cs;
-
-	if (pvt->umc)
-		return read_umc_base_mask(pvt);
 
 	for_each_chip_select(cs, 0, pvt) {
 		int reg0   = DCSB0 + (cs * 4);
@@ -2912,7 +2909,6 @@ static void read_mc_regs(struct amd64_pvt *pvt)
 	}
 
 skip:
-	read_dct_base_mask(pvt);
 
 	determine_memory_type(pvt);
 
@@ -3393,6 +3389,7 @@ static int dct_hw_info_get(struct amd64_pvt *pvt)
 		return ret;
 
 	dct_prep_chip_selects(pvt);
+	dct_read_base_mask(pvt);
 	read_mc_regs(pvt);
 
 	return 0;
@@ -3405,6 +3402,7 @@ static int umc_hw_info_get(struct amd64_pvt *pvt)
 		return -ENOMEM;
 
 	umc_prep_chip_selects(pvt);
+	umc_read_base_mask(pvt);
 	read_mc_regs(pvt);
 
 	return 0;
