d4d11dde600bdf5327a353b8745e2b88cbeeb359
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2023-02-03 20:17:53 -0500

    uek-misc: Initial version
    This driver automatically enables the on_exadata jump label
    when an Exadata instance has been detected. There are two detection
    mechanism:
    
     - via DMI, factory sets one of type 11 to the string "00010000", like so:
    	Handle 0x0026, DMI type 11, 5 bytes
    	OEM Strings
    		String 1: SUNW-PRMS-1
    		String 2:
    		String 3: storage-variant:8dbp
    		String 4: 00010000
    		String 5:
       so lets check for that.
    
     - via the sched_uek=wakeidle or exadata=on command line parameter.
    
    Move the sched/core definition of on_exadata into misc/uek module.
    
    Collapsing related commits into the same basic patch.
    
    Orabug: 35049221
    
    Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Mark Nicholson <mark.j.nicholson@oracle.com>

---
+++ drivers/misc/uek/uek.c
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates.
+ */
+
+#include <linux/dmi.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+#define UEK_MISC_VER  "0.1"
+
+MODULE_AUTHOR("Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>");
+MODULE_DESCRIPTION("uek");
+MODULE_LICENSE("GPL");
+MODULE_VERSION(UEK_MISC_VER);
+
+DEFINE_STATIC_KEY_FALSE(on_exadata);
+EXPORT_SYMBOL_GPL(on_exadata);
+
+/* Override to disable optimizations on Exadata systems. */
+static bool exadata_disable;
+
+static int __init uek_params(char *str)
+{
+	if (!str)
+		return 0;
+
+	if (strncmp(str, "exadata", 7) == 0) {
+		static_branch_enable(&on_exadata);
+		return 1;
+	} else if ((strncmp(str, "noexadata", 9) == 0)) {
+		exadata_disable = true;
+		return 1;
+	}
+
+	return 1;
+}
+__setup("uek=", uek_params);
+
+static int detect_exadata_dmi(char **reason)
+{
+	static const char * const oemstrs[] = {"SUNW-PRMS-1", "00010000"};
+	static const struct dmi_system_id oracle_mbs[] = {
+		{
+			.matches = {
+				DMI_MATCH(DMI_SYS_VENDOR, "Oracle Corporation"),
+			},
+		},
+		{
+			.matches = {
+				DMI_MATCH(DMI_CHASSIS_ASSET_TAG, "OracleCloud.com"),
+			},
+		},
+		{}
+	};
+
+	unsigned int i, ok = 0;
+
+	/* Not Oracle system? .. Bye. */
+	if (!dmi_check_system(oracle_mbs))
+		goto err;
+
+	/* Check for Type 11 and make sure it has the right markings. */
+	for (i = 0; i < ARRAY_SIZE(oemstrs); i++)
+		if (dmi_find_device(DMI_DEV_TYPE_OEM_STRING, oemstrs[i], NULL))
+			ok++;
+
+	if (ok == 2) {
+		*reason = "via DMI";
+		return 0;
+	}
+
+err:
+	return -ENODEV;
+}
+
+static int detect_exadata_bootline(char **reason)
+{
+	if (static_key_enabled(&on_exadata)) {
+		*reason = "via command line";
+		return 0;
+	}
+	return -ENODEV;
+}
+
+static int uek_misc_init(void)
+{
+	int ret;
+	char *reason = NULL;
+
+	/* Boot time override engaged */
+	if (exadata_disable)
+		return -ENODEV;
+
+	ret = detect_exadata_bootline(&reason);
+	if (!ret)
+		goto enable;
+
+	ret = detect_exadata_dmi(&reason);
+	if (ret)
+		return ret;
+
+enable:
+	/* Go-Go Exadata goodness! */
+	static_branch_enable(&on_exadata);
+
+	pr_info("Detected Exadata (%s)", reason);
+
+	return 0;
+}
+
+core_initcall(uek_misc_init);
