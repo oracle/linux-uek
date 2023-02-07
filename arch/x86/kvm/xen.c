bc9ded791050456f060b884ac849b1758219eac9
Vijayendra Suman <vijayendra.suman@oracle.com>
2023-02-07 08:25:06 -0800

    Revert "KVM: x86: Fix wall clock writes in Xen shared_info not to mark page dirty"
    kvm_gfn_to_pfn_cache_init is not defined, which is leading to
    compilation error
    
    arch/x86/kvm/xen.c:41:23: error: implicit declaration of function 'kvm_gfn_to_pfn_cache_init'
    
    This reverts commit 75f03213882ce0f993ae6b2a64465e32631dd05a.
    
    Orabug: 34929435
    
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/x86/kvm/xen.c
+++ arch/x86/kvm/xen.c
@@ -24,11 +24,8 @@ DEFINE_STATIC_KEY_DEFERRED_FALSE(kvm_xen_enabled, HZ);
 static int kvm_xen_shared_info_init(struct kvm *kvm, gfn_t gfn)
 {
 	struct gfn_to_pfn_cache *gpc = &kvm->arch.xen.shinfo_cache;
-	struct pvclock_wall_clock *wc;
 	gpa_t gpa = gfn_to_gpa(gfn);
-	u32 *wc_sec_hi;
-	u32 wc_version;
-	u64 wall_nsec;
+	int wc_ofs, sec_hi_ofs;
 	int ret = 0;
 	int idx = srcu_read_lock(&kvm->srcu);
 
@@ -37,63 +34,32 @@ static int kvm_xen_shared_info_init(struct kvm *kvm, gfn_t gfn)
 		goto out;
 	}
 
-	do {
-		ret = kvm_gfn_to_pfn_cache_init(kvm, gpc, NULL, false, true,
-						gpa, PAGE_SIZE, false);
-		if (ret)
-			goto out;
-
-		/*
-		 * This code mirrors kvm_write_wall_clock() except that it writes
-		 * directly through the pfn cache and doesn't mark the page dirty.
-		 */
-		wall_nsec = ktime_get_real_ns() - get_kvmclock_ns(kvm);
-
-		/* It could be invalid again already, so we need to check */
-		read_lock_irq(&gpc->lock);
-
-		if (gpc->valid)
-			break;
-
-		read_unlock_irq(&gpc->lock);
-	} while (1);
+	ret = kvm_gfn_to_pfn_cache_init(kvm, gpc, NULL, false, true, gpa,
+					PAGE_SIZE, false);
+	if (ret)
+		goto out;
 
 	/* Paranoia checks on the 32-bit struct layout */
 	BUILD_BUG_ON(offsetof(struct compat_shared_info, wc) != 0x900);
 	BUILD_BUG_ON(offsetof(struct compat_shared_info, arch.wc_sec_hi) != 0x924);
 	BUILD_BUG_ON(offsetof(struct pvclock_vcpu_time_info, version) != 0);
 
+	/* 32-bit location by default */
+	wc_ofs = offsetof(struct compat_shared_info, wc);
+	sec_hi_ofs = offsetof(struct compat_shared_info, arch.wc_sec_hi);
+
 #ifdef CONFIG_X86_64
 	/* Paranoia checks on the 64-bit struct layout */
 	BUILD_BUG_ON(offsetof(struct shared_info, wc) != 0xc00);
 	BUILD_BUG_ON(offsetof(struct shared_info, wc_sec_hi) != 0xc0c);
 
-	if (IS_ENABLED(CONFIG_64BIT) && kvm->arch.xen.long_mode) {
-		struct shared_info *shinfo = gpc->khva;
-
-		wc_sec_hi = &shinfo->wc_sec_hi;
-		wc = &shinfo->wc;
-	} else
-#endif
-	{
-		struct compat_shared_info *shinfo = gpc->khva;
-
-		wc_sec_hi = &shinfo->arch.wc_sec_hi;
-		wc = &shinfo->wc;
+	if (kvm->arch.xen.long_mode) {
+		wc_ofs = offsetof(struct shared_info, wc);
+		sec_hi_ofs = offsetof(struct shared_info, wc_sec_hi);
 	}
+#endif
 
-	/* Increment and ensure an odd value */
-	wc_version = wc->version = (wc->version + 1) | 1;
-	smp_wmb();
-
-	wc->nsec = do_div(wall_nsec,  1000000000);
-	wc->sec = (u32)wall_nsec;
-	*wc_sec_hi = wall_nsec >> 32;
-	smp_wmb();
-
-	wc->version = wc_version + 1;
-	read_unlock_irq(&gpc->lock);
-
+	kvm_write_wall_clock(kvm, gpa + wc_ofs, sec_hi_ofs - wc_ofs);
 	kvm_make_all_cpus_request(kvm, KVM_REQ_MASTERCLOCK_UPDATE);
 
 out:
