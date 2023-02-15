bf531ad8f14b2841c28e259f16120ffde6be5845
Si-Wei Liu <si-wei.liu@oracle.com>
2023-02-14 17:30:40 -0800

    vdpa/mlx5: should not activate virtq object when suspended
    Otherwise the virtqueue object to instate could point to invalid address
    that was unmapped from the MTT:
    
      mlx5_core 0000:41:04.2: mlx5_cmd_out_err:782:(pid 8321):
      CREATE_GENERAL_OBJECT(0xa00) op_mod(0xd) failed, status
      bad parameter(0x3), syndrome (0x5fa1c), err(-22)
    
    Fixes: cae15c2ed8e6 ("vdpa/mlx5: Implement susupend virtqueue callback")
    Cc: Eli Cohen <elic@nvidia.com>
    Signed-off-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Reviewed-by: Eli Cohen <elic@nvidia.com>
    
    Message-Id: <1676424640-11673-1-git-send-email-si-wei.liu@oracle.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit 09e65ee9059d76b89cb713795748805efd3f50c6)
    
    Orabug: 35210546
    
    Signed-off-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/vdpa/mlx5/net/mlx5_vnet.c
+++ drivers/vdpa/mlx5/net/mlx5_vnet.c
@@ -2369,7 +2369,7 @@ static int mlx5_vdpa_change_map(struct mlx5_vdpa_dev *mvdev,
 	if (err)
 		goto err_mr;
 
-	if (!(mvdev->status & VIRTIO_CONFIG_S_DRIVER_OK))
+	if (!(mvdev->status & VIRTIO_CONFIG_S_DRIVER_OK) || mvdev->suspended)
 		goto err_mr;
 
 	restore_channels_info(ndev);
@@ -2537,6 +2537,7 @@ static int mlx5_vdpa_reset(struct vdpa_device *vdev)
 	clear_vqs_ready(ndev);
 	mlx5_vdpa_destroy_mr(&ndev->mvdev);
 	ndev->mvdev.status = 0;
+	ndev->mvdev.suspended = false;
 	ndev->cur_num_vqs = 0;
 	ndev->mvdev.cvq.received_desc = 0;
 	ndev->mvdev.cvq.completed_desc = 0;
@@ -2783,6 +2784,8 @@ static int mlx5_vdpa_suspend(struct vdpa_device *vdev)
 	struct mlx5_vdpa_virtqueue *mvq;
 	int i;
 
+	mlx5_vdpa_info(mvdev, "suspending device\n");
+
 	down_write(&ndev->reslock);
 	ndev->nb_registered = false;
 	mlx5_notifier_unregister(mvdev->mdev, &ndev->nb);
@@ -2792,6 +2795,7 @@ static int mlx5_vdpa_suspend(struct vdpa_device *vdev)
 		suspend_vq(ndev, mvq);
 	}
 	mlx5_vdpa_cvq_suspend(mvdev);
+	mvdev->suspended = true;
 	up_write(&ndev->reslock);
 	return 0;
 }
