e8f5fc768156930c4ce91a375a6fc270569e0a7a
Waiman Long <longman@redhat.com>
2023-02-16 10:15:27 +0000

    fs/dcache: Add sysctl parameter neg-dentry-pc as a soft limit on negative dentries
    A new sysctl parameter "neg-dentry-pc" is added to /proc/sys/fs whose
    value represents a soft limit on the total number of negative dentries
    allowable in a system as a percentage of the total system memory.
    The allowable range of this new parameter is 0-10 where 0 means no
    soft limit.
    
    A warning message will be printed if the soft limit is exceeded.
    
    Signed-off-by: Waiman Long <longman@redhat.com>
    
    Orabug: 33468829
    
    Backported https://lkml.org/lkml/2018/7/6/880
    
    Signed-off-by: Gautham Ananthakrishna <gautham.ananthakrishna@oracle.com>
    Reviewed-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -82,7 +82,7 @@
 #endif
 
 /* shared constants to be used in various sysctls */
-const int sysctl_vals[] = { 0, 1, 2, 3, 4, 100, 200, 1000, 3000, INT_MAX, 65535, -1 };
+const int sysctl_vals[] = { 0, 1, 2, 3, 4, 10, 100, 200, 1000, 3000, INT_MAX, 65535, -1 };
 EXPORT_SYMBOL(sysctl_vals);
 
 const unsigned long sysctl_long_vals[] = { 0, 1, LONG_MAX };
