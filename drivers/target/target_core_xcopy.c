1051a0676d14140468ee1efac580c194e3786011
Mike Christie <michael.christie@oracle.com>
2023-03-18 20:56:14 -0500

    scsi: target: Pass in cmd counter to use during cmd setup
    Allow target_get_sess_cmd() users to pass in the cmd counter they want to
    use. Right now we pass in the session's cmd counter but in a subsequent
    commit iSCSI will switch from per session to per conn.
    
    Signed-off-by: Mike Christie <michael.christie@oracle.com>
    Link: https://lore.kernel.org/r/20230319015620.96006-4-michael.christie@oracle.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 8e288be8606ad87c1726618eacfb8fbd3ab4b806)
    
    Orabug: 35404019
    Signed-off-by: Gulam Mohamed <gulam.mohamed@oracle.com>
    Reviewed-by: Mike Christie <michael.christie@oracle.com>
    
    Conflicts:
    	drivers/target/target_core_xcopy.c

--- drivers/target/target_core_xcopy.c
+++ drivers/target/target_core_xcopy.c
@@ -592,8 +592,8 @@ static int target_xcopy_read_source(
 		(unsigned long long)src_lba, src_sectors, length);
 
 	__target_init_cmd(se_cmd, &xcopy_pt_tfo, &xcopy_pt_sess, length,
-			  DMA_FROM_DEVICE, 0, &xpt_cmd.sense_buffer[0], 0);
-
+			  DMA_FROM_DEVICE, 0, &xpt_cmd.sense_buffer[0], 0,
+			  NULL);
 	rc = target_xcopy_setup_pt_cmd(&xpt_cmd, xop, src_dev, &cdb[0],
 				remote_port);
 	if (rc < 0) {
@@ -637,8 +637,8 @@ static int target_xcopy_write_destination(
 		(unsigned long long)dst_lba, dst_sectors, length);
 
 	__target_init_cmd(se_cmd, &xcopy_pt_tfo, &xcopy_pt_sess, length,
-			  DMA_TO_DEVICE, 0, &xpt_cmd.sense_buffer[0], 0);
-
+			  DMA_TO_DEVICE, 0, &xpt_cmd.sense_buffer[0], 0,
+			  NULL);
 	rc = target_xcopy_setup_pt_cmd(&xpt_cmd, xop, dst_dev, &cdb[0],
 				remote_port);
 	if (rc < 0) {
