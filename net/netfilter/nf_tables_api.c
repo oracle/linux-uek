791c98dcb5a1ec4f5d160784f90becaa91b6b648
Pablo Neira Ayuso <pablo@netfilter.org>
2023-05-27 18:08:07 +0200

    netfilter: nf_tables: stricter validation of element data
    [ 7e6bc1f6cabcd30aba0b11219d8e01b952eacbb6 ]
    
    Make sure element data type and length do not mismatch the one specified
    by the set declaration.
    
    Fixes: 7d7402642eaf ("netfilter: nf_tables: variable sized set element keys / data")
    Reported-by: Hugues ANGUELKOV <hanguelkov@randorisec.fr>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit acd3e18fbcae6c5f9d79d75455569fc132227162)
    
    Orabug: 34362008
    CVE: CVE-2022-34918
    
    Signed-off-by: Rhythm Mahajan <rhythm.m.mahajan@oracle.com>
    Reviewed-by: Vijayendra Suman <vijayendra.suman@oracle.com>
    
     Conflicts:
    	net/netfilter/nf_tables_api.c
    The function nft_setelem_parse_data is not present in UEK 4 and was
    introduced in later versions of kernel due to refactoring of code. The
    code amended by the original fix is present in UEK 4, so necessary
    conflict resolution was done accordingly.
    
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -3306,6 +3306,7 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 	struct nft_trans *trans;
 	u64 timeout;
 	u32 flags;
+	u32 dtype;
 	u8 ulen;
 	int err;
 
@@ -3374,7 +3375,11 @@ static int nft_add_set_elem(struct nft_ctx *ctx, struct nft_set *set,
 			goto err2;
 
 		err = -EINVAL;
-		if (set->dtype != NFT_DATA_VERDICT && d2.len != set->dlen)
+		if (set->dtype == NFT_DATA_VERDICT)
+			dtype = NFT_DATA_VERDICT;
+		else
+			dtype = NFT_DATA_VALUE;
+		if (dtype != d2.type || d2.len != set->dlen)
 			goto err3;
 
 		dreg = nft_type_to_reg(set->dtype);
