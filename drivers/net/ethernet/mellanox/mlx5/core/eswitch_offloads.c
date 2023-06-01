a1745e8ce31f910405e54b137ff45d0c9b7ad716
Jiri Pirko <jiri@nvidia.com>
2023-06-01 14:16:41 +0200

    net/mlx5: Return -EOPNOTSUPP in mlx5_devlink_port_fn_migratable_set() directly
    Instead of initializing "err" variable, just return "-EOPNOTSUPP"
    directly where it is needed.
    
    Signed-off-by: Jiri Pirko <jiri@nvidia.com>
    Reviewed-by: Shay Drory <shayd@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 36385281
    
    (cherry picked from commit c0ae0092927222a1399465325ff778a78cc5f826)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: c0ae0092927222a1399465325ff778a78cc5f826
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch_offloads.c
@@ -4321,7 +4321,7 @@ int mlx5_devlink_port_fn_migratable_set(struct devlink_port *port, bool enable,
 	struct mlx5_vport *vport;
 	void *query_ctx;
 	void *hca_caps;
-	int err = -EOPNOTSUPP;
+	int err;
 
 	esw = mlx5_devlink_eswitch_get(port->devlink);
 	if (IS_ERR(esw))
@@ -4329,7 +4329,7 @@ int mlx5_devlink_port_fn_migratable_set(struct devlink_port *port, bool enable,
 
 	if (!MLX5_CAP_GEN(esw->dev, migration)) {
 		NL_SET_ERR_MSG_MOD(extack, "Device doesn't support migration");
-		return err;
+		return -EOPNOTSUPP;
 	}
 
 	vport = mlx5_devlink_port_fn_get_vport(port, esw);
