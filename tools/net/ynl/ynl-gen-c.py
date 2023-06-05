45b9fcbac2565d87149cda9984cbf7661ddeaefb
Jakub Kicinski <kuba@kernel.org>
2023-06-05 12:01:05 -0700

    tools: ynl-gen: clean up stray new lines at the end of reply-less requests
    Do not print empty lines before closing brackets.
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 36385281
    
    (cherry picked from commit a99bfdf647953f8ac4ae71f373113e8ac194f1c0)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: a99bfdf647953f8ac4ae71f373113e8ac194f1c0
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- tools/net/ynl/ynl-gen-c.py
+++ tools/net/ynl/ynl-gen-c.py
@@ -943,9 +943,10 @@ class CodeWriter:
     def _is_cond(cls, line):
         return line.startswith('if') or line.startswith('while') or line.startswith('for')
 
-    def p(self, line, add_ind=0):
+    def p(self, line, add_ind=0, eat_nl=False):
         if self._nl:
-            self._out.write('\n')
+            if not eat_nl:
+                self._out.write('\n')
             self._nl = False
         ind = self._ind
         if line[-1] == ':':
@@ -970,7 +971,7 @@ class CodeWriter:
         if line and line[0] not in {';', ','}:
             line = ' ' + line
         self._ind -= 1
-        self.p('}' + line)
+        self.p('}' + line, eat_nl=True)
 
     def write_doc_line(self, doc, indent=True):
         words = doc.split()
