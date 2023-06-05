3d46241235f435cfe2fb5a8a012d27803e81cd66
Mark Zhang <markzhang@nvidia.com>
2023-06-05 13:33:23 +0300

    RDMA/cma: Always set static rate to 0 for RoCE
    Set static rate to 0 as it should be discovered by path query and
    has no meaning for RoCE.
    This also avoid of using the rtnl lock and ethtool API, which is
    a bottleneck when try to setup many rdma-cm connections at the same
    time, especially with multiple processes.
    
    Fixes: 3c86aa70bf67 ("RDMA/cm: Add RDMA CM support for IBoE devices")
    Signed-off-by: Mark Zhang <markzhang@nvidia.com>
    Link: https://lore.kernel.org/r/f72a4f8b667b803aee9fa794069f61afb5839ce4.1685960567.git.leon@kernel.org
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    
    Orabug: 37100215
    
    (cherry picked from commit 58030c76cce473b6cfd630bbecb97215def0dff8)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	Minor manual changes were needed due to missing commit
    b5de0c60cc30 ("RDMA/cma: Fix use after free race in roce multicast join")
    in uek6.
    
    Signed-off-by: Praveen Kumar Kannoju <praveen.kannoju@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- drivers/infiniband/core/cma.c
+++ drivers/infiniband/core/cma.c
@@ -3214,7 +3214,7 @@ static int cma_resolve_iboe_route(struct rdma_id_private *id_priv)
 	route->path_rec->traffic_class = tos;
 	route->path_rec->mtu = iboe_get_mtu(ndev->mtu);
 	route->path_rec->rate_selector = IB_SA_EQ;
-	route->path_rec->rate = iboe_get_rate(ndev);
+	route->path_rec->rate = IB_RATE_PORT_CURRENT;
 	dev_put(ndev);
 	route->path_rec->packet_life_time_selector = IB_SA_EQ;
 	/* In case ACK timeout is set, use this value to calculate
@@ -4871,7 +4871,7 @@ static int cma_iboe_join_multicast(struct rdma_id_private *id_priv,
 		err = -ENODEV;
 		goto out2;
 	}
-	mc->multicast.ib->rec.rate = iboe_get_rate(ndev);
+	mc->multicast.ib->rec.rate = IB_RATE_PORT_CURRENT;
 	mc->multicast.ib->rec.hop_limit = 1;
 	mc->multicast.ib->rec.mtu = iboe_get_mtu(ndev->mtu);
 
