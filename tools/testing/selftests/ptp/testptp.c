3ba7a3a1a767058c21ceaa4d29aad2dd2c227ab4
Rahul Rameshbabu <rrameshbabu@nvidia.com>
2023-06-12 14:14:56 -0700

    ptp: Add .getmaxphase callback to ptp_clock_info
    Enables advertisement of the maximum offset supported by the phase control
    functionality of PHCs. The callback is used to return an error if an offset
    not supported by the PHC is used in ADJ_OFFSET. The ioctls
    PTP_CLOCK_GETCAPS and PTP_CLOCK_GETCAPS2 now advertise the maximum offset a
    PHC's phase control functionality is capable of supporting. Introduce new
    sysfs node, max_phase_adjustment.
    
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Richard Cochran <richardcochran@gmail.com>
    Cc: Maciek Machnikowski <maciek@machnikowski.net>
    Signed-off-by: Rahul Rameshbabu <rrameshbabu@nvidia.com>
    Acked-by: Richard Cochran <richardcochran@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Orabug: 35622106
    
    (cherry picked from commit c3b60ab7a4dff6e6e608e685b70ddc3d6b2aca81)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: c3b60ab7a4dff6e6e608e685b70ddc3d6b2aca81
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- tools/testing/selftests/ptp/testptp.c
+++ tools/testing/selftests/ptp/testptp.c
@@ -282,7 +282,8 @@ int main(int argc, char *argv[])
 			       "  %d pulse per second\n"
 			       "  %d programmable pins\n"
 			       "  %d cross timestamping\n"
-			       "  %d adjust_phase\n",
+			       "  %d adjust_phase\n"
+			       "  %d maximum phase adjustment (ns)\n",
 			       caps.max_adj,
 			       caps.n_alarm,
 			       caps.n_ext_ts,
@@ -290,7 +291,8 @@ int main(int argc, char *argv[])
 			       caps.pps,
 			       caps.n_pins,
 			       caps.cross_timestamping,
-			       caps.adjust_phase);
+			       caps.adjust_phase,
+			       caps.max_phase_adj);
 		}
 	}
 
