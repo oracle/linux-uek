ffe23efc65086b84de3c8fc66d05dc81afe12b80
Nikolay Borisov <nik.borisov@suse.com>
2023-06-23 14:14:07 +0300

    x86/elf: Make loading of 32bit processes depend on ia32_enabled()
    Major aspect of ia32 emulation is the ability to load 32bit processes.
    That's currently decided (among others) by compat_elf_check_arch().
    
    Make the macro use ia32_enabled() to decide if IA32 compat is
    enabled before loading a 32bit process.
    
    Signed-off-by: Nikolay Borisov <nik.borisov@suse.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20230623111409.3047467-5-nik.borisov@suse.com
    
    (cherry picked from commit 5ae2702d7c482edbf002499e23a2e22ac4047af1)
    [ KABI: Including arch/x86/include/asm/elf.h causes a KABI failure which
      is avoided by explicitly checking the __ia32_enabled variable rather than
      calling the ia32_enabled() helper in that #include file. ]
    Orabug: 36298741
    CVE: CVE-2024-25744
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- arch/x86/include/asm/elf.h
+++ arch/x86/include/asm/elf.h
@@ -159,8 +159,10 @@ do {						\
 #define elf_check_arch(x)			\
 	((x)->e_machine == EM_X86_64)
 
+/* Including asm/ia32.h causes KABI breakage so access variable directly */
+extern bool __ia32_enabled;
 #define compat_elf_check_arch(x)					\
-	(elf_check_arch_ia32(x) ||					\
+	((elf_check_arch_ia32(x) && __ia32_enabled) ||			\
 	 (IS_ENABLED(CONFIG_X86_X32_ABI) && (x)->e_machine == EM_X86_64))
 
 static inline void elf_common_init(struct thread_struct *t,
