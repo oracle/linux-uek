a5dfea8fd682de0baaf7b504ab0f83898ae20ca4
Florian Westphal <fw@strlen.de>
2023-07-05 18:54:17 +0200

    netfilter: nf_tables: use net_generic infra for transaction data
    [ 0854db2aaef3fcdd3498a9d299c60adea2aa3dc6 ]
    
    This moves all nf_tables pernet data from struct net to a net_generic
    extension, with the exception of the gencursor.
    
    The latter is used in the data path and also outside of the nf_tables
    core. All others are only used from the configuration plane.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 11352851944ca4e13c971aef1af2398f7fd68ba5)
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Notes:
    Use UEK_KABI_DEPRECATE to resolve kABI issue.

--- include/net/netns/nftables.h
+++ include/net/netns/nftables.h
@@ -3,15 +3,16 @@
 #define _NETNS_NFTABLES_H_
 
 #include <linux/list.h>
+#include <linux/uek_kabi.h>
 
 struct netns_nftables {
-	struct list_head	tables;
-	struct list_head	commit_list;
-	struct list_head	module_list;
-	struct mutex		commit_mutex;
-	unsigned int		base_seq;
+	UEK_KABI_DEPRECATE(struct list_head, tables)
+	UEK_KABI_DEPRECATE(struct list_head, commit_list)
+	UEK_KABI_DEPRECATE(struct list_head, module_list)
+	UEK_KABI_DEPRECATE(struct mutex, commit_mutex)
+	UEK_KABI_DEPRECATE(unsigned int, base_seq)
 	u8			gencursor;
-	u8			validate_state;
+	UEK_KABI_DEPRECATE(u8, validate_state)
 };
 
 #endif
