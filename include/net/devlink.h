85f6c9448728d8882c4d3d2acccda8f1d865ae46
Randy Dunlap <rdunlap@infradead.org>
2023-07-13 21:51:22 -0700

    devlink: fix kernel-doc notation warnings
    Spell function or struct member names correctly.
    Use ':' instead of '-' for struct member entries.
    Mark one field as private in kernel-doc.
    Add a few entries that were missing.
    Fix a typo.
    
    These changes prevent kernel-doc warnings:
    
    devlink.h:252: warning: Function parameter or member 'field_id' not described in 'devlink_dpipe_match'
    devlink.h:267: warning: Function parameter or member 'field_id' not described in 'devlink_dpipe_action'
    devlink.h:310: warning: Function parameter or member 'match_values_count' not described in 'devlink_dpipe_entry'
    devlink.h:355: warning: Function parameter or member 'list' not described in 'devlink_dpipe_table'
    devlink.h:374: warning: Function parameter or member 'actions_dump' not described in 'devlink_dpipe_table_ops'
    devlink.h:374: warning: Function parameter or member 'matches_dump' not described in 'devlink_dpipe_table_ops'
    devlink.h:374: warning: Function parameter or member 'entries_dump' not described in 'devlink_dpipe_table_ops'
    devlink.h:374: warning: Function parameter or member 'counters_set_update' not described in 'devlink_dpipe_table_ops'
    devlink.h:374: warning: Function parameter or member 'size_get' not described in 'devlink_dpipe_table_ops'
    devlink.h:384: warning: Function parameter or member 'headers' not described in 'devlink_dpipe_headers'
    devlink.h:384: warning: Function parameter or member 'headers_count' not described in 'devlink_dpipe_headers'
    devlink.h:398: warning: Function parameter or member 'unit' not described in 'devlink_resource_size_params'
    devlink.h:487: warning: Function parameter or member 'id' not described in 'devlink_param'
    devlink.h:645: warning: Function parameter or member 'overwrite_mask' not described in 'devlink_flash_update_params'
    
    Fixes: 1555d204e743 ("devlink: Support for pipeline debug (dpipe)")
    Fixes: d9f9b9a4d05f ("devlink: Add support for resource abstraction")
    Fixes: eabaef1896bc ("devlink: Add devlink_param register and unregister")
    Fixes: 5d5b4128c4ca ("devlink: introduce flash update overwrite mask")
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Jiri Pirko <jiri@resnulli.us>
    Cc: Moshe Shemesh <moshe@mellanox.com>
    Cc: Jacob Keller <jacob.e.keller@intel.com>
    Link: https://lore.kernel.org/r/20230714045127.18752-5-rdunlap@infradead.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 35622106
    
    (cherry picked from commit 839f55c5ebdfc2c0825bc7e711a7dedaca11f84e)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: 839f55c5ebdfc2c0825bc7e711a7dedaca11f84e
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -242,7 +242,7 @@ struct devlink_dpipe_field {
 /**
  * struct devlink_dpipe_header - dpipe header object
  * @name: header name
- * @id: index, global/local detrmined by global bit
+ * @id: index, global/local determined by global bit
  * @fields: fields
  * @fields_count: number of fields
  * @global: indicates if header is shared like most protocol header
@@ -262,7 +262,7 @@ struct devlink_dpipe_header {
  * @header_index: header index (packets can have several headers of same
  *		  type like in case of tunnels)
  * @header: header
- * @fieled_id: field index
+ * @field_id: field index
  */
 struct devlink_dpipe_match {
 	enum devlink_dpipe_match_type type;
@@ -277,7 +277,7 @@ struct devlink_dpipe_match {
  * @header_index: header index (packets can have several headers of same
  *		  type like in case of tunnels)
  * @header: header
- * @fieled_id: field index
+ * @field_id: field index
  */
 struct devlink_dpipe_action {
 	enum devlink_dpipe_action_type type;
@@ -313,7 +313,7 @@ struct devlink_dpipe_value {
  * struct devlink_dpipe_entry - table entry object
  * @index: index of the entry in the table
  * @match_values: match values
- * @matche_values_count: count of matches tuples
+ * @match_values_count: count of matches tuples
  * @action_values: actions values
  * @action_values_count: count of actions values
  * @counter: value of counter
@@ -363,7 +363,9 @@ struct devlink_dpipe_table_ops;
  */
 struct devlink_dpipe_table {
 	void *priv;
+	/* private: */
 	struct list_head list;
+	/* public: */
 	const char *name;
 	bool counters_enabled;
 	bool counter_control_extern;
@@ -376,13 +378,13 @@ struct devlink_dpipe_table {
 
 /**
  * struct devlink_dpipe_table_ops - dpipe_table ops
- * @actions_dump - dumps all tables actions
- * @matches_dump - dumps all tables matches
- * @entries_dump - dumps all active entries in the table
- * @counters_set_update - when changing the counter status hardware sync
+ * @actions_dump: dumps all tables actions
+ * @matches_dump: dumps all tables matches
+ * @entries_dump: dumps all active entries in the table
+ * @counters_set_update:  when changing the counter status hardware sync
  *			  maybe needed to allocate/free counter related
  *			  resources
- * @size_get - get size
+ * @size_get: get size
  */
 struct devlink_dpipe_table_ops {
 	int (*actions_dump)(void *priv, struct sk_buff *skb);
@@ -395,8 +397,8 @@ struct devlink_dpipe_table_ops {
 
 /**
  * struct devlink_dpipe_headers - dpipe headers
- * @headers - header array can be shared (global bit) or driver specific
- * @headers_count - count of headers
+ * @headers: header array can be shared (global bit) or driver specific
+ * @headers_count: count of headers
  */
 struct devlink_dpipe_headers {
 	struct devlink_dpipe_header **headers;
@@ -408,7 +410,7 @@ struct devlink_dpipe_headers {
  * @size_min: minimum size which can be set
  * @size_max: maximum size which can be set
  * @size_granularity: size granularity
- * @size_unit: resource's basic unit
+ * @unit: resource's basic unit
  */
 struct devlink_resource_size_params {
 	u64 size_min;
@@ -478,6 +480,7 @@ struct devlink_flash_notify {
 
 /**
  * struct devlink_param - devlink configuration parameter data
+ * @id: devlink parameter id number
  * @name: name of the parameter
  * @generic: indicates if the parameter is generic or driver specific
  * @type: parameter type
@@ -647,6 +650,7 @@ enum devlink_param_generic_id {
  * struct devlink_flash_update_params - Flash Update parameters
  * @fw: pointer to the firmware data to update from
  * @component: the flash component to update
+ * @overwrite_mask: which types of flash update are supported (may be %0)
  *
  * With the exception of fw, drivers must opt-in to parameters by
  * setting the appropriate bit in the supported_flash_update_params field in
