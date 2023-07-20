2a400134623cd75d5bb667490674db1c6d10a036
Jakub Kicinski <kuba@kernel.org>
2023-07-19 18:04:40 -0700

    eth: bnxt: handle invalid Tx completions more gracefully
    Invalid Tx completions should never happen (tm) but when they do
    they crash the host, because driver blindly trusts that there is
    a valid skb pointer on the ring.
    
    The completions I've seen appear to be some form of FW / HW
    miscalculation or staleness, they have typical (small) values
    (<100), but they are most often higher than number of queued
    descriptors. They usually happen after boot.
    
    Instead of crashing, print a warning and schedule a reset.
    
    Link: https://lore.kernel.org/r/20230720010440.1967136-4-kuba@kernel.org
    Reviewed-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 2b56b3d9924160d84c37d085628525666e3b1cea)
    
    Orabug: 36075753
    
    Signed-off-by: Praveen Kumar Kannoju <praveen.kannoju@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    
    Conflicts:
            drivers/net/ethernet/broadcom/bnxt/bnxt.c - Minor renaming and
    relocating code in the upstream commit 'fea2993a', unrelated to the fix.
    'bnxt_sched_reset_txr' has been relocated below 'bnxt_queue_sp_work' to avoid
    compilation issue as we are not incorporating the upstream commit 'fea2993a'.
    In addition because 'bnxt_queue_sp_work' uses single argument in uek6 and uek7,
    'set_bit' has been explicitly used to avoid incorporating another upstream
    commit '9b1a00fd'.
            drivers/net/ethernet/broadcom/bnxt/bnxt.h - Minor context change
    unrelated to this fix.
    
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -997,6 +997,7 @@ struct bnxt_napi {
 					  int);
 	int			tx_pkts;
 	u8			events;
+	u8			tx_fault:1;
 
 	u32			flags;
 #define BNXT_NAPI_FLAG_XDP	0x1
@@ -2264,6 +2265,8 @@ int bnxt_get_avail_msix(struct bnxt *bp, int num);
 int bnxt_reserve_rings(struct bnxt *bp, bool irq_re_init);
 void bnxt_tx_disable(struct bnxt *bp);
 void bnxt_tx_enable(struct bnxt *bp);
+void bnxt_sched_reset_txr(struct bnxt *bp, struct bnxt_tx_ring_info *txr,
+			  int idx);
 int bnxt_update_link(struct bnxt *bp, bool chng_link_state);
 int bnxt_hwrm_set_pause(struct bnxt *);
 int bnxt_hwrm_set_link_setting(struct bnxt *, bool, bool);
