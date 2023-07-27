249acb0432c154b35b414234a0fe039a6ba47279
Baskaran Kannan <Baski.Kannan@amd.com>
2023-07-27 11:21:59 -0500

    hwmon: (k10temp) Enable AMD3255 Proc to show negative temperature
    commit e146503ac68418859fb063a3a0cd9ec93bc52238 upstream.
    
    Industrial processor i3255 supports temperatures -40 deg celcius
    to 105 deg Celcius. The current implementation of k10temp_read_temp
    rounds off any negative temperatures to '0'. To fix this,
    the following changes have been made.
    
    A flag 'disp_negative' is added to struct k10temp_data to support
    AMD i3255 processors. Flag 'disp_negative' is set if 3255 processor
    is found during k10temp_probe.  Flag 'disp_negative' is used to
    determine whether to round off negative temperatures to '0' in
    k10temp_read_temp.
    
    Signed-off-by: Baskaran Kannan <Baski.Kannan@amd.com>
    Link: https://lore.kernel.org/r/20230727162159.1056136-1-Baski.Kannan@amd.com
    Fixes: aef17ca12719 ("hwmon: (k10temp) Only apply temperature offset if result is positive")
    Cc: stable@vger.kernel.org
    [groeck: Fixed multi-line comment]
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 3f3cdca84432eccba2447df20ddf0e8e95bafdc5)
    
    Conflict - our Zenbleed additions kept
    
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/hwmon/k10temp.c
+++ drivers/hwmon/k10temp.c
@@ -77,6 +77,33 @@ static DEFINE_MUTEX(nb_smu_ind_mutex);
 #define ZEN_CUR_TEMP_RANGE_SEL_MASK		BIT(19)
 #define ZEN_CUR_TEMP_TJ_SEL_MASK		GENMASK(17, 16)
 
+#define ZEN_SVI_BASE				0x0005A000
+
+/* F17h thermal registers through SMN */
+#define F17H_M01H_SVI_TEL_PLANE0		(ZEN_SVI_BASE + 0xc)
+#define F17H_M01H_SVI_TEL_PLANE1		(ZEN_SVI_BASE + 0x10)
+#define F17H_M31H_SVI_TEL_PLANE0		(ZEN_SVI_BASE + 0x14)
+#define F17H_M31H_SVI_TEL_PLANE1		(ZEN_SVI_BASE + 0x10)
+
+#define F17H_M01H_CFACTOR_ICORE			1000000	/* 1A / LSB	*/
+#define F17H_M01H_CFACTOR_ISOC			250000	/* 0.25A / LSB	*/
+#define F17H_M31H_CFACTOR_ICORE			1000000	/* 1A / LSB	*/
+#define F17H_M31H_CFACTOR_ISOC			310000	/* 0.31A / LSB	*/
+
+/* F19h thermal registers through SMN */
+#define F19H_M01_SVI_TEL_PLANE0			(ZEN_SVI_BASE + 0x14)
+#define F19H_M01_SVI_TEL_PLANE1			(ZEN_SVI_BASE + 0x10)
+
+#define F19H_M01H_CFACTOR_ICORE			1000000	/* 1A / LSB	*/
+#define F19H_M01H_CFACTOR_ISOC			310000	/* 0.31A / LSB	*/
+
+/*
+ * AMD's Industrial processor 3255 supports temperature from -40 deg to 105 deg Celsius.
+ * Use the model name to identify 3255 CPUs and set a flag to display negative temperature.
+ * Do not round off to zero for negative Tctl or Tdie values if the flag is set
+ */
+#define AMD_I3255_STR				"3255"
+
 struct k10temp_data {
 	struct pci_dev *pdev;
 	void (*read_htcreg)(struct pci_dev *pdev, u32 *regval);
@@ -86,6 +113,7 @@ struct k10temp_data {
 	u32 show_temp;
 	bool is_zen;
 	u32 ccd_offset;
+	bool disp_negative;
 };
 
 #define TCTL_BIT	0
@@ -204,12 +232,12 @@ static int k10temp_read_temp(struct device *dev, u32 attr, int channel,
 		switch (channel) {
 		case 0:		/* Tctl */
 			*val = get_raw_temp(data);
-			if (*val < 0)
+			if (*val < 0 && !data->disp_negative)
 				*val = 0;
 			break;
 		case 1:		/* Tdie */
 			*val = get_raw_temp(data) - data->temp_offset;
-			if (*val < 0)
+			if (*val < 0 && !data->disp_negative)
 				*val = 0;
 			break;
 		case 2 ... 13:		/* Tccd{1-12} */
@@ -404,6 +432,11 @@ static int k10temp_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 	data->pdev = pdev;
 	data->show_temp |= BIT(TCTL_BIT);	/* Always show Tctl */
+
+	if (boot_cpu_data.x86 == 0x17 &&
+	    strstr(boot_cpu_data.x86_model_id, AMD_I3255_STR)) {
+		data->disp_negative = true;
+	}
 
 	if (boot_cpu_data.x86 == 0x15 &&
 	    ((boot_cpu_data.x86_model & 0xf0) == 0x60 ||
