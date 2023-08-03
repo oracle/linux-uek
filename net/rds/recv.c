oracle/linux-uek

commit c888ff60c695626aeaa44fd437751a9b53a92a78

net/rds: Introduce RDS-INQ feature to RDS protocol
"In-Queue" lets application decide ahead of time if
the next call to recvmsg() should be made or not. The
application can call recvmsg() again if there are bytes
pending to be pulled out of the receive queue instead of
waiting for it to fail.

Implementing "In-Queue" capability to RDS. The implementation
is agnostic to transport protocols (TCP or IB). This introduces
SO_RDS_INQ and RDS_CMSG_INQ socket option and control message
type.

Few RDS applications rely on recvmsg() system call to receive messages
from peer. Usually the application call recvmsg() in a tight loop in
combination with poll() system call. Application iterates until an error'ed
recvmsg() is met to exit the loop. Thus the decisive system-call on
loop-termination is an extra system call. In order to reduce the number of
system-calls and improve system-load, a hint from kernel via control-messages
can help avoid the extra one. RDS-INQ feature supplies a hint on
pending-bytes in the receive queue via control message (RDA_CMSG_INQ)
embedded in recvmsg() system call. Thus loop can be terminated without
issuing an extra system-call. On a rds-socket this feature can be enabled
via setsockopt(...,SO_RDS_INQ) and its status can be queried using
getsockopt(...,SO_RDS_INQ).

To demonstrate the usage, rds-stress has been modified and tested
successfully. The table below shows the results and percentage improvement
on the number of system-call made. It also show the error'ed system call
made in the absence of this feature.

---------------------------------------------------------
|               |       recvmsg() without RDS-INQ       |
---------------------------------------------------------
|               |       Calls   |       Errors          |
---------------------------------------------------------
| RDS-TCP       |       274886 |        127362          |
---------------------------------------------------------

net/rds/recv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2006, 2023 Oracle and/or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -704,6 +704,17 @@ static int rds_cmsg_recv(struct rds_incoming *inc, struct msghdr *msg,
 
 		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_RXPATH_LATENCY,
 				sizeof(t), &t);
+		if (ret)
+			goto out;
+	}
+
+	if (rs->rs_inq) {
+		unsigned long flags;
+
+		read_lock_irqsave(&rs->rs_recv_lock, flags);
+		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_INQ,
+			       sizeof(rs->rs_rcv_bytes), &rs->rs_rcv_bytes);
+		read_unlock_irqrestore(&rs->rs_recv_lock, flags);
 		if (ret)
 			goto out;
 	}

include/uapi/linux/rds.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2008, 2023 Oracle and/or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -70,6 +70,11 @@
  */
 #define SO_RDS_MSG_RXPATH_LATENCY	10
 #define RDS6_CONN_RESET			11
+/* Socket option to enable notify via control
+ * message when more bytes are available to
+ * read.
+ */
+#define SO_RDS_INQ			12
 
 /* supported values for SO_RDS_TRANSPORT */
 #define	RDS_TRANS_IB	0
@@ -130,6 +135,9 @@ struct rds_cmsg_rx_trace {
  *	socket option. Legitimate points are defined in
  *	enum rds_message_rxpath_latency. More points can be added in
  *	future. CSMG format is struct rds_cmsg_rx_trace.
+ * RDS_CMSG_INQ
+ *	When enabled from socket options, this control message returns
+ *	the pending bytes yet to be read from this receive queue.
  */
 #define RDS_CMSG_RDMA_ARGS		1
 #define RDS_CMSG_RDMA_DEST		2
@@ -142,6 +150,7 @@ struct rds_cmsg_rx_trace {
 #define RDS_CMSG_MASKED_ATOMIC_CSWP	9
 #define RDS_CMSG_ASYNC_SEND		10
 #define RDS_CMSG_RXPATH_LATENCY		11
+#define RDS_CMSG_INQ			12
 
 #define RDS_INFO_FIRST			10000
 #define RDS_INFO_COUNTERS		10000

net/rds/af_rds.c
@@ -700,7 +700,6 @@ static int rds_recv_track_latency(struct rds_sock *rs, sockptr_t optval,
 	return 0;
 }
 
-
 static int rds_setsockopt(struct socket *sock, int level, int optname,
 			  sockptr_t optval, unsigned int optlen)
 {
@@ -761,6 +760,9 @@ static int rds_setsockopt(struct socket *sock, int level, int optname,
 	case SO_RDS_MSG_RXPATH_LATENCY:
 		ret = rds_recv_track_latency(rs, optval, optlen);
 		break;
+	case SO_RDS_INQ:
+		ret = rds_set_bool_option(&rs->rs_inq, optval, optlen);
+		break;
 	default:
 		ret = -ENOPROTOOPT;
 	}
@@ -809,6 +811,17 @@ static int rds_getsockopt(struct socket *sock, int level, int optname,
 		if (put_user(trans, (int __user *)optval) ||
 		    put_user(sizeof(int), optlen))
 			ret = -EFAULT;
+		else
+			ret = 0;
+		break;
+	case SO_RDS_INQ:
+		if (len < sizeof(int)) {
+			ret = -EINVAL;
+			break;
+		}
+		if (put_user(rs->rs_inq, (int __user *)optval) ||
+		    put_user(sizeof(int), optlen))
+			ret = -EINVAL;
 		else
 			ret = 0;
 		break;

net/rds/rds.h
@@ -926,6 +926,7 @@ struct rds_sock {
 	struct mutex		rs_trans_lock;
 	void			*rs_trans_private;
 	pid_t                   rs_pid;
+	unsigned char		rs_inq;
 };
 
 static inline struct rds_sock *rds_sk_to_rs(const struct sock *sk)

commit 92ed905ba9fd2983e6db212d2ecef9a8ca1470cf

net/rds: dont expose kernel address to usercopy
Direct access to rs->rs_recv_bytes when supplying to
put_cmsg() causes a kernel WARNING

[ 1107.832931] Bad or missing usercopy whitelist? Kernel memory exposure
attempt detected from SLUB object 'UDPv6' (offset 1272, size 4)!
[ 1107.845000] WARNING: CPU: 17 PID: 3524 at mm/usercopy.c:75
usercopy_warn+0x98/0xcb
[ 1107.852567] Modules linked in: rpcsec_gss...
[ 1107.950521] CPU: 17 PID: 3524 Comm: rds-stress Kdump: loaded Not tainted
5.15.0 #2
[ 1107.958078] Hardware name: Oracle Corporation SUN FIRE X4170 M3
/MOTHER BOARD ASSEMBL  , BIOS 17120100 11/09/2016
[ 1107.968667] RIP: 0010:usercopy_warn+0x98/0xcb
[ 1107.973018] Code: 89 41 51 49 89 c0 49 89 f1 49 0f 44 d2 49 c7 c2 e8 d1 de
89 48 89 f9 48 c7 c7 18 d1 de 89 4d 0f 44 d3 4c 89 d6 e8 2f f5 d1 ff <0f> 0b
48 83 c4 18 31 c0 89 c2 89 c1 89 c6 89 c7 41 89 c0 41 89 c1
[ 1107.991756] RSP: 0018:ffffa393a209fb18 EFLAGS: 00010046
[ 1107.996973] RAX: 0000000000000000 RBX: 0000000000000004 RCX:
0000000000000000
[ 1108.004096] RDX: 0000000000000000 RSI: 0000000000000000 RDI:
0000000000000000
[ 1108.011220] RBP: ffff971110e832fc R08: 0000000000000000 R09:
0000000000000000
[ 1108.018344] R10: 0000000000000000 R11: 0000000000000000 R12:
ffff971110e832f8
[ 1108.025468] R13: 0000000000000001 R14: 0000000000000004 R15:
0000000000000114
[ 1108.032594] FS:  00007fe6d3c70740(0000) GS:ffff972fffc40000(0000)
knlGS:0000000000000000
[ 1108.040669] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
[ 1108.046408] CR2: 00007ffd9dee25d8 CR3: 0000002101a00001 CR4:
00000000000606e0
[ 1108.053531] Call Trace:
[ 1108.055977]  <TASK>
[ 1108.058074]  ? show_trace_log_lvl+0x1d6/0x2f9
[ 1108.062424]  ? show_trace_log_lvl+0x1d6/0x2f9
[ 1108.066775]  ? __check_object_size.part.0+0x15b/0x22f
[ 1108.071818]  ? usercopy_warn+0x98/0xcb
[ 1108.075561]  ? __warn+0x80/0x113
[ 1108.078786]  ? usercopy_warn+0x98/0xcb
[ 1108.082529]  ? report_bug+0xa4/0x11d
[ 1108.086102]  ? handle_bug+0x35/0x8b
[ 1108.089584]  ? exc_invalid_op+0x14/0x75
[ 1108.093417]  ? asm_exc_invalid_op+0x16/0x1b
[ 1108.097593]  ? usercopy_warn+0x98/0xcb
[ 1108.101336]  ? usercopy_warn+0x98/0xcb
[ 1108.105082]  __check_object_size.part.0+0x15b/0x22f
[ 1108.109952]  put_cmsg+0x112/0x1f7
[ 1108.113263]  rds_cmsg_recv+0x1ff/0x270 [rds]
[ 1108.117554]  rds_recvmsg+0x27a/0x610 [rds]
[ 1108.121662]  ? refill_stock+0x3e/0x9f
[ 1108.125318]  ____sys_recvmsg+0x9f/0x1b8
[ 1108.129151]  ___sys_recvmsg+0xb5/0x149
[ 1108.132893]  __sys_recvmsg+0x7e/0xd2
[ 1108.136465]  do_syscall_64+0x3b/0x8d
[ 1108.140034]  entry_SYSCALL_64_after_hwframe+0x63/0x0
[ 1108.144992] RIP: 0033:0x7fe6d35ae6c8
[ 1108.148562] Code: ff eb b1 66 2e 0f 1f 84 00 00 00 00 00 0f 1f 44 00 00 f3
0f 1e fa 48 8d 05 75 50 29 00 8b 00 85 c0 75 17 b8 2f 00 00 00 0f 05 <48> 3d
00 f0 ff ff 77 58 c3 0f 1f 80 00 00 00 00 41 54 41 89 d4 55
[ 1108.167300] RSP: 002b:00007ffd9dee25d8 EFLAGS: 00000246 ORIG_RAX:
000000000000002f
[ 1108.174857] RAX: ffffffffffffffda RBX: 00007ffd9dee3890 RCX:
00007fe6d35ae6c8
[ 1108.181981] RDX: 0000000000000040 RSI: 00007ffd9dee3890 RDI:
0000000000000003
[ 1108.189104] RBP: 00007ffd9dee3b70 R08: 00000000000b8f11 R09:
0000000000000000
[ 1108.196228] R10: 0000000000000000 R11: 0000000000000246 R12:
0000000000000dc2
[ 1108.203354] R13: 0000000000000000 R14: 00007fe6d3c81400 R15:
00007ffd9dee3da0
[ 1108.210478]  </TASK>
[ 1108.212661] ---[ end trace d98a90e3c1ed88d1 ]---

Fixing the warning using a local variable.

Orabug: 35596878

Fixes: c888ff60c695 ("net/rds: Introduce RDS-INQ feature to RDS protocol")

net/rds/recv.c
@@ -715,10 +715,12 @@ static int rds_cmsg_recv(struct rds_incoming *inc, struct msghdr *msg,
 
 	if (rs->rs_inq) {
 		unsigned long flags;
+		int rcv_bytes;
 
 		read_lock_irqsave(&rs->rs_recv_lock, flags);
+		rcv_bytes = rs->rs_rcv_bytes;
 		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_INQ,
-			       sizeof(rs->rs_rcv_bytes), &rs->rs_rcv_bytes);
+			       sizeof(rcv_bytes), &rcv_bytes);
 		read_unlock_irqrestore(&rs->rs_recv_lock, flags);
 		if (ret)
 			goto out;