d6d2f65efbe3074e8baa84bfe312d1067906a50f
Brett Creeley <brett.creeley@amd.com>
2023-08-07 13:57:50 -0700

    pds_core: Require callers of register/unregister to pass PF drvdata
    Pass a pointer to the PF's private data structure rather than
    bouncing in and out of the PF's PCI function address.
    
    Signed-off-by: Shannon Nelson <shannon.nelson@amd.com>
    Signed-off-by: Brett Creeley <brett.creeley@amd.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20230807205755.29579-4-brett.creeley@amd.com
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    (cherry picked from commit b021d05e106e14b603a584b38ce62720e7d0f363)
    Orabug: 35424097
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Tested-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    Reviewed-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>

--- include/linux/pds/pds_common.h
+++ include/linux/pds/pds_common.h
@@ -60,9 +60,11 @@ enum pds_core_logical_qtype {
 	PDS_CORE_QTYPE_MAX     = 16   /* don't change - used in struct size */
 };
 
+struct pdsc;
+
 int pdsc_register_notify(struct notifier_block *nb);
 void pdsc_unregister_notify(struct notifier_block *nb);
 void *pdsc_get_pf_struct(struct pci_dev *vf_pdev);
-int pds_client_register(struct pci_dev *pf_pdev, char *devname);
-int pds_client_unregister(struct pci_dev *pf_pdev, u16 client_id);
+int pds_client_register(struct pdsc *pf, char *devname);
+int pds_client_unregister(struct pdsc *pf, u16 client_id);
 #endif /* _PDS_COMMON_H_ */
