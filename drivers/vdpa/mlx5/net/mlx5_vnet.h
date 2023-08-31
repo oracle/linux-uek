8cd3b1c76f637e53deca694760f00fc1b9aca9bd
Dragos Tatulea <dtatulea@nvidia.com>
2023-08-31 18:50:56 +0300

    vdpa/mlx5: Fix firmware error on creation of 1k VQs
    A firmware error is triggered when configuring a 9k MTU on the PF after
    switching to switchdev mode and then using a vdpa device with larger
    (1k) rings:
    mlx5_cmd_out_err: CREATE_GENERAL_OBJECT(0xa00) op_mod(0xd) failed, status bad resource(0x5), syndrome (0xf6db90), err(-22)
    
    This is due to the fact that the hw VQ size parameters are computed
    based on the umem_1/2/3_buffer_param_a/b capabilities and all
    device capabilities are read only when the driver is moved to switchdev mode.
    
    The problematic configuration flow looks like this:
    1) Create VF
    2) Unbind VF
    3) Switch PF to switchdev mode.
    4) Bind VF
    5) Set PF MTU to 9k
    6) create vDPA device
    7) Start VM with vDPA device and 1K queue size
    
    Note that setting the MTU before step 3) doesn't trigger this issue.
    
    This patch reads the forementioned umem parameters at the latest point
    possible before the VQs of the device are created.
    
    v2:
    - Allocate output with kmalloc to reduce stack frame size.
    - Removed stable from cc.
    
    Fixes: 1a86b377aa21 ("vdpa/mlx5: Add VDPA driver for supported mlx5 devices")
    Signed-off-by: Dragos Tatulea <dtatulea@nvidia.com>
    Message-Id: <20230831155702.1080754-1-dtatulea@nvidia.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit abb0dcf9938c93f765abf8cb45567cadef0af6b2)
    Orabug: 36072714
    Signed-off-by: Jonah Palmer <jonah.palmer@oracle.com>
    Reviewed-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/vdpa/mlx5/net/mlx5_vnet.h
+++ drivers/vdpa/mlx5/net/mlx5_vnet.h
@@ -50,6 +50,15 @@ struct mlx5_vdpa_net {
 	struct mlx5_vdpa_wq_ent cvq_ent;
 	struct hlist_head macvlan_hash[MLX5V_MACVLAN_SIZE];
 	struct dentry *debugfs;
+
+	u32 umem_1_buffer_param_a;
+	u32 umem_1_buffer_param_b;
+
+	u32 umem_2_buffer_param_a;
+	u32 umem_2_buffer_param_b;
+
+	u32 umem_3_buffer_param_a;
+	u32 umem_3_buffer_param_b;
 };
 
 struct mlx5_vdpa_counter {
