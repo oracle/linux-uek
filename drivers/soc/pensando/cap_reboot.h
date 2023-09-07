cad427ba10d17bf3c9d896eccdb105683a101b5c
David Clear <dclear@amd.com>
2023-09-07 14:34:35 -0700

    soc/pensando: pcie driver
    pcie driver provides these services on elba
    1) panic blink handler runs after panic, restart when host restarts
    2) sysfs panic_reboot node to restart immediately on panic
    3) /dev/penpcie controlled access to pcie clock domain registers
    
    Signed-off-by: David Clear <dclear@amd.com>
    
    Orabug: 36721455
    
    Signed-off-by: Joseph Dobosenski <joseph.dobosenski@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ drivers/soc/pensando/cap_reboot.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2020-2021, Pensando Systems Inc.
+ */
+
+#ifndef __CAP_REBOOT_H__
+#define __CAP_REBOOT_H__
+
+bool cap_panic_reboot(void);
+
+#endif
