cbf2d2bb56d611924140636341d9a66daa20bedb
David Clear <dclear@amd.com>
2023-09-06 18:35:43 -0700

    drivers/pensando/soc: Boot State Machine (BSM) integration.
    The Pensando BSM interacts with the boot firmware to provide a level
    of recovery should a Linux image fail to start.  If the BSM is
    armed and a kernel panic + reset occur, then the boot firmware
    will consider the loaded image as bad and will select the alternate
    image to boot next.
    
    Signed-off-by: David Clear <dclear@amd.com>
    
    Orabug: 36721455
    
    Signed-off-by: Joseph Dobosenski <joseph.dobosenski@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ drivers/soc/pensando/bsm_dev.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019-2021, Pensando Systems Inc.
+ */
+
+#ifndef _BSM_DEV_H_
+#define _BSM_DEV_H_
+
+#define BSM_WDT_LSB		10
+#define BSM_WDT_MASK		0x1
+#define BSM_FWID_LSB		8
+#define BSM_FWID_MASK		0x3
+#define BSM_ATTEMPT_LSB		6
+#define BSM_ATTEMPT_MASK	0x3
+#define BSM_TRACK_LSB		4
+#define BSM_TRACK_MASK		0x3
+#define BSM_STAGE_LSB		2
+#define BSM_STAGE_MASK		0x3
+#define BSM_RUNNING_LSB		1
+#define BSM_RUNNING_MASK	0x1
+#define BSM_AUTOBOOT_LSB	0
+#define BSM_AUTOBOOT_MASK	0x1
+
+#endif
