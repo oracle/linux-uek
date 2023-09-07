a3119c3300c39772463b3e53b3d021f7962e197a
David Clear <dclear@amd.com>
2023-09-06 17:03:38 -0700

    drivers/reset: Add emmc hardware reset
    Add multi-function driver with sub-device reset driver to
    execute emmc hardware reset.
    
    Signed-off-by: David Clear <dclear@amd.com>
    
    Orabug: 36721455
    
    Signed-off-by: Joseph Dobosenski <joseph.dobosenski@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ include/dt-bindings/reset/amd,pensando-elba-reset.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2022 Advanced Micro Devices, Inc.
+ */
+
+#ifndef _DT_BINDINGS_RESET_AMD_PENSANDO_ELBA_RESET_H
+#define _DT_BINDINGS_RESET_AMD_PENSANDO_ELBA_RESET_H
+
+#define EMMC_HW_RESET		0
+
+#endif
