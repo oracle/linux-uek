bef0b48d2700961ad1fcc0ba29d5d7b54bf26012
Maor Dickman <maord@nvidia.com>
2023-09-10 21:44:01 -0700

    net/mlx5: E-Switch, change VFs default admin state to auto in switchdev
    This change change VFs default admin state to auto in switchdev causing the VF
    link to follow the uplink link instead of the VF representor.
    
    Signed-off-by: Maor Dickman <maord@nvidia.com>
    
    Orabug: 33447083
    
    This is an out of tree patch to fix VF link propagation
    problem reported on LUCI. There is a requirement to propagate
    link up/down events to all the VFs of a given PF. The driver
    feature has been removed by vendor as regular driver update process.
    Thus, to fulfil the requirement, a change has been proposed by nic
    vendor where the representor link state is set to Up without confirming
    with the device firmware. The change set the vport link-state to auto.
    This change is been maintained as an out of tree patch.
    
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    
    Orabug: 33447083
    
    topic/v6.4 => LUCI
    
    (cherry picked from commmit 22add4599c4ceee4e522e3debe27619e2b818dd3)
    
    cherry-pick-repo=UEK/upstream-tracking.git
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
    	minor conflict when in applying last hunk where
    	link state is set to AUTO.
    
    The out of tree patch has been ported to latest LUCI
    linux kernel 6.5.0-rc7+
    
    Signed-off-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Qing Huang <qing.huang@oracle.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
@@ -675,8 +675,10 @@ void mlx5e_rep_deactivate_channels(struct mlx5e_priv *priv)
 static int mlx5e_rep_open(struct net_device *dev)
 {
 	struct mlx5e_priv *priv = netdev_priv(dev);
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 	struct mlx5e_rep_priv *rpriv = priv->ppriv;
 	struct mlx5_eswitch_rep *rep = rpriv->rep;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 	int err;
 
 	mutex_lock(&priv->state_lock);
@@ -684,11 +686,15 @@ static int mlx5e_rep_open(struct net_device *dev)
 	if (err)
 		goto unlock;
 
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 	if (!mlx5_modify_vport_admin_state(priv->mdev,
 					   MLX5_VPORT_STATE_OP_MOD_ESW_VPORT,
 					   rep->vport, 1,
 					   MLX5_VPORT_ADMIN_STATE_UP))
 		netif_carrier_on(dev);
+#else
+	netif_carrier_on(dev);
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 
 unlock:
 	mutex_unlock(&priv->state_lock);
@@ -698,15 +704,19 @@ unlock:
 static int mlx5e_rep_close(struct net_device *dev)
 {
 	struct mlx5e_priv *priv = netdev_priv(dev);
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 	struct mlx5e_rep_priv *rpriv = priv->ppriv;
 	struct mlx5_eswitch_rep *rep = rpriv->rep;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 	int ret;
 
 	mutex_lock(&priv->state_lock);
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 	mlx5_modify_vport_admin_state(priv->mdev,
 				      MLX5_VPORT_STATE_OP_MOD_ESW_VPORT,
 				      rep->vport, 1,
 				      MLX5_VPORT_ADMIN_STATE_DOWN);
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 	ret = mlx5e_close_locked(dev);
 	mutex_unlock(&priv->state_lock);
 	return ret;
@@ -775,22 +785,28 @@ static int mlx5e_rep_change_mtu(struct net_device *netdev, int new_mtu)
 
 static int mlx5e_rep_change_carrier(struct net_device *dev, bool new_carrier)
 {
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 	struct mlx5e_priv *priv = netdev_priv(dev);
 	struct mlx5e_rep_priv *rpriv = priv->ppriv;
 	struct mlx5_eswitch_rep *rep = rpriv->rep;
 	int err;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 
 	if (new_carrier) {
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 		err = mlx5_modify_vport_admin_state(priv->mdev, MLX5_VPORT_STATE_OP_MOD_ESW_VPORT,
 						    rep->vport, 1, MLX5_VPORT_ADMIN_STATE_UP);
 		if (err)
 			return err;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 		netif_carrier_on(dev);
 	} else {
+#ifdef WITHOUT_ORACLE_EXTENSIONS
 		err = mlx5_modify_vport_admin_state(priv->mdev, MLX5_VPORT_STATE_OP_MOD_ESW_VPORT,
 						    rep->vport, 1, MLX5_VPORT_ADMIN_STATE_DOWN);
 		if (err)
 			return err;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 		netif_carrier_off(dev);
 	}
 	return 0;
