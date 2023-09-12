abf981890237700d1db6c8eca0686693c53eff2c
David Clear <dclear@amd.com>
2023-09-12 15:32:00 -0700

    drivers/soc/pensando: boot_count to sysfs for kdump.log
    Add boot_count to sysfs. Augment the kdump timestamp with
    the boot_count number. Also, adjust the kdump timestamp.
    
    Signed-off-by: David Clear <dclear@amd.com>
    
    Orabug: 36721455
    
    Signed-off-by: Joseph Dobosenski <joseph.dobosenski@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/soc/pensando/cap_reboot.h
+++ drivers/soc/pensando/cap_reboot.h
@@ -1,11 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2020-2021, Pensando Systems Inc.
+ * Copyright (c) 2020-2022, Pensando Systems Inc.
  */
 
 #ifndef __CAP_REBOOT_H__
 #define __CAP_REBOOT_H__
 
 bool cap_panic_reboot(void);
+unsigned long cap_boot_count(void);
 
 #endif
