14a83313471f120fc00c2446f85949ca9d4a4e71
Or Har-Toov <ohartoov@nvidia.com>
2023-09-20 13:07:40 +0300

    IB/core: Add support for XDR link speed
    Add new IBTA speed XDR, the new rate that was added to Infiniband spec
    as part of XDR and supporting signaling rate of 200Gb.
    
    In order to report that value to rdma-core, add new u32 field to
    query_port response.
    
    Signed-off-by: Or Har-Toov <ohartoov@nvidia.com>
    Reviewed-by: Mark Zhang <markzhang@nvidia.com>
    Link: https://lore.kernel.org/r/9d235fc600a999e8274010f0e18b40fa60540e6c.1695204156.git.leon@kernel.org
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    
    Orabug: 36680931
    
    (cherry picked from commit 703289ce43f740b0096724300107df82d008552f)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    unmodified-from-upstream: 703289ce43f740b0096724300107df82d008552f
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -549,6 +549,7 @@ enum ib_port_speed {
 	IB_SPEED_EDR	= 32,
 	IB_SPEED_HDR	= 64,
 	IB_SPEED_NDR	= 128,
+	IB_SPEED_XDR	= 256,
 };
 
 enum ib_stat_flag {
@@ -828,6 +829,7 @@ enum ib_rate {
 	IB_RATE_50_GBPS  = 20,
 	IB_RATE_400_GBPS = 21,
 	IB_RATE_600_GBPS = 22,
+	IB_RATE_800_GBPS = 23,
 };
 
 /**
