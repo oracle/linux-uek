9242c3239566f7f9bc449e0448e41d0cda3d2326
Zhang Rui <rui.zhang@intel.com>
2023-09-25 20:04:17 +0800

    iommu/vt-d: Avoid memory allocation in iommu_suspend()
    [ Upstream commit 59df44bfb0ca4c3ee1f1c3c5d0ee8e314844799e ]
    
    The iommu_suspend() syscore suspend callback is invoked with IRQ disabled.
    Allocating memory with the GFP_KERNEL flag may re-enable IRQs during
    the suspend callback, which can cause intermittent suspend/hibernation
    problems with the following kernel traces:
    
    Calling iommu_suspend+0x0/0x1d0
    ------------[ cut here ]------------
    WARNING: CPU: 0 PID: 15 at kernel/time/timekeeping.c:868 ktime_get+0x9b/0xb0
    ...
    CPU: 0 PID: 15 Comm: rcu_preempt Tainted: G     U      E      6.3-intel #r1
    RIP: 0010:ktime_get+0x9b/0xb0
    ...
    Call Trace:
     <IRQ>
     tick_sched_timer+0x22/0x90
     ? __pfx_tick_sched_timer+0x10/0x10
     __hrtimer_run_queues+0x111/0x2b0
     hrtimer_interrupt+0xfa/0x230
     __sysvec_apic_timer_interrupt+0x63/0x140
     sysvec_apic_timer_interrupt+0x7b/0xa0
     </IRQ>
     <TASK>
     asm_sysvec_apic_timer_interrupt+0x1f/0x30
    ...
    ------------[ cut here ]------------
    Interrupts enabled after iommu_suspend+0x0/0x1d0
    WARNING: CPU: 0 PID: 27420 at drivers/base/syscore.c:68 syscore_suspend+0x147/0x270
    CPU: 0 PID: 27420 Comm: rtcwake Tainted: G     U  W   E      6.3-intel #r1
    RIP: 0010:syscore_suspend+0x147/0x270
    ...
    Call Trace:
     <TASK>
     hibernation_snapshot+0x25b/0x670
     hibernate+0xcd/0x390
     state_store+0xcf/0xe0
     kobj_attr_store+0x13/0x30
     sysfs_kf_write+0x3f/0x50
     kernfs_fop_write_iter+0x128/0x200
     vfs_write+0x1fd/0x3c0
     ksys_write+0x6f/0xf0
     __x64_sys_write+0x1d/0x30
     do_syscall_64+0x3b/0x90
     entry_SYSCALL_64_after_hwframe+0x72/0xdc
    
    Given that only 4 words memory is needed, avoid the memory allocation in
    iommu_suspend().
    
    CC: stable@kernel.org
    Fixes: 33e07157105e ("iommu/vt-d: Avoid GFP_ATOMIC where it is not needed")
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Tested-by: Ooi, Chin Hao <chin.hao.ooi@intel.com>
    Link: https://lore.kernel.org/r/20230921093956.234692-1-rui.zhang@intel.com
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Link: https://lore.kernel.org/r/20230925120417.55977-2-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 29298c85a81abdc512e87537515ed4b1a9601d0e)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3584,13 +3584,6 @@ static int iommu_suspend(void)
 	struct intel_iommu *iommu = NULL;
 	unsigned long flag;
 
-	for_each_active_iommu(iommu, drhd) {
-		iommu->iommu_state = kcalloc(MAX_SR_DMAR_REGS, sizeof(u32),
-					     GFP_KERNEL);
-		if (!iommu->iommu_state)
-			goto nomem;
-	}
-
 	iommu_flush_all();
 
 	for_each_active_iommu(iommu, drhd) {
@@ -3610,12 +3603,6 @@ static int iommu_suspend(void)
 		raw_spin_unlock_irqrestore(&iommu->register_lock, flag);
 	}
 	return 0;
-
-nomem:
-	for_each_active_iommu(iommu, drhd)
-		kfree(iommu->iommu_state);
-
-	return -ENOMEM;
 }
 
 static void iommu_resume(void)
@@ -3647,9 +3634,6 @@ static void iommu_resume(void)
 
 		raw_spin_unlock_irqrestore(&iommu->register_lock, flag);
 	}
-
-	for_each_active_iommu(iommu, drhd)
-		kfree(iommu->iommu_state);
 }
 
 static struct syscore_ops iommu_syscore_ops = {
