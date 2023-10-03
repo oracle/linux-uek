330bc638cd4372287a1a133b087b025bac3ad478
Leon Romanovsky <leonro@nvidia.com>
2023-10-03 20:57:20 +0300

    xfrm: generalize xdo_dev_state_update_curlft to allow statistics update
    In order to allow drivers to fill all statistics, change the name
    of xdo_dev_state_update_curlft to be xdo_dev_state_update_stats.
    
    Acked-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Orabug: 37357287
    
    (cherry picked from commit fd2bc4195d5107f88c1b90e1ec935888ccbfc5c0)
    cherry-pick-repo=kernel/git/torvalds/linux.git
    
    Conflicts:
    	include/linux/netdevice.h
    
    Update the UEK_KABI_USE solution by renaming xdo_dev_state_update_curlft
    in struct xfrmdev_ops to xfrm_dev_state_update_stats as needed.
    
    Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1090,7 +1090,7 @@ struct xfrmdev_ops {
 	UEK_KABI_USE(1, int	(*xdo_dev_policy_add) (struct xfrm_policy *x, struct netlink_ext_ack *extack))
 	UEK_KABI_USE(2, void	(*xdo_dev_policy_delete) (struct xfrm_policy *x))
 	UEK_KABI_USE(3, void	(*xdo_dev_policy_free) (struct xfrm_policy *x))
-	UEK_KABI_USE(4, void	(*xdo_dev_state_update_curlft) (struct xfrm_state *x))
+	UEK_KABI_USE(4, void	(*xdo_dev_state_update_stats) (struct xfrm_state *x))
 	UEK_KABI_USE(5, int	(*xdo_dev_state_add_new) (struct xfrm_state *x, struct netlink_ext_ack *extack))
 	UEK_KABI_RESERVE(6)
 	UEK_KABI_RESERVE(7)
