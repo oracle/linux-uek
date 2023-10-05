e0f45254a195f6e0394aebd9431cb52569074f09
Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
2023-10-05 09:49:05 -0700

    fs/nls: make load_nls() take a const parameter
    [ Upstream commit c1ed39ec116272935528ca9b348b8ee79b0791da ]
    
    load_nls() take a char * parameter, use it to find nls module in list or
    construct the module name to load it.
    
    This change make load_nls() take a const parameter, so we don't need do
    some cast like this:
    
            ses->local_nls = load_nls((char *)ctx->local_nls->charset);
    
    Suggested-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Winston Wen <wentao@uniontech.com>
    Reviewed-by: Paulo Alcantara <pc@manguebit.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 544333d62418ff4bf7283a2221ec726489a18c4f)
    Fixed kABI breakage since argument for load_nls() changed from
    'char *' to 'const char *'
    	modified:   include/linux/nls.h
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- include/linux/nls.h
+++ include/linux/nls.h
@@ -47,7 +47,11 @@ enum utf16_endian {
 /* nls_base.c */
 extern int __register_nls(struct nls_table *, struct module *);
 extern int unregister_nls(struct nls_table *);
+#ifdef __GENKSYMS__
 extern struct nls_table *load_nls(char *);
+#else
+extern struct nls_table *load_nls(const char *charset);
+#endif
 extern void unload_nls(struct nls_table *);
 extern struct nls_table *load_nls_default(void);
 #define register_nls(nls) __register_nls((nls), THIS_MODULE)
