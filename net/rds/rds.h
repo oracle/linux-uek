oracle/linux-uek

commit 561a2cbc50d627190eb0cf847760b6a802258a0c

net/rds: Introduce RDS-INQ feature to RDS protocol
"In-Queue" lets application decide ahead of time if
the next call to recvmsg() should be made or not. The
application can call recvmsg() again if there are bytes
pending to be pulled out of the receive queue instead of
waiting for it to fail.

Implementing "In-Queue" capability to RDS. The implementation
is agnostic to transport protocols (TCP or IB). This introduces
SO_RDS_INQ and RDS_CMSG_INQ socket option and control message
type.

Few RDS applications rely on recvmsg() system call to receive messages
from peer. Usually the application call recvmsg() in a tight loop in
combination with poll() system call. Application iterates until an error'ed
recvmsg() is met to exit the loop. Thus the decisive system-call on
loop-termination is an extra system call. In order to reduce the number of
system-calls and improve system-load, a hint from kernel via control-messages
can help avoid the extra one. RDS-INQ feature supplies a hint on
pending-bytes in the receive queue via control message (RDA_CMSG_INQ)
embedded in recvmsg() system call. Thus loop can be terminated without
issuing an extra system-call. On a rds-socket this feature can be enabled
via setsockopt(...,SO_RDS_INQ) and its status can be queried using
getsockopt(...,SO_RDS_INQ).

To demonstrate the usage, rds-stress has been modified and tested
successfully. The table below shows the results and percentage improvement
on the number of system-call made. It also show the error'ed system call
made in the absence of this feature.

---------------------------------------------------------
|               |       recvmsg() without RDS-INQ       |
---------------------------------------------------------
|               |       Calls   |       Errors          |
---------------------------------------------------------
| RDS-TCP       |       274886 |        127362          |
---------------------------------------------------------

net/rds/rds.h
@@ -926,6 +926,7 @@ struct rds_sock {
 	struct mutex		rs_trans_lock;
 	void			*rs_trans_private;
 	pid_t                   rs_pid;
+	unsigned char		rs_inq;
 };
 
 static inline struct rds_sock *rds_sk_to_rs(const struct sock *sk)

net/rds/recv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2006, 2023 Oracle and/or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -704,6 +704,17 @@ static int rds_cmsg_recv(struct rds_incoming *inc, struct msghdr *msg,
 
 		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_RXPATH_LATENCY,
 				sizeof(t), &t);
+		if (ret)
+			goto out;
+	}
+
+	if (rs->rs_inq) {
+		unsigned long flags;
+
+		read_lock_irqsave(&rs->rs_recv_lock, flags);
+		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_INQ,
+			       sizeof(rs->rs_rcv_bytes), &rs->rs_rcv_bytes);
+		read_unlock_irqrestore(&rs->rs_recv_lock, flags);
 		if (ret)
 			goto out;
 	}

include/uapi/linux/rds.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2021 Oracle and/or its affiliates.
+ * Copyright (c) 2008, 2023 Oracle and/or its affiliates.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -70,6 +70,11 @@
  */
 #define SO_RDS_MSG_RXPATH_LATENCY	10
 #define RDS6_CONN_RESET			11
+/* Socket option to enable notify via control
+ * message when more bytes are available to
+ * read.
+ */
+#define SO_RDS_INQ			12
 
 /* supported values for SO_RDS_TRANSPORT */
 #define	RDS_TRANS_IB	0
@@ -130,6 +135,9 @@ struct rds_cmsg_rx_trace {
  *	socket option. Legitimate points are defined in
  *	enum rds_message_rxpath_latency. More points can be added in
  *	future. CSMG format is struct rds_cmsg_rx_trace.
+ * RDS_CMSG_INQ
+ *	When enabled from socket options, this control message returns
+ *	the pending bytes yet to be read from this receive queue.
  */
 #define RDS_CMSG_RDMA_ARGS		1
 #define RDS_CMSG_RDMA_DEST		2
@@ -142,6 +150,7 @@ struct rds_cmsg_rx_trace {
 #define RDS_CMSG_MASKED_ATOMIC_CSWP	9
 #define RDS_CMSG_ASYNC_SEND		10
 #define RDS_CMSG_RXPATH_LATENCY		11
+#define RDS_CMSG_INQ			12
 
 #define RDS_INFO_FIRST			10000
 #define RDS_INFO_COUNTERS		10000

net/rds/af_rds.c
@@ -700,7 +700,6 @@ static int rds_recv_track_latency(struct rds_sock *rs, sockptr_t optval,
 	return 0;
 }
 
-
 static int rds_setsockopt(struct socket *sock, int level, int optname,
 			  sockptr_t optval, unsigned int optlen)
 {
@@ -761,6 +760,9 @@ static int rds_setsockopt(struct socket *sock, int level, int optname,
 	case SO_RDS_MSG_RXPATH_LATENCY:
 		ret = rds_recv_track_latency(rs, optval, optlen);
 		break;
+	case SO_RDS_INQ:
+		ret = rds_set_bool_option(&rs->rs_inq, optval, optlen);
+		break;
 	default:
 		ret = -ENOPROTOOPT;
 	}
@@ -809,6 +811,17 @@ static int rds_getsockopt(struct socket *sock, int level, int optname,
 		if (put_user(trans, (int __user *)optval) ||
 		    put_user(sizeof(int), optlen))
 			ret = -EFAULT;
+		else
+			ret = 0;
+		break;
+	case SO_RDS_INQ:
+		if (len < sizeof(int)) {
+			ret = -EINVAL;
+			break;
+		}
+		if (put_user(rs->rs_inq, (int __user *)optval) ||
+		    put_user(sizeof(int), optlen))
+			ret = -EINVAL;
 		else
 			ret = 0;
 		break;

commit b8489f3a95a4cfdcd04c3adcbd98a0906deda755

net/rds: report pending-messages count in RDS_INQ response
RDS_CMSG_INQ response reports number of pending-bytes in the receive queue.
Exadata is using the RDS interface for zero-byte messages. As such, it is
preferred to change the semantics of RDS_INQ to number of messages,
instead of number of bytes.

Making a change to actively maintain a count of receive queue entries yet to
be processed. Whenever application tries to seek the pending receive queue
entries via recvmsg(), this count is read and returned as a response.

This counter is increamented when a new entry is queued to the rs_recv_queue
in rds_recv_incoming() while it is decremented as soon as any message is
pulled out of the receive queue via rds_still_queued().

Orabug: 35596047

Fixes: 561a2cbc50d6 ("Introduce RDS-INQ feature to RDS protocol”)
Signed-off-by: Devesh Sharma <devesh.s.sharma@oracle.com>
Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>

net/rds/rds.h
@@ -903,6 +903,11 @@ struct rds_sock {
 	rwlock_t		rs_recv_lock;
 	int			rs_rcv_bytes;
 	struct list_head	rs_recv_queue;
+	/*
+	 * rs_recv_pending actively counts the yet-to-be-processed
+	 * entried in the rs_recv_queue queue
+	 */
+	int			rs_recv_pending;
 
 	/* just for stats reporting */
 	struct list_head	rs_item;

net/rds/recv.c
@@ -494,6 +494,8 @@ void rds_recv_incoming(struct rds_connection *conn, struct in6_addr *saddr,
 			}
 			rds_inc_addref(inc);
 			list_add_tail(&inc->i_item, &rs->rs_recv_queue);
+			/* New entry to receive queue, increment pending */
+			rs->rs_recv_pending++;
 			inc->i_rx_lat_trace[RDS_MSG_RX_END] = local_clock();
 			__rds_wake_sk_sleep(sk);
 		}
@@ -530,6 +532,8 @@ static int rds_next_incoming(struct rds_sock *rs, struct rds_incoming **inc)
 					  struct rds_incoming,
 					  i_item);
 			rds_inc_addref(*inc);
+			/* Pulled a pending entry, decrement the count */
+			rs->rs_recv_pending--;
 		}
 		read_unlock_irqrestore(&rs->rs_recv_lock, flags);
 	}
@@ -710,13 +714,13 @@ static int rds_cmsg_recv(struct rds_incoming *inc, struct msghdr *msg,
 
 	if (rs->rs_inq) {
 		unsigned long flags;
-		int rcv_bytes;
+		int r_count;
 
 		read_lock_irqsave(&rs->rs_recv_lock, flags);
-		rcv_bytes = rs->rs_rcv_bytes;
-		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_INQ,
-			       sizeof(rcv_bytes), &rcv_bytes);
+		r_count = rs->rs_recv_pending;
 		read_unlock_irqrestore(&rs->rs_recv_lock, flags);
+		ret = put_cmsg(msg, SOL_RDS, RDS_CMSG_INQ,
+			       sizeof(r_count), &r_count);
 		if (ret)
 			goto out;
 	}
@@ -863,6 +867,8 @@ void rds_clear_recv_queue(struct rds_sock *rs)
 				      -be32_to_cpu(inc->i_hdr.h_len),
 				      inc->i_hdr.h_dport);
 		list_del_init(&inc->i_item);
+		/* Entry deleted, decrement the counter */
+		rs->rs_recv_pending--;
 		rds_inc_put(inc);
 	}
 	write_unlock_irqrestore(&rs->rs_recv_lock, flags);