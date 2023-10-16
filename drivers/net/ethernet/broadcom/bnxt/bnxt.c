2b772b23b4464449abedcdb2575c085bd39b96b4
Jakub Kicinski <kuba@kernel.org>
2023-10-16 10:16:40 -0700

    eth: bnxt: fix backward compatibility with older devices
    Recent FW interface update bumped the size of struct hwrm_func_cfg_input
    above 128B which is the max some devices support.
    
    Probe on Stratus (BCM957452) with FW 20.8.3.11 fails with:
    
       bnxt_en ...: Unable to reserve tx rings
       bnxt_en ...: 2nd rings reservation failed.
       bnxt_en ...: Not enough rings available.
    
    Once probe is fixed other errors pop up:
    
       bnxt_en ...: Failed to set async event completion ring.
    
    This is because __hwrm_send() rejects requests larger than
    bp->hwrm_max_ext_req_len with -E2BIG. Since the driver doesn't
    actually access any of the new fields, yet, trim the length.
    It should be safe.
    
    Similar workaround exists for backing_store_cfg_input.
    Although that one mins() to a constant of 256, not 128
    we'll effectively use here. Michael explains: "the backing
    store cfg command is supported by relatively newer firmware
    that will accept 256 bytes at least."
    
    To make debugging easier in the future add a warning
    for oversized requests.
    
    Fixes: 754fbf604ff6 ("bnxt_en: Update firmware interface to 1.10.2.171")
    Reviewed-by: Michael Chan <michael.chan@broadcom.com>
    Link: https://lore.kernel.org/r/20231016171640.1481493-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 73b24e7ce8f1f47e2b73c9b6724188898f4ca6fd)
    
    Orabug: 36826374
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -5876,7 +5876,7 @@ static int bnxt_hwrm_set_async_event_cr(struct bnxt *bp, int idx)
 	if (BNXT_PF(bp)) {
 		struct hwrm_func_cfg_input *req;
 
-		rc = hwrm_req_init(bp, req, HWRM_FUNC_CFG);
+		rc = bnxt_hwrm_func_cfg_short_req_init(bp, &req);
 		if (rc)
 			return rc;
 
@@ -6287,7 +6287,7 @@ __bnxt_hwrm_reserve_pf_rings(struct bnxt *bp, int tx_rings, int rx_rings,
 	struct hwrm_func_cfg_input *req;
 	u32 enables = 0;
 
-	if (hwrm_req_init(bp, req, HWRM_FUNC_CFG))
+	if (bnxt_hwrm_func_cfg_short_req_init(bp, &req))
 		return NULL;
 
 	req->fid = cpu_to_le16(0xffff);
@@ -8634,7 +8634,7 @@ static int bnxt_hwrm_set_br_mode(struct bnxt *bp, u16 br_mode)
 	else
 		return -EINVAL;
 
-	rc = hwrm_req_init(bp, req, HWRM_FUNC_CFG);
+	rc = bnxt_hwrm_func_cfg_short_req_init(bp, &req);
 	if (rc)
 		return rc;
 
@@ -8652,7 +8652,7 @@ static int bnxt_hwrm_set_cache_line_size(struct bnxt *bp, int size)
 	if (BNXT_VF(bp) || bp->hwrm_spec_code < 0x10803)
 		return 0;
 
-	rc = hwrm_req_init(bp, req, HWRM_FUNC_CFG);
+	rc = bnxt_hwrm_func_cfg_short_req_init(bp, &req);
 	if (rc)
 		return rc;
 
