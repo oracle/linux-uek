7a2c35d8c08d8f304ea38ae11212da2fca3257a0
Ray Asbury <rasbury@marvell.com>
2023-10-24 17:39:00 -0600

    Change struct octeontx2_pcie_console_nexus
    Change the structure octeontx2_pcie_console_nexus to
    prevent a BUILD_BUG_ON issue due to an unused spinlock_t
    when the CONFIG_DEBUG_SPINLOCK is enabled.
    
    Signed-off-by: Ray Asbury <rasbury@marvell.com>
    Change-Id: I81128f4a859eda89da7b565c126d3054cbfef6ca
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/114527
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/soc/marvell/octeontx2-pcicons/otx2-pci-console.h
+++ drivers/soc/marvell/octeontx2-pcicons/otx2-pci-console.h
@@ -54,7 +54,7 @@ struct octeontx_pcie_console_nexus {
 	u8                    minor_version;
 	u8                    flags;
 	u8                    num_consoles;
-	spinlock_t            excl_lock;
+	__le32                unused_excl_lock;
 #ifdef CONFIG_PREEMPT_RT
 	u32                   pad_excl_lock;
 #else
