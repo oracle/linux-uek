80c1276ae0121f30e0615e4195244e49957ae8bd
Markus Theil <theil.markus@gmail.com>
2023-10-31 19:52:22 +0100

    mstflint_access: Update driver code to v4.26.0-1 from Github
    Orabug: 38074279
    
    (cherry picked from commit be99828c2a8af46426c003a5ee8daa44f8831ade)
    cherry-pick-repo=github.com/Mellanox/mstflint.git
    unmodified-from-upstream: be99828c2a8af46426c003a5ee8daa44f8831ade
    (cherry picked from commit d122c2df1ab9c87165b1b291fa61e3f06137699e)
    cherry-pick-repo=github.com/Mellanox/mstflint.git
    unmodified-from-upstream: d122c2df1ab9c87165b1b291fa61e3f06137699e
    
    We upgraded the mstflint_access driver code from v4.25.0-1 to v4.26.0-1.
    
    The kernel module source was fetched from:
        https://github.com/Mellanox/mstflint.git tag v4.26.0-1 in the "kernel"
        sub-directory.
    
    Both commits were cherry-picked cleanly.
    
    Signed-off-by: Mark Haywood <mark.haywood@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/net/ethernet/mellanox/mstflint_access/mst_main.c
+++ drivers/net/ethernet/mellanox/mstflint_access/mst_main.c
@@ -1650,7 +1650,11 @@ static struct mst_dev_data* mst_device_create(enum dev_type type, struct pci_dev
 
     if (alloc_chrdev_region(&dev->my_dev, 0, 1, dev->name))
         mst_err("failed to allocate chrdev_region\n");
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 4, 0)
+    dev->cl = class_create(dev->name);
+#else
     dev->cl = class_create(THIS_MODULE, dev->name);
+#endif
     if (dev->cl == NULL)
     {
         pr_alert("Class creation failed\n");
