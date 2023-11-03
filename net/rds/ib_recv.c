2570f28495591bf201ce4f77fac54ec13dba8949
Greg Jumper <greg.jumper@oracle.com>
2023-11-03 14:53:50 -0700

    Revert "net/rds: Track "rds" module statistics per network namespace"
    This reverts commit 68b8a1f112495b8d6aea6c57280d40fa91914324.
    
    Orabug: 35811116
    
    Signed-off-by: Greg Jumper <greg.jumper@oracle.com>
    
    Orabug: 35811116
    
    LUCI => v6.6-rc7
    
    Conflicts:
      - Resolved conflict at (*inc_copy_to_user)() line in net/rds/rds.h due to
        following new context line introducing (*recv_need_bufs)().
    
    Signed-off-by: Greg Jumper <greg.jumper@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- net/rds/ib_recv.c
+++ net/rds/ib_recv.c
@@ -765,8 +765,7 @@ static struct lfstack_el *rds_ib_recv_cache_get(struct rds_ib_refill_cache *cach
 	return item;
 }
 
-int rds_ib_inc_copy_to_user(struct rds_sock *rs, struct rds_incoming *inc,
-			    struct iov_iter *to)
+int rds_ib_inc_copy_to_user(struct rds_incoming *inc, struct iov_iter *to)
 {
 	struct rds_csum csum;
 	struct rds_ib_connection *ic = inc->i_conn->c_transport_data;
@@ -803,7 +802,7 @@ int rds_ib_inc_copy_to_user(struct rds_sock *rs, struct rds_incoming *inc,
 		if (ret != to_copy)
 			return -EFAULT;
 
-		rds_stats_add(rs->rs_stats, s_copy_to_user, to_copy);
+		rds_stats_add(s_copy_to_user, to_copy);
 		atomic64_add(to_copy, &conn->c_recv_bytes);
 		frag_off += to_copy;
 		copied += to_copy;
@@ -829,7 +828,7 @@ int rds_ib_inc_copy_to_user(struct rds_sock *rs, struct rds_incoming *inc,
 	}
 
 	if (unlikely(inc->i_payload_csum.csum_enabled) && copied) {
-		rds_stats_inc(rs->rs_stats, s_recv_payload_csum_ib);
+		rds_stats_inc(s_recv_payload_csum_ib);
 		rds_check_csum(inc, &csum);
 	}
 
@@ -1129,7 +1128,7 @@ static void rds_ib_cong_recv(struct rds_connection *conn,
 	trace_rds_receive(&ibinc->ii_inc, NULL, conn, NULL,
 			  &conn->c_faddr, &conn->c_laddr);
 
-	rds_cong_map_updated(conn, map, uncongested);
+	rds_cong_map_updated(map, uncongested);
 }
 
 static void rds_ib_process_recv(struct rds_connection *conn,
@@ -1168,7 +1167,7 @@ static void rds_ib_process_recv(struct rds_connection *conn,
 		rds_conn_drop(conn, DR_IB_HEADER_CORRUPTED, 0);
 		pr_warn("RDS/IB: incoming message from %pI6c has corrupted header - forcing a reconnect\n",
 			&conn->c_faddr);
-		rds_stats_inc(conn->c_stats, s_recv_drop_bad_checksum);
+		rds_stats_inc(s_recv_drop_bad_checksum);
 		return;
 	}
 
@@ -1269,7 +1268,7 @@ static void rds_ib_process_recv(struct rds_connection *conn,
 		 * the complete frame, and after bumping the next_rx
 		 * sequence. */
 		if (hdr->h_flags & RDS_FLAG_ACK_REQUIRED) {
-			rds_stats_inc(conn->c_stats, s_recv_ack_required);
+			rds_stats_inc(s_recv_ack_required);
 			state->ack_required = 1;
 		}
 
@@ -1309,7 +1308,7 @@ void rds_ib_srq_process_recv(struct rds_connection *conn,
 		printk(KERN_WARNING "RDS: from %pI6c has corrupted header - "
 			"forcing a reconnect\n",
 			&conn->c_faddr);
-		rds_stats_inc(conn->c_stats, s_recv_drop_bad_checksum);
+		rds_stats_inc(s_recv_drop_bad_checksum);
 		rds_ib_frag_free(ic, recv->r_frag);
 		recv->r_frag = NULL;
 		return;
@@ -1374,7 +1373,7 @@ void rds_ib_srq_process_recv(struct rds_connection *conn,
 			state->ack_next_valid = 1;
 		}
 		if (hdr->h_flags & RDS_FLAG_ACK_REQUIRED) {
-			rds_stats_inc(conn->c_stats, s_recv_ack_required);
+			rds_stats_inc(s_recv_ack_required);
 			state->ack_required = 1;
 		}
 		rds_inc_put(&ibinc->ii_inc);
