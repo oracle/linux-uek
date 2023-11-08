cfe19ef457e7c4baf0601a61953ee04bb59350d9
Anthony Yznaga <anthony.yznaga@oracle.com>
2023-11-08 14:56:18 -0800

    Revert "vfio: revert "iommu driver notify callback""
    This reverts commit e592296cd6e15ddeebe4c8411365c550da65c8bf.
    
    This is needed in order to re-enable support for using VFIO_UPDATE_VADDR
    with vfio mediated devices which is needed to fully support Live Update.
    
    Orabug: 35370252
    Signed-off-by: Anthony Yznaga <anthony.yznaga@oracle.com>
    Reviewed-by: Steve Sistare <steven.sistare@oracle.com>

--- drivers/vfio/vfio.h
+++ drivers/vfio/vfio.h
@@ -196,6 +196,11 @@ static inline bool vfio_device_is_noiommu(struct vfio_device *vdev)
 #endif /* CONFIG_VFIO_GROUP */
 
 #if IS_ENABLED(CONFIG_VFIO_CONTAINER)
+/* events for the backend driver notify callback */
+enum vfio_iommu_notify_type {
+	VFIO_IOMMU_CONTAINER_CLOSE = 0,
+};
+
 /**
  * struct vfio_iommu_driver_ops - VFIO IOMMU driver callbacks
  */
@@ -226,6 +231,8 @@ struct vfio_iommu_driver_ops {
 				  void *data, size_t count, bool write);
 	struct iommu_domain *(*group_iommu_domain)(void *iommu_data,
 						   struct iommu_group *group);
+	void		(*notify)(void *iommu_data,
+				  enum vfio_iommu_notify_type event);
 };
 
 struct vfio_iommu_driver {
