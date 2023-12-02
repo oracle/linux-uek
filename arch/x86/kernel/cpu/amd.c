5da3800c9d876b806fdcbcf5c0a7a64dd9763c97
Borislav Petkov (AMD) <bp@alien8.de>
2023-12-02 12:50:23 +0100

    x86/CPU/AMD: Add X86_FEATURE_ZEN1
    Commit 232afb557835d6f6859c73bf610bad308c96b131 upstream.
    
    Add a synthetic feature flag specifically for first generation Zen
    machines. There's need to have a generic flag for all Zen generations so
    make X86_FEATURE_ZEN be that flag.
    
    Fixes: 30fa92832f40 ("x86/CPU/AMD: Add ZenX generations flags")
    Suggested-by: Brian Gerst <brgerst@gmail.com>
    Suggested-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/dc3835e3-0731-4230-bbb9-336bbe3d042b@amd.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 48b4a4c96e491ae4657d514b036f406bf2431a71)
    
    Conflicts:
            arch/x86/kernel/cpu/amd.c
            Context change for code
    
    Signed-off-by: Samasth Norway Ananda <samasth.norway.ananda@oracle.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -641,7 +641,7 @@ static void bsp_init_amd(struct cpuinfo_x86 *c)
 		switch (c->x86_model) {
 		case 0x00 ... 0x2f:
 		case 0x50 ... 0x5f:
-			setup_force_cpu_cap(X86_FEATURE_ZEN);
+			setup_force_cpu_cap(X86_FEATURE_ZEN1);
 			break;
 		case 0x30 ... 0x4f:
 		case 0x60 ... 0x7f:
@@ -1058,14 +1058,14 @@ void init_spectral_chicken(struct cpuinfo_x86 *c)
 
 static void init_amd_zen_common(struct cpuinfo_x86 *c)
 {
+	setup_force_cpu_cap(X86_FEATURE_ZEN);
 	set_cpu_cap(c, X86_FEATURE_NT_GOOD);
-
 #ifdef CONFIG_NUMA
 	node_reclaim_distance = 32;
 #endif
 }
 
-static void init_amd_zen(struct cpuinfo_x86 *c)
+static void init_amd_zen1(struct cpuinfo_x86 *c)
 {
 	init_amd_zen_common(c);
 	fix_erratum_1386(c);
@@ -1190,8 +1190,8 @@ static void init_amd(struct cpuinfo_x86 *c)
 	case 0x16: init_amd_jg(c); break;
 	}
 
-	if (boot_cpu_has(X86_FEATURE_ZEN))
-		init_amd_zen(c);
+	if (boot_cpu_has(X86_FEATURE_ZEN1))
+		init_amd_zen1(c);
 	else if (boot_cpu_has(X86_FEATURE_ZEN2))
 		init_amd_zen2(c);
 	else if (boot_cpu_has(X86_FEATURE_ZEN3))
