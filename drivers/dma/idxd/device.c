de90960f18a5e63c7848c4cca54e5cdc6126c3e7
Tom Zanussi <tom.zanussi@linux.intel.com>
2023-12-05 15:25:18 -0600

    dmaengine: idxd: Rename drv_enable/disable_wq to idxd_drv_enable/disable_wq, and export
    Rename drv_enable_wq and drv_disable_wq to idxd_drv_enable_wq and
    idxd_drv_disable_wq respectively, so that they're no longer too
    generic to be exported.  This also matches existing naming within the
    idxd driver.
    
    And to allow idxd sub-drivers to enable and disable wqs, export them.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Fenghua Yu <fenghua.yu@intel.com>
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit d7ad915d817c8ce07a6101292497dddbab0429a3)
    Orabug: 36747435
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>
    Reviewed-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/dma/idxd/device.c
+++ drivers/dma/idxd/device.c
@@ -1368,7 +1368,7 @@ err_irq:
 	return rc;
 }
 
-int drv_enable_wq(struct idxd_wq *wq)
+int idxd_drv_enable_wq(struct idxd_wq *wq)
 {
 	struct idxd_device *idxd = wq->idxd;
 	struct device *dev = &idxd->pdev->dev;
@@ -1505,8 +1505,9 @@ err_map_portal:
 err:
 	return rc;
 }
+EXPORT_SYMBOL_NS_GPL(idxd_drv_enable_wq, IDXD);
 
-void drv_disable_wq(struct idxd_wq *wq)
+void idxd_drv_disable_wq(struct idxd_wq *wq)
 {
 	struct idxd_device *idxd = wq->idxd;
 	struct device *dev = &idxd->pdev->dev;
@@ -1526,6 +1527,7 @@ void drv_disable_wq(struct idxd_wq *wq)
 	wq->type = IDXD_WQT_NONE;
 	wq->client_count = 0;
 }
+EXPORT_SYMBOL_NS_GPL(idxd_drv_disable_wq, IDXD);
 
 int idxd_device_drv_probe(struct idxd_dev *idxd_dev)
 {
