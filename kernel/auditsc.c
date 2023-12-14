90f637879b1bf66f913d35af1f6487d1a0a1f009
Håkon Bugge <haakon.bugge@oracle.com>
2023-12-14 18:58:09 +0100

    Revert "audit: Apply special optimizations"
    This reverts commit 72416fe601e07a395525d5ec063a54ec3e828751.
    
    When forward porting this commit to LUCI from uek-7-u2, it applied
    cleanly. But that was a false positive, because the codegen
    optimizations were applied to the wrong function,
    audit_filter_syscall(), due to re-factoring of the code in upstream.
    
    Hence, revert this commit and re-apply it on the correct function,
    __audit_filter_op().
    
    Orabug: 36100150
    
    Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
    Reviewed-by: Konrad Wilk <Konrad.wilk@oracle.com>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -865,7 +865,6 @@ static void audit_filter_uring(struct task_struct *tsk,
  * high enough that we already know we have to write an audit record
  * (i.e., the state is AUDIT_STATE_BUILD).
  */
-#pragma GCC optimize("unswitch-loops", "align-loops=16", "align-jumps=16")
 static void audit_filter_syscall(struct task_struct *tsk,
 				 struct audit_context *ctx)
 {
@@ -877,7 +876,6 @@ static void audit_filter_syscall(struct task_struct *tsk,
 			NULL, ctx->major);
 	rcu_read_unlock();
 }
-#pragma GCC reset_options
 
 /*
  * Given an audit_name check the inode hash table to see if they match.
