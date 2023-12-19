be98961de1f17cbda897f940f083d04fd4c62628
Sai Krishna <saikrishnag@marvell.com>
2023-12-19 14:38:19 +0530

    octeontx2-pf: Reorder tearing down of PTP RX info work queue, mailbox
    In rare cases, on reboot of device PTP RX info work queue is getting
    scheduled after the corresponding MBOX has been teared down. This may
    lead to kernel crash. This patch reorders the teardown functions
    to fixes the crash.
    
    Signed-off-by: Sai Krishna <saikrishnag@marvell.com>
    Change-Id: I7362e3e73496a57dc7379e40bb9c8f55f7718671
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/118793
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    
    Orabug: 36725601
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Brian Maly <brian.maly@oracle.com>
    Reviewed-by: Tom Saeger <tom.saeger@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -3415,12 +3415,12 @@ static void otx2_remove(struct pci_dev *pdev)
 	otx2_unregister_dl(pf);
 	unregister_netdev(netdev);
 	cn10k_mcs_free(pf);
-	otx2_sriov_disable(pf->pdev);
+	otx2_ptp_destroy(pf);
 	otx2_sriov_vfcfg_cleanup(pf);
+	otx2_sriov_disable(pf->pdev);
 	if (pf->otx2_wq)
 		destroy_workqueue(pf->otx2_wq);
 
-	otx2_ptp_destroy(pf);
 	otx2_mcam_flow_del(pf);
 	otx2_shutdown_tc(pf);
 	otx2_shutdown_qos(pf);
