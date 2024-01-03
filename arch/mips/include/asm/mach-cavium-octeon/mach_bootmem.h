e6ccc37180194dd24a64a59109ae4a374308fb34
Anagha K J <aj@marvell.com>
2024-01-03 15:16:45 +0530

    Updates to octeon header files
    Defined cpu_has_octeon2_isa and cpu_has_local_ebase
    Defined gpio get/set/request functions
    Added interrupt sources from CIU_INTX_EN0
    Set up the TLB index,force 64-bit addressing enabled and clear NMI and SR
    Added node_data structure based on CONFIG_NUMA
    Set MAX_PHYSMEM_BITS based on CONFIG_NUMA
    Added functions for logically mapping cpu to node
    get the cpumask,parent node and cpu mask of pci bus
    Added empty mach_bootmem.h in arch/mips/include/asm/mach-generic/
    Define MAX_PHYSMEM_BITS to 38 in mach-generic/physmem.h file
    
    Signed-off-by: Anagha K J <aj@marvell.com>
    
    Orabug: 36725623
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Vijay Kumar <vijay.ac.kumar@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

---
+++ arch/mips/include/asm/mach-cavium-octeon/mach_bootmem.h
@@ -0,0 +1,8 @@
+#ifndef _ASM_MACH_CAVIUM_OCTEON_MACH_BOOTMEM_H
+#define _ASM_MACH_CAVIUM_OCTEON_MACH_BOOTMEM_H
+
+void mach_bootmem_init(void);
+
+#define mach_bootmem_init mach_bootmem_init
+
+#endif /* _ASM_MACH_CAVIUM_OCTEON_MACH_BOOTMEM_H */
