8cdcc78af262cb182d7380b6d8e1e5616b27ce42
Brett Creeley <brett.creeley@amd.com>
2024-01-29 15:40:32 -0800

    pds_core: Use struct pdsc for the pdsc_adminq_isr private data
    The initial design for the adminq interrupt was done based
    on client drivers having their own adminq and adminq
    interrupt. So, each client driver's adminq isr would use
    their specific adminqcq for the private data struct. For the
    time being the design has changed to only use a single
    adminq for all clients. So, instead use the struct pdsc for
    the private data to simplify things a bit.
    
    This also has the benefit of not dereferencing the adminqcq
    to access the pdsc struct when the PDSC_S_STOPPING_DRIVER bit
    is set and the adminqcq has actually been cleared/freed.
    
    Fixes: 01ba61b55b20 ("pds_core: Add adminq processing and commands")
    Signed-off-by: Brett Creeley <brett.creeley@amd.com>
    Reviewed-by: Shannon Nelson <shannon.nelson@amd.com>
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Link: https://lore.kernel.org/r/20240129234035.69802-4-brett.creeley@amd.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 951705151e50f9022bc96ec8b3fd5697380b1df6)
    Orabug: 38467344
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/net/ethernet/amd/pds_core/adminq.c
+++ drivers/net/ethernet/amd/pds_core/adminq.c
@@ -145,8 +145,8 @@ void pdsc_work_thread(struct work_struct *work)
 
 irqreturn_t pdsc_adminq_isr(int irq, void *data)
 {
-	struct pdsc_qcq *qcq = data;
-	struct pdsc *pdsc = qcq->pdsc;
+	struct pdsc *pdsc = data;
+	struct pdsc_qcq *qcq;
 
 	/* Don't process AdminQ when it's not up */
 	if (!pdsc_adminq_inc_if_up(pdsc)) {
@@ -155,6 +155,7 @@ irqreturn_t pdsc_adminq_isr(int irq, void *data)
 		return IRQ_HANDLED;
 	}
 
+	qcq = &pdsc->adminqcq;
 	queue_work(pdsc->wq, &qcq->work);
 	pds_core_intr_mask(&pdsc->intr_ctrl[qcq->intx], PDS_CORE_INTR_MASK_CLEAR);
 	refcount_dec(&pdsc->adminq_refcnt);
