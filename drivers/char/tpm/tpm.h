b601e69206a8284ce0a435197a7d35f017c9bcad
Ross Philipson <ross.philipson@oracle.com>
2024-01-31 15:50:14 -0800

    tpm: Add ability to set the preferred locality the TPM chip uses
    Curently the locality is hard coded to 0 but for DRTM support, access
    is needed to localities 1 through 4.
    
    Orabug: 33297039
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

--- drivers/char/tpm/tpm.h
+++ drivers/char/tpm/tpm.h
@@ -267,6 +267,7 @@ static inline void tpm_msleep(unsigned int delay_msec)
 int tpm_chip_bootstrap(struct tpm_chip *chip);
 int tpm_chip_start(struct tpm_chip *chip);
 void tpm_chip_stop(struct tpm_chip *chip);
+bool tpm_chip_preferred_locality(struct tpm_chip *chip, int locality);
 struct tpm_chip *tpm_find_get_ops(struct tpm_chip *chip);
 
 struct tpm_chip *tpm_chip_alloc(struct device *dev,
