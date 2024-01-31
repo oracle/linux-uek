282ee90f15ec937af0d9f42acf9f7ecb82ac22c5
Ross Philipson <ross.philipson@oracle.com>
2024-01-31 15:50:14 -0800

    tpm: Add ability to set the default locality the TPM chip uses
    Curently the locality is hard coded to 0 but for DRTM support, access
    is needed to localities 1 through 4.
    
    Orabug: 33297039
    
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

--- include/linux/tpm.h
+++ include/linux/tpm.h
@@ -219,6 +219,9 @@ struct tpm_chip {
 	u8 null_ec_key_y[EC_PT_SZ];
 	struct tpm2_auth *auth;
 #endif
+
+	/* preferred locality - default 0 */
+	int default_locality;
 };
 
 #define TPM_HEADER_SIZE		10
@@ -467,6 +470,7 @@ static inline ssize_t tpm_ret_to_err(ssize_t ret)
 extern int tpm_is_tpm2(struct tpm_chip *chip);
 extern __must_check int tpm_try_get_ops(struct tpm_chip *chip);
 extern void tpm_put_ops(struct tpm_chip *chip);
+extern bool tpm_chip_set_default_locality(struct tpm_chip *chip, int locality);
 extern ssize_t tpm_transmit_cmd(struct tpm_chip *chip, struct tpm_buf *buf,
 				size_t min_rsp_body_length, const char *desc);
 extern int tpm_pcr_read(struct tpm_chip *chip, u32 pcr_idx,
