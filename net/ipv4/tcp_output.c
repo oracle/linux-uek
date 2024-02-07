5c376f3d31358b97b207868b0013085665fdf0b9
Sherry Yang <sherry.yang@oracle.com>
2024-02-07 11:02:59 -0800

    Revert "tcp: fix excessive TLP and RACK timeouts from HZ rounding"
    This reverts commit eb82ba861194bae98fc5b78246501d659bb79752.
    
    This commit causes NFS dio_sanity tests failure. But the issue doesn't
    happen on linux-stable 5.15.y, which means it is very likely caused by
    conflicts between upstream commit with UEK code base.
    
    To unblock Feb Errata, revert the commit first.
    
    Orabug: 36241828
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2743,7 +2743,7 @@ bool tcp_schedule_loss_probe(struct sock *sk, bool advancing_rto)
 {
 	struct inet_connection_sock *icsk = inet_csk(sk);
 	struct tcp_sock *tp = tcp_sk(sk);
-	u32 timeout, timeout_us, rto_delta_us;
+	u32 timeout, rto_delta_us;
 	int early_retrans;
 
 	/* Don't do any loss probe on a Fast Open connection before 3WHS
@@ -2767,12 +2767,11 @@ bool tcp_schedule_loss_probe(struct sock *sk, bool advancing_rto)
 	 * sample is available then probe after TCP_TIMEOUT_INIT.
 	 */
 	if (tp->srtt_us) {
-		timeout_us = tp->srtt_us >> 2;
+		timeout = usecs_to_jiffies(tp->srtt_us >> 2);
 		if (tp->packets_out == 1)
-			timeout_us += sysctl_tcp_delack_max;
+			timeout += sysctl_tcp_delack_max;
 		else
-			timeout_us += TCP_TIMEOUT_MIN_US;
-		timeout = usecs_to_jiffies(timeout_us);
+			timeout += TCP_TIMEOUT_MIN;
 	} else {
 		timeout = TCP_TIMEOUT_INIT;
 	}
