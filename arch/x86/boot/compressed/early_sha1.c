171413e0e36937cde39aa51116162a16f6066bf0
Daniel P. Smith <dpsmith@apertussolutions.com>
2024-03-04 12:13:24 -0800

    x86: Add early SHA-1 support for Secure Launch early measurements
    For better or worse, Secure Launch needs SHA-1 and SHA-256. The
    choice of hashes used lie with the platform firmware, not with
    software, and is often outside of the users control.
    
    Even if we'd prefer to use SHA-256-only, if firmware elected to start us
    with the SHA-1 and SHA-256 backs active, we still need SHA-1 to parse
    the TPM event log thus far, and deliberately cap the SHA-1 PCRs in order
    to safely use SHA-256 for everything else.
    
    The SHA-1 code here has its origins in the code from the main kernel:
    
    commit c4d5b9ffa31f ("crypto: sha1 - implement base layer for SHA-1")
    
    A modified version of this code was introduced to the lib/crypto/sha1.c
    to bring it in line with the SHA-256 code and allow it to be pulled into the
    setup kernel in the same manner as SHA-256 is.
    
    Orabug: 33297039
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

---
+++ arch/x86/boot/compressed/early_sha1.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2024 Apertus Solutions, LLC.
+ */
+
+#include <linux/init.h>
+#include <linux/linkage.h>
+#include <linux/string.h>
+#include <asm/boot.h>
+#include <asm/unaligned.h>
+
+#include "../../../../lib/crypto/sha1.c"
