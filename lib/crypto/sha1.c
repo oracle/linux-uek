4b2f9d339418e11dca507821636b4553b275ad7c
Daniel P. Smith <dpsmith@apertussolutions.com>
2024-03-04 12:13:24 -0800

    x86: Add early SHA-1 support for Secure Launch early measurements
    Secure Launch is written to be compliant with the Intel TXT Measured Launch
    Developer's Guide. The MLE Guide dictates that the system can be configured to
    use both the SHA-1 and SHA-2 hashing algorithms.
    
    Regardless of the preference towards SHA-2, if the firmware elected to start
    with the SHA-1 and SHA-2 banks active and the dynamic launch was configured to
    include SHA-1, Secure Launch is obligated to record measurements for all
    algorithms requested in the launch configuration.
    
    The user environment or the integrity management does not desire to use SHA-1,
    it is free to just ignore the SHA-1 bank in any integrity operation with the
    TPM. If there is a larger concern about the SHA-1 bank being active, it is free
    to deliberately cap the SHA-1 PCRs, recording the event in the D-RTM log.
    
    The SHA-1 code here has its origins in the code from the main kernel:
    
    commit c4d5b9f ("crypto: sha1 - implement base layer for SHA-1")
    
    A modified version of this code was introduced to the lib/crypto/sha1.c to bring
    it in line with the SHA-256 code and allow it to be pulled into the setup kernel
    in the same manner as SHA-256 is.
    
    Orabug: 33297039
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

--- lib/crypto/sha1.c
+++ lib/crypto/sha1.c
@@ -137,5 +137,86 @@ void sha1_init(__u32 *buf)
 }
 EXPORT_SYMBOL(sha1_init);
 
+static void __sha1_transform(u32 *digest, const char *data)
+{
+	u32 ws[SHA1_WORKSPACE_WORDS];
+
+	sha1_transform(digest, data, ws);
+
+	/* Ensure local data for generating digest is cleared in all cases */
+	memzero_explicit(ws, sizeof(ws));
+}
+
+static void sha1_update(struct sha1_state *sctx, const u8 *data, unsigned int len)
+{
+	unsigned int partial = sctx->count % SHA1_BLOCK_SIZE;
+	int blocks;
+
+	sctx->count += len;
+
+	if (unlikely((partial + len) < SHA1_BLOCK_SIZE))
+		goto out;
+
+	if (partial) {
+		int p = SHA1_BLOCK_SIZE - partial;
+
+		memcpy(sctx->buffer + partial, data, p);
+		data += p;
+		len -= p;
+
+		__sha1_transform(sctx->state, sctx->buffer);
+	}
+
+	blocks = len / SHA1_BLOCK_SIZE;
+	len %= SHA1_BLOCK_SIZE;
+
+	if (blocks) {
+		while (blocks--) {
+			__sha1_transform(sctx->state, data);
+			data += SHA1_BLOCK_SIZE;
+		}
+	}
+	partial = 0;
+
+out:
+	memcpy(sctx->buffer + partial, data, len);
+}
+
+static void sha1_final(struct sha1_state *sctx, u8 *out)
+{
+	const int bit_offset = SHA1_BLOCK_SIZE - sizeof(__be64);
+	unsigned int partial = sctx->count % SHA1_BLOCK_SIZE;
+	__be64 *bits = (__be64 *)(sctx->buffer + bit_offset);
+	__be32 *digest = (__be32 *)out;
+	int i;
+
+	sctx->buffer[partial++] = 0x80;
+	if (partial > bit_offset) {
+		memset(sctx->buffer + partial, 0x0, SHA1_BLOCK_SIZE - partial);
+		partial = 0;
+
+		__sha1_transform(sctx->state, sctx->buffer);
+	}
+
+	memset(sctx->buffer + partial, 0x0, bit_offset - partial);
+	*bits = cpu_to_be64(sctx->count << 3);
+	__sha1_transform(sctx->state, sctx->buffer);
+
+	for (i = 0; i < SHA1_DIGEST_SIZE / sizeof(__be32); i++)
+		put_unaligned_be32(sctx->state[i], digest++);
+
+	*sctx = (struct sha1_state){};
+}
+
+void sha1(const u8 *data, unsigned int len, u8 *out)
+{
+	struct sha1_state sctx = {0};
+
+	sha1_init(sctx.state);
+	sha1_update(&sctx, data, len);
+	sha1_final(&sctx, out);
+}
+EXPORT_SYMBOL(sha1);
+
 MODULE_DESCRIPTION("SHA-1 Algorithm");
 MODULE_LICENSE("GPL");
