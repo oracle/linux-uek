84ab696b52720b399a20c35b3cac212be9de2dcd
Juan Garcia <juan.ma.garcia@oracle.com>
2024-03-19 16:59:10 +0000

    rds/rdma: print connection up/down time while dropping/connecting
    Display connected duration at connection drop and connection reconnect
    duration at reconnect complete.
    
    dmesg:
    Active Side:
    [  123.308800] Resetting RDS/IB connection
    <192.168.100.251,192.168.100.199,0>
    [  123.310029] RDS/IB: connection
    <::ffff:192.168.100.251,::ffff:192.168.100.199,0> dropped due to
    'user reset'(up for 10260 ms)
    
    Passive Side:
    [   98.666586] RDS/IB: connection
    <::ffff:192.168.100.199,::ffff:192.168.100.251,0> dropped due to
    'DISCONNECTED event'(up for 10254 ms)
    [   98.686766] RDS/IB: Active  conn ffff9e696efca000 i_cm_id
    ffff9e696ef2cc00, frag 16KB, connected
    <::ffff:192.168.100.199,::ffff:192.168.100.251,0> (down for 18 ms)
    version
        4.1
    
    Orabug: 36264661
    
    Signed-off-by: Juan Garcia <juan.ma.garcia@oracle.com>
    Reviewed-by: Rama Nichanamatlu <rama.nichanamatlu@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- net/rds/ib_cm.c
+++ net/rds/ib_cm.c
@@ -343,15 +343,17 @@ void rds_ib_cm_connect_complete(struct rds_connection *conn, struct rdma_cm_even
 		}
 	}
 
-	printk(KERN_NOTICE "RDS/IB: %s conn %p i_cm_id %p, frag %dKB, connected <%pI6c,%pI6c,%d> version %u.%u%s%s\n",
+	printk(KERN_NOTICE "RDS/IB: %s conn %p i_cm_id %p, frag %dKB, connected <%pI6c,%pI6c,%d> (down for %u ms) version %u.%u%s%s\n",
 	       ic->i_active_side ? "Active " : "Passive",
 	       conn, ic->i_cm_id, ic->i_frag_sz / SZ_1K,
 	       &conn->c_laddr, &conn->c_faddr, conn->c_tos,
+	       jiffies_to_msecs(jiffies - conn->c_path->cp_conn_ts_jf),
 	       RDS_PROTOCOL_MAJOR(conn->c_version),
 	       RDS_PROTOCOL_MINOR(conn->c_version),
 	       ic->i_flowctl ? ", flow control" : "",
 	       conn->c_acl_en ? ", ACL Enabled" : "");
 
+	conn->c_path->cp_conn_ts_jf = jiffies;
 	/* The connection might have been dropped under us*/
 	if (!ic->i_cm_id) {
 		rds_conn_drop(conn, DR_IB_CONN_DROP_RACE, 0);
