098b91701c1c1a8ecc31b3cd0dcd6aa6888354a3
Libo Chen <libo.chen@oracle.com>
2024-03-21 23:41:56 -0700

    arm64/uek-misc: add a new boot parameter uek=cls to turn on/off CLS sched domain at boot time
    This allows customers to enable cluster sched domain level on their
    AmpereOne machines via uek=cls command line parameter while keeping others
    as if CONFIG_SCHED_CLUSTER is unset.
    
    The default topology ARM64 bases off is {SMT, CLS, MC, DIE} once
    CONFIG_SCHED_CLUSTER is enabled (see kernel/sched/topology.c). We want by
    default (even if CONFIG_SCHED_CLUSTER is set) to have {SMT, MC, DIE}
    which leaves out CLS.
    
    Orabug: 36473714
    
    Signed-off-by: Libo Chen <libo.chen@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/misc/uek/uek.c
+++ drivers/misc/uek/uek.c
@@ -20,7 +20,9 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION(UEK_MISC_VER);
 
 DEFINE_STATIC_KEY_FALSE(on_exadata);
+DEFINE_STATIC_KEY_FALSE(cls_enabled);
 EXPORT_SYMBOL_GPL(on_exadata);
+EXPORT_SYMBOL_GPL(cls_enabled);
 
 /* Override to disable optimizations on Exadata systems. */
 static bool exadata_disable;
@@ -35,6 +37,9 @@ static int __init uek_params(char *str)
 		return 1;
 	} else if ((strncmp(str, "noexadata", 9) == 0)) {
 		exadata_disable = true;
+		return 1;
+	} else if (strncmp(str, "cls", 3) == 0) {
+		static_branch_enable(&cls_enabled);
 		return 1;
 	}
 
