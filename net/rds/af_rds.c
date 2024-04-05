df316450bb71abb8dc837a8cc7ae6efd3ae4c1c5
Juan Garcia <juan.ma.garcia@oracle.com>
2024-04-05 19:32:16 +0000

    rds/rdma: Fix congestion value for userspace consumption
    In commit 978f85094d8 ("rds: Include congested flag in rds_sock
    struct.") we passed -1 from kernel when the rs->rs_congested field was
    0. This was to differentiate it from an unsupported kernel in userspace.
    Now userspace can handle unsupported kernel by itself, hence no need
    to do remapping in kernel. This patch sends the rs->rs_congested value
    to userspace without doing any remapping.
    
    Orabug: 36264649
    
    Fixes: 978f85094d8 ("rds: Include congested flag in rds_sock struct.")
    Reviewed-by: Arumugam Kolappan <aru.kolappan@oracle.com>
    Signed-off-by: Juan Garcia <juan.ma.garcia@oracle.com>

--- net/rds/af_rds.c
+++ net/rds/af_rds.c
@@ -1253,13 +1253,6 @@ static void rds6_sock_inc_info(struct socket *sock, unsigned int len,
 }
 #endif
 
-/* Userspace cannot differentiate between 0 indicating a
- * a non congestion state or on a older kernel where this
- * value is not set and would contain 0 by default on userspace
- * struct initialization. Thus we send -1 to indicate that a non
- * congested state to userspace (Applies to rds6_sock_info as well).
- */
-
 static void rds_sock_info(struct socket *sock, unsigned int len,
 			  struct rds_info_iterator *iter,
 			  struct rds_info_lengths *lens)
@@ -1283,13 +1276,9 @@ static void rds_sock_info(struct socket *sock, unsigned int len,
 		sinfo.connected_port = rs->rs_conn_port;
 		sinfo.inum = sock_i_ino(rds_rs_to_sk(rs));
 		sinfo.pid = rs->rs_pid;
+		sinfo.cong = rs->rs_congested;
 		if (rs->rs_transport)
 			strscpy(sinfo.t_name, rs->rs_transport->t_name, sizeof(sinfo.t_name));
-		if (rs->rs_congested)
-			sinfo.cong = rs->rs_congested;
-		else
-			sinfo.cong = -1;
-
 		rds_info_copy(iter, &sinfo, sizeof(sinfo));
 	}
 
@@ -1324,13 +1313,9 @@ static void rds6_sock_info(struct socket *sock, unsigned int len,
 		sinfo6.connected_port = rs->rs_conn_port;
 		sinfo6.inum = sock_i_ino(rds_rs_to_sk(rs));
 		sinfo6.pid = rs->rs_pid;
+		sinfo6.cong = rs->rs_congested;
 		if (rs->rs_transport)
 			strscpy(sinfo6.t_name, rs->rs_transport->t_name, sizeof(sinfo6.t_name));
-		if (rs->rs_congested)
-			sinfo6.cong = rs->rs_congested;
-		else
-			sinfo6.cong = -1;
-
 		rds_info_copy(iter, &sinfo6, sizeof(sinfo6));
 	}
 
