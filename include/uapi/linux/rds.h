67f9c1d4621474264ce96bdb59732aeb64fa94a1
Juan Garcia <juan.ma.garcia@oracle.com>
2024-04-06 04:42:16 +0000

    rds: Include transport protocol name in rds-info -k output
    Add transport protocol in rds-info output. When we work with RDS tools
    and sockets, we want to now the transport name.
    
    Orabug: 36264649
    
    Reviewed-by: Arumugam Kolappan <aru.kolappan@oracle.com>
    Signed-off-by: Juan Garcia <juan.ma.garcia@oracle.com>

--- include/uapi/linux/rds.h
+++ include/uapi/linux/rds.h
@@ -77,7 +77,7 @@
 
 /* supported values for SO_RDS_TRANSPORT */
 #define	RDS_TRANS_IB	0
-#define	RDS_TRANS_GAP	1
+#define	RDS_TRANS_LOOP	1
 #define	RDS_TRANS_TCP	2
 #define	RDS_TRANS_COUNT	3
 #define	RDS_TRANS_NONE	(~0)
@@ -246,6 +246,7 @@ struct rds_info_socket {
 	__u64		inum;
 	pid_t		pid;
 	__s32		cong;
+	__u8		t_name[TRANSNAMSIZ];
 } __attribute__((packed));
 
 struct rds6_info_socket {
@@ -258,6 +259,7 @@ struct rds6_info_socket {
 	__u64		inum;
 	pid_t		pid;
 	__s32		cong;
+	__u8		t_name[TRANSNAMSIZ];
 } __attribute__((packed));
 
 struct rds_info_tcp_socket {
