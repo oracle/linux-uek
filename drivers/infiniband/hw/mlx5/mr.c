094c124f606dddec7a23d6332ccf2200f91864ae
Qing Huang <qing.huang@oracle.com>
2024-05-14 17:34:18 -0700

    Revert "RDMA/mlx5: Set MR cache limit for both PF and VF"
    This reverts commit e258d4fe2adeb818cbc156dc964bfb85251dfe61.
    
    Orabug: 36466391
    
    Conflicts:
    	drivers/infiniband/hw/mlx5/mr.c
    
    Contextual conflicts caused by upstream backport commit:
    49f1e946ef02 ("RDMA/mlx5: Introduce mlx5r_cache_rb_key")
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/infiniband/hw/mlx5/mr.c
+++ drivers/infiniband/hw/mlx5/mr.c
@@ -883,16 +883,12 @@ mlx5r_cache_create_ent_locked(struct mlx5_ib_dev *dev,
 		else
 			order = order_base_2(rb_key.ndescs) - 2;
 
-#ifdef WITHOUT_ORACLE_EXTENSIONS
 		if ((dev->mdev->profile.mask & MLX5_PROF_MASK_MR_CACHE) &&
 		    !dev->is_rep && mlx5_core_is_pf(dev->mdev) &&
 		    mlx5r_umr_can_load_pas(dev, 0))
 			ent->limit = dev->mdev->profile.mr_cache[order].limit;
 		else
 			ent->limit = 0;
-#else
-			ent->limit = dev->mdev->profile.mr_cache[order].limit;
-#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 
 		mlx5_mkey_cache_debugfs_add_ent(dev, ent);
 	} else {
