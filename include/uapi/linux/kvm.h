8f5d3bba751c0f1c25f9e71e0e2731d527cfe60f
Michael Roth <michael.roth@amd.com>
2024-06-13 18:10:06 -0500

    KVM: Introduce KVM_EXIT_SNP_REQ_CERTS for SNP certificate-fetching
    For SEV-SNP, the host can optionally provide a certificate table to the
    guest when it issues an attestation request to firmware (see GHCB 2.0
    specification regarding "SNP Extended Guest Requests"). This certificate
    table can then be used to verify the endorsement key used by firmware to
    sign the attestation report.
    
    While it is possible for guests to obtain the certificates through other
    means, handling it via the host provides more flexibility in being able
    to keep the certificate data in sync with the endorsement key throughout
    host-side operations that might resulting in the endorsement key
    changing.
    
    In the case of KVM, userspace will be responsible for fetching the
    certificate table and keeping it in sync with any modifications to the
    endorsement key by other userspace management tools. Define a new
    KVM_EXIT_SNP_REQ_CERTS event where userspace is provided with the GPA of
    the buffer the guest has provided as part of the attestation request so
    that userspace can write the certificate data into it while relying on
    filesystem-based locking to keep the certificates up-to-date relative to
    the endorsement keys installed/utilized by firmware at the time the
    certificates are fetched.
    
      [Melody: Update the documentation scheme about how file locking is
      expected to happen.]
    
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Tested-by: Liam Merwick <liam.merwick@oracle.com>
    Tested-by: Dionna Glaze <dionnaglaze@google.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Melody Wang <huibo.wang@amd.com>
    (cherry picked from commit f605820c871cd811aa5215df6e62a083b244a515)
    [ cherry-pick-repo: https://github.com/AMDESE/linux.git ]
    Orabug: 37968448
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Ian Chin Wang <ian.chin.wang@oracle.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -135,6 +135,12 @@ struct kvm_xen_exit {
 	} u;
 };
 
+struct kvm_exit_snp_req_certs {
+	__u64 gfn;
+	__u64 npages;
+	__u64 ret;
+};
+
 #define KVM_S390_GET_SKEYS_NONE   1
 #define KVM_S390_SKEYS_MAX        1048576
 
@@ -178,6 +184,7 @@ struct kvm_xen_exit {
 #define KVM_EXIT_NOTIFY           37
 #define KVM_EXIT_LOONGARCH_IOCSR  38
 #define KVM_EXIT_MEMORY_FAULT     39
+#define KVM_EXIT_SNP_REQ_CERTS    40
 
 /* For KVM_EXIT_INTERNAL_ERROR */
 /* Emulate instruction failed. */
@@ -446,6 +453,8 @@ struct kvm_run {
 			__u64 gpa;
 			__u64 size;
 		} memory_fault;
+		/* KVM_EXIT_SNP_REQ_CERTS */
+		struct kvm_exit_snp_req_certs snp_req_certs;
 		/* Fix the size of the union. */
 		char padding[256];
 	};
