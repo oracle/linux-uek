32fae3fd0e5808f5e70d9928fb3990bc3a90f440
Gerd Rausch <gerd.rausch@oracle.com>
2024-07-09 16:14:36 -0700

    RDMA: Flip the meaning of "-1" and "0" in ibv_create_cq / ib_create_cq
    UEK-2 added a feature for "completion-vector load balancing",
    if the CQ was created with "comp_vector=-1".
    
    That feature re-emerged in UEK-4 for the MLX4-driver,
    but curiously "comp_vector=0" was used to indicate load-balancing.
    
    Later on, in UEK-5, the value of "comp_vector=-1" was introduced
    to indicate "I really, really want an actual value of zero".
    
    So in a way we're in the unfortunate situation today that:
    * We're upstream incompatible since UEK-4
    * We've flipped the semantic meaning of "comp_vector=-1" and
      "comp_vector=0" compared to how they were originally in UEK-2
    
    This commit brings back the semantics of UEK-2 and allows "comp_vector=-1"
    to mean "load balancing" (in an upstream-compatible manner),
    and reinstates "comp_vector=0" to actually mean zero.
    
    That allows kernel / ULP protocols that have vector randomization
    to work and do their accounting properly, while still allowing
    applications that want the load-balancing to be done by the kernel
    to work as well.
    
    The one consumer of this load-balancing functionality that we know of
    is in the VOS library, file "ipclw_ib.c".
    Prior to "ipclw_ib.c < @@/main/22" it attempted to call
    "ibv_create_cq(..., -1);" first, and if that failed,
    then called "ibv_create_cq(..., 0);".
    Later versions of this code "ipclw_ib.c >= @@/main/22" simply randomize
    the vector, ranging from 0 to (num_comp_vectors - 1).
    
    Hence those old versions of the VOS code work with either
    load-balancing approaches, and the new & vector-randomized VOS
    code works even better if comp_vector=0 actually uses zero,
    because it no longer gets an non-zero vector instead:
    Cases where the vector-randomization asked for a vector of zero,
    but a non-zero vector was effectively used instead.
    With "num_comp_vectors=20", that'd be a "1/20 * 19/20 = 4.75%" miss-rate.
    
    Orabug: 37081626
    
    Fixes: 360703cfa058 ("IB/core: Introduce IB_CQ_FORCE_ZERO_CV")
    Fixes: 35b09c695eee ("net/mlx4: fix non-determinism when comp_vector is zero")
    Fixes: 653ae3b35733 ("net/mlx5: fix non-determinism when comp_vector is zero")
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Håkon Bugge <haakon.bugge@oracle.com>

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -1214,8 +1214,14 @@ static int create_cq(struct uverbs_attr_bundle *attrs,
 	struct ib_cq_init_attr attr = {};
 	struct ib_device *ib_dev;
 
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+	if (cmd->comp_vector >= attrs->ufile->device->num_comp_vectors &&
+	    !((int)cmd->comp_vector == -1 && attrs->ufile->device->can_balance_comp_vectors))
+		return -EINVAL;
+#else /* WITHOUT_ORACLE_EXTENSIONS */
 	if (cmd->comp_vector >= attrs->ufile->device->num_comp_vectors)
 		return -EINVAL;
+#endif /* WITHOUT_ORACLE_EXTENSIONS */
 
 	if (!cmd->cqe)
 		return -EINVAL;
