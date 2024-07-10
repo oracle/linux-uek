077d02d7100abdd50c3ff8c3ee7868d761ceaa7c
Edwin Peer <edwin.peer@broadcom.com>
2024-07-10 01:21:47 -0400

    bnxt_en: improve firmware timeout messaging
    upstream commit 8c6f36d93449e8de7b49a67727e3d09b67c73126
    
    While it has always been possible to infer that an HWRM command was
    abandoned due to an unhealthy firmware status by the shortened timeout
    reported, this change improves the log messaging to account for this
    case explicitly. In the interests of further clarity, the firmware
    status is now also reported in these new messages.
    
    v2: Remove inline keyword for hwrm_wait_must_abort() in .c file.
    
    Reviewed-by: Andy Gospodarek <gospo@broadcom.com>
    Signed-off-by: Edwin Peer <edwin.peer@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 8c6f36d93449e8de7b49a67727e3d09b67c73126)
    
    Orabug: 36826374
    
    Conflicts:
        drivers/net/ethernet/broadcom/bnxt/bnxt.h
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -2276,7 +2276,6 @@ int bnxt_hwrm_alloc_wol_fltr(struct bnxt *bp);
 int bnxt_hwrm_free_wol_fltr(struct bnxt *bp);
 int bnxt_hwrm_func_resc_qcaps(struct bnxt *bp, bool all);
 int bnxt_hwrm_func_qcaps(struct bnxt *bp);
-bool bnxt_is_fw_healthy(struct bnxt *bp);
 int bnxt_hwrm_fw_set_time(struct bnxt *);
 int bnxt_open_nic(struct bnxt *, bool, bool);
 int bnxt_half_open_nic(struct bnxt *bp);
