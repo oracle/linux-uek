029ed86edecf2108f69953ff001ebb98aa7b01bd
Gerd Rausch <gerd.rausch@oracle.com>
2024-07-31 00:39:29 -0700

    net/rds: Rewind RDS to 8cbd9606a636 in order to rebase UEK commits on top of it
    The first commit for RDS in UEK4 starting at
       "git merge-base remotes/uek/uek-4.1-QU6 v4.14"
    was 2f6ea6814dbaeb225dd3a6ebc3edb38affa2f5af.
    
    It rewinds RDS back to 8cbd9606a6367c221a7bbcc47f3ab1a8c31b6437, so we do the exact same thing here for LUCI.
    
    Which is also the exact same thing that was done for UEK7, UEK6 and UEK5.
    
    Orabug: 33590087
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>

--- net/rds/loop.c
+++ net/rds/loop.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006 Oracle.  All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -31,29 +31,13 @@
  *
  */
 #include <linux/kernel.h>
-#include <linux/slab.h>
 #include <linux/in.h>
-#include <net/net_namespace.h>
-#include <net/netns/generic.h>
-#include <linux/ipv6.h>
 
-#include "rds_single_path.h"
 #include "rds.h"
 #include "loop.h"
 
 static DEFINE_SPINLOCK(loop_conns_lock);
 static LIST_HEAD(loop_conns);
-static atomic_t rds_loop_unloading = ATOMIC_INIT(0);
-
-static void rds_loop_set_unloading(void)
-{
-	atomic_set(&rds_loop_unloading, 1);
-}
-
-static bool rds_loop_is_unloading(struct rds_connection *conn)
-{
-	return atomic_read(&rds_loop_unloading) != 0;
-}
 
 /*
  * This 'loopback' transport is a special case for flows that originate
@@ -76,47 +60,43 @@ static int rds_loop_xmit(struct rds_connection *conn, struct rds_message *rm,
 			 unsigned int hdr_off, unsigned int sg,
 			 unsigned int off)
 {
-	struct scatterlist *sgp = &rm->data.op_sg[sg];
-	int ret = sizeof(struct rds_header) +
-			be32_to_cpu(rm->m_inc.i_hdr.h_len);
-
-	/* Do not send cong updates to loopback */
-	if (rm->m_inc.i_hdr.h_flags & RDS_FLAG_CONG_BITMAP) {
-		rds_cong_map_updated(conn->c_fcong, ~(u64) 0);
-		ret = min_t(int, ret, sgp->length - conn->c_xmit_data_off);
-		goto out;
-	}
-
 	BUG_ON(hdr_off || sg || off);
 
-	rds_inc_init(&rm->m_inc, conn, &conn->c_laddr);
-	/* For the embedded inc. Matching put is in loop_inc_free() */
-	rds_message_addref(rm);
+	rds_inc_init(&rm->m_inc, conn, conn->c_laddr);
+	rds_message_addref(rm); /* for the inc */
 
-	rds_recv_incoming(conn, &conn->c_laddr, &conn->c_faddr, &rm->m_inc,
-			  GFP_KERNEL);
+	rds_recv_incoming(conn, conn->c_laddr, conn->c_faddr, &rm->m_inc,
+			  GFP_KERNEL, KM_USER0);
 
 	rds_send_drop_acked(conn, be64_to_cpu(rm->m_inc.i_hdr.h_sequence),
 			    NULL);
 
 	rds_inc_put(&rm->m_inc);
-out:
-	return ret;
+
+	return sizeof(struct rds_header) + be32_to_cpu(rm->m_inc.i_hdr.h_len);
 }
 
-/*
- * See rds_loop_xmit(). Since our inc is embedded in the rm, we
- * make sure the rm lives at least until the inc is done.
- */
-static void rds_loop_inc_free(struct rds_incoming *inc)
+static int rds_loop_xmit_cong_map(struct rds_connection *conn,
+				  struct rds_cong_map *map,
+				  unsigned long offset)
 {
-	struct rds_message *rm = container_of(inc, struct rds_message, m_inc);
+	unsigned long i;
 
-	rds_message_put(rm);
+	BUG_ON(offset);
+	BUG_ON(map != conn->c_lcong);
+
+	for (i = 0; i < RDS_CONG_MAP_PAGES; i++) {
+		memcpy((void *)conn->c_fcong->m_page_addrs[i],
+		       (void *)map->m_page_addrs[i], PAGE_SIZE);
+	}
+
+	rds_cong_map_updated(conn->c_fcong, ~(u64) 0);
+
+	return sizeof(struct rds_header) + RDS_CONG_MAP_BYTES;
 }
 
 /* we need to at least give the thread something to succeed */
-static int rds_loop_recv_path(struct rds_conn_path *cp)
+static int rds_loop_recv(struct rds_connection *conn)
 {
 	return 0;
 }
@@ -137,8 +117,8 @@ static int rds_loop_conn_alloc(struct rds_connection *conn, gfp_t gfp)
 	struct rds_loop_connection *lc;
 	unsigned long flags;
 
-	lc = kzalloc(sizeof(struct rds_loop_connection), gfp);
-	if (!lc)
+	lc = kzalloc(sizeof(struct rds_loop_connection), GFP_KERNEL);
+	if (lc == NULL)
 		return -ENOMEM;
 
 	INIT_LIST_HEAD(&lc->loop_node);
@@ -155,22 +135,18 @@ static int rds_loop_conn_alloc(struct rds_connection *conn, gfp_t gfp)
 static void rds_loop_conn_free(void *arg)
 {
 	struct rds_loop_connection *lc = arg;
-	unsigned long flags;
-
 	rdsdebug("lc %p\n", lc);
-	spin_lock_irqsave(&loop_conns_lock, flags);
 	list_del(&lc->loop_node);
-	spin_unlock_irqrestore(&loop_conns_lock, flags);
 	kfree(lc);
 }
 
-static int rds_loop_conn_path_connect(struct rds_conn_path *cp)
+static int rds_loop_conn_connect(struct rds_connection *conn)
 {
-	rds_connect_complete(cp->cp_conn);
+	rds_connect_complete(conn);
 	return 0;
 }
 
-static void rds_loop_conn_path_shutdown(struct rds_conn_path *cp)
+static void rds_loop_conn_shutdown(struct rds_connection *conn)
 {
 }
 
@@ -179,8 +155,6 @@ void rds_loop_exit(void)
 	struct rds_loop_connection *lc, *_lc;
 	LIST_HEAD(tmp_list);
 
-	rds_loop_set_unloading();
-	synchronize_rcu();
 	/* avoid calling conn_destroy with irqs off */
 	spin_lock_irq(&loop_conns_lock);
 	list_splice(&loop_conns, &tmp_list);
@@ -193,46 +167,6 @@ void rds_loop_exit(void)
 	}
 }
 
-static void rds_loop_kill_conns(struct net *net)
-{
-	struct rds_loop_connection *lc, *_lc;
-	LIST_HEAD(tmp_list);
-
-	spin_lock_irq(&loop_conns_lock);
-	list_for_each_entry_safe(lc, _lc, &loop_conns, loop_node)  {
-		struct net *c_net = read_pnet(&lc->conn->c_net);
-
-		if (net != c_net)
-			continue;
-		list_move_tail(&lc->loop_node, &tmp_list);
-	}
-	spin_unlock_irq(&loop_conns_lock);
-
-	list_for_each_entry_safe(lc, _lc, &tmp_list, loop_node) {
-		WARN_ON(lc->conn->c_passive);
-		rds_conn_destroy(lc->conn);
-	}
-}
-
-static void __net_exit rds_loop_exit_net(struct net *net)
-{
-	rds_loop_kill_conns(net);
-}
-
-static struct pernet_operations rds_loop_net_ops = {
-	.exit = rds_loop_exit_net,
-};
-
-int rds_loop_net_init(void)
-{
-	return register_pernet_device(&rds_loop_net_ops);
-}
-
-void rds_loop_net_exit(void)
-{
-	unregister_pernet_device(&rds_loop_net_ops);
-}
-
 /*
  * This is missing .xmit_* because loop doesn't go through generic
  * rds_send_xmit() and doesn't call rds_recv_incoming().  .listen_stop and
@@ -241,14 +175,14 @@ void rds_loop_net_exit(void)
  */
 struct rds_transport rds_loop_transport = {
 	.xmit			= rds_loop_xmit,
-	.recv_path		= rds_loop_recv_path,
+	.xmit_cong_map		= rds_loop_xmit_cong_map,
+	.recv			= rds_loop_recv,
 	.conn_alloc		= rds_loop_conn_alloc,
 	.conn_free		= rds_loop_conn_free,
-	.conn_path_connect	= rds_loop_conn_path_connect,
-	.conn_path_shutdown	= rds_loop_conn_path_shutdown,
+	.conn_connect		= rds_loop_conn_connect,
+	.conn_shutdown		= rds_loop_conn_shutdown,
 	.inc_copy_to_user	= rds_message_inc_copy_to_user,
-	.inc_free		= rds_loop_inc_free,
+	.inc_purge		= rds_message_inc_purge,
+	.inc_free		= rds_message_inc_free,
 	.t_name			= "loopback",
-	.t_type			= RDS_TRANS_LOOP,
-	.t_unloading		= rds_loop_is_unloading,
 };
