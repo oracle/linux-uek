ekuiter/torte-linux

xfrm: fix netdev reference count imbalance
In cited commit, netdev_tracker_alloc() is called for the newly
allocated xfrm state, but dev_hold() is missed, which causes netdev
reference count imbalance, because netdev_put() is called when the
state is freed in xfrm_dev_state_free(). Fix the issue by replacing
netdev_tracker_alloc() with netdev_hold().

Fixes: b3eb87c0bdf9 ("xfrm: add TX datapath support for IPsec packet offload mode")
Signed-off-by: Jianbo Liu <jianbol@nvidia.com>
Reviewed-by: Cosmin Ratiu <cratiu@nvidia.com>
Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>

net/xfrm/xfrm_state.c
@@ -1331,8 +1331,7 @@ found:
 			xso->dev = xdo->dev;
 			xso->real_dev = xdo->real_dev;
 			xso->flags = XFRM_DEV_OFFLOAD_FLAG_ACQ;
-			netdev_tracker_alloc(xso->dev, &xso->dev_tracker,
-					     GFP_ATOMIC);
+			netdev_hold(xso->dev, &xso->dev_tracker, GFP_ATOMIC);
 			error = xso->dev->xfrmdev_ops->xdo_dev_state_add(x, NULL);
 			if (error) {
 				xso->dir = 0;

oracle/linux-uek

Fixes: f8a70afafc17 ("xfrm: add TX datapath support for IPsec packet offload mode")
Orabug: 36848687
(cherry picked from commit 9199b915e9fad7f5eff6160d24ff6b38e970107d)
cherry-pick-repo=kernel/git/torvalds/linux.git
Conflicts:
	net/xfrm/xfrm_state.c
Minor conflict in a context caused by Oracle only code for
xdo_dev_state_add_new.
Also, use the old function name dev_hold_track instead of the new name
netdev_hold, since UEK7 currently misses the upstream commit d62607c3fe45
("net: rename reference+tracking helpers").
Signed-off-by: Mikhael Goikhman <migo@nvidia.com>
Signed-off-by: Qing Huang <qing.huang@oracle.com>
Reviewed-by: Devesh Sharma <devesh.s.sharma@oracle.com>
Signed-off-by: Brian Maly <brian.maly@oracle.com>

net/xfrm/xfrm_state.c
@@ -1272,8 +1272,7 @@ found:
 			xso->dev = xdo->dev;
 			xso->real_dev = xdo->real_dev;
 			xso->flags = XFRM_DEV_OFFLOAD_FLAG_ACQ;
-			netdev_tracker_alloc(xso->dev, &xso->dev_tracker,
-					     GFP_ATOMIC);
+			dev_hold_track(xso->dev, &xso->dev_tracker, GFP_ATOMIC);
 			if (xso->dev->xfrmdev_ops->xdo_dev_state_add)
 				error = xso->dev->xfrmdev_ops->xdo_dev_state_add(x);
 			else