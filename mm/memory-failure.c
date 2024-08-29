intel-lab-lkp/linux

mm/memory-failure: move hwpoison_filter() higher up
Move hwpoison_filter() higher up as there is no need to spend a lot
cycles only to find out later that the page is supposed to be skipped
from hwpoison handling.

Signed-off-by: Jane Chu <jane.chu@oracle.com>
Reviewed-by: Oscar Salvador <osalvador@suse.de>
Acked-by: Miaohe Lin <linmiaohe@huawei.com>

mm/memory-failure.c
@@ -2300,6 +2300,18 @@ try_again:
 	}
 
 	folio = page_folio(p);
+
+	/* filter pages that are protected from hwpoison test by users */
+	folio_lock(folio);
+	if (hwpoison_filter(p)) {
+		ClearPageHWPoison(p);
+		folio_unlock(folio);
+		folio_put(folio);
+		res = -EOPNOTSUPP;
+		goto unlock_mutex;
+	}
+	folio_unlock(folio);
+
 	if (folio_test_large(folio)) {
 		/*
 		 * The flag must be set after the refcount is bumped
@@ -2362,14 +2374,6 @@ try_again:
 	 * status correctly, we save a copy of the page flags at this time.
 	 */
 	page_flags = folio->flags;
-
-	if (hwpoison_filter(p)) {
-		ClearPageHWPoison(p);
-		folio_unlock(folio);
-		folio_put(folio);
-		res = -EOPNOTSUPP;
-		goto unlock_mutex;
-	}
 
 	/*
 	 * __munlock_folio() may clear a writeback folio's LRU flag without

oracle/linux-uek

Move hwpoison_filter() higher up as there is no need to spend a lot cycles
only to find out later that the page is supposed to be skipped from
hwpoison handling.
Link: https://lkml.kernel.org/r/20240524215306.2705454-5-jane.chu@oracle.com
Cc: Naoya Horiguchi <nao.horiguchi@gmail.com>
Cc: Oscar Salvador <oalvador@suse.de>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
(cherry picked from commit 9b0ab153d76972a3bee4f363058ff06edf9255a3)
Conflicts:
	mm/memory-failure.c
Due to missing folio conversion, etc, resolve by hand merge.
Orabug: 36947110
Reviewed-by: Sidhartha Kumar <sidhartha.kumar@oracle.com>
Reviewed-by: Larry Bassel <larry.bassel@oracle.com>
Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

mm/memory-failure.c
@@ -1626,6 +1626,16 @@ int memory_failure(unsigned long pfn, int flags)
 		goto unlock_mutex;
 	}
 
+	lock_page(p);
+	if (hwpoison_filter(p)) {
+		if (TestClearPageHWPoison(p))
+			num_poisoned_pages_dec();
+		unlock_page(p);
+		put_hwpoison_page(p);
+		goto unlock_mutex;
+	}
+	unlock_page(p);
+
 	if (PageTransHuge(hpage)) {
 		if (try_to_split_thp_page(p) < 0) {
 			res = -EBUSY;
@@ -1684,13 +1694,6 @@ int memory_failure(unsigned long pfn, int flags)
 	if (!PageHWPoison(p)) {
 		pr_err("Memory failure: %#lx: just unpoisoned\n", pfn);
 		num_poisoned_pages_dec();
-		unlock_page(p);
-		put_hwpoison_page(p);
-		goto unlock_mutex;
-	}
-	if (hwpoison_filter(p)) {
-		if (TestClearPageHWPoison(p))
-			num_poisoned_pages_dec();
 		unlock_page(p);
 		put_hwpoison_page(p);
 		goto unlock_mutex;