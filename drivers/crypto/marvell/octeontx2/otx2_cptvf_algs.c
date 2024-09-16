1cb896de9d1095aaf633161e380b995ad831c5c5
Dave Kleikamp <dave.kleikamp@oracle.com>
2024-09-16 17:36:19 -0500

    Revert "crypto: octeontx2: add support for ECDSA P192, P256 and P384"
    This reverts commit 71dc23310f588730a0c9ca5edad97d8df7235a28.
    
    Orabug: 37062541
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>

--- drivers/crypto/marvell/octeontx2/otx2_cptvf_algs.c
+++ drivers/crypto/marvell/octeontx2/otx2_cptvf_algs.c
@@ -18,7 +18,6 @@
 #include "otx2_cptvf_algs.h"
 #include "otx2_cpt_reqmgr.h"
 #include "cn10k_cpt.h"
-#include "cpt_asym.h"
 
 /* Size of salt in AES GCM mode */
 #define AES_GCM_SALT_SIZE 4
@@ -1892,14 +1891,9 @@ static void swap_func(void *lptr, void *rptr, int size)
 int otx2_cpt_crypto_init(struct pci_dev *pdev, struct module *mod,
 			 int num_queues, int num_devices)
 {
-	struct otx2_cptvf_dev *cptvf = pci_get_drvdata(pdev);
-	int ret, count;
+	int ret = 0;
+	int count;
 
-	ret = cpt_ae_fpm_tbl_get(pdev, &cptvf->fpm_tbl);
-	if (ret) {
-		dev_err(&pdev->dev, "Couldn't get AE fpm table\n");
-		return ret;
-	}
 	mutex_lock(&mutex);
 	count = atomic_read(&se_devices.count);
 	if (count >= OTX2_CPT_MAX_LFS_NUM) {
@@ -1925,18 +1919,13 @@ int otx2_cpt_crypto_init(struct pci_dev *pdev, struct module *mod,
 	sort(se_devices.desc, count, sizeof(struct cpt_device_desc),
 	     compare_func, swap_func);
 
-	mutex_unlock(&mutex);
-	return 0;
-
 unlock:
 	mutex_unlock(&mutex);
-	cpt_ae_fpm_tbl_free(pdev, &cptvf->fpm_tbl);
 	return ret;
 }
 
 void otx2_cpt_crypto_exit(struct pci_dev *pdev, struct module *mod)
 {
-	struct otx2_cptvf_dev *cptvf = pci_get_drvdata(pdev);
 	struct cpt_device_table *dev_tbl;
 	bool dev_found = false;
 	int i, j, count;
@@ -1966,5 +1955,4 @@ void otx2_cpt_crypto_exit(struct pci_dev *pdev, struct module *mod)
 
 unlock:
 	mutex_unlock(&mutex);
-	cpt_ae_fpm_tbl_free(pdev, &cptvf->fpm_tbl);
 }
