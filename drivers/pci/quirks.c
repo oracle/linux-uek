aa41285cefc98469ff50009b33f414c0011003a3
Alan Adamson <alan.adamson@oracle.com>
2024-09-23 16:56:19 -0700

    pci: add hotplug patch support for SOLIDIGM Aura10 AIC 0x025e:0x0b60
    This patch includes the SOLIDIGM Aura10 AIC to the list of devices that
    needs to use the commit c7410ef93b68 "PCI: pciehp: Add quirk to handle spurious
    DLLSC on a x4x4 SSD"
    
    Vendor ID: 0x025e
    Device ID: 0x0b60
    Subsystem ID: 0x48a3
    
    Signed-off-by: Alan Adamson <alan.adamson@oracle.com>
    Reviewed-by: Prasad Singamsetty <prasad.singamsetty@oracle.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>
    Signed-off-by: Brian Maly <brian.maly@oracle.com>
    
    Orabug: 36797601
    
    (hand-merged)
    Signed-off-by: Alan Adamson <alan.adamson@oracle.com>
    Reviewed-by: Himanshu Madhani <himanshu.madhani@oracle.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -6332,4 +6332,5 @@ static void quirk_shared_pcc_and_link_slot(struct pci_dev *pdev)
 }
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_INTEL, 0x0b60, quirk_shared_pcc_and_link_slot);
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_SOLIDIGM, 0x2b59, quirk_shared_pcc_and_link_slot);
+DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_SOLIDIGM, 0x0b60, quirk_shared_pcc_and_link_slot);
 #endif /* CONFIG_HOTPLUG_PCI_PCIE */
