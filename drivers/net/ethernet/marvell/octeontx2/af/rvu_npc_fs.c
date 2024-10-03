5c1c81666fc7939740d00dcf89b06a66eb8d9070
Ratheesh Kannoth <rkannoth@marvell.com>
2024-10-03 15:46:20 +0530

    octeontx2-af: Fix npc_mcam_get_hit_status mbox for cn10k
    cn20k needs to read hit status from different registers. This
    is TBD. virtual index concept is not part of cn10k.
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Change-Id: I3f09840399781fa3ffb3cda4b80e2501332f4967
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/136606
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/137211
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    
    Orabug: 39318904
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc_fs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc_fs.c
@@ -1962,6 +1962,9 @@ int rvu_mbox_handler_npc_mcam_get_hit_status(struct rvu *rvu,
 	int blkaddr;
 	bool clear;
 
+	if (is_cn20k(rvu->pdev))
+		return NPC_MCAM_INVALID_REQ;
+
 	blkaddr = rvu_get_blkaddr(rvu, BLKTYPE_NPC, 0);
 	if (blkaddr < 0)
 		return NPC_MCAM_INVALID_REQ;
@@ -1992,7 +1995,7 @@ int rvu_mbox_handler_npc_mcam_get_hit_status(struct rvu *rvu,
 			if (bank == bank_end && hit_start > hit_end)
 				return 0;
 
-			bitmap = npc_cn20k_vidx2idx(req->mcam_ids[arr_idx]);
+			bitmap = req->mcam_ids[arr_idx];
 			if (bitmap) {
 				val = rvu_read64(rvu, blkaddr,
 						 NPC_AF_MCAM_BANKX_HITX(bank, hit_start));
