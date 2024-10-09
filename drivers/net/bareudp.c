a2ee177db3e482486e8956e0150fce6c2af0bf3d
Menglong Dong <menglong8.dong@gmail.com>
2024-10-09 10:28:21 +0800

    net: tunnel: make skb_vlan_inet_prepare() return drop reasons
    [ Upstream commit 9990ddf47d4168088e2246c3d418bf526e40830d ]
    
    Make skb_vlan_inet_prepare return the skb drop reasons, which is just
    what pskb_may_pull_reason() returns. Meanwhile, adjust all the call of
    it.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 6dedc8c2a4bfc64ef07410634e944c0506eb98c1)
    Orabug: 39055945
    Signed-off-by: Yifei Liu <yifei.l.liu@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/net/bareudp.c
+++ drivers/net/bareudp.c
@@ -312,7 +312,7 @@ static int bareudp_xmit_skb(struct sk_buff *skb, struct net_device *dev,
 	__be32 saddr;
 	int err;
 
-	if (!skb_vlan_inet_prepare(skb, skb->protocol != htons(ETH_P_TEB)))
+	if (skb_vlan_inet_prepare(skb, skb->protocol != htons(ETH_P_TEB)))
 		return -EINVAL;
 
 	if (!sock)
@@ -378,7 +378,7 @@ static int bareudp6_xmit_skb(struct sk_buff *skb, struct net_device *dev,
 	__be16 sport;
 	int err;
 
-	if (!skb_vlan_inet_prepare(skb, skb->protocol != htons(ETH_P_TEB)))
+	if (skb_vlan_inet_prepare(skb, skb->protocol != htons(ETH_P_TEB)))
 		return -EINVAL;
 
 	if (!sock)
