30dbf7804e2bc66ca434022b9fc75bd9b0a90996
Gautham Ananthakrishna <gautham.ananthakrishna@oracle.com>
2024-10-10 07:14:21 +0000

    fs/dcache: allow fractional values in fs.negative-dentry-limit
    The existing sysctl parameter fs.negative-dentry-limit implements a
    means to specify a limit to the amount of negative dentry in the
    system as a percentage of available system memory.
    
    Currently this parameter has a integer range of 0 - 100, 0 means no
    limitation, 1 - 100 is calibrated to 0.1% - 10% of the total system
    memory. So each unit of negative-dentry-limit represents 0.1% the
    system memory, that is (1/1000) th of the total memory.
    
    While working on customer issues, we found the minimal limitation
    0.1% was still too much for system with TB memory, we saw millions
    of dentry from one directory, and it caused systemd in high sys%
    because systemd was watching changes from that directory using
    fsnotify, which required walking the dentry list with dentry lock
    held.
    
    This patch changes the interface from integer to string and allows
    setting fractional values in the range of 0 or 0.001 - 100.
    
    Orabug: 37156523
    
    Signed-off-by: Gautham Ananthakrishna <gautham.ananthakrishna@oracle.com>
    Signed-off-by: Srivathsa Dara <srivathsa.d.dara@oracle.com>
    Reviewed-by: Junxiao Bi <junxiao.bi@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2022,12 +2022,10 @@ static struct ctl_table fs_table[] = {
 	},
 	{
 		.procname	= "negative-dentry-limit",
-		.data		= &neg_dentry_pc,
-		.maxlen		= sizeof(neg_dentry_pc),
+		.data		= &neg_dentry_limit,
+		.maxlen		= NEG_DENTRY_LIMIT_BUFLEN,
 		.mode		= 0644,
 		.proc_handler	= proc_neg_dentry_pc,
-		.extra1		= &zero,
-		.extra2		= &one_hundred,
 	},
 	{ }
 };
