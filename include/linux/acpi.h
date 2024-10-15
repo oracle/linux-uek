2d6078048371529f9468e1de459e62e542974f04
Dave Kleikamp <dave.kleikamp@oracle.com>
2024-10-15 15:28:26 -0500

    Revert "ACPI / PPTT: Provide a helper to walk processor containers"
    This reverts commit 34f8b973558b16898324e97596978dbac60b3fee.
    
    Orabug: 37144820
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Jack Vogel <jack.vogel@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1374,7 +1374,6 @@ static inline int lpit_read_residency_count_address(u64 *address)
 }
 #endif
 
-typedef int (*acpi_pptt_cpu_callback_t)(struct acpi_pptt_processor *, void *);
 #ifdef CONFIG_ACPI_PPTT
 int acpi_pptt_cpu_is_thread(unsigned int cpu);
 int find_acpi_cpu_topology(unsigned int cpu, int level);
@@ -1382,8 +1381,6 @@ int find_acpi_cpu_topology_cluster(unsigned int cpu);
 int find_acpi_cpu_topology_package(unsigned int cpu);
 int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
 int find_acpi_cpu_cache_topology(unsigned int cpu, int level);
-u32 acpi_pptt_count_containers(void);
-int acpi_pptt_for_each_container(acpi_pptt_cpu_callback_t callback, void *arg);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1406,15 +1403,6 @@ static inline int find_acpi_cpu_topology_hetero_id(unsigned int cpu)
 	return -EINVAL;
 }
 static inline int find_acpi_cpu_cache_topology(unsigned int cpu, int level)
-{
-	return -EINVAL;
-}
-static inline u32 acpi_pptt_count_containers(void)
-{
-	return 0;
-}
-static inline int
-acpi_pptt_for_each_container(acpi_pptt_cpu_callback_t *callback, void *arg)
 {
 	return -EINVAL;
 }
