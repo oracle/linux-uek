afad9987773987d27853ddc71dde448685394dde
Daniel P. Smith <dpsmith@apertussolutions.com>
2024-11-05 12:23:32 -0800

    tpm, tpm_tis: Address positive localities in tpm_tis_request_locality()
    Validate that the input locality is within the correct range, as specified
    by TCG standards, and increase the locality count also for the positive
    localities.
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Signed-off-by: Jarkko Sakkinen <jarkko@kernel.org>
    
    Orabug: 33297039
    
    (cherry picked from commit 366a92941dc5e6a56d2ce8bd232379f904f05bd3)
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Kanth Ghatraju <kanth.ghatraju@oracle.com>

--- drivers/char/tpm/tpm_tis_core.c
+++ drivers/char/tpm/tpm_tis_core.c
@@ -233,10 +233,16 @@ static int tpm_tis_request_locality(struct tpm_chip *chip, int l)
 	struct tpm_tis_data *priv = dev_get_drvdata(&chip->dev);
 	int ret = 0;
 
+	if (l < 0 || l > TPM_MAX_LOCALITY) {
+		dev_warn(&chip->dev, "%s: failed to request unknown locality: %d\n",
+			 __func__, l);
+		return -EINVAL;
+	}
+
 	mutex_lock(&priv->locality_count_mutex);
 	if (priv->locality_count == 0)
 		ret = __tpm_tis_request_locality(chip, l);
-	if (!ret)
+	if (ret >= 0)
 		priv->locality_count++;
 	mutex_unlock(&priv->locality_count_mutex);
 	return ret;
