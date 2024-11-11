5ac51c642de34e3b8bc1a8d50b3da2c8c90c9800
Kim Phillips <kim.phillips@amd.com>
2024-11-11 17:37:34 +0100

    x86/process: Move the buffer clearing before MONITOR
    Do it before the monitor so that VERW doesn't disarm the monitor and
    thus never enter C1.
    
      [ bp: Have it call the idle variant in mwait_idle() too. ]
    
    Signed-off-by: Kim Phillips <kim.phillips@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    
    Orabug: 38172250
    CVE: CVE-2024-36350, CVE-2024-36357
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Ankur Arora <ankur.a.arora@oracle.com>
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -724,6 +724,8 @@ static __cpuidle void mwait_idle(void)
 
 		x86_spec_ctrl_set(SPEC_CTRL_IDLE_ENTER);
 
+		x86_idle_clear_cpu_buffers();
+
 		__monitor((void *)&current_thread_info()->flags, 0, 0);
 		if (!need_resched()) {
 			__sti_mwait(0, 0);
