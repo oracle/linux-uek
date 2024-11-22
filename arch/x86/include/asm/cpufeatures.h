dfa9d05ea246bb7f1077617fa3094d909c612819
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2024-11-22 16:28:38 -0500

    x86/bugs: Adjust SRSO mitigation to new features
    If the machine has:
    
      CPUID Fn8000_0021_EAX[30] (SRSO_USER_KERNEL_NO) -- If this bit is 1, it
      indicates the CPU is not subject to the SRSO vulnerability across
      user/kernel boundaries.
    
    have it fall back to IBPB on VMEXIT only, in the case it is going to run
    VMs:
    
      Speculative Return Stack Overflow: CPU user/kernel transitions protected, falling back to IBPB-on-VMEXIT
      Speculative Return Stack Overflow: Mitigation: IBPB on VMEXIT only
    
    Then, on the first guest start and in case the machine has
    
      CPUID Fn8000_0021_EAX[31] (SRSO_MSR_FIX). If this bit is 1, it indicates
      that software may use MSR BP_CFG[BpSpecReduce] to mitigate SRSO.
    
    enable this BpSpecReduce bit to mitigate SRSO across guest/host
    boundaries.
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    
    Orabug: 37347449
    
    This is an out-of-tree patch, it will go upstream later. See
    https://www.amd.com/content/dam/amd/en/documents/corporate/cr/speculative-return-stack-overflow-whitepaper.pdf
    for definition of new bits.
    
    Changes to the AMD patch:
    * kvm_set_cpu_caps(): expose IBPB_BRTYPE and SRSO_USER_KERNEL_NO bits to leaf 0x80000021
      so that they can be passed to guest.
    * Don't try to remember which VCPU set the MSR BP_CFG[BpSpecReduce]
      since this doesn't work when VCPU thread migrates. Instead, set the
      bit on all CPUs in svm_hardware_enable(). FTR, the original hunk was
      added to svm_prepare_guest_switch() with:
    
        if (cpu_feature_enabled(X86_FEATURE_SRSO_MSR_FIX)) {
             if (!svm->bp_spec_reduce) {
                    if (msr_set_bit(MSR_ZEN4_BP_CFG, MSR_ZEN4_BP_CFG_BP_SPEC_REDUCE_BIT) >= 0)
                            svm->bp_spec_reduce = true;
                    else
                            WARN_ON_ONCE(1);
            }
       }
    
    Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Reviewed-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -470,6 +470,8 @@
 #define X86_FEATURE_SBPB		(20*32+27) /* Selective Branch Prediction Barrier */
 #define X86_FEATURE_IBPB_BRTYPE		(20*32+28) /* MSR_PRED_CMD[IBPB] flushes all branch type predictions */
 #define X86_FEATURE_SRSO_NO		(20*32+29) /* CPU is not affected by SRSO */
+#define X86_FEATURE_SRSO_USER_KERNEL_NO	(20*32+30) /* CPU is not affected by SRSO across user/kernel boundaries */
+#define X86_FEATURE_SRSO_MSR_FIX	(20*32+31) /* MSR BP_CFG[BpSpecReduce] can be used to mitigate SRSO */
 
 /*
  * Extended auxiliary flags: Linux defined - for features scattered in various
