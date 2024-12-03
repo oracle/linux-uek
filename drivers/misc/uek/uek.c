12829d2b8ff408087efaf8d4d4b94ffb0a361d3d
Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
2024-12-03 19:35:04 +0000

    uek-misc: Add identification for OCI platform
    The Oracle Server UEFI BIOS Specification does not assign an identifier
    in SMBIOS OEM Strings structures to uniquely identify OCI hosts.
    Therefore, categorize as OCI all Oracle platforms that do not have the
    Exadata specific signature in DMI, and instead use the general purpose
    system (GPS) string "00000000", as documented Chapter 39.6.9.1 - PRMS
    Requirement for Type 11.
    New uek module parameter options are provided i.e. 'uek=oci|nooci' in
    order to override the default behavior when necessary.
    
    Jira: LINUX-15531
    Orabug: 37459409
    
    Suggested-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

--- drivers/misc/uek/uek.c
+++ drivers/misc/uek/uek.c
@@ -12,7 +12,7 @@
 #include <asm/virt.h>
 #endif
 
-#define UEK_MISC_VER  "0.2"
+#define UEK_MISC_VER  "0.3"
 
 MODULE_AUTHOR("Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>");
 MODULE_DESCRIPTION("uek");
@@ -21,14 +21,41 @@ MODULE_VERSION(UEK_MISC_VER);
 
 DEFINE_STATIC_KEY_FALSE(on_exadata);
 DEFINE_STATIC_KEY_FALSE(cls_enabled);
+
 EXPORT_SYMBOL_GPL(on_exadata);
 EXPORT_SYMBOL_GPL(cls_enabled);
 
 DEFINE_STATIC_KEY_FALSE(on_oci);
 EXPORT_SYMBOL_GPL(on_oci);
 
+
+/*
+ * The Oracle Server UEFI BIOS Specification lists requirements imposed by PRMS
+ * (Platform Resource Management Specification) for SMBIOS structures.
+ * PRMS-003 requires an OEM Strings (Type 11) structure with a string of the
+ * form "SUNW-PRMS-1".
+ */
+#define PRMS_003_ID		"SUNW-PRMS-1"
+
+/*
+ * In Type 11 structures (OEM Strings), and specifically Type 11 String 4, BIOS
+ * must provide system profile identification by setting String 4 to "00010000"
+ * for Exadata systems, and "00000000" for general purpose systems (GPS).
+ */
+#define EXADATA_STR4_ID		"00010000"
+#define GPS_STR4_ID		"00000000"
+
+enum orcl_platform_type {
+	ORCL_EXADATA,
+	ORCL_OCI,
+	ORCL_NOID,
+};
+
 /* Override to disable optimizations on Exadata systems. */
 static bool exadata_disable;
+
+/* Disables oci specific behavior */
+static bool oci_disable;
 
 static int __init uek_params(char *str)
 {
@@ -41,10 +68,14 @@ static int __init uek_params(char *str)
 	} else if ((strncmp(str, "noexadata", 9) == 0)) {
 		exadata_disable = true;
 		return 1;
-	} else if (strncmp(str, "oci", 3) == 0) {
-		static_branch_enable(&on_oci);
 	} else if (strncmp(str, "cls", 3) == 0) {
 		static_branch_enable(&cls_enabled);
+		return 1;
+	} else if (strncmp(str, "oci", 3) == 0) {
+		static_branch_enable(&on_oci);
+		return 1;
+	} else if (strncmp(str, "nooci", 5) == 0) {
+		oci_disable = true;
 		return 1;
 	}
 
@@ -52,9 +83,16 @@ static int __init uek_params(char *str)
 }
 __setup("uek=", uek_params);
 
-static int detect_exadata_dmi(char **reason)
+static inline bool dmi_oem_string_present(const char *id_str)
 {
-	static const char * const oemstrs[] = {"SUNW-PRMS-1", "00010000"};
+	return !!dmi_find_device(DMI_DEV_TYPE_OEM_STRING, id_str, NULL);
+}
+
+/*
+ * Attempt to identify the running platform based on DMI structures.
+ */
+static enum orcl_platform_type detect_platform_dmi(void)
+{
 	static const struct dmi_system_id oracle_mbs[] = {
 		{
 			.matches = {
@@ -69,59 +107,115 @@ static int detect_exadata_dmi(char **reason)
 		{}
 	};
 
-	unsigned int i, ok = 0;
-
-	/* Not Oracle system? .. Bye. */
+	/* Attempt to identify an Oracle platform signature */
 	if (!dmi_check_system(oracle_mbs))
-		goto err;
+		return ORCL_NOID;
 
-	/* Check for Type 11 and make sure it has the right markings. */
-	for (i = 0; i < ARRAY_SIZE(oemstrs); i++)
-		if (dmi_find_device(DMI_DEV_TYPE_OEM_STRING, oemstrs[i], NULL))
-			ok++;
+	/*
+	 * Query OEM Strings structures (Type 11) for identifying data.
+	 *
+	 * A requirement is that platforms implementing all or part of the
+	 * PRMS specification must contain an OEM Strings (Type 11) structure
+	 * with a string of the form "SUNW-PRMS-1" (PRMS-003).
+	 *
+	 * Additionally, PRMS requirements state that Exadata systems set
+	 * String 4 to "00010000", while other systems have "00000000".
+	 * Use this to uniquely identify Exadata platforms.
+	 */
 
-	if (ok == 2) {
-		*reason = "via DMI";
-		return 0;
+	/* Validate that "SUNW-PRMS-1" is present */
+	if (!dmi_oem_string_present(PRMS_003_ID)) {
+		pr_debug("SMBIOS data could be missing or incomplete");
+		return ORCL_NOID;
 	}
 
-err:
-	return -ENODEV;
+	/* Search for specific system profiles encoded in OEM Strings struct */
+	if (dmi_oem_string_present(EXADATA_STR4_ID)) {
+		/* Exadata signature detected*/
+		return ORCL_EXADATA;
+	} else if (dmi_oem_string_present(GPS_STR4_ID)) {
+		/*
+		 * Categorize Oracle platforms with general purpose signature
+		 * as being an OCI platform.
+		 * Refine further if a unique identifier is later assigned to
+		 * OCI system profile.
+		 */
+		return ORCL_OCI;
+	} else {
+		/*
+		 * No known system profile detected on OEM Strings structure
+		 */
+		pr_debug("Unable to identify system profile from OEM Strings");
+		return ORCL_NOID;
+	}
 }
 
-static int detect_exadata_bootline(char **reason)
+static enum orcl_platform_type detect_bootline_options(void)
 {
 	if (static_key_enabled(&on_exadata)) {
-		*reason = "via command line";
-		return 0;
+		return ORCL_EXADATA;
 	}
-	return -ENODEV;
+	if (static_key_enabled(&on_oci)) {
+		return ORCL_OCI;
+	}
+	return ORCL_NOID;
+}
+
+static inline void enable_exadata(char **reason)
+{
+	static_branch_enable(&on_exadata);
+	pr_info("Detected Exadata (%s)", *reason);
+}
+
+static inline void enable_oci(char **reason)
+{
+	static_branch_enable(&on_oci);
+	pr_info("Detected OCI (%s)", *reason);
 }
 
 static int uek_misc_init(void)
 {
-	int ret;
+	enum orcl_platform_type plat_detected = ORCL_NOID;
 	char *reason = NULL;
 
-	/* Boot time override engaged */
-	if (exadata_disable)
+	/* Boot time override options */
+	if (exadata_disable && oci_disable)
 		return -ENODEV;
 
-	ret = detect_exadata_bootline(&reason);
-	if (!ret)
-		goto enable;
+	plat_detected = detect_bootline_options();
 
-	ret = detect_exadata_dmi(&reason);
-	if (ret)
-		return ret;
+	if (plat_detected != ORCL_NOID) {
+		reason = "via command line";
+	} else {
+		/*
+		 * If a platform option was not specified in the kernel
+		 * commandline via 'uek=*' parameter, fallback to try to
+		 * determine the platform using DMI info.
+		 */
+		plat_detected = detect_platform_dmi();
+		reason = "via DMI";
+	}
 
-enable:
-	/* Go-Go Exadata goodness! */
-	static_branch_enable(&on_exadata);
+	switch (plat_detected) {
+	case ORCL_EXADATA:
+		if (exadata_disable)
+			return -ENODEV;
+		/* Go-Go Exadata goodness! */
+		enable_exadata(&reason);
+		return 0;
+	case ORCL_OCI:
+		if (oci_disable)
+			return -ENODEV;
+		enable_oci(&reason);
+		return 0;
+	case ORCL_NOID:
+	default:
+		pr_debug("Unable to identify platform");
+		return -ENODEV;
+	}
 
-	pr_info("Detected Exadata (%s)", reason);
-
-	return 0;
+	/* Unreachable */
+	return -ENODEV;
 }
 
 core_initcall(uek_misc_init);
