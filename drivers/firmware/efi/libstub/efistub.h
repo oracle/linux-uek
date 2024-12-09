ae9f05ff5a3f02a197069a4ced3cfb5929a96ae8
Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>
2024-12-09 13:20:39 -0500

    efi/libstub: Bump up EFI_MMAP_NR_SLACK_SLOTS to 32
    commit ec4696925da6b9baec38345184403ce9e29a2e48 upstream.
    
    Recent platforms require more slack slots than the current value of
    EFI_MMAP_NR_SLACK_SLOTS, otherwise they fail to boot. The current
    workaround is to append `efi=disable_early_pci_dma` to the kernel's
    cmdline. So, bump up EFI_MMAP_NR_SLACK_SLOTS to 32 to allow those
    platforms to boot with the aforementioned workaround.
    
    Signed-off-by: Hamza Mahfooz <hamzamahfooz@linux.microsoft.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Allen Pais <apais@linux.microsoft.com>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 66d182770fbfef05421b101d13be572bab65e127)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/firmware/efi/libstub/efistub.h
+++ drivers/firmware/efi/libstub/efistub.h
@@ -179,7 +179,7 @@ void efi_set_u64_form(u32 lo, u32 hi, u64 *data)
  * the EFI memory map. Other related structures, e.g. x86 e820ext, need
  * to factor in this headroom requirement as well.
  */
-#define EFI_MMAP_NR_SLACK_SLOTS	8
+#define EFI_MMAP_NR_SLACK_SLOTS	32
 
 typedef struct efi_generic_dev_path efi_device_path_protocol_t;
 
