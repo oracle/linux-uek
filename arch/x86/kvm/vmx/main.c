a6b1e9b1fb289b4444c4c078385b4ec3c05d0068
Isaku Yamahata <isaku.yamahata@intel.com>
2024-12-10 08:49:43 +0800

    KVM: TDX: Make TDX VM type supported
    Now all the necessary code for TDX is in place, it's ready to run TDX
    guest.  Advertise the VM type of KVM_X86_TDX_VM so that the user space
    VMM like QEMU can start to use it.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Signed-off-by: Binbin Wu <binbin.wu@linux.intel.com>
    [TDX "the rest" v2:
     - No change.
    TDX "the rest" v1:
     - Move down to the end of patch series.]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 161d34609f9b319d0706eaf7d2cb7a2731251b25)
    Orabug: 38359602
    Signed-off-by: Ross Philipson <ross.philipson@oracle.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>

--- arch/x86/kvm/vmx/main.c
+++ arch/x86/kvm/vmx/main.c
@@ -1084,6 +1084,7 @@ static int __init vt_init(void)
 				sizeof(struct vcpu_tdx));
 		vcpu_align = max_t(unsigned, vcpu_align,
 				__alignof__(struct vcpu_tdx));
+		kvm_caps.supported_vm_types |= BIT(KVM_X86_TDX_VM);
 	}
 
 	/*
