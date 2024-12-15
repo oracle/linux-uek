981b4c740948f13ab8bf2612474efb849df71017
Vegard Nossum <vegard.nossum@oracle.com>
2024-12-15 12:32:09 +0000

    crypto/fips140: add FIPS 140 module loader
    Add a mechanism for loading a FIPS module (and its checksum) from a byte
    array compiled into vmlinux.
    
    fips140-module.o and fips140-digest.o are files generated during the build
    of the FIPS cryptographic module; fips140-module.o provides the following
    symbols:
    
     - _binary_fips140_ko_start
     - _binary_fips140_ko_end
    
    while fips140-digest.o provides the following symbol:
    
     - _binary_fips140_hmac_start
    
    Provide two new config options:
    
    - CONFIG_CRYPTO_FIPS_LOADER: include the FIPS module loader in vmlinux.
    - CONFIG_CRYPTO_FIPS_MODULE: without this, the FIPS module is just an
      empty array (useful for building a kernel first, then building the
      FIPS module against it, then building a new kernel with the FIPS
      module included).
    
    Co-developed-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- include/linux/fips.h
+++ include/linux/fips.h
@@ -2,11 +2,28 @@
 #ifndef _FIPS_H
 #define _FIPS_H
 
+#include <linux/init.h>
+
+#include <crypto/sha2.h> /* SHA256_DIGEST_SIZE */
+
 #ifdef CONFIG_CRYPTO_FIPS
+/*
+ * fips_enabled = FIPS mode was requested on the command line
+ * fips_operational = FIPS module has run self-tests etc. and is operational
+ */
 extern int fips_enabled;
+extern int fips_operational;
+
 extern struct atomic_notifier_head fips_fail_notif_chain;
 
 void fips_fail_notify(void);
+
+/* FIPS-certified module blob and digest */
+extern const u8 __initconst _binary_fips140_ko_start[];
+extern const u8 __initconst _binary_fips140_ko_end[];
+extern const u8 __initconst _binary_fips140_hmac_start[SHA256_DIGEST_SIZE];
+
+#define _binary_fips140_ko_size (_binary_fips140_ko_end - _binary_fips140_ko_start)
 
 #else
 #define fips_enabled 0
