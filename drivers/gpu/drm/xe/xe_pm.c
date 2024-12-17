e5f2a3ec6650a26d720698753242ab90122ba27a
Rodrigo Vivi <rodrigo.vivi@intel.com>
2024-12-17 18:05:47 -0500

    drm/xe/pm: Also avoid missing outer rpm warning on system suspend
    [ Upstream commit f2eedadf19979109415928f5ea9ba9a73262aa8f ]
    
    Fix the false-positive "Missing outer runtime PM protection" warning
    triggered by
    release_async_domains() -> intel_runtime_pm_get_noresume() ->
    xe_pm_runtime_get_noresume()
    during system suspend.
    
    xe_pm_runtime_get_noresume() is supposed to warn if the device is not in
    the runtime resumed state, using xe_pm_runtime_get_if_in_use() for this.
    However the latter function will fail if called during runtime or system
    suspend/resume, regardless of whether the device is runtime resumed or
    not.
    
    Based on the above suppress the warning during system suspend/resume,
    similarly to how this is done during runtime suspend/resume.
    
    Suggested-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241217230547.1667561-1-rodrigo.vivi@intel.com
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Stable-dep-of: bb36170d959f ("drm/xe/pm: Disable D3Cold for BMG only on specific platforms")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit c8a5ec95c96168853f2bcc68f53942e6c0421472)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/gpu/drm/xe/xe_pm.c
+++ drivers/gpu/drm/xe/xe_pm.c
@@ -6,6 +6,7 @@
 #include "xe_pm.h"
 
 #include <linux/pm_runtime.h>
+#include <linux/suspend.h>
 
 #include <drm/drm_managed.h>
 #include <drm/ttm/ttm_placement.h>
@@ -622,7 +623,8 @@ static bool xe_pm_suspending_or_resuming(struct xe_device *xe)
 	struct device *dev = xe->drm.dev;
 
 	return dev->power.runtime_status == RPM_SUSPENDING ||
-		dev->power.runtime_status == RPM_RESUMING;
+		dev->power.runtime_status == RPM_RESUMING ||
+		pm_suspend_target_state != PM_SUSPEND_ON;
 #else
 	return false;
 #endif
