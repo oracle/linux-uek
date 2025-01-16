434d2f7c8501cf9751e65c66333b15b7704aa42f
John Garry <john.g.garry@oracle.com>
2025-01-16 17:02:54 +0000

    block: Add common atomic writes enable flag
    Currently only stacked devices need to explicitly enable atomic writes by
    setting BLK_FEAT_ATOMIC_WRITES_STACKED flag.
    
    This does not work well for device mapper stacking devices, as there many
    sets of limits are stacked and what is the 'bottom' and 'top' device can
    swapped. This means that BLK_FEAT_ATOMIC_WRITES_STACKED needs to be set
    for many queue limits, which is messy.
    
    Generalize enabling atomic writes enabling by ensuring that all devices
    must explicitly set a flag - that includes NVMe, SCSI sd, and md raid.
    
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    Reviewed-by: Mike Snitzer <snitzer@kernel.org>
    Link: https://lore.kernel.org/r/20250116170301.474130-2-john.g.garry@oracle.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Orabug: 38026089
    
    (cherry picked from commit 6a7e17b22062c84a111d7073c67cc677c4190f32)
    
    Acked-by: "Darrick J. Wong" <darrick.wong@oracle.com>
    Signed-off-by: John Garry <john.g.garry@oracle.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/md/raid10.c
+++ drivers/md/raid10.c
@@ -4023,7 +4023,7 @@ static int raid10_set_queue_limits(struct mddev *mddev)
 	lim.max_write_zeroes_sectors = 0;
 	lim.io_min = mddev->chunk_sectors << 9;
 	lim.io_opt = lim.io_min * raid10_nr_stripes(conf);
-	lim.features |= BLK_FEAT_ATOMIC_WRITES_STACKED;
+	lim.features |= BLK_FEAT_ATOMIC_WRITES;
 	err = mddev_stack_rdev_limits(mddev, &lim, MDDEV_STACK_INTEGRITY);
 	if (err)
 		return err;
