fb3e8b2e3dca8a2996c487f74eb30de950642078
Jakub Kicinski <kuba@kernel.org>
2025-01-23 15:16:20 -0800

    net: page_pool: don't try to stash the napi id
    Page ppol tried to cache the NAPI ID in page pool info to avoid
    having a dependency on the life cycle of the NAPI instance.
    Since commit under Fixes the NAPI ID is not populated until
    napi_enable() and there's a good chance that page pool is
    created before NAPI gets enabled.
    
    Protect the NAPI pointer with the existing page pool mutex,
    the reading path already holds it. napi_id itself we need
    to READ_ONCE(), it's protected by netdev_lock() which are
    not holding in page pool.
    
    Before this patch napi IDs were missing for mlx5:
    
     # ./cli.py --spec netlink/specs/netdev.yaml --dump page-pool-get
    
     [{'id': 144, 'ifindex': 2, 'inflight': 3072, 'inflight-mem': 12582912},
      {'id': 143, 'ifindex': 2, 'inflight': 5568, 'inflight-mem': 22806528},
      {'id': 142, 'ifindex': 2, 'inflight': 5120, 'inflight-mem': 20971520},
      {'id': 141, 'ifindex': 2, 'inflight': 4992, 'inflight-mem': 20447232},
      ...
    
    After:
    
     [{'id': 144, 'ifindex': 2, 'inflight': 3072, 'inflight-mem': 12582912,
       'napi-id': 565},
      {'id': 143, 'ifindex': 2, 'inflight': 4224, 'inflight-mem': 17301504,
       'napi-id': 525},
      {'id': 142, 'ifindex': 2, 'inflight': 4288, 'inflight-mem': 17563648,
       'napi-id': 524},
      ...
    
    Fixes: 86e25f40aa1e ("net: napi: Add napi_config")
    Reviewed-by: Mina Almasry <almasrymina@google.com>
    Reviewed-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://patch.msgid.link/20250123231620.1086401-1-kuba@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Orabug: 37944844
    
    (cherry picked from commit 67e4bb2ced0f2d8fbd414f932daea94ba63ae4c4)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    unmodified-from-upstream: 67e4bb2ced0f2d8fbd414f932daea94ba63ae4c4
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -6646,7 +6646,7 @@ EXPORT_SYMBOL(napi_busy_loop);
 static void __napi_hash_add_with_id(struct napi_struct *napi,
 				    unsigned int napi_id)
 {
-	napi->napi_id = napi_id;
+	WRITE_ONCE(napi->napi_id, napi_id);
 	hlist_add_head_rcu(&napi->napi_hash_node,
 			   &napi_hash[napi->napi_id % HASH_SIZE(napi_hash)]);
 }
