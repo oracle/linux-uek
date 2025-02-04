d8969ac0b1a0956432879a31bd755ef8f8300f1f
Subbaraya Sundeep <sbhatta@marvell.com>
2025-02-03 23:03:24 -0800

    octeontx2-af: Misc changes in NIX block
    This patch covers changes which were missed from 6.1
    while upstreaming.
    
    Change-Id: I8e61d98bc80c7c72d733a9633aca56142f5f5697
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    
    Orabug: 39318904
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_nix.c
@@ -318,14 +318,22 @@ static bool is_valid_txschq(struct rvu *rvu, int blkaddr,
 		if ((nix_get_tx_link(rvu, map_func) !=
 		     nix_get_tx_link(rvu, pcifunc)) &&
 		     (rvu_get_pf(rvu->pdev, map_func) !=
-				rvu_get_pf(rvu->pdev, pcifunc)))
+				rvu_get_pf(rvu->pdev, pcifunc))) {
+			dev_err(rvu->dev,
+				"Invalid tx link: pcifunc %x map pcifunc %x\n",
+				pcifunc, map_func);
 			return false;
-		else
+		} else {
 			return true;
+		}
 	}
 
-	if (map_func != pcifunc)
+	if (map_func != pcifunc) {
+		dev_err(rvu->dev,
+			"pcifunc %x map pcifunc %x not equal\n", pcifunc,
+			map_func);
 		return false;
+	}
 
 	return true;
 }
@@ -635,7 +643,8 @@ int rvu_mbox_handler_nix_alloc_bpids(struct rvu *rvu,
 	 * application. Find the bpid is it already allocate or
 	 * allocate a new one.
 	 */
-	if (req->type > NIX_INTF_TYPE_CPT) {
+	mutex_lock(&rvu->rsrc_lock);
+	if (req->type > NIX_INTF_TYPE_CPT || req->type == NIX_INTF_TYPE_LBK) {
 		for (bpid = 0; bpid < bp->bpids.max; bpid++) {
 			if (bp->intf_map[bpid] == req->type) {
 				rsp->bpids[cnt] = bpid + bp->free_pool_base;
@@ -645,19 +654,21 @@ int rvu_mbox_handler_nix_alloc_bpids(struct rvu *rvu,
 			}
 		}
 		if (rsp->bpid_cnt)
-			return 0;
+			goto exit;
 	}
 
 	for (cnt = 0; cnt < req->bpid_cnt; cnt++) {
 		bpid = rvu_alloc_rsrc(&bp->bpids);
 		if (bpid < 0)
-			return 0;
+			goto exit;
 		rsp->bpids[cnt] = bpid + bp->free_pool_base;
 		bp->intf_map[bpid] = req->type;
 		bp->fn_map[bpid] = pcifunc;
 		bp->ref_cnt[bpid]++;
 		rsp->bpid_cnt++;
 	}
+exit:
+	mutex_unlock(&rvu->rsrc_lock);
 	return 0;
 }
 
@@ -676,6 +687,7 @@ int rvu_mbox_handler_nix_free_bpids(struct rvu *rvu,
 		return err;
 
 	bp = &nix_hw->bp;
+	mutex_lock(&rvu->rsrc_lock);
 	for (cnt = 0; cnt < req->bpid_cnt; cnt++) {
 		bpid = req->bpids[cnt] - bp->free_pool_base;
 		bp->ref_cnt[bpid]--;
@@ -687,6 +699,7 @@ int rvu_mbox_handler_nix_free_bpids(struct rvu *rvu,
 				bp->fn_map[id] = 0;
 		}
 	}
+	mutex_unlock(&rvu->rsrc_lock);
 	return 0;
 }
 
@@ -1670,6 +1683,7 @@ int rvu_mbox_handler_nix_cn10k_aq_enq(struct rvu *rvu,
 	return rvu_nix_aq_enq_inst(rvu, (struct nix_aq_enq_req *)req,
 				  (struct nix_aq_enq_rsp *)rsp);
 }
+EXPORT_SYMBOL(rvu_mbox_handler_nix_cn10k_aq_enq);
 
 int rvu_mbox_handler_nix_hwctx_disable(struct rvu *rvu,
 				       struct hwctx_disable_req *req,
@@ -1861,6 +1875,9 @@ int rvu_mbox_handler_nix_lf_alloc(struct rvu *rvu,
 	if (is_rep_dev(rvu, pcifunc)) {
 		pfvf->tx_chan_base = RVU_SWITCH_LBK_CHAN;
 		pfvf->tx_chan_cnt = 1;
+		/* Setting the TX link as that of LBK */
+		rsp->tx_link = hw->cgx_links;
+		rvu_npc_set_pkind(rvu, NPC_RX_LBK_PKIND, pfvf);
 		goto exit;
 	}
 
@@ -1892,8 +1909,9 @@ free_mem:
 	rc = -ENOMEM;
 
 exit:
-	/* Set macaddr of this PF/VF */
-	ether_addr_copy(rsp->mac_addr, pfvf->mac_addr);
+	if (!is_rep_dev(rvu, pcifunc))
+		/* Set macaddr of this PF/VF */
+		ether_addr_copy(rsp->mac_addr, pfvf->mac_addr);
 
 	/* set SQB size info */
 	cfg = rvu_read64(rvu, blkaddr, NIX_AF_SQ_CONST);
@@ -2300,7 +2318,6 @@ static int nix_check_txschq_alloc_req(struct rvu *rvu, int lvl, u16 pcifunc,
 		return 0;
 
 	link = nix_get_tx_link(rvu, pcifunc);
-
 	/* For traffic aggregating scheduler level, one queue is enough */
 	if (lvl >= hw->cap.nix_tx_aggr_lvl) {
 		if (req_schq != 1)
@@ -2728,6 +2745,8 @@ static int nix_smq_flush(struct rvu *rvu, int blkaddr,
 		link = (cgx_id * rvu->hw->lmac_per_cgx) + lmac_id;
 		dev_info(rvu->dev, "NIX_AF_TX_LINKX_NORM_CREDIT:0x%llx\n",
 			 rvu_read64(rvu, blkaddr, NIX_AF_TX_LINKX_NORM_CREDIT(link)));
+		dev_info(rvu->dev, "NIX_AF_AQ_STATUS:0x%llx\n",
+			 rvu_read64(rvu, blkaddr, NIX_AF_AQ_STATUS));
 	}
 
 	/* Set NIX_AF_TL3_TL2_LINKX_CFG[ENA] for the TL3/TL2 queue */
@@ -3283,6 +3302,7 @@ static int nix_tx_vtag_alloc(struct rvu *rvu, int blkaddr,
 	mutex_unlock(&vlan->rsrc_lock);
 
 	regval = size ? vtag : vtag << 32;
+	regval |= (vtag & ~GENMASK_ULL(47, 0)) << 48;
 
 	rvu_write64(rvu, blkaddr,
 		    NIX_AF_TX_VTAG_DEFX_DATA(index), regval);
@@ -5251,10 +5271,6 @@ static int rvu_nix_block_init(struct rvu *rvu, struct nix_hw *nix_hw)
 			return err;
 
 		err = nix_setup_txvlan(rvu, nix_hw);
-		if (err)
-			return err;
-
-		err = nix_setup_bpids(rvu, nix_hw, blkaddr);
 		if (err)
 			return err;
 
