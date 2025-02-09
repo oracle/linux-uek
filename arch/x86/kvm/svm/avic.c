oracle/linux-uek

commit cff47850c96902297fb060248609b374eada744e

uek-misc: Clear the AVIC capability on platforms with known errata
To avoid modifying the KVM/SVM code, remove the AVIC capability from
being advertised in the cpuinfo_x86::x86_capability array, so that SVM
code does not detect its presence and avoids initialization of AVIC data
structures on platforms with known errata.

As a proxy for a platform identifier, use the fact that only Zen4 and
newer platforms have the X2AVIC capability, and hide the AVIC support
from the earlier ones. The exception to this is Exadata which does
enable AVIC on Zen3/Milan/E4, so this modification is only done on
systems not identified as Exadata by the heuristics employed in the uek
module.

Jira: LINUX-15531
Orabug: 37459410

Suggested-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
Reviewed-by: Joao Martins <joao.m.martins@oracle.com>
Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

drivers/misc/uek/uek.c
@@ -169,6 +169,23 @@ static inline void enable_oci(char **reason)
 {
 	static_branch_enable(&on_oci);
 	pr_info("Detected OCI (%s)", *reason);
+
+#ifdef CONFIG_X86_64
+	/*
+	 * For certain platforms with known errata we avoid advertising support
+	 * for the AVIC feature so that initialization is not attempted by SVM
+	 * code.
+	 * As a proxy for a platform identifier, use the fact that only Zen4 and
+	 * newer platforms have X2AVIC capability, and avoid exposing the AVIC
+	 * capability on earlier ones.
+	 */
+	if (!boot_cpu_has(X86_FEATURE_X2AVIC) &&
+	    boot_cpu_has(X86_FEATURE_AVIC)) {
+		pr_notice("Disabling AVIC on pre-Zen4 processors\n");
+		setup_clear_cpu_cap(X86_FEATURE_AVIC);
+	}
+#endif
+
 }
 
 static int uek_misc_init(void)

commit c6f5d7398a51bc9a4ea0326cf6da5e9b3fd47da4

uek-misc: Move AVIC selective enablement check to SVM module
The uek module removes the AVIC capability from being advertised so that the
SVM code does not attempt initialization of AVIC data structures on platforms
with known errata in OCI. It does so by unsetting the correspondig bit in the
cpuinfo_x86::x86_capability array. However, this approach triggers a warning
after the recent commit ee8962082a441 ("x86/alternatives: Catch late X86_FEATURE
modifiers").

Move the logic disabling AVIC from the uek module to a more appropriate location
in the SVM/AVIC initialization code that already evaluates various blocking
conditions. The platform identification code, which sets the on_oci static key,
remains in the uek module, and KVM's dependency on it ensures the correct
loading order even in non-default builds where uek is not built-in.

Orabug: 37496123

Fixes: cff47850c9690 ("uek-misc: Clear the AVIC capability on platforms with known errata")
Signed-off-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>

arch/x86/kvm/svm/avic.c
@@ -1200,6 +1200,21 @@ bool avic_hardware_setup(void)
 	}
 
 	if (boot_cpu_has(X86_FEATURE_AVIC)) {
+#ifndef WITHOUT_ORACLE_EXTENSIONS
+		if (!boot_cpu_has(X86_FEATURE_X2AVIC) &&
+		    static_key_enabled(&on_oci)) {
+			/*
+			 * For certain platforms with known errata, avoid
+			 * enabling the AVIC feature even if firmware advertises
+			 * the capability.
+			 * As a proxy for a platform identifier, use the fact
+			 * that only Zen4 and newer platforms have X2AVIC
+			 * capability, and avoid enabling AVIC on earlier ones.
+			 */
+			pr_notice("Disabling AVIC on pre-Zen4 processors\n");
+			return false;
+		}
+#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 		pr_info("AVIC enabled\n");
 	} else if (force_avic) {
 		/*

drivers/misc/uek/uek.c
@@ -171,23 +171,6 @@ static inline void enable_oci(char **reason)
 {
 	static_branch_enable(&on_oci);
 	pr_info("Detected OCI (%s)", *reason);
-
-#ifdef CONFIG_X86_64
-	/*
-	 * For certain platforms with known errata we avoid advertising support
-	 * for the AVIC feature so that initialization is not attempted by SVM
-	 * code.
-	 * As a proxy for a platform identifier, use the fact that only Zen4 and
-	 * newer platforms have X2AVIC capability, and avoid exposing the AVIC
-	 * capability on earlier ones.
-	 */
-	if (!boot_cpu_has(X86_FEATURE_X2AVIC) &&
-	    boot_cpu_has(X86_FEATURE_AVIC)) {
-		pr_notice("Disabling AVIC on pre-Zen4 processors\n");
-		setup_clear_cpu_cap(X86_FEATURE_AVIC);
-	}
-#endif
-
 }
 
 static int uek_misc_init(void)