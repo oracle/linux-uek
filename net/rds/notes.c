41a8b7968b5b62b9c84785b0119a28a22730032b
Gerd Rausch <gerd.rausch@oracle.com>
2025-02-11 15:10:38 -0800

    net/rds: Add support RDS_FEATURE ELF notes
    ELF notes are a mechanism by which an object file
    can publish compatibility information.
    
    https://refspecs.linuxbase.org/elf/elf.pdf
    
    Quote:
    --------%<--------%<--------%<--------%<--------%<--------%<--------%<--------
    Sometimes a vendor or system builder needs to mark an object file with special
    information that other programs will check for conformance, compatibility, etc.
    Sections of type SHT_NOTE and program header elements of type PT_NOTE can be
    used for this purpose.
    --------%<--------%<--------%<--------%<--------%<--------%<--------%<--------
    
    On Linux, ELF notes from loaded kernel modules get published automatically
    under /sys/module/<module_name>/notes.
    
    This allows an application to implement easy checks if a feature
    (e.g. "cmsg_tos") is available in the currently loaded RDS kernel module
    by simply doing a:
    
    --------%<--------%<--------%<--------%<--------%<--------%<--------%<--------
    bool rds_cmsg_tos_supported =
        access("/sys/module/rds/notes/.note.rds.feature-cmsg_tos", F_OK) == 0;
    --------%<--------%<--------%<--------%<--------%<--------%<--------%<--------
    
    A single "type = 0" note contains the date of this feature
    as version number for potential future compatibility checks.
    
    Orabug: 38063319
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

---
+++ net/rds/notes.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates.
+ */
+
+#include <linux/elfnote.h>
+
+/* RDS_FEATURE
+ *
+ * Adds an ELFNOTE (type = 0) for the named feature with date as version.
+ *
+ * The date ought to be ISO8601-style (e.g. "2025-05-01") for user-space
+ * to be able to use a string ">=" in order to determine if a declared
+ * feature is compatible with the application.
+ *
+ * Even though about 50000 feature can be declared before things blow up,
+ * *ONLY* declare a feature if it is known that a user-space application
+ * *actually* ends up checking it.
+ *
+ * Otherwise there's too much bloat and clutter in the
+ * /sys/module/rds/notes directory, which slows things down.
+ */
+
+#define RDS_FEATURE(feature, date) ELFNOTE64("rds.feature-" #feature, 0, date)
