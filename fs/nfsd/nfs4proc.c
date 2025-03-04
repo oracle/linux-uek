2f826fec984d28fc3eff83b91c2057c2d0078995
Sherry Yang <sherry.yang@oracle.com>
2025-03-04 10:18:56 -0800

    Revert "NFSD: Limit the number of concurrent async COPY operations"
    This reverts commit 85f78092018a0c5e71a1f1a8dfac6169073623b1.
    
    This commit caused kernel panic, revert it. The original cve fix will be
    tracked in bug 37664124, taken by nfs team.
    
    Orabug: 37660195
    
    Signed-off-by: Sherry Yang <sherry.yang@oracle.com>
    Reviewed-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- fs/nfsd/nfs4proc.c
+++ fs/nfsd/nfs4proc.c
@@ -1103,7 +1103,6 @@ void nfs4_put_copy(struct nfsd4_copy *copy)
 {
 	if (!refcount_dec_and_test(&copy->refcount))
 		return;
-	atomic_dec(&copy->cp_nn->pending_async_copies);
 	kfree(copy);
 }
 
@@ -1532,16 +1531,10 @@ nfsd4_copy(struct svc_rqst *rqstp, struct nfsd4_compound_state *cstate,
 	if (!copy->cp_synchronous) {
 		struct nfsd_net *nn = net_generic(SVC_NET(rqstp), nfsd_net_id);
 
+		status = nfserrno(-ENOMEM);
 		async_copy = kzalloc(sizeof(struct nfsd4_copy), GFP_KERNEL);
 		if (!async_copy)
 			goto out_err;
-		async_copy->cp_nn = nn;
-		/* Arbitrary cap on number of pending async copy operations */
-		if (atomic_inc_return(&nn->pending_async_copies) >
-				(int)rqstp->rq_pool->sp_nrthreads) {
-			atomic_dec(&nn->pending_async_copies);
-			goto out_err;
-		}
 		if (!nfs4_init_copy_state(nn, copy))
 			goto out_err;
 		refcount_set(&async_copy->refcount, 1);
@@ -1568,7 +1561,7 @@ out:
 out_err:
 	if (async_copy)
 		cleanup_async_copy(async_copy);
-	status = nfserr_jukebox;
+	status = nfserrno(-ENOMEM);
 	if (!copy->cp_intra)
 		nfsd4_interssc_disconnect(copy->ss_mnt);
 	goto out;
