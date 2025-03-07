83a877fee776faff1e41667ce606d04c492c444c
Dave Jiang <dave.jiang@intel.com>
2025-03-07 13:55:31 -0700

    cxl: Add FWCTL support to CXL
    Add fwctl support code to allow sending of CXL feature commands from
    userspace through as ioctls via FWCTL. Provide initial setup bits. The
    CXL PCI probe function will call devm_cxl_setup_fwctl() after the
    cxl_memdev has been enumerated in order to setup FWCTL char device under
    the cxl_memdev like the existing memdev char device for issuing CXL raw
    mailbox commands from userspace via ioctls.
    
    Link: https://patch.msgid.link/r/20250307205648.1021626-2-dave.jiang@intel.com
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Li Ming <ming.li@zohomail.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Orabug: 38006732
    
    (cherry picked from commit 858ce2f56b5253063f61f6b1c58a6dbf5d71da0b)
    cherry-pick-repo: kernel/git/torvalds/linux.git
    
    Conflicts:
    	drivers/cxl/core/features.c
    
    Remove quotes from EXPORT_SYMBOL_NS_GPL and MODULE_IMPORT_NS's params,
    since UEK8 currently misses upstream commit:
    cdd30ebb1b9f ("module: Convert symbol namespace to string literal").
    
    Signed-off-by: Qing Huang <qing.huang@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- drivers/cxl/pci.c
+++ drivers/cxl/pci.c
@@ -919,6 +919,10 @@ static int cxl_pci_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (rc)
 		return rc;
 
+	rc = devm_cxl_setup_fwctl(cxlmd);
+	if (rc)
+		dev_dbg(&pdev->dev, "No CXL FWCTL setup\n");
+
 	pmu_count = cxl_count_regblock(pdev, CXL_REGLOC_RBI_PMU);
 	for (i = 0; i < pmu_count; i++) {
 		struct cxl_pmu_regs pmu_regs;
