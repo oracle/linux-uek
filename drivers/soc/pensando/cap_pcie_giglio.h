47eeaf40fec6b0d348c54da5ca5086df3e0ebed5
Brad Larson <bradley.larson@amd.com>
2025-04-02 11:48:51 -0700

    soc/pensando: Add AMD Pensando SoC drivers
    Support for Elba, Giglio and Salina SoC based boards
    
    Signed-off-by: Brad Larson <bradley.larson@amd.com>
    
    Conflicts:
    	drivers/soc/pensando/cap_crash.c
    	drivers/soc/pensando/cap_mem.c
    	drivers/soc/pensando/cap_pcie.c
    	drivers/soc/pensando/cap_rstcause.c
    	drivers/soc/pensando/kpcimgr.c
    	drivers/soc/pensando/kpcimgr_api.h
    	drivers/soc/pensando/penfw_drv.c
    	drivers/soc/pensando/sbus.c
    
    Updates required for 6.8 based patches to work with 6.12.
    
    Orabug: 38097144
    
    Signed-off-by: Joe Dobosenski <joseph.dobosenski@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

---
+++ drivers/soc/pensando/cap_pcie_giglio.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Pensando Systems Inc.
+ */
+
+#ifndef __CAP_PCIE_GIGLIO_H__
+#define __CAP_PCIE_GIGLIO_H__
+
+#include "cap_pcie_common.h"
+
+#define PCIEPORT_NPORTS		4
+
+#endif /* __CAP_PCIE_GIGLIO_H__ */
