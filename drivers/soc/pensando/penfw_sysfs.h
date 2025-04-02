59f83032148eb7efe7503c27c2d7a44df4d4b55a
Brad Larson <bradley.larson@amd.com>
2025-04-02 11:48:51 -0700

    soc/pensando: Add AMD Pensando SoC drivers
    Support for Elba, Giglio and Salina SoC based boards
    
    Signed-off-by: Brad Larson <bradley.larson@amd.com>
    
    Conflicts:
    	drivers/soc/pensando/cap_crash.c
    	drivers/soc/pensando/cap_mem.c
    	drivers/soc/pensando/cap_pcie.c
    	drivers/soc/pensando/cap_rstcause.c
    	drivers/soc/pensando/kpcimgr.c
    	drivers/soc/pensando/kpcimgr_api.h
    	drivers/soc/pensando/penfw_drv.c
    	drivers/soc/pensando/sbus.c
    
    Updates required for 6.8 based patches to work with 6.12.
    
    Orabug: 38097144
    
    Signed-off-by: Joe Dobosenski <joseph.dobosenski@oracle.com>
    Reviewed-by: Dave Kleikamp <dave.kleikamp@oracle.com>

---
+++ drivers/soc/pensando/penfw_sysfs.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021, Pensando Systems Inc.
+ */
+#ifndef __PENFW_SYSFS_H__
+#define __PENFW_SYSFS_H__
+
+int penfw_sysfs_init(struct device *penfwDevice);
+int penfw_sysfs_deinit(void);
+
+#endif /* __PENFW_SYSFS_H__ */
