a21aff2433baa86b0fb3e668ed57e453ce2225d3
Sabrina Dubroca <sd@queasysnail.net>
2025-04-09 15:59:57 +0200

    espintcp: remove encap socket caching to avoid reference leak
    [ Upstream commit 028363685bd0b7a19b4a820f82dd905b1dc83999 ]
    
    The current scheme for caching the encap socket can lead to reference
    leaks when we try to delete the netns.
    
    The reference chain is: xfrm_state -> enacp_sk -> netns
    
    Since the encap socket is a userspace socket, it holds a reference on
    the netns. If we delete the espintcp state (through flush or
    individual delete) before removing the netns, the reference on the
    socket is dropped and the netns is correctly deleted. Otherwise, the
    netns may not be reachable anymore (if all processes within the ns
    have terminated), so we cannot delete the xfrm state to drop its
    reference on the socket.
    
    This patch results in a small (~2% in my tests) performance
    regression.
    
    A GC-type mechanism could be added for the socket cache, to clear
    references if the state hasn't been used "recently", but it's a lot
    more complex than just not caching the socket.
    
    Fixes: e27cca96cd68 ("xfrm: add espintcp (RFC 8229)")
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 9cbca30102028f9ad3d2098f935c4368f581fd07)
    
    KABI: use DEPRECATE with some deleted pointer
    
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- include/net/xfrm.h
+++ include/net/xfrm.h
@@ -238,7 +238,7 @@ struct xfrm_state {
 
 	/* Data for encapsulator */
 	struct xfrm_encap_tmpl	*encap;
-	struct sock __rcu	*encap_sk;
+	UEK_KABI_DEPRECATE(struct sock __rcu *,	encap_sk)
 
 	/* NAT keepalive */
 	u32			nat_keepalive_interval; /* seconds */
