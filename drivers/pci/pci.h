2bae34a52935558d0367aba01b0f78ac6775355a
Bjorn Helgaas <bhelgaas@google.com>
2025-05-22 18:21:22 -0500

    PCI/AER: Convert aer_get_device_error_info(), aer_print_error() to index
    Previously aer_get_device_error_info() and aer_print_error() took a pointer
    to struct aer_err_info and a pointer to a pci_dev.  Typically the pci_dev
    was one of the elements of the aer_err_info.dev[] array (DPC was an
    exception, where the dev[] array was unused).
    
    Convert aer_get_device_error_info() and aer_print_error() to take an index
    into the aer_err_info.dev[] array instead.  A future patch will add
    per-device ratelimit information, so the index makes it convenient to find
    the ratelimit associated with the device.
    
    To accommodate DPC, set info->dev[0] to the DPC port before using these
    interfaces.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Reviewed-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Link: https://patch.msgid.link/20250522232339.1525671-17-helgaas@kernel.org
    (cherry picked from commit 94bc15c3484aa1dbbd01aee8f9eaa5dc347a01a8)
    
    Orabug: 38126943
    
     Conflicts:
    	drivers/pci/pci.h - code context
    	drivers/pci/pcie/aer.c - comment conflict
    	drivers/pci/pcie/dpc.c - reapply changes in dpc_process_error()
    				 so it matches the internal implementation
    
    Signed-off-by: Karolina Stolarek <karolina.stolarek@oracle.com>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -506,8 +506,8 @@ struct aer_err_info {
 	struct pcie_tlp_log tlp;	/* TLP Header */
 };
 
-int aer_get_device_error_info(struct pci_dev *dev, struct aer_err_info *info);
-void aer_print_error(struct pci_dev *dev, struct aer_err_info *info);
+int aer_get_device_error_info(struct aer_err_info *info, int i);
+void aer_print_error(struct aer_err_info *info, int i);
 #endif	/* CONFIG_PCIEAER */
 
 #ifdef CONFIG_PCIEPORTBUS
