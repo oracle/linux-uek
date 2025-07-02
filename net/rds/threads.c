oracle/linux-uek

commit 3930fc5a9cd9343fa43e03e3c0ee8a58425a3973

rds: Avoid queuing superfluous send and recv work
Queuing work on the connect path's work-queue is done from a plurality
of places and contexts.  Two more bits in cp_flags are defined, and
together with some helper functions, the work is only queued if not
already queued.

This to avoid the work queue being overloaded, reducing the connection
management performance, since the connection management uses the same
work queue.

Orabug: 29764668

Signed-off-by: Håkon Bugge <haakon.bugge@oracle.com>
Reviewed-by: Dag Moxnes <dag.moxnes@oracle.com>
Tested-by: Michael Nowak <michael.nowak@oracle.com>
Tested-by: Gerald Gibson <gerald.gibson@oracle.com>

Orabug: 33590097

UEK6 => UEK7

(cherry picked from commit f9f82b29369645a0fb7732cf23b241ccaa1b3f2f)
cherry-pick-repo=UEK/production/linux-uek.git

Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: William Kucharski <william.kucharski@oracle.com>

Orabug: 37347036

UEK7 => LUCI => UEK8

(cherry picked from commit add76130df19ceb6e78d3a84d7f98937312caadb)
cherry-pick-repo=UEK/production/linux-uek.git

Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
Reviewed-by: William Kucharski <william.kucharski@oracle.com>

net/rds/threads.c
@@ -123,8 +123,10 @@ void rds_connect_path_complete(struct rds_conn_path *cp, int curr)
 
 	cp->cp_reconnect_jiffies = 0;
 	set_bit(RCMQ_BITOFF_CONGU_PENDING, &conn->c_map_queued);
-	queue_delayed_work(cp->cp_wq, &cp->cp_send_w, 0);
-	queue_delayed_work(cp->cp_wq, &cp->cp_recv_w, 0);
+	rds_clear_queued_send_work_bit(cp);
+	rds_cond_queue_send_work(cp, 0);
+	rds_clear_queued_recv_work_bit(cp);
+	rds_cond_queue_recv_work(cp, 0);
 	queue_delayed_work(cp->cp_wq, &cp->cp_hb_w, 0);
 	cancel_delayed_work(&cp->cp_reconn_w);
 	cp->cp_hb_start = 0;
@@ -267,24 +269,29 @@ void rds_send_worker(struct work_struct *work)
 	struct rds_conn_path *cp = container_of(work,
 						struct rds_conn_path,
 						cp_send_w.work);
+	unsigned long delay;
 	int ret;
 
 	if (rds_conn_path_state(cp) == RDS_CONN_UP) {
+		rds_clear_queued_send_work_bit(cp);
 		clear_bit(RDS_LL_SEND_FULL, &cp->cp_flags);
 		ret = rds_send_xmit(cp);
 		cond_resched();
 		rds_rtd(RDS_RTD_SND_EXT, "conn %p ret %d\n", cp->cp_conn, ret);
+
 		switch (ret) {
 		case -EAGAIN:
 			rds_stats_inc(s_send_immediate_retry);
-			queue_delayed_work(cp->cp_wq, &cp->cp_send_w, 0);
+			delay = 0;
 			break;
 		case -ENOMEM:
 			rds_stats_inc(s_send_delayed_retry);
-			queue_delayed_work(cp->cp_wq, &cp->cp_send_w, 2);
+			delay = 2;
 		default:
-			break;
+			return;
 		}
+
+		rds_cond_queue_send_work(cp, delay);
 	}
 }
 
@@ -293,22 +300,25 @@ void rds_recv_worker(struct work_struct *work)
 	struct rds_conn_path *cp = container_of(work,
 						struct rds_conn_path,
 						cp_recv_w.work);
+	unsigned long delay;
 	int ret;
 
 	if (rds_conn_path_state(cp) == RDS_CONN_UP) {
+		rds_clear_queued_recv_work_bit(cp);
 		ret = cp->cp_conn->c_trans->recv_path(cp);
 		rds_rtd(RDS_RTD_RCV_EXT, "conn %p ret %d\n", cp->cp_conn, ret);
 		switch (ret) {
 		case -EAGAIN:
 			rds_stats_inc(s_recv_immediate_retry);
-			queue_delayed_work(cp->cp_wq, &cp->cp_recv_w, 0);
+			delay = 0;
 			break;
 		case -ENOMEM:
 			rds_stats_inc(s_recv_delayed_retry);
-			queue_delayed_work(cp->cp_wq, &cp->cp_recv_w, 2);
+			delay = 2;
 		default:
-			break;
+			return;
 		}
+		rds_cond_queue_recv_work(cp, delay);
 	}
 }
 

net/rds/connection.c
@@ -499,9 +499,14 @@ static void rds_conn_path_destroy(struct rds_conn_path *cp, int shutdown)
 	if (!cp->cp_transport_data)
 		return;
 
-	/* make sure lingering queued work won't try to ref the conn */
-	cancel_delayed_work_sync(&cp->cp_send_w);
-	cancel_delayed_work_sync(&cp->cp_recv_w);
+	/* make sure lingering queued work won't try to ref the
+	 * conn. If there is work queued, we cancel it (and set the
+	 * bit to avoid any re-queueing)
+	 */
+	if (test_and_set_bit(RDS_SEND_WORK_QUEUED, &cp->cp_flags))
+		cancel_delayed_work_sync(&cp->cp_send_w);
+	if (test_and_set_bit(RDS_RECV_WORK_QUEUED, &cp->cp_flags))
+		cancel_delayed_work_sync(&cp->cp_recv_w);
 
 	rds_conn_path_drop(cp, DR_CONN_DESTROY);
 	flush_work(&cp->cp_down_w);

net/rds/ib_recv.c
@@ -760,11 +760,9 @@ release_out:
 	 * if we should requeue.
 	 */
 	if (rds_conn_up(conn) &&
-	   (must_wake || (can_wait && ring_low)
-			|| rds_ib_ring_empty(&ic->i_recv_ring))) {
-		queue_delayed_work(conn->c_path[0].cp_wq,
-				   &conn->c_path[0].cp_recv_w, 1);
-	}
+	   (must_wake || (can_wait && ring_low) ||
+	    rds_ib_ring_empty(&ic->i_recv_ring)))
+		rds_cond_queue_recv_work(conn->c_path + 0, 1);
 	if (can_wait)
 		cond_resched();
 }

net/rds/ib_send.c
@@ -492,8 +492,7 @@ void rds_ib_send_add_credits(struct rds_connection *conn, unsigned int credits)
 
 	atomic_add(IB_SET_SEND_CREDITS(credits), &ic->i_credits);
 	if (test_and_clear_bit(RDS_LL_SEND_FULL, &conn->c_flags))
-		queue_delayed_work(conn->c_path[0].cp_wq,
-				   &conn->c_path[0].cp_send_w, 0);
+		rds_cond_queue_send_work(conn->c_path + 0, 0);
 
 	WARN_ON(IB_GET_SEND_CREDITS(credits) >= 16384);
 

net/rds/rds.h
@@ -165,6 +165,8 @@ static inline const char *conn_state_mnem(int state)
 #define RDS_IN_XMIT		2
 #define RDS_RECV_REFILL		3
 #define RDS_DESTROY_PENDING	4
+#define RDS_SEND_WORK_QUEUED	5
+#define RDS_RECV_WORK_QUEUED	6
 
 #define RDS_RDMA_RESOLVE_TO_MAX_INDEX   5
 #define RDS_ADDR_RES_TM_INDEX_MAX 5
@@ -994,6 +996,36 @@ void rds_for_each_conn_info(struct socket *sock, unsigned int len,
 			    u64 *buffer,
 			    size_t item_len);
 char *conn_drop_reason_str(enum rds_conn_drop_src reason);
+
+static inline void rds_cond_queue_send_work(struct rds_conn_path *cp, unsigned long delay)
+{
+	if (!test_and_set_bit(RDS_SEND_WORK_QUEUED, &cp->cp_flags))
+		queue_delayed_work(cp->cp_wq, &cp->cp_send_w, delay);
+}
+
+static inline void rds_clear_queued_send_work_bit(struct rds_conn_path *cp)
+{
+	/* clear_bit() does not imply a memory barrier */
+	smp_mb__before_atomic();
+	clear_bit(RDS_SEND_WORK_QUEUED, &cp->cp_flags);
+	/* clear_bit() does not imply a memory barrier */
+	smp_mb__after_atomic();
+}
+
+static inline void rds_cond_queue_recv_work(struct rds_conn_path *cp, unsigned long delay)
+{
+	if (!test_and_set_bit(RDS_RECV_WORK_QUEUED, &cp->cp_flags))
+		queue_delayed_work(cp->cp_wq, &cp->cp_recv_w, delay);
+}
+
+static inline void rds_clear_queued_recv_work_bit(struct rds_conn_path *cp)
+{
+	/* clear_bit() does not imply a memory barrier */
+	smp_mb__before_atomic();
+	clear_bit(RDS_RECV_WORK_QUEUED, &cp->cp_flags);
+	/* clear_bit() does not imply a memory barrier */
+	smp_mb__after_atomic();
+}
 
 static inline int
 rds_conn_path_transition(struct rds_conn_path *cp, int old, int new)

net/rds/send.c
@@ -535,7 +535,7 @@ over_batch:
 		     !list_empty(&cp->cp_send_queue)) && !raced) {
 			if (batch_count < send_batch_count)
 				goto restart;
-			queue_delayed_work(cp->cp_wq, &cp->cp_send_w, 1);
+			rds_cond_queue_send_work(cp, 1);
 		} else if (raced) {
 			rds_stats_inc(s_send_lock_queue_raced);
 		}
@@ -1597,7 +1597,7 @@ int rds_sendmsg(struct socket *sock, struct msghdr *msg, size_t payload_len)
 
 	ret = rds_send_xmit(cpath);
 	if (ret == -ENOMEM || ret == -EAGAIN)
-		queue_delayed_work(cpath->cp_wq, &cpath->cp_send_w, 1);
+		rds_cond_queue_send_work(cpath, 1);
 
 	rds_message_put(rm);
 	return payload_len;
@@ -1726,8 +1726,7 @@ int rds_send_internal(struct rds_connection *conn, struct rds_sock *rs,
 		rds_stats_inc(s_send_queue_full);
 
 		/* force a requeue of the work for later */
-		queue_delayed_work(conn->c_path[0].cp_wq,
-				   &conn->c_path[0].cp_send_w, 1);
+		rds_cond_queue_send_work(conn->c_path + 0, 1);
 
 		ret = -EAGAIN;
 		goto out;
@@ -1740,8 +1739,7 @@ int rds_send_internal(struct rds_connection *conn, struct rds_sock *rs,
 	rds_stats_inc(s_send_queued);
 
 	/* always hand the send off to the worker thread */
-	queue_delayed_work(conn->c_path[0].cp_wq,
-			   &conn->c_path[0].cp_send_w, 1);
+	rds_cond_queue_send_work(conn->c_path + 0, 1);
 
 	rdsdebug("message sent for rs %p, conn %p, len %d, %pI6c:%u->%pI6c:%u\n",
 		 rs, conn, skb->len, &dst->saddr, dst->sport, &dst->daddr,
@@ -1822,7 +1820,7 @@ static int rds_send_probe(struct rds_conn_path *cp, __be16 sport,
 	rds_stats_inc(s_send_pong);
 
 	if (!test_bit(RDS_LL_SEND_FULL, &cp->cp_flags))
-		queue_delayed_work(cp->cp_wq, &cp->cp_send_w, 0);
+		rds_cond_queue_send_work(cp, 0);
 
 	rds_message_put(rm);
 	return 0;

net/rds/tcp.c
@@ -178,8 +178,16 @@ void rds_tcp_reset_callbacks(struct socket *sock,
 	wait_event(cp->cp_waitq, !test_bit(RDS_IN_XMIT, &cp->cp_flags));
 	lock_sock(osock->sk);
 	/* reset receive side state for rds_tcp_data_recv() for osock  */
-	cancel_delayed_work_sync(&cp->cp_send_w);
-	cancel_delayed_work_sync(&cp->cp_recv_w);
+
+	/* make sure lingering queued work won't try to ref the
+	 * conn. If there is work queued, we cancel it (and set the bit
+	 * to avoid any re-queueing)
+	 */
+
+	if (test_and_set_bit(RDS_SEND_WORK_QUEUED, &cp->cp_flags))
+		cancel_delayed_work_sync(&cp->cp_send_w);
+	if (test_and_set_bit(RDS_RECV_WORK_QUEUED, &cp->cp_flags))
+		cancel_delayed_work_sync(&cp->cp_recv_w);
 	if (tc->t_tinc) {
 		rds_inc_put(&tc->t_tinc->ti_inc);
 		tc->t_tinc = NULL;

net/rds/tcp_recv.c
@@ -324,7 +324,7 @@ void rds_tcp_data_ready(struct sock *sk)
 	rds_tcp_stats_inc(s_tcp_data_ready_calls);
 
 	if (rds_tcp_read_sock(cp, GFP_ATOMIC) == -ENOMEM)
-		queue_delayed_work(cp->cp_wq, &cp->cp_recv_w, 0);
+		rds_cond_queue_recv_work(cp, 0);
 out:
 	read_unlock(&sk->sk_callback_lock);
 	ready(sk);

commit fe9fdbae5da4c666e1dc0ec70562f842c49b07fd

net/rds: Add missing switch/break in rds_recv_worker()
There is a missing break with an unintentional fallthrough
in rds_recv_worker() when c_trans->recv_path(cp) returns -ENOMEM.
While rds_ib_recv_path() sets ret = 0, rds_tcp_read_sock()
can return non-zero and rds_tcp_data_recv() does return -ENOMEM
in some cases, so it's reachable via rds_tcp.ko. Add the missing
break; so we can reschedule the recv_worker with a delay of 2.

Orabug: 38118666

Fixes: 3930fc5a9cd9 ("rds: Avoid queuing superfluous send and recv work")
Reviewed-by: Gerd Rausch <gerd.rausch@oracle.com>
Signed-off-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

net/rds/threads.c
@@ -404,6 +404,7 @@ void rds_recv_worker(struct work_struct *work)
 			rds_stats_inc(cp->cp_conn->c_stats,
 				      s_recv_delayed_retry);
 			delay = 2;
+			break;
 		default:
 			return;
 		}