876b8122b8a9f5a4d33587957668fb9484af13f9
Boris Ostrovsky <boris.ostrovsky@oracle.com>
2025-07-31 08:23:12 -0400

    Revert "KVM: SVM: Advertize TSA CPUID bits to guests"
    Synchronize UEK8 with upstream stable AMD embargo code.
    
    Orabug: 38264060
    
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -814,7 +814,7 @@ void kvm_set_cpu_caps(void)
 
 	kvm_cpu_cap_mask(CPUID_8000_0021_EAX,
 		F(NO_NESTED_DATA_BP) | F(LFENCE_RDTSC) | 0 /* SmmPgCfgLock */ |
-		F(VERW_CLEAR) | F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */ |
+		F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */ |
 		F(IBPB_BRTYPE) | F(SRSO_USER_KERNEL_NO) |
 		F(WRMSR_XX_BASE_NS)
 	);
@@ -826,10 +826,6 @@ void kvm_set_cpu_caps(void)
 	kvm_cpu_cap_init_kvm_defined(CPUID_8000_0022_EAX,
 		F(PERFMON_V2)
 	);
-
-	kvm_cpu_cap_check_and_set(X86_FEATURE_VERW_CLEAR);
-	kvm_cpu_cap_check_and_set(X86_FEATURE_TSA_SQ_NO);
-	kvm_cpu_cap_check_and_set(X86_FEATURE_TSA_L1_NO);
 
 	/*
 	 * Synthesize "LFENCE is serializing" into the AMD-defined entry in
@@ -1381,9 +1377,8 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		entry->eax = entry->ebx = entry->ecx = entry->edx = 0;
 		break;
 	case 0x80000021:
-		entry->ebx = entry->edx = 0;
+		entry->ebx = entry->ecx = entry->edx = 0;
 		cpuid_entry_override(entry, CPUID_8000_0021_EAX);
-		cpuid_entry_override(entry, CPUID_8000_0021_ECX);
 		break;
 	/* AMD Extended Performance Monitoring and Debug */
 	case 0x80000022: {
