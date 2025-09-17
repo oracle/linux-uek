7871bbf0eebc308f3908e378da0a4b6762c9acd6
Ryan Roberts <ryan.roberts@arm.com>
2025-09-17 12:02:10 -0700

    arm64: mm: split linear mapping if BBML2 unsupported on secondary CPUs
    The kernel linear mapping is painted in very early stage of system boot.
    The cpufeature has not been finalized yet at this point. So the linear
    mapping is determined by the capability of boot CPU only. If the boot
    CPU supports BBML2, large block mappings will be used for linear
    mapping.
    
    But the secondary CPUs may not support BBML2, so repaint the linear
    mapping if large block mapping is used and the secondary CPUs don't
    support BBML2 once cpufeature is finalized on all CPUs.
    
    If the boot CPU doesn't support BBML2 or the secondary CPUs have the
    same BBML2 capability with the boot CPU, repainting the linear mapping
    is not needed.
    
    Repainting is implemented by the boot CPU, which we know supports BBML2,
    so it is safe for the live mapping size to change for this CPU. The
    linear map region is walked using the pagewalk API and any discovered
    large leaf mappings are split to pte mappings using the existing helper
    functions. Since the repainting is performed inside of a stop_machine(),
    we must use GFP_ATOMIC to allocate the extra intermediate pgtables. But
    since we are still early in boot, it is expected that there is plenty of
    memory available so we will never need to sleep for reclaim, and so
    GFP_ATOMIC is acceptable here.
    
    The secondary CPUs are all put into a waiting area with the idmap in
    TTBR0 and reserved map in TTBR1 while this is performed since they
    cannot be allowed to observe any size changes on the live mappings. Some
    of this infrastructure is reused from the kpti case. Specifically we
    share the same flag (was __idmap_kpti_flag, now idmap_kpti_bbml2_flag)
    since it means we don't have to reserve any extra pgtable memory to
    idmap the extra flag.
    
    Co-developed-by: Yang Shi <yang@os.amperecomputing.com>
    Signed-off-by: Yang Shi <yang@os.amperecomputing.com>
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 3df6979d222b8638a60aa6921b73cb5f3e4f5dcb)
    
    Conflicts:
        arch/arm64/mm/mmu.c
    	Context difference
    
    Orabug: 38749874
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -85,6 +85,7 @@
 #include <asm/kvm_host.h>
 #include <asm/mmu.h>
 #include <asm/mmu_context.h>
+#include <asm/mmu.h>
 #include <asm/mte.h>
 #include <asm/processor.h>
 #include <asm/smp.h>
@@ -1950,6 +1951,7 @@ static void __init kpti_install_ng_mappings(void)
 	if (arm64_use_ng_mappings)
 		return;
 
+	init_idmap_kpti_bbml2_flag();
 	stop_machine(__kpti_install_ng_mappings, NULL, cpu_online_mask);
 }
 
@@ -3669,6 +3671,7 @@ void __init setup_system_features(void)
 {
 	setup_system_capabilities();
 
+	linear_map_maybe_split_to_ptes();
 	kpti_install_ng_mappings();
 
 	sve_setup();
