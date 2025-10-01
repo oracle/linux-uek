c159947dae62b67d06201bb43678c09184dc680d
Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
2025-10-01 13:15:17 +0200

    rds: Free all frags when rds_ib_recv_cache_put() fails
    There is a loop in rds_ib_inc_free which is executed if
    rds_ib_recv_cache_put() fails.  The loop is supposed to follow the
    linked list of frags freeing each frag, but due to the fact that
    rds_ib_free_one_frag() set the next-pointer for the frag to NULL, we
    only frees the first frag in the list.
    
    The problem is fixed by saving the next-pointer before calling
    rds_ib_free_one_frag().
    
    Orabug: 38492234
    
    Fixes: 761ac3154607 ("RDS/IB Fix allocation warning")
    Signed-off-by: Hans Westgaard Ry <hans.westgaard.ry@oracle.com>
    Reviewed-by: William Kucharski <william.kucharski@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- net/rds/ib_recv.c
+++ net/rds/ib_recv.c
@@ -185,6 +185,7 @@ void rds_ib_inc_free(struct rds_incoming *inc)
 		} else {
 			list_del_init(&frag->f_item);
 		}
+		frag->f_cache_entry.next = NULL;
 		count++;
 		if (!first_frag)
 			first_frag = frag;
@@ -211,9 +212,9 @@ void rds_ib_inc_free(struct rds_incoming *inc)
 
 			while (el) {
 				frag = container_of(el, struct rds_page_frag, f_cache_entry);
+				el = lfstack_next(el);
 				rds_ib_free_one_frag(frag, ic->i_frag_sz);
 				rds_ib_stats_sub(s_ib_frag_pages_in_caches, ic->i_frag_pages);
-				el = lfstack_next(el);
 			}
 		}
 
