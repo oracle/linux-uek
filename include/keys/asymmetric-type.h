7713605a6c65e0fc45f3b9bce1ef794649ecf22c
Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
2025-10-24 11:07:42 -0700

    crypto: convert asymmetric_keys to CRYPTO_API()
    Orabug: 38493695
    
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Sherry Yang <sherry.yang@oracle.com>

--- include/keys/asymmetric-type.h
+++ include/keys/asymmetric-type.h
@@ -12,6 +12,13 @@
 
 #include <linux/key-type.h>
 #include <linux/verification.h>
+#include <crypto/api.h>
+
+#ifndef FIPS_MODULE
+#define key_type_asymmetric nonfips_key_type_asymmetric
+#else
+#define key_type_asymmetric fips_key_type_asymmetric
+#endif
 
 extern struct key_type key_type_asymmetric;
 
@@ -56,16 +63,12 @@ struct asymmetric_key_ids {
 	void		*id[3];
 };
 
-extern bool asymmetric_key_id_same(const struct asymmetric_key_id *kid1,
-				   const struct asymmetric_key_id *kid2);
+DECLARE_CRYPTO_API(asymmetric_key_id_same, bool, (const struct asymmetric_key_id *kid1, const struct asymmetric_key_id *kid2), (kid1, kid2));
 
-extern bool asymmetric_key_id_partial(const struct asymmetric_key_id *kid1,
-				      const struct asymmetric_key_id *kid2);
+DECLARE_CRYPTO_API(asymmetric_key_id_partial, bool, (const struct asymmetric_key_id *kid1, const struct asymmetric_key_id *kid2), (kid1, kid2));
 
-extern struct asymmetric_key_id *asymmetric_key_generate_id(const void *val_1,
-							    size_t len_1,
-							    const void *val_2,
-							    size_t len_2);
+DECLARE_CRYPTO_API(asymmetric_key_generate_id, struct asymmetric_key_id *, (const void *val_1, size_t len_1, const void *val_2, size_t len_2), (val_1, len_1, val_2, len_2));
+
 static inline
 const struct asymmetric_key_ids *asymmetric_key_ids(const struct key *key)
 {
@@ -78,15 +81,9 @@ const struct public_key *asymmetric_key_public_key(const struct key *key)
 	return key->payload.data[asym_crypto];
 }
 
-extern struct key *find_asymmetric_key(struct key *keyring,
-				       const struct asymmetric_key_id *id_0,
-				       const struct asymmetric_key_id *id_1,
-				       const struct asymmetric_key_id *id_2,
-				       bool partial);
+DECLARE_CRYPTO_API(find_asymmetric_key, struct key *, (struct key *keyring, const struct asymmetric_key_id *id_0, const struct asymmetric_key_id *id_1, const struct asymmetric_key_id *id_2, bool partial), (keyring, id_0, id_1, id_2, partial));
 
-int x509_load_certificate_list(const u8 cert_list[], const unsigned long list_size,
-			       const struct key *keyring);
-
+DECLARE_CRYPTO_API(x509_load_certificate_list, int, (const u8 cert_list[], const unsigned long list_size, const struct key *keyring), (cert_list, list_size, keyring));
 /*
  * The payload is at the discretion of the subtype.
  */
