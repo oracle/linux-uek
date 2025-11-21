766bf470e3a3830c24682508e46bd985f0e2aac2
Hariprasad Kelam <hkelam@marvell.com>
2025-11-21 12:23:52 +0530

    octeontx2-af: Add new CGX_CMDs to set and get PHY modulation type
    Implement commands to set and get PHY line-side modulation type
    
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Change-Id: I395cfbe9240a23eeb97712662936acf111bfb56d
    
    Orabug: 39318904
    
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Reviewed-by: Thomas Tai <thomas.tai@oracle.com>
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cgx.c
@@ -1463,6 +1463,31 @@ void rvu_mac_reset(struct rvu *rvu, u16 pcifunc)
 		dev_err(rvu->dev, "Failed to reset MAC\n");
 }
 
+int rvu_mbox_handler_cgx_set_phy_mod_type(struct rvu *rvu,
+					  struct cgx_phy_mod_type *req,
+					  struct msg_rsp *rsp)
+{
+	int pf = rvu_get_pf(req->hdr.pcifunc);
+	u8 cgx_id, lmac_id;
+
+	rvu_get_cgx_lmac_id(rvu->pf2cgxlmac_map[pf], &cgx_id, &lmac_id);
+	return cgx_set_phy_mod_type(req->mod, rvu_cgx_pdata(cgx_id, rvu),
+				    lmac_id);
+}
+
+int rvu_mbox_handler_cgx_get_phy_mod_type(struct rvu *rvu, struct msg_req *req,
+					  struct cgx_phy_mod_type *rsp)
+{
+	int pf = rvu_get_pf(req->hdr.pcifunc);
+	u8 cgx_id, lmac_id;
+
+	rvu_get_cgx_lmac_id(rvu->pf2cgxlmac_map[pf], &cgx_id, &lmac_id);
+	rsp->mod = cgx_get_phy_mod_type(rvu_cgx_pdata(cgx_id, rvu), lmac_id);
+	if (rsp->mod < 0)
+		return rsp->mod;
+	return 0;
+}
+
 /* Dont allow cgx mapped VFs to overwrite PKIND config
  * incase of special PKINDs are configured like (HIGIG/EDSA)
  */
