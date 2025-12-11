b22b62493dbfd4d6268d017237fb1beb0cb1bd3c
Haoxiang Li <lihaoxiang@isrc.iscas.ac.cn>
2025-12-11 15:37:56 +0800

    fjes: Add missing iounmap in fjes_hw_init()
    commit 15ef641a0c6728d25a400df73922e80ab2cf029c upstream.
    
    In error paths, add fjes_hw_iounmap() to release the
    resource acquired by fjes_hw_iomap(). Add a goto label
    to do so.
    
    Fixes: 8cdc3f6c5d22 ("fjes: Hardware initialization routine")
    Cc: stable@vger.kernel.org
    Signed-off-by: Haoxiang Li <lihaoxiang@isrc.iscas.ac.cn>
    Signed-off-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20251211073756.101824-1-lihaoxiang@isrc.iscas.ac.cn
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 96e9b4b4eebe55cfc18911763330c3b4217dda80)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/net/fjes/fjes_hw.c
+++ drivers/net/fjes/fjes_hw.c
@@ -333,7 +333,7 @@ int fjes_hw_init(struct fjes_hw *hw)
 
 	ret = fjes_hw_reset(hw);
 	if (ret)
-		return ret;
+		goto err_iounmap;
 
 	fjes_hw_set_irqmask(hw, REG_ICTL_MASK_ALL, true);
 
@@ -346,14 +346,20 @@ int fjes_hw_init(struct fjes_hw *hw)
 	hw->max_epid = fjes_hw_get_max_epid(hw);
 	hw->my_epid = fjes_hw_get_my_epid(hw);
 
-	if ((hw->max_epid == 0) || (hw->my_epid >= hw->max_epid))
-		return -ENXIO;
+	if ((hw->max_epid == 0) || (hw->my_epid >= hw->max_epid)) {
+		ret = -ENXIO;
+		goto err_iounmap;
+	}
 
 	ret = fjes_hw_setup(hw);
 
 	hw->hw_info.trace = vzalloc(FJES_DEBUG_BUFFER_SIZE);
 	hw->hw_info.trace_size = FJES_DEBUG_BUFFER_SIZE;
 
+	return ret;
+
+err_iounmap:
+	fjes_hw_iounmap(hw);
 	return ret;
 }
 
