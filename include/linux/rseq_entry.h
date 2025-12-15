309218831a8d8447872f82d5c09624e65267c458
Thomas Gleixner <tglx@linutronix.de>
2025-12-15 17:52:28 +0100

    rseq: Implement rseq_grant_slice_extension()
    Provide the actual decision function, which decides whether a time slice
    extension is granted in the exit to user mode path when NEED_RESCHED is
    evaluated.
    
    The decision is made in two stages. First an inline quick check to avoid
    going into the actual decision function. This checks whether:
    
     #1 the functionality is enabled
    
     #2 the exit is a return from interrupt to user mode
    
     #3 any TIF bit, which causes extra work is set. That includes TIF_RSEQ,
        which means the task was already scheduled out.
    
    The slow path, which implements the actual user space ABI, is invoked
    when:
    
      A) #1 is true, #2 is true and #3 is false
    
         It checks whether user space requested a slice extension by setting
         the request bit in the rseq slice_ctrl field. If so, it grants the
         extension and stores the slice expiry time, so that the actual exit
         code can double check whether the slice is already exhausted before
         going back.
    
      B) #1 - #3 are true _and_ a slice extension was granted in a previous
         loop iteration
    
         In this case the grant is revoked.
    
    In case that the user space access faults or invalid state is detected, the
    task is terminated with SIGSEGV.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20251215155709.195303303@linutronix.de
    (cherry picked from commit dfb630f548a7c715efb0651c6abf334dca75cd52)
    
    Orabug: 39047421
    
    Conflicts
     include/linux/rseq_entry.h
     - Updated references to correct members in struct task_struct, 'rseq_slice'
       and 'rseq_slice_expires'.
     - There is no scoped_user_rw_access(), used user_write_access_begin()/
       user_write_access_end() instead.
     - There is no current->rseq_len member. Using sizeof(*rseq).
     - Replaced scoped_guard(irq) with local_irq_disable() / local_irq_enable().
    
    Signed-off-by: Prakash Sangappa <prakash.sangappa@oracle.com>
    Reviewed-by: Joseph Salisbury <joseph.salisbury@oracle.com>
    Reviewed-by: Daniel Jordan <daniel.m.jordan@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- include/linux/rseq_entry.h
+++ include/linux/rseq_entry.h
@@ -65,10 +65,121 @@ static __always_inline void rseq_slice_clear_grant(struct task_struct *t)
 	t->rseq_slice.state.granted = false;
 }
 
+static __always_inline bool rseq_grant_slice_extension(bool work_pending)
+{
+	struct task_struct *curr = current;
+	struct rseq_slice_ctrl usr_ctrl;
+	union rseq_slice_state state;
+	struct rseq __user *rseq;
+
+	if (!rseq_slice_extension_enabled())
+		return false;
+
+	/* If not enabled or not a return from interrupt, nothing to do. */
+	state = curr->rseq_slice.state;
+	state.enabled &= curr->rseq_slice.user_irq;
+	if (likely(!state.state))
+		return false;
+
+	rseq = curr->rseq;
+	if (!user_write_access_begin(rseq, sizeof(*rseq)))
+		goto efault_sig;
+	/*
+	 * Quick check conditions where a grant is not possible or
+	 * needs to be revoked.
+	 *
+	 *  1) Any TIF bit which needs to do extra work aside of
+	 *     rescheduling prevents a grant.
+	 *
+	 *  2) A previous rescheduling request resulted in a slice
+	 *     extension grant.
+	 */
+	if (unlikely(work_pending || state.granted)) {
+		/* Clear user control unconditionally. No point for checking */
+		unsafe_put_user(0U, &rseq->slice_ctrl.all, efault);
+		rseq_slice_clear_grant(curr);
+		user_write_access_end();
+		return false;
+	}
+
+	unsafe_get_user(usr_ctrl.all, &rseq->slice_ctrl.all, efault);
+	if (likely(!(usr_ctrl.request))) {
+		user_write_access_end();
+		return false;
+	}
+
+	/* Grant the slice extension */
+	usr_ctrl.request = 0;
+	usr_ctrl.granted = 1;
+	unsafe_put_user(usr_ctrl.all, &rseq->slice_ctrl.all, efault);
+
+	user_write_access_end();
+	rseq_stat_inc(rseq_stats.s_granted);
+
+	curr->rseq_slice.state.granted = true;
+	/* Store expiry time for arming the timer on the way out */
+	curr->rseq_slice_expires = data_race(rseq_slice_ext_nsecs) + ktime_get_mono_fast_ns();
+	/*
+	 * This is racy against a remote CPU setting TIF_NEED_RESCHED in
+	 * several ways:
+	 *
+	 * 1)
+	 *	CPU0			CPU1
+	 *	clear_tsk()
+	 *				set_tsk()
+	 *	clear_preempt()
+	 *				Raise scheduler IPI on CPU0
+	 *	--> IPI
+	 *	    fold_need_resched() -> Folds correctly
+	 * 2)
+	 *	CPU0			CPU1
+	 *				set_tsk()
+	 *	clear_tsk()
+	 *	clear_preempt()
+	 *				Raise scheduler IPI on CPU0
+	 *	--> IPI
+	 *	    fold_need_resched() <- NOOP as TIF_NEED_RESCHED is false
+	 *
+	 * #1 is not any different from a regular remote reschedule as it
+	 *    sets the previously not set bit and then raises the IPI which
+	 *    folds it into the preempt counter
+	 *
+	 * #2 is obviously incorrect from a scheduler POV, but it's not
+	 *    differently incorrect than the code below clearing the
+	 *    reschedule request with the safety net of the timer.
+	 *
+	 * The important part is that the clearing is protected against the
+	 * scheduler IPI and also against any other interrupt which might
+	 * end up waking up a task and setting the bits in the middle of
+	 * the operation:
+	 *
+	 *	clear_tsk()
+	 *	---> Interrupt
+	 *		wakeup_on_this_cpu()
+	 *		set_tsk()
+	 *		set_preempt()
+	 *	clear_preempt()
+	 *
+	 * which would be inconsistent state.
+	 */
+	local_irq_disable();
+	clear_tsk_need_resched(curr);
+	clear_preempt_need_resched();
+	local_irq_enable();
+	return true;
+
+efault:
+	user_write_access_end();
+efault_sig:
+	force_sig(SIGSEGV);
+	return false;
+}
+
 #else /* CONFIG_RSEQ_SLICE_EXTENSION */
 static inline bool rseq_slice_extension_enabled(void) { return false; }
 static inline bool rseq_arm_slice_extension_timer(void) { return false; }
 static inline void rseq_slice_clear_grant(struct task_struct *t) { }
+static inline bool rseq_grant_slice_extension(bool work_pending) { return false; }
 #endif /* !CONFIG_RSEQ_SLICE_EXTENSION */
 
 static __always_inline void rseq_note_user_irq_entry(void)
@@ -92,6 +203,7 @@ static __always_inline bool rseq_exit_to_user_mode_restart(void)
 #else /* CONFIG_RSEQ */
 static inline void rseq_note_user_irq_entry(void) { }
 static inline bool rseq_exit_to_user_mode_restart(void) { return false; }
+static inline bool rseq_grant_slice_extension(bool work_pending) { return false; }
 #endif /* !CONFIG_RSEQ */
 
 #endif /* _LINUX_RSEQ_ENTRY_H */
