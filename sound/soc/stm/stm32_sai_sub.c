6bd7f1ce00b298d349abeb372d3847195d623b88
Johan Hovold <johan@kernel.org>
2026-01-05 10:10:33 -0500

    ASoC: stm32: sai: fix OF node leak on probe
    [ Upstream commit 23261f0de09427367e99f39f588e31e2856a690e ]
    
    The reference taken to the sync provider OF node when probing the
    platform device is currently only dropped if the set_sync() callback
    fails during DAI probe.
    
    Make sure to drop the reference on platform probe failures (e.g. probe
    deferral) and on driver unbind.
    
    This also avoids a potential use-after-free in case the DAI is ever
    reprobed without first rebinding the platform driver.
    
    Fixes: 5914d285f6b7 ("ASoC: stm32: sai: Add synchronization support")
    Fixes: d4180b4c02e7 ("ASoC: stm32: sai: fix set_sync service")
    Cc: Olivier Moysan <olivier.moysan@st.com>
    Cc: stable@vger.kernel.org      # 4.16: d4180b4c02e7
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Reviewed-by: olivier moysan <olivier.moysan@foss.st.com>
    Link: https://patch.msgid.link/20251124104908.15754-4-johan@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 7daa50a2157e41c964b745ab1dc378b5b3b626d1)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- sound/soc/stm/stm32_sai_sub.c
+++ sound/soc/stm/stm32_sai_sub.c
@@ -1436,7 +1436,8 @@ static int stm32_sai_sub_parse_of(struct platform_device *pdev,
 				dev_err(&pdev->dev,
 					"External synchro not supported\n");
 				of_node_put(args.np);
-				return -EINVAL;
+				ret = -EINVAL;
+				goto err_put_sync_provider;
 			}
 			sai->sync = SAI_SYNC_EXTERNAL;
 
@@ -1445,7 +1446,8 @@ static int stm32_sai_sub_parse_of(struct platform_device *pdev,
 			    (sai->synci > (SAI_GCR_SYNCIN_MAX + 1))) {
 				dev_err(&pdev->dev, "Wrong SAI index\n");
 				of_node_put(args.np);
-				return -EINVAL;
+				ret = -EINVAL;
+				goto err_put_sync_provider;
 			}
 
 			if (of_property_match_string(args.np, "compatible",
@@ -1459,7 +1461,8 @@ static int stm32_sai_sub_parse_of(struct platform_device *pdev,
 			if (!sai->synco) {
 				dev_err(&pdev->dev, "Unknown SAI sub-block\n");
 				of_node_put(args.np);
-				return -EINVAL;
+				ret = -EINVAL;
+				goto err_put_sync_provider;
 			}
 		}
 
@@ -1469,13 +1472,15 @@ static int stm32_sai_sub_parse_of(struct platform_device *pdev,
 
 	of_node_put(args.np);
 	sai->sai_ck = devm_clk_get(&pdev->dev, "sai_ck");
-	if (IS_ERR(sai->sai_ck))
-		return dev_err_probe(&pdev->dev, PTR_ERR(sai->sai_ck),
-				     "Missing kernel clock sai_ck\n");
+	if (IS_ERR(sai->sai_ck)) {
+		ret = dev_err_probe(&pdev->dev, PTR_ERR(sai->sai_ck),
+				    "Missing kernel clock sai_ck\n");
+		goto err_put_sync_provider;
+	}
 
 	ret = clk_prepare(sai->pdata->pclk);
 	if (ret < 0)
-		return ret;
+		goto err_put_sync_provider;
 
 	if (STM_SAI_IS_F4(sai->pdata))
 		return 0;
@@ -1497,6 +1502,8 @@ static int stm32_sai_sub_parse_of(struct platform_device *pdev,
 
 err_unprepare_pclk:
 	clk_unprepare(sai->pdata->pclk);
+err_put_sync_provider:
+	of_node_put(sai->np_sync_provider);
 
 	return ret;
 }
@@ -1567,6 +1574,7 @@ static int stm32_sai_sub_probe(struct platform_device *pdev)
 
 err_unprepare_pclk:
 	clk_unprepare(sai->pdata->pclk);
+	of_node_put(sai->np_sync_provider);
 
 	return ret;
 }
@@ -1579,6 +1587,7 @@ static int stm32_sai_sub_remove(struct platform_device *pdev)
 	snd_dmaengine_pcm_unregister(&pdev->dev);
 	snd_soc_unregister_component(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
+	of_node_put(sai->np_sync_provider);
 
 	return 0;
 }
