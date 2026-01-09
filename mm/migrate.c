1e5075a38652f8e5ab87020974d8c69a6cf7fea6
Matthew Wilcox (Oracle) <willy@infradead.org>
2026-01-09 04:13:42 +0000

    migrate: correct lock ordering for hugetlb file folios
    commit b7880cb166ab62c2409046b2347261abf701530e upstream.
    
    Syzbot has found a deadlock (analyzed by Lance Yang):
    
    1) Task (5749): Holds folio_lock, then tries to acquire i_mmap_rwsem(read lock).
    2) Task (5754): Holds i_mmap_rwsem(write lock), then tries to acquire
    folio_lock.
    
    migrate_pages()
      -> migrate_hugetlbs()
        -> unmap_and_move_huge_page()     <- Takes folio_lock!
          -> remove_migration_ptes()
            -> __rmap_walk_file()
              -> i_mmap_lock_read()       <- Waits for i_mmap_rwsem(read lock)!
    
    hugetlbfs_fallocate()
      -> hugetlbfs_punch_hole()           <- Takes i_mmap_rwsem(write lock)!
        -> hugetlbfs_zero_partial_page()
         -> filemap_lock_hugetlb_folio()
          -> filemap_lock_folio()
            -> __filemap_get_folio        <- Waits for folio_lock!
    
    The migration path is the one taking locks in the wrong order according to
    the documentation at the top of mm/rmap.c.  So expand the scope of the
    existing i_mmap_lock to cover the calls to remove_migration_ptes() too.
    
    This is (mostly) how it used to be after commit c0d0381ade79.  That was
    removed by 336bf30eb765 for both file & anon hugetlb pages when it should
    only have been removed for anon hugetlb pages.
    
    Link: https://lkml.kernel.org/r/20260109041345.3863089-2-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Fixes: 336bf30eb765 ("hugetlbfs: fix anon huge page migration race")
    Reported-by: syzbot+2d9c96466c978346b55f@syzkaller.appspotmail.com
    Link: https://lore.kernel.org/all/68e9715a.050a0220.1186a4.000d.GAE@google.com
    Debugged-by: Lance Yang <lance.yang@linux.dev>
    Acked-by: David Hildenbrand (Red Hat) <david@kernel.org>
    Acked-by: Zi Yan <ziy@nvidia.com>
    Cc: Alistair Popple <apopple@nvidia.com>
    Cc: Byungchul Park <byungchul@sk.com>
    Cc: Gregory Price <gourry@gourry.net>
    Cc: Jann Horn <jannh@google.com>
    Cc: Joshua Hahn <joshua.hahnjy@gmail.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Rakie Kim <rakie.kim@sk.com>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Ying Huang <ying.huang@linux.alibaba.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit b75070823b89009f5123fd0e05a8e0c3d39937c1)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -1443,6 +1443,7 @@ static int unmap_and_move_huge_page(new_folio_t get_new_folio,
 	int page_was_mapped = 0;
 	struct anon_vma *anon_vma = NULL;
 	struct address_space *mapping = NULL;
+	enum ttu_flags ttu = 0;
 
 	if (folio_ref_count(src) == 1) {
 		/* page was freed from under us. So we are done. */
@@ -1483,8 +1484,6 @@ static int unmap_and_move_huge_page(new_folio_t get_new_folio,
 		goto put_anon;
 
 	if (folio_mapped(src)) {
-		enum ttu_flags ttu = 0;
-
 		if (!folio_test_anon(src)) {
 			/*
 			 * In shared mappings, try_to_unmap could potentially
@@ -1501,9 +1500,6 @@ static int unmap_and_move_huge_page(new_folio_t get_new_folio,
 
 		try_to_migrate(src, ttu);
 		page_was_mapped = 1;
-
-		if (ttu & TTU_RMAP_LOCKED)
-			i_mmap_unlock_write(mapping);
 	}
 
 	if (!folio_mapped(src))
@@ -1511,7 +1507,11 @@ static int unmap_and_move_huge_page(new_folio_t get_new_folio,
 
 	if (page_was_mapped)
 		remove_migration_ptes(src,
-			rc == MIGRATEPAGE_SUCCESS ? dst : src, 0);
+			rc == MIGRATEPAGE_SUCCESS ? dst : src,
+				ttu ? RMP_LOCKED : 0);
+
+	if (ttu & TTU_RMAP_LOCKED)
+		i_mmap_unlock_write(mapping);
 
 unlock_put_anon:
 	folio_unlock(dst);
