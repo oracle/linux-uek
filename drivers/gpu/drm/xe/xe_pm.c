a13d1baee543767c517995167c8ebb12ab300892
Karthik Poosa <karthik.poosa@intel.com>
2026-01-23 23:02:38 +0530

    drm/xe/pm: Disable D3Cold for BMG only on specific platforms
    [ Upstream commit bb36170d959fad7f663f91eb9c32a84dd86bef2b ]
    
    Restrict D3Cold disablement for BMG to unsupported NUC platforms,
    instead of disabling it on all platforms.
    
    Signed-off-by: Karthik Poosa <karthik.poosa@intel.com>
    Fixes: 3e331a6715ee ("drm/xe/pm: Temporarily disable D3Cold on BMG")
    Link: https://patch.msgid.link/20260123173238.1642383-1-karthik.poosa@intel.com
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    (cherry picked from commit 39125eaf8863ab09d70c4b493f58639b08d5a897)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit c0de1cc6a69432fd850cf6ad9a16855da45cd9a6)
    Signed-off-by: Jack Vogel <jack.vogel@oracle.com>

--- drivers/gpu/drm/xe/xe_pm.c
+++ drivers/gpu/drm/xe/xe_pm.c
@@ -7,6 +7,7 @@
 
 #include <linux/pm_runtime.h>
 #include <linux/suspend.h>
+#include <linux/dmi.h>
 
 #include <drm/drm_managed.h>
 #include <drm/ttm/ttm_placement.h>
@@ -270,9 +271,15 @@ int xe_pm_init_early(struct xe_device *xe)
 
 static u32 vram_threshold_value(struct xe_device *xe)
 {
-	/* FIXME: D3Cold temporarily disabled by default on BMG */
-	if (xe->info.platform == XE_BATTLEMAGE)
-		return 0;
+	if (xe->info.platform == XE_BATTLEMAGE) {
+		const char *product_name;
+
+		product_name = dmi_get_system_info(DMI_PRODUCT_NAME);
+		if (product_name && strstr(product_name, "NUC13RNG")) {
+			drm_warn(&xe->drm, "BMG + D3Cold not supported on this platform\n");
+			return 0;
+		}
+	}
 
 	return DEFAULT_VRAM_THRESHOLD;
 }
