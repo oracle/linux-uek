3860f58649cd2984e1032ff571e5833b3e8d3a01
Sharath Srinivasan <sharath.srinivasan@oracle.com>
2026-01-28 16:18:48 -0800

    Revert "IB/mlx5: Implement clear counters"
    This reverts commit 43d4ec76664bb3e6c18404ce22194f831db4ae36.
    
    echo 1 > /sys/class/infiniband/mlx5_1/ports/1/hw_counters/clear_counters
    no longer works starting CX7 fw 28.47.1088 and we're using this
    as an opportunity to revert this no-longer-necessary and out-of-tree
    functionality from the mlx5 driver.
    
    Orabug: 38923520
    
    Conflicts:
    	drivers/net/ethernet/mellanox/mlx5/core/lag.c
    	- Remove mlx5_cmd_query_cong_counter() as we no longer need it
    	  after 5701041db0ee ("net/mlx5: Update lag.c new cmd interface")
    
    Reviewed-by: Qing Huang <qing.huang@oracle.com>
    Signed-off-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -1180,10 +1180,6 @@ bool mlx5_lag_is_active(struct mlx5_core_dev *dev);
 bool mlx5_lag_is_master(struct mlx5_core_dev *dev);
 bool mlx5_lag_is_shared_fdb(struct mlx5_core_dev *dev);
 struct net_device *mlx5_lag_get_roce_netdev(struct mlx5_core_dev *dev);
-#ifndef WITHOUT_ORACLE_EXTENSIONS
-int mlx5_cmd_query_cong_counter(struct mlx5_core_dev *dev,
-				bool reset, void *out, int out_size);
-#endif /* !WITHOUT_ORACLE_EXTENSIONS */
 u8 mlx5_lag_get_slave_port(struct mlx5_core_dev *dev,
 			   struct net_device *slave);
 int mlx5_lag_query_cong_counters(struct mlx5_core_dev *dev,
