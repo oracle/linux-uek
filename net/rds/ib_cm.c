ee12f0cf7511e3723d466329e554a36db0508087
Gerd Rausch <gerd.rausch@oracle.com>
2026-02-05 11:17:49 -0800

    net/rds: Schedule rds_send_worker if there's more work to do
    Much like other callers of "rds_send_xmit" (i.e. "rds_sendmsg", "rds_send_worker"),
    make "rds_ib_send_cb" not drop the ball if "rds_send_xmit" signals that
    there's more work to do.
    
    Orabug: 38144693
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Reviewed-by: Manjunath Patil <manjunath.b.patil@oracle.com>
    Reviewed-by: Sharath Srinivasan <sharath.srinivasan@oracle.com>

--- net/rds/ib_cm.c
+++ net/rds/ib_cm.c
@@ -811,6 +811,7 @@ static void rds_ib_tx(struct rds_ib_connection *ic)
 static void rds_ib_send_cb(struct rds_ib_connection *ic)
 {
 	struct rds_connection *conn = ic->conn;
+	int ret;
 
 	rds_ib_stats_inc(s_ib_tasklet_call);
 
@@ -818,8 +819,11 @@ static void rds_ib_send_cb(struct rds_ib_connection *ic)
 
 	if (rds_conn_up(conn) &&
 	   (!test_bit(RDS_LL_SEND_FULL, &conn->c_flags) ||
-	    test_bit(RCMQ_BITOFF_CONGU_PENDING, &conn->c_map_queued)))
-		rds_send_xmit(&ic->conn->c_path[0]);
+	    test_bit(RCMQ_BITOFF_CONGU_PENDING, &conn->c_map_queued))) {
+		ret = rds_send_xmit(ic->conn->c_path);
+		if (ret == -ENOMEM || ret == -EAGAIN)
+			rds_cond_queue_send_work(ic->conn->c_path, 1);
+	}
 }
 
 void rds_ib_tasklet_fn_send(unsigned long data)
