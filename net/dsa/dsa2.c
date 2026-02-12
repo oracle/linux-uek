237acabbae18b8cb783867050079de555436a869
Vladimir Oltean <vladimir.oltean@nxp.com>
2026-02-12 10:52:36 +0000

    net: dsa: free routing table on probe failure
    [ Upstream commit 8bf108d7161ffc6880ad13a0cc109de3cf631727 ]
    
    If complete = true in dsa_tree_setup(), it means that we are the last
    switch of the tree which is successfully probing, and we should be
    setting up all switches from our probe path.
    
    After "complete" becomes true, dsa_tree_setup_cpu_ports() or any
    subsequent function may fail. If that happens, the entire tree setup is
    in limbo: the first N-1 switches have successfully finished probing
    (doing nothing but having allocated persistent memory in the tree's
    dst->ports, and maybe dst->rtable), and switch N failed to probe, ending
    the tree setup process before anything is tangible from the user's PoV.
    
    If switch N fails to probe, its memory (ports) will be freed and removed
    from dst->ports. However, the dst->rtable elements pointing to its ports,
    as created by dsa_link_touch(), will remain there, and will lead to
    use-after-free if dereferenced.
    
    If dsa_tree_setup_switches() returns -EPROBE_DEFER, which is entirely
    possible because that is where ds->ops->setup() is, we get a kasan
    report like this:
    
    ==================================================================
    BUG: KASAN: slab-use-after-free in mv88e6xxx_setup_upstream_port+0x240/0x568
    Read of size 8 at addr ffff000004f56020 by task kworker/u8:3/42
    
    Call trace:
     __asan_report_load8_noabort+0x20/0x30
     mv88e6xxx_setup_upstream_port+0x240/0x568
     mv88e6xxx_setup+0xebc/0x1eb0
     dsa_register_switch+0x1af4/0x2ae0
     mv88e6xxx_register_switch+0x1b8/0x2a8
     mv88e6xxx_probe+0xc4c/0xf60
     mdio_probe+0x78/0xb8
     really_probe+0x2b8/0x5a8
     __driver_probe_device+0x164/0x298
     driver_probe_device+0x78/0x258
     __device_attach_driver+0x274/0x350
    
    Allocated by task 42:
     __kasan_kmalloc+0x84/0xa0
     __kmalloc_cache_noprof+0x298/0x490
     dsa_switch_touch_ports+0x174/0x3d8
     dsa_register_switch+0x800/0x2ae0
     mv88e6xxx_register_switch+0x1b8/0x2a8
     mv88e6xxx_probe+0xc4c/0xf60
     mdio_probe+0x78/0xb8
     really_probe+0x2b8/0x5a8
     __driver_probe_device+0x164/0x298
     driver_probe_device+0x78/0x258
     __device_attach_driver+0x274/0x350
    
    Freed by task 42:
     __kasan_slab_free+0x48/0x68
     kfree+0x138/0x418
     dsa_register_switch+0x2694/0x2ae0
     mv88e6xxx_register_switch+0x1b8/0x2a8
     mv88e6xxx_probe+0xc4c/0xf60
     mdio_probe+0x78/0xb8
     really_probe+0x2b8/0x5a8
     __driver_probe_device+0x164/0x298
     driver_probe_device+0x78/0x258
     __device_attach_driver+0x274/0x350
    
    The simplest way to fix the bug is to delete the routing table in its
    entirety. dsa_tree_setup_routing_table() has no problem in regenerating
    it even if we deleted links between ports other than those of switch N,
    because dsa_link_touch() first checks whether the port pair already
    exists in dst->rtable, allocating if not.
    
    The deletion of the routing table in its entirety already exists in
    dsa_tree_teardown(), so refactor that into a function that can also be
    called from the tree setup error path.
    
    In my analysis of the commit to blame, it is the one which added
    dsa_link elements to dst->rtable. Prior to that, each switch had its own
    ds->rtable which is freed when the switch fails to probe. But the tree
    is potentially persistent memory.
    
    Fixes: c5f51765a1f6 ("net: dsa: list DSA links in the fabric")
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Link: https://patch.msgid.link/20250414213001.2957964-1-vladimir.oltean@nxp.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [ Backport the fix to net/dsa/dsa2.c in v5.15.y for dsa2.c was
    renamed back into dsa.c by commit
    47d2ce03dcfb ("net: dsa: rename dsa2.c back into dsa.c and create its header")
    since v6.2. ]
    Signed-off-by: Bin Lan <lanbincn@139.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 51df5513cca6349d0bea01bab95cd96cf869976e)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- net/dsa/dsa2.c
+++ net/dsa/dsa2.c
@@ -1064,6 +1064,16 @@ static void dsa_tree_teardown_lags(struct dsa_switch_tree *dst)
 	kfree(dst->lags);
 }
 
+static void dsa_tree_teardown_routing_table(struct dsa_switch_tree *dst)
+{
+	struct dsa_link *dl, *next;
+
+	list_for_each_entry_safe(dl, next, &dst->rtable, list) {
+		list_del(&dl->list);
+		kfree(dl);
+	}
+}
+
 static int dsa_tree_setup(struct dsa_switch_tree *dst)
 {
 	bool complete;
@@ -1081,7 +1091,7 @@ static int dsa_tree_setup(struct dsa_switch_tree *dst)
 
 	err = dsa_tree_setup_cpu_ports(dst);
 	if (err)
-		return err;
+		goto teardown_rtable;
 
 	err = dsa_tree_setup_switches(dst);
 	if (err)
@@ -1108,14 +1118,14 @@ teardown_switches:
 	dsa_tree_teardown_switches(dst);
 teardown_cpu_ports:
 	dsa_tree_teardown_cpu_ports(dst);
+teardown_rtable:
+	dsa_tree_teardown_routing_table(dst);
 
 	return err;
 }
 
 static void dsa_tree_teardown(struct dsa_switch_tree *dst)
 {
-	struct dsa_link *dl, *next;
-
 	if (!dst->setup)
 		return;
 
@@ -1129,10 +1139,7 @@ static void dsa_tree_teardown(struct dsa_switch_tree *dst)
 
 	dsa_tree_teardown_cpu_ports(dst);
 
-	list_for_each_entry_safe(dl, next, &dst->rtable, list) {
-		list_del(&dl->list);
-		kfree(dl);
-	}
+	dsa_tree_teardown_routing_table(dst);
 
 	pr_info("DSA: tree %d torn down\n", dst->index);
 
