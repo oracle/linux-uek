6d72f828c72d85a3ed74f4171209704120c1877c
Pablo Neira Ayuso <pablo@netfilter.org>
2026-03-12 13:48:48 +0100

    netfilter: xt_CT: drop pending enqueued packets on template removal
    [ Upstream commit f62a218a946b19bb59abdd5361da85fa4606b96b ]
    
    Templates refer to objects that can go away while packets are sitting in
    nfqueue refer to:
    
    - helper, this can be an issue on module removal.
    - timeout policy, nfnetlink_cttimeout might remove it.
    
    The use of templates with zone and event cache filter are safe, since
    this just copies values.
    
    Flush these enqueued packets in case the template rule gets removed.
    
    Fixes: 24de58f46516 ("netfilter: xt_CT: allow to attach timeout policy + glue code")
    Reported-by: Yiming Qian <yimingqian591@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit cc57506dd66555899560b9c0f24e813f034e12ec)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- net/netfilter/xt_CT.c
+++ net/netfilter/xt_CT.c
@@ -16,6 +16,7 @@
 #include <net/netfilter/nf_conntrack_ecache.h>
 #include <net/netfilter/nf_conntrack_timeout.h>
 #include <net/netfilter/nf_conntrack_zones.h>
+#include "nf_internals.h"
 
 static inline int xt_ct_target(struct sk_buff *skb, struct nf_conn *ct)
 {
@@ -269,6 +270,9 @@ static void xt_ct_tg_destroy(const struct xt_tgdtor_param *par,
 	struct nf_conn_help *help;
 
 	if (ct) {
+		if (info->helper[0] || info->timeout[0])
+			nf_queue_nf_hook_drop(par->net);
+
 		help = nfct_help(ct);
 		if (help)
 			nf_conntrack_helper_put(help->helper);
