697cbbed4ea04264e34b6859b371c3512e0795b1
Stephen Brennan <stephen.s.brennan@oracle.com>
2026-03-20 16:45:53 -0700

    perf report: Add comm_nodigit sort key
    The "comm" column allows grouping events by the process command. It is
    intended to group like programs, despite having different PIDs. But some
    workloads may adjust their own command, so that a unique identifier
    (e.g. a PID or some other numeric value) is part of the command name.
    This destroys the utility of "comm", forcing perf to place each unique
    process name into its own bucket, which can contribute to a
    combinatorial explosion of memory use in perf report.
    
    Create a less strict version of this column, which ignores digits when
    comparing command names. Commands whose names are the same (ignoring
    digits) are sorted into the same histogram buckets, and displayed with
    the placeholder value "<N>" in the place of digits. For example,
    hypothetical command names "kworker/1" "kworker/2" "kworker/3" would
    sort into the same bucket and be represented as "kworker/<N>".
    
    Committer testing:
    
      $ perf report -s comm,comm_nodigit | grep -F "<N>"
           0.01%  CPU 6/TCG        CPU <N>/TCG
           0.01%  kworker/53:2-mm  kworker/<N>:<N>-mm
           0.01%  migration/24     migration/<N>
           0.01%  kworker/24:1-ev  kworker/<N>:<N>-ev
           0.01%  llvmpipe-8       llvmpipe-<N>
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    (cherry picked from commit e397dd81bc45a991c43a97e010aa3fbe72ac833b)
    
    Orabug: 39327019
    Conflicts:
    - tools/perf/Documentation/perf-report.txt - trivial
    - tools/perf/util/sort.h - trivial
    - tools/perf/util/sort.c - update sort key logic due to commit
      cbd41c6d4c26c ("perf report: Update sort key state from -F option")
      missing in this tree
    
    Reviewed-by: Mark Tinguely <mark.tinguely@oracle.com>
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- tools/perf/util/hist.h
+++ tools/perf/util/hist.h
@@ -37,6 +37,7 @@ enum hist_column {
 	HISTC_DSO,
 	HISTC_THREAD,
 	HISTC_COMM,
+	HISTC_COMM_NODIGIT,
 	HISTC_CGROUP_ID,
 	HISTC_CGROUP,
 	HISTC_PARENT,
@@ -301,6 +302,7 @@ struct perf_hpp_list {
 	int socket;
 	int thread;
 	int comm;
+	int comm_nodigit;
 };
 
 extern struct perf_hpp_list perf_hpp_list;
