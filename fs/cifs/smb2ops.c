9ba34b9c8e87148e3847816f4a2bf8fda5f1635b
Shuhao Fu <sfual@cse.ust.hk>
2026-03-31 15:00:54 +0800

    smb: client: Fix refcount leak for cifs_sb_tlink
    [ Upstream commit c2b77f42205ef485a647f62082c442c1cd69d3fc ]
    
    Fix three refcount inconsistency issues related to `cifs_sb_tlink`.
    
    Comments for `cifs_sb_tlink` state that `cifs_put_tlink()` needs to be
    called after successful calls to `cifs_sb_tlink()`. Three calls fail to
    update refcount accordingly, leading to possible resource leaks.
    
    Fixes: 8ceb98437946 ("CIFS: Move rename to ops struct")
    Fixes: 2f1afe25997f ("cifs: Use smb 2 - 3 and cifsacl mount options getacl functions")
    Fixes: 366ed846df60 ("cifs: Use smb 2 - 3 and cifsacl mount options setacl function")
    Cc: stable@vger.kernel.org
    Signed-off-by: Shuhao Fu <sfual@cse.ust.hk>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Johnny Hao <johnny_haocn@sina.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit d3c8ea197055c260119a13360e8202a27e53e1e4)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- fs/cifs/smb2ops.c
+++ fs/cifs/smb2ops.c
@@ -3437,8 +3437,7 @@ get_smb2_acl_by_path(struct cifs_sb_info *cifs_sb,
 	utf16_path = cifs_convert_path_to_utf16(path, cifs_sb);
 	if (!utf16_path) {
 		rc = -ENOMEM;
-		free_xid(xid);
-		return ERR_PTR(rc);
+		goto put_tlink;
 	}
 
 	oparms.tcon = tcon;
@@ -3466,6 +3465,7 @@ get_smb2_acl_by_path(struct cifs_sb_info *cifs_sb,
 		SMB2_close(xid, tcon, fid.persistent_fid, fid.volatile_fid);
 	}
 
+put_tlink:
 	cifs_put_tlink(tlink);
 	free_xid(xid);
 
@@ -3506,8 +3506,7 @@ set_smb2_acl(struct cifs_ntsd *pnntsd, __u32 acllen,
 	utf16_path = cifs_convert_path_to_utf16(path, cifs_sb);
 	if (!utf16_path) {
 		rc = -ENOMEM;
-		free_xid(xid);
-		return rc;
+		goto put_tlink;
 	}
 
 	oparms.tcon = tcon;
@@ -3527,6 +3526,7 @@ set_smb2_acl(struct cifs_ntsd *pnntsd, __u32 acllen,
 		SMB2_close(xid, tcon, fid.persistent_fid, fid.volatile_fid);
 	}
 
+put_tlink:
 	cifs_put_tlink(tlink);
 	free_xid(xid);
 	return rc;
