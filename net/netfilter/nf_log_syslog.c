19afc303f685d96405e3a827913783c0c16728f3
Zhengchuan Liang <zcliangcn@gmail.com>
2026-04-04 17:39:48 +0800

    netfilter: require Ethernet MAC header before using eth_hdr()
    [ Upstream commit 62443dc21114c0bbc476fa62973db89743f2f137 ]
    
    `ip6t_eui64`, `xt_mac`, the `bitmap:ip,mac`, `hash:ip,mac`, and
    `hash:mac` ipset types, and `nf_log_syslog` access `eth_hdr(skb)`
    after either assuming that the skb is associated with an Ethernet
    device or checking only that the `ETH_HLEN` bytes at
    `skb_mac_header(skb)` lie between `skb->head` and `skb->data`.
    
    Make these paths first verify that the skb is associated with an
    Ethernet device, that the MAC header was set, and that it spans at
    least a full Ethernet header before accessing `eth_hdr(skb)`.
    
    Suggested-by: Florian Westphal <fw@strlen.de>
    Tested-by: Ren Wei <enjou1224z@gmail.com>
    Signed-off-by: Zhengchuan Liang <zcliangcn@gmail.com>
    Signed-off-by: Ren Wei <n05ec@lzu.edu.cn>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 4435888e1bf139d2bfe5911643d4217382136743)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- net/netfilter/nf_log_syslog.c
+++ net/netfilter/nf_log_syslog.c
@@ -72,7 +72,10 @@ dump_arp_packet(struct nf_log_buf *m,
 	else
 		logflags = NF_LOG_DEFAULT_MASK;
 
-	if (logflags & NF_LOG_MACDECODE) {
+	if ((logflags & NF_LOG_MACDECODE) &&
+	    skb->dev && skb->dev->type == ARPHRD_ETHER &&
+	    skb_mac_header_was_set(skb) &&
+	    skb_mac_header_len(skb) >= ETH_HLEN) {
 		nf_log_buf_add(m, "MACSRC=%pM MACDST=%pM ",
 			       eth_hdr(skb)->h_source, eth_hdr(skb)->h_dest);
 		nf_log_dump_vlan(m, skb);
@@ -781,6 +784,9 @@ static void dump_ipv4_mac_header(struct nf_log_buf *m,
 
 	switch (dev->type) {
 	case ARPHRD_ETHER:
+		if (!skb_mac_header_was_set(skb) || skb_mac_header_len(skb) < ETH_HLEN)
+			return;
+
 		nf_log_buf_add(m, "MACSRC=%pM MACDST=%pM ",
 			       eth_hdr(skb)->h_source, eth_hdr(skb)->h_dest);
 		nf_log_dump_vlan(m, skb);
