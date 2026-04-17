b31a13aed8f27e05b40db7a85dcfba6a8be9ac0b
Prathyushi Nangia <prathyushi.nangia@amd.com>
2026-04-17 13:58:07 +0000

    x86/CPU/AMD: Add a fix for AMD-SB-7052
    Fixup tabbing while at it.
    
    Signed-off-by: Prathyushi Nangia <prathyushi.nangia@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Cc: stable@vger.kernel.org
    
    Orabug: 39344560
    CVE: CVE-2025-54518
    
    Conflicts:
    	arch/x86/kernel/cpu/amd.c - conflic in init_amd_zen2 due
    	to UEK8 specific commit b8a4a6a6bf "x86/amd: Disable IBS
    	on Rome processors due to erratum 1215"
    
    Signed-off-by: Karolina Stolarek <karolina.stolarek@oracle.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Signed-off-by: Samasth Norway Ananda <samasth.norway.ananda@oracle.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1024,6 +1024,10 @@ static void init_amd_zen2(struct cpuinfo_x86 *c)
 		clear_cpu_cap(c, X86_FEATURE_IBS);
 		pr_warn_once(FW_BUG "Erratum 1215 present, disabling IBS");
 	}
+
+	if (!cpu_has(c, X86_FEATURE_HYPERVISOR))
+		msr_set_bit(MSR_ZEN4_BP_CFG, MSR_ZEN2_BP_CFG_BUG_FIX_BIT);
+
 	/* Disable RDSEED on AMD Cyan Skillfish because of an error. */
 	if (c->x86_model == 0x47 && c->x86_stepping == 0x0) {
 		clear_cpu_cap(c, X86_FEATURE_RDSEED);
