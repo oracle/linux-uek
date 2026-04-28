ebd64300e9f09c7113f27de36c6ecb494fa878ae
Herbert Xu <herbert@gondor.apana.org.au>
2026-04-28 13:07:58 +0800

    padata: Fix pd UAF once and for all
    [ Upstream commit 71203f68c7749609d7fc8ae6ad054bdedeb24f91 ]
    
    There is a race condition/UAF in padata_reorder that goes back
    to the initial commit.  A reference count is taken at the start
    of the process in padata_do_parallel, and released at the end in
    padata_serial_worker.
    
    This reference count is (and only is) required for padata_replace
    to function correctly.  If padata_replace is never called then
    there is no issue.
    
    In the function padata_reorder which serves as the core of padata,
    as soon as padata is added to queue->serial.list, and the associated
    spin lock released, that padata may be processed and the reference
    count on pd would go away.
    
    Fix this by getting the next padata before the squeue->serial lock
    is released.
    
    In order to make this possible, simplify padata_reorder by only
    calling it once the next padata arrives.
    
    Fixes: 16295bec6398 ("padata: Generic parallelization/serialization interface")
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    [ Adjust context of padata_find_next(). Replace
    cpumask_next_wrap(cpu, pd->cpumask.pcpu) with
    cpumask_next_wrap(cpu, pd->cpumask.pcpu, -1, false) in padata_reorder() in
    v5.15 according to dc5bb9b769c9 ("cpumask: deprecate cpumask_next_wrap()") and
    f954a2d37637 ("padata: switch padata_find_next() to using cpumask_next_wrap()")
    . ]
    Signed-off-by: Bin Lan <lanbincn@139.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 73f132e60857038416540c3599b1de6033d7575a)
    Signed-off-by: Samasth Norway Ananda <samasth.norway.ananda@oracle.com>

--- include/linux/padata.h
+++ include/linux/padata.h
@@ -92,7 +92,6 @@ struct padata_cpumask {
  * @cpu: Next CPU to be processed.
  * @cpumask: The cpumasks in use for parallel and serial workers.
  * @reorder_work: work struct for reordering.
- * @lock: Reorder lock.
  */
 struct parallel_data {
 	struct padata_shell		*ps;
@@ -103,8 +102,6 @@ struct parallel_data {
 	unsigned int			processed;
 	int				cpu;
 	struct padata_cpumask		cpumask;
-	struct work_struct		reorder_work;
-	spinlock_t                      ____cacheline_aligned lock;
 };
 
 /**
