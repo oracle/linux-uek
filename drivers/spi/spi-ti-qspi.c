8fac5ce0ea853d4843a07a62ca1426a61a45d9e6
Johan Hovold <johan@kernel.org>
2026-05-13 15:34:14 -0400

    spi: ti-qspi: fix controller deregistration
    [ Upstream commit 0c18a1bacbb1d8b8aa34d3d004a2cb8226c8b1ea ]
    
    Make sure to deregister the controller before disabling underlying
    resources like clocks during driver unbind.
    
    Note that the controller is suspended before disabling and releasing
    resources since commit 3ac066e2227c ("spi: spi-ti-qspi: Suspend the
    queue before removing the device") which avoids issues like unclocked
    accesses but prevents SPI device drivers from doing I/O during
    deregistration.
    
    Fixes: 3b3a80019ff1 ("spi: ti-qspi: one only one interrupt handler")
    Cc: stable@vger.kernel.org	# 3.13
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Link: https://patch.msgid.link/20260410081757.503099-24-johan@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    [ renamed spi_controller_* API calls to legacy spi_master_* equivalents and qspi->host to qspi->master ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 83568b18fc85a1f4d4981282f975a894aab2280a)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/spi/spi-ti-qspi.c
+++ drivers/spi/spi-ti-qspi.c
@@ -895,7 +895,7 @@ no_dma:
 	qspi->mmap_enabled = false;
 	qspi->current_cs = -1;
 
-	ret = devm_spi_register_master(&pdev->dev, master);
+	ret = spi_register_master(master);
 	if (!ret)
 		return 0;
 
@@ -910,19 +910,17 @@ free_master:
 static void ti_qspi_remove(struct platform_device *pdev)
 {
 	struct ti_qspi *qspi = platform_get_drvdata(pdev);
-	int rc;
 
-	rc = spi_master_suspend(qspi->master);
-	if (rc) {
-		dev_alert(&pdev->dev, "spi_master_suspend() failed (%pe)\n",
-			  ERR_PTR(rc));
-		return;
-	}
+	spi_master_get(qspi->master);
+
+	spi_unregister_master(qspi->master);
 
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 
 	ti_qspi_dma_cleanup(qspi);
+
+	spi_master_put(qspi->master);
 }
 
 static const struct dev_pm_ops ti_qspi_pm_ops = {
