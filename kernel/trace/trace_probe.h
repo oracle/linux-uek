1b602cd790af7d989b367edbb92ff325e337266c
Steven Rostedt <rostedt@goodmis.org>
2026-05-15 11:20:51 -0400

    tracing/probes: Limit size of event probe to 3K
    [ Upstream commit b2aa3b4d64e460ac606f386c24e7d8a873ce6f1a ]
    
    There currently isn't a max limit an event probe can be. One could make an
    event greater than PAGE_SIZE, which makes the event useless because if
    it's bigger than the max event that can be recorded into the ring buffer,
    then it will never be recorded.
    
    A event probe should never need to be greater than 3K, so make that the
    max size. As long as the max is less than the max that can be recorded
    onto the ring buffer, it should be fine.
    
    Cc: stable@vger.kernel.org
    Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Acked-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Fixes: 93ccae7a22274 ("tracing/kprobes: Support basic types on dynamic events")
    Link: https://patch.msgid.link/20260428122302.706610ba@gandalf.local.home
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    [ adjusted context for missing later-kernel infrastructure and used `goto out` instead of `goto fail` ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 70cc336f5b58d0e7a051a9bb7efb18a260b3dc02)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- kernel/trace/trace_probe.h
+++ kernel/trace/trace_probe.h
@@ -33,6 +33,7 @@
 #define MAX_ARRAY_LEN		64
 #define MAX_ARG_NAME_LEN	32
 #define MAX_STRING_SIZE		PATH_MAX
+#define MAX_PROBE_EVENT_SIZE	3072
 
 /* Reserved field names */
 #define FIELD_STRING_IP		"__probe_ip"
@@ -455,7 +456,8 @@ extern int traceprobe_define_arg_fields(struct trace_event_call *event_call,
 	C(FAIL_REG_PROBE,	"Failed to register probe event"),\
 	C(DIFF_PROBE_TYPE,	"Probe type is different from existing probe"),\
 	C(DIFF_ARG_TYPE,	"Argument type or name is different from existing probe"),\
-	C(SAME_PROBE,		"There is already the exact same probe event"),
+	C(SAME_PROBE,		"There is already the exact same probe event"),\
+	C(EVENT_TOO_BIG,	"Event too big (too many fields?)"),
 
 #undef C
 #define C(a, b)		TP_ERR_##a
