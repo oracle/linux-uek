c3bb11c6ea1c47447be6831fd7f1dbd2a465778c
Jann Horn <jannh@google.com>
2026-05-19 16:29:38 +0200

    fuse: reject fuse_notify() pagecache ops on directories
    commit 9c954499d43aefac01c5dfb57a82b13d2dcf4b94 upstream.
    
    The operations FUSE_NOTIFY_STORE and FUSE_NOTIFY_RETRIEVE allow the
    FUSE daemon to actively write/read pagecache contents.
    
    For directories with FOPEN_CACHE_DIR, the pagecache is used as
    kernel-internal cache storage, and userspace is not supposed to have
    direct access to this cache - in particular, fuse_parse_cache() will hit
    WARN_ON() if the cache contains bogus data.
    
    Reject FUSE_NOTIFY_STORE and FUSE_NOTIFY_RETRIEVE on anything other than
    regular files with -EINVAL.
    
    Fixes: 5d7bc7e8680c ("fuse: allow using readdir cache")
    Cc: stable@vger.kernel.org
    Signed-off-by: Jann Horn <jannh@google.com>
    Link: https://patch.msgid.link/20260519-fuse-dir-pagecache-v2-1-5428fa48e175@google.com
    Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit bd23fa0c16c5c86e5b7713224ffbb87d9db81cca)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -1599,6 +1599,10 @@ static int fuse_notify_store(struct fuse_conn *fc, unsigned int size,
 	inode = fuse_ilookup(fc, nodeid,  NULL);
 	if (!inode)
 		goto out_up_killsb;
+	if (!S_ISREG(inode->i_mode)) {
+		err = -EINVAL;
+		goto out_iput;
+	}
 
 	mapping = inode->i_mapping;
 	index = outarg.offset >> PAGE_SHIFT;
@@ -1770,7 +1774,10 @@ static int fuse_notify_retrieve(struct fuse_conn *fc, unsigned int size,
 
 	inode = fuse_ilookup(fc, nodeid, &fm);
 	if (inode) {
-		err = fuse_retrieve(fm, inode, &outarg);
+		if (!S_ISREG(inode->i_mode))
+			err = -EINVAL;
+		else
+			err = fuse_retrieve(fm, inode, &outarg);
 		iput(inode);
 	}
 	up_read(&fc->killsb);
