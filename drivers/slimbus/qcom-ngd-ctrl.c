6e8ac6eebf4af0d6fb5d00ecbf1fb62ce2f251c4
Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
2026-05-30 21:44:21 +0100

    slimbus: qcom-ngd-ctrl: Avoid ABBA on tx_lock/ctrl->lock
    commit 55f2ea9ff83cc27a85526b14bc9b32f96a08d6ec upstream.
    
    During the SSR/PDR down notification the tx_lock is taken with the
    intent to provide synchronization with active DMA transfers.
    
    But during this period qcom_slim_ngd_down() is invoked, which ends up in
    slim_report_absent(), which takes the slim_controller lock. In multiple
    other codepaths these two locks are taken in the opposite order (i.e.
    slim_controller then tx_lock).
    
    The result is a lockdep splat, and a possible deadlock:
    
      rprocctl/449 is trying to acquire lock:
      ffff00009793e620 (&ctrl->lock){+.+.}-{4:4}, at: slim_report_absent (drivers/slimbus/core.c:322) slimbus
    
      but task is already holding lock:
      ffff00009793fb50 (&ctrl->tx_lock){+.+.}-{4:4}, at: qcom_slim_ngd_ssr_pdr_notify (drivers/slimbus/qcom-ngd-ctrl.c:1475) slim_qcom_ngd_ctrl
    
      which lock already depends on the new lock.
    
      Possible unsafe locking scenario:
    
            CPU0                    CPU1
            ----                    ----
       lock(&ctrl->tx_lock);
                                    lock(&ctrl->lock);
                                    lock(&ctrl->tx_lock);
       lock(&ctrl->lock);
    
    The assumption is that the comment refers to the desire to not call
    qcom_slim_ngd_exit_dma() while we have an ongoing DMA TX transaction.
    But any such transaction is initiated and completed within a single
    qcom_slim_ngd_xfer_msg().
    
    Prior to calling qcom_slim_ngd_exit_dma() the slim_controller is torn
    down, all child devices are notified that the slimbus is gone and the
    child devices are removed.
    
    Stop taking the tx_lock in qcom_slim_ngd_ssr_pdr_notify() to avoid the
    deadlock.
    
    Fixes: a899d324863a ("slimbus: qcom-ngd-ctrl: add Sub System Restart support")
    Cc: stable@vger.kernel.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@oss.qualcomm.com>
    Signed-off-by: Srinivas Kandagatla <srini@kernel.org>
    Link: https://patch.msgid.link/20260530204421.116824-9-srini@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 3d1561537237c6cc1db76155183d8bbdac2339f0)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1394,15 +1394,12 @@ static int qcom_slim_ngd_ssr_pdr_notify(struct qcom_slim_ngd_ctrl *ctrl,
 	switch (action) {
 	case QCOM_SSR_BEFORE_SHUTDOWN:
 	case SERVREG_SERVICE_STATE_DOWN:
-		/* Make sure the last dma xfer is finished */
-		mutex_lock(&ctrl->tx_lock);
 		if (ctrl->state != QCOM_SLIM_NGD_CTRL_DOWN) {
 			pm_runtime_get_noresume(ctrl->ctrl.dev);
 			ctrl->state = QCOM_SLIM_NGD_CTRL_DOWN;
 			qcom_slim_ngd_down(ctrl);
 			qcom_slim_ngd_exit_dma(ctrl);
 		}
-		mutex_unlock(&ctrl->tx_lock);
 		break;
 	case QCOM_SSR_AFTER_POWERUP:
 	case SERVREG_SERVICE_STATE_UP:
