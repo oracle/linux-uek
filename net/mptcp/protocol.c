1d10176399c6bf52f9d2afd65cb926fbdcee9244
Shardul Bankar <shardul.b@mpiricsoftware.com>
2026-05-30 15:47:45 -0400

    mptcp: do not drop partial packets
    [ Upstream commit 50c2d91c5dfa0e465826ec1f8dbad9cdc254bd85 ]
    
    When a packet arrives with map_seq < ack_seq < end_seq, the beginning
    of the packet has already been acknowledged but the end contains new
    data. Currently the entire packet is dropped as "old data," forcing
    the sender to retransmit.
    
    Instead, skip the already-acked bytes by adjusting the skb offset and
    enqueue only the new portion. Update bytes_received and ack_seq to
    reflect the new data consumed.
    
    A previous attempt at this fix has been sent by Paolo Abeni [1], but had
    issues [2]: it also added a zero-window check and changed rcv_wnd_sent
    initialization, which caused test regressions. This version addresses
    only the partial packet handling without modifying receive window
    accounting.
    
    Fixes: ab174ad8ef76 ("mptcp: move ooo skbs into msk out of order queue.")
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/c9b426a4e163aa3c4fe8b80c79f1a610f47ae7d8.1763075056.git.pabeni@redhat.com [1]
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/600 [2]
    Signed-off-by: Shardul Bankar <shardul.b@mpiricsoftware.com>
    [pabeni@redhat.com: update map]
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Signed-off-by: Matthieu Baerts (NGI0) <matttbe@kernel.org>
    Link: https://patch.msgid.link/20260515-net-mptcp-misc-fixes-7-1-rc4-v2-1-701e96419f2f@kernel.org
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    [ dropped `msk->bytes_received += copy_len;` and relocated the `drop:` label to the function end for the existing RCVPRUNED `goto drop;` ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 4649d600cb4a82f58b3a0dc42cfaa5e6b8b2bfd7)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -329,10 +329,26 @@ static bool __mptcp_move_skb(struct mptcp_sock *msk, struct sock *ssk,
 		return false;
 	}
 
-	/* old data, keep it simple and drop the whole pkt, sender
-	 * will retransmit as needed, if needed.
+	/* Completely old data? */
+	if (!after64(MPTCP_SKB_CB(skb)->end_seq, msk->ack_seq)) {
+		MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_DUPDATA);
+		mptcp_drop(sk, skb);
+		return false;
+	}
+
+	/* Partial packet: map_seq < ack_seq < end_seq.
+	 * Skip the already-acked bytes and enqueue the new data.
 	 */
-	MPTCP_INC_STATS(sock_net(sk), MPTCP_MIB_DUPDATA);
+	copy_len = MPTCP_SKB_CB(skb)->end_seq - msk->ack_seq;
+	MPTCP_SKB_CB(skb)->offset += msk->ack_seq - MPTCP_SKB_CB(skb)->map_seq;
+	MPTCP_SKB_CB(skb)->map_seq += msk->ack_seq -
+				      MPTCP_SKB_CB(skb)->map_seq;
+	WRITE_ONCE(msk->ack_seq, msk->ack_seq + copy_len);
+
+	skb_set_owner_r(skb, sk);
+	__skb_queue_tail(&sk->sk_receive_queue, skb);
+	return true;
+
 drop:
 	mptcp_drop(sk, skb);
 	return false;
