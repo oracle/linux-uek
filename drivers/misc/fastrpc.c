524ec78a2e1809cd7e0754ace65c4b969e82c95c
Junrui Luo <moonafterrain@outlook.com>
2026-05-30 21:45:26 +0100

    misc: fastrpc: fix DMA address corruption due to find_vma misuse
    commit 464c6ad2aa16e1e1df9d559289199356493d1e00 upstream.
    
    fastrpc_get_args() uses find_vma() to look up the VMA for a user-provided
    pointer and compute a DMA address offset. When the address falls in a gap
    before the returned VMA, (ptr & PAGE_MASK) - vma->vm_start underflows,
    corrupting the DMA address sent to the DSP.
    
    Replace find_vma() with vma_lookup(), which returns NULL when the address
    is not contained within any VMA.
    
    Cc: stable@vger.kernel.org
    Fixes: 80f3afd72bd4 ("misc: fastrpc: consider address offset before sending to DSP")
    Reported-by: Yuhao Jiang <danisjiang@gmail.com>
    Signed-off-by: Junrui Luo <moonafterrain@outlook.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Srinivas Kandagatla <srini@kernel.org>
    Link: https://patch.msgid.link/20260530204528.116920-3-srini@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 2d0f47e27c1fa718b29c69aa7c96a2c5161bc2c2)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -875,7 +875,7 @@ static int fastrpc_get_args(u32 kernel, struct fastrpc_invoke_ctx *ctx)
 			pages[i].addr = ctx->maps[i]->phys;
 
 			mmap_read_lock(current->mm);
-			vma = find_vma(current->mm, ctx->args[i].ptr);
+			vma = vma_lookup(current->mm, ctx->args[i].ptr);
 			if (vma)
 				pages[i].addr += (ctx->args[i].ptr & PAGE_MASK) -
 						 vma->vm_start;
