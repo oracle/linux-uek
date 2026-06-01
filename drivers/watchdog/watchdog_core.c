2e4145ee407f44ee263ffd342f137d71575ac037
Yuho Choi <dbgh9129@gmail.com>
2026-06-01 15:20:05 -0400

    watchdog: unregister PM notifier on watchdog unregister
    [ Upstream commit a298c7302ee9584a7a1ac1e8acbede8d98ab51a4 ]
    
    watchdog_register_device() registers wdd->pm_nb when
    WDOG_NO_PING_ON_SUSPEND is set, but watchdog_unregister_device() does not
    remove it. This leaves an embedded notifier block on the PM notifier chain
    after the watchdog device has been unregistered.
    
    A later suspend/resume notification can then call watchdog_pm_notifier()
    with a stale watchdog_device pointer, or at minimum after wdd->wd_data has
    been cleared by watchdog_dev_unregister().
    
    Unregister the PM notifier before tearing down the watchdog device.
    
    Fixes: 60bcd91aafd2 ("watchdog: introduce watchdog_dev_suspend/resume")
    Signed-off-by: Yuho Choi <dbgh9129@gmail.com>
    Link: https://lore.kernel.org/r/20260601192005.1970805-1-dbgh9129@gmail.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit cd2d1b1f99308f7680d75a377daaff363f1cc736)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>
    
    Conflicts:
    	drivers/watchdog/watchdog_core.c
    Context difference due to commit from Bug Number 36725601
    dd7726b9d2d0 ("drivers: watchdog: Add support for panic notifier
    callback")

--- drivers/watchdog/watchdog_core.c
+++ drivers/watchdog/watchdog_core.c
@@ -408,6 +408,10 @@ static void __watchdog_unregister_device(struct watchdog_device *wdd)
 	if (test_bit(WDOG_STOP_ON_PANIC, &wdd->status))
 		atomic_notifier_chain_unregister(&panic_notifier_list,
 						 &wdd->panic_nb);
+
+	if (test_bit(WDOG_NO_PING_ON_SUSPEND, &wdd->status))
+		unregister_pm_notifier(&wdd->pm_nb);
+
 	watchdog_dev_unregister(wdd);
 	ida_simple_remove(&watchdog_ida, wdd->id);
 }
