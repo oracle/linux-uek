f0f31dfe1bb07f21c91be8e107fa440193379cfa
Til Kaiser <mail@tk154.de>
2026-06-07 15:49:40 +0200

    net: mvpp2: sync RX data at the hardware packet offset
    [ Upstream commit 180235600934bef6add3be637c296d6cf3272e67 ]
    
    mvpp2 programs the RX queue packet offset, so hardware writes received
    data at dma_addr + MVPP2_SKB_HEADROOM. The current CPU sync starts at
    dma_addr and only covers rx_bytes + MVPP2_MH_SIZE bytes, which syncs the
    unused headroom and misses the same number of bytes at the packet tail.
    
    On non-coherent DMA systems this can leave the CPU reading stale cache
    contents for the end of the received frame.
    
    Use dma_sync_single_range_for_cpu() with MVPP2_SKB_HEADROOM as the range
    offset so the sync covers the Marvell header and packet data actually
    written by hardware.
    
    Fixes: e1921168bbd4 ("mvpp2: sync only the received frame")
    Signed-off-by: Til Kaiser <mail@tk154.de>
    Link: https://patch.msgid.link/20260607134943.21996-2-mail@tk154.de
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 19f8bc139e9b149d1e5bf75ae761d1bb8dd3e7d8)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
+++ drivers/net/ethernet/marvell/mvpp2/mvpp2_main.c
@@ -3945,9 +3945,10 @@ static int mvpp2_rx(struct mvpp2_port *port, struct napi_struct *napi,
 			dma_dir = DMA_FROM_DEVICE;
 		}
 
-		dma_sync_single_for_cpu(dev->dev.parent, dma_addr,
-					rx_bytes + MVPP2_MH_SIZE,
-					dma_dir);
+		dma_sync_single_range_for_cpu(dev->dev.parent, dma_addr,
+					      MVPP2_SKB_HEADROOM,
+					      rx_bytes + MVPP2_MH_SIZE,
+					      dma_dir);
 
 		/* Buffer header not supported */
 		if (rx_status & MVPP2_RXD_BUF_HDR)
