8cf61e73409db948cbbfc7dc424b00348feb8988
Yin Tirui <yintirui@huawei.com>
2026-06-15 21:35:03 -0400

    mm/huge_memory: update file PMD counter before folio_put()
    [ Upstream commit 8d878059924f12c1bc24556a92ec56add74de3c8 ]
    
    __split_huge_pmd_locked() updates the file/shmem RSS counter after
    dropping the PMD mapping's folio reference.  If folio_put() drops the last
    reference, mm_counter_file() can later read freed folio state via
    folio_test_swapbacked().
    
    Move the counter update before folio_put().
    
    Link: https://lore.kernel.org/20260526101337.1984081-1-yintirui@huawei.com
    Fixes: fadae2953072 ("thp: use mm_file_counter to determine update which rss counter")
    Signed-off-by: Yin Tirui <yintirui@huawei.com>
    Reviewed-by: Lorenzo Stoakes <ljs@kernel.org>
    Acked-by: David Hildenbrand (arm) <david@kernel.org>
    Reviewed-by: Lance Yang <lance.yang@linux.dev>
    Reviewed-by: Dev Jain <dev.jain@arm.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Chen Jun <chenjun102@huawei.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Nico Pache <npache@redhat.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Yang Shi <yang.shi@linux.alibaba.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 108963978a681c0c468d279cac2b930c27672877)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2108,7 +2108,9 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 			if (!PageReferenced(page) && pmd_young(old_pmd))
 				SetPageReferenced(page);
 			page_remove_rmap(page, true);
+			add_mm_counter(mm, mm_counter_file(page), -HPAGE_PMD_NR);
 			put_page(page);
+			return;
 		}
 		add_mm_counter(mm, mm_counter_file(page), -HPAGE_PMD_NR);
 		return;
