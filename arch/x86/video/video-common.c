ce77f54dd526bdb57b6fadf8537e11da45246292
Mario Limonciello <mario.limonciello@amd.com>
2026-06-23 07:15:05 -0700

    x86/video: Only fall back to vga_default_device() without screen info
    commit 596b3678326d3d1aed7c19423b6746f1ce09688a upstream.
    
    Some multi GPU systems may have a VGA compatible device, but that might not be
    used for display.  If due to enumeration order this device is found before the
    one actually used for display then multiple devices may show the boot_display
    attribute, confusing userspace.
    
    When screen info is valid, use it exclusively to find the primary device so
    that only the device backing the framebuffer is reported.  Only when no
    framebuffer has been set up does it make sense to fall back to the default VGA
    device.  This ensures at most one primary graphics device, preferably the one
    with the framebuffer.
    
    Fixes: ad90860bd10ee ("fbcon: Use screen info to find primary device")
    Closes: https://lore.kernel.org/linux-pci/20260618081803.2790848-1-aaron.ma@canonical.com/#t
    Reported-by: Aaron Ma <aaron.ma@canonical.com>
    Suggested-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Tested-by: Aaron Ma <aaron.ma@canonical.com>
    Cc: <stable@kernel.org>
    Link: https://patch.msgid.link/20260623141505.1816786-1-mario.limonciello@amd.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/video/video-common.c
+++ arch/x86/video/video-common.c
@@ -43,11 +43,9 @@ bool video_is_primary_device(struct device *dev)
 	if (!pci_is_display(pdev))
 		return false;
 
-	if (pdev == vga_default_device())
-		return true;
-
 #ifdef CONFIG_SCREEN_INFO
 	numres = screen_info_resources(si, res, ARRAY_SIZE(res));
+	if (numres > 0) {
 	for (i = 0; i < numres; ++i) {
 		if (!(res[i].flags & IORESOURCE_MEM))
 			continue;
@@ -55,9 +53,16 @@ bool video_is_primary_device(struct device *dev)
 		if (pci_find_resource(pdev, &res[i]))
 			return true;
 	}
-#endif
 
 	return false;
+}
+#endif
+
+	/*
+	 * No framebuffer was set up by the firmware/bootloader, so fall back
+	 * to the default VGA device.
+	 */
+	return pdev == vga_default_device();
 }
 EXPORT_SYMBOL(video_is_primary_device);
 
