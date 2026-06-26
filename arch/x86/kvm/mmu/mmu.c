f293f70b8f7fc2baaf14427a67d8356b441417c6
David Matlack <dmatlack@google.com>
2026-06-26 13:26:22 +0200

    KVM: x86/mmu: Use a bool for direct
    commit 27a59d57f073f21f029df1517c2c0a1abea5b0ce upstream.
    
    The parameter "direct" can either be true or false, and all of the
    callers pass in a bool variable or true/false literal, so just use the
    type bool.
    
    No functional change intended.
    
    Reviewed-by: Lai Jiangshan <jiangshanlai@gmail.com>
    Reviewed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Message-Id: <20220516232138.1783324-3-dmatlack@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    [gregory.herrero@oracle.com: Resolve the remaining UEK6 context by
    changing kvm_mmu_get_page()'s direct argument from int to bool.]
    
    Orabug: 39673871
    
    Signed-off-by: Gregory Herrero <gregory.herrero@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Dongli Zhang <dongli.zhang@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -2171,7 +2171,7 @@ static void drop_parent_pte(struct kvm_mmu_page *sp,
 	mmu_spte_clear_no_track(parent_pte);
 }
 
-static struct kvm_mmu_page *kvm_mmu_alloc_page(struct kvm_vcpu *vcpu, int direct)
+static struct kvm_mmu_page *kvm_mmu_alloc_page(struct kvm_vcpu *vcpu, bool direct)
 {
 	struct kvm_mmu_page *sp;
 
@@ -2536,7 +2536,7 @@ static struct kvm_mmu_page *kvm_mmu_get_page(struct kvm_vcpu *vcpu,
 					     gfn_t gfn,
 					     gva_t gaddr,
 					     unsigned level,
-					     int direct,
+					     bool direct,
 					     unsigned int access)
 {
 	union kvm_mmu_page_role role;
