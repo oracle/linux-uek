9a8d29fed2539c4021e41aadd628c35aebdfb6d2
Imran Khan <imran.f.khan@oracle.com>
2026-07-09 17:17:08 +0800

    drm/amdkfd: fix invalid GTT pointer in DQM cleanup
    The backport of upstream commit 80614c509810 ("drm/amdkfd: fix a memory leak
    in device_queue_manager_init()") passes &mqd->gtt_mem to
    amdgpu_amdkfd_free_gtt_mem(), like the upstream commit does.
    
    But in upstream, amdgpu_amdkfd_free_gtt_mem() takes a void ** and uses
    it as struct amdgpu_bo **, while in v5.15.y amdgpu_amdkfd_free_gtt_mem()
    still takes a void * and casts it to struct amdgpu_bo *.
    
    So with the above mentioned backport address of the mqd->gtt_mem field
    is wrongly interpreted as amdgpu_bo * in amdgpu_amdkfd_free_gtt_mem()
    and this causes NULL pointer dereference in the clean up path taken
    due to KFD initialization failure.
    
    A sample call stack leading to NULL pointer deref has been shown below:
    
    [  896.238134] kfd kfd: amdgpu: error getting iommu info. is the iommu enabled?
    [  896.238143] kfd kfd: amdgpu: Error initializing iommuv2
    [  896.238154] BUG: kernel NULL pointer dereference, address: 0000000000000000
    [  896.238163] #PF: supervisor write access in kernel mode
    [  896.238173] #PF: error_code(0x0002) - not-present page
    [  896.238182] PGD 0 P4D 0
    [  896.238200] Oops: 0002 [#1] SMP NOPTI
    [  896.238212] CPU: 0 PID: 2813 Comm: modprobe Kdump: loaded Tainted: G        W           5.15.0-320.202.8.4.el8uek.x86_64 #2
    [  896.238226] Hardware name: HP HP t540 Thin Client/873A, BIOS M44 v01.03 04/22/2021
    [  896.238233] RIP: 0010:ww_mutex_lock+0x20/0x8a
    [  896.238249] Code: 1f 84 00 00 00 00 00 0f 1f 00 0f 1f 44 00 00 41 55 41 54 49 89 f4 55 48 89 fd e8 db e4 ff ff 31 c0 65 48 8b 14 25 40 fc 01 00 <f0> 48 0f b1 55 00 75 2f 4d 85 e4 74 18 41 83 44 24 10 01 4c 89 65
    [  896.238260] RSP: 0018:ffffd34ac084fa98 EFLAGS: 00010246
    [  896.238272] RAX: 0000000000000000 RBX: 0000000000000000 RCX: 0000000000000000
    [  896.238283] RDX: ffff8ed089708000 RSI: 0000000000000000 RDI: 0000000000000000
    [  896.238293] RBP: 0000000000000000 R08: 0000000000000000 R09: 0000000000000000
    [  896.238304] R10: 0000000000000000 R11: 0000000000000000 R12: 0000000000000000
    [  896.238315] R13: ffff8ed08aa77841 R14: 0000000000000000 R15: ffff8ed0813280d0
    [  896.238328] FS:  00007ff2788b0740(0000) GS:ffff8ed138a00000(0000) knlGS:0000000000000000
    [  896.238338] CS:  0010 DS: 0000 ES: 0000 CR0: 0000000080050033
    [  896.238347] CR2: 0000000000000000 CR3: 00000001034d4000 CR4: 00000000003506f0
    [  896.238356] Call Trace:
    [  896.238366]  <TASK>
    [  896.238379]  amdgpu_amdkfd_free_gtt_mem+0x38/0xe1 [amdgpu]
    [  896.238794]  device_queue_manager_uninit+0x32/0x50 [amdgpu]
    [  896.238794]  kgd2kfd_device_init.cold+0x20d/0x3fc [amdgpu]
    [  896.238794]  amdgpu_amdkfd_device_init+0x137/0x170 [amdgpu]
    [  896.238794]  amdgpu_device_ip_init+0x3e3/0x411 [amdgpu]
    [  896.238794]  amdgpu_device_init.cold+0x54a/0xa02 [amdgpu]
    ......
    
    Fixes: e11e8a29b304 ("drm/amdkfd: fix a memory leak in device_queue_manager_init()")
    
    Orabug: 39605741
    Signed-off-by: Imran Khan <imran.f.khan@oracle.com>
    Reviewed-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_device_queue_manager.c
@@ -1852,7 +1852,7 @@ static void deallocate_hiq_sdma_mqd(struct kfd_dev *dev,
 {
 	WARN(!mqd, "No hiq sdma mqd trunk to free");
 
-	amdgpu_amdkfd_free_gtt_mem(dev->kgd, &mqd->gtt_mem);
+	amdgpu_amdkfd_free_gtt_mem(dev->kgd, mqd->gtt_mem);
 }
 
 struct device_queue_manager *device_queue_manager_init(struct kfd_dev *dev)
