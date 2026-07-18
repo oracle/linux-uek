c84be558943240adaf96ec884297a9c896b5a48e
Andrii Nakryiko <andrii@kernel.org>
2026-07-18 17:50:20 -0400

    bpf: Consistently use bpf_rcu_lock_held() everywhere
    [ Upstream commit 48a97ffc6c826640907d13b199e29008f4fe2c15 ]
    
    We have many places which open-code what's now is bpf_rcu_lock_held()
    macro, so replace all those places with a clean and short macro invocation.
    For that, move bpf_rcu_lock_held() macro into include/linux/bpf.h.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/bpf/20251014201403.4104511-1-andrii@kernel.org
    Stable-dep-of: 2f884d371faf ("bpf: Allow LPM map access from sleepable BPF programs")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit d841f14eeba341c0ae3f8c554a538519c5166350)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>
    
    Conflicts:
    	include/linux/bpf.h
    Context difference due to inclusion of KABI header file

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -23,6 +23,7 @@
 #include <linux/slab.h>
 #include <linux/percpu-refcount.h>
 #include <linux/bpfptr.h>
+#include <linux/rcupdate_trace.h>
 #include <linux/uek_kabi.h>
 
 struct bpf_verifier_env;
@@ -1527,6 +1528,9 @@ out:
 			_ret = (_cn ? NET_XMIT_DROP : -EPERM);		\
 		_ret;					\
 	})
+
+#define bpf_rcu_lock_held() \
+	(rcu_read_lock_held() || rcu_read_lock_trace_held() || rcu_read_lock_bh_held())
 
 #ifdef CONFIG_BPF_SYSCALL
 DECLARE_PER_CPU(int, bpf_prog_active);
