973acdc8f19e57988231ad52d739efbd55f14436
Marc Zyngier <maz@kernel.org>
2026-07-20 16:52:42 -0700

    KVM: Move wiping of the kvm->vcpus array to common code
    All architectures have similar loops iterating over the vcpus,
    freeing one vcpu at a time, and eventually wiping the reference
    off the vcpus array. They are also inconsistently taking
    the kvm->lock mutex when wiping the references from the array.
    
    Make this code common, which will simplify further changes.
    The locking is dropped altogether, as this should only be called
    when there is no further references on the kvm structure.
    
    Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Message-Id: <20211116160403.4074052-2-maz@kernel.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit 27592ae8dbe41033261b6fdf27d78998aabd2665)
    Conflicts:
      - No RISC-V KVM in UEK7.
      - Context conflicts at kvm_vcpu_destroy(). The order of
        kvm_dirty_ring_free() and kvm_arch_vcpu_destroy() has been changed due
        to the UEK7 commit 34724990d0ec ("KVM: avoid NULL pointer dereference
        in kvm_dirty_ring_push").
      - Only this commit is required, although it is from a big patchset.
        The main idea is to introduce kvm_destroy_vcpus() as prerequisite to
        backport more commits to fix UAF issue.
        https://lore.kernel.org/all/20211116160403.4074052-1-maz@kernel.org
    Orabug: 39651843
    Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
    Reviewed-by: Alejandro Jimenez <alejandro.j.jimenez@oracle.com>
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -887,7 +887,7 @@ static inline int kvm_vcpu_get_idx(struct kvm_vcpu *vcpu)
 	return vcpu->vcpu_idx;
 }
 
-void kvm_vcpu_destroy(struct kvm_vcpu *vcpu);
+void kvm_destroy_vcpus(struct kvm *kvm);
 
 void vcpu_load(struct kvm_vcpu *vcpu);
 void vcpu_put(struct kvm_vcpu *vcpu);
