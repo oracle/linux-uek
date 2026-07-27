8902282b14800dc05cc1fbd5684eee6bdc4f40d7
Abdun Nihaal <nihaal@cse.iitm.ac.in>
2026-07-27 15:32:24 -0400

    scsi: lpfc: Fix memory leak in lpfc_sli4_driver_resource_setup()
    [ Upstream commit 1bd28625e25be549ee7c47532e7c3ef91c682410 ]
    
    The memory allocated for mboxq using mempool_alloc() is not freed in
    some of the early exit error paths. Fix that by moving the
    mempool_free() call to an earlier point after last use.
    
    Fixes: d79c9e9d4b3d ("scsi: lpfc: Support dynamic unbounded SGL lists on G7 hardware.")
    Cc: stable@vger.kernel.org
    Signed-off-by: Abdun Nihaal <nihaal@cse.iitm.ac.in>
    Reviewed-by: Justin Tee <justin.tee@broadcom.com>
    Link: https://patch.msgid.link/20260707065304.949135-1-nihaal@cse.iitm.ac.in
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 7795037f15289ac1ed620603511b67d820a93762)
    Signed-off-by: Vijayendra Suman <vijayendra.suman@oracle.com>

--- drivers/scsi/lpfc/lpfc_init.c
+++ drivers/scsi/lpfc/lpfc_init.c
@@ -8194,6 +8194,7 @@ lpfc_sli4_driver_resource_setup(struct lpfc_hba *phba)
 			goto out_free_bsmbx;
 		}
 	}
+	mempool_free(mboxq, phba->mbox_mem_pool);
 
 	/*
 	 * 1 for cmd, 1 for rsp, NVME adds an extra one
@@ -8318,8 +8319,6 @@ lpfc_sli4_driver_resource_setup(struct lpfc_hba *phba)
 		rc = -ENOMEM;
 		goto out_free_sg_dma_buf;
 	}
-
-	mempool_free(mboxq, phba->mbox_mem_pool);
 
 	/* Verify OAS is supported */
 	lpfc_sli4_oas_verify(phba);
