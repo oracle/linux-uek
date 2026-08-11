bc7938d37d90077be6a97726bb8fdf130c256428
Song Liu <song@kernel.org>
2026-08-11 14:56:16 -0400

    fsnotify, lsm: Decouple fsnotify from lsm
    [ Upstream commit 1cda52f1b4611f4daa9d89e69d9428fb4137dc3f ]
    
    Currently, fsnotify_open_perm() is called from security_file_open().
    This is a a bit unexpected and creates otherwise unnecessary dependency
    of CONFIG_FANOTIFY_ACCESS_PERMISSIONS on CONFIG_SECURITY. Fix this by
    calling fsnotify_open_perm() directly.
    
    Signed-off-by: Song Liu <song@kernel.org>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20241013002248.3984442-1-song@kernel.org
    Stable-dep-of: db1856ea9196 ("binfmt_misc: restore write access when removing an entry")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit b7212a0cc80afc260507aaca7c8391a92e51cfc6)
    Use UEK_KABI_FAKE_INCLUDE so the header remains visible only while generating symbol versions.
    Signed-off-by: Saeed Mirzamohammadi <saeed.mirzamohammadi@oracle.com>

--- security/security.c
+++ security/security.c
@@ -19,7 +19,7 @@
 #include <linux/kernel.h>
 #include <linux/kernel_read_file.h>
 #include <linux/lsm_hooks.h>
-#include <linux/fsnotify.h>
+#include UEK_KABI_FAKE_INCLUDE(<linux/fsnotify.h>)
 #include <linux/mman.h>
 #include <linux/mount.h>
 #include <linux/personality.h>
@@ -3214,13 +3214,7 @@ int security_file_receive(struct file *file)
  */
 int security_file_open(struct file *file)
 {
-	int ret;
-
-	ret = call_int_hook(file_open, file);
-	if (ret)
-		return ret;
-
-	return fsnotify_open_perm(file);
+	return call_int_hook(file_open, file);
 }
 
 /**
